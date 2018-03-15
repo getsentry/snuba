77199968f0b43ef19a9be3143da832fab88e9863
Brett Hoerner <brett@bretthoerner.com>
2018-03-15 09:54:50 -0500

    Add BaseTest.

---
+++ tests/base.py
@@ -0,0 +1,52 @@
+import collections
+
+from clickhouse_driver import Client
+
+from snuba import settings
+
+
+class BaseTest(object):
+    def setup_method(self, test_method):
+        self.table = 'test'
+        self.conn = Client('localhost')
+        self.conn.execute("""
+            CREATE TABLE %(table)s (
+                %(columns)s
+            ) ENGINE = Memory
+        """ % {'table': self.table, 'columns': settings.COLUMNS})
+
+    def teardown_method(self, test_method):
+        self.conn.execute("DROP TABLE %s" % self.table)
+        self.conn.disconnect()
+
+    def _write_events(self, events):
+        if not isinstance(events, collections.Iterable):
+            events = [events]
+
+        rows = []
+        for event in events:
+            row = []
+            for colname in settings.WRITER_COLUMNS:
+                value = event.get(colname, None)
+
+                # Hack to handle default value for array columns
+                if value is None and '.' in colname:
+                    value = []
+
+                row.append(value)
+            rows.append(row)
+
+        return self._write_rows(rows)
+
+    def _write_rows(self, rows):
+        if not isinstance(rows, collections.Iterable):
+            rows = [rows]
+
+        self.conn.execute("""
+            INSERT INTO %(table)s (
+                %(colnames)s
+            ) VALUES
+            """ % {
+                'colnames': ", ".join(settings.WRITER_COLUMNS),
+                'table': self.table,
+            }, rows)
--- tests/test_writer.py
+++ tests/test_writer.py
@@ -1,71 +1,27 @@
-import collections
+import petname
+import random
 import uuid
+import sys
 from datetime import datetime
-import pytest
-from clickhouse_driver import Client
 
-from snuba import settings
+from base import BaseTest
 
 
-class TestWriter(object):
-    def setup_method(self, test_method):
-        self.table = 'test'
-        self.conn = Client('localhost')
-        self.conn.execute("""
-            CREATE TABLE %(table)s (
-                %(columns)s
-            ) ENGINE = Memory
-        """ % {'table': self.table, 'columns': settings.COLUMNS})
-
-    def teardown_method(self, test_method):
-        self.conn.execute("DROP TABLE %s" % self.table)
-        self.conn.disconnect()
-
-    def _write_events(self, events):
-        if not isinstance(events, collections.Iterable):
-            events = [events]
-
-        rows = []
-        for event in events:
-            row = []
-            for colname in settings.WRITER_COLUMNS:
-                value = event.get(colname, None)
-
-                # Hack to handle default value for array columns
-                if value is None and '.' in colname:
-                    value = []
-
-                row.append(value)
-            rows.append(row)
-
-        return self._write_rows(rows)
-
-    def _write_rows(self, rows):
-        if not isinstance(rows, collections.Iterable):
-            rows = [rows]
-
-        self.conn.execute("""
-            INSERT INTO %(table)s (
-                %(colnames)s
-            ) VALUES
-            """ % {
-                'colnames': ", ".join(settings.WRITER_COLUMNS),
-                'table': self.table,
-            }, rows)
-
+class TestWriter(BaseTest):
     def test_foo(self):
         # only the required fields
         events = [{
-            'event_id': 'x' * 32,
+            'event_id': uuid.uuid4().hex,
             'timestamp': datetime.now(),
-            'platform': 'foo',
-            'message': 'foo',
-            'primary_hash': 'x' * 16,
-            'project_id': 1,
+            'platform': petname.generate(),
+            'message': petname.generate(),
+            'primary_hash': uuid.uuid4().hex[:16],
+            'project_id': random.randint(0, sys.maxint),
             'received': datetime.now(),
         }]
 
         self._write_events(events)
 
         res = self.conn.execute("SELECT count() FROM %s" % self.table)
-        print(res)
+
+        assert res[0][0] == 1
