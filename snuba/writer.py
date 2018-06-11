eded9ed114eb32db220c9f541923f99842f34c6d
Brett Hoerner <brett@bretthoerner.com>
2018-06-11 13:06:35 -0500

    Size missing/new Nested columns according to their existing siblings (#74)

--- snuba/writer.py
+++ snuba/writer.py
@@ -21,14 +21,42 @@ def row_from_processed_event(event, columns=settings.WRITER_COLUMNS):
     values = []
     for colname in columns:
         value = event.get(colname, None)
-
-        # Hack to handle default value for array columns
         if value is None and '.' in colname:
-            value = []
-
+            value = _create_missing_array(colname, event)
         values.append(value)
 
     return values
+
+
+def _create_missing_array(colname, event):
+    """
+    ClickHouse `Nested` columns are implemented as arrays and sibling columns
+    must have the same length as one another. The documentation states:
+    > During insertion, the system checks that they have the same length.
+    But as of this writing, this doesn't seem to be true:
+    https://github.com/yandex/ClickHouse/issues/2231
+
+    When a new `Nested` column is added to the schema, it may be missing
+    from pre-existing events in the processed data topic. We need
+    to write arrays of the same length as the sibling columns, so we
+    look at the event for the first sibling column and use its length
+    (since all siblings will have the same length).
+
+    It's important to note that this is (1) not fast and (2) only done
+    temporarily against processed events that are missing the new column.
+    Once the processor is updated and the writer moves on to those new
+    events, this method will not be called.
+    """
+    prefix, _ = colname.split('.', 1)
+    prefix += '.'
+
+    sibling_length = 0
+    for key in event.keys():
+        if key.startswith(prefix):
+            sibling_length = len(event[key])
+            break
+
+    return [None] * sibling_length
 
 
 def write_rows(connection, table, columns, rows, types_check=False):
