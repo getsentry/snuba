3cc8c98cea337a02267325a8d3f7baba2695d6c3
Alex Hofsteede <alex@hofsteede.com>
2018-08-06 16:04:04 -0600

    Support NOT IN condition (#143)

--- snuba/schemas.py
+++ snuba/schemas.py
@@ -199,7 +199,7 @@ QUERY_SCHEMA = {
                     # Operator
                     'type': 'string',
                     # TODO  enforce literal = NULL for unary operators
-                    'enum': ['>', '<', '>=', '<=', '=', '!=', 'IN', 'IS NULL', 'IS NOT NULL', 'LIKE', 'NOT LIKE'],
+                    'enum': ['>', '<', '>=', '<=', '=', '!=', 'IN', 'NOT IN', 'IS NULL', 'IS NOT NULL', 'LIKE', 'NOT LIKE'],
                 }, {
                     # Literal
                     'anyOf': [
