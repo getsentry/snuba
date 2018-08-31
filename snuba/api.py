d39db00fc5779a15e4c926e735ec60fcdd5fba67
alex-hofsteede <alex@hofsteede.com>
2018-08-31 14:02:01 -0700

    add a test that exceptions get captured

--- snuba/api.py
+++ snuba/api.py
@@ -7,6 +7,7 @@ from flask import Flask, render_template, request
 from hashlib import md5
 from markdown import markdown
 import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
 import simplejson as json
 
 from snuba import generalizer, schemas, settings, state, util
@@ -46,7 +47,7 @@ application = Flask(__name__, static_url_path='')
 application.testing = settings.TESTING
 application.debug = settings.DEBUG
 
-sentry_sdk.init(dsn=settings.SENTRY_DSN)
+sentry_sdk.init(dsn=settings.SENTRY_DSN, integrations=[FlaskIntegration()])
 
 
 @application.route('/')
@@ -308,3 +309,7 @@ if application.debug or application.testing:
         clickhouse_rw.execute("DROP TABLE IF EXISTS %s" % settings.CLICKHOUSE_TABLE)
         ensure_table_exists()
         return ('ok', 200, {'Content-Type': 'text/plain'})
+
+    @application.route('/tests/error')
+    def error():
+        1/0
