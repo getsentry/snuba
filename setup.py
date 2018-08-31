d759377244e81a9ca8ac326a796348bb4ea34512
alex-hofsteede <alex@hofsteede.com>
2018-08-31 14:23:36 -0700

    put github dep in setup.py

--- setup.py
+++ setup.py
@@ -15,6 +15,9 @@ setup(
     packages=find_packages(exclude=['tests']),
     zip_safe=False,
     install_requires=install_requires,
+    dependency_links=[
+        'git+https://github.com/getsentry/sentry-python.git#egg=sentry_sdk',
+    ],
     entry_points={
         'console_scripts': [
             'snuba=snuba.cli:main',
