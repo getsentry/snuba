4e06cb55035bbfbfece467671abb2035352fd430
Alex Hofsteede <alex@hofsteede.com>
2018-09-07 14:35:55 -0700

    test: Basic test to see that consumer runs (#170)
    * test: Basic test to see that consumer runs
    
    * check for empty stderr as well
    
    * Revert "check for empty stderr as well"
    
    This reverts commit ff077dd7f30d18e71c0f873937aba68655e2ffdc.

---
+++ tests/test_cli.py
@@ -0,0 +1,17 @@
+import signal
+import subprocess
+import time
+
+class TestCli(object):
+
+    def test_consumer_cli(self):
+        """
+        Check that the consumer daemon runs until it is killed
+        """
+        proc = subprocess.Popen(['snuba', 'consumer'])
+        time.sleep(1)
+        proc.poll()
+        assert proc.returncode is None # still running
+
+        proc.send_signal(signal.SIGINT)
+        proc.wait()
