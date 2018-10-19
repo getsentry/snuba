25cdfa461f4c530d73c54d612ca927f51848fe83
Brett Hoerner <brett@bretthoerner.com>
2018-10-19 10:58:51 -0500

    use_uncompressed_cache=0

--- snuba/cli/replacer.py
+++ snuba/cli/replacer.py
@@ -47,14 +47,16 @@ def replacer(replacements_topic, consumer_group, bootstrap_server, clickhouse_se
         dogstatsd_host, dogstatsd_port, 'snuba.replacer', tags=["group:%s" % consumer_group]
     )
 
-    # Replacing existing rows requires reconstructing the entire tuple for each
-    # event (via a SELECT), which is a Hard Thing (TM) for columnstores to do. With
-    # the default settings it's common for ClickHouse to go over the default max_memory_usage
-    # of 10GB per query. Lowering the max_block_size reduces memory usage, and increasing the
-    # max_memory_usage gives the query more breathing room.
     client_settings = {
+        # Replacing existing rows requires reconstructing the entire tuple for each
+        # event (via a SELECT), which is a Hard Thing (TM) for columnstores to do. With
+        # the default settings it's common for ClickHouse to go over the default max_memory_usage
+        # of 10GB per query. Lowering the max_block_size reduces memory usage, and increasing the
+        # max_memory_usage gives the query more breathing room.
         'max_block_size': settings.REPLACER_MAX_BLOCK_SIZE,
         'max_memory_usage': settings.REPLACER_MAX_MEMORY_USAGE,
+        # Don't use up production cache for the count() queries.
+        'use_uncompressed_cache': 0,
     }
 
     clickhouse = ClickhousePool(
