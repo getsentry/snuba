84d1856a3f2d7ec637a4b2e6e70c524411c4ad16
Brett Hoerner <brett@bretthoerner.com>
2018-10-19 09:39:35 -0500

    no commit log for replacer

--- snuba/cli/replacer.py
+++ snuba/cli/replacer.py
@@ -30,11 +30,9 @@ from snuba import settings
 @click.option('--log-level', default=settings.LOG_LEVEL, help='Logging level to use.')
 @click.option('--dogstatsd-host', default=settings.DOGSTATSD_HOST, help='Host to send DogStatsD metrics to.')
 @click.option('--dogstatsd-port', default=settings.DOGSTATSD_PORT, type=int, help='Port to send DogStatsD metrics to.')
-@click.option('--commit-log-topic', default='snuba-commit-log',
-              help='Topic for committed offsets to be written to, triggering post-processing task(s)')
 def replacer(replacements_topic, consumer_group, bootstrap_server, clickhouse_server, distributed_table_name,
              max_batch_size, max_batch_time_ms, auto_offset_reset, queued_max_messages_kbytes,
-             queued_min_messages, log_level, dogstatsd_host, dogstatsd_port, commit_log_topic):
+             queued_min_messages, log_level, dogstatsd_host, dogstatsd_port):
 
     import sentry_sdk
     from snuba import util
@@ -73,7 +71,8 @@ def replacer(replacements_topic, consumer_group, bootstrap_server, clickhouse_se
         metrics=metrics,
         bootstrap_servers=bootstrap_server,
         group_id=consumer_group,
-        commit_log_topic=commit_log_topic,
+        producer=None,
+        commit_log_topic=None,
         auto_offset_reset=auto_offset_reset,
     )
 
