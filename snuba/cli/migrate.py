69bb31bc119c4f48685a0937e585d725a744ad23
Brett Hoerner <brett@bretthoerner.com>
2018-11-19 09:41:33 -0600

    Add simple local development schema migration tool. (#214)
    * Add simple local development schema migration tool.
    
    This tool only runs on the `dev` table, and can either be run via the
    CLI or it runs automatically on server start.
    
    `group_id` is the first column we've added since people have started
    doing local development against Snuba, it is intended as an
    example/skeleton.
    
    * Warn about schema diffs.

---
+++ snuba/cli/migrate.py
@@ -0,0 +1,26 @@
+import logging
+import sys
+import click
+from clickhouse_driver import Client
+
+from snuba import settings
+
+
+@click.command()
+@click.option('--log-level', default=settings.LOG_LEVEL, help='Logging level to use.')
+def migrate(log_level):
+    from snuba.migrate import logger, run
+
+    logging.basicConfig(level=getattr(logging, log_level.upper()), format='%(asctime)s %(message)s')
+
+    if settings.CLICKHOUSE_TABLE != 'dev':
+        logger.error("The migration tool is only intended for local development environment.")
+        sys.exit(1)
+
+    host, port = settings.CLICKHOUSE_SERVER.split(':')
+    clickhouse = Client(
+        host=host,
+        port=port,
+    )
+
+    run(clickhouse, settings.CLICKHOUSE_TABLE)
