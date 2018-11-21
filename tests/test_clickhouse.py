44c8c25e7a53d3067b422da1dc974646a0335ee0
Brett Hoerner <brett@bretthoerner.com>
2018-11-21 10:24:35 -0600

    Improve on ClickHouse schema modeling
    * Add `Column` and `FlattenedColumn` types so we can do things like:
      `ALL_COLUMNS['exception_frames.in_app'].base_name ==
      'exception_frames'`
    * Stop using `str()` for schema printing as it was confusing, switch to
      `for_schema()` method.
    * Use real `__repr__` and `__eq__` so messing around in REPL makes
      sense.

---
+++ tests/test_clickhouse.py
@@ -0,0 +1,41 @@
+from base import BaseTest
+
+from snuba.clickhouse import (
+    ALL_COLUMNS,
+    Array, ColumnSet, Nested, Nullable, String, UInt,
+    escape_col
+)
+
+
+class TestClickhouse(BaseTest):
+    def test_escape_col(self):
+        assert escape_col(None) is None
+        assert escape_col('') == ''
+        assert escape_col('foo') == 'foo'
+        assert escape_col('foo.bar') == 'foo.bar'
+        assert escape_col('foo:bar') == '`foo:bar`'
+
+    def test_flattened(self):
+        assert ALL_COLUMNS['group_id'].type == UInt(64)
+        assert ALL_COLUMNS['group_id'].name == 'group_id'
+        assert ALL_COLUMNS['group_id'].base_name is None
+        assert ALL_COLUMNS['group_id'].flattened_name == 'group_id'
+
+        assert ALL_COLUMNS['exception_frames.in_app'].type == Array(Nullable(UInt(8)))
+        assert ALL_COLUMNS['exception_frames.in_app'].name == 'in_app'
+        assert ALL_COLUMNS['exception_frames.in_app'].base_name == 'exception_frames'
+        assert ALL_COLUMNS['exception_frames.in_app'].flattened_name == 'exception_frames.in_app'
+
+    def test_schema(self):
+        cols = ColumnSet([
+            ('foo', UInt(8)),
+            ('bar', Nested([
+                ('qux:mux', String())
+            ]))
+        ])
+
+        assert cols.column_names == ['foo', 'bar.qux:mux']
+        assert cols.escaped_column_names == ['foo', '`bar.qux:mux`']
+        assert cols.for_schema() == 'foo UInt8, bar Nested(`qux:mux` String)'
+        assert cols['foo'].type == UInt(8)
+        assert cols['bar.qux:mux'].type == Array(String())
