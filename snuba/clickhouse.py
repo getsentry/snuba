67c7a55d07ee658813520893673ab3740b774c51
Alex Hofsteede <alex@hofsteede.com>
2019-01-23 14:46:36 -0800

    feat: Retry CH on connection failure/timeout (#261)

--- snuba/clickhouse.py
+++ snuba/clickhouse.py
@@ -56,32 +56,55 @@ class ClickhousePool(object):
             self.pool.put(None)
 
     def execute(self, *args, **kwargs):
+        """
+        Execute a clickhouse query with a single quick retry in case of
+        connection failure.
+
+        This should smooth over any Clickhouse instance restarts, but will also
+        return relatively quickly with an error in case of more persistent
+        failures.
+        """
         try:
             conn = self.pool.get(block=True)
 
-            # Lazily create connection instances
-            if conn is None:
-                conn = self._create_conn()
+            attempts_remaining = 2
+            while attempts_remaining > 0:
+                attempts_remaining -= 1
+                # Lazily create connection instances
+                if conn is None:
+                    conn = self._create_conn()
 
-            try:
-                return conn.execute(*args, **kwargs)
-            except (errors.NetworkError, errors.SocketTimeoutError) as e:
-                # Force a reconnection next time
-                conn = None
-                raise e
+                try:
+                    result = conn.execute(*args, **kwargs)
+                    return result
+                except (errors.NetworkError, errors.SocketTimeoutError) as e:
+                    # Force a reconnection next time
+                    conn = None
+                    if attempts_remaining == 0:
+                        raise e
         finally:
             self.pool.put(conn, block=False)
 
     def execute_robust(self, *args, **kwargs):
-        retries = 3
+        """
+        Execute a clickhouse query with a bit more tenacity. Make more retry
+        attempts, (infinite in the case of too many simultaneous queries
+        errors) and wait a second between retries.
+
+        This is used by the writer, which needs to either complete its current
+        write successfully or else quit altogether. Note that each retry in this
+        loop will be doubled by the retry in execute()
+        """
+        attempts_remaining = 3
         while True:
             try:
                 return self.execute(*args, **kwargs)
             except (errors.NetworkError, errors.SocketTimeoutError) as e:
-                logger.warning("Write to ClickHouse failed: %s (%d retries)", str(e), retries)
-                if retries <= 0:
+                # Try 3 times on connection issues.
+                logger.warning("Write to ClickHouse failed: %s (%d tries left)", str(e), attempts_remaining)
+                attempts_remaining -= 1
+                if attempts_remaining <= 0:
                     raise
-                retries -= 1
 
                 if self.metrics:
                     self.metrics.increment('clickhouse.network-error')
@@ -91,12 +114,13 @@ class ClickhousePool(object):
             except errors.ServerException as e:
                 logger.warning("Write to ClickHouse failed: %s (retrying)", str(e))
                 if e.code == errors.ErrorCodes.TOO_MANY_SIMULTANEOUS_QUERIES:
+                    # Try forever if the server is overloaded.
                     if self.metrics:
                         self.metrics.increment('clickhouse.too-many-queries')
-
                     time.sleep(1)
                     continue
                 else:
+                    # Quit immediately for other types of server errors.
                     raise
 
     def _create_conn(self):
