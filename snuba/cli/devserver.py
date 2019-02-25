859a0509f0b7a9dabf3064ee397c090d00735008
Matt Robenolt <matt@ydekproductions.com>
2019-02-25 12:29:54 -0800

    devserver: Make sure `uwsgi` `--needs-app`

--- snuba/cli/devserver.py
+++ snuba/cli/devserver.py
@@ -8,7 +8,6 @@ def devserver(bootstrap, workers):
     "Starts all Snuba processes for local development."
     import os
     import sys
-    import time
     from subprocess import list2cmdline, call
     from honcho.manager import Manager
 
@@ -23,7 +22,12 @@ def devserver(bootstrap, workers):
             sys.exit(returncode)
 
     daemons = [
-        ('api', ['uwsgi', '--master', '--manage-script-name', '--wsgi-file', 'snuba/api.py', '--http', '0.0.0.0:1218', '--http-keepalive']),
+        ('api', [
+            'uwsgi', '--master', '--manage-script-name',
+            '--wsgi-file', 'snuba/api.py',
+            '--http', '0.0.0.0:1218',
+            '--http-keepalive', '--needs-app',
+        ]),
     ]
 
     if not workers:
