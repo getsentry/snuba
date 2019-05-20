7b7f1c8656b438333f6e00c104572373ca0e5719
Lauryn Brown <lauryndbrown@gmail.com>
2019-05-20 11:32:23 -0700

    Removed extra paren in the or/and tests

--- tests/test_util.py
+++ tests/test_util.py
@@ -227,14 +227,14 @@ class TestUtil(BaseTest):
 
         assert complex_column_expr(tuplify(['or', ['a', 'b']]), body.copy()) == 'or(a, b)'
         assert complex_column_expr(tuplify(['and', ['a', 'b']]), body.copy()) == 'and(a, b)'
-        assert complex_column_expr(tuplify(['or', [['or', ['a', 'b']], 'c']]), body.copy()) == 'or((or(a, b)), c)'
-        assert complex_column_expr(tuplify(['and', [['and', ['a', 'b']], 'c']]), body.copy()) == 'and((and(a, b)), c)'
+        assert complex_column_expr(tuplify(['or', [['or', ['a', 'b']], 'c']]), body.copy()) == 'or(or(a, b), c)'
+        assert complex_column_expr(tuplify(['and', [['and', ['a', 'b']], 'c']]), body.copy()) == 'and(and(a, b), c)'
         # (A OR B) AND C
-        assert complex_column_expr(tuplify(['and', [['or', ['a', 'b']], 'c']]), body.copy()) == 'and((or(a, b)), c)'
+        assert complex_column_expr(tuplify(['and', [['or', ['a', 'b']], 'c']]), body.copy()) == 'and(or(a, b), c)'
         # (A AND B) OR C
-        assert complex_column_expr(tuplify(['or', [['and', ['a', 'b']], 'c']]), body.copy()) == 'or((and(a, b)), c)'
+        assert complex_column_expr(tuplify(['or', [['and', ['a', 'b']], 'c']]), body.copy()) == 'or(and(a, b), c)'
         # A OR B OR C OR D
-        assert complex_column_expr(tuplify(['or', [['or', [['or', ['c', 'd']], 'b']], 'a']]), body.copy()) == 'or((or((or(c, d)), b)), a)'
+        assert complex_column_expr(tuplify(['or', [['or', [['or', ['c', 'd']], 'b']], 'a']]), body.copy()) == 'or(or(or(c, d), b), a)'
 
         assert complex_column_expr(tuplify(['if', [['in', ['release', 'tuple', ["'foo'"], ], ], 'release', "'other'"], 'release', ]), body.copy()) == "(if(in(release, tuple('foo')), release, 'other') AS release)"
         assert complex_column_expr(tuplify(['if', ['in', ['release', 'tuple', ["'foo'"]], 'release', "'other'", ], 'release']), body.copy()) == "(if(in(release, tuple('foo')), release, 'other') AS release)"
