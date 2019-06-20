ffe5824c0db303ad0b5ea90a9f2f37320e8a2b6e
Lyn Nagara <lyn.nagara@gmail.com>
2019-06-20 12:01:58 -0700

    feat(datasets): Move events specific utils into dataset class (#339)
    Move util code that references events specific column names into the
    events dataset class. Similar implementation as in
    https://github.com/getsentry/snuba/pull/294
    
    Addresses #333

--- snuba/datasets/events.py
+++ snuba/datasets/events.py
@@ -1,7 +1,32 @@
-from snuba.clickhouse import Array, ColumnSet, DateTime, FixedString, Float, Nested, Nullable, UInt, String
+import re
+from snuba import state
+from snuba.clickhouse import (
+    Array,
+    ColumnSet,
+    DateTime,
+    escape_col,
+    FixedString,
+    Float,
+    Nested,
+    Nullable,
+    String,
+    UInt,
+)
 from snuba.datasets import DataSet
 from snuba.datasets.schema import ReplacingMergeTreeSchema
 from snuba.processor import MessageProcessor, process_message
+from snuba.settings import TIME_GROUP_COLUMNS
+from snuba.util import (
+    alias_expr,
+    all_referenced_columns,
+    escape_literal,
+    string_col,
+    time_expr,
+)
+
+
+# A column name like "tags[url]"
+NESTED_COL_EXPR_RE = re.compile('^(tags|contexts)\[([a-zA-Z0-9_\.:-]+)\]$')
 
 
 class EventsDataSet(DataSet):
@@ -199,6 +224,90 @@ class EventsDataSet(DataSet):
 
     def get_required_columns(self):
         return self.__required_columns
+
+    def column_expr(self, column_name, body):
+        if NESTED_COL_EXPR_RE.match(column_name):
+            return self._tag_expr(column_name)
+        elif column_name in ['tags_key', 'tags_value']:
+            return  self._tags_expr(column_name, body)
+        elif column_name == 'issue':
+            return  'group_id'
+        elif column_name == 'message':
+            # Because of the rename from message->search_message without backfill,
+            # records will have one or the other of these fields.
+            # TODO this can be removed once all data has search_message filled in.
+            return  'coalesce(search_message, message)'
+        else:
+            return  escape_col(column_name)
+
+
+    def _tag_expr(self, column_name):
+        """
+        Return an expression for the value of a single named tag.
+
+        For tags/contexts, we expand the expression depending on whether the tag is
+        "promoted" to a top level column, or whether we have to look in the tags map.
+        """
+        col, tag = NESTED_COL_EXPR_RE.match(column_name).group(1, 2)
+
+        # For promoted tags, return the column name.
+        if col in self.get_promoted_columns():
+            actual_tag = self.get_tag_column_map()[col].get(tag, tag)
+            if actual_tag in self.get_promoted_columns()[col]:
+                return string_col(self, actual_tag)
+
+        # For the rest, return an expression that looks it up in the nested tags.
+        return u'{col}.value[indexOf({col}.key, {tag})]'.format(**{
+            'col': col,
+            'tag': escape_literal(tag)
+        })
+
+
+    def _tags_expr(self, column_name, body):
+        """
+        Return an expression that array-joins on tags to produce an output with one
+        row per tag.
+        """
+        assert column_name in ['tags_key', 'tags_value']
+        col, k_or_v = column_name.split('_', 1)
+        nested_tags_only = state.get_config('nested_tags_only', 1)
+
+        # Generate parallel lists of keys and values to arrayJoin on
+        if nested_tags_only:
+            key_list = '{}.key'.format(col)
+            val_list = '{}.value'.format(col)
+        else:
+            promoted = self.get_promoted_columns()[col]
+            col_map = self.get_column_tag_map()[col]
+            key_list = u'arrayConcat([{}], {}.key)'.format(
+                u', '.join(u'\'{}\''.format(col_map.get(p, p)) for p in promoted),
+                col
+            )
+            val_list = u'arrayConcat([{}], {}.value)'.format(
+                ', '.join(string_col(self, p) for p in promoted),
+                col
+            )
+
+        cols_used = all_referenced_columns(body) & set(['tags_key', 'tags_value'])
+        if len(cols_used) == 2:
+            # If we use both tags_key and tags_value in this query, arrayjoin
+            # on (key, value) tag tuples.
+            expr = (u'arrayJoin(arrayMap((x,y) -> [x,y], {}, {}))').format(
+                key_list,
+                val_list
+            )
+
+            # put the all_tags expression in the alias cache so we can use the alias
+            # to refer to it next time (eg. 'all_tags[1] AS tags_key'). instead of
+            # expanding the whole tags expression again.
+            expr = alias_expr(expr, 'all_tags', body)
+            return u'({})[{}]'.format(expr, 1 if k_or_v == 'key' else 2)
+        else:
+            # If we are only ever going to use one of tags_key or tags_value, don't
+            # bother creating the k/v tuples to arrayJoin on, or the all_tags alias
+            # to re-use as we won't need it.
+            return 'arrayJoin({})'.format(key_list if k_or_v == 'key' else val_list)
+
 
 
 class EventsProcessor(MessageProcessor):
