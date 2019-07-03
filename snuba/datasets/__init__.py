0bfdba264874be5dcdf515bef03ec28fe589afb8
Lyn Nagara <lyn.nagara@gmail.com>
2019-07-03 11:22:08 -0700

    ref: Remove events specific methods from DataSet class
    Addresses #334

--- snuba/datasets/__init__.py
+++ snuba/datasets/__init__.py
@@ -58,56 +58,10 @@ class DataSet(object):
     def get_default_partitions(self):
         return 1
 
-    # These method should be removed once we will have dataset specific query processing in
-    # the dataset class instead of util.py and when the dataset specific logic for processing
-    # Kafka messages will be in the dataset as well.
+    def column_expr(self, column_name, body):
+        """
+        Return an expression for the column name. Handle special column aliases
+        that evaluate to something else.
 
-    def get_metadata_columns(self):
-        raise NotImplementedError
-
-    def get_promoted_tag_columns(self):
-        raise NotImplementedError
-
-    def get_promoted_context_tag_columns(self):
-        raise NotImplementedError
-
-    def get_promoted_context_columns(self):
-        raise NotImplementedError
-
-    def get_required_columns(self):
-        raise NotImplementedError
-
-    def column_expr(self):
-        raise NotImplementedError
-
-    def get_promoted_columns(self):
-        # The set of columns, and associated keys that have been promoted
-        # to the top level table namespace.
-        return {
-            'tags': frozenset(col.flattened for col in (self.get_promoted_tag_columns() + self.get_promoted_context_tag_columns())),
-            'contexts': frozenset(col.flattened for col in self.get_promoted_context_columns()),
-        }
-
-    def get_column_tag_map(self):
-        # For every applicable promoted column,  a map of translations from the column
-        # name  we save in the database to the tag we receive in the query.
-        promoted_context_tag_columns = self.get_promoted_context_tag_columns()
-
-        return {
-            'tags': {col.flattened: col.flattened.replace('_', '.') for col in promoted_context_tag_columns},
-            'contexts': {},
-        }
-
-    def get_tag_column_map(self):
-        # And a reverse map from the tags the client expects to the database columns
-        return {
-            col: dict(map(reversed, trans.items())) for col, trans in self.get_column_tag_map().items()
-        }
-
-    def get_promoted_tags(self):
-        # The canonical list of foo.bar strings that you can send as a `tags[foo.bar]` query
-        # and they can/will use a promoted column.
-        return {
-            col: [self.get_column_tag_map()[col].get(x, x) for x in self.get_promoted_columns()[col]]
-            for col in self.get_promoted_columns()
-        }
+        """
+        return column_name
