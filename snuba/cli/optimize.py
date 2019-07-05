5187cf9f37d94b9f26643509dce076ec95df0718
Ted Kaemming <ted@kaemming.com>
2019-07-05 10:09:53 -0700

    handle other commands that take clickhouse addresses

--- snuba/cli/optimize.py
+++ snuba/cli/optimize.py
@@ -8,8 +8,10 @@ from snuba.datasets.factory import get_dataset
 
 
 @click.command()
-@click.option('--clickhouse-server', multiple=True,
-              help='Clickhouse server to optimize.')
+@click.option('--clickhouse-host', default=settings.CLICKHOUSE_HOST,
+              help='Clickhouse server to write to.')
+@click.option('--clickhouse-port', default=settings.CLICKHOUSE_PORT, type=int,
+              help='Clickhouse native port to write to.')
 @click.option('--database', default='default',
               help='Name of the database to target.')
 @click.option('--dataset', default='events', type=click.Choice(['events']),
@@ -17,7 +19,7 @@ from snuba.datasets.factory import get_dataset
 @click.option('--timeout', default=10000, type=int,
               help='Clickhouse connection send/receive timeout, must be long enough for OPTIMIZE to complete.')
 @click.option('--log-level', default=settings.LOG_LEVEL, help='Logging level to use.')
-def optimize(clickhouse_server, database, dataset, timeout, log_level):
+def optimize(clickhouse_host, clickhouse_port, database, dataset, timeout, log_level):
     from datetime import datetime
     from snuba.clickhouse import ClickhousePool
     from snuba.optimize import run_optimize, logger
@@ -33,8 +35,6 @@ def optimize(clickhouse_server, database, dataset, timeout, log_level):
 
     today = datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0)
     for server in clickhouse_server:
-        clickhouse = ClickhousePool(
-            server.split(':')[0], port=int(server.split(':')[1]), send_receive_timeout=timeout
-        )
+        clickhouse = ClickhousePool(clickhouse_host, clickhouse_port, send_receive_timeout=timeout)
         num_dropped = run_optimize(clickhouse, database, table, before=today)
         logger.info("Optimized %s partitions on %s" % (num_dropped, server))
