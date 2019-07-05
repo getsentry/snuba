d253449bd52187d819836e30aac93b3721b21f99
Ted Kaemming <ted@kaemming.com>
2019-07-04 23:34:18 -0700

    just kidding, looks like this one is used in tests

--- snuba/writer.py
+++ snuba/writer.py
@@ -3,11 +3,11 @@ import logging
 logger = logging.getLogger('snuba.writer')
 
 
-def write_rows(connection, dataset, rows):
+def write_rows(connection, dataset, rows, types_check=False):
     table = dataset.get_schema().get_table_name()
     columns = dataset.get_schema().get_columns()
     connection.execute_robust("""
         INSERT INTO %(table)s (%(colnames)s) VALUES""" % {
         'colnames': ", ".join(col.escaped for col in columns),
         'table': table,
-    }, rows, types_check=False)
+    }, rows, types_check=types_check)
