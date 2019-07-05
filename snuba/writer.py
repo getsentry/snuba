db74baf2a637e1b08572aa6a5d1ac7d738aa8769
Ted Kaemming <ted@kaemming.com>
2019-07-05 09:51:24 -0700

    actually, remove it

--- snuba/writer.py
+++ snuba/writer.py
@@ -3,11 +3,11 @@ import logging
 logger = logging.getLogger('snuba.writer')
 
 
-def write_rows(connection, dataset, rows, types_check=False):
+def write_rows(connection, dataset, rows):
     table = dataset.get_schema().get_table_name()
     columns = dataset.get_schema().get_columns()
     connection.execute_robust("""
         INSERT INTO %(table)s (%(colnames)s) VALUES""" % {
         'colnames': ", ".join(col.escaped for col in columns),
         'table': table,
-    }, rows, types_check=types_check)
+    }, rows, types_check=False)
