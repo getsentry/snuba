7a327bc3f78bdc974491645228b9073935dd1b6c
Manu Zope <manu@sentry.io>
2019-08-02 13:20:44 -0700

    build(prewhere): Change default prewhere settings try 2
    Original PR: https://github.com/getsentry/snuba/pull/384
    Revert PR: https://github.com/getsentry/snuba/pull/389
    
    `sorted` was being called on the following:
    ```
    [
      (1, (('isNull', ('issue',)), '!=', 1)),
      (5, ('project_id', 'IN', (2,))),
      (1, ('issue', 'IN', (2, 1))),
      (5, ('project_id', 'IN', (2,)))
    ]
    ```
    
    Once `sorted` encounters a tie in the first element, it tries to
    sort by the second element in the tuple, which caused problems.
    
    We only care about sorting on the first element in the tuple cause
    that will tell us which order to include the prewhere conditions.

--- snuba/api.py
+++ snuba/api.py
@@ -263,7 +263,7 @@ def parse_and_run_query(validated_body, timer):
         prewhere_candidates = sorted([
             (min(settings.PREWHERE_KEYS.index(col) for col in cols if col in settings.PREWHERE_KEYS), cond)
             for cols, cond in prewhere_candidates
-        ])
+        ], key=lambda column_and_cond: column_and_cond[0])
         if prewhere_candidates:
             prewhere_conditions = [cond for _, cond in prewhere_candidates][:settings.MAX_PREWHERE_CONDITIONS]
 
