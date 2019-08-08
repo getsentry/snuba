dc36336228722eb77616904677c05cd242b68c99
Burak Yigit Kaya <ben@byk.im>
2019-08-08 13:42:15 +0300

    refactor/simplify

--- snuba/settings.py
+++ snuba/settings.py
@@ -10,18 +10,18 @@ def _load_settings(obj=locals()):
     import os
 
     settings = os.environ.get('SNUBA_SETTINGS', 'base')
-    if not settings.startswith('/') and not settings.startswith('settings_'):
-        settings = '.settings_%s' % settings
-    elif not settings.endswith('.py'):
-        settings += '.py'
 
-    if settings.startswith('/'):
+    if settings.starstwith('/'):
+        if not settings.endswith('.py'):
+            settings += '.py'
+
         # Code below is adapted from https://stackoverflow.com/a/41595552/90297S
         settings_spec = importlib.util.spec_from_file_location('snuba.settings.custom', settings)
         settings_module = importlib.util.module_from_spec(settings_spec)
         settings_spec.loader.exec_module(settings_module)
     else:
-        settings_module = importlib.import_module(settings, 'snuba')
+        module_format = '.%s' if settings.startswith('settings_') else '.settings_'
+        settings_module = importlib.import_module(module_format % settings, 'snuba')
 
     for attr in dir(settings_module):
         if attr.isupper():
