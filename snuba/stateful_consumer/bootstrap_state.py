be1876bfd91e5842228f03fea9b4bb0d16998f13
Filippo Pacifici <filippo.pacifici@gmail.com>
2019-08-21 22:39:10 -0700

    Split protocol out

--- snuba/stateful_consumer/bootstrap_state.py
+++ snuba/stateful_consumer/bootstrap_state.py
@@ -1,12 +1,6 @@
 from snuba.stateful_consumer import StateOutput
 from snuba.stateful_consumer.state_context import State
 from snuba.consumers.simple_consumer import create_simple_consumer
-from snuba.stateful_consumer.control_protocol import (
-    parse_control_message,
-    SnapshotInit,
-    SnapshotLoaded,
-    SnapshotAbort
-)
 from snuba import settings
 
 import simplejson as json
@@ -15,37 +9,6 @@ from typing import Any, Sequence, Tuple
 from confluent_kafka import TopicPartition
 
 logger = logging.getLogger('snuba.snapshot-load')
-
-
-class FoundSnapshots:
-    def __init__(self):
-        self.__snapshots = {}
-        self.__current_open_snapshot = None
-
-    def open_snapshot(self, snapshot_id: str) -> None:
-        if snapshot_id in self.__snapshots:
-            logger.error(
-                "Found more than one snapshot init for snapshot %s. IGNORING!",
-                snapshot_id,
-            )
-
-        if self.__current_open_snapshot:
-            logger.error(
-                "Overlapping snapshots found on the control topic. IGNORING!"
-            )
-            return
-        self.__snapshots[snapshot_id] = True
-        self.__current_open_snapshot = snapshot_id
-
-    def close_snapshot(self, snapshot_id: str) -> None:
-        self.__current_open_snapshot = None
-        self.__snapshots[snapshot_id] = False
-
-    def is_snapshot_managed(self, snapshot_id: str) -> bool:
-        return snapshot_id in self.__snapshots
-
-    def is_snapshot_active(self) -> bool:
-        return self.__current_open_snapshot is not None
 
 
 class BootstrapState(State[StateOutput]):
@@ -86,38 +49,6 @@ class BootstrapState(State[StateOutput]):
         message = self.__consumer.consume(1)
         while message:
             value = json.loads(message.value())
-            parsed_message = parse_control_message(value)
-
-            if isinstance(parsed_message, SnapshotInit):
-                if parsed_message.product != expected_product:
-                    if not current_snapshots.is_snapshot_active():
-                        watermark = message.offset()
-                else:
-                    current_snapshots.open_snapshot(parsed_message.id)
-                    output = StateOutput.SNAPSHOT_INIT_RECEIVED
-            elif isinstance(parsed_message, SnapshotAbort):
-                if not current_snapshots.is_snapshot_managed(parsed_message.id):
-                    if not current_snapshots.is_snapshot_active():
-                        watermark = message.offset()
-                else:
-                    current_snapshots.close_snapshot(parsed_message.id)
-                    if not current_snapshots.is_snapshot_active():
-                        watermark = message.offset()
-                        output = StateOutput.NO_SNAPSHOT
-            elif isinstance(parsed_message, SnapshotLoaded):
-                if not current_snapshots.is_snapshot_managed(parsed_message.id):
-                    if not current_snapshots.is_snapshot_active():
-                        watermark = message.offset()
-                else:
-                    current_snapshots.close_snapshot(parsed_message.id)
-                    if not current_snapshots.is_snapshot_active():
-                        output = StateOutput.SNAPSHOT_LOAD_PRODUCT
-
-            self.__consumer.commit(TopicPartition(
-                partitions[0].topic,
-                partitions[0].partition,
-                watermark,
-            ))
             message = self.__consumer.consume(1)
 
         return (output, None)
