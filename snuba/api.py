a4883c95b5b9a70dcbe1197041e163e58fbe2bda
Ted Kaemming <ted@kaemming.com>
2019-08-21 17:07:28 -0700

    mypy has coverage though

--- snuba/api.py
+++ snuba/api.py
@@ -412,7 +412,7 @@ def sdk_distribution(*, timer: Timer):
         ['count()', None, 'count'],
     ]
     request.query['groupby'].extend(['sdk_name', 'rtime'])
-    request.query.extensions['project']['project'] = []
+    request.extensions['project']['project'] = []
 
     dataset = get_dataset('events')
     ensure_table_exists(dataset)
