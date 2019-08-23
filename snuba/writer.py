0b9cce12f412518b3379111bf0a3c263b10e1bf4
ted kaemming <ted@getsentry.com>
2019-08-23 13:22:09 -0700

    ref(writer): Improve handling of ClickHouse error messages over HTTP interface (#439)

--- snuba/writer.py
+++ snuba/writer.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import re
 from datetime import datetime
 from typing import Any, Iterable, List, Mapping
 
@@ -10,7 +11,7 @@ from urllib.parse import urlencode, urljoin
 from snuba.clickhouse import DATETIME_FORMAT, Array
 
 
-logger = logging.getLogger('snuba.writer')
+logger = logging.getLogger("snuba.writer")
 
 WriterTableRow = Mapping[str, Any]
 
@@ -39,10 +40,34 @@ class NativeDriverBatchWriter(BatchWriter):
 
     def write(self, rows: Iterable[WriterTableRow]):
         columns = self.__schema.get_columns()
-        self.__connection.execute_robust("INSERT INTO %(table)s (%(colnames)s) VALUES" % {
-            'colnames': ", ".join(col.escaped for col in columns),
-            'table': self.__schema.get_table_name(),
-        }, [self.__row_to_column_list(columns, row) for row in rows], types_check=False)
+        self.__connection.execute_robust(
+            "INSERT INTO %(table)s (%(colnames)s) VALUES"
+            % {
+                "colnames": ", ".join(col.escaped for col in columns),
+                "table": self.__schema.get_table_name(),
+            },
+            [self.__row_to_column_list(columns, row) for row in rows],
+            types_check=False,
+        )
+
+
+class ClickHouseError(Exception):
+    def __init__(self, code: int, type: str, message: str):
+        self.code = code
+        self.type = type
+        self.message = message
+
+    def __str__(self) -> str:
+        return f"[{self.code}] {self.type}: {self.message}"
+
+    def __repr__(self) -> str:
+        return f"<{type(self).__name__}: {self}>"
+
+
+CLICKHOUSE_ERROR_RE = re.compile(
+    r"^Code: (?P<code>\d+), e.displayText\(\) = (?P<type>(?:\w+)::(?:\w+)): (?P<message>.+)$",
+    re.MULTILINE,
+)
 
 
 class HTTPBatchWriter(BatchWriter):
@@ -58,24 +83,36 @@ class HTTPBatchWriter(BatchWriter):
         else:
             raise TypeError
 
-    def __encode(self, row: WriterTableRow):
-        return json.dumps(row, default=self.__default).encode('utf-8')
+    def __encode(self, row: WriterTableRow) -> bytes:
+        return json.dumps(row, default=self.__default).encode("utf-8")
 
     def write(self, rows: Iterable[WriterTableRow]):
-        parameters = self.__options.copy()
-        parameters['query'] = f"INSERT INTO {self.__table_name} FORMAT JSONEachRow"
-        resp = self.__pool.urlopen(
-            'POST',
-            '/?' + urlencode(parameters),
-            headers={
-                'Connection': 'keep-alive',
-                'Accept-Encoding': 'gzip,deflate',
-            },
+        response = self.__pool.urlopen(
+            "POST",
+            "/?"
+            + urlencode(
+                {
+                    **self.__options,
+                    "query": f"INSERT INTO {self.__table_name} FORMAT JSONEachRow",
+                }
+            ),
+            headers={"Connection": "keep-alive", "Accept-Encoding": "gzip,deflate"},
             body=map(self.__encode, rows),
             chunked=True,
         )
-        if resp.status // 100 != 2:
-            raise HTTPError(f"{resp.status} Unexpected")
+
+        if response.status != 200:
+            # XXX: This should be switched to just parse the JSON body after
+            # https://github.com/yandex/ClickHouse/issues/6272 is available.
+            content = response.data.decode("utf8")
+            details = CLICKHOUSE_ERROR_RE.match(content)
+            if details is not None:
+                code, type, message = details.groups()
+                raise ClickHouseError(int(code), type, message)
+            else:
+                raise HTTPError(
+                    f"Received unexpected {response.status} response: {content}"
+                )
 
 
 class BufferedWriterWrapper:
