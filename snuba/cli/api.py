7fc9d76e55f034d2968158bf47aa46a413a346c7
Matt Robenolt <matt@ydekproductions.com>
2019-08-26 10:51:39 -0700

    Revert "api: Switch to running with mywsgi (#353)"
    This reverts commit 2d34bb481fe74f938d8f99ccd8884e6706566a23.

--- snuba/cli/api.py
+++ snuba/cli/api.py
@@ -3,23 +3,10 @@ import click
 
 @click.command()
 @click.option('--debug', is_flag=True)
-@click.option('--processes', default=1)
-def api(debug, processes):
+def api(debug):
     from snuba import settings
+    from snuba.api import application
+    from werkzeug.serving import WSGIRequestHandler
 
-    if debug:
-        if processes > 1:
-            raise click.ClickException("processes can only be 1 in debug")
-
-        from snuba.api import application
-        from werkzeug.serving import WSGIRequestHandler
-
-        WSGIRequestHandler.protocol_version = "HTTP/1.1"
-        application.run(port=settings.PORT, threaded=True, debug=debug)
-    else:
-        import mywsgi
-        mywsgi.run(
-            "snuba.api:application",
-            "0.0.0.0:%d" % settings.PORT,
-            processes=processes,
-        )
+    WSGIRequestHandler.protocol_version = "HTTP/1.1"
+    application.run(port=settings.PORT, threaded=True, debug=debug)
