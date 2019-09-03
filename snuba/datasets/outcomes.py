08cc64b77130c373922c485e4cf17dde8ac27881
Manu Zope <manu@sentry.io>
2019-09-03 15:10:29 -0700

    Create materialized view and read table

--- snuba/datasets/outcomes.py
+++ snuba/datasets/outcomes.py
@@ -12,8 +12,14 @@ from snuba.clickhouse.columns import (
 )
 from snuba.datasets import Dataset
 from snuba.processor import _ensure_valid_date, MessageProcessor, _unicodify
-from snuba.datasets.schema import MergeTreeSchema
+from snuba.datasets.schema import MergeTreeSchema, SummingMergeTreeSchema, MaterializedViewSchema
 from snuba import settings
+
+
+WRITE_SCHEMA_LOCAL_TABLE_NAME = 'outcomes_raw_local'
+WRITE_SCHEMA_DIST_TABLE_NAME = 'outcomes_raw_dist'
+READ_SCHEMA_LOCAL_TABLE_NAME = 'outcomes_hourly_local'
+READ_SCHEMA_DIST_TABLE_NAME = 'outcomes_hourly_dist'
 
 
 class OutcomesProcessor(MessageProcessor):
@@ -41,7 +47,7 @@ class OutcomesDataset(Dataset):
     """
 
     def __init__(self):
-        columns = ColumnSet([
+        write_columns = ColumnSet([
             ('org_id', UInt(64)),
             ('project_id', UInt(64)),
             ('key_id', Nullable(UInt(64))),
@@ -51,20 +57,78 @@ class OutcomesDataset(Dataset):
             ('event_id', Nullable(UUID())),
         ])
 
-        schema = MergeTreeSchema(
-            columns=columns,
+        self.__write_schema = MergeTreeSchema(
+            columns=write_columns,
             # TODO: change to outcomes.raw_local when we add multi DB support
-            local_table_name='outcomes_raw_local',
-            dist_table_name='outcomes_raw_dist',
+            local_table_name=WRITE_SCHEMA_LOCAL_TABLE_NAME,
+            dist_table_name=WRITE_SCHEMA_DIST_TABLE_NAME,
             order_by='(org_id, project_id, timestamp)',
             partition_by='(toMonday(timestamp))',
             settings={
                 'index_granularity': 16384
             })
 
+        read_columns = ColumnSet([
+            ('org_id', UInt(64)),
+            ('project_id', UInt(64)),
+            ('key_id', UInt(64)),
+            ('timestamp', DateTime()),
+            ('outcome', UInt(8)),
+            ('reason', LowCardinality(String())),
+            ('times_seen', UInt(64)),
+        ])
+
+        self.__read_schema = SummingMergeTreeSchema(
+            columns=read_columns,
+            local_table_name=READ_SCHEMA_LOCAL_TABLE_NAME,
+            dist_table_name=READ_SCHEMA_DIST_TABLE_NAME,
+            order_by='(org_id, project_id, key_id, outcome, reason, timestamp)',
+            partition_by='(toMonday(timestamp))',
+            settings={
+                'index_granularity': 256
+            }
+        )
+
+        materialized_view_columns = ColumnSet([
+            ('org_id', UInt(64)),
+            ('project_id', UInt(64)),
+            ('key_id', UInt(64)),
+            ('timestamp', DateTime()),
+            ('outcome', UInt(8)),
+            ('reason', String()),
+            ('times_seen', UInt(64)),
+        ])
+
+        query = """
+               SELECT
+                   org_id,
+                   project_id,
+                   ifNull(key_id, 0) AS key_id,
+                   toStartOfHour(timestamp) AS timestamp,
+                   outcome,
+                   ifNull(reason, 'none') AS reason,
+                   count() AS times_seen
+               FROM %(src_table_name)s
+               GROUP BY org_id, project_id, key_id, timestamp, outcome, reason
+               """
+
+        self.__materialized_view = MaterializedViewSchema(
+            local_table_name='outcomes_mv_hourly_local',
+            dist_table_name='outcomes_mv_hourly_dist',
+            columns=materialized_view_columns,
+            query=query,
+            local_src_table_name=WRITE_SCHEMA_LOCAL_TABLE_NAME,
+            dist_src_table_name=WRITE_SCHEMA_DIST_TABLE_NAME,
+            local_dest_table_name=READ_SCHEMA_LOCAL_TABLE_NAME,
+            dist_dest_table_name=READ_SCHEMA_DIST_TABLE_NAME
+        )
+
         super(OutcomesDataset, self).__init__(
-            write_schema=schema,
-            read_schema=schema,
+            write_schema=self.__write_schema,
+            read_schema=self.__read_schema,
             processor=OutcomesProcessor(),
             default_topic="outcomes",
         )
+
+    def get_schemas(self):
+        return [self.__write_schema, self.__read_schema, self.__materialized_view]
--- snuba/datasets/schema.py
+++ snuba/datasets/schema.py
@@ -5,21 +5,21 @@ def local_dataset_mode():
     return settings.DATASET_MODE == "local"
 
 
-class TableSchema(object):
+class Schema(object):
     """
     Represents the full set of columns in a clickhouse table, this only contains
-    basic metadata for now. The code to generate the schema comes in a followup PR.
+    basic metadata for now.
     """
 
     TEST_TABLE_PREFIX = "test_"
 
     def __init__(self, local_table_name, dist_table_name, columns):
-        self.__columns = columns
+        self._columns = columns
 
         self.__local_table_name = local_table_name
         self.__dist_table_name = dist_table_name
 
-    def __make_test_table(self, table_name):
+    def _make_test_table(self, table_name):
         return table_name if not settings.TESTING else "%s%s" % (self.TEST_TABLE_PREFIX, table_name)
 
     def get_local_table_name(self):
@@ -27,7 +27,7 @@ class TableSchema(object):
         This returns the local table name for a distributed environment.
         It is supposed to be used in DDL commands and for maintenance.
         """
-        return self.__make_test_table(self.__local_table_name)
+        return self._make_test_table(self.__local_table_name)
 
     def get_table_name(self):
         """
@@ -35,12 +35,20 @@ class TableSchema(object):
         In distributed mode this will be a distributed table. In local mode it is a local table.
         """
         table_name = self.__local_table_name if local_dataset_mode() else self.__dist_table_name
-        return self.__make_test_table(table_name)
+        return self._make_test_table(table_name)
 
+    def get_local_table_definition(self):
+        raise NotImplementedError
+
+    def get_columns(self):
+        return self._columns
+
+
+class TableSchema(Schema):
     def _get_table_definition(self, name, engine):
         return """
-        CREATE TABLE IF NOT EXISTS %(name)s (%(columns)s) ENGINE = %(engine)s""" % {
-            'columns': self.__columns.for_schema(),
+            CREATE TABLE IF NOT EXISTS %(name)s (%(columns)s) ENGINE = %(engine)s""" % {
+            'columns': self._columns.for_schema(),
             'engine': engine,
             'name': name,
         }
@@ -53,9 +61,6 @@ class TableSchema(object):
 
     def _get_local_engine(self):
         raise NotImplementedError
-
-    def get_columns(self):
-        return self.__columns
 
 
 class MergeTreeSchema(TableSchema):
@@ -118,3 +123,62 @@ class ReplacingMergeTreeSchema(MergeTreeSchema):
 
     def _get_engine_type(self):
         return "ReplacingMergeTree(%s)" % self.__version_column
+
+
+class SummingMergeTreeSchema(MergeTreeSchema):
+
+    def _get_engine_type(self):
+        return "SummingMergeTree()"
+
+
+class MaterializedViewSchema(Schema):
+
+    def __init__(
+            self,
+            local_table_name,
+            dist_table_name,
+            columns,
+            query,
+            local_src_table_name,
+            dist_src_table_name,
+            local_dest_table_name,
+            dist_dest_table_name
+    ):
+        super(MaterializedViewSchema, self).__init__(
+            columns=columns,
+            local_table_name=local_table_name,
+            dist_table_name=dist_table_name,
+        )
+
+        # Make sure the caller has provided a src_table_name in the query
+        assert query % {'src_table_name': local_src_table_name} != query
+
+        self.__query = query
+        self.__local_src_table_name = local_src_table_name
+        self.__dist_src_table_name = dist_src_table_name
+        self.__local_dest_table_name = local_dest_table_name
+        self.__dist_dest_table_name = dist_dest_table_name
+
+    def __get_local_src_table_name(self):
+        return self._make_test_table(self.__local_src_table_name)
+
+    def __get_local_dest_table_name(self):
+        return self._make_test_table(self.__local_dest_table_name)
+
+    def __get_table_definition(self, name, src_table_name, dest_table_name):
+        return """
+        CREATE MATERIALIZED VIEW %(name)s TO %(dest_table_name)s (%(columns)s) AS %(query)s""" % {
+            'name': name,
+            'dest_table_name': dest_table_name,
+            'columns': self._columns.for_schema(),
+            'query': self.__query,
+        } % {
+            'src_table_name': src_table_name,
+        }
+
+    def get_local_table_definition(self):
+        return self.__get_table_definition(
+            self.get_local_table_name(),
+            self.__get_local_src_table_name(),
+            self.__get_local_dest_table_name()
+        )
