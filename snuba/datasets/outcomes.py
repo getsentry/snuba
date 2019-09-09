627cb05f31bd20bdedadfe1e5005c12258538d1a
Manu <manu@sentry.io>
2019-09-09 09:25:00 -0700

    feat: Allow creation of outcomes, with a materialized view, locally (#428)
    With outcomes, we need to support a read schema, a write schema and a materialized view funneling data from the write schema to the read schema.
    
    This PR allows the creation of outcomes, with the materialized view locally.

--- snuba/datasets/outcomes.py
+++ snuba/datasets/outcomes.py
@@ -11,9 +11,16 @@ from snuba.clickhouse.columns import (
     UUID,
 )
 from snuba.datasets import Dataset
+from snuba.datasets.dataset_schemas import DatasetSchemas
 from snuba.processor import _ensure_valid_date, MessageProcessor, _unicodify
-from snuba.datasets.schema import MergeTreeSchema
+from snuba.datasets.schema import MergeTreeSchema, SummingMergeTreeSchema, MaterializedViewSchema
 from snuba import settings
+
+
+WRITE_LOCAL_TABLE_NAME = 'outcomes_raw_local'
+WRITE_DIST_TABLE_NAME = 'outcomes_raw_dist'
+READ_LOCAL_TABLE_NAME = 'outcomes_hourly_local'
+READ_DIST_TABLE_NAME = 'outcomes_hourly_dist'
 
 
 class OutcomesProcessor(MessageProcessor):
@@ -41,7 +48,7 @@ class OutcomesDataset(Dataset):
     """
 
     def __init__(self):
-        columns = ColumnSet([
+        write_columns = ColumnSet([
             ('org_id', UInt(64)),
             ('project_id', UInt(64)),
             ('key_id', Nullable(UInt(64))),
@@ -51,19 +58,83 @@ class OutcomesDataset(Dataset):
             ('event_id', Nullable(UUID())),
         ])
 
-        schema = MergeTreeSchema(
-            columns=columns,
+        write_schema = MergeTreeSchema(
+            columns=write_columns,
             # TODO: change to outcomes.raw_local when we add multi DB support
-            local_table_name='outcomes_raw_local',
-            dist_table_name='outcomes_raw_dist',
+            local_table_name=WRITE_LOCAL_TABLE_NAME,
+            dist_table_name=WRITE_DIST_TABLE_NAME,
             order_by='(org_id, project_id, timestamp)',
             partition_by='(toMonday(timestamp))',
             settings={
                 'index_granularity': 16384
             })
 
+        read_columns = ColumnSet([
+            ('org_id', UInt(64)),
+            ('project_id', UInt(64)),
+            ('key_id', UInt(64)),
+            ('timestamp', DateTime()),
+            ('outcome', UInt(8)),
+            ('reason', LowCardinality(String())),
+            ('times_seen', UInt(64)),
+        ])
+
+        read_schema = SummingMergeTreeSchema(
+            columns=read_columns,
+            local_table_name=READ_LOCAL_TABLE_NAME,
+            dist_table_name=READ_DIST_TABLE_NAME,
+            order_by='(org_id, project_id, key_id, outcome, reason, timestamp)',
+            partition_by='(toMonday(timestamp))',
+            settings={
+                'index_granularity': 256
+            }
+        )
+
+        materialized_view_columns = ColumnSet([
+            ('org_id', UInt(64)),
+            ('project_id', UInt(64)),
+            ('key_id', UInt(64)),
+            ('timestamp', DateTime()),
+            ('outcome', UInt(8)),
+            ('reason', String()),
+            ('times_seen', UInt(64)),
+        ])
+
+        # TODO: Find a better way to specify a query for a materialized view
+        # The problem right now is that we have a way to define our columns in a ColumnSet abstraction but the query
+        # doesn't use it.
+        query = """
+               SELECT
+                   org_id,
+                   project_id,
+                   ifNull(key_id, 0) AS key_id,
+                   toStartOfHour(timestamp) AS timestamp,
+                   outcome,
+                   ifNull(reason, 'none') AS reason,
+                   count() AS times_seen
+               FROM %(source_table_name)s
+               GROUP BY org_id, project_id, key_id, timestamp, outcome, reason
+               """
+
+        materialized_view = MaterializedViewSchema(
+            local_materialized_view_name='outcomes_mv_hourly_local',
+            dist_materialized_view_name='outcomes_mv_hourly_dist',
+            columns=materialized_view_columns,
+            query=query,
+            local_source_table_name=WRITE_LOCAL_TABLE_NAME,
+            local_destination_table_name=READ_LOCAL_TABLE_NAME,
+            dist_source_table_name=WRITE_DIST_TABLE_NAME,
+            dist_destination_table_name=READ_DIST_TABLE_NAME
+        )
+
+        dataset_schemas = DatasetSchemas(
+            read_schema=read_schema,
+            write_schema=write_schema,
+            intermediary_schemas=[materialized_view]
+        )
+
         super(OutcomesDataset, self).__init__(
-            schema=schema,
+            dataset_schemas=dataset_schemas,
             processor=OutcomesProcessor(),
             default_topic="outcomes",
         )
