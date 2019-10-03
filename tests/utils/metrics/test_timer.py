d31dd3fa8bc6b5804806e57dc8ce875d238326c9
Ted Kaemming <ted@kaemming.com>
2019-10-03 14:23:39 -0700

    use testing clock

--- tests/utils/metrics/test_timer.py
+++ tests/utils/metrics/test_timer.py
@@ -6,20 +6,30 @@ def test_timer():
     time = TestingClock()
 
     t = Timer("timer", clock=time)
-    time.sleep(0.001)
+    time.sleep(10)
     t.mark("thing1")
-    time.sleep(0.001)
+    time.sleep(10)
     t.mark("thing2")
-    snapshot = t.finish()
+    assert t.finish() == {
+        'timestamp': 0.0,
+        'duration_ms': (10.0 + 10.0) * 1000,
+        'marks_ms': {
+            'thing1': 10.0 * 1000,
+            'thing2': 10.0 * 1000,
+        }
+    }
 
     # Test that we can add more time under the same marks and the time will
     # be cumulatively added under those keys.
-    time.sleep(0.001)
+    time.sleep(10.0)
     t.mark("thing1")
-    time.sleep(0.001)
+    time.sleep(10.0)
     t.mark("thing2")
-    snapshot_2 = t.finish()
-
-    assert snapshot["marks_ms"].keys() == snapshot_2["marks_ms"].keys()
-    assert snapshot["marks_ms"]["thing1"] < snapshot_2["marks_ms"]["thing1"]
-    assert snapshot["marks_ms"]["thing2"] < snapshot_2["marks_ms"]["thing2"]
+    assert t.finish() == {
+        'timestamp': 0.0,
+        'duration_ms': (10.0 + 10.0) * 2 * 1000,
+        'marks_ms': {
+            'thing1': 10.0 * 2 * 1000,
+            'thing2': 10.0 * 2 * 1000,
+        }
+    }
