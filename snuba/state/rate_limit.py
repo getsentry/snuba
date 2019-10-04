fb6d13af4d85e9d4edadaa6e50cd0261e6c8f6a6
Manu Zope <manu@sentry.io>
2019-10-03 20:23:32 -0700

    Make rate limit a contextmanager decorator function

--- snuba/state/rate_limit.py
+++ snuba/state/rate_limit.py
@@ -1,10 +1,17 @@
 from collections import namedtuple
-from contextlib import AbstractContextManager, ExitStack
+from contextlib import contextmanager, AbstractContextManager, ExitStack
 from dataclasses import dataclass
 import logging
 import time
 from types import TracebackType
-from typing import Mapping, MutableMapping, Optional, Sequence, Type
+from typing import (
+    Iterator,
+    Mapping,
+    MutableMapping,
+    Optional,
+    Sequence,
+    Type
+)
 import uuid
 
 from snuba import state
@@ -31,7 +38,8 @@ class RateLimitExceeded(Exception):
     """
 
 
-class RateLimit(AbstractContextManager):
+@contextmanager
+def rate_limit(rate_limit_params: RateLimitParameters) -> Iterator[Mapping[str, float]]:
     """
     A context manager for rate limiting that allows for limiting based on
     on a rolling-window per-second rate as well as the number of requests
@@ -51,83 +59,68 @@ class RateLimit(AbstractContextManager):
                                  now
     """
 
-    def __init__(self, rate_limit_params: RateLimitParameters) -> None:
-        self.__rate_limit_params = rate_limit_params
-        self.__did_run = False
-        self.__query_id = uuid.uuid4()
-        self.__bucket = '{}{}'.format(state.ratelimit_prefix, self.__rate_limit_params.bucket)
+    bucket = '{}{}'.format(state.ratelimit_prefix, rate_limit_params.bucket)
+    query_id = uuid.uuid4()
 
-    def __enter__(self) -> Mapping[str, float]:
-        now = time.time()
-        bypass_rate_limit, rate_history_s = state.get_configs([
-            ('bypass_rate_limit', 0),
-            ('rate_history_sec', 3600)
-        ])
+    now = time.time()
+    bypass_rate_limit, rate_history_s = state.get_configs([
+        ('bypass_rate_limit', 0),
+        ('rate_history_sec', 3600)
+    ])
 
-        if bypass_rate_limit == 1:
-            return {}
+    if bypass_rate_limit == 1:
+        yield {}
 
-        pipe = state.rds.pipeline(transaction=False)
-        pipe.zremrangebyscore(self.__bucket, '-inf', '({:f}'.format(now - rate_history_s))  # cleanup
-        pipe.zadd(self.__bucket, now + state.max_query_duration_s, self.__query_id)  # add query
-        if self.__rate_limit_params.per_second_limit is None:
-            pipe.exists("nosuchkey")  # no-op if we don't need per-second
-        else:
-            pipe.zcount(self.__bucket, now - state.rate_lookback_s, now)  # get historical
-        if self.__rate_limit_params.concurrent_limit is None:
-            pipe.exists("nosuchkey")  # no-op if we don't need concurrent
-        else:
-            pipe.zcount(self.__bucket, '({:f}'.format(now), '+inf')  # get concurrent
+    pipe = state.rds.pipeline(transaction=False)
+    pipe.zremrangebyscore(bucket, '-inf', '({:f}'.format(now - rate_history_s))  # cleanup
+    pipe.zadd(bucket, now + state.max_query_duration_s, query_id)  # add query
+    if rate_limit_params.per_second_limit is None:
+        pipe.exists("nosuchkey")  # no-op if we don't need per-second
+    else:
+        pipe.zcount(bucket, now - state.rate_lookback_s, now)  # get historical
+    if rate_limit_params.concurrent_limit is None:
+        pipe.exists("nosuchkey")  # no-op if we don't need concurrent
+    else:
+        pipe.zcount(bucket, '({:f}'.format(now), '+inf')  # get concurrent
 
+    try:
+        _, _, historical, concurrent = pipe.execute()
+        historical = int(historical)
+        concurrent = int(concurrent)
+    except Exception as ex:
+        logger.exception(ex)
+        yield {}  # fail open if redis is having issues
+
+    per_second = historical / float(state.rate_lookback_s)
+
+    rate_limit_name = rate_limit_params.rate_limit_name
+
+    stats: Mapping[str, float] = {
+        '{}_rate'.format(rate_limit_name): per_second,
+        '{}_concurrent'.format(rate_limit_name): concurrent,
+
+    }
+
+    Reason = namedtuple('reason', 'scope name val limit')
+    reasons = [
+        Reason(rate_limit_name, 'concurrent', concurrent, rate_limit_params.concurrent_limit),
+        Reason(rate_limit_name, 'per-second', per_second, rate_limit_params.per_second_limit),
+    ]
+
+    reason = next((r for r in reasons if r.limit is not None and r.val > r.limit), None)
+
+    if reason:
+        state.rds.zrem(bucket, query_id)  # not allowed / not counted
+        raise RateLimitExceeded(
+            '{r.scope} {r.name} of {r.val:.0f} exceeds limit of {r.limit:.0f}'.format(r=reason)
+        )
+
+    try:
+        yield stats
+    finally:
         try:
-            _, _, historical, concurrent = pipe.execute()
-            historical = int(historical)
-            concurrent = int(concurrent)
-            self.__did_run = True
-        except Exception as ex:
-            logger.exception(ex)
-            return {}  # fail open if redis is having issues
-
-        per_second = historical / float(state.rate_lookback_s)
-
-        rate_limit_name = self.__rate_limit_params.rate_limit_name
-
-        stats: Mapping[str, float] = {
-            '{}_rate'.format(rate_limit_name): per_second,
-            '{}_concurrent'.format(rate_limit_name): concurrent,
-
-        }
-
-        Reason = namedtuple('reason', 'scope name val limit')
-        reasons = [
-            Reason(rate_limit_name, 'concurrent', concurrent, self.__rate_limit_params.concurrent_limit),
-            Reason(rate_limit_name, 'per-second', per_second, self.__rate_limit_params.per_second_limit),
-        ]
-
-        reason = next((r for r in reasons if r.limit is not None and r.val > r.limit), None)
-
-        if reason:
-            self.__return_query_to_start_time()
-            raise RateLimitExceeded(
-                '{r.scope} {r.name} of {r.val:.0f} exceeds limit of {r.limit:.0f}'.format(r=reason)
-            )
-
-        return stats
-
-    def __return_query_to_start_time(self) -> None:
-        state.rds.zincrby(self.__bucket, self.__query_id, -float(state.max_query_duration_s))
-
-    def __exit__(
-            self,
-            exc_type: Optional[Type[BaseException]],
-            exc_val: Optional[BaseException],
-            exc_tb: Optional[TracebackType]
-    ) -> None:
-        try:
-            if self.__did_run:
-                self.__return_query_to_start_time()
-            else:
-                state.rds.zrem(self.__bucket, self.__query_id)  # not allowed / not counted
+            # return the query to its start time
+            state.rds.zincrby(bucket, query_id, -float(state.max_query_duration_s))
         except Exception as ex:
             logger.exception(ex)
             pass
@@ -160,14 +153,14 @@ class RateLimitAggregator(AbstractContextManager):
     """
 
     def __init__(self, rate_limit_params: Sequence[RateLimitParameters]) -> None:
-        self.rate_limits = map(RateLimit, rate_limit_params)
+        self.rate_limit_params = rate_limit_params
         self.stack = ExitStack()
 
     def __enter__(self) -> MutableMapping[str, float]:
         stats: MutableMapping[str, float] = {}
 
-        for rate_limit in self.rate_limits:
-            child_stats = self.stack.enter_context(rate_limit)
+        for rate_limit_param in self.rate_limit_params:
+            child_stats: Mapping[str, float] = self.stack.enter_context(rate_limit(rate_limit_param))
             stats.update(child_stats)
 
         return stats
--- tests/state/test_rate_limit.py
+++ tests/state/test_rate_limit.py
@@ -4,38 +4,38 @@ import uuid
 
 from base import BaseTest
 from snuba import state
-from snuba.state.rate_limit import RateLimitAggregator, RateLimitExceeded, RateLimitParameters
+from snuba.state.rate_limit import rate_limit, RateLimitAggregator, RateLimitExceeded, RateLimitParameters
 
 
 class TestRateLimit(BaseTest):
     def test_concurrent_limit(self):
         # No concurrent limit should not raise
         rate_limit_params = RateLimitParameters('foo', 'bar', None, None)
-        with RateLimitAggregator([rate_limit_params]):
+        with rate_limit(rate_limit_params):
             pass
 
         # 0 concurrent limit
         rate_limit_params = RateLimitParameters('foo', 'bar', None, 0)
 
         with pytest.raises(RateLimitExceeded):
-            with RateLimitAggregator([rate_limit_params]):
+            with rate_limit(rate_limit_params):
                 pass
 
         # Concurrent limit 1 with consecutive queries should not raise
         rate_limit_params = RateLimitParameters('foo', 'bar', None, 1)
 
-        with RateLimitAggregator([rate_limit_params]):
+        with rate_limit(rate_limit_params):
             pass
 
-        with RateLimitAggregator([rate_limit_params]):
+        with rate_limit(rate_limit_params):
             pass
 
         # # Concurrent limit with concurrent queries
         rate_limit_params = RateLimitParameters('foo', 'bar', None, 1)
 
         with pytest.raises(RateLimitExceeded):
-            with RateLimitAggregator([rate_limit_params]):
-                with RateLimitAggregator([rate_limit_params]):
+            with rate_limit(rate_limit_params):
+                with rate_limit(rate_limit_params):
                     pass
 
         # Concurrent with different buckets should not raise
@@ -52,28 +52,52 @@ class TestRateLimit(BaseTest):
         # Create 30 queries at time 0, should all be allowed
         with patch.object(state.time, 'time', lambda: 0):
             for _ in range(30):
-                with RateLimitAggregator([rate_limit_params]):
+                with rate_limit(rate_limit_params):
                     pass
 
         # Create another 30 queries at time 30, should also be allowed
         with patch.object(state.time, 'time', lambda: 30):
             for _ in range(30):
-                with RateLimitAggregator([rate_limit_params]):
+                with rate_limit(rate_limit_params):
                     pass
 
         with patch.object(state.time, 'time', lambda: 60):
             # 1 more query should be allowed at T60 because it does not make the previous
             # rate exceed 1/sec until it has finished.
-            with RateLimitAggregator([rate_limit_params]):
+            with rate_limit(rate_limit_params):
                 pass
 
             # But the next one should not be allowed
             with pytest.raises(RateLimitExceeded):
-                with RateLimitAggregator([rate_limit_params]):
+                with rate_limit(rate_limit_params):
                     pass
 
         # Another query at time 61 should be allowed because the first 30 queries
         # have fallen out of the lookback window
         with patch.object(state.time, 'time', lambda: 61):
-            with RateLimitAggregator([rate_limit_params]):
+            with rate_limit(rate_limit_params):
+                pass
+
+    def test_aggregator(self):
+        # do not raise with multiple valid rate limits
+        rate_limit_params_outer = RateLimitParameters('foo', 'bar', None, 5)
+        rate_limit_params_inner = RateLimitParameters('foo', 'bar', None, 5)
+
+        with RateLimitAggregator([rate_limit_params_outer, rate_limit_params_inner]):
+            pass
+
+        rate_limit_params_outer = RateLimitParameters('foo', 'bar', None, 0)
+        rate_limit_params_inner = RateLimitParameters('foo', 'bar', None, 5)
+
+        # raise when the inner rate limit should fail
+        with pytest.raises(RateLimitExceeded):
+            with RateLimitAggregator([rate_limit_params_outer, rate_limit_params_inner]):
+                pass
+
+        rate_limit_params_outer = RateLimitParameters('foo', 'bar', None, 5)
+        rate_limit_params_inner = RateLimitParameters('foo', 'bar', None, 0)
+
+        # raise when the outer rate limit should fail
+        with pytest.raises(RateLimitExceeded):
+            with RateLimitAggregator([rate_limit_params_outer, rate_limit_params_inner]):
                 pass
