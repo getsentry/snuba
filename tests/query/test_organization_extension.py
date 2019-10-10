5c9f4cb596f350306b78bd6e535be01679542618
Manu Zope <manu@sentry.io>
2019-10-10 09:34:39 -0700

    feat(outcomes): Make organization a required attribute

---
+++ tests/query/test_organization_extension.py
@@ -0,0 +1,35 @@
+import pytest
+from jsonschema.exceptions import ValidationError
+
+from snuba.query.organization_extension import OrganizationExtension
+from snuba.query.query import Query
+from snuba.request.request_settings import RequestSettings
+from snuba.schemas import validate_jsonschema
+
+
+def test_organization_extension_query_processing_happy_path():
+    extension = OrganizationExtension()
+    raw_data = {'organization': 2}
+
+    valid_data = validate_jsonschema(raw_data, extension.get_schema())
+    query = Query({
+        "conditions": []
+    })
+    request_settings = RequestSettings(turbo=False, consistent=False, debug=False)
+
+    extension.get_processor().process_query(query, valid_data, request_settings)
+
+    assert query.get_conditions() == [('org_id', '=', 2)]
+
+
+def test_invalid_data_does_not_validate():
+    extension = OrganizationExtension()
+
+    with pytest.raises(ValidationError):
+        validate_jsonschema({'organization': '2'}, extension.get_schema())
+
+    with pytest.raises(ValidationError):
+        validate_jsonschema({'organization': 0}, extension.get_schema())
+
+    with pytest.raises(ValidationError):
+        validate_jsonschema({'organization': [2]}, extension.get_schema())
