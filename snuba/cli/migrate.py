919371d806ecaad369ecbdeecdd7392ad0f1c5d9
Filippo Pacifici <filippo.pacifici@gmail.com>
2019-10-23 13:43:52 -0700

    Fix script

--- snuba/cli/migrate.py
+++ snuba/cli/migrate.py
@@ -13,7 +13,7 @@ from snuba.util import local_dataset_mode
 def migrate(log_level):
     from snuba.migrate import logger, run
     # TODO: this only supports one dataset so far. More work is needed for the others.
-    dataset = get_dataset('transactions')
+    dataset = get_dataset('events')
     logging.basicConfig(level=getattr(logging, log_level.upper()), format='%(asctime)s %(message)s')
 
     if not local_dataset_mode():
