7225a6086107c602815e7c1f096041eea92fb454
Filippo Pacifici <fpacifici@sentry.io>
2019-10-25 15:01:00 -0700

    fix(apm) De-materialize duration column (#545)
    We ran into a problem with Clickhouse materialized column. Somehow, on distributed tables, the materialized column duration does not work anymore. 0 is being explicitly passed to the local tables. We don't know why, so we are just turning it into an ordinary column.
    
    Also it seems our consumer is causing a major issue with the fabric of space time (probably it created a wormhole) and the result is that we observed a relevant amount of transactions that finished before they started. Adding a metric to log if the issue is with related with the content of the transaction event.

--- snuba/datasets/events.py
+++ snuba/datasets/events.py
@@ -1,5 +1,5 @@
 from datetime import timedelta
-from typing import Mapping, Sequence, Union
+from typing import Mapping, Sequence, Tuple, Union
 
 from snuba.clickhouse.columns import (
     Array,
@@ -16,7 +16,7 @@ from snuba.datasets.dataset import ColumnSplitSpec, TimeSeriesDataset
 from snuba.datasets.dataset_schemas import DatasetSchemas
 from snuba.datasets.table_storage import TableWriter, KafkaStreamLoader
 from snuba.datasets.events_processor import EventsProcessor
-from snuba.datasets.schemas.tables import ReplacingMergeTreeSchema
+from snuba.datasets.schemas.tables import MigrationSchemaColumn, ReplacingMergeTreeSchema
 from snuba.datasets.tags_column_processor import TagColumnProcessor
 from snuba.query.query import Query
 from snuba.query.types import Condition
@@ -27,7 +27,7 @@ from snuba.query.project_extension import ProjectExtension, ProjectWithGroupsPro
 from snuba.util import qualified_column
 
 
-def events_migrations(clickhouse_table: str, current_schema: Mapping[str, str]) -> Sequence[str]:
+def events_migrations(clickhouse_table: str, current_schema: Mapping[str, MigrationSchemaColumn]) -> Sequence[str]:
     # Add/remove known migrations
     ret = []
     if 'group_id' not in current_schema:
