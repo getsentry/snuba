30d018cb212ccb7286436af7fc2ba281d7bb1031
Ted Kaemming <ted@kaemming.com>
2019-11-11 17:55:43 -0800

    also make port configurable, lol

--- snuba/cli/api.py
+++ snuba/cli/api.py
@@ -1,12 +1,14 @@
 import click
 
+from snuba import settings
+
 
 @click.command()
 @click.option('--debug', is_flag=True)
-def api(debug):
-    from snuba import settings
+@click.option('--port', type=int, default=settings.PORT)
+def api(*, debug: bool, port: int):
     from snuba.views import application
     from werkzeug.serving import WSGIRequestHandler
 
     WSGIRequestHandler.protocol_version = "HTTP/1.1"
-    application.run(port=settings.PORT, threaded=True, debug=debug)
+    application.run(port=port, threaded=True, debug=debug)
