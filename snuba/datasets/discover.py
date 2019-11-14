9ffcf181b23d5e62eb0eb7c0de9c999276910657
Lyn Nagara <lyn.nagara@gmail.com>
2019-11-14 19:30:51 +0000

    feat(discover): Add message as a common field in Discover
    For a transaction this is an alias for the "transaction_name" field.

--- snuba/datasets/discover.py
+++ snuba/datasets/discover.py
@@ -1,5 +1,5 @@
 from datetime import timedelta
-from typing import Mapping, Optional, Sequence
+from typing import Mapping, Sequence
 
 from snuba.clickhouse.columns import (
     Array,
@@ -144,6 +144,7 @@ class DiscoverDataset(TimeSeriesDataset):
                 ("sentry:dist", Nullable(String())),
                 ("sentry:user", Nullable(String())),
                 ("transaction", Nullable(String())),
+                ("message", Nullable(String())),
                 # User
                 ("user_id", Nullable(String())),
                 ("username", Nullable(String())),
@@ -166,7 +167,6 @@ class DiscoverDataset(TimeSeriesDataset):
                 ("server_name", Nullable(String())),
                 ("site", Nullable(String())),
                 ("url", Nullable(String())),
-                ("message", Nullable(String())),
                 ("search_message", Nullable(String())),
                 ("title", Nullable(String())),
                 ("location", Nullable(String())),
@@ -273,6 +273,8 @@ class DiscoverDataset(TimeSeriesDataset):
             if column_name == "email":
                 return "user_email"
             if column_name == "transaction":
+                return "transaction_name"
+            if column_name == "message":
                 return "transaction_name"
             if self.__events_columns.get(column_name):
                 return "NULL"
