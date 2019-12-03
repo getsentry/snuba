35ff5d1d6f6288fc4b7e2c0041fcd2997c4d648d
Filippo Pacifici <filippo.pacifici@gmail.com>
2019-12-03 15:38:13 -0800

    Formatter for Clickhouse AST query

--- snuba/clickhouse/astquery.py
+++ snuba/clickhouse/astquery.py
@@ -1,7 +1,10 @@
 from typing import Optional
 
+from snuba import settings
 from snuba.clickhouse.query import ClickhouseQuery
+from snuba.clickhouse.formatter import ClickhouseExpressionFormatter
 from snuba.query.expressions import Expression
+from snuba.query.parsing import ParsingContext
 from snuba.query.query import Query
 from snuba.request.request_settings import RequestSettings
 
@@ -23,7 +26,7 @@ class AstClickhouseQuery(ClickhouseQuery):
         # to process this query independently from the Snuba Query
         # and there is no risk in doing so since they are immutable.
         self.__selected_columns = query.get_selected_columns_from_ast()
-        self.__condition = query.get_conditions_from_ast()
+        self.__condition = query.get_condition_from_ast()
         self.__groupby = query.get_groupby_from_ast()
         self.__having = query.get_having_from_ast()
         self.__orderby = query.get_orderby_from_ast()
@@ -36,15 +39,123 @@ class AstClickhouseQuery(ClickhouseQuery):
 
         # Clickhouse specific fields
         self.__prewhere: Optional[Expression] = None
-        self.__final = settings.get_turbo()
+        self.__turbo = settings.get_turbo()
+        self.__final = query.get_final()
 
         # Attributes that should be clickhouse specific that
         # have to be removed from the Snuba Query
         self.__sample = query.get_sample()
         self.__hastotals = query.has_totals()
 
+        self.__settings = settings
+
+        self.__formatted_query: Optional[str] = None
+
     def format_sql(self) -> str:
-        """
-        TODO: Do something interesting here.
-        """
-        raise NotImplementedError
+        if self.__formatted_query:
+            return self.__formatted_query
+
+        parsing_context = ParsingContext()
+
+        selected_cols = [
+            e.accept(ClickhouseExpressionFormatter(parsing_context))
+            for e in self.__selected_columns
+        ]
+        columns = ", ".join(selected_cols)
+        select_clause = f"SELECT {columns}"
+
+        from_clause = f"FROM {self.__data_source.format_from()}"
+
+        if self.__final:
+            from_clause = f"{from_clause} FINAL"
+
+        if not self.__data_source.supports_sample():
+            sample_rate = None
+        else:
+            if self.__sample:
+                sample_rate = self.__sample
+            elif self.__settings.get_turbo():
+                sample_rate = settings.TURBO_SAMPLE_RATE
+            else:
+                sample_rate = None
+        if sample_rate:
+            from_clause = f"{from_clause} SAMPLE {sample_rate}"
+
+        join_clause = ""
+        if self.__arrayjoin:
+            array_join = self.__arrayjoin.accept(
+                ClickhouseExpressionFormatter(parsing_context)
+            )
+            join_clause = f"ARRAY JOIN {array_join}"
+
+        where_clause = ""
+        if self.__condition:
+            formatted_condition = self.__condition.accept(
+                ClickhouseExpressionFormatter(parsing_context)
+            )
+            where_clause = f"WHERE {formatted_condition}"
+
+        prewhere_clause = ""
+        if self.__prewhere:
+            formatted_prewhere = self.__prewhere.accept(
+                ClickhouseExpressionFormatter(parsing_context)
+            )
+            prewhere_clause = f"PREWHERE {formatted_prewhere}"
+
+        group_clause = ""
+        if self.__groupby:
+            # reformat to use aliases generated during the select clause formatting.
+            groupby_expressions = [
+                e.accept(ClickhouseExpressionFormatter(parsing_context))
+                for e in self.__groupby
+            ]
+            formatted_groupby = ", ".join(groupby_expressions)
+            group_clause = f"GROUP BY ({formatted_groupby})"
+            if self.__hastotals:
+                group_clause = f"{group_clause} WITH TOTALS"
+
+        having_clause = ""
+        if self.__having:
+            assert self.__groupby, "found HAVING clause with no GROUP BY"
+            formatted_having = self.__having.accept(
+                ClickhouseExpressionFormatter(parsing_context)
+            )
+            having_clause = f"HAVING {formatted_having}"
+
+        order_clause = ""
+        if self.__orderby:
+            orderby = [
+                f"{e.node.accept(ClickhouseExpressionFormatter(parsing_context))} {e.direction.value}"
+                for e in self.__orderby
+            ]
+            formatted_orderby = ", ".join(orderby)
+            order_clause = f"ORDER BY {formatted_orderby}"
+
+        limitby_clause = ""
+        if self.__limitby is not None:
+            limitby_clause = "LIMIT {} BY {}".format(*self.__limitby)
+
+        limit_clause = ""
+        if self.__limit is not None:
+            limit_clause = f"LIMIT {self.__offset}, {self.__limit}"
+
+        self.__formatted_query = " ".join(
+            [
+                c
+                for c in [
+                    select_clause,
+                    from_clause,
+                    join_clause,
+                    prewhere_clause,
+                    where_clause,
+                    group_clause,
+                    having_clause,
+                    order_clause,
+                    limitby_clause,
+                    limit_clause,
+                ]
+                if c
+            ]
+        )
+
+        return self.__formatted_query
---
+++ tests/clickhouse/test_query_format.py
@@ -0,0 +1,196 @@
+import pytest
+
+from snuba.clickhouse.astquery import AstClickhouseQuery
+from snuba.clickhouse.columns import ColumnSet
+from snuba.datasets.schemas.tables import TableSource
+from snuba.query.query import OrderBy, OrderByDirection, Query
+from snuba.query.expressions import (
+    Column,
+    CurriedFunctionCall,
+    FunctionCall,
+    Literal,
+)
+from snuba.query.conditions import binary_condition
+from snuba.request import RequestSettings
+
+test_cases = [
+    (
+        # Simple query with aliases and multiple tables
+        Query(
+            {},
+            TableSource("my_table", ColumnSet([])),
+            selected_columns=[
+                Column(None, "column1", None),
+                Column(None, "column2", "table1"),
+                Column("al", "column3", None),
+            ],
+            condition=binary_condition(
+                None,
+                "eq",
+                lhs=Column("al", "column3", None),
+                rhs=Literal(None, "blabla"),
+            ),
+            groupby=[
+                Column(None, "column1", None),
+                Column(None, "column2", "table1"),
+                Column("al", "column3", None),
+                Column(None, "column4", None),
+            ],
+            having=binary_condition(
+                None, "eq", lhs=Column(None, "column1", None), rhs=Literal(None, 123),
+            ),
+            order_by=[
+                OrderBy(OrderByDirection.ASC, Column(None, "column1", None)),
+                OrderBy(OrderByDirection.DESC, Column(None, "column2", "table1")),
+            ],
+        ),
+        (
+            "SELECT column1, table1.column2, (column3 AS al) "
+            "FROM my_table "
+            "WHERE eq(al, 'blabla') "
+            "GROUP BY (column1, table1.column2, al, column4) "
+            "HAVING eq(column1, 123) "
+            "ORDER BY column1 ASC, table1.column2 DESC"
+        ),
+    ),
+    (
+        # Query with complex functions
+        Query(
+            {},
+            TableSource("my_table", ColumnSet([])),
+            selected_columns=[
+                CurriedFunctionCall(
+                    "my_complex_math",
+                    FunctionCall(
+                        None,
+                        "doSomething",
+                        [
+                            Column(None, "column1", None),
+                            Column(None, "column2", "table1"),
+                            Column("al", "column3", None),
+                        ],
+                    ),
+                    [Column(None, "column1", None)],
+                )
+            ],
+            condition=binary_condition(
+                None,
+                "and",
+                lhs=binary_condition(
+                    None,
+                    "eq",
+                    lhs=Column("al", "column3", None),
+                    rhs=Literal(None, "blabla"),
+                ),
+                rhs=binary_condition(
+                    None,
+                    "neq",  # yes, not very smart
+                    lhs=Column("al", "column3", None),
+                    rhs=Literal(None, "blabla"),
+                ),
+            ),
+            groupby=[
+                CurriedFunctionCall(
+                    "my_complex_math",
+                    FunctionCall(
+                        None,
+                        "doSomething",
+                        [
+                            Column(None, "column1", None),
+                            Column(None, "column2", "table1"),
+                            Column("al", "column3", None),
+                        ],
+                    ),
+                    [Column(None, "column1", None)],
+                )
+            ],
+            order_by=[
+                OrderBy(
+                    OrderByDirection.ASC,
+                    FunctionCall(None, "f", [Column(None, "column1", None)]),
+                )
+            ],
+        ),
+        (
+            "SELECT (doSomething(column1, table1.column2, (column3 AS al))(column1) AS my_complex_math) "
+            "FROM my_table "
+            "WHERE and(eq(al, 'blabla'), neq(al, 'blabla')) "
+            "GROUP BY (my_complex_math) "
+            "ORDER BY f(column1) ASC"
+        ),
+    ),
+    (
+        # Query with escaping
+        Query(
+            {},
+            TableSource("my_table", ColumnSet([])),
+            selected_columns=[
+                Column("al1", "field_##$$%", None),
+                Column("al2", "f@!@", "t&^%$"),
+            ],
+            groupby=[
+                Column("al1", "field_##$$%", None),
+                Column("al2", "f@!@", "t&^%$"),
+            ],
+            order_by=[OrderBy(OrderByDirection.ASC, Column(None, "column1", None))],
+        ),
+        (
+            "SELECT (`field_##$$%` AS al1), (`t&^%$`.`f@!@` AS al2) "
+            "FROM my_table "
+            "GROUP BY (al1, al2) "
+            "ORDER BY column1 ASC"
+        ),
+    ),
+]
+
+
+@pytest.mark.parametrize("query, formatted", test_cases)
+def test_format_expressions(query: Query, formatted: str) -> None:
+    request_settings = RequestSettings(turbo=False, consistent=False, debug=False)
+    clickhouse_query = AstClickhouseQuery(query, request_settings)
+    assert clickhouse_query.format_sql() == formatted
+
+
+def test_format_clickhouse_specific_query() -> None:
+    """
+    Adds a few of the Clickhosue specific fields to the query.
+    """
+
+    query = Query(
+        {"sample": 0.1, "totals": True, "limitby": (10, "environment")},
+        TableSource("my_table", ColumnSet([])),
+        selected_columns=[
+            Column(None, "column1", None),
+            Column(None, "column2", "table1"),
+        ],
+        condition=binary_condition(
+            None, "eq", lhs=Column(None, "column1", None), rhs=Literal(None, "blabla"),
+        ),
+        groupby=[Column(None, "column1", None), Column(None, "column2", "table1")],
+        having=binary_condition(
+            None, "eq", lhs=Column(None, "column1", None), rhs=Literal(None, 123),
+        ),
+        order_by=[OrderBy(OrderByDirection.ASC, Column(None, "column1", None))],
+        array_join=Column(None, "column1", None),
+    )
+
+    query.set_final(True)
+    query.set_offset(50)
+    query.set_limit(100)
+
+    request_settings = RequestSettings(turbo=False, consistent=False, debug=False)
+    clickhouse_query = AstClickhouseQuery(query, request_settings)
+
+    expected = (
+        "SELECT column1, table1.column2 "
+        "FROM my_table FINAL SAMPLE 0.1 "
+        "ARRAY JOIN column1 "
+        "WHERE eq(column1, 'blabla') "
+        "GROUP BY (column1, table1.column2) WITH TOTALS "
+        "HAVING eq(column1, 123) "
+        "ORDER BY column1 ASC "
+        "LIMIT 10 BY environment "
+        "LIMIT 50, 100"
+    )
+
+    assert clickhouse_query.format_sql() == expected
