659782d8a27ab0be51e97d4dd530b4dae9805837
Ted Kaemming <ted@kaemming.com>
2019-12-03 15:29:21 -0800

    make schedule task a dataclass instead of a tuple -- we will probably be passing these around a lot

--- snuba/subscriptions/scheduler.py
+++ snuba/subscriptions/scheduler.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
-from typing import Generic, Iterator, Tuple, TypeVar
+from dataclasses import dataclass
+from typing import Generic, Iterator, TypeVar
 
 from snuba.subscriptions.types import TTimestamp
 from snuba.utils.types import Interval
@@ -8,11 +9,31 @@ from snuba.utils.types import Interval
 TTask = TypeVar("TTask")
 
 
+@dataclass(frozen=True)
+class ScheduledTask(Generic[TTimestamp, TTask]):
+    """
+    A scheduled task represents a unit of work (a task) that is intended to
+    be executed at (or around) a specific time.
+    """
+
+    # The time that this task was scheduled to execute.
+    timestamp: TTimestamp
+
+    # The task that should be executed.
+    task: TTask
+
+
 class Scheduler(ABC, Generic[TTimestamp, TTask]):
+    """
+    The scheduler maintains the scheduling state for various tasks and
+    provides the ability to query the schedule to find tasks that were
+    scheduled between a time interval.
+    """
+
     @abstractmethod
     def find(
         self, interval: Interval[TTimestamp]
-    ) -> Iterator[Tuple[TTimestamp, TTask]]:
+    ) -> Iterator[ScheduledTask[TTimestamp, TTask]]:
         """
         Return all of the tasks that were scheduled to be executed between
         the lower bound (exclusive) and upper bound (inclusive) of the
