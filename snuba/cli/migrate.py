907d602f595e982feb327f75cc47a3d0006ff6de
Ted Kaemming <ted@kaemming.com>
2019-12-05 20:29:01 -0800

    fix internal type shadow

--- snuba/cli/migrate.py
+++ snuba/cli/migrate.py
@@ -2,6 +2,7 @@ import logging
 import sys
 import click
 from clickhouse_driver import Client
+from typing import Optional
 
 from snuba import settings
 from snuba.datasets.factory import get_dataset, DATASET_NAMES
@@ -11,18 +12,21 @@ from snuba.util import local_dataset_mode
 @click.command()
 @click.option("--log-level", default=settings.LOG_LEVEL, help="Logging level to use.")
 @click.option(
-    "--dataset", type=click.Choice(DATASET_NAMES), help="The dataset to target",
+    "--dataset",
+    "dataset_name",
+    type=click.Choice(DATASET_NAMES),
+    help="The dataset to target",
 )
-def migrate(*, log_level: str, dataset: str) -> None:
+def migrate(*, log_level: str, dataset_name: Optional[str]) -> None:
     from snuba.migrate import logger, run
 
     logging.basicConfig(
         level=getattr(logging, log_level.upper()), format="%(asctime)s %(message)s"
     )
-    dataset_names = [dataset] if dataset else DATASET_NAMES
-    for dataset_name in dataset_names:
-        dataset = get_dataset(dataset_name)
-        logger.info("Migrating dataset %s", dataset_name)
+    dataset_names = [dataset_name] if dataset_name else DATASET_NAMES
+    for name in dataset_names:
+        dataset = get_dataset(name)
+        logger.info("Migrating dataset %s", name)
         if not local_dataset_mode():
             logger.error("The migration tool can only work on local dataset mode.")
             sys.exit(1)
