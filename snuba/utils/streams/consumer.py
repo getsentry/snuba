a785b75205b6e399b09b17e0089d2ed01c1b708b
Ted Kaemming <ted@kaemming.com>
2019-12-18 16:29:10 -0800

    add comment to consumer about how offsets are dealt with

--- snuba/utils/streams/consumer.py
+++ snuba/utils/streams/consumer.py
@@ -36,6 +36,43 @@ logger = logging.getLogger(__name__)
 
 
 class Consumer(Generic[TPayload], ABC):
+    """
+    This abstract class provides an interface for consuming messages from a
+    multiplexed collection of partitioned topic streams.
+
+    Partitions support sequential access, as well as random access by
+    offsets. There are three types of offsets that a consumer interacts with:
+    working offsets, staged offsets, and committed offsets. Offsets always
+    represent the starting offset of the *next* message to be read. (For
+    example, committing an offset of X means the next message fetched via
+    poll will have a least an offset of X, and the last message read had an
+    offset less than X.)
+
+    The working offsets are used track the current read position within a
+    partition. This can be also be considered as a cursor, or as high
+    watermark. Working offsets are local to the consumer process. They are
+    not shared with other consumer instances in the same consumer group and
+    do not persist beyond the lifecycle of the consumer instance, unless they
+    are committed.
+
+    Committed offsets are managed by an external arbiter/service, and are
+    used as the starting point for a consumer when it is assigned a partition
+    during the subscription process. To ensure that a consumer roughly "picks
+    up where it left off" after restarting, or that another consumer in the
+    same group doesn't read messages that have been processed by another
+    consumer within the same group during a rebalance operation, offsets must
+    be regularly committed by calling ``commit_offsets`` after they have been
+    staged with ``stage_offsets``. Offsets are not staged or committed
+    automatically!
+
+    During rebalance operations, working offsets are rolled back to the
+    latest committed offset for a partition, and staged offsets are cleared
+    after the revocation callback provided to ``subscribe`` is called. (This
+    occurs even if the consumer retains ownership of the partition across
+    assignments.) For this reason, it is generally good practice to ensure
+    offsets are committed as part of the revocation callback.
+    """
+
     @abstractmethod
     def subscribe(
         self,
@@ -51,10 +88,19 @@ class Consumer(Generic[TPayload], ABC):
 
     @abstractmethod
     def stage_offsets(self, offsets: Mapping[Partition, int]) -> None:
+        """
+        Stage offsets to be committed. If an offset has already been staged
+        for a given partition, that offset is overwritten (even if the offset
+        moves in reverse.)
+        """
         raise NotImplementedError
 
     @abstractmethod
     def commit_offsets(self) -> Mapping[Partition, int]:
+        """
+        Commit staged offsets. The return value of this method is a mapping
+        of streams with their committed offsets as values.
+        """
         raise NotImplementedError
 
     @abstractmethod
