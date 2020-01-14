9bc48f512e6d16a3c9890f1a77a0411b7c3895dc
Ted Kaemming <ted@kaemming.com>
2020-01-14 14:49:53 -0800

    ref(web): Create separate WSGI entrypoint
    This removes the implicit module-level initialization for logging and
    Sentry to a path where it will only be invoked by the uwsgi entrypoint.
    This prevents conflicts with the CLI where the effective log level was
    dependent on import ordering.

---
+++ snuba/web/wsgi.py
@@ -0,0 +1,25 @@
+def configure() -> None:
+    import logging
+    import os
+
+    from snuba import settings
+
+    logging.basicConfig(
+        level=getattr(logging, settings.LOG_LEVEL.upper()),
+        format="%(asctime)s %(message)s",
+    )
+
+    import sentry_sdk
+    from sentry_sdk.integrations.flask import FlaskIntegration
+    from sentry_sdk.integrations.gnu_backtrace import GnuBacktraceIntegration
+
+    sentry_sdk.init(
+        dsn=settings.SENTRY_DSN,
+        integrations=[FlaskIntegration(), GnuBacktraceIntegration()],
+        release=os.getenv("SNUBA_RELEASE"),
+    )
+
+
+configure()
+
+from snuba.web.views import application  # noqa
