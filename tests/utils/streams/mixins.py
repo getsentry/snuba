2981bfdfc50fc1b30bfbb9cf6209576e5cc0da02
Ted Kaemming <ted@kaemming.com>
2020-01-15 11:49:16 -0800

    avoid using absolute offsets when possible

--- tests/utils/streams/mixins.py
+++ tests/utils/streams/mixins.py
@@ -1,13 +1,12 @@
 import uuid
 from abc import ABC, abstractmethod
-from concurrent.futures import wait
 from contextlib import closing
 from typing import ContextManager, Mapping, Sequence
 
 import pytest
 
 from snuba.utils.streams.consumer import Consumer, ConsumerError, EndOfPartition
-from snuba.utils.streams.producer import Producer
+from snuba.utils.streams.producer import MessageDetails, Producer
 from snuba.utils.streams.types import Message, Partition, Topic
 
 
@@ -29,20 +28,18 @@ class StreamsTestMixin(ABC):
 
         with self.get_topic() as topic:
             with closing(self.get_producer()) as producer:
-                assert (
-                    wait(
-                        [producer.produce(topic, i) for i in range(2)], timeout=5.0,
-                    ).not_done
-                    == set()
-                )
+                messages: Sequence[MessageDetails] = [
+                    future.result(timeout=5.0)
+                    for future in [producer.produce(topic, i) for i in range(2)]
+                ]
 
             consumer = self.get_consumer(group)
 
             def assignment_callback(partitions: Mapping[Partition, int]) -> None:
                 assignment_callback.called = True
-                assert partitions == {Partition(topic, 0): 0}
+                assert partitions == {Partition(topic, 0): messages[0].offset}
 
-                consumer.seek({Partition(topic, 0): 1})
+                consumer.seek({Partition(topic, 0): messages[1].offset})
 
                 with pytest.raises(ConsumerError):
                     consumer.seek({Partition(topic, 1): 0})
@@ -50,10 +47,10 @@ class StreamsTestMixin(ABC):
             def revocation_callback(partitions: Sequence[Partition]) -> None:
                 revocation_callback.called = True
                 assert partitions == [Partition(topic, 0)]
-                assert consumer.tell() == {Partition(topic, 0): 1}
+                assert consumer.tell() == {Partition(topic, 0): messages[1].offset}
 
                 # Not sure why you'd want to do this, but it shouldn't error.
-                consumer.seek({Partition(topic, 0): 0})
+                consumer.seek({Partition(topic, 0): messages[0].offset})
 
             # TODO: It'd be much nicer if ``subscribe`` returned a future that we could
             # use to wait for assignment, but we'd need to be very careful to avoid
@@ -65,14 +62,14 @@ class StreamsTestMixin(ABC):
             message = consumer.poll(10.0)  # XXX: getting the subcription is slow
             assert isinstance(message, Message)
             assert message.partition == Partition(topic, 0)
-            assert message.offset == 1
+            assert message.offset == messages[1].offset
             assert message.payload == 1
 
-            assert consumer.tell() == {Partition(topic, 0): 2}
+            assert consumer.tell() == {Partition(topic, 0): message.get_next_offset()}
             assert getattr(assignment_callback, "called", False)
 
-            consumer.seek({Partition(topic, 0): 0})
-            assert consumer.tell() == {Partition(topic, 0): 0}
+            consumer.seek({Partition(topic, 0): messages[0].offset})
+            assert consumer.tell() == {Partition(topic, 0): messages[0].offset}
 
             with pytest.raises(ConsumerError):
                 consumer.seek({Partition(topic, 1): 0})
@@ -84,7 +81,7 @@ class StreamsTestMixin(ABC):
             message = consumer.poll(1.0)
             assert isinstance(message, Message)
             assert message.partition == Partition(topic, 0)
-            assert message.offset == 0
+            assert message.offset == messages[0].offset
             assert message.payload == 0
 
             assert consumer.commit_offsets() == {}
@@ -105,7 +102,7 @@ class StreamsTestMixin(ABC):
             assert consumer.tell() == {}
 
             with pytest.raises(ConsumerError):
-                consumer.seek({Partition(topic, 0): 0})
+                consumer.seek({Partition(topic, 0): messages[0].offset})
 
             consumer.close()
 
@@ -125,7 +122,7 @@ class StreamsTestMixin(ABC):
                 consumer.tell()
 
             with pytest.raises(RuntimeError):
-                consumer.seek({Partition(topic, 0): 0})
+                consumer.seek({Partition(topic, 0): messages[0].offset})
 
             # with pytest.raises(RuntimeError):
             #     consumer.pause([Partition(topic, 0)])
@@ -148,14 +145,14 @@ class StreamsTestMixin(ABC):
             message = consumer.poll(10.0)  # XXX: getting the subscription is slow
             assert isinstance(message, Message)
             assert message.partition == Partition(topic, 0)
-            assert message.offset == 1
+            assert message.offset == messages[1].offset
             assert message.payload == 1
 
             try:
                 assert consumer.poll(1.0) is None
             except EndOfPartition as error:
                 assert error.partition == Partition(topic, 0)
-                assert error.offset == 2
+                assert error.offset == message.get_next_offset()
             else:
                 raise AssertionError("expected EndOfPartition error")
 
