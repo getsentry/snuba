1fc2204f5fc7dd5f88917649a146197e4e53a37a
Ted Kaemming <ted@kaemming.com>
2020-01-14 17:22:33 -0800

    add comment clarifying not implemented error

--- snuba/web/converters.py
+++ snuba/web/converters.py
@@ -9,4 +9,7 @@ class DatasetConverter(BaseConverter):
         return get_dataset(value)
 
     def to_url(self, value: Dataset):
+        # This isn't needed right now, but should be straightforward to
+        # implement if necessary once there is a way to map a dataset instance
+        # back to it's name. (Basically, the inverse of ``get_dataset``.)
         raise NotImplementedError
