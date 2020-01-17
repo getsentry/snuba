d1b53cca20c1d6aa920ebe549577a916aa0209d6
Filippo Pacifici <filippo.pacifici@gmail.com>
2020-01-16 18:16:26 -0800

    Fix merge again

--- snuba/migrate.py
+++ snuba/migrate.py
@@ -46,7 +46,6 @@ def run(conn, dataset):
     if dataset.get_table_writer():
         schemas.append(dataset.get_table_writer().get_schema())
     schemas.append(dataset.get_dataset_schemas().get_read_schema())
-    schemas.extend(dataset.get_dataset_schemas().get_intermediary_schemas())
 
     for schema in schemas:
         _run_schema(conn, schema)
