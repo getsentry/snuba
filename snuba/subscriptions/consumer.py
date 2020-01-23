86619038ec6e40fe4afd7ed10d867f23a267cc21
Ted Kaemming <ted@kaemming.com>
2020-01-23 11:12:42 -0800

    also add paused to tick consumer

--- snuba/subscriptions/consumer.py
+++ snuba/subscriptions/consumer.py
@@ -131,6 +131,9 @@ class TickConsumer(Consumer[Tick]):
     def resume(self, partitions: Sequence[Partition]) -> None:
         self.__consumer.resume(partitions)
 
+    def paused(self) -> Sequence[Partition]:
+        return self.__consumer.paused()
+
     def tell(self) -> Mapping[Partition, int]:
         # If there is no previous message for a partition, return the current
         # consumer offset, otherwise return the previous message offset (which
