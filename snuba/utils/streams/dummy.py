4ce74cdafc3d0a7ff9d7cacea9d87c02689eebda
ted kaemming <ted@getsentry.com>
2020-01-24 16:02:36 -0800

    ref(streams): Testing improvements (#722)

--- snuba/utils/streams/dummy.py
+++ snuba/utils/streams/dummy.py
@@ -141,9 +141,9 @@ class DummyConsumer(Consumer[TPayload]):
                 payload = messages[offset]
             except IndexError:
                 if offset == len(messages):
-                    if (
-                        self.__enable_end_of_partition
-                        and offset > self.__last_eof_at.get(partition, 0)
+                    if self.__enable_end_of_partition and (
+                        partition not in self.__last_eof_at
+                        or offset > self.__last_eof_at[partition]
                     ):
                         self.__last_eof_at[partition] = offset
                         raise EndOfPartition(partition, offset)
