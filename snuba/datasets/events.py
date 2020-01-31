be09bd3751b0a0c40a8ba545576fffa9cedf26b9
Filippo Pacifici <filippo.pacifici@gmail.com>
2020-01-31 00:55:44 -0800

    Custom Prewhere

--- snuba/datasets/events.py
+++ snuba/datasets/events.py
@@ -22,7 +22,7 @@ from snuba.datasets.schemas.tables import (
 )
 from snuba.datasets.tags_column_processor import TagColumnProcessor
 from snuba.query.processors.basic_functions import BasicFunctionsProcessor
-from snuba.query.processors.prewhere import PrewhereProcessor
+from snuba.query.processors.prewhere import CustomPrewhereProcessor
 from snuba.query.query import Query
 from snuba.query.extensions import QueryExtension
 from snuba.query.parsing import ParsingContext
@@ -398,4 +398,7 @@ class EventsDataset(TimeSeriesDataset):
         }
 
     def get_query_processors(self) -> Sequence[QueryProcessor]:
-        return [BasicFunctionsProcessor(), PrewhereProcessor()]
+        return [
+            BasicFunctionsProcessor(),
+            CustomPrewhereProcessor(custom_candidates=["type"]),
+        ]
--- snuba/query/processors/prewhere.py
+++ snuba/query/processors/prewhere.py
@@ -1,6 +1,9 @@
-from typing import Optional, Sequence
+import json
+from typing import Optional, Sequence, Set
 
-from snuba import settings, util
+from snuba import settings, state, util
+from snuba.query.conditions import is_in_condition
+from snuba.query.expressions import Column, Expression, FunctionCall, Literal
 from snuba.query.query import Query
 from snuba.query.query_processor import QueryProcessor
 from snuba.query.types import Condition
@@ -22,8 +25,11 @@ class PrewhereProcessor(QueryProcessor):
     def __init__(self, max_prewhere_conditions: Optional[int] = None) -> None:
         self.__max_prewhere_conditions: int = max_prewhere_conditions if max_prewhere_conditions is not None else settings.MAX_PREWHERE_CONDITIONS
 
+    def _get_prewhere_keys(self, query: Query) -> Sequence[str]:
+        return query.get_data_source().get_prewhere_candidates()
+
     def process_query(self, query: Query, request_settings: RequestSettings,) -> None:
-        prewhere_keys = query.get_data_source().get_prewhere_candidates()
+        prewhere_keys = self._get_prewhere_keys(query)
         if not prewhere_keys:
             return
         prewhere_conditions: Sequence[Condition] = []
@@ -61,3 +67,51 @@ class PrewhereProcessor(QueryProcessor):
                 list(filter(lambda cond: cond not in prewhere_conditions, conditions))
             )
         query.set_prewhere(prewhere_conditions)
+
+
+class CustomPrewhereProcessor(PrewhereProcessor):
+    def __init__(
+        self,
+        custom_candidates: Sequence[str],
+        max_prewhere_conditions: Optional[int] = None,
+    ) -> None:
+        super().__init__(max_prewhere_conditions)
+        self.__custom_candidates = custom_candidates
+
+    def _get_prewhere_keys(self, query: Query) -> Sequence[str]:
+        candidates = query.get_data_source().get_prewhere_candidates()
+
+        custom_projects_config = state.get_config("prewhere_custom_key_projects", "[]")
+        custom_projects = json.loads(custom_projects_config)
+
+        condition = query.get_condition_from_ast()
+
+        def is_project_condition(node: Expression) -> bool:
+            return (
+                is_in_condition(node)
+                and isinstance(node, FunctionCall)
+                and node.parameters[0] == Column(None, "project_id", None)
+            )
+
+        project_conditions = filter(lambda node: is_project_condition(node), condition)
+
+        def extract_project_ids(node: Expression) -> Set[int]:
+            assert isinstance(condition.parameters[1], FunctionCall)
+            literals = node.parameters[1].parameters
+            return {
+                int(literal.value)
+                for literal in literals
+                if isinstance(literal, Literal)
+            }
+
+        projects_lists_in_query = map(
+            lambda node: extract_project_ids(node), project_conditions
+        )
+        projects_in_query = set()
+        for projects in projects_lists_in_query:
+            projects_in_query = projects_in_query | projects
+
+        if any(p in custom_projects for p in projects_in_query):
+            return self.__custom_candidates + candidates
+        else:
+            return candidates
