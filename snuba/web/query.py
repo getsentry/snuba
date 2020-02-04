bbaec0aeefda502ec814fc5cbc20db20175e6198
Lyn Nagara <lyn.nagara@gmail.com>
2020-02-04 10:54:45 -0800

    remove unnecessary stuff

--- snuba/web/query.py
+++ snuba/web/query.py
@@ -263,8 +263,6 @@ def record_query(request: Request, timer: Timer, query_list: MutableSequence[Cli
             {
                 "request": request.body,
                 "referrer": http_request.referrer,
-                "timing": last_query.timer.for_json(),
-                "status": last_query.status,
                 "query_list": [q.to_dict() for q in query_list],
             }
         )
