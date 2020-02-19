83b1285fe6be48d105e1b6f3a22a014d32353953
Filippo Pacifici <filippo.pacifici@gmail.com>
2020-02-19 14:59:40 -0800

    feat(errors) Wire up replacers (#782)
    This puts all together and make us able to run replacers on the errors dataset:
    
    adds the replacer to the table writer for errors
    fixes a couple of issues when fetching the list of columns in the errors_replacer to skip materialized columns
    a lot of tests.

--- tests/base.py
+++ tests/base.py
@@ -1,6 +1,8 @@
 import calendar
-from hashlib import md5
+
+from copy import deepcopy
 from datetime import datetime, timedelta
+from hashlib import md5
 import uuid
 
 from snuba import settings
@@ -24,6 +26,7 @@ def wrap_raw_event(event):
         "platform": event["platform"],
         "datetime": event["datetime"],
         "data": event,
+        "organization_id": event["organization_id"],
     }
 
 
@@ -37,7 +40,7 @@ def get_event():
     raw_event["received"] = int(
         calendar.timegm((timestamp - timedelta(seconds=1)).timetuple())
     )
-    return wrap_raw_event(raw_event)
+    return wrap_raw_event(deepcopy(raw_event))
 
 
 class BaseTest(object):
@@ -118,7 +121,6 @@ class BaseEventsTest(BaseDatasetTest):
                 .process_message(event)
             )
             out.extend(processed.data)
-
         return self.write_processed_records(out)
 
     def write_processed_events(self, events):
