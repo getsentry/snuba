8367bee9a5796e2cae798114608e87a47be4d900
Ted Kaemming <ted@kaemming.com>
2020-02-20 13:06:04 -0800

    add executor latency

--- snuba/subscriptions/executor.py
+++ snuba/subscriptions/executor.py
@@ -1,11 +1,15 @@
 from __future__ import annotations
 
+import time
 from concurrent.futures import Future, ThreadPoolExecutor
+from datetime import datetime
 
 from snuba.datasets.dataset import Dataset
+from snuba.request import Request
 from snuba.subscriptions.consumer import Tick
 from snuba.subscriptions.data import Subscription
 from snuba.subscriptions.scheduler import ScheduledTask
+from snuba.utils.metrics.backends.abstract import MetricsBackend
 from snuba.utils.metrics.timer import Timer
 from snuba.web.query import ClickhouseQueryResult, parse_and_run_query
 
@@ -18,9 +22,23 @@ class SubscriptionExecutor:
     to make the query, and the result is then returned as a `Future`.
     """
 
-    def __init__(self, dataset: Dataset, executor_pool: ThreadPoolExecutor):
+    def __init__(
+        self,
+        dataset: Dataset,
+        executor_pool: ThreadPoolExecutor,
+        metrics: MetricsBackend,
+    ):
         self.__dataset = dataset
         self.__executor_pool = executor_pool
+        self.__metrics = metrics
+
+    def __run_query(
+        self, scheduled_at: datetime, request: Request, timer: Timer
+    ) -> ClickhouseQueryResult:
+        self.__metrics.timing(
+            "executor.latency", (time.time() - scheduled_at.timestamp()) * 1000,
+        )
+        return parse_and_run_query(self.__dataset, request, timer)
 
     def execute(
         self, task: ScheduledTask[Subscription], tick: Tick
@@ -35,7 +53,7 @@ class SubscriptionExecutor:
             future.set_exception(e)
         else:
             future = self.__executor_pool.submit(
-                parse_and_run_query, self.__dataset, request, timer
+                self.__run_query, task.timestamp, request, timer
             )
         return future
 
