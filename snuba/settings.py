53d6be9a9fd94c3be9ef1fb732b584e881181793
Lyn Nagara <lyn.nagara@gmail.com>
2020-03-04 15:54:46 -0800

    avoid import star

--- snuba/settings.py
+++ snuba/settings.py
@@ -1,4 +1,94 @@
-from snuba.settings_base import *  # NOQA
+import os
+from typing import MutableMapping
+
+
+LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO")
+LOG_FORMAT = "%(asctime)s %(message)s"
+
+TESTING = False
+DEBUG = True
+
+PORT = 1218
+
+DEFAULT_DATASET_NAME = "events"
+DISABLED_DATASETS = {}
+DATASET_MODE = "local"
+
+# Clickhouse Options
+# TODO: Warn about using `CLICKHOUSE_SERVER`, users should use the new settings instead.
+[default_clickhouse_host, default_clickhouse_port] = os.environ.get(
+    "CLICKHOUSE_SERVER", "localhost:9000"
+).split(":", 1)
+CLICKHOUSE_HOST = os.environ.get("CLICKHOUSE_HOST", default_clickhouse_host)
+CLICKHOUSE_PORT = int(os.environ.get("CLICKHOUSE_PORT", default_clickhouse_port))
+CLICKHOUSE_HTTP_PORT = int(os.environ.get("CLICKHOUSE_HTTP_PORT", 8123))
+CLICKHOUSE_MAX_POOL_SIZE = 25
+
+# Dogstatsd Options
+DOGSTATSD_HOST = "localhost"
+DOGSTATSD_PORT = 8125
+
+# Redis Options
+USE_REDIS_CLUSTER = False
+REDIS_CLUSTER_STARTUP_NODES = None
+REDIS_HOST = os.environ.get("REDIS_HOST", "localhost")
+REDIS_PORT = 6379
+REDIS_PASSWORD = None
+REDIS_DB = 1
+
+USE_RESULT_CACHE = False
+
+# Query Recording Options
+RECORD_QUERIES = False
+QUERIES_TOPIC = "snuba-queries"
+
+# Runtime Config Options
+CONFIG_MEMOIZE_TIMEOUT = 10
+
+# Sentry Options
+SENTRY_DSN = None
+
+# Snuba Options
+
+SNAPSHOT_LOAD_PRODUCT = "snuba"
+
+SNAPSHOT_CONTROL_TOPIC_INIT_TIMEOUT = 30
+BULK_CLICKHOUSE_BUFFER = 10000
+
+# Processor/Writer Options
+DEFAULT_BROKERS = ["localhost:9092"]
+DEFAULT_DATASET_BROKERS = {}
+
+DEFAULT_MAX_BATCH_SIZE = 50000
+DEFAULT_MAX_BATCH_TIME_MS = 2 * 1000
+DEFAULT_QUEUED_MAX_MESSAGE_KBYTES = 10000
+DEFAULT_QUEUED_MIN_MESSAGES = 10000
+DISCARD_OLD_EVENTS = True
+CLICKHOUSE_HTTP_CHUNK_SIZE = 1
+
+DEFAULT_RETENTION_DAYS = 90
+RETENTION_OVERRIDES = {}
+
+MAX_PREWHERE_CONDITIONS = 1
+
+STATS_IN_RESPONSE = False
+
+PAYLOAD_DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
+
+REPLACER_MAX_BLOCK_SIZE = 512
+REPLACER_MAX_MEMORY_USAGE = 10 * (1024 ** 3)  # 10GB
+# TLL of Redis key that denotes whether a project had replacements
+# run recently. Useful for decidig whether or not to add FINAL clause
+# to queries.
+REPLACER_KEY_TTL = 12 * 60 * 60
+REPLACER_MAX_GROUP_IDS_TO_EXCLUDE = 256
+
+TURBO_SAMPLE_RATE = 0.1
+
+PROJECT_STACKTRACE_BLACKLIST = set()
+
+TOPIC_PARTITION_COUNTS: MutableMapping[str, int] = {}  # (topic name, # of partitions)
+
 
 def _load_settings(obj=locals()):
     """Load settings from the path provided in the SNUBA_SETTINGS environment
