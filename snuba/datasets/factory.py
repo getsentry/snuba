4d67bab68e50d6f2f1a4fd77a4cd624f4e2b24ed
Lyn Nagara <lyn.nagara@gmail.com>
2020-03-05 11:07:11 -0800

    move check to dataset_query

--- snuba/datasets/factory.py
+++ snuba/datasets/factory.py
@@ -84,3 +84,8 @@ def enforce_table_writer(dataset: Dataset) -> TableWriter:
     table_writer = dataset.get_table_writer()
     assert table_writer is not None, f"Dataset{dataset} is not writable"
     return table_writer
+
+def ensure_not_internal(dataset: Dataset) -> None:
+    name = get_dataset_name(dataset)
+    if name in INTERNAL_DATASET_NAMES:
+        raise InvalidDatasetError(f"Dataset {name} is internal")
