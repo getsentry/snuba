cff722f8ca9cd78e6778ec53d7ab5f0669690bbe
Ted Kaemming <ted@kaemming.com>
2020-03-05 14:19:20 -0800

    add metrics wrapper

---
+++ snuba/utils/metrics/backends/wrapper.py
@@ -0,0 +1,48 @@
+from collections import ChainMap
+from typing import Optional, Union
+
+from snuba.utils.metrics.abstract import MetricsBackend
+from snuba.utils.metrics.types import Tags
+
+
+class MetricsWrapper(MetricsBackend):
+    def __init__(
+        self,
+        backend: MetricsBackend,
+        name: Optional[str] = None,
+        tags: Optional[Tags] = None,
+    ) -> None:
+        self.__backend = backend
+        self.__name = name
+        self.__tags = tags
+
+    def __merge_name(self, name: str) -> str:
+        if self.__name is None:
+            return name
+        else:
+            return f"{self.__name}.{name}"
+
+    def __merge_tags(self, tags: Optional[Tags]) -> Optional[Tags]:
+        if self.__tags is None:
+            return tags
+        elif tags is None:
+            return self.__tags
+        else:
+            return ChainMap(tags, self.__tags)
+
+    def increment(
+        self, name: str, value: Union[int, float] = 1, tags: Optional[Tags] = None
+    ) -> None:
+        self.__backend.increment(
+            self.__merge_name(name), value, self.__merge_tags(tags)
+        )
+
+    def gauge(
+        self, name: str, value: Union[int, float], tags: Optional[Tags] = None
+    ) -> None:
+        self.__backend.gauge(self.__merge_name(name), value, self.__merge_tags(tags))
+
+    def timing(
+        self, name: str, value: Union[int, float], tags: Optional[Tags] = None
+    ) -> None:
+        self.__backend.timing(self.__merge_name(name), value, self.__merge_tags(tags))
