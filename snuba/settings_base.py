95ee06d169180814623c2770c8b13efd1a152d53
Lyn Nagara <lyn.nagara@gmail.com>
2020-03-06 11:49:56 -0800

    replace settings_base

---
+++ snuba/settings_base.py
@@ -0,0 +1,5 @@
+# Do not remove this file. Previous versions of the settings file format
+# needed to import this, and custom settings files may still expect it.
+import warnings
+
+warnings.warn("Settings files should not import settings_base.py", DeprecationWarning)
