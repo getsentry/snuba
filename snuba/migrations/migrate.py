f1fd378cc11a15cf8190a4b59dda9485b5d6ef74
Lyn Nagara <lyn.nagara@gmail.com>
2020-03-11 16:34:01 -0700

    replace migrate read schemas

--- snuba/migrations/migrate.py
+++ snuba/migrations/migrate.py
@@ -32,6 +32,7 @@ def run(conn, dataset):
     schemas = []
     if dataset.get_table_writer():
         schemas.append(dataset.get_table_writer().get_schema())
+    schemas.append(dataset.get_dataset_schemas().get_read_schema())
 
     for schema in schemas:
         _run_schema(conn, schema)
