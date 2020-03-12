921fa79b8b555d83c6435919389fa1ff0d27253c
Lyn Nagara <lyn.nagara@gmail.com>
2020-03-12 11:28:38 -0700

    support expressions with optional spaces

--- snuba/migrations/parse_schema.py
+++ snuba/migrations/parse_schema.py
@@ -39,20 +39,20 @@ grammar = Grammar(
     uint_size        = "8" / "16" / "32" / "64"
     float            = "Float" float_size
     float_size       = "32" / "64"
-    fixedstring      = "FixedString" open_paren fixedstring_size close_paren
+    fixedstring      = "FixedString" open_paren space* fixedstring_size space* close_paren
     fixedstring_size = ~r"\d+"
-    enum             = "Enum" enum_size open_paren enum_pairs close_paren
+    enum             = "Enum" enum_size open_paren space* enum_pairs space* close_paren
     enum_size        = "8" / "16"
-    enum_pairs       = (enum_pair (comma space)?)*
-    enum_pair        = quote enum_str quote space equal space enum_val
+    enum_pairs       = (enum_pair (space* comma space*)?)*
+    enum_pair        = quote enum_str quote space* equal space* enum_val
     enum_str         = ~r"([a-zA-Z0-9]+)"
     enum_val         = ~r"\d+"
-    agg              = "AggregateFunction" open_paren agg_func comma space agg_types close_paren
+    agg              = "AggregateFunction" open_paren space* agg_func space* comma space* agg_types space* close_paren
     agg_func         = ~r"[a-zA-Z]+\([a-zA-Z0-9\,\.\s]+\)|[a-zA-Z]+"
-    agg_types        = (primitive (comma space)?)*
-    array            = "Array" open_paren (primitive / nullable) close_paren
-    lowcardinality   = "LowCardinality" open_paren (primitive / nullable) close_paren
-    nullable         = "Nullable" open_paren (primitive) close_paren
+    agg_types        = (primitive (space* comma space*)?)*
+    array            = "Array" open_paren space* (primitive / nullable) space* close_paren
+    lowcardinality   = "LowCardinality" open_paren space* (primitive / nullable) space* close_paren
+    nullable         = "Nullable" open_paren space* (primitive) space* close_paren
     open_paren       = "("
     close_paren      = ")"
     equal            = "="
@@ -83,11 +83,11 @@ class Visitor(NodeVisitor):
         return Float(size)
 
     def visit_fixedstring(self, node: Node, visited_children: Iterable[Any]) -> ColumnType:
-        size = int(node.children[2].text)
+        size = int(node.children[3].text)
         return FixedString(size)
 
     def visit_enum(self, node: Node, visited_children: Iterable[Any]) -> ColumnType:
-        _enum, _size, _open, pairs, _close = visited_children
+        _enum, _size, _open, _sp, pairs, _sp, _close = visited_children
         return Enum(pairs)
 
     def visit_enum_pairs(self, node: Node, visited_children: Iterable[Any]) -> Sequence[Tuple[str, int]]:
@@ -104,7 +104,7 @@ class Visitor(NodeVisitor):
         return int(node.text)
 
     def visit_agg(self, node: Node, visited_children: Iterable[Any]) -> str:
-        (_agg, _paren, agg_func, _comma, _space, agg_types, _paren) = visited_children
+        (_agg, _paren, _sp, agg_func, _sp, _comma, _sp, agg_types, _sp, _paren) = visited_children
         return AggregateFunction(agg_func, *agg_types)
 
     def visit_agg_func(self, node: Node, visited_children: Iterable[Any]) -> str:
@@ -114,15 +114,15 @@ class Visitor(NodeVisitor):
         return [c[0] for c in visited_children]
 
     def visit_lowcardinality(self, node: Node, visited_children: Iterable[Any]) -> ColumnType:
-        (_lc, _paren, inner_type, _paren) = visited_children
+        (_lc, _paren, _sp, inner_type, _sp, _paren) = visited_children
         return LowCardinality(inner_type)
 
     def visit_nullable(self, node: Node, visited_children: Iterable[Any]) -> ColumnType:
-        (_null, _paren, inner_type, _paren) = visited_children
+        (_null, _paren, _sp, inner_type, _sp, _paren) = visited_children
         return Nullable(inner_type)
 
     def visit_array(self, node: Node, visited_children: Iterable[Any]) -> ColumnType:
-        (_arr, _paren, inner_type, _paren) = visited_children
+        (_arr, _paren, _sp, inner_type, _sp, _paren) = visited_children
         return Array(inner_type)
 
     def generic_visit(self, node: Node, visited_children: Iterable[Any]) -> Any:
