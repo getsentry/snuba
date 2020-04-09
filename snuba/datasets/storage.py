a6eb6cc6bdcc8521bda7e6f06ab9220dc9595b3f
Lyn Nagara <lyn.nagara@gmail.com>
2020-04-09 12:26:03 -0700

    ref: Storages are CDC aware
    This moves the CDC information into the storage (rather than the
    dataset). Since CDC loads data directly to a table, it seems unnecessary
    to go through dataset to achieve this.
    
    The other motivation for this is it will then allow consumers to be run
    on storages rather than datasets as well.

--- snuba/datasets/storage.py
+++ snuba/datasets/storage.py
@@ -106,6 +106,21 @@ class WritableTableStorage(ReadableTableStorage, WritableStorage):
         return self.__table_writer
 
 
+class CdcStorage(WritableTableStorage):
+    def __init__(
+        self, *, default_control_topic: str, postgres_table: str, **kwargs,
+    ):
+        super().__init__(**kwargs)
+        self.__default_control_topic = default_control_topic
+        self.__postgres_table = postgres_table
+
+    def get_default_control_topic(self) -> str:
+        return self.__default_control_topic
+
+    def get_postgres_table(self) -> str:
+        return self.__postgres_table
+
+
 class QueryStorageSelector(ABC):
     """
     The component provided by a dataset and used at the beginning of the
