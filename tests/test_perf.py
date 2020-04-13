fdcc3229b24b659a48b4934e4ffc0ccaa6364a37
Lyn Nagara <lyn.nagara@gmail.com>
2020-04-13 13:25:50 -0700

    ref: Deprecate dataset.get_table_writer() (#876)
    We don't need this anymore. Instead we should get any properties
    of a writable storage via dataset.get_writable_storage() instead.

--- tests/test_perf.py
+++ tests/test_perf.py
@@ -7,7 +7,9 @@ from snuba import perf
 class TestPerf(BaseEventsTest):
     def test(self):
         dataset = get_dataset("events")
-        table = dataset.get_table_writer().get_schema().get_local_table_name()
+        storage = dataset.get_writable_storage()
+        assert storage is not None
+        table = storage.get_table_writer().get_schema().get_local_table_name()
 
         assert self.clickhouse.execute("SELECT COUNT() FROM %s" % table)[0][0] == 0
 
