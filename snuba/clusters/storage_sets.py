f394935128e79530be55ced38e267369d1e5249b
Lyn Nagara <lyn.nagara@gmail.com>
2020-04-14 13:39:16 -0700

    add groups and todo

--- snuba/clusters/storage_sets.py
+++ snuba/clusters/storage_sets.py
@@ -14,7 +14,13 @@ class StorageSetKey(Enum):
 
 
 STORAGE_SETS: Mapping[StorageSetKey, Set[StorageKey]] = {
-    StorageSetKey.EVENTS: {StorageKey.ERRORS, StorageKey.EVENTS, StorageKey.GROUPEDMESSAGES},
+    StorageSetKey.EVENTS: {
+        StorageKey.ERRORS,
+        StorageKey.EVENTS,
+        StorageKey.GROUPEDMESSAGES,
+        # TODO: Remove once groups are no longer storages
+        StorageKey.GROUPS,
+    },
     # Should group assignee be on the events cluster?
     StorageSetKey.GROUPASSIGNEES: {StorageKey.GROUPASSIGNEES},
     StorageSetKey.OUTCOMES: {StorageKey.OUTCOMES_RAW, StorageKey.OUTCOMES_HOURLY},
