8bd5bd502f9156e54ecdcb5d3b9e47661cd3ccde
Filippo Pacifici <filippo.pacifici@gmail.com>
2020-04-14 19:33:57 -0700

    Fix comment

--- snuba/datasets/plans/split_strategy.py
+++ snuba/datasets/plans/split_strategy.py
@@ -8,8 +8,11 @@ from snuba.web import RawQueryResult
 
 class StorageQuerySplitStrategy(ABC):
     """
-    An execution strategy that implements one query split algorithm. For example
-    a StorageQuerySplitStrategy can implement the time based query splitting.
+    Implements a query split algorithm. It works in a similar way as a
+    QueryExecutionStrategy, it takes a Request and a QueryRunner and decides if it
+    can split the query into more efficient parts.
+    If it can split the query it uses the QueryRunner to execute every chunk, otherwise
+    it immediately returns None.
     """
 
     @abstractmethod
