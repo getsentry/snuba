fa28a1306495a0188fb179dc1dc5ff71fc175eda
Ted Kaemming <ted@kaemming.com>
2020-04-16 12:00:45 -0700

    everything but the lua

--- snuba/state/cache.py
+++ snuba/state/cache.py
@@ -1,3 +1,4 @@
+import uuid
 from abc import ABC, abstractmethod
 from typing import Callable, Generic, Optional, TypeVar
 
@@ -33,7 +34,7 @@ class Cache(Generic[T], ABC):
         raise NotImplementedError
 
     @abstractmethod
-    def get_or_execute(self, key: str, function: Callable[[], T], timeout: float) -> T:
+    def get_or_execute(self, key: str, function: Callable[[], T], timeout: int) -> T:
         """
         Attempts to get a value from the cache. On a cache miss, the return
         value of the provided function is used to populate the cache for
@@ -64,6 +65,14 @@ class Cache(Generic[T], ABC):
         raise NotImplementedError
 
 
+TASK_RESULT = 0
+TASK_EXECUTE = 1
+TASK_WAIT = 2
+
+TASK_GET = ""
+TASK_SET = ""
+
+
 class RedisCache(Cache[T]):
     def __init__(
         self, client: RedisClientType, prefix: str, codec: Codec[str, T]
@@ -88,3 +97,60 @@ class RedisCache(Cache[T]):
             self.__codec.encode(value),
             ex=get_config("cache_expiry_sec", 1),
         )
+
+    def get_or_execute(self, key: str, function: Callable[[], T], timeout: int) -> T:
+        key = self.__build_key(key)
+
+        task_id = uuid.uuid1().hex
+        keys = [
+            key,
+            f"{key}:task",
+            f"{key}:task:{task_id}",
+        ]
+        argv = [uuid.uuid1().hex, timeout]
+        result = self.__client.eval(TASK_GET, len(keys), [*keys, *argv])
+
+        if result[0] == TASK_RESULT:
+            return self.__codec.decode(result[1])
+        elif result[1] == TASK_EXECUTE:
+            active_task_id = result[1][0]
+            notify_list_key = f"{key}:task:{active_task_id}:notify"
+            try:
+                success, value = True, function()
+            except Exception:
+                success = False
+            finally:
+                keys = [key, f"{key}:task", f"{key}:task:{task_id}", notify_list_key]
+                notify_list_timeout = 60
+                key_timeout = get_config("cache_expiry_sec", 1)
+                argv = [
+                    key_timeout,
+                    notify_list_timeout,
+                ]
+                if success:
+                    argv.append(self.__codec.encode(value))
+                # TODO: Ideally this would let us know if this took effect or not.
+                self.__client.eval(TASK_SET, len(keys), [*keys, *argv])
+                return value
+        elif result[2] == TASK_WAIT:
+            active_task_id = result[1][0]
+            active_task_timeout_remaining = int(result[1][1])
+
+            notify_list_key = f"{key}:task:{active_task_id}:notify"
+            effective_timeout = min(active_task_timeout_remaining, timeout)
+            block_result = self.__client.blpop([notify_list_key], effective_timeout)
+            if block_result is None:
+                if effective_timeout == timeout:
+                    raise TimeoutError("client timeout reached")
+                else:
+                    raise ExecutionTimeoutError("execution timeout reached")
+            else:
+                assert block_result[0][0] == notify_list_key
+
+            raw_value = self.__client.get(key)
+            if raw_value is None:
+                raise ExecutionError("cache value not set")
+            else:
+                return self.__codec.decode(raw_value)
+        else:
+            raise ValueError("unexpected result from script")
