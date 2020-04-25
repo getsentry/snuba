705bbd8a2744a1e7c9a6fb41d85202daef2797db
Filippo Pacifici <filippo.pacifici@gmail.com>
2020-04-24 18:34:37 -0700

    Change the parameter name

--- snuba/datasets/plans/query_plan.py
+++ snuba/datasets/plans/query_plan.py
@@ -61,7 +61,7 @@ class QueryPlanExecutionStrategy(ABC):
     def execute(
         self,
         request: Request,
-        query_processors: Sequence[QueryProcessor],
+        db_query_processors: Sequence[QueryProcessor],
         runner: QueryRunner,
     ) -> QueryResult:
         """
