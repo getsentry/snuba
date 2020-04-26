411bd58415a6778568aa2a7479891065b611ab8b
Filippo Pacifici <filippo.pacifici@gmail.com>
2020-04-25 21:32:38 -0700

    Update comments

--- snuba/clickhouse/processors.py
+++ snuba/clickhouse/processors.py
@@ -6,10 +6,17 @@ from snuba.request.request_settings import RequestSettings
 
 class QueryProcessor(ABC):
     """
-    This class represents the same concept as the Logical QueryProcessor (see
-    query.processors.logical).
-    The only difference is that it works on the Physical Query instead of the
-    Logical Query.
+    A transformation applied to a Clickhouse Query. This transformation mutates the
+    Query object in place.
+
+    Processors that extend this class are executed during the Clickhouse specific part
+    of the query execution pipeline.
+    As their logical counterparts, Clickhouse query processors are stateless and are
+    independent from each other. Each processor must leave the query in a valid state
+    and must not depend on the execution of another processor before or after.
+
+    Processors are designed to be stateless. There is no guarantee whether the same
+    instance may be reused.
     """
 
     @abstractmethod
