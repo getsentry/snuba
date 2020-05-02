ff0bbbc49b16f7d847ddce57a3fb4c8005272c4f
Lyn Nagara <lyn.nagara@gmail.com>
2020-05-01 17:34:43 -0700

    fixed the todo

--- snuba/snapshots/loaders/__init__.py
+++ snuba/snapshots/loaders/__init__.py
@@ -14,7 +14,4 @@ class BulkLoader(ABC):
 
     @abstractmethod
     def load(self, writer: BufferedWriterWrapper) -> None:
-        # TODO: We shouldn't pass the clickhouse connection here, since both
-        # writer and clickhouse_ro are derived from the storage, this function
-        # should probably just take a writable storage instead.
         raise NotImplementedError
