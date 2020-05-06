getsentry/snuba

commit 18dd7ea1261a85345f7ddd76f333e6d7860736e0

feat(clusters): Cluster provides HTTP writer (#907)
The cluster now provides the HTTP writer based on the host/port
configuration for that cluster. This was previously based on the
global CLICKHOUSE_HOST and CLICKHOUSE_HTTP_PORT values.

The table writer is now instantiated by the writable storage since the
storage set key is shared and determines the cluster configuration that
will apply to the writer. As a result, I needed to remove most of the
subclasses of table writer and pass various writer options into the storage
instead.

snuba/datasets/table_storage.py
@@ -3,15 +3,18 @@ import json
 import rapidjson
 
 from datetime import datetime
-from typing import Optional, Sequence
+from typing import Any, Mapping, Optional, Sequence
 
 from snuba import settings
 from snuba.clickhouse import DATETIME_FORMAT
+from snuba.clusters.cluster import ClickhouseCluster, ClickhouseWriterOptions
 from snuba.datasets.message_filters import StreamMessageFilter
 from snuba.datasets.schemas.tables import WritableTableSchema
 from snuba.processor import MessageProcessor
 from snuba.replacers.replacer_processor import ReplacerProcessor
+from snuba.snapshots import BulkLoadSource
 from snuba.snapshots.loaders import BulkLoader
+from snuba.snapshots.loaders.single_table import RowProcessor, SingleTableBulkLoader
 from snuba.utils.streams.kafka import KafkaPayload
 from snuba.writer import BatchWriter
 
@@ -94,7 +97,7 @@ class KafkaStreamLoader:
 
 class TableWriter:
     """
-    This class provides to a dataset write support on a Clickhouse table.
+    This class provides to a storage write support on a Clickhouse table.
     It is schema aware (the Clickhouse write schema), it provides a writer
     to write on Clickhouse and a two loaders: one for bulk load of the table
     and the other for streaming load.
@@ -109,13 +112,17 @@ class TableWriter:
 
     def __init__(
         self,
+        cluster: ClickhouseCluster,
         write_schema: WritableTableSchema,
         stream_loader: KafkaStreamLoader,
         replacer_processor: Optional[ReplacerProcessor] = None,
+        writer_options: ClickhouseWriterOptions = None,
     ) -> None:
+        self.__cluster = cluster
         self.__table_schema = write_schema
         self.__stream_loader = stream_loader
         self.__replacer_processor = replacer_processor
+        self.__writer_options = writer_options
 
     def get_schema(self) -> WritableTableSchema:
         return self.__table_schema
@@ -124,7 +131,6 @@ class TableWriter:
         self, options=None, table_name=None, rapidjson_serialize=False
     ) -> BatchWriter:
         from snuba import settings
-        from snuba.clickhouse.http import HTTPBatchWriter
 
         def default(value):
             if isinstance(value, datetime):
@@ -132,17 +138,18 @@ class TableWriter:
             else:
                 raise TypeError
 
-        return HTTPBatchWriter(
-            self.__table_schema,
-            settings.CLICKHOUSE_HOST,
-            settings.CLICKHOUSE_HTTP_PORT,
+        table_name = table_name or self.__table_schema.get_table_name()
+
+        options = self.__update_writer_options(options)
+
+        return self.__cluster.get_writer(
+            table_name,
             lambda row: (
                 rapidjson.dumps(row, default=default)
                 if rapidjson_serialize
                 else json.dumps(row, default=default)
             ).encode("utf-8"),
             options,
-            table_name,
             chunk_size=settings.CLICKHOUSE_HTTP_CHUNK_SIZE,
         )
 
@@ -155,24 +162,36 @@ class TableWriter:
         # once we will be confident it is reliable enough.
 
         from snuba import settings
-        from snuba.clickhouse.http import HTTPBatchWriter
 
-        return HTTPBatchWriter(
-            self.__table_schema,
-            settings.CLICKHOUSE_HOST,
-            settings.CLICKHOUSE_HTTP_PORT,
+        table_name = table_name or self.__table_schema.get_table_name()
+
+        options = self.__update_writer_options(options)
+
+        return self.__cluster.get_writer(
+            table_name,
             lambda row: rapidjson.dumps(row).encode("utf-8"),
             options,
-            table_name,
-            chunk_size=settings.BULK_CLICKHOUSE_BUFFER,
+            chunk_size=settings.CLICKHOUSE_HTTP_CHUNK_SIZE,
         )
 
-    def get_bulk_loader(self, source, dest_table) -> BulkLoader:
+    def get_bulk_loader(
+        self,
+        source: BulkLoadSource,
+        source_table: str,
+        dest_table: str,
+        row_processor: RowProcessor,
+    ) -> BulkLoader:
         """
         Returns the instance of the bulk loader to populate the dataset from an
         external source when present.
         """
-        raise NotImplementedError
+        return SingleTableBulkLoader(
+            source=source,
+            source_table=source_table,
+            dest_table=dest_table,
+            row_processor=row_processor,
+            clickhouse_ro=self.__cluster.get_clickhouse_ro(),
+        )
 
     def get_stream_loader(self) -> KafkaStreamLoader:
         return self.__stream_loader
@@ -183,3 +202,12 @@ class TableWriter:
         replacements on the table it manages.
         """
         return self.__replacer_processor
+
+    def __update_writer_options(
+        self, options: ClickhouseWriterOptions = None,
+    ) -> Mapping[str, Any]:
+        if options is None:
+            options = {}
+        if self.__writer_options:
+            return {**options, **self.__writer_options}
+        return options

snuba/cli/bulk_load.py
@@ -25,11 +25,7 @@ from snuba.writer import BufferedWriterWrapper
 @click.option("--dest-table", help="Clickhouse destination table.")
 @click.option("--log-level", help="Logging level to use.")
 def bulk_load(
-    *,
-    storage_name: str,
-    dest_table: Optional[str],
-    source: str,
-    log_level: Optional[str] = None,
+    *, storage_name: str, dest_table: str, source: str, log_level: Optional[str] = None,
 ) -> None:
     setup_logging(log_level)
     setup_sentry()
@@ -47,13 +43,16 @@ def bulk_load(
         product=settings.SNAPSHOT_LOAD_PRODUCT, path=source,
     )
 
-    loader = table_writer.get_bulk_loader(snapshot_source, dest_table)
+    loader = table_writer.get_bulk_loader(
+        snapshot_source,
+        storage.get_postgres_table(),
+        dest_table,
+        storage.get_row_processor(),
+    )
     # TODO: see whether we need to pass options to the writer
     writer = BufferedWriterWrapper(
         table_writer.get_bulk_writer(table_name=dest_table),
         settings.BULK_CLICKHOUSE_BUFFER,
     )
 
-    clickhouse_ro = storage.get_cluster().get_clickhouse_ro()
-
-    loader.load(writer, clickhouse_ro)
+    loader.load(writer)

snuba/clickhouse/http.py
@@ -1,12 +1,11 @@
 import re
 from urllib.parse import urlencode
-from typing import Callable, Iterable
+from typing import Any, Callable, Iterable, Mapping, Optional
 
 from urllib3.connectionpool import HTTPConnectionPool
 from urllib3.exceptions import HTTPError
 
 from snuba.clickhouse.errors import ClickhouseError
-from snuba.datasets.schemas.tables import TableSchema
 from snuba.writer import BatchWriter, WriterTableRow
 
 
@@ -19,31 +18,22 @@ CLICKHOUSE_ERROR_RE = re.compile(
 class HTTPBatchWriter(BatchWriter):
     def __init__(
         self,
-        schema: TableSchema,
-        host,
-        port,
+        table_name: str,
+        host: str,
+        port: int,
         encoder: Callable[[WriterTableRow], bytes],
-        options=None,
-        table_name=None,
-        chunk_size: int = 1,
+        options: Optional[Mapping[str, Any]] = None,
+        chunk_size: Optional[int] = 1,
     ):
         """
         Builds a writer to send a batch to Clickhouse.
-
-        :param schema: The dataset schema to take the table name from
-        :param host: Clickhosue host
-        :param port: Clickhosue port
-        :param encoder: A function that will be applied to each row to turn it into bytes
-        :param options: options passed to Clickhouse
-        :param table_name: Overrides the table coming from the schema (generally used for uplaoding
-            on temporary tables)
-        :param chunk_size: The chunk size (in rows).
-            We send data to the server with Transfer-Encoding: chunked. If 0 we send the entire
-            content in one chunk.
+        The encoder function will be applied to each row to turn it into bytes.
+        We send data to the server with Transfer-Encoding: chunked. If chunk size is 0
+        we send the entire content in one chunk, otherwise it is the rows per chunk.
         """
         self.__pool = HTTPConnectionPool(host, port)
         self.__options = options if options is not None else {}
-        self.__table_name = table_name or schema.get_table_name()
+        self.__table_name = table_name
         self.__chunk_size = chunk_size
         self.__encoder = encoder
 
@@ -58,7 +48,7 @@ class HTTPBatchWriter(BatchWriter):
         if chunk:
             yield b"".join(chunk)
 
-    def write(self, rows: Iterable[WriterTableRow]):
+    def write(self, rows: Iterable[WriterTableRow]) -> None:
         response = self.__pool.urlopen(
             "POST",
             "/?"

snuba/clusters/cluster.py
@@ -1,14 +1,19 @@
 from abc import ABC, abstractmethod
-from typing import Generic, Set
+from typing import Any, Callable, Generic, Mapping, Optional, Set, TypeVar
 
 from snuba import settings
+from snuba.clickhouse.http import HTTPBatchWriter
 from snuba.clickhouse.native import ClickhousePool, NativeDriverReader
 from snuba.clickhouse.sql import SqlQuery
 from snuba.clusters.storage_sets import StorageSetKey
 from snuba.reader import Reader, TQuery
+from snuba.writer import BatchWriter, WriterTableRow
 
 
-class Cluster(ABC, Generic[TQuery]):
+TWriterOptions = TypeVar("TWriterOptions")
+
+
+class Cluster(ABC, Generic[TQuery, TWriterOptions]):
     """
     A cluster is responsible for managing a collection of database nodes.
 
@@ -36,17 +41,31 @@ class Cluster(ABC, Generic[TQuery]):
     def get_reader(self) -> Reader[TQuery]:
         raise NotImplementedError
 
+    @abstractmethod
+    def get_writer(
+        self,
+        table_name: str,
+        encoder: Callable[[WriterTableRow], bytes],
+        options: TWriterOptions,
+        chunk_size: Optional[int],
+    ) -> BatchWriter:
+        raise NotImplementedError
 
-class ClickhouseCluster(Cluster[SqlQuery]):
+
+ClickhouseWriterOptions = Optional[Mapping[str, Any]]
+
+
+class ClickhouseCluster(Cluster[SqlQuery, ClickhouseWriterOptions]):
     """
-    ClickhouseCluster provides a reader and Clickhouse connections that are shared by all
-    storages located on the cluster
+    ClickhouseCluster provides a reader, writer and Clickhouse connections that are
+    shared by all storages located on the cluster
     """
 
     def __init__(self, host: str, port: int, http_port: int, storage_sets: Set[str]):
         super().__init__(storage_sets)
         self.__host = host
         self.__port = port
+        self.__http_port = http_port
         self.__clickhouse_rw = ClickhousePool(host, port)
         self.__clickhouse_ro = ClickhousePool(
             host, port, client_settings={"readonly": True},
@@ -64,6 +83,17 @@ class ClickhouseCluster(Cluster[SqlQuery]):
 
     def get_reader(self) -> Reader[SqlQuery]:
         return self.__reader
+
+    def get_writer(
+        self,
+        table_name: str,
+        encoder: Callable[[WriterTableRow], bytes],
+        options: ClickhouseWriterOptions,
+        chunk_size: Optional[int],
+    ) -> BatchWriter:
+        return HTTPBatchWriter(
+            table_name, self.__host, self.__http_port, encoder, options, chunk_size
+        )
 
 
 CLUSTERS = [

snuba/datasets/cdc/__init__.py
@@ -1,13 +1,23 @@
 from snuba.datasets.storage import WritableTableStorage
+from snuba.snapshots.loaders.single_table import RowProcessor
 
 
 class CdcStorage(WritableTableStorage):
     def __init__(
-        self, *, default_control_topic: str, postgres_table: str, **kwargs,
+        self,
+        *,
+        default_control_topic: str,
+        postgres_table: str,
+        row_processor: RowProcessor,
+        **kwargs,
     ):
         super().__init__(**kwargs)
         self.__default_control_topic = default_control_topic
         self.__postgres_table = postgres_table
+        self.__row_processor = row_processor
+
+    def get_row_processor(self) -> RowProcessor:
+        return self.__row_processor
 
     def get_default_control_topic(self) -> str:
         return self.__default_control_topic

snuba/datasets/cdc/groupedmessage_processor.py
@@ -107,7 +107,7 @@ class GroupedMessageRow(CdcMessageRow):
 
 
 class GroupedMessageProcessor(CdcProcessor):
-    def __init__(self, postgres_table):
+    def __init__(self, postgres_table: str):
         super(GroupedMessageProcessor, self).__init__(
             pg_table=postgres_table, message_row_class=GroupedMessageRow,
         )

snuba/datasets/dataset_schemas.py
@@ -25,6 +25,9 @@ class StorageSchemas(object):
     def get_read_schema(self) -> Schema:
         return self.__read_schema
 
+    def get_write_schema(self) -> Optional[WritableTableSchema]:
+        return self.__write_schema
+
     def __get_unique_schemas(self) -> Sequence[Schema]:
         unique_schemas: List[Schema] = []
 

snuba/datasets/storage.py
@@ -2,13 +2,18 @@ from abc import ABC, abstractmethod
 from typing import Optional, Sequence
 
 from snuba.clickhouse.processors import QueryProcessor
-from snuba.clusters.cluster import ClickhouseCluster, get_cluster
+from snuba.clusters.cluster import (
+    ClickhouseCluster,
+    ClickhouseWriterOptions,
+    get_cluster,
+)
 from snuba.clusters.storage_sets import StorageSetKey
 from snuba.datasets.dataset_schemas import StorageSchemas
 from snuba.datasets.plans.split_strategy import QuerySplitStrategy
 from snuba.datasets.storages import StorageKey
-from snuba.datasets.table_storage import TableWriter
+from snuba.datasets.table_storage import KafkaStreamLoader, TableWriter
 from snuba.query.logical import Query
+from snuba.replacers.replacer_processor import ReplacerProcessor
 from snuba.request.request_settings import RequestSettings
 
 
@@ -131,14 +136,24 @@ class WritableTableStorage(ReadableTableStorage, WritableStorage):
         storage_key: StorageKey,
         storage_set_key: StorageSetKey,
         schemas: StorageSchemas,
-        table_writer: TableWriter,
-        query_processors: Optional[Sequence[QueryProcessor]] = None,
+        query_processors: Sequence[QueryProcessor],
+        stream_loader: KafkaStreamLoader,
         query_splitters: Optional[Sequence[QuerySplitStrategy]] = None,
+        replacer_processor: Optional[ReplacerProcessor] = None,
+        writer_options: ClickhouseWriterOptions = None,
     ) -> None:
         super().__init__(
             storage_key, storage_set_key, schemas, query_processors, query_splitters
         )
-        self.__table_writer = table_writer
+        write_schema = schemas.get_write_schema()
+        assert write_schema is not None
+        self.__table_writer = TableWriter(
+            cluster=get_cluster(storage_set_key),
+            write_schema=write_schema,
+            stream_loader=stream_loader,
+            replacer_processor=replacer_processor,
+            writer_options=writer_options,
+        )
 
     def get_table_writer(self) -> TableWriter:
         return self.__table_writer

commit da72a3af376c8e774d5e4ef00cf9c0b2e0f8965a

fix: Fix bulk writer chunk size
This fixes an error introduced in #907 that incorrectly changed the
chunk size for the bulk writer to reference the wrong setting.

snuba/datasets/table_storage.py
@@ -171,7 +171,7 @@ class TableWriter:
             table_name,
             lambda row: rapidjson.dumps(row).encode("utf-8"),
             options,
-            chunk_size=settings.CLICKHOUSE_HTTP_CHUNK_SIZE,
+            chunk_size=settings.BULK_CLICKHOUSE_BUFFER,
         )
 
     def get_bulk_loader(