dd53fedb76a0188c1599ee9960ecfa3c1ebd03a5
Filippo Pacifici <filippo.pacifici@gmail.com>
2020-05-10 17:30:35 -0700

    Remove the matcher

--- snuba/datasets/plans/translator/mapping_rules.py
+++ snuba/datasets/plans/translator/mapping_rules.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Any, Generic, Callable, Mapping, Optional, TypeVar, Tuple
+from typing import Generic, Optional, TypeVar
 
 from snuba.query.expressions import Expression, ExpressionVisitor
 
@@ -19,35 +19,3 @@ class StructuredExpressionMapper(ABC, Generic[TExpIn, TExpOut]):
         self, expression: TExpIn, children_translator: ExpressionVisitor[TExpOut]
     ) -> Optional[TExpOut]:
         raise NotImplementedError
-
-
-ExpressionMatcher = Callable[
-    [TExpIn], Tuple[bool, Mapping[str, Any], Mapping[str, TExpIn]]
-]
-StructuredRuleExecutor = Callable[
-    [TExpIn, Mapping[str, Any], Mapping[str, TExpOut]], TExpOut,
-]
-
-
-class StructuredMatchingMappingRule(
-    StructuredExpressionMapper[TExpIn, TExpOut], Generic[TExpIn, TExpOut]
-):
-    def __init__(
-        self,
-        matcher: ExpressionMatcher[TExpIn],
-        executor: StructuredRuleExecutor[TExpIn, TExpOut],
-    ) -> None:
-        self.__matcher = matcher
-        self.__executor = executor
-
-    def attemptMap(
-        self, expression: TExpIn, children_translator: ExpressionVisitor[TExpOut]
-    ) -> Optional[TExpOut]:
-        accepted_expression, parameters, to_translate = self.__matcher(expression)
-        if not accepted_expression:
-            return None
-        translated_params = {
-            param: exp.accept(children_translator)
-            for param, exp in to_translate.items()
-        }
-        return self.__executor(expression, parameters, translated_params)
