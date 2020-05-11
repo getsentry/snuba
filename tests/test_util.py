a36e78bde4e3927676d6c7165fe0bc8377a3ffff
evanh <evanh@users.noreply.github.com>
2020-05-11 14:30:21 -0400

    fix(timeseries) Always use the Universal time zone in timeseries (#869)
    Always use the Universal timezone to avoid timezone mismatch errors.

--- tests/test_util.py
+++ tests/test_util.py
@@ -28,14 +28,14 @@ class TestUtil(BaseTest):
     def test_escape(self):
         assert escape_literal(r"'") == r"'\''"
         assert escape_literal(r"\'") == r"'\\\''"
-        assert escape_literal(date(2001, 1, 1)) == "toDate('2001-01-01')"
+        assert escape_literal(date(2001, 1, 1)) == "toDate('2001-01-01', 'Universal')"
         assert (
             escape_literal(datetime(2001, 1, 1, 1, 1, 1))
-            == "toDateTime('2001-01-01T01:01:01')"
+            == "toDateTime('2001-01-01T01:01:01', 'Universal')"
         )
         assert (
             escape_literal([1, "a", date(2001, 1, 1)])
-            == "(1, 'a', toDate('2001-01-01'))"
+            == "(1, 'a', toDate('2001-01-01', 'Universal'))"
         )
 
     def test_escape_identifier(self):
