4fe1b25de1d0c621c5dd8a9bbd56d86d195ec0ef
Filippo Pacifici <filippo.pacifici@gmail.com>
2020-05-13 14:24:09 -0700

    Finalize test and cleanup

--- snuba/query/matcher.py
+++ snuba/query/matcher.py
@@ -1,117 +1,201 @@
+from __future__ import annotations
+
 from abc import ABC, abstractmethod
-from dataclasses import dataclass
-from typing import Mapping, Optional, Tuple, Union
+from dataclasses import dataclass, field
+from datetime import date, datetime
+from typing import Generic, Mapping, Optional, Tuple, TypeVar
 
 from snuba.query.expressions import Column as ColumnExpr
 from snuba.query.expressions import Expression, FunctionCall
 
-Node = Union[Expression, Optional[str]]
+TNode = TypeVar("TNode")
 
 
-class Matcher(ABC):
+@dataclass(frozen=True)
+class MatchResult:
+    """
+    Contains the parameters found in an expression by a Pattern.
+    Parameters are the equivalent to groups in regular expressions, they
+    are named parts of the expression we are matching that are identified
+    when a valid match is found.
+    """
+
+    expressions: Mapping[str, Expression] = field(default_factory=dict)
+    strings: Mapping[str, Optional[str]] = field(default_factory=dict)
+    ints: Mapping[str, Optional[int]] = field(default_factory=dict)
+    floats: Mapping[str, Optional[float]] = field(default_factory=dict)
+    dates: Mapping[str, Optional[date]] = field(default_factory=dict)
+    datetimes: Mapping[str, Optional[datetime]] = field(default_factory=dict)
+
+    def merge(self, values: MatchResult) -> MatchResult:
+        return MatchResult(
+            expressions={**self.expressions, **values.expressions},
+            strings={**self.strings, **values.strings},
+            ints={**self.ints, **values.ints},
+            floats={**self.floats, **values.floats},
+            dates={**self.dates, **values.dates},
+            datetimes={**self.datetimes, **values.datetimes},
+        )
+
+
+class Pattern(ABC, Generic[TNode]):
+    """
+    Tries to match a given node (like an AST Expression) with the rules provided
+    when instantiating this class. This is meant to work like a regular expression
+    but for AST expressions.
+
+    When a node is provided to this method, if this method return an instance of
+    MatchResult it means the node fully matches the pattern.
+    If the pattern defines named parameters, their value is returned as part of
+    the MatchResult object returned.
+    More on how to define parameters in the Param class.
+
+    This is not supposed to ever throw as long as the node is properly is a valid
+    data structure, even if it does not match the pattern.
+    """
+
     @abstractmethod
-    def match(self, node: Node) -> Tuple[bool, Mapping[str, Node]]:
+    def match(self, node: TNode) -> Optional[MatchResult]:
+        """
+        Returns a MatchResult if the node provided matches this pattern
+        otherwise it returns None.
+        """
         raise NotImplementedError
 
 
 @dataclass(frozen=True)
-class Param(Matcher):
+class Param(Pattern[TNode], Generic[TNode]):
+    """
+    Defines a named parameter in a Pattern. When matching the overall Pattern,
+    if the Pattern nested in this object matches, the corresponding input node
+    will be given a name and returned as part of the MatchResult return value.
+    This is meant to represent what a group is in a regex.
+
+    Params can be nested, and they can wrap any valid Pattern.
+    """
+
     name: str
-    matcher: Matcher
+    pattern: Pattern[TNode]
 
-    def match(self, node: Node) -> Tuple[bool, Mapping[str, Node]]:
-        positive_match, parameters = self.matcher.match(node)
-        if not positive_match:
-            return (False, {})
+    def match(self, node: TNode) -> Optional[MatchResult]:
+        result = self.pattern.match(node)
+        if not result:
+            return None
 
-        return (True, {**parameters, **{self.name: node}})
-
-
-@dataclass(frozen=True)
-class Any(Matcher):
-    def match(self, node: Node) -> Tuple[bool, Mapping[str, Node]]:
-        return (True, {})
-
-
-@dataclass(frozen=True)
-class String(Matcher):
-    value: str
-
-    def match(self, node: Node) -> Tuple[bool, Mapping[str, Node]]:
-        return (
-            isinstance(node, str) and node == self.value,
-            {},
+        return result.merge(
+            MatchResult(
+                expressions={self.name: node} if isinstance(node, Expression) else {},
+                strings={self.name: node} if isinstance(node, str) else {},
+                ints={self.name: node} if isinstance(node, int) else {},
+                floats={self.name: node} if isinstance(node, float) else {},
+                dates={self.name: node} if isinstance(node, date) else {},
+                datetimes={self.name: node} if isinstance(node, datetime) else {},
+            )
         )
 
 
 @dataclass(frozen=True)
-class Column(Matcher):
-    alias: Optional[Matcher] = None
-    column_name: Optional[Matcher] = None
-    table_name: Optional[Matcher] = None
+class AnyExpression(Pattern[Expression]):
+    """
+    Successfully matches any expression
+    """
 
-    def match(self, node: Node) -> Tuple[bool, Mapping[str, Node]]:
-        if not isinstance(node, ColumnExpr):
-            return (False, {})
-
-        params: Mapping[str, Node] = {}
-        if self.alias:
-            alias_match, alias_params = self.alias.match(node.alias)
-            if not alias_match:
-                return (False, {})
-            else:
-                params = {**params, **alias_params}
-
-        if self.column_name:
-            column_match, column_params = self.column_name.match(node.column_name)
-            if not column_match:
-                return (False, {})
-            else:
-                params = {**params, **column_params}
-
-        if self.table_name:
-            table_match, table_params = self.table_name.match(node.table_name)
-            if not table_match:
-                return (False, {})
-            else:
-                params = {**params, **table_params}
-
-        return (True, params)
+    def match(self, node: Expression) -> Optional[MatchResult]:
+        return MatchResult()
 
 
 @dataclass(frozen=True)
-class Function(Matcher):
-    alias: Optional[Matcher] = None
-    function_name: Optional[Matcher] = None
-    function_parameters: Optional[Tuple[Matcher, ...]] = None
+class AnyString(Pattern[Optional[str]]):
+    """
+    Successfully matches any string
+    """
 
-    def match(self, node: Node) -> Tuple[bool, Mapping[str, Node]]:
+    def match(self, node: Optional[str]) -> Optional[MatchResult]:
+        return MatchResult()
+
+
+@dataclass(frozen=True)
+class String(Pattern[Optional[str]]):
+    """
+    Matches one specific string (or None).
+    """
+
+    value: Optional[str]
+
+    def match(self, node: Optional[str]) -> Optional[MatchResult]:
+        return MatchResult() if node == self.value else None
+
+
+@dataclass(frozen=True)
+class Column(Pattern[Expression]):
+    """
+    Matches a Column in an AST expression.
+    The column is defined by alias, name and table. For each a Pattern can be
+    provided. If one of these Patters is left None, that field will be ignored
+    when matching (equivalent to Any, but less verbose).
+    """
+
+    alias: Optional[Pattern[Optional[str]]] = None
+    column_name: Optional[Pattern[Optional[str]]] = None
+    table_name: Optional[Pattern[Optional[str]]] = None
+
+    def match(self, node: Expression) -> Optional[MatchResult]:
+        if not isinstance(node, ColumnExpr):
+            return None
+
+        result = MatchResult()
+        for pattern, value in (
+            (self.alias, node.alias),
+            (self.column_name, node.column_name),
+            (self.table_name, node.table_name),
+        ):
+            if pattern:
+                partial_result = pattern.match(value)
+                if not partial_result:
+                    return None
+                result = result.merge(partial_result)
+
+        return result
+
+
+@dataclass(frozen=True)
+class Function(Pattern[Expression]):
+    """
+    Matches a Function in the AST expression.
+    It works like the Column Pattern. if alias, function_name and function_parameters
+    are provided, they have to match, otherwise they are ignored.
+    """
+
+    alias: Optional[Pattern[Optional[str]]] = None
+    function_name: Optional[Pattern[Optional[str]]] = None
+    function_parameters: Optional[Tuple[Pattern[Expression], ...]] = None
+
+    def match(self, node: Expression) -> Optional[MatchResult]:
         if not isinstance(node, FunctionCall):
-            return (False, {})
+            return None
 
-        params: Mapping[str, Node] = {}
-        if self.alias:
-            alias_match, alias_params = self.alias.match(node.alias)
-            if not alias_match:
-                return (False, {})
-            else:
-                params = {**params, **alias_params}
-
-        if self.function_name:
-            function_match, function_params = self.function_name.match(
-                node.function_name
-            )
-            if not function_match:
-                return (False, {})
-            else:
-                params = {**params, **function_params}
+        result = MatchResult()
+        for pattern, value in (
+            (self.alias, node.alias),
+            (self.function_name, node.function_name),
+        ):
+            if pattern:
+                partial_result = pattern.match(value)
+                if not partial_result:
+                    return None
+                result = result.merge(partial_result)
 
         if self.function_parameters:
-            for p in enumerate(self.function_parameters):
-                parameter_match, parameter_params = p[1].match(node.parameters[p[0]])
-                if not parameter_match:
-                    return (False, {})
+            if len(self.function_parameters) != len(node.parameters):
+                return None
+            for index, param_pattern in enumerate(self.function_parameters):
+                p_result = param_pattern.match(node.parameters[index])
+                if not p_result:
+                    return None
                 else:
-                    params = {**params, **parameter_params}
+                    result = result.merge(p_result)
 
-        return (True, params)
+        return result
+
+
+# TODO: Add more Patterns when needed.
--- tests/query/test_matcher.py
+++ tests/query/test_matcher.py
@@ -1,29 +1,122 @@
+from typing import Optional
 
-from snuba.query.expressions import FunctionCall
+import pytest
+
 from snuba.query.expressions import Column as ColumnExpr
-from snuba.query.matcher import Any, Param, Function, String
+from snuba.query.expressions import Expression, FunctionCall
+from snuba.query.matcher import (
+    AnyExpression,
+    AnyString,
+    Column,
+    Function,
+    MatchResult,
+    Param,
+    Pattern,
+    String,
+)
 
-def test_base_expression() -> None:
-    expression = FunctionCall(
+test_cases = [
+    (
+        Column(None, String("test_col"), String("table")),
+        ColumnExpr("alias_we_don't_care_of", "test_col", "table"),
+        MatchResult(),
+    ),  # Simple single node column that matches
+    (
+        Column(None, String("test_col"), None),
+        ColumnExpr(None, "not_a_test_col", None),
         None,
-        "f",
-        (
-            FunctionCall(None, "f2", (ColumnExpr(None, "c", None), ColumnExpr(None, "c2", None))),
-            ColumnExpr(None, "c3", None),
-        )
-    )
-
-    matcher = Function(
+    ),  # Simple single node column that does not match
+    (
+        AnyExpression(),
+        ColumnExpr(None, "something_irrelevant", None),
+        MatchResult(),
+    ),  # Pattern that matches anything
+    (
+        Column(None, Param("col_name", AnyString()), None),
+        ColumnExpr(None, "something_relevant", "not_that_we_care_about_the_table"),
+        MatchResult(strings={"col_name": "something_relevant"}),
+    ),  # Pattern that matches any string and returns it
+    (
+        Function(
+            None,
+            String("f_name"),
+            (
+                Param("p_1", Column(None, AnyString(), None)),
+                Param("p_2", Column(None, AnyString(), None)),
+            ),
+        ),
+        FunctionCall(
+            "irrelevant_alias",
+            "f_name",
+            (
+                ColumnExpr(None, "c_name1", None),
+                ColumnExpr("another_irrelevant_alias", "c_name2", None),
+            ),
+        ),
+        MatchResult(
+            expressions={
+                "p_1": ColumnExpr(None, "c_name1", None),
+                "p_2": ColumnExpr("another_irrelevant_alias", "c_name2", None),
+            }
+        ),
+    ),  # Match any a function given a function name and returns the columns
+    (
+        Function(
+            None,
+            String("f_name"),
+            (
+                Function(None, String("f"), (Column(None, String("my_col"), None),)),
+                Param(
+                    "second_function",
+                    Function(None, Param("second_function_name", AnyString()), None),
+                ),
+            ),
+        ),
+        FunctionCall(
+            "irrelevant",
+            "relevant_and_wrong",
+            (
+                FunctionCall(None, "f", (ColumnExpr(None, "my_col", None),)),
+                FunctionCall(None, "bla", tuple()),
+            ),
+        ),
         None,
-        Param("f_name", Any()),
-        (Param("inner", Function(None, Param("inner_name", String("f2")), None)), Any())
-    )
+    ),  # Complex structure with nested parameters. No match
+    (
+        Function(
+            None,
+            String("f_name"),
+            (
+                Function(None, String("f"), (Column(None, String("my_col"), None),)),
+                Param(
+                    "second_function",
+                    Function(None, Param("second_function_name", AnyString()), None),
+                ),
+            ),
+        ),
+        FunctionCall(
+            "irrelevant",
+            "f_name",
+            (
+                FunctionCall(None, "f", (ColumnExpr(None, "my_col", None),)),
+                FunctionCall(None, "second_name", tuple()),
+            ),
+        ),
+        MatchResult(
+            strings={"second_function_name": "second_name"},
+            expressions={
+                "second_function": FunctionCall(None, "second_name", tuple()),
+            },
+        ),
+    ),  # Complex structure with nested parameters. This matches
+]
 
-    result, params = matcher.match(expression)
 
-    assert result == True
-    assert params == {
-        "f_name": "f",
-        "inner": FunctionCall(None, "f2", (ColumnExpr(None, "c", None), ColumnExpr(None, "c2", None))),
-        "inner_name": "f2",
-    }
+@pytest.mark.parametrize("pattern, expression, expected_result", test_cases)
+def test_base_expression(
+    pattern: Pattern[Expression],
+    expression: Expression,
+    expected_result: Optional[MatchResult],
+) -> None:
+    res = pattern.match(expression)
+    assert res == expected_result
