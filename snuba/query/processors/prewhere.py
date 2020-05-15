c30377c44fc82ec2b62d9f2410a4efcacbe73c5c
Filippo Pacifici <filippo.pacifici@gmail.com>
2020-05-15 14:49:15 -0700

    AST to support prewhere

--- snuba/query/processors/prewhere.py
+++ snuba/query/processors/prewhere.py
@@ -3,6 +3,14 @@ from typing import Optional, Sequence
 from snuba import settings, util
 from snuba.clickhouse.processors import QueryProcessor
 from snuba.clickhouse.query import Query
+from snuba.query.accessors import get_columns_in_expression
+from snuba.query.conditions import (
+    OPERATOR_TO_FUNCTION,
+    BooleanFunctions,
+    combine_conditions,
+    get_first_level_conditions,
+)
+from snuba.query.expressions import Column, FunctionCall
 from snuba.query.types import Condition
 from snuba.request.request_settings import RequestSettings
 
@@ -18,6 +26,8 @@ ALLOWED_OPERATORS = [
     "IS NOT NULL",
     "LIKE",
 ]
+
+ALLOWED_AST_OPERATORS = [OPERATOR_TO_FUNCTION[o] for o in ALLOWED_OPERATORS]
 
 
 class PrewhereProcessor(QueryProcessor):
@@ -56,6 +66,24 @@ class PrewhereProcessor(QueryProcessor):
             and cond[1] in ALLOWED_OPERATORS
             and any(col in prewhere_keys for col in util.columns_in_expr(cond[0]))
         ]
+
+        ast_condition = query.get_condition_from_ast()
+        if ast_condition:
+            ast_prewhere_candidates = [
+                (get_columns_in_expression(cond), cond)
+                for cond in ast_condition
+                if isinstance(cond, FunctionCall)
+                and len(cond.parameters) > 0
+                and isinstance(cond.parameters[0], Column)
+                and cond.function_name in ALLOWED_AST_OPERATORS
+                and any(
+                    col.column_name in prewhere_keys
+                    for col in get_columns_in_expression(cond)
+                )
+            ]
+        else:
+            ast_prewhere_candidates = []
+
         # Use the condition that has the highest priority (based on the
         # position of its columns in the prewhere keys list)
         prewhere_candidates = sorted(
@@ -70,6 +98,20 @@ class PrewhereProcessor(QueryProcessor):
             ],
             key=lambda priority_and_col: priority_and_col[0],
         )
+        ast_prewhere_candidates = sorted(
+            [
+                (
+                    min(
+                        prewhere_keys.index(col.column_name)
+                        for col in cols
+                        if col.column_name in prewhere_keys
+                    ),
+                    cond,
+                )
+                for cols, cond in ast_prewhere_candidates
+            ],
+            key=lambda priority_and_col: priority_and_col[0],
+        )
         if prewhere_candidates:
             prewhere_conditions = [cond for _, cond in prewhere_candidates][
                 :max_prewhere_conditions
@@ -77,4 +119,24 @@ class PrewhereProcessor(QueryProcessor):
             query.set_conditions(
                 list(filter(lambda cond: cond not in prewhere_conditions, conditions))
             )
-        query.set_prewhere(prewhere_conditions)
+            query.set_prewhere(prewhere_conditions)
+
+        if ast_condition and ast_prewhere_candidates:
+            ast_prewhere_conditions = [cond for _, cond in ast_prewhere_candidates][
+                :max_prewhere_conditions
+            ]
+            top_level_conditions = get_first_level_conditions(ast_condition)
+            query.replace_ast_condition(
+                combine_conditions(
+                    list(
+                        filter(
+                            lambda cond: cond not in ast_prewhere_conditions,
+                            top_level_conditions,
+                        )
+                    ),
+                    BooleanFunctions.AND,
+                )
+            )
+            query.replace_ast_condition(
+                combine_conditions(ast_prewhere_conditions, BooleanFunctions.AND)
+            )
