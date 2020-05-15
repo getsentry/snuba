8927b33b97f45059bf98a8685b61c88255e83d79
Ted Kaemming <ted@kaemming.com>
2020-05-15 15:20:50 -0700

    partitioner testing doesn't require the subscription setup/teardown

--- tests/subscriptions/test_partitioner.py
+++ tests/subscriptions/test_partitioner.py
@@ -3,15 +3,11 @@ from datetime import timedelta
 from snuba.datasets.table_storage import KafkaTopicSpec
 from snuba.subscriptions.data import SubscriptionData
 from snuba.subscriptions.partitioner import TopicSubscriptionDataPartitioner
-from tests.subscriptions import BaseSubscriptionTest
 
 
-class TestBuildRequest(BaseSubscriptionTest):
-    def test(self):
-        data = SubscriptionData(
-            123, [], [], timedelta(minutes=10), timedelta(minutes=1)
-        )
-        partitioner = TopicSubscriptionDataPartitioner(
-            KafkaTopicSpec("topic", partitions_number=64)
-        )
-        assert partitioner.build_partition_id(data) == 18
+def test_partitioning() -> None:
+    data = SubscriptionData(123, [], [], timedelta(minutes=10), timedelta(minutes=1))
+    partitioner = TopicSubscriptionDataPartitioner(
+        KafkaTopicSpec("topic", partitions_number=64)
+    )
+    assert partitioner.build_partition_id(data) == 18
