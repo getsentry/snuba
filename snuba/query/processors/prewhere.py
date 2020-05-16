a7f009afec1ef854b42babf4939c18bb5e121012
Filippo Pacifici <filippo.pacifici@gmail.com>
2020-05-15 17:25:06 -0700

    Reorganize the prewhere class

--- snuba/query/processors/prewhere.py
+++ snuba/query/processors/prewhere.py
@@ -1,4 +1,5 @@
-from typing import Optional, Sequence
+from abc import ABC, abstractmethod
+from typing import Generic, Iterable, Optional, Sequence, Tuple, TypeVar
 
 from snuba import settings, util
 from snuba.clickhouse.processors import QueryProcessor
@@ -10,7 +11,7 @@ from snuba.query.conditions import (
     combine_conditions,
     get_first_level_conditions,
 )
-from snuba.query.expressions import Column, FunctionCall
+from snuba.query.expressions import Column, Expression, FunctionCall
 from snuba.query.types import Condition
 from snuba.request.request_settings import RequestSettings
 
@@ -26,8 +27,6 @@ ALLOWED_OPERATORS = [
     "IS NOT NULL",
     "LIKE",
 ]
-
-ALLOWED_AST_OPERATORS = [OPERATOR_TO_FUNCTION[o] for o in ALLOWED_OPERATORS]
 
 
 class PrewhereProcessor(QueryProcessor):
@@ -52,43 +51,34 @@ class PrewhereProcessor(QueryProcessor):
         prewhere_keys = query.get_data_source().get_prewhere_candidates()
         if not prewhere_keys:
             return
-        prewhere_conditions: Sequence[Condition] = []
-        # Add any condition to PREWHERE if:
-        # - It is a single top-level condition (not OR-nested), and
-        # - Any of its referenced columns are in prewhere_keys
-        conditions = query.get_conditions()
-        if not conditions:
-            return
-        prewhere_candidates = [
-            (util.columns_in_expr(cond[0]), cond)
-            for cond in conditions
-            if util.is_condition(cond)
-            and cond[1] in ALLOWED_OPERATORS
-            and any(col in prewhere_keys for col in util.columns_in_expr(cond[0]))
-        ]
 
-        ast_condition = query.get_condition_from_ast()
-        if ast_condition:
-            ast_prewhere_candidates = [
-                (get_columns_in_expression(cond), cond)
-                for cond in get_first_level_conditions(ast_condition)
-                if isinstance(cond, FunctionCall)
-                and len(cond.parameters) > 0
-                and cond.function_name in ALLOWED_AST_OPERATORS
-                and any(
-                    col.column_name in prewhere_keys
-                    for col in get_columns_in_expression(cond)
-                )
-            ]
-        else:
-            ast_prewhere_candidates = []
+        LegacyPrewhereProcessor().process_query(
+            query, max_prewhere_conditions, prewhere_keys
+        )
+        ASTPrewhereProcessor().process_query(
+            query, max_prewhere_conditions, prewhere_keys
+        )
+
+
+TCondition = TypeVar("TCondition")
+TColumn = TypeVar("TColumn")
+
+
+class PrewhereProcessorDelegate(ABC, Generic[TCondition, TColumn]):
+    def process_query(
+        self, query: Query, max_prewhere_conditions: int, prewhere_keys: Sequence[str]
+    ) -> None:
+        prewhere_candidates = self._get_prewhere_candidates(query, prewhere_keys)
+
         # Use the condition that has the highest priority (based on the
         # position of its columns in the prewhere keys list)
-        prewhere_candidates = sorted(
+        sorted_candidates = sorted(
             [
                 (
                     min(
-                        prewhere_keys.index(col) for col in cols if col in prewhere_keys
+                        prewhere_keys.index(self._get_column_name(col))
+                        for col in cols
+                        if self._get_column_name(col) in prewhere_keys
                     ),
                     cond,
                 )
@@ -96,48 +86,107 @@ class PrewhereProcessor(QueryProcessor):
             ],
             key=lambda priority_and_col: priority_and_col[0],
         )
-        ast_prewhere_candidates = sorted(
+
+        prewhere_conditions = [cond for _, cond in sorted_candidates][
+            :max_prewhere_conditions
+        ]
+
+        self._update_conditions(query, prewhere_conditions)
+
+    @abstractmethod
+    def _get_prewhere_candidates(
+        self, query: Query, prewhere_keys: Sequence[str]
+    ) -> Sequence[Tuple[Iterable[TColumn], TCondition]]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def _get_column_name(self, column: TColumn) -> str:
+        raise NotImplementedError
+
+    @abstractmethod
+    def _update_conditions(
+        self, query: Query, prewhere_conditions: Sequence[TCondition]
+    ) -> None:
+        raise NotImplementedError
+
+
+class LegacyPrewhereProcessor(PrewhereProcessorDelegate[Condition, str]):
+    def _get_prewhere_candidates(
+        self, query: Query, prewhere_keys: Sequence[str]
+    ) -> Sequence[Tuple[Iterable[str], Condition]]:
+        # Add any condition to PREWHERE if:
+        # - It is a single top-level condition (not OR-nested), and
+        # - Any of its referenced columns are in prewhere_keys
+        conditions = query.get_conditions()
+        if not conditions:
+            return []
+        return [
+            (util.columns_in_expr(cond[0]), cond)
+            for cond in conditions
+            if util.is_condition(cond)
+            and cond[1] in ALLOWED_OPERATORS
+            and any(col in prewhere_keys for col in util.columns_in_expr(cond[0]))
+        ]
+
+    def _get_column_name(self, column: str) -> str:
+        return column
+
+    def _update_conditions(
+        self, query: Query, prewhere_conditions: Sequence[Condition]
+    ) -> None:
+        conditions = query.get_conditions()
+        if not conditions:
+            return
+        query.set_conditions(
+            list(filter(lambda cond: cond not in prewhere_conditions, conditions))
+        )
+        query.set_prewhere(prewhere_conditions)
+
+
+class ASTPrewhereProcessor(PrewhereProcessorDelegate[Expression, Column]):
+
+    allowed_ast_operators = [OPERATOR_TO_FUNCTION[o] for o in ALLOWED_OPERATORS]
+
+    def _get_prewhere_candidates(
+        self, query: Query, prewhere_keys: Sequence[str]
+    ) -> Sequence[Tuple[Iterable[Column], Expression]]:
+        ast_condition = query.get_condition_from_ast()
+        return (
             [
-                (
-                    min(
-                        prewhere_keys.index(col.column_name)
-                        for col in cols
-                        if col.column_name in prewhere_keys
-                    ),
-                    cond,
+                (get_columns_in_expression(cond), cond)
+                for cond in get_first_level_conditions(ast_condition)
+                if isinstance(cond, FunctionCall)
+                and len(cond.parameters) > 0
+                and cond.function_name in self.allowed_ast_operators
+                and any(
+                    col.column_name in prewhere_keys
+                    for col in get_columns_in_expression(cond)
                 )
-                for cols, cond in ast_prewhere_candidates
-            ],
-            key=lambda priority_and_col: priority_and_col[0],
+            ]
+            if ast_condition
+            else []
         )
 
-        if prewhere_candidates:
-            prewhere_conditions = [cond for _, cond in prewhere_candidates][
-                :max_prewhere_conditions
-            ]
-            query.set_conditions(
-                list(filter(lambda cond: cond not in prewhere_conditions, conditions))
-            )
-            query.set_prewhere(prewhere_conditions)
+    def _get_column_name(self, column: Column) -> str:
+        return column.column_name
 
-        if ast_condition and ast_prewhere_candidates:
-            ast_prewhere_conditions = [cond for _, cond in ast_prewhere_candidates][
-                :max_prewhere_conditions
-            ]
-            top_level_conditions = get_first_level_conditions(ast_condition)
-            new_conditions = list(
-                filter(
-                    lambda cond: cond not in ast_prewhere_conditions,
-                    top_level_conditions,
-                )
-            )
-            query.replace_ast_condition(
-                combine_conditions(new_conditions, BooleanFunctions.AND)
-                if new_conditions
-                else None
-            )
-            query.replace_prewhere_ast_condition(
-                combine_conditions(ast_prewhere_conditions, BooleanFunctions.AND)
-                if ast_prewhere_conditions
-                else None
-            )
+    def _update_conditions(
+        self, query: Query, prewhere_conditions: Sequence[Expression]
+    ) -> None:
+        ast_condition = query.get_condition_from_ast()
+        if not ast_condition:
+            return
+        top_level_conditions = get_first_level_conditions(ast_condition)
+        new_conditions = list(
+            filter(lambda cond: cond not in prewhere_conditions, top_level_conditions)
+        )
+        query.replace_ast_condition(
+            combine_conditions(new_conditions, BooleanFunctions.AND)
+            if new_conditions
+            else None
+        )
+        query.replace_prewhere_ast_condition(
+            combine_conditions(prewhere_conditions, BooleanFunctions.AND)
+            if prewhere_conditions
+            else None
+        )
