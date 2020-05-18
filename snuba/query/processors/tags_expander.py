9e650bb5a3c86175ea61a8e8672dce06e70a46dc
Filippo Pacifici <filippo.pacifici@gmail.com>
2020-05-18 10:48:21 -0700

    fix(parser) Parse tags_key/tags_value as FunctionCall and not Column (#937)
    tags_key/tags_value should be transformed into FunctionCall early on in the query processing.

---
+++ snuba/query/processors/tags_expander.py
@@ -0,0 +1,38 @@
+from dataclasses import replace
+
+from snuba.query.expressions import Column, Expression, FunctionCall
+from snuba.query.logical import Query
+from snuba.query.processors import QueryProcessor
+from snuba.request.request_settings import RequestSettings
+
+
+class TagsExpanderProcessor(QueryProcessor):
+    """
+    Transforms the special syntax we provide to expand the tags column into a call
+    to arrayJoin so that, after this query processor, tags_key and tags_value special
+    columns are not treated as a special case but they are valid expressions on a
+    valid column name.
+    """
+
+    def process_query(self, query: Query, request_settings: RequestSettings) -> None:
+        def transform_expression(exp: Expression) -> Expression:
+            # This is intentionally not configurable in order to discourage creating
+            # a special syntax for expressions that should be function calls.
+            if isinstance(exp, Column) and exp.column_name in (
+                "tags_key",
+                "tags_value",
+            ):
+                return FunctionCall(
+                    exp.alias or exp.column_name,
+                    "arrayJoin",
+                    (
+                        replace(
+                            exp,
+                            alias=None,
+                            column_name=exp.column_name.replace("_", "."),
+                        ),
+                    ),
+                )
+            return exp
+
+        query.transform_expressions(transform_expression)
