fee3cbc16f5694d03ce24eac370987bb9a5d946b
Filippo Pacifici <filippo.pacifici@gmail.com>
2020-05-21 22:12:44 -0700

    Add test

--- tests/clickhouse/translators/snuba/test_multistep.py
+++ tests/clickhouse/translators/snuba/test_multistep.py
@@ -9,28 +9,51 @@ from snuba.query.expressions import (
     Argument,
     Lambda,
 )
-from snuba.clickhouse.translators.snuba.rulesbased import TranslationRules
+from snuba.clickhouse.translators.snuba.rulesbased import (
+    TranslationRules,
+    SnubaClickhouseRulesTranslator,
+)
+from snuba.clickhouse.translators.snuba.multistep import (
+    MultiStepSnubaClickhouseTranslator,
+)
 from snuba.clickhouse.translators.snuba.rules import SimpleColumnMapper
 
 
 class FakeMultiTableStorageTranslator(ExpressionVisitor[Expression]):
     def visitLiteral(self, exp: Literal) -> Expression:
-        return exp.accept(self)
+        return exp
 
     def visitColumn(self, exp: Column) -> Expression:
-        return exp.accept(self)
+        if exp.column_name == "col_2":
+            return Column(exp.alias, "col_2b", "table_2b")
+        else:
+            return exp
 
     def visitSubscriptableReference(self, exp: SubscriptableReference) -> Expression:
-        return exp.accept(self)
+        return exp
 
     def visitFunctionCall(self, exp: FunctionCall) -> Expression:
-        return Column(exp.alias, "some_preaggregated_col", "a_table")
+        return exp
 
     def visitCurriedFunctionCall(self, exp: CurriedFunctionCall) -> Expression:
-        return exp.accept(self)
+        return exp
 
     def visitArgument(self, exp: Argument) -> Expression:
-        return exp.accept(self)
+        return exp
 
     def visitLambda(self, exp: Lambda) -> Expression:
-        return exp.accept(self)
+        return exp
+
+
+def test_multistep() -> None:
+    snuba_clickhouse_rules = TranslationRules(
+        columns=[SimpleColumnMapper("col", None, "col_2", None)]
+    )
+    translator = MultiStepSnubaClickhouseTranslator(
+        snuba_steps=[],
+        snuba_clickhouse_step=SnubaClickhouseRulesTranslator(snuba_clickhouse_rules),
+        clickhouse_steps=[FakeMultiTableStorageTranslator()],
+    )
+    expression = Column("alias", "col", None)
+    new_expression = expression.accept(translator)
+    assert new_expression == Column("alias", "col_2b", "table_2b")
