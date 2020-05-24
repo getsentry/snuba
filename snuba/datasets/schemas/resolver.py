a05e1ae464f203d8579981b9a625f6e7619bb02a
Filippo Pacifici <filippo.pacifici@gmail.com>
2020-05-24 15:00:27 -0700

    Add first implem

--- snuba/datasets/schemas/resolver.py
+++ snuba/datasets/schemas/resolver.py
@@ -1,9 +1,17 @@
 from abc import ABC, abstractmethod
-from typing import Optional, Sequence
+from typing import NamedTuple, Optional, Sequence
 
+from snuba.clickhouse.columns import ColumnSet
 from snuba.query.expressions import Column
 
+
 ColumnParts = Sequence[str]
+
+
+class ResolvedCol(NamedTuple):
+    table_name: Optional[str]
+    column_name: str
+    path: Sequence[str]
 
 
 class ColumnResolver(ABC):
@@ -15,17 +23,43 @@ class ColumnResolver(ABC):
     decompose a column expression found in a query into entity (table), base column name and
     path for nested columns.
 
-    In order to keep the coupling with the query language minimum, this expects the parser to
-    provide a structured representation of a column as a sequence of strings instead of the bare
-    representation (string) found in the query.
-    Still this cannot be totally decoupled from the query language.
+    TODO: Revisit this interface when we introduce entities. We may have to increase its
+    responsibilities depending on how we decide to infer, given a column, which entity defines it.
+    If we require the query to fully qualify all columns (errors.message for example), this can
+    stay as it is. If instead we will infer the requested entity from the context of the query
+    like we do today, this will need to know a lot more about the query, be coupled to that
+    structure and being potentially able to make wider changes to the query itself.
+    Moving such responsibility here would be easy enough that we can keep this simpler till
+    entities are introduced.
     """
 
     @abstractmethod
-    def resolve_column(self, query_column: ColumnParts) -> Optional[Column]:
+    def resolve_column(self, query_column: str) -> Optional[ResolvedCol]:
         """
         Transforms the column parts found in the query (like ["events", "tags", "value"]) into
         a valid Column object for the logical AST. If the column cannot be resolved, this
         returns None.
         """
         raise NotImplementedError
+
+
+class SingleTableResolver(ColumnResolver):
+    def __init__(
+        self,
+        columns: ColumnSet,
+        virtual_column_names: Sequence[str],
+        table_name: Optional[str] = None,
+    ) -> None:
+        self.__columns = columns
+        self.__virtual_column_names = virtual_column_names
+        self.__table_name = table_name
+
+    def resolve_column(self, query_column: str) -> Optional[Column]:
+        schema_column = self.__columns.columns.get(query_column)
+        if schema_column:
+            return ResolvedCol(table_name=self.__table_name,)
+
+        elif query_column[0] in self.__virtual_column_names:
+            pass
+        else:
+            return None
