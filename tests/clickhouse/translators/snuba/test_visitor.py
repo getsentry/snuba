96333c9c3694ed1b5cb0a6824bc2f91e6aa474a7
Filippo Pacifici <filippo.pacifici@gmail.com>
2020-05-27 09:47:06 -0700

    feat(AST): Rule based expression translator (#936)
    This introduces an expression translator infrastructure we could use to translate Snuba Queries into Clickhouse queries that supports mapping rules defined for each storage and associated to the relationship between entity and storage.
    
    This PR introduces the Expression Translator, which is capable of translating one AST expression into another one, potentially in a different AST.
    The translation is executed by scanning the expression tree through a visitor and applying a list of rules for each node.
    If a rule applies it returns the translated value, if it does not it returns None.
    A followup PR will wire it up into a Query Translator that will translate all the expression in a Query building a new Query.
    
    Multiple sets of rules can be composed to translate joins and (with small extensions) to run a multi step translation for multi table storages (Snuba ast to Clickhouse ast and a second step from Clickhouse ast to Clickhouse ast).

---
+++ tests/clickhouse/translators/snuba/test_visitor.py
@@ -0,0 +1,54 @@
+import pytest
+
+from snuba.clickhouse.translators.snuba.mapping import (
+    SnubaClickhouseMappingTranslator,
+    TranslationMappers,
+)
+from snuba.query.expressions import (
+    Argument,
+    Column,
+    CurriedFunctionCall,
+    Expression,
+    FunctionCall,
+    Lambda,
+    Literal,
+    SubscriptableReference,
+)
+
+test_data = [
+    Column("alias", "col", "table"),
+    Literal("alias", 123),
+    Argument("alias", "arg"),
+    SubscriptableReference(
+        "tags[asd]", Column(None, "tags", None), Literal(None, "release")
+    ),
+    FunctionCall(
+        "alias",
+        "f",
+        (
+            Column(None, "col", "table"),
+            Literal(None, 123),
+            FunctionCall(None, "f1", (Column(None, "col2", None),)),
+        ),
+    ),
+    CurriedFunctionCall(
+        None,
+        FunctionCall(None, "f", (Column(None, "col", None), Literal(None, 12))),
+        (Column(None, "col3", None),),
+    ),
+    Lambda(None, ("a", "b"), FunctionCall(None, "f", (Argument(None, "a"),))),
+]
+
+
+@pytest.mark.parametrize("expression", test_data)
+def test_default_translation(expression: Expression) -> None:
+    """
+    Ensures that a translation that relies on the default translation rules
+    produces a deep copy of the original expression.
+    """
+
+    translated = expression.accept(
+        SnubaClickhouseMappingTranslator(TranslationMappers())
+    )
+
+    assert translated == expression
