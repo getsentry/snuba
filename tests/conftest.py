8045d18a6c60a4d5aecc377a9cd20a25f66f4ce7
Lyn Nagara <lyn.nagara@gmail.com>
2020-06-04 10:33:33 -0700

    test: Run all tests in a separate ClickHouse database
    We should run our tests in a separate ClickHouse database. This will
    allow us to keep tests better separated from dev and simplify the
    table name logic and not rely on preprepending the table names with `test_`.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,20 @@
+from snuba import settings
+from snuba.clickhouse.native import ClickhousePool
 from snuba.environment import setup_sentry
 
 
-def pytest_configure():
-    """ Set up the Sentry SDK to avoid errors hidden by configuration """
+def pytest_configure() -> None:
+    """
+    Set up the Sentry SDK to avoid errors hidden by configuration.
+    Ensure the snuba_test database exists
+    """
     setup_sentry()
+
+    # There is only one cluster in test, so fetch the host from there.
+    cluster = settings.CLUSTERS[0]
+
+    connection = ClickhousePool(
+        cluster["host"], cluster["port"], "default", "", "default",
+    )
+
+    connection.execute("CREATE DATABASE IF NOT EXISTS snuba_test;")
