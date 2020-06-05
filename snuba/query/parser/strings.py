3f7c1b96d3b77e20b6ff8fdfae1c68c5e8f34f79
Filippo Pacifici <filippo.pacifici@gmail.com>
2020-06-04 17:41:42 -0700

    Apply aliases during parsing

--- snuba/query/parser/strings.py
+++ snuba/query/parser/strings.py
@@ -14,22 +14,29 @@ NESTED_COL_EXPR_RE = re.compile(r"^([a-zA-Z0-9_\.]+)\[([a-zA-Z0-9_\.:-]+)\]$")
 
 def parse_string_to_expr(val: str) -> Expression:
     """
-    Parses a string expression found in a Snuba Query into an AST expression.
-    This is supposed to work for any non structured expression in the Snuba Query.
+    Parses a string expression found in a Snuba Query into an AST
+    expression. This is supposed to work for any non structured expression
+    in the Snuba Query.
+
+    This guarantees that string expressions that correspond to columns
+    the user expect to see in the response (columns and tags[] expressions)
+    have an alias in the AST. This alias corresponds to the string the
+    user put into the query (which is the name of the column they will
+    find in the result).
+    This is done so that the column name provided by the user is preserved
+    across the query processing pipeline and it is applied to the Clickhouse
+    query as alias.
+
+    As a consequence, the only requirement, when translating or transforming
+    an expression is to preserve the alias of the input expression regardless
+    to how it was generated, and by whom.
     """
     match = NESTED_COL_EXPR_RE.match(val)
     if match:
         col_name = match[1]
         key_name = match[2]
         return SubscriptableReference(
-            # We set the alias of this expression with the original value found in the Query
-            # (tags[asd]) so that it will be preserved across the query processing pipeline
-            # and it will be applied to the Clickhouse query as alias. As a consequence,
-            # no matter what expression this value is transformed into, the client will receive
-            # the value they provided in the response (as alias).
-            alias=val,
-            column=Column(None, None, col_name),
-            key=Literal(None, key_name),
+            alias=val, column=Column(None, None, col_name), key=Literal(None, key_name),
         )
 
     # TODO: This will use the schema of the dataset/entity to decide if the expression is
