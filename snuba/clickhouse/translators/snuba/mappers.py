d6711ca3d4f8505b084e1bf08b070952aad54efa
Filippo Pacifici <filippo.pacifici@gmail.com>
2020-06-05 09:41:40 -0700

    feat(ast): Sessions translator (#993)
    Adds the query translator for the sessions dataset. Like #992, #985 and #978
    
    It also adds a ColumnToCurriedFunction mapper since there is a curried function used in the sessions dataset.

--- snuba/clickhouse/translators/snuba/mappers.py
+++ snuba/clickhouse/translators/snuba/mappers.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from typing import Optional, Tuple
 
@@ -5,10 +6,11 @@ from snuba.clickhouse.translators.snuba import SnubaClickhouseStrictTranslator
 from snuba.clickhouse.translators.snuba.allowed import (
     ColumnMapper,
     SubscriptableReferenceMapper,
+    ValidColumnMappings,
 )
 from snuba.query.dsl import arrayElement
 from snuba.query.expressions import Column as ColumnExpr
-from snuba.query.expressions import Expression
+from snuba.query.expressions import CurriedFunctionCall, Expression
 from snuba.query.expressions import FunctionCall as FunctionCallExpr
 from snuba.query.expressions import Literal as LiteralExpr
 from snuba.query.expressions import OptionalScalarType, SubscriptableReference
@@ -25,93 +27,105 @@ from snuba.util import qualified_column
 
 
 @dataclass(frozen=True)
-class ColumnToColumn(ColumnMapper):
+class ColumnToExpression(ColumnMapper, ABC):
+    """
+    Provides some common logic for all the mappers that transform a specific
+    column identified by table and name into one of the allowed expressions
+    a column can be translated into.
+    This exists for convenience just to avoid some code duplication.
+    """
+
+    from_table_name: Optional[str]
+    from_col_name: str
+
+    def attempt_map(
+        self,
+        expression: ColumnExpr,
+        children_translator: SnubaClickhouseStrictTranslator,
+    ) -> Optional[ValidColumnMappings]:
+        if (
+            expression.column_name == self.from_col_name
+            and expression.table_name == self.from_table_name
+        ):
+            return self._produce_output(expression)
+        else:
+            return None
+
+    @abstractmethod
+    def _produce_output(self, expression: ColumnExpr,) -> ValidColumnMappings:
+        raise NotImplementedError
+
+
+@dataclass(frozen=True)
+class ColumnToColumn(ColumnToExpression):
     """
     Maps a column with a name and a table into a column with a different name and table.
 
     The alias is not transformed.
     """
 
-    from_table_name: Optional[str]
-    from_col_name: str
     to_table_name: Optional[str]
     to_col_name: str
 
-    def attempt_map(
-        self,
-        expression: ColumnExpr,
-        children_translator: SnubaClickhouseStrictTranslator,
-    ) -> Optional[ColumnExpr]:
-        if (
-            expression.column_name == self.from_col_name
-            and expression.table_name == self.from_table_name
-        ):
-            return ColumnExpr(
-                alias=expression.alias
-                or qualified_column(self.from_col_name, self.from_table_name or ""),
-                table_name=self.to_table_name,
-                column_name=self.to_col_name,
-            )
-        else:
-            return None
+    def _produce_output(self, expression: ColumnExpr) -> ColumnExpr:
+        return ColumnExpr(
+            alias=expression.alias
+            or qualified_column(self.from_col_name, self.from_table_name or ""),
+            table_name=self.to_table_name,
+            column_name=self.to_col_name,
+        )
 
 
 @dataclass(frozen=True)
-class ColumnToLiteral(ColumnMapper):
+class ColumnToLiteral(ColumnToExpression):
     """
     Maps a column name into a hardcoded literal preserving the alias.
     """
 
-    from_table_name: Optional[str]
-    from_col_name: str
     to_literal_value: OptionalScalarType
 
-    def attempt_map(
-        self,
-        expression: ColumnExpr,
-        children_translator: SnubaClickhouseStrictTranslator,
-    ) -> Optional[LiteralExpr]:
-        if (
-            expression.table_name == self.from_table_name
-            and expression.column_name == self.from_col_name
-        ):
-            return LiteralExpr(
-                alias=expression.alias
-                or qualified_column(self.from_col_name, self.from_table_name or ""),
-                value=self.to_literal_value,
-            )
-        else:
-            return None
+    def _produce_output(self, expression: ColumnExpr,) -> LiteralExpr:
+        return LiteralExpr(
+            alias=expression.alias
+            or qualified_column(self.from_col_name, self.from_table_name or ""),
+            value=self.to_literal_value,
+        )
 
 
 @dataclass(frozen=True)
-class ColumnToFunction(ColumnMapper):
+class ColumnToFunction(ColumnToExpression):
     """
     Maps a column into a function expression that preserves the alias.
     """
 
-    from_table_name: Optional[str]
-    from_col_name: str
     to_function_name: str
     to_function_params: Tuple[Expression, ...]
 
-    def attempt_map(
-        self,
-        expression: ColumnExpr,
-        children_translator: SnubaClickhouseStrictTranslator,
-    ) -> Optional[FunctionCallExpr]:
-        if (
-            expression.table_name == self.from_table_name
-            and expression.column_name == self.from_col_name
-        ):
-            return FunctionCallExpr(
-                alias=expression.alias
-                or qualified_column(self.from_col_name, self.from_table_name or ""),
-                function_name=self.to_function_name,
-                parameters=self.to_function_params,
-            )
-        else:
-            return None
+    def _produce_output(self, expression: ColumnExpr) -> FunctionCallExpr:
+        return FunctionCallExpr(
+            alias=expression.alias
+            or qualified_column(self.from_col_name, self.from_table_name or ""),
+            function_name=self.to_function_name,
+            parameters=self.to_function_params,
+        )
+
+
+@dataclass(frozen=True)
+class ColumnToCurriedFunction(ColumnToExpression):
+    """
+    Maps a column into a curried function expression that preserves the alias.
+    """
+
+    to_internal_function: FunctionCallExpr
+    to_function_params: Tuple[Expression, ...]
+
+    def _produce_output(self, expression: ColumnExpr) -> CurriedFunctionCall:
+        return CurriedFunctionCall(
+            alias=expression.alias
+            or qualified_column(self.from_col_name, self.from_table_name or ""),
+            internal_function=self.to_internal_function,
+            parameters=self.to_function_params,
+        )
 
 
 @dataclass(frozen=True)
@@ -146,48 +160,39 @@ class SubscriptableMapper(SubscriptableReferenceMapper):
 
 
 @dataclass(frozen=True)
-class ColumnToMapping(ColumnMapper):
+class ColumnToMapping(ColumnToExpression):
     """
     Maps a column into a mapping expression thus into a Clickhouse
     array access.
     """
 
-    from_column_table: Optional[str]
-    from_column_name: str
-    to_nested_col_table: Optional[str]
+    to_nested_col_table_name: Optional[str]
     to_nested_col_name: str
-    to_nested_tag_key: str
+    to_nested_mapping_key: str
 
-    def attempt_map(
-        self,
-        expression: ColumnExpr,
-        children_translator: SnubaClickhouseStrictTranslator,
-    ) -> Optional[FunctionCallExpr]:
-        if (
-            expression.table_name == self.from_column_table
-            and expression.column_name == self.from_column_name
-        ):
-            return build_mapping_expr(
-                expression.alias
-                or qualified_column(
-                    self.from_column_name, self.from_column_table or ""
-                ),
-                self.to_nested_col_table,
-                self.to_nested_col_name,
-                LiteralExpr(None, self.to_nested_tag_key),
-            )
-        else:
-            return None
+    def _produce_output(self, expression: ColumnExpr) -> FunctionCallExpr:
+        return build_mapping_expr(
+            expression.alias
+            or qualified_column(self.from_col_name, self.from_table_name or ""),
+            self.to_nested_col_table_name,
+            self.to_nested_col_name,
+            LiteralExpr(None, self.to_nested_mapping_key),
+        )
 
 
 def build_mapping_expr(
-    alias: Optional[str], table_name: Optional[str], col_name: str, tag_key: Expression
+    alias: Optional[str],
+    table_name: Optional[str],
+    col_name: str,
+    mapping_key: Expression,
 ) -> FunctionCallExpr:
     return arrayElement(
         alias,
         ColumnExpr(None, table_name, f"{col_name}.value"),
         FunctionCallExpr(
-            None, "indexOf", (ColumnExpr(None, table_name, f"{col_name}.key"), tag_key),
+            None,
+            "indexOf",
+            (ColumnExpr(None, table_name, f"{col_name}.key"), mapping_key),
         ),
     )
 
