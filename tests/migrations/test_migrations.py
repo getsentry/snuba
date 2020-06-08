6bb3ed822f0f6d88f1470f49a0fb0107ab357ca1
Lyn Nagara <lyn.nagara@gmail.com>
2020-06-08 12:55:10 -0700

    update test

--- tests/migrations/test_migrations.py
+++ tests/migrations/test_migrations.py
@@ -4,3 +4,13 @@ from snuba.migrations import migrations
 def test_run_migration() -> None:
     manager = migrations.Runner()
     manager.run_migration(migrations.App.SYSTEM, "0001_migrations")
+
+    from snuba.clusters.cluster import ClickhouseClientSettings, get_cluster
+    from snuba.clusters.storage_sets import StorageSetKey
+
+    connection = get_cluster(StorageSetKey.MIGRATIONS).get_query_connection(
+        ClickhouseClientSettings.MIGRATE
+    )
+    assert connection.execute(
+        "SELECT app, migration_id, status FROM migrations_local FINAL;"
+    ) == [("system", "0001_migrations", "completed")]
