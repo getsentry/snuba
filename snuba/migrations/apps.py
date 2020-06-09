027fe9082e411e05a123a1f67598b02d533d3388
Lyn Nagara <lyn.nagara@gmail.com>
2020-06-09 16:33:12 -0700

    add docstring

--- snuba/migrations/apps.py
+++ snuba/migrations/apps.py
@@ -5,23 +5,33 @@ from snuba.migrations.migration import App, Migration
 
 
 class AppLoader(ABC):
+    """
+    Provides the list of migrations associated with an app and a loader that returns
+    the requested migration.
+    """
+
     @abstractmethod
     def load_migration(self, migration_id: str) -> Migration:
         raise NotImplementedError
 
 
 class DirectoryLoader(AppLoader):
+    """
+    Loads migrations that are defined as files of a directory. The file name
+    represents the migration ID.
+    """
+
     def __init__(self, module: str):
         self.__module = module
 
     def load_migration(self, migration_id: str) -> Migration:
-        module = import_module(f"snuba.migrations.{self.__module}.{migration_id}")
+        module = import_module(f"{self.__module}.{migration_id}")
         return module.Migration()  # type: ignore
 
 
 _REGISTERED_APPS = {
-    App.SYSTEM: DirectoryLoader("system"),
-    App.SNUBA: DirectoryLoader("snuba"),
+    App.SYSTEM: DirectoryLoader("snuba.migrations.system"),
+    App.SNUBA: DirectoryLoader("snuba.migrations.snuba"),
 }
 
 
