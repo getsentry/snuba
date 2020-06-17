41682f589be31c4547d2b68582beeb6035acb8c0
Riya Chakraborty <riyachakraborty@Riyas-MacBook-Pro.local>
2020-06-17 18:56:02 -0500

    Intermediate changes for Mandatory Condition Applier and assoc. test.

--- snuba/query/processors/mandatory_condition_applier.py
+++ snuba/query/processors/mandatory_condition_applier.py
@@ -2,21 +2,21 @@ from snuba.query.logical import Query
 from snuba.clickhouse.processors import QueryProcessor
 from snuba.request.request_settings import RequestSettings
 
-from snuba.request import Request
-from snuba.datasets.dataset import Dataset
+# from snuba.request import Request
+# from snuba.datasets.dataset import Dataset
 from snuba.query.conditions import combine_and_conditions
 
 
 class MandatoryConditionApplier(QueryProcessor):
     def process_query(self, query: Query, request_settings: RequestSettings) -> None:
 
-        query_plan = Dataset.get_query_plan_builder().build_plan(Request)
+        # query_plan = Dataset.get_query_plan_builder().build_plan(Request)
 
-        relational_source = query_plan.query.get_data_source()
+        relational_source = query.get_data_source()
         mandatory_conditions = relational_source.get_mandatory_conditions()
-        query_plan.query.add_conditions([c.legacy for c in mandatory_conditions])
+        query.add_conditions([c.legacy for c in mandatory_conditions])
 
         if len(mandatory_conditions) > 0:
-            query_plan.query.add_condition_to_ast(
+            query.add_condition_to_ast(
                 combine_and_conditions([c.ast for c in mandatory_conditions])
             )
--- tests/query/processors/test_mandatory_condition_applier.py
+++ tests/query/processors/test_mandatory_condition_applier.py
@@ -11,8 +11,8 @@ from snuba.request.request_settings import HTTPRequestSettings
 
 
 test_data = [
-    ("table1", [["time1", "=", "1"]]),
-    ("table2", [["time2", "=", "2"]]),
+    ("table1", ["time1", "=", "1"]),
+    ("table2", ["time2", "=", "2"]),
 ]
 
 
@@ -36,7 +36,7 @@ def test_prewhere(table, mand_condition) -> None:
     query = Query(body, TableSource(table, cols, mand_condition, ["c1"]),)
 
     request_settings = HTTPRequestSettings(consistent=consistent)
-    processor = MandatoryConditionApplier("sentry_dist", "sentry_ro")
+    processor = MandatoryConditionApplier()
     processor.process_query(query, request_settings)
 
     source = query.get_data_source()
@@ -46,6 +46,4 @@ def test_prewhere(table, mand_condition) -> None:
     assert source.get_prewhere_candidates() == ["c1"]
 
     # want to check that the mandatory condition from TableSource object was added
-    assert source.get_mandatory_conditions() == body["conditions"].append(
-        mand_condition[0]
-    )
+    assert source.get_conditions() == body["conditions"].append(mand_condition)
