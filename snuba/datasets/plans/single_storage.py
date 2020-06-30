bcd9a0bae41a0a0fa1c15b8e05cd63e7785bd2cc
Riya Chakraborty <47572810+ayirr7@users.noreply.github.com>
2020-06-30 11:49:20 -0500

    feat (dataset) Add Mandatory Condition Applier to Clickhouse Query Plan Builder (#1056)
    * Add Mandatory Condition Applier (Query Processor) to Clickhouse Query Plan Builder.
    
    * Add Mandatory Condition Applier as query processor to Clickhouse query plan builder.
    
    * Removed old mandatory condition apply logic (due to implementation of new Query Processor).
    
    * Removed TO DO comment (as task has been completed).

--- snuba/datasets/plans/single_storage.py
+++ snuba/datasets/plans/single_storage.py
@@ -26,6 +26,8 @@ from snuba.request.request_settings import RequestSettings
 from snuba.util import with_span
 from snuba.web import QueryResult
 
+from snuba.query.processors.mandatory_condition_applier import MandatoryConditionApplier
+
 
 class SimpleQueryPlanExecutionStrategy(QueryPlanExecutionStrategy):
     def __init__(
@@ -111,6 +113,7 @@ class SingleStorageQueryPlanBuilder(ClickhouseQueryPlanBuilder):
                 db_query_processors=[
                     *self.__storage.get_query_processors(),
                     *self.__post_processors,
+                    MandatoryConditionApplier(),
                 ],
                 splitters=self.__storage.get_query_splitters(),
             ),
@@ -150,6 +153,7 @@ class SelectedStorageQueryPlanBuilder(ClickhouseQueryPlanBuilder):
                 db_query_processors=[
                     *storage.get_query_processors(),
                     *self.__post_processors,
+                    MandatoryConditionApplier(),
                 ],
                 splitters=storage.get_query_splitters(),
             ),
