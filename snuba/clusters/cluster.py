37618e6354da8dd22559b638e091eae5f7d7298e
Lyn Nagara <lyn.nagara@gmail.com>
2020-06-30 10:29:39 -0700

    remove unused method

--- snuba/clusters/cluster.py
+++ snuba/clusters/cluster.py
@@ -244,9 +244,6 @@ class ClickhouseCluster(Cluster[SqlQuery, ClickhouseWriterOptions]):
     def get_cluster_name(self) -> Optional[str]:
         return self.__cluster_name
 
-    def get_distributed_cluster_name(self) -> Optional[str]:
-        return self.__distributed_cluster_name
-
     def get_database(self) -> str:
         return self.__database
 
