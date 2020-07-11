4de46471da3b58cfdb54d21b6c8dcd38807121bf
Riya Chakraborty <riyamch@yahoo.com>
2020-07-10 22:25:05 -0500

    Fixed Simple Arithmetic Expression Parser (generic_visit is identical to existing parser's now).

--- snuba/query/parser/simple_arithm_expressions.py
+++ snuba/query/parser/simple_arithm_expressions.py
@@ -1,7 +1,9 @@
 from parsimonious.grammar import Grammar
 from parsimonious.nodes import NodeVisitor
 
-from snuba.query.expressions import FunctionCall, Literal
+from snuba.query.expressions import Literal
+from snuba.query.dsl import plus, multiply
+
 
 grammar = Grammar(
     r"""
@@ -24,27 +26,34 @@ class PrefixVisitor(NodeVisitor):
         return Literal(None, int(node.text))
 
     def visit_term(self, node, children):
+        print(children)
         factor, term = children
 
-        if all(x is None for x in term):
+        if term is None:
             # A check for any None child nodes
             # that arose from empty space ''
             return factor
         else:
-            return FunctionCall(None, term[0][0], (factor, term[0][1]))
+            # return FunctionCall(None, term[0][0], (factor, term[0][1]))
+            return multiply(factor, term[1])
 
     def visit_expression(self, node, children):
+        print(children)
         term, exp = children
 
-        if all(x is None for x in exp):
+        if exp is None:
             return term
         else:
-            return FunctionCall(None, exp[0][0], (term, exp[0][1]))
+            # return FunctionCall(None, exp[0][0], (term, exp[0][1]))
+            return plus(term, exp[1])
 
     def generic_visit(self, node, children):
+        if isinstance(children, list) and len(children) == 1:
+            return children[0]
+
         return children or node.text
 
 
-exp_tree = grammar.parse("5*4*3+2*1+6")
+exp_tree = grammar.parse("7*5*4*3+2*1+6")
 parsed_exp = PrefixVisitor().visit(exp_tree)
 print(parsed_exp)
