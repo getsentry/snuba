25f94b5cb216b7d14ce51331e3df32606d0ebeb8
Filippo Pacifici <filippo.pacifici@gmail.com>
2020-07-20 18:20:48 -0700

    Add distributed migrationn

--- snuba/migrations/snuba_migrations/querylog/0002_status_type_change.py
+++ snuba/migrations/snuba_migrations/querylog/0002_status_type_change.py
@@ -11,37 +11,43 @@ class Migration(migration.MultiStepMigration):
     now that the support for low cardinality strings is better.
     """
 
-    blocking = False
+    blocking = True
 
-    def forwards_local(self) -> Sequence[operations.Operation]:
+    def __forward_migrations(self, table_name: str) -> Sequence[operations.Operation]:
         return [
             operations.ModifyColumn(
                 StorageSetKey.QUERYLOG,
-                "querylog_local",
+                table_name,
                 Column("status", LowCardinality(String())),
             ),
             operations.ModifyColumn(
                 StorageSetKey.QUERYLOG,
-                "querylog_local",
+                table_name,
                 Column("clickhouse_queries.status", Array(LowCardinality(String()))),
             ),
         ]
 
-    def backwards_local(self) -> Sequence[operations.Operation]:
+    def __backwards_migrations(self, table_name: str) -> Sequence[operations.Operation]:
         status_type = Enum([("success", 0), ("error", 1), ("rate-limited", 2)])
         return [
             operations.ModifyColumn(
-                StorageSetKey.QUERYLOG, "querylog_local", Column("status", status_type),
+                StorageSetKey.QUERYLOG, table_name, Column("status", status_type),
             ),
             operations.ModifyColumn(
                 StorageSetKey.QUERYLOG,
-                "querylog_local",
+                table_name,
                 Column("clickhouse_queries.status", Array(status_type)),
             ),
         ]
 
+    def forwards_local(self) -> Sequence[operations.Operation]:
+        return self.__forward_migrations("querylog_local")
+
+    def backwards_local(self) -> Sequence[operations.Operation]:
+        return self.__backwards_migrations("querylog_local")
+
     def forwards_dist(self) -> Sequence[operations.Operation]:
-        return []
+        return self.__forward_migrations("querylog_dist")
 
     def backwards_dist(self) -> Sequence[operations.Operation]:
-        return []
+        return self.__backwards_migrations("querylog_dist")
