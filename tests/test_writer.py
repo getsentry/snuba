035a75a6c1866239647ffd137fb75185bb08dc26
Ted Kaemming <ted@kaemming.com>
2020-07-22 16:57:31 -0700

    Simplify `TestHTTPBatchWriter`.

--- tests/test_writer.py
+++ tests/test_writer.py
@@ -1,16 +1,9 @@
-from typing import Iterable
-
 import pytest
 
 from snuba.clickhouse.errors import ClickhouseError
 from snuba.clickhouse.http import HTTPBatchWriter
 from snuba.datasets.factory import enforce_table_writer
 from tests.base import BaseEventsTest
-
-
-class FakeHTTPWriter(HTTPBatchWriter):
-    def chunk(self, rows: Iterable[bytes]) -> Iterable[bytes]:
-        return self._prepare_chunks(rows)
 
 
 class TestHTTPBatchWriter(BaseEventsTest):
@@ -33,27 +26,27 @@ class TestHTTPBatchWriter(BaseEventsTest):
         else:
             assert False, "expected error"
 
-    test_data = [
-        (1, [b"a", b"b", b"c"], [b"a", b"b", b"c"],),
-        (0, [b"a", b"b", b"c"], [b"abc"],),
-        (2, [b"a", b"b", b"c"], [b"ab", b"c"],),
-        (2, [b"a", b"b", b"c", b"d"], [b"ab", b"cd"],),
-        (100000, [b"a", b"b", b"c"], [b"abc"],),
-        (5, [], [],),
-    ]
-
-    @pytest.mark.parametrize("chunk_size, input, expected_chunks", test_data)
+    @pytest.mark.parametrize(
+        "chunk_size, input, expected_chunks",
+        [
+            (1, [b"a", b"b", b"c"], [b"a", b"b", b"c"],),
+            (0, [b"a", b"b", b"c"], [b"abc"],),
+            (2, [b"a", b"b", b"c"], [b"ab", b"c"],),
+            (2, [b"a", b"b", b"c", b"d"], [b"ab", b"cd"],),
+            (100000, [b"a", b"b", b"c"], [b"abc"],),
+            (5, [], [],),
+        ],
+    )
     def test_chunks(self, chunk_size, input, expected_chunks):
-        writer = FakeHTTPWriter(
-            "mysterious_inexistent_table",
-            "0:0:0:0",
-            9000,
-            "default",
-            "",
-            "default",
-            None,
-            chunk_size,
+        writer = HTTPBatchWriter(
+            table_name="mysterious_inexistent_table",
+            host="0:0:0:0",
+            port=9000,
+            user="default",
+            password="",
+            database="default",
+            chunk_size=chunk_size,
         )
-        chunks = writer.chunk(input)
+        chunks = writer._prepare_chunks(input)
         for chunk, expected in zip(chunks, expected_chunks):
             assert chunk == expected
