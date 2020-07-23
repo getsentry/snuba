24c5f046b27c609ffbaaaabb52da8e409d71063c
Ted Kaemming <ted@kaemming.com>
2020-07-22 17:07:29 -0700

    Add metrics to HTTPBatchWriter.

--- tests/test_writer.py
+++ tests/test_writer.py
@@ -3,6 +3,7 @@ import pytest
 from snuba.clickhouse.errors import ClickhouseError
 from snuba.clickhouse.http import HTTPBatchWriter
 from snuba.datasets.factory import enforce_table_writer
+from tests.backends.metrics import TestingMetricsBackend, Timing
 from tests.base import BaseEventsTest
 
 
@@ -38,15 +39,31 @@ class TestHTTPBatchWriter(BaseEventsTest):
         ],
     )
     def test_chunks(self, chunk_size, input, expected_chunks):
+        metrics = TestingMetricsBackend()
+
+        table_name = "mysterious_inexistent_table"
         writer = HTTPBatchWriter(
-            table_name="mysterious_inexistent_table",
+            table_name=table_name,
             host="0:0:0:0",
             port=9000,
             user="default",
             password="",
             database="default",
             chunk_size=chunk_size,
+            metrics=metrics,
         )
+
         chunks = writer._prepare_chunks(input)
         for chunk, expected in zip(chunks, expected_chunks):
             assert chunk == expected
+
+        assert metrics.calls == [
+            Timing("writer.chunk.size", len(chunk), {"table_name": table_name})
+            for chunk in expected_chunks
+        ] + [
+            Timing(
+                "writer.total.size",
+                sum(map(len, expected_chunks)),
+                {"table_name": table_name},
+            )
+        ]
