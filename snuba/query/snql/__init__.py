55ec4200c9b078bd30add8c4ef3abb98da64c27e
Riya Chakraborty <riyamch@yahoo.com>
2020-07-29 17:06:40 -0500

    Added basic skeleton for potential SnQL grammar.

--- snuba/query/snql/__init__.py
+++ snuba/query/snql/__init__.py
@@ -1,13 +1,36 @@
-from typing import Any, MutableMapping
 from snuba.datasets.dataset import Dataset
 from snuba.query.logical import Query
+from parsimonious.grammar import Grammar
 
 
-def parse_snql_query(body: MutableMapping[str, Any], dataset: Dataset) -> Query:
+grammar = Grammar(
+    r"""
+    query_exp = "MATCH" overall_clause
+
+    overall_clause = match_clause where_clause? collect_clause? having_clause? order_by_clause?
+
+    match_clause = space* "(" clause ")" space*
+    where_clause = space* "WHERE" clause space*
+    collect_clause = space* "COLLECT" clause "BY" clause space*
+    having_clause = space* "HAVING" clause space*
+    order_by_clause = space* "ORDER BY" clause ("ASC"/"DESC") space*
+
+    clause = space* ~r"[-=><\w]+" space*
+    space = " "
+"""
+)
+
+exp_tree = grammar.parse(
+    "MATCH (cake) WHERE icing=chocolate COLLECT soon BY thursday HAVING candles ORDER BY names ASC"
+)
+print(exp_tree)
+
+
+def parse_snql_query(body: str, dataset: Dataset) -> Query:
     """
     Parses the query body generating the AST. This only takes into
     account the initial query body. Extensions are parsed by extension
     processors and are supposed to update the AST.
     """
 
-    return Query(body, None, None, None, None, None, None, None,)
+    return Query({}, None,)
