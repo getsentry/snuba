73f2fd0c66ecc7cef1ed0f414101ae86b04583a6
Filippo Pacifici <filippo.pacifici@gmail.com>
2020-07-28 17:21:07 -0700

    Fix comment

--- snuba/datasets/dataset.py
+++ snuba/datasets/dataset.py
@@ -136,9 +136,9 @@ class Dataset(object):
 
     def get_expression_validators(self) -> Sequence[ExpressionValidator]:
         """
-        Provides the a sequence of expression validators for this dataset.
-        The typical use case is validating that calls to dataset specific
-        functions are well formed.
+        Provides a sequence of expression validators for this dataset.
+        The typical use case is the validation that calls to dataset
+        specific functions are well formed.
         """
         return []
 
