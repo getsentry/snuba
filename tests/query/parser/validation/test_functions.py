fdba3ed832207d864b4bb23bf1f1e603136fdaa0
Filippo Pacifici <filippo.pacifici@gmail.com>
2020-07-30 16:00:35 -0700

    feat(slo) Introduce a one pass query validator applied after parsing. (#1182)
    Adds the infrastructure to run multiple expression validators on a query in one single pass over the AST.
    Each validator is an instance of the class ExpressionValidator and it validates one individual expression.
    The context each validator has is just the expression and the dataset.
    One of this validator validates function calls, which is the most common use case. Each function is validated by one specific FunctionCallValidator.
    There is a sequence of function validators applied to all queries (well, there will be, now the sequence is empty) and each dataset can provides its own function validators to be applied as well.

---
+++ tests/query/parser/validation/test_functions.py
@@ -0,0 +1,72 @@
+from __future__ import annotations
+
+from typing import Mapping, Optional, Sequence, Type
+from unittest.mock import MagicMock
+
+import pytest
+
+import snuba.query.parser.validation.functions as functions
+from snuba.clickhouse.columns import ColumnSet
+from snuba.query.expressions import Column, Expression, FunctionCall
+from snuba.query.parser.exceptions import InvalidExpressionException
+from snuba.query.parser.validation.functions import FunctionCallsValidator
+from snuba.query.validation import FunctionCallValidator, InvalidFunctionCallException
+from snuba.state import set_config
+
+
+class FakeValidator(FunctionCallValidator):
+    def __init__(self, fails: bool):
+        self.__fails = fails
+
+    def validate(self, parameters: Sequence[Expression], schema: ColumnSet) -> None:
+        if self.__fails:
+            raise InvalidFunctionCallException()
+
+        return
+
+
+test_cases = [
+    pytest.param({}, {}, None, id="No validators"),
+    pytest.param(
+        {"f": FakeValidator(True)},
+        {},
+        InvalidExpressionException,
+        id="Default validator failure",
+    ),
+    pytest.param(
+        {},
+        {"f": FakeValidator(True)},
+        InvalidExpressionException,
+        id="Dataset validator failure",
+    ),
+    pytest.param(
+        {"f": FakeValidator(False), "g": FakeValidator(True)},
+        {"k": FakeValidator(True)},
+        None,
+        id="No failure",
+    ),
+]
+
+
+@pytest.mark.parametrize(
+    "default_validators, dataset_validators, exception", test_cases
+)
+def test_functions(
+    default_validators: Mapping[str, FunctionCallValidator],
+    dataset_validators: Mapping[str, FunctionCallValidator],
+    exception: Optional[Type[InvalidExpressionException]],
+) -> None:
+    set_config("enforce_expression_validation", 1)
+    functions.default_validators = default_validators
+    dataset = MagicMock()
+    dataset.get_function_call_validators.return_value = dataset_validators
+    dataset.get_abstract_columnset.return_value = ColumnSet([])
+
+    expression = FunctionCall(
+        None, "f", (Column(alias=None, table_name=None, column_name="col"),)
+    )
+    if exception is None:
+        FunctionCallsValidator().validate(expression, dataset)
+    else:
+        with pytest.raises(exception):
+            FunctionCallsValidator().validate(expression, dataset)
