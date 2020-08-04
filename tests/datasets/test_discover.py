ba0b42772070cb40aa3047ac5142fb6596282145
Evan Hicks <evan.hicks@sentry.io>
2020-08-04 12:07:20 -0400

    fix(discover) Change discover function to use AST
    Change the discover table detection function to use the AST instead of the old
    query structure.

--- tests/datasets/test_discover.py
+++ tests/datasets/test_discover.py
@@ -1,3 +1,4 @@
+import simplejson as json
 import pytest
 from typing import Any, MutableMapping
 from tests.base import BaseDatasetTest
@@ -15,12 +16,113 @@ def get_dataset_source(dataset_name):
 
 
 test_data = [
-    ({"conditions": [["type", "=", "transaction"]]}, "transactions_local",),
-    ({"conditions": [["type", "!=", "transaction"]]}, "sentry_local",),
-    ({"conditions": []}, "sentry_local",),
-    ({"conditions": [["duration", "=", 0]]}, "transactions_local",),
+    ({"conditions": [["type", "=", "transaction"]]}, "transactions_local"),
+    ({"conditions": [["type", "=", "error"]]}, "sentry_local"),
+    (
+        {"conditions": [[["type", "=", "error"], ["type", "=", "transaction"]]]},
+        "sentry_local",
+    ),
+    (
+        {
+            "conditions": [
+                [
+                    [
+                        "or",
+                        [
+                            ["equals", ["type", "transaction"]],
+                            ["equals", ["type", "default"]],
+                        ],
+                    ],
+                    "=",
+                    1,
+                ]
+            ]
+        },
+        "sentry_local",
+    ),
+    (
+        {
+            "conditions": [
+                [
+                    [
+                        "and",
+                        [
+                            ["equals", ["type", "transaction"]],
+                            ["notEquals", ["type", "error"]],
+                        ],
+                    ],
+                    "=",
+                    1,
+                ]
+            ]
+        },
+        "transactions_local",
+    ),
+    (
+        {
+            "conditions": [
+                [
+                    [
+                        "and",
+                        [
+                            ["notEquals", ["type", "transaction"]],
+                            ["notEquals", ["type", "error"]],
+                        ],
+                    ],
+                    "=",
+                    1,
+                ]
+            ]
+        },
+        "sentry_local",
+    ),
+    ({"conditions": [["type", "!=", "transaction"]]}, "sentry_local"),
+    ({"conditions": []}, "sentry_local"),
+    ({"conditions": [["duration", "=", 0]]}, "transactions_local"),
     (
         {"conditions": [["event_id", "=", "asdasdasd"], ["duration", "=", 0]]},
+        "transactions_local",
+    ),
+    (
+        {"conditions": [["group_id", "=", "asdasdasd"], ["duration", "=", 0]]},
+        "sentry_local",
+    ),
+    (
+        {
+            "conditions": [
+                [
+                    [
+                        "and",
+                        [
+                            ["notEquals", ["type", "transaction"]],
+                            ["notEquals", ["type", "error"]],
+                        ],
+                    ],
+                    "=",
+                    1,
+                ],
+                ["duration", "=", 0],
+            ]
+        },
+        "sentry_local",
+    ),
+    (
+        {
+            "conditions": [
+                [
+                    [
+                        "and",
+                        [
+                            ["notEquals", ["type", "default"]],
+                            ["notEquals", ["type", "error"]],
+                        ],
+                    ],
+                    "=",
+                    1,
+                ],
+                ["duration", "=", 0],
+            ]
+        },
         "transactions_local",
     ),
     # No conditions, other referenced columns
@@ -52,4 +154,6 @@ class TestDiscover(BaseDatasetTest):
         for physical_processor in plan.plan_processors:
             physical_processor.process_query(plan.query, request.settings)
 
-        assert plan.query.get_data_source().format_from() == expected_table
+        assert plan.query.get_data_source().format_from() == expected_table, json.dumps(
+            query_body
+        )
