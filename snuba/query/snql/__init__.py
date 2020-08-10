767a2487fc95d0f855fbcb218da7cde07c2b2ca0
Riya Chakraborty <riyamch@yahoo.com>
2020-08-10 11:01:40 -0500

    Modified init to account for unit tests.

--- snuba/query/snql/__init__.py
+++ snuba/query/snql/__init__.py
@@ -1,47 +1,34 @@
-from typing import (
-    Any,
-    Iterable,
-    List,
-    Tuple,
-    Union,
-)
+from typing import Any, Iterable, List, Tuple, Union
 
 from parsimonious.grammar import Grammar
 from parsimonious.nodes import Node, NodeVisitor
 
 from snuba.datasets.dataset import Dataset
-from snuba.query.expressions import (
-    Column,
-    Expression,
-    Literal,
-)
+from snuba.query.expressions import Column, Expression, Literal
 from snuba.query.logical import Query, SelectedExpression
-
-
 from snuba.query.snql.expression_visitor import (
-    LowPriOperator,
+    HighPriArithmetic,
     HighPriOperator,
-    LowPriTuple,
     HighPriTuple,
     LowPriArithmetic,
-    HighPriArithmetic,
-    exp_visit_function_name,
-    exp_visit_column_name,
-    exp_visit_low_pri_tuple,
-    exp_visit_high_pri_tuple,
-    exp_visit_low_pri_op,
-    exp_visit_high_pri_op,
-    exp_visit_parameter,
-    exp_visit_parameters_list,
-    exp_visit_function_call,
-    exp_visit_quoted_literal,
-    exp_generic_visit,
-    exp_visit_high_pri_arithmetic,
-    exp_visit_low_pri_arithmetic,
-    exp_visit_arithmetic_term,
-    exp_visit_numeric_literal,
+    LowPriOperator,
+    LowPriTuple,
+    generic_visit,
+    visit_arithmetic_term,
+    visit_column_name,
+    visit_function_call,
+    visit_function_name,
+    visit_high_pri_arithmetic,
+    visit_high_pri_op,
+    visit_high_pri_tuple,
+    visit_low_pri_arithmetic,
+    visit_low_pri_op,
+    visit_low_pri_tuple,
+    visit_numeric_literal,
+    visit_parameter,
+    visit_parameters_list,
+    visit_quoted_literal,
 )
-
 
 snql_grammar = Grammar(
     fr"""
@@ -95,59 +82,59 @@ class SnQLVisitor(NodeVisitor):
         return Query({}, None, collect, None,)
 
     def visit_function_name(self, node: Node, visited_children: Iterable[Any]) -> str:
-        return exp_visit_function_name(self, node, visited_children)
+        return visit_function_name(node, visited_children)
 
     def visit_column_name(self, node: Node, visited_children: Iterable[Any]) -> Column:
-        return exp_visit_column_name(self, node, visited_children)
+        return visit_column_name(node, visited_children)
 
     def visit_low_pri_tuple(
         self, node: Node, visited_children: Tuple[LowPriOperator, Expression]
     ) -> LowPriTuple:
-        return exp_visit_low_pri_tuple(self, node, visited_children)
+        return visit_low_pri_tuple(node, visited_children)
 
     def visit_high_pri_tuple(
         self, node: Node, visited_children: Tuple[HighPriOperator, Expression]
     ) -> HighPriTuple:
-        return exp_visit_high_pri_tuple(self, node, visited_children)
+        return visit_high_pri_tuple(node, visited_children)
 
     def visit_low_pri_op(
         self, node: Node, visited_children: Iterable[Any]
     ) -> LowPriOperator:
-        return exp_visit_low_pri_op(self, node, visited_children)
+        return visit_low_pri_op(node, visited_children)
 
     def visit_high_pri_op(
         self, node: Node, visited_children: Iterable[Any]
     ) -> HighPriOperator:
-        return exp_visit_high_pri_op(self, node, visited_children)
+        return visit_high_pri_op(node, visited_children)
 
     def visit_arithmetic_term(
         self, node: Node, visited_children: Tuple[Any, Expression, Any]
     ) -> Expression:
-        return exp_visit_arithmetic_term(self, node, visited_children)
+        return visit_arithmetic_term(node, visited_children)
 
     def visit_low_pri_arithmetic(
         self,
         node: Node,
         visited_children: Tuple[Any, Expression, Any, LowPriArithmetic],
     ) -> Expression:
-        return exp_visit_low_pri_arithmetic(self, node, visited_children)
+        return visit_low_pri_arithmetic(node, visited_children)
 
     def visit_high_pri_arithmetic(
         self,
         node: Node,
         visited_children: Tuple[Any, Expression, Any, HighPriArithmetic],
     ) -> Expression:
-        return exp_visit_high_pri_arithmetic(self, node, visited_children)
+        return visit_high_pri_arithmetic(node, visited_children)
 
     def visit_numeric_literal(
         self, node: Node, visited_children: Iterable[Any]
     ) -> Literal:
-        return exp_visit_numeric_literal(self, node, visited_children)
+        return visit_numeric_literal(node, visited_children)
 
     def visit_quoted_literal(
         self, node: Node, visited_children: Tuple[Any, Node, Any]
     ) -> Literal:
-        return exp_visit_quoted_literal(self, node, visited_children)
+        return visit_quoted_literal(node, visited_children)
 
     def visit_collect_clause(self, node, visited_children):
         _, _, selected_columns, _ = visited_children
@@ -173,14 +160,14 @@ class SnQLVisitor(NodeVisitor):
     def visit_parameter(
         self, node: Node, visited_children: Tuple[Expression, Any, Any, Any]
     ) -> Expression:
-        return exp_visit_parameter(self, node, visited_children)
+        return visit_parameter(node, visited_children)
 
     def visit_parameters_list(
         self,
         node: Node,
         visited_children: Tuple[Union[Expression, List[Expression]], Expression],
     ) -> List[Expression]:
-        return exp_visit_parameters_list(self, node, visited_children)
+        return visit_parameters_list(node, visited_children)
 
     def visit_function_call(
         self,
@@ -189,15 +176,10 @@ class SnQLVisitor(NodeVisitor):
             str, Any, List[Expression], Any, Union[Node, List[Expression]]
         ],
     ) -> Expression:
-        return exp_visit_function_call(self, node, visited_children)
+        return visit_function_call(node, visited_children)
 
     def generic_visit(self, node: Node, visited_children: Any) -> Any:
-        return exp_generic_visit(self, node, visited_children)
-
-
-exp_tree = snql_grammar.parse("COLLECT 4-5, 3*g(c), c")
-parsed_exp = SnQLVisitor().visit(exp_tree)
-print(parsed_exp.get_selected_columns_from_ast())
+        return generic_visit(node, visited_children)
 
 
 def parse_snql_query(body: str, dataset: Dataset) -> Query:
@@ -206,5 +188,7 @@ def parse_snql_query(body: str, dataset: Dataset) -> Query:
     account the initial query body. Extensions are parsed by extension
     processors and are supposed to update the AST.
     """
-
-    return Query({}, None,)
+    exp_tree = snql_grammar.parse(body)
+    parsed_exp = SnQLVisitor().visit(exp_tree)
+    selected_columns = parsed_exp.get_selected_columns_from_ast()
+    return Query({}, None, selected_columns,)
