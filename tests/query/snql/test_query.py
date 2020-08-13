14a80604e4bb92e7f852daa425a6a5338c621227
Riya Chakraborty <riyamch@yahoo.com>
2020-08-12 23:38:07 -0500

    Added condition to test case.

--- tests/query/snql/test_query.py
+++ tests/query/snql/test_query.py
@@ -2,6 +2,10 @@ import pytest
 
 from snuba import state
 from snuba.datasets.factory import get_dataset
+from snuba.query.conditions import (
+    binary_condition,
+    combine_or_conditions,
+)
 from snuba.query.expressions import Column, FunctionCall, Literal
 from snuba.query.logical import OrderBy, OrderByDirection, Query, SelectedExpression
 from snuba.query.snql import parse_snql_query
@@ -9,7 +13,7 @@ from snuba.query.snql import parse_snql_query
 
 test_cases = [
     pytest.param(
-        "MATCH (blah) COLLECT 4-5, 3*g(c), c BY d, 2+7 ORDER BY f DESC, m ASC",
+        "MATCH (blah) COLLECT 4-5, 3*g(c), c BY d, 2+7 HAVING times_seen>1 OR last_seen=2 ORDER BY f DESC, m ASC",
         Query(
             {},
             None,
@@ -35,6 +39,22 @@ test_cases = [
                 Column(None, None, "d"),
                 FunctionCall(None, "plus", (Literal(None, 2), Literal(None, 7),),),
             ],
+            having=combine_or_conditions(
+                [
+                    binary_condition(
+                        None,
+                        "greater",
+                        Column(None, None, "times_seen"),
+                        Literal(None, 1),
+                    ),
+                    binary_condition(
+                        None,
+                        "equals",
+                        Column(None, None, "last_seen"),
+                        Literal(None, 2),
+                    ),
+                ]
+            ),
             order_by=[
                 OrderBy(OrderByDirection.DESC, Column(None, None, "f")),
                 OrderBy(OrderByDirection.ASC, Column(None, None, "m")),
@@ -57,3 +77,4 @@ def test_format_expressions(query_body: str, expected_query: Query) -> None:
     )
     assert query.get_orderby_from_ast() == expected_query.get_orderby_from_ast()
     assert query.get_groupby_from_ast() == expected_query.get_groupby_from_ast()
+    assert query.get_having_from_ast() == expected_query.get_having_from_ast()
