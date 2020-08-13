c48d0cf17f5b6d9927bdaf0eb7e15742253e5a19
Riya Chakraborty <riyamch@yahoo.com>
2020-08-13 00:26:40 -0500

    Add another test case for combined conditions.

--- tests/query/snql/test_query.py
+++ tests/query/snql/test_query.py
@@ -4,6 +4,7 @@ from snuba import state
 from snuba.datasets.factory import get_dataset
 from snuba.query.conditions import (
     binary_condition,
+    combine_and_conditions,
     combine_or_conditions,
 )
 from snuba.query.expressions import Column, FunctionCall, Literal
@@ -13,7 +14,7 @@ from snuba.query.snql import parse_snql_query
 
 test_cases = [
     pytest.param(
-        "MATCH (blah) COLLECT 4-5, 3*g(c), c BY d, 2+7 HAVING times_seen>1 OR last_seen=2 ORDER BY f DESC, m ASC",
+        "MATCH (blah) WHERE a<3 COLLECT 4-5, 3*g(c), c BY d, 2+7 HAVING times_seen>1 OR last_seen=2 ORDER BY f DESC",
         Query(
             {},
             None,
@@ -35,6 +36,9 @@ test_cases = [
                 ),
                 SelectedExpression("c", Column(None, None, "c"),),
             ],
+            condition=binary_condition(
+                None, "less", Column(None, None, "a"), Literal(None, 3)
+            ),
             groupby=[
                 Column(None, None, "d"),
                 FunctionCall(None, "plus", (Literal(None, 2), Literal(None, 7),),),
@@ -55,12 +59,61 @@ test_cases = [
                     ),
                 ]
             ),
-            order_by=[
-                OrderBy(OrderByDirection.DESC, Column(None, None, "f")),
-                OrderBy(OrderByDirection.ASC, Column(None, None, "m")),
-            ],
+            order_by=[OrderBy(OrderByDirection.DESC, Column(None, None, "f"))],
         ),
-        id="Simple COLLECT, GROUPBY, ORDERBY clause example",
+        id="Example 1",
+    ),
+    pytest.param(
+        "MATCH (blah) WHERE time_seen<3 COLLECT 4-5, 3*g(c), c HAVING b!=1 OR times_seen>1 AND last_seen=2",
+        Query(
+            {},
+            None,
+            selected_columns=[
+                SelectedExpression(
+                    "4-5",
+                    FunctionCall(None, "minus", (Literal(None, 4), Literal(None, 5),),),
+                ),
+                SelectedExpression(
+                    "3*g(c)",
+                    FunctionCall(
+                        None,
+                        "multiply",
+                        (
+                            Literal(None, 3),
+                            FunctionCall(None, "g", (Column(None, None, "c"),),),
+                        ),
+                    ),
+                ),
+                SelectedExpression("c", Column(None, None, "c"),),
+            ],
+            condition=binary_condition(
+                None, "less", Column(None, None, "time_seen"), Literal(None, 3)
+            ),
+            having=combine_or_conditions(
+                [
+                    binary_condition(
+                        None, "notEquals", Column(None, None, "b"), Literal(None, 1)
+                    ),
+                    combine_and_conditions(
+                        [
+                            binary_condition(
+                                None,
+                                "greater",
+                                Column(None, None, "times_seen"),
+                                Literal(None, 1),
+                            ),
+                            binary_condition(
+                                None,
+                                "equals",
+                                Column(None, None, "last_seen"),
+                                Literal(None, 2),
+                            ),
+                        ]
+                    ),
+                ],
+            ),
+        ),
+        id="Example 2",
     ),
 ]
 
@@ -77,4 +130,5 @@ def test_format_expressions(query_body: str, expected_query: Query) -> None:
     )
     assert query.get_orderby_from_ast() == expected_query.get_orderby_from_ast()
     assert query.get_groupby_from_ast() == expected_query.get_groupby_from_ast()
+    assert query.get_condition_from_ast() == expected_query.get_condition_from_ast()
     assert query.get_having_from_ast() == expected_query.get_having_from_ast()
