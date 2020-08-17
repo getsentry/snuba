178d00e4732a88f7f0219d80ae030590081497fa
Riya Chakraborty <riyamch@yahoo.com>
2020-08-16 22:00:50 -0500

    Modified logic for conditions, improved type hints and grammar rules.

--- snuba/query/snql/__init__.py
+++ snuba/query/snql/__init__.py
@@ -1,8 +1,7 @@
-from typing import Any, Iterable, List, Sequence, Tuple, Union
+from typing import Any, Iterable, NamedTuple, List, Sequence, Tuple, Union
 
 from parsimonious.grammar import Grammar
 from parsimonious.nodes import Node, NodeVisitor
-
 from snuba.datasets.dataset import Dataset
 from snuba.query.conditions import (
     OPERATOR_TO_FUNCTION,
@@ -47,10 +46,14 @@ snql_grammar = Grammar(
     having_clause         = space* "HAVING" or_expression space*
     order_by_clause       = space* "ORDER BY" order_list space*
 
-    condition             = low_pri_arithmetic condition_op (column_name / numeric_literal)
+    condition             = low_pri_arithmetic condition_op (function_call / column_name / numeric_literal) space*
     condition_op          = "=" / "!=" / ">" / ">=" / "<" / "<="
-    and_expression        = space* condition space* ("AND" condition)*
-    or_expression         = space* and_expression space* ("OR" and_expression)*
+    interm_condition      = open_paren or_expression close_paren
+
+    and_expression        = space* (condition / interm_condition) space* (and_tuple)*
+    or_expression         = space* and_expression space* (or_tuple)*
+    and_tuple             = "AND" condition
+    or_tuple              = "OR" and_expression
 
     collect_list          = collect_columns* (selected_expression)
     collect_columns       = selected_expression space* comma space*
@@ -65,10 +68,15 @@ snql_grammar = Grammar(
     clause                = space* ~r"[-=><\w]+" space*
 
     low_pri_arithmetic    = space* high_pri_arithmetic space* (low_pri_tuple)*
-    high_pri_arithmetic   = space* arithmetic_term space* (high_pri_tuple)*
+    high_pri_arithmetic   = (space* arithmetic_term space* (high_pri_tuple)*)
+
     low_pri_tuple         = low_pri_op high_pri_arithmetic
     high_pri_tuple        = high_pri_op arithmetic_term
-    arithmetic_term       = (space*) (function_call / numeric_literal / column_name) (space*)
+
+    arithmetic_term       = (space*) (function_call / numeric_literal / column_name / interm_arithm) (space*)
+    interm_arithm         = open_paren low_pri_arithmetic close_paren
+
+
     low_pri_op            = "+" / "-"
     high_pri_op           = "/" / "*"
     param_expression      = low_pri_arithmetic / quoted_literal
@@ -91,6 +99,16 @@ snql_grammar = Grammar(
 )
 
 
+class AndTuple(NamedTuple):
+    op: str
+    exp: Expression
+
+
+class OrTuple(NamedTuple):
+    op: str
+    exp: Expression
+
+
 class SnQLVisitor(NodeVisitor):
     """
     Builds Snuba AST expressions from the Parsimonious parse tree.
@@ -105,13 +123,47 @@ class SnQLVisitor(NodeVisitor):
             orderby = None
         if isinstance(having, Node):
             having = None
-        return Query({}, None, collect, None, where, None, groupby, having, orderby)
+        return Query(
+            body={},
+            data_source=None,
+            selected_columns=collect,
+            array_join=None,
+            condition=where,
+            prewhere=None,
+            groupby=groupby,
+            having=having,
+            order_by=orderby,
+        )
 
     def visit_function_name(self, node: Node, visited_children: Iterable[Any]) -> str:
         return visit_function_name(node, visited_children)
 
     def visit_column_name(self, node: Node, visited_children: Iterable[Any]) -> Column:
         return visit_column_name(node, visited_children)
+
+    def visit_and_tuple(
+        self, node: Node, visited_children: Tuple[Node, Expression]
+    ) -> AndTuple:
+        and_string, exp = visited_children
+        return AndTuple(and_string.text, exp)
+
+    def visit_or_tuple(
+        self, node: Node, visited_children: Tuple[Node, Expression]
+    ) -> OrTuple:
+        or_string, exp = visited_children
+        return OrTuple(or_string.text, exp)
+
+    def visit_interm_condition(
+        self, node: Node, visited_children: Tuple[Any, Expression, Any]
+    ) -> Expression:
+        _, condition, _ = visited_children
+        return condition
+
+    def visit_interm_arithm(
+        self, node: Node, visited_children: Tuple[Any, Expression, Any]
+    ) -> Expression:
+        _, arithm, _ = visited_children
+        return arithm
 
     def visit_low_pri_tuple(
         self, node: Node, visited_children: Tuple[LowPriOperator, Expression]
@@ -150,6 +202,7 @@ class SnQLVisitor(NodeVisitor):
         node: Node,
         visited_children: Tuple[Any, Expression, Any, HighPriArithmetic],
     ) -> Expression:
+
         return visit_high_pri_arithmetic(node, visited_children)
 
     def visit_numeric_literal(
@@ -163,14 +216,14 @@ class SnQLVisitor(NodeVisitor):
         return visit_quoted_literal(node, visited_children)
 
     def visit_where_clause(
-        self, node: Node, visited_children: Tuple[Any, Any, Sequence[Expression], Any]
-    ) -> Sequence[Expression]:
+        self, node: Node, visited_children: Tuple[Any, Any, Expression, Any]
+    ) -> Expression:
         _, _, conditions, _ = visited_children
         return conditions
 
     def visit_having_clause(
-        self, node: Node, visited_children: Tuple[Any, Any, Sequence[Expression], Any]
-    ) -> Sequence[Expression]:
+        self, node: Node, visited_children: Tuple[Any, Any, Expression, Any]
+    ) -> Expression:
         _, _, conditions, _ = visited_children
         return conditions
 
@@ -178,34 +231,42 @@ class SnQLVisitor(NodeVisitor):
         self, node: Node, visited_children: Tuple[Any, Expression, Any, Expression]
     ) -> Expression:
         _, left_condition, _, and_condition = visited_children
+
         # in the case of one Condition
         # and_condition will be an empty Node
-        if not isinstance(and_condition, Node):
-            _, right_condition = and_condition
-            return combine_and_conditions([left_condition, right_condition])
+        if isinstance(and_condition, Node):
+            return left_condition
+        if isinstance(and_condition, (AndTuple, OrTuple)):
+            _, exp = and_condition
+            return combine_and_conditions([left_condition, exp])
+        elif isinstance(and_condition, list):
+            for elem in and_condition:
+                _, exp = elem
+                left_condition = combine_and_conditions([left_condition, exp])
         return left_condition
 
     def visit_or_expression(
         self, node: Node, visited_children: Tuple[Any, Expression, Any, Expression]
     ) -> Expression:
         _, left_condition, _, or_condition = visited_children
+
         # in the case of one Condition
         # or_condition will be an empty Node
-        if not isinstance(or_condition, Node):
-            _, right_condition = or_condition
-            return combine_or_conditions([left_condition, right_condition])
+        if isinstance(or_condition, Node):
+            return left_condition
+        if isinstance(or_condition, (AndTuple, OrTuple)):
+            _, exp = or_condition
+            return combine_or_conditions([left_condition, exp])
+        elif isinstance(or_condition, list):
+            for elem in or_condition:
+                _, exp = elem
+                left_condition = combine_or_conditions([left_condition, exp])
         return left_condition
 
-    def visit_condition_comma(
-        self, node: Node, visited_children: Tuple[Expression, Any, Any, Any]
-    ) -> Expression:
-        bin_condition, _, _, _ = visited_children
-        return bin_condition
-
     def visit_condition(
-        self, node: Node, visited_children: Tuple[Expression, str, Expression]
+        self, node: Node, visited_children: Tuple[Expression, str, Expression, Any]
     ) -> Expression:
-        exp, op, literal = visited_children
+        exp, op, literal, _ = visited_children
         return binary_condition(None, op, exp, literal)
 
     def visit_condition_op(self, node: Node, visited_children: Iterable[Any]) -> str:
