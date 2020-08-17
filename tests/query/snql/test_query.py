a8685c0e234f188f428d5b90f542ea76d02a26e2
Riya Chakraborty <riyamch@yahoo.com>
2020-08-16 21:59:29 -0500

    Added condition test cases.

--- tests/query/snql/test_query.py
+++ tests/query/snql/test_query.py
@@ -2,11 +2,7 @@ import pytest
 
 from snuba import state
 from snuba.datasets.factory import get_dataset
-from snuba.query.conditions import (
-    binary_condition,
-    combine_and_conditions,
-    combine_or_conditions,
-)
+from snuba.query.conditions import binary_condition
 from snuba.query.expressions import Column, FunctionCall, Literal
 from snuba.query.logical import OrderBy, OrderByDirection, Query, SelectedExpression
 from snuba.query.snql import parse_snql_query
@@ -14,7 +10,7 @@ from snuba.query.snql import parse_snql_query
 
 test_cases = [
     pytest.param(
-        "MATCH (blah) WHERE a<3 COLLECT 4-5, 3*g(c), c BY d, 2+7 HAVING times_seen>1 OR last_seen=2 ORDER BY f DESC",
+        "MATCH (blah) WHERE a<3 COLLECT 4-5, 3*g(c), c BY d, 2+7 ORDER BY f DESC",
         Query(
             {},
             None,
@@ -43,77 +39,247 @@ test_cases = [
                 Column(None, None, "d"),
                 FunctionCall(None, "plus", (Literal(None, 2), Literal(None, 7),),),
             ],
-            having=combine_or_conditions(
-                [
-                    binary_condition(
-                        None,
-                        "greater",
-                        Column(None, None, "times_seen"),
-                        Literal(None, 1),
-                    ),
-                    binary_condition(
-                        None,
-                        "equals",
-                        Column(None, None, "last_seen"),
-                        Literal(None, 2),
-                    ),
-                ]
-            ),
             order_by=[OrderBy(OrderByDirection.DESC, Column(None, None, "f"))],
         ),
         id="Example 1",
     ),
     pytest.param(
-        "MATCH (blah) WHERE time_seen<3 COLLECT 4-5, 3*g(c), c HAVING b!=1 OR times_seen>1 AND last_seen=2",
+        "MATCH (blah) WHERE time_seen<3 AND last_seen=2 AND c=2 AND d=3 COLLECT a",
         Query(
             {},
             None,
             selected_columns=[
                 SelectedExpression(
-                    "4-5",
-                    FunctionCall(None, "minus", (Literal(None, 4), Literal(None, 5),),),
-                ),
-                SelectedExpression(
-                    "3*g(c)",
+                    name="a",
+                    expression=Column(alias=None, table_name=None, column_name="a"),
+                )
+            ],
+            condition=FunctionCall(
+                alias=None,
+                function_name="and",
+                parameters=(
                     FunctionCall(
-                        None,
-                        "multiply",
-                        (
-                            Literal(None, 3),
-                            FunctionCall(None, "g", (Column(None, None, "c"),),),
+                        alias=None,
+                        function_name="and",
+                        parameters=(
+                            FunctionCall(
+                                alias=None,
+                                function_name="and",
+                                parameters=(
+                                    FunctionCall(
+                                        alias=None,
+                                        function_name="less",
+                                        parameters=(
+                                            Column(
+                                                alias=None,
+                                                table_name=None,
+                                                column_name="time_seen",
+                                            ),
+                                            Literal(alias=None, value=3),
+                                        ),
+                                    ),
+                                    FunctionCall(
+                                        alias=None,
+                                        function_name="equals",
+                                        parameters=(
+                                            Column(
+                                                alias=None,
+                                                table_name=None,
+                                                column_name="last_seen",
+                                            ),
+                                            Literal(alias=None, value=2),
+                                        ),
+                                    ),
+                                ),
+                            ),
+                            FunctionCall(
+                                alias=None,
+                                function_name="equals",
+                                parameters=(
+                                    Column(
+                                        alias=None, table_name=None, column_name="c"
+                                    ),
+                                    Literal(alias=None, value=2),
+                                ),
+                            ),
+                        ),
+                    ),
+                    FunctionCall(
+                        alias=None,
+                        function_name="equals",
+                        parameters=(
+                            Column(alias=None, table_name=None, column_name="d"),
+                            Literal(alias=None, value=3),
                         ),
                     ),
                 ),
-                SelectedExpression("c", Column(None, None, "c"),),
-            ],
-            condition=binary_condition(
-                None, "less", Column(None, None, "time_seen"), Literal(None, 3)
-            ),
-            having=combine_or_conditions(
-                [
-                    binary_condition(
-                        None, "notEquals", Column(None, None, "b"), Literal(None, 1)
-                    ),
-                    combine_and_conditions(
-                        [
-                            binary_condition(
-                                None,
-                                "greater",
-                                Column(None, None, "times_seen"),
-                                Literal(None, 1),
-                            ),
-                            binary_condition(
-                                None,
-                                "equals",
-                                Column(None, None, "last_seen"),
-                                Literal(None, 2),
-                            ),
-                        ]
-                    ),
-                ],
             ),
         ),
         id="Example 2",
+    ),
+    pytest.param(
+        "MATCH (blah) WHERE time_seen<3 OR last_seen=afternoon OR name=bob COLLECT a",
+        Query(
+            {},
+            None,
+            selected_columns=[
+                SelectedExpression(
+                    name="a",
+                    expression=Column(alias=None, table_name=None, column_name="a"),
+                )
+            ],
+            condition=FunctionCall(
+                alias=None,
+                function_name="or",
+                parameters=(
+                    FunctionCall(
+                        alias=None,
+                        function_name="or",
+                        parameters=(
+                            FunctionCall(
+                                alias=None,
+                                function_name="less",
+                                parameters=(
+                                    Column(
+                                        alias=None,
+                                        table_name=None,
+                                        column_name="time_seen",
+                                    ),
+                                    Literal(alias=None, value=3),
+                                ),
+                            ),
+                            FunctionCall(
+                                alias=None,
+                                function_name="equals",
+                                parameters=(
+                                    Column(
+                                        alias=None,
+                                        table_name=None,
+                                        column_name="last_seen",
+                                    ),
+                                    Column(
+                                        alias=None,
+                                        table_name=None,
+                                        column_name="afternoon",
+                                    ),
+                                ),
+                            ),
+                        ),
+                    ),
+                    FunctionCall(
+                        alias=None,
+                        function_name="equals",
+                        parameters=(
+                            Column(alias=None, table_name=None, column_name="name"),
+                            Column(alias=None, table_name=None, column_name="bob"),
+                        ),
+                    ),
+                ),
+            ),
+        ),
+        id="Example 3",
+    ),
+    pytest.param(
+        "MATCH (blah) WHERE name!=bob OR last_seen<afternoon AND location=gps(x,y,z) OR times_seen>0 COLLECT a",
+        Query(
+            {},
+            None,
+            selected_columns=[
+                SelectedExpression(
+                    name="a",
+                    expression=Column(alias=None, table_name=None, column_name="a"),
+                )
+            ],
+            condition=FunctionCall(
+                alias=None,
+                function_name="or",
+                parameters=(
+                    FunctionCall(
+                        alias=None,
+                        function_name="or",
+                        parameters=(
+                            FunctionCall(
+                                alias=None,
+                                function_name="notEquals",
+                                parameters=(
+                                    Column(
+                                        alias=None, table_name=None, column_name="name"
+                                    ),
+                                    Column(
+                                        alias=None, table_name=None, column_name="bob"
+                                    ),
+                                ),
+                            ),
+                            FunctionCall(
+                                alias=None,
+                                function_name="and",
+                                parameters=(
+                                    FunctionCall(
+                                        alias=None,
+                                        function_name="less",
+                                        parameters=(
+                                            Column(
+                                                alias=None,
+                                                table_name=None,
+                                                column_name="last_seen",
+                                            ),
+                                            Column(
+                                                alias=None,
+                                                table_name=None,
+                                                column_name="afternoon",
+                                            ),
+                                        ),
+                                    ),
+                                    FunctionCall(
+                                        alias=None,
+                                        function_name="equals",
+                                        parameters=(
+                                            Column(
+                                                alias=None,
+                                                table_name=None,
+                                                column_name="location",
+                                            ),
+                                            FunctionCall(
+                                                alias=None,
+                                                function_name="gps",
+                                                parameters=(
+                                                    Column(
+                                                        alias=None,
+                                                        table_name=None,
+                                                        column_name="x",
+                                                    ),
+                                                    Column(
+                                                        alias=None,
+                                                        table_name=None,
+                                                        column_name="y",
+                                                    ),
+                                                    Column(
+                                                        alias=None,
+                                                        table_name=None,
+                                                        column_name="z",
+                                                    ),
+                                                ),
+                                            ),
+                                        ),
+                                    ),
+                                ),
+                            ),
+                        ),
+                    ),
+                    FunctionCall(
+                        alias=None,
+                        function_name="greater",
+                        parameters=(
+                            Column(
+                                alias=None, table_name=None, column_name="times_seen"
+                            ),
+                            Literal(alias=None, value=0),
+                        ),
+                    ),
+                ),
+            ),
+        ),
+        id="Example 4",
     ),
 ]
 
