184f86aa12e63e58814bb61bb0ec90b2c91e4f9c
Evan Hicks <evan.hicks@sentry.io>
2020-08-25 16:38:55 -0400

    stupid mistake

--- snuba/query/processors/performance_expressions.py
+++ snuba/query/processors/performance_expressions.py
@@ -1,4 +1,4 @@
-from snuba.clickhouse.columns import ColumnSet, String
+from snuba.clickhouse.columns import ColumnSet, UInt
 from snuba.query.validation.signature import Column as ColType, Literal as LiteralType
 from snuba.query.processors.custom_function import CustomFunction
 
@@ -7,6 +7,6 @@ def apdex_processor(columns: ColumnSet):
     return CustomFunction(
         columns,
         "apdex",
-        [("column", ColType({String})), ("satisfied", LiteralType({int}))],
+        [("column", ColType({UInt})), ("satisfied", LiteralType({int}))],
         "divide(plus(countIf(lessOrEquals(column, satisfied)), divide(countIf(and(greater(column, satisfied), lessOrEquals(column, multiply(satisfied, 4)))), 2)), count())",
     )
