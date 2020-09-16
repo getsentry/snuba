a289f29ca6ec0c38942b00583b319f3cdc6fa8f0
Filippo Pacifici <filippo.pacifici@gmail.com>
2020-09-15 18:30:08 -0700

    Fix spans schema

--- snuba/datasets/storages/spans.py
+++ snuba/datasets/storages/spans.py
@@ -11,25 +11,23 @@ from snuba.clickhouse.columns import (
     WithDefault,
 )
 from snuba.clusters.storage_sets import StorageSetKey
-from snuba.datasets.schemas import MandatoryCondition
-from snuba.datasets.schemas.tables import ReplacingMergeTreeSchema
+from snuba.datasets.schemas.tables import WritableTableSchema
 from snuba.datasets.spans_processor import UNKNOWN_SPAN_STATUS, SpansMessageProcessor
 from snuba.datasets.storage import WritableTableStorage
 from snuba.datasets.storages import StorageKey
 from snuba.datasets.storages.tags_hash_map import TAGS_HASH_MAP_COLUMN
 from snuba.datasets.table_storage import KafkaStreamLoader
-from snuba.query.conditions import ConditionFunctions, binary_condition
-from snuba.query.expressions import Column, Literal
 from snuba.query.processors.prewhere import PrewhereProcessor
 from snuba.web.split import TimeSplitQueryStrategy
 
 columns = ColumnSet(
     [
         ("project_id", UInt(64)),
-        ("transaction_id", UUID()),  # event_id of the transaction
+        ("trace_id", UUID()),
+        ("transaction_span_id", UInt(64)),
         ("span_id", UInt(64)),
+        ("parent_span_id", UInt(64)),
         ("transaction_name", LowCardinality(String())),
-        ("parent_span_id", UInt(64)),  # This should be able to replace transaction_id
         ("description", LowCardinality(String())),  # description in span
         ("op", LowCardinality(String())),
         ("status", WithDefault(UInt(8), str(UNKNOWN_SPAN_STATUS)),),
@@ -45,37 +43,12 @@ columns = ColumnSet(
     ]
 )
 
-schema = ReplacingMergeTreeSchema(
+schema = WritableTableSchema(
     columns=columns,
     local_table_name="spans_experimental_local",
     dist_table_name="spans_experimental_dist",
     storage_set_key=StorageSetKey.TRANSACTIONS,
-    mandatory_conditions=[
-        MandatoryCondition(
-            ("deleted", "=", 0),
-            binary_condition(
-                None,
-                ConditionFunctions.EQ,
-                Column(None, None, "deleted"),
-                Literal(None, 0),
-            ),
-        )
-    ],
-    prewhere_candidates=["transaction_id", "description"],
-    order_by="(project_id, toStartOfDay(finish_ts), cityHash64(parent_span_id), op, cityHash64(span_id))",
-    partition_by="(toMonday(finish_ts))",
-    version_column="deleted",
-    sample_expr="cityHash64(span_id)",
-    ttl_expr="finish_ts + toIntervalDay(retention_days)",
-    settings={"index_granularity": "8192"},
-    # Tags hashmap is a materialized column. Clickhouse does not allow
-    # us to create a materialized column that references a nested one
-    # during create statement
-    # (https://github.com/ClickHouse/ClickHouse/issues/12586), so the
-    # materialization is added with a migration.
-    skipped_cols_on_creation={"_tags_hash_map"},
 )
-
 
 storage = WritableTableStorage(
     storage_key=StorageKey.SPANS,
@@ -86,5 +59,4 @@ storage = WritableTableStorage(
         processor=SpansMessageProcessor(), default_topic="events",
     ),
     query_splitters=[TimeSplitQueryStrategy(timestamp_col="finish_ts")],
-    writer_options={"insert_allow_materialized_columns": 1},
 )
--- snuba/migrations/snuba_migrations/spans_experimental/0001_spans_experimental.py
+++ snuba/migrations/snuba_migrations/spans_experimental/0001_spans_experimental.py
@@ -19,12 +19,15 @@ from snuba.migrations import migration, operations, table_engines
 
 UNKNOWN_SPAN_STATUS = 2
 
+tags_col = Column("tags", Nested([("key", String()), ("value", String())]))
+
 columns = [
     Column("project_id", UInt(64)),
-    Column("transaction_id", UUID()),  # event_id of the transaction
+    Column("trace_id", UUID()),
+    Column("transaction_span_id", UInt(64)),
     Column("span_id", UInt(64)),
+    Column("parent_span_id", UInt(64)),
     Column("transaction_name", LowCardinality(String())),
-    Column("parent_span_id", UInt(64)),  # This should be able to replace transaction_id
     Column("description", LowCardinality(String())),  # description in span
     Column("op", LowCardinality(String())),
     Column("status", WithDefault(UInt(8), str(UNKNOWN_SPAN_STATUS)),),
@@ -33,7 +36,7 @@ columns = [
     Column("finish_ts", DateTime()),
     Column("finish_ns", UInt(32)),
     Column("duration", UInt(32)),
-    Column("tags", Nested([("key", String()), ("value", String())])),
+    tags_col,
     Column("retention_days", UInt(16)),
     Column("deleted", UInt(8)),
 ]
@@ -51,7 +54,7 @@ class Migration(migration.MultiStepMigration):
                 engine=table_engines.ReplacingMergeTree(
                     storage_set=StorageSetKey.TRANSACTIONS,
                     version_column="deleted",
-                    order_by="(project_id, toStartOfDay(finish_ts), cityHash64(parent_span_id), op, cityHash64(span_id))",
+                    order_by="(project_id, toStartOfDay(finish_ts), cityHash64(transaction_span_id), op, cityHash64(trace_id), cityHash64(span_id))",
                     partition_by="(toMonday(finish_ts))",
                     sample_by="cityHash64(span_id)",
                     ttl="finish_ts + toIntervalDay(retention_days)",
@@ -78,30 +81,19 @@ class Migration(migration.MultiStepMigration):
         ]
 
     def forwards_dist(self) -> Sequence[operations.Operation]:
-        # We removed the materialized for the dist table DDL.
-        def strip_materialized(columns: Sequence[Column]) -> None:
-            for col in columns:
-                if isinstance(col.type, Materialized):
-                    col.type = col.type.inner_type
-
-        dist_columns = deepcopy(columns)
-        strip_materialized(dist_columns)
+        dest_index = columns.index(tags_col)
+        dist_cols = deepcopy(columns)
+        dist_cols.insert(dest_index, Column("_tags_hash_map", Array(UInt(64))))
 
         return [
             operations.CreateTable(
                 storage_set=StorageSetKey.TRANSACTIONS,
                 table_name="spans_experimental_dist",
-                columns=dist_columns,
+                columns=dist_cols,
                 engine=table_engines.Distributed(
                     local_table_name="spans_experimental_local",
-                    sharding_key="cityHash64(parent_span_id)",
+                    sharding_key="cityHash64(transaction_span_id)",
                 ),
-            ),
-            operations.AddColumn(
-                storage_set=StorageSetKey.TRANSACTIONS,
-                table_name="spans_experimental_dist",
-                column=Column("_tags_hash_map", Array(UInt(64))),
-                after="tags.value",
             ),
         ]
 
