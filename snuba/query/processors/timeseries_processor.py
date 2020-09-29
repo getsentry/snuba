1b1bcd9ecc199599035c770b89846986efb024a9
Evan Hicks <evan.hicks@sentry.io>
2020-09-29 11:51:42 -0400

    try to cleanup

--- snuba/query/processors/timeseries_processor.py
+++ snuba/query/processors/timeseries_processor.py
@@ -7,7 +7,6 @@ from snuba.query.expressions import Column, Expression, FunctionCall, Literal
 from snuba.query.logical import Query
 from snuba.query.matchers import (
     Any,
-    AnyExpression,
     Column as ColumnMatch,
     FunctionCall as FunctionCallMatch,
     Literal as LiteralMatch,
@@ -29,10 +28,15 @@ class TimeSeriesProcessor(QueryProcessor):
     def __init__(
         self, time_group_columns: Mapping[str, str], time_parse_columns: Sequence[str]
     ) -> None:
-        self.__time_group_columns = time_group_columns
-        self.__time_parse_columns = time_parse_columns
+        # Column names that should be mapped to different columns.
+        self.__time_replace_columns = time_group_columns
+
+        # Columns names that should be grouped based on the query granularity
+        self.__time_grouped_columns = set(time_group_columns.values())
+
+        # time_parse_columns is a list of columns that, if used in a condition, should be compared with datetimes.
+        # TODO: This handles only the simplest case, what happens if the column is wrapped in a function?
         self.condition_match = FunctionCallMatch(
-            None,
             Or(
                 [
                     String(ConditionFunctions.GT),
@@ -44,43 +48,38 @@ class TimeSeriesProcessor(QueryProcessor):
                 ]
             ),
             (
-                Or(
-                    [
-                        ColumnMatch(
-                            None,
-                            None,
-                            Param(
-                                "column_name",
-                                Or([String(tc) for tc in self.__time_parse_columns]),
-                            ),
-                        ),
-                        FunctionCallMatch(
-                            None,
-                            None,
-                            (
-                                ColumnMatch(
-                                    None,
-                                    None,
-                                    Param(
-                                        "column_name",
-                                        Or(
-                                            [
-                                                String(tc)
-                                                for tc in self.__time_parse_columns
-                                            ]
-                                        ),
-                                    ),
-                                ),
-                                AnyExpression(),
-                            ),
-                        ),
-                    ]
+                ColumnMatch(
+                    None,
+                    Param(
+                        "column_name", Or([String(tc) for tc in time_parse_columns]),
+                    ),
                 ),
-                Param("literal", LiteralMatch(None, Any(str))),
+                Param("literal", LiteralMatch(Any(str))),
             ),
         )
 
-    def time_expr(
+    def __replace_time_column(self, exp: Expression) -> Expression:
+        if isinstance(exp, Column):
+            if exp.column_name in self.__time_replace_columns:
+                real_column_name = self.__time_replace_columns[exp.column_name]
+                return Column(exp.alias, exp.table_name, real_column_name)
+        return exp
+
+    def __group_time_column(
+        self, exp: Expression, granularity: Optional[int]
+    ) -> Expression:
+        if isinstance(exp, Column):
+            if exp.column_name in self.__time_grouped_columns:
+                if granularity is None:
+                    granularity = 3600
+                time_column_fn = self.__group_time_function(
+                    exp.column_name, granularity, exp.alias
+                )
+                return time_column_fn
+
+        return exp
+
+    def __group_time_function(
         self, column_name: str, granularity: int, alias: Optional[str]
     ) -> FunctionCall:
         function_call = {
@@ -126,7 +125,7 @@ class TimeSeriesProcessor(QueryProcessor):
 
         return function_call
 
-    def process_condition(self, exp: Expression) -> Expression:
+    def __process_condition(self, exp: Expression) -> Expression:
         result = self.condition_match.match(exp)
         if result is not None:
             literal = result.expression("literal")
@@ -149,22 +148,18 @@ class TimeSeriesProcessor(QueryProcessor):
         return exp
 
     def process_query(self, query: Query, request_settings: RequestSettings) -> None:
-        def process_column(exp: Expression) -> Expression:
-            if isinstance(exp, Column):
-                if exp.column_name in self.__time_group_columns:
-                    real_column_name = self.__time_group_columns[exp.column_name]
-                    granularity = query.get_granularity()
-                    if granularity is None:
-                        granularity = 3600
-                    time_column_fn = self.time_expr(
-                        real_column_name, granularity, exp.alias
-                    )
-                    return time_column_fn
+        # Process the query in 3 steps:
+        # 1) Use time_group_columns to map the old column names to the new column names
+        query.transform_expressions(self.__replace_time_column)
 
-            return exp
-
-        query.transform_expressions(process_column)
-
+        # 2) Now that all the column names are correct, search the conditions for time based
+        # conditions and make sure the comparison is with a datetime
         condition = query.get_condition_from_ast()
         if condition:
-            query.set_ast_condition(condition.transform(self.process_condition))
+            query.set_ast_condition(condition.transform(self.__process_condition))
+
+        # 3) Map the time_group_columns into functions based on the query granularity.
+        granularity = query.get_granularity()
+        query.transform_expressions(
+            lambda exp: self.__group_time_column(exp, granularity)
+        )
