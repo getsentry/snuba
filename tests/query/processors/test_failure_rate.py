0bdff4accdacf6667a9fd66341c022bfd357c5d8
Evan Hicks <evan.hicks@sentry.io>
2020-10-07 16:25:07 -0400

    fix(discover) Fix error caused by failure_rate()
    If failure_rate is used in an impossible function, the `transaction_status`
    column will be changed to `NULL`. Clickhouse will error when presented with an
    expression of the form `NULL IN (0, 1, 2)`. So change the function to instead be
    chained AND to mitigate this.

--- tests/query/processors/test_failure_rate.py
+++ tests/query/processors/test_failure_rate.py
@@ -3,6 +3,7 @@ from snuba.clickhouse.formatter import ClickhouseExpressionFormatter
 from snuba.datasets.schemas.tables import TableSource
 from snuba.query.conditions import (
     binary_condition,
+    combine_and_conditions,
     ConditionFunctions,
 )
 from snuba.query.dsl import count, divide
@@ -33,19 +34,16 @@ def test_failure_rate_format_expressions() -> None:
                         None,
                         "countIf",
                         (
-                            binary_condition(
-                                None,
-                                ConditionFunctions.NOT_IN,
-                                Column(None, None, "transaction_status"),
-                                FunctionCall(
-                                    None,
-                                    "tuple",
-                                    (
-                                        Literal(alias=None, value=0),
-                                        Literal(alias=None, value=1),
-                                        Literal(alias=None, value=2),
-                                    ),
-                                ),
+                            combine_and_conditions(
+                                [
+                                    binary_condition(
+                                        None,
+                                        ConditionFunctions.NEQ,
+                                        Column(None, None, "transaction_status"),
+                                        Literal(None, code),
+                                    )
+                                    for code in [0, 1, 2]
+                                ]
                             ),
                         ),
                     ),
@@ -68,5 +66,5 @@ def test_failure_rate_format_expressions() -> None:
         ClickhouseExpressionFormatter()
     )
     assert ret == (
-        "(divide(countIf(notIn(transaction_status, tuple(0, 1, 2))), count()) AS perf)"
+        "(divide(countIf(notEquals(transaction_status, 0) AND notEquals(transaction_status, 1) AND notEquals(transaction_status, 2)), count()) AS perf)"
     )
