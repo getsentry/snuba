e37b3a301773a5984c6787f586cc6fb3d0810991
Lyn Nagara <lyn.nagara@gmail.com>
2020-10-07 16:46:04 -0700

    test: BaseApiTest does not extend BaseEventsTest
    Our API tests should not assume that the dataset under test is the
    events dataset. Some time ago there was only one dataset that could be
    queried, but this is no longer the case.

--- tests/base.py
+++ tests/base.py
@@ -74,7 +74,7 @@ class BaseEventsTest(BaseDatasetTest):
         self.write_processed_messages(processed_messages)
 
 
-class BaseApiTest(BaseEventsTest):
+class BaseApiTest(BaseDatasetTest):
     def setup_method(self, test_method, dataset_name="events"):
         super().setup_method(test_method, dataset_name)
         from snuba.web.views import application
