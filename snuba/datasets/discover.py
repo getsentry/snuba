139a8de40121c4fbff3678edb12aa7c125f9bb57
Lyn Nagara <lyn.nagara@gmail.com>
2020-10-13 13:31:00 -0700

    ref(discover): Simplify entity selection methods
    Renames table -> entity where relevant.

--- snuba/datasets/discover.py
+++ snuba/datasets/discover.py
@@ -1,5 +1,4 @@
 import logging
-from typing import Union
 
 from snuba import environment
 from snuba.clickhouse.columns import ColumnSet
@@ -25,31 +24,33 @@ from snuba.query.subscripts import subscript_key_column_name
 from snuba.utils.metrics.wrapper import MetricsWrapper
 
 
+EVENTS = EntityKey.EVENTS
+TRANSACTIONS = EntityKey.DISCOVER_TRANSACTIONS
+EVENTS_AND_TRANSACTIONS = EntityKey.DISCOVER
+
+
 class DiscoverDataset(Dataset):
     def __init__(self) -> None:
-        self.discover_entity = EntityKey.DISCOVER
-        self.discover_transactions_entity = EntityKey.DISCOVER_TRANSACTIONS
-        super().__init__(default_entity=self.discover_entity)
+        super().__init__(default_entity=EntityKey.DISCOVER)
 
     # XXX: This is temporary code that will eventually need to be ported to Sentry
     # since SnQL will require an entity to always be specified by the user.
     def select_entity(self, query: Query) -> EntityKey:
-        table = detect_table(query, EVENTS_COLUMNS, TRANSACTIONS_COLUMNS, True)
+        selected_entity = match_query_to_entity(
+            query, EVENTS_COLUMNS, TRANSACTIONS_COLUMNS
+        )
 
-        if table == EntityKey.TRANSACTIONS:
-            selected_entity = self.discover_transactions_entity
-        else:
-            selected_entity = self.discover_entity
+        track_bad_query(query, selected_entity, EVENTS_COLUMNS, TRANSACTIONS_COLUMNS)
+
+        # For now we resolve the events entity to Discover.
+        if selected_entity == EVENTS:
+            return EVENTS_AND_TRANSACTIONS
 
         return selected_entity
 
 
 metrics = MetricsWrapper(environment.metrics, "api.discover")
 logger = logging.getLogger(__name__)
-
-EVENTS = EntityKey.EVENTS
-TRANSACTIONS = EntityKey.TRANSACTIONS
-EVENTS_AND_TRANSACTIONS = "events_and_transactions"
 
 
 EVENT_CONDITION = FunctionCallMatch(
@@ -65,9 +66,9 @@ TRANSACTION_FUNCTIONS = FunctionCallMatch(
 )
 
 
-def match_query_to_table(
+def match_query_to_entity(
     query: Query, events_only_columns: ColumnSet, transactions_only_columns: ColumnSet
-) -> Union[EntityKey, str]:
+) -> EntityKey:
     # First check for a top level condition on the event type
     condition = query.get_condition_from_ast()
     event_types = set()
@@ -98,7 +99,7 @@ def match_query_to_table(
     if len(event_types) > 0 and "transaction" not in event_types:
         return EVENTS
 
-    # If we cannot clearly pick a table from the top level conditions, then
+    # If we cannot clearly pick an entity from the top level conditions, then
     # inspect the columns requested to infer a selection.
     has_event_columns = False
     has_transaction_columns = False
@@ -136,98 +137,80 @@ def match_query_to_table(
         return EVENTS_AND_TRANSACTIONS
 
 
-def detect_table(
+def track_bad_query(
     query: Query,
+    selected_entity: EntityKey,
     events_only_columns: ColumnSet,
     transactions_only_columns: ColumnSet,
-    track_bad_queries: bool,
-) -> EntityKey:
-    """
-    Given a query, we attempt to guess whether it is better to fetch data from the
-    "events", "transactions" or future merged storage.
+) -> None:
+    event_columns = set()
+    transaction_columns = set()
+    for col in query.get_all_ast_referenced_columns():
+        if events_only_columns.get(col.column_name):
+            event_columns.add(col.column_name)
+        elif transactions_only_columns.get(col.column_name):
+            transaction_columns.add(col.column_name)
 
-    The merged storage resolves to the events storage until errors and transactions
-    are split into separate physical tables.
-    """
-    selected_table = match_query_to_table(
-        query, events_only_columns, transactions_only_columns
-    )
+    for subscript in query.get_all_ast_referenced_subscripts():
+        schema_col_name = subscript_key_column_name(subscript)
+        if events_only_columns.get(schema_col_name):
+            event_columns.add(schema_col_name)
+        if transactions_only_columns.get(schema_col_name):
+            transaction_columns.add(schema_col_name)
 
-    if track_bad_queries:
-        event_columns = set()
-        transaction_columns = set()
-        for col in query.get_all_ast_referenced_columns():
-            if events_only_columns.get(col.column_name):
-                event_columns.add(col.column_name)
-            elif transactions_only_columns.get(col.column_name):
-                transaction_columns.add(col.column_name)
+    event_mismatch = event_columns and selected_entity == TRANSACTIONS
+    transaction_mismatch = transaction_columns and selected_entity in [
+        EVENTS,
+        EVENTS_AND_TRANSACTIONS,
+    ]
 
-        for subscript in query.get_all_ast_referenced_subscripts():
-            schema_col_name = subscript_key_column_name(subscript)
-            if events_only_columns.get(schema_col_name):
-                event_columns.add(schema_col_name)
-            if transactions_only_columns.get(schema_col_name):
-                transaction_columns.add(schema_col_name)
+    if event_mismatch or transaction_mismatch:
+        missing_columns = ",".join(
+            sorted(event_columns if event_mismatch else transaction_columns)
+        )
+        selected_entity_str = (
+            str(selected_entity.value)
+            if isinstance(selected_entity, EntityKey)
+            else selected_entity
+        )
 
-        event_mismatch = event_columns and selected_table == TRANSACTIONS
-        transaction_mismatch = transaction_columns and selected_table in [
-            EVENTS,
-            EVENTS_AND_TRANSACTIONS,
-        ]
+        metrics.increment(
+            "query.impossible",
+            tags={
+                "selected_table": selected_entity_str,
+                "missing_columns": missing_columns,
+            },
+        )
+        logger.warning(
+            "Discover generated impossible query",
+            extra={
+                "selected_table": selected_entity_str,
+                "missing_columns": missing_columns,
+            },
+            exc_info=True,
+        )
 
-        if event_mismatch or transaction_mismatch:
-            missing_columns = ",".join(
-                sorted(event_columns if event_mismatch else transaction_columns)
-            )
-            selected_table_str = (
-                str(selected_table.value)
-                if isinstance(selected_table, EntityKey)
-                else selected_table
-            )
+    if selected_entity == EVENTS_AND_TRANSACTIONS and (
+        event_columns or transaction_columns
+    ):
+        # Not possible in future with merge table
+        missing_events_columns = ",".join(sorted(event_columns))
+        missing_transactions_columns = ",".join(sorted(transaction_columns))
+        metrics.increment(
+            "query.impossible-merge-table",
+            tags={
+                "missing_events_columns": missing_events_columns,
+                "missing_transactions_columns": missing_transactions_columns,
+            },
+        )
+        logger.warning(
+            "Discover generated impossible query - merge table",
+            extra={
+                "missing_events_columns": missing_events_columns,
+                "missing_transactions_columns": missing_transactions_columns,
+            },
+            exc_info=True,
+        )
 
-            metrics.increment(
-                "query.impossible",
-                tags={
-                    "selected_table": selected_table_str,
-                    "missing_columns": missing_columns,
-                },
-            )
-            logger.warning(
-                "Discover generated impossible query",
-                extra={
-                    "selected_table": selected_table_str,
-                    "missing_columns": missing_columns,
-                },
-                exc_info=True,
-            )
-
-        if selected_table == EVENTS_AND_TRANSACTIONS and (
-            event_columns or transaction_columns
-        ):
-            # Not possible in future with merge table
-            missing_events_columns = ",".join(sorted(event_columns))
-            missing_transactions_columns = ",".join(sorted(transaction_columns))
-            metrics.increment(
-                "query.impossible-merge-table",
-                tags={
-                    "missing_events_columns": missing_events_columns,
-                    "missing_transactions_columns": missing_transactions_columns,
-                },
-            )
-            logger.warning(
-                "Discover generated impossible query - merge table",
-                extra={
-                    "missing_events_columns": missing_events_columns,
-                    "missing_transactions_columns": missing_transactions_columns,
-                },
-                exc_info=True,
-            )
-
-        else:
-            metrics.increment("query.success")
-
-    # Default for events and transactions is events
-    final_table = (
-        EntityKey.EVENTS if selected_table != TRANSACTIONS else EntityKey.TRANSACTIONS
-    )
-    return final_table
+    else:
+        metrics.increment("query.success")
