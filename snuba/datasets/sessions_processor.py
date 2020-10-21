46533692da2361033cda8fb777ffacd0792338ef
Arpad Borsos <arpad.borsos@sentry.io>
2020-10-21 16:18:21 +0200

    feat: Ingest and store aggregated session counts

--- snuba/datasets/sessions_processor.py
+++ snuba/datasets/sessions_processor.py
@@ -20,6 +20,7 @@ STATUS_MAPPING = {
     "exited": 1,
     "crashed": 2,
     "abnormal": 3,
+    "errored": 4,
 }
 
 metrics = MetricsWrapper(environment.metrics, "sessions.processor")
@@ -30,6 +31,73 @@ class SessionsProcessor(MessageProcessor):
         # some old relays accidentally emit rows without release
         if message["release"] is None:
             return None
+
+        if message.get("aggregates") is not None:
+            template = {
+                "session_id": NIL_UUID,
+                "seq": 0,
+                "org_id": message["org_id"],
+                "project_id": message["project_id"],
+                "retention_days": message["retention_days"],
+                "duration": MAX_UINT32,
+                "received": datetime.now(),
+                "release": message["release"],
+                "environment": message.get("environment") or "",
+            }
+
+            batch = []
+            for group in message["aggregates"]:
+                started = _ensure_valid_date(
+                    datetime.utcfromtimestamp(group["started"])
+                )
+                if started is None:
+                    continue
+
+                group_template = dict(
+                    template,
+                    distinct_id=str(uuid.UUID(group.get("distinct_id") or NIL_UUID)),
+                    started=started,
+                )
+
+                if group.get("exited") is not None:
+                    batch.append(
+                        dict(
+                            group_template,
+                            status=STATUS_MAPPING["exited"],
+                            errors=0,
+                            quantity=group["exited"],
+                        )
+                    )
+                if group.get("errored") is not None:
+                    batch.append(
+                        dict(
+                            group_template,
+                            status=STATUS_MAPPING["errored"],
+                            errors=1,
+                            quantity=group["errored"],
+                        )
+                    )
+                if group.get("abnormal") is not None:
+                    batch.append(
+                        dict(
+                            group_template,
+                            status=STATUS_MAPPING["abnormal"],
+                            errors=1,
+                            quantity=group["abnormal"],
+                        )
+                    )
+                if group.get("crashed") is not None:
+                    batch.append(
+                        dict(
+                            group_template,
+                            status=STATUS_MAPPING["crashed"],
+                            errors=1,
+                            quantity=group["crashed"],
+                        )
+                    )
+
+            return InsertBatch([batch])
+
         if message["duration"] is None:
             duration = None
         else:
@@ -69,5 +137,6 @@ class SessionsProcessor(MessageProcessor):
             "started": started if started is not None else datetime.now(),
             "release": message["release"],
             "environment": message.get("environment") or "",
+            "quantity": 1,
         }
         return InsertBatch([processed])
