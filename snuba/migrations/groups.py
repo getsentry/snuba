785fae972cc129d09806cbc03a0a888d0489e0ac
Arpad Borsos <arpad.borsos@sentry.io>
2020-10-30 14:45:08 +0100

    feat: Add schema support for pre-aggregated sessions
    This adds a new `quantity` column to the raw set, and `X_preaggr` columns to the
    materialized view table.
    
    Also adds the `user_agent` and `os` columns to both which will be normalized and
    provided at a later point by relay.

--- snuba/migrations/groups.py
+++ snuba/migrations/groups.py
@@ -147,7 +147,7 @@ class SessionsLoader(DirectoryLoader):
         super().__init__("snuba.migrations.snuba_migrations.sessions")
 
     def get_migrations(self) -> Sequence[str]:
-        return ["0001_sessions"]
+        return ["0001_sessions", "0002_sessions_aggregates"]
 
 
 class QuerylogLoader(DirectoryLoader):
