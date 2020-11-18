6371c1f8fc785d622cc300b61b24e45352a097ac
Lyn Nagara <lyn.nagara@gmail.com>
2020-11-18 10:16:16 -0800

    update test

--- tests/utils/test_threaded_function_delegator.py
+++ tests/utils/test_threaded_function_delegator.py
@@ -6,11 +6,18 @@ from snuba.utils.threaded_function_delegator import ThreadedFunctionDelegator
 
 
 def test() -> None:
-    cv = threading.Condition()
+    condition = threading.Condition()
+
+    condition_add_delay = threading.Condition()
+
+    def artificial_delay() -> int:
+        with condition_add_delay:
+            condition_add_delay.wait()
+        return 2
 
     callables = {
         "one": Mock(return_value=1),
-        "two": Mock(return_value=2),
+        "two": Mock(side_effect=artificial_delay),
         "three": Mock(return_value=3),
     }
 
@@ -18,17 +25,23 @@ def test() -> None:
         return ("one", ["two"])
 
     def callback_func(args: List[Tuple[str, int]]) -> None:
-        with cv:
-            assert args == [("one", 1), ("two", 2)]
-            cv.notify()
+        assert args == [("one", 1), ("two", 2)]
+        with condition:
+            condition.notify()
 
     delegator = ThreadedFunctionDelegator[Any, int](
         callables=callables, selector_func=selector_func, callback_func=callback_func,
     )
 
-    with cv:
-        result = delegator.execute(5)
-        cv.wait()
+    result = delegator.execute(5)
+
+    # Allow the second callable to return
+    with condition_add_delay:
+        condition_add_delay.notify()
+
+    # Wait until the callback has finished running
+    with condition:
+        condition.wait()
 
     assert result == 1
 
