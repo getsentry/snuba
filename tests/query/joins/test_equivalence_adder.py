962f59a646e6a8747c60909c3eddbaebc8259308
Filippo Pacifici <filippo.pacifici@gmail.com>
2020-12-11 09:21:05 -0800

    feat(join) Generate semantically equivalent conditions for joined query (#1538)
    This transform a query like:
    
    ```
    MATCH e: Events -asdasd-> g: Groupedmessage
    COLLECT something
    WHERE f(e.project_id) = 1
    ```
    
    Into
    
    ```
    MATCH e: Events -asdasd-> g: Groupedmessage
    COLLECT something
    WHERE f(e.project_id) = 1 AND f(g.project_id) = 1
    ```

---
+++ tests/query/joins/test_equivalence_adder.py
@@ -0,0 +1,276 @@
+from functools import partial
+
+import pytest
+from snuba.datasets.entities import EntityKey
+from snuba.datasets.entities.factory import ENTITY_IMPL
+from snuba.query import SelectedExpression
+from snuba.query.composite import CompositeQuery
+from snuba.query.conditions import (
+    BooleanFunctions,
+    ConditionFunctions,
+    binary_condition,
+    combine_and_conditions,
+)
+from snuba.query.data_source.join import (
+    IndividualNode,
+    JoinClause,
+    JoinCondition,
+    JoinConditionExpression,
+    JoinType,
+)
+from snuba.query.data_source.simple import Entity as EntitySource
+from snuba.query.expressions import Column, Expression, FunctionCall, Literal
+from snuba.query.joins.equivalence_adder import (
+    _classify_single_column_condition,
+    _replace_col,
+    add_equivalent_conditions,
+)
+from snuba.query.joins.pre_processor import QualifiedCol
+from tests.query.joins.equivalence_schema import (
+    EVENTS_SCHEMA,
+    GROUPS_SCHEMA,
+    Events,
+    GroupedMessage,
+)
+
+
+def test_classify_and_replace() -> None:
+    condition = binary_condition(
+        ConditionFunctions.EQ, Column(None, "ev", "project_id"), Literal(None, 1)
+    )
+    assert _classify_single_column_condition(condition, {"ev": EntityKey.EVENTS}) == (
+        QualifiedCol(EntityKey.EVENTS, "project_id"),
+        "ev",
+    )
+
+    assert condition.transform(
+        partial(_replace_col, "ev", "project_id", "gr", "project_id")
+    ) == binary_condition(
+        ConditionFunctions.EQ, Column(None, "gr", "project_id"), Literal(None, 1)
+    )
+
+
+ENTITY_GROUP_JOIN = JoinClause(
+    IndividualNode("ev", EntitySource(EntityKey.EVENTS, EVENTS_SCHEMA, None)),
+    IndividualNode("gr", EntitySource(EntityKey.GROUPEDMESSAGES, GROUPS_SCHEMA, None)),
+    [
+        JoinCondition(
+            JoinConditionExpression("ev", "group_id"),
+            JoinConditionExpression("gr", "id"),
+        )
+    ],
+    JoinType.INNER,
+    None,
+)
+
+
+TEST_REPLACEMENT = [
+    pytest.param(
+        binary_condition(
+            ConditionFunctions.EQ, Column(None, "ev", "event_id"), Literal(None, 1)
+        ),
+        ENTITY_GROUP_JOIN,
+        binary_condition(
+            ConditionFunctions.EQ, Column(None, "ev", "event_id"), Literal(None, 1)
+        ),
+        id="No condition to add",
+    ),
+    pytest.param(
+        binary_condition(
+            ConditionFunctions.EQ, Column(None, "ev", "event_id"), Literal(None, 1)
+        ),
+        JoinClause(
+            IndividualNode("ev", EntitySource(EntityKey.EVENTS, EVENTS_SCHEMA, None)),
+            IndividualNode("ev2", EntitySource(EntityKey.EVENTS, EVENTS_SCHEMA, None)),
+            [
+                JoinCondition(
+                    JoinConditionExpression("ev", "event_id"),
+                    JoinConditionExpression("ev2", "event_id"),
+                )
+            ],
+            JoinType.INNER,
+            None,
+        ),
+        combine_and_conditions(
+            [
+                binary_condition(
+                    ConditionFunctions.EQ,
+                    Column(None, "ev", "event_id"),
+                    Literal(None, 1),
+                ),
+                binary_condition(
+                    ConditionFunctions.EQ,
+                    Column(None, "ev2", "event_id"),
+                    Literal(None, 1),
+                ),
+            ]
+        ),
+        id="Self join. Duplicate condition",
+    ),
+    pytest.param(
+        binary_condition(
+            ConditionFunctions.EQ, Column(None, "ev", "project_id"), Literal(None, 1)
+        ),
+        ENTITY_GROUP_JOIN,
+        combine_and_conditions(
+            [
+                binary_condition(
+                    ConditionFunctions.EQ,
+                    Column(None, "ev", "project_id"),
+                    Literal(None, 1),
+                ),
+                binary_condition(
+                    ConditionFunctions.EQ,
+                    Column(None, "gr", "project_id"),
+                    Literal(None, 1),
+                ),
+            ],
+        ),
+        id="Add project_id simple condition",
+    ),
+    pytest.param(
+        binary_condition(
+            ConditionFunctions.EQ,
+            FunctionCall(
+                None,
+                "some_func",
+                (Column(None, "ev", "project_id"), Column(None, "ev", "message")),
+            ),
+            Literal(None, 1),
+        ),
+        ENTITY_GROUP_JOIN,
+        binary_condition(
+            ConditionFunctions.EQ,
+            FunctionCall(
+                None,
+                "some_func",
+                (Column(None, "ev", "project_id"), Column(None, "ev", "message")),
+            ),
+            Literal(None, 1),
+        ),
+        id="Top level condition containing multiple columns. No addition",
+    ),
+    pytest.param(
+        FunctionCall(
+            None,
+            BooleanFunctions.OR,
+            (
+                binary_condition(
+                    ConditionFunctions.EQ,
+                    Column(None, "ev", "project_id"),
+                    Literal(None, 1),
+                ),
+                binary_condition(
+                    ConditionFunctions.EQ,
+                    Column(None, "gr", "message"),
+                    Literal(None, "asd"),
+                ),
+            ),
+        ),
+        ENTITY_GROUP_JOIN,
+        FunctionCall(
+            None,
+            BooleanFunctions.OR,
+            (
+                binary_condition(
+                    ConditionFunctions.EQ,
+                    Column(None, "ev", "project_id"),
+                    Literal(None, 1),
+                ),
+                binary_condition(
+                    ConditionFunctions.EQ,
+                    Column(None, "gr", "message"),
+                    Literal(None, "asd"),
+                ),
+            ),
+        ),
+        id="Or condition with columns of both tables. No addition",
+    ),
+    pytest.param(
+        binary_condition(
+            ConditionFunctions.EQ,
+            FunctionCall(None, "func", (Column(None, "ev", "project_id"),)),
+            Literal(None, 1),
+        ),
+        ENTITY_GROUP_JOIN,
+        combine_and_conditions(
+            [
+                binary_condition(
+                    ConditionFunctions.EQ,
+                    FunctionCall(None, "func", (Column(None, "ev", "project_id"),)),
+                    Literal(None, 1),
+                ),
+                binary_condition(
+                    ConditionFunctions.EQ,
+                    FunctionCall(None, "func", (Column(None, "gr", "project_id"),)),
+                    Literal(None, 1),
+                ),
+            ],
+        ),
+        id="Complex condition, one column. Add a new one",
+    ),
+    pytest.param(
+        combine_and_conditions(
+            [
+                binary_condition(
+                    ConditionFunctions.EQ,
+                    FunctionCall(None, "func", (Column(None, "ev", "project_id"),)),
+                    Literal(None, 1),
+                ),
+                binary_condition(
+                    ConditionFunctions.EQ, Column(None, "gr", "id"), Literal(None, 1),
+                ),
+            ]
+        ),
+        ENTITY_GROUP_JOIN,
+        combine_and_conditions(
+            [
+                binary_condition(
+                    ConditionFunctions.EQ,
+                    FunctionCall(None, "func", (Column(None, "ev", "project_id"),)),
+                    Literal(None, 1),
+                ),
+                binary_condition(
+                    ConditionFunctions.EQ, Column(None, "gr", "id"), Literal(None, 1),
+                ),
+                binary_condition(
+                    ConditionFunctions.EQ,
+                    FunctionCall(None, "func", (Column(None, "gr", "project_id"),)),
+                    Literal(None, 1),
+                ),
+                binary_condition(
+                    ConditionFunctions.EQ,
+                    Column(None, "ev", "group_id"),
+                    Literal(None, 1),
+                ),
+            ]
+        ),
+        id="Multiple valid conditions to add",
+    ),
+]
+
+
+@pytest.mark.parametrize(
+    "initial_condition, join_clause, expected_expr", TEST_REPLACEMENT
+)
+def test_add_equivalent_condition(
+    initial_condition: Expression,
+    join_clause: JoinClause[EntitySource],
+    expected_expr: Expression,
+) -> None:
+    ENTITY_IMPL[EntityKey.EVENTS] = Events()
+    ENTITY_IMPL[EntityKey.GROUPEDMESSAGES] = GroupedMessage()
+
+    query = CompositeQuery(
+        from_clause=join_clause,
+        selected_columns=[
+            SelectedExpression(
+                "group_id", FunctionCall("something", "f", (Column(None, "gr", "id"),))
+            )
+        ],
+        condition=initial_condition,
+    )
+    add_equivalent_conditions(query)
+    assert query.get_condition_from_ast() == expected_expr
+
+    ENTITY_IMPL.clear()
--- tests/query/joins/test_equivalences.py
+++ tests/query/joins/test_equivalences.py
@@ -1,145 +1,27 @@
-from abc import ABC
-from typing import Mapping, Sequence
-from unittest.mock import Mock
-
 import pytest
-from snuba.clickhouse.columns import UUID, ColumnSet, String, UInt
 from snuba.datasets.entities import EntityKey
 from snuba.datasets.entities.factory import ENTITY_IMPL
-from snuba.datasets.entity import Entity
 from snuba.query.data_source.join import (
-    ColumnEquivalence,
     IndividualNode,
     JoinClause,
     JoinCondition,
     JoinConditionExpression,
-    JoinRelationship,
     JoinType,
 )
 from snuba.query.data_source.simple import Entity as EntitySource
-from snuba.query.extensions import QueryExtension
 from snuba.query.joins.pre_processor import (
     EquivalenceGraph,
     QualifiedCol,
     get_equivalent_columns,
 )
-from snuba.query.processors import QueryProcessor
-
-EVENTS_SCHEMA = ColumnSet(
-    [
-        ("event_id", UUID()),
-        ("project_id", UInt(32)),
-        ("message", String()),
-        ("group_id", UInt(32)),
-        ("user_id", UInt(64)),
-    ]
+from tests.query.joins.equivalence_schema import (
+    EVENTS_SCHEMA,
+    GROUPS_ASSIGNEE,
+    GROUPS_SCHEMA,
+    Events,
+    GroupAssignee,
+    GroupedMessage,
 )
-
-GROUPS_SCHEMA = ColumnSet(
-    [
-        ("id", UUID()),
-        ("project_id", UInt(32)),
-        ("message", String()),
-        ("user_id", UInt(64)),
-    ]
-)
-
-GROUPS_ASSIGNEE = ColumnSet(
-    [
-        ("group_id", UUID()),
-        ("project_id", UInt(32)),
-        ("message", String()),
-        ("user_id", UInt(64)),
-    ]
-)
-
-
-class FakeEntity(Entity, ABC):
-    def get_extensions(self) -> Mapping[str, QueryExtension]:
-        return {}
-
-    def get_query_processors(self) -> Sequence[QueryProcessor]:
-        return []
-
-
-class Events(FakeEntity):
-    def __init__(self) -> None:
-        super().__init__(
-            storages=[],
-            query_pipeline_builder=Mock(),
-            abstract_column_set=EVENTS_SCHEMA,
-            join_relationships={
-                "grouped": JoinRelationship(
-                    rhs_entity=EntityKey.GROUPEDMESSAGES,
-                    columns=[("group_id", "id")],
-                    join_type=JoinType.INNER,
-                    equivalences=[ColumnEquivalence("project_id", "project_id")],
-                ),
-                "assigned_group": JoinRelationship(
-                    rhs_entity=EntityKey.GROUPASSIGNEE,
-                    columns=[("group_id", "group_id")],
-                    join_type=JoinType.INNER,
-                    equivalences=[ColumnEquivalence("project_id", "project_id")],
-                ),
-                # This makes no sense but it is for the sake of the test
-                "assigned_user": JoinRelationship(
-                    rhs_entity=EntityKey.GROUPASSIGNEE,
-                    columns=[("user_id", "user_id")],
-                    join_type=JoinType.INNER,
-                    equivalences=[ColumnEquivalence("project_id", "project_id")],
-                ),
-            },
-            writable_storage=None,
-        )
-
-
-class GroupedMessage(FakeEntity):
-    def __init__(self) -> None:
-        super().__init__(
-            storages=[],
-            query_pipeline_builder=Mock(),
-            abstract_column_set=GROUPS_SCHEMA,
-            join_relationships={
-                "events": JoinRelationship(
-                    rhs_entity=EntityKey.EVENTS,
-                    columns=[("id", "group_id")],
-                    join_type=JoinType.INNER,
-                    equivalences=[ColumnEquivalence("project_id", "project_id")],
-                ),
-                "assigned": JoinRelationship(
-                    rhs_entity=EntityKey.GROUPASSIGNEE,
-                    columns=[("user_id", "user_id")],
-                    join_type=JoinType.INNER,
-                    equivalences=[],
-                ),
-            },
-            writable_storage=None,
-        )
-
-
-class GroupAssignee(FakeEntity):
-    def __init__(self) -> None:
-        super().__init__(
-            storages=[],
-            query_pipeline_builder=Mock(),
-            abstract_column_set=GROUPS_ASSIGNEE,
-            join_relationships={
-                "events": JoinRelationship(
-                    rhs_entity=EntityKey.EVENTS,
-                    columns=[("group_id", "group_id")],
-                    join_type=JoinType.INNER,
-                    equivalences=[ColumnEquivalence("project_id", "project_id")],
-                ),
-                "user_assigned": JoinRelationship(
-                    rhs_entity=EntityKey.EVENTS,
-                    columns=[("user_id", "user_id")],
-                    join_type=JoinType.INNER,
-                    equivalences=[ColumnEquivalence("project_id", "project_id")],
-                ),
-            },
-            writable_storage=None,
-        )
-
 
 TEST_CASES = [
     pytest.param(
