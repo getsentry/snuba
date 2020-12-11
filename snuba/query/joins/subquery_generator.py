ad731cef9653cd0282638be3d689e3c8dc79aa98
Filippo Pacifici <filippo.pacifici@gmail.com>
2020-12-10 18:12:49 -0800

    Add tests and docs

--- snuba/query/joins/subquery_generator.py
+++ snuba/query/joins/subquery_generator.py
@@ -11,11 +11,15 @@ from snuba.query.data_source.join import (
     JoinVisitor,
 )
 from snuba.query.data_source.simple import Entity
-from snuba.query.expressions import Column, Expression, SubscriptableReference
+from snuba.query.expressions import Column, Expression
 from snuba.query.logical import Query as LogicalQuery
 
 
 class SubqueryDraft:
+    """
+    Data structure used to progressively build a subquery.
+    """
+
     def __init__(self, data_source: Entity) -> None:
         self.__data_source = data_source
         self.__selected_expressions: Set[SelectedExpression] = set()
@@ -27,7 +31,9 @@ class SubqueryDraft:
         return LogicalQuery(
             {},
             from_clause=self.__data_source,
-            selected_columns=list(self.__selected_expressions),
+            selected_columns=list(
+                sorted(self.__selected_expressions, key=lambda selected: selected.name)
+            ),
         )
 
 
@@ -36,6 +42,12 @@ def aliasify_column(col_name: str) -> str:
 
 
 class SubqueriesInitializer(JoinVisitor[Mapping[str, SubqueryDraft], Entity]):
+    """
+    Visits a join clause and generates SubqueryDraft instances
+    for each node. It adds the columns needed for the ON clause
+    to the selected clause of the subqueries as well.
+    """
+
     def visit_individual_node(
         self, node: IndividualNode[Entity]
     ) -> Mapping[str, SubqueryDraft]:
@@ -50,6 +62,11 @@ class SubqueriesInitializer(JoinVisitor[Mapping[str, SubqueryDraft], Entity]):
         for condition in node.keys:
             combined[condition.left.table_alias].add_select_expression(
                 SelectedExpression(
+                    # Setting a name for the selected columns in a subquery
+                    # is not terribly useful, as this name would not
+                    # be used anywhere.
+                    # The external query references subquery columns by
+                    # their aliases as this is what Clickhouse will do.
                     aliasify_column(condition.left.column),
                     Column(
                         aliasify_column(condition.left.column),
@@ -72,6 +89,11 @@ class SubqueriesInitializer(JoinVisitor[Mapping[str, SubqueryDraft], Entity]):
 
 
 class SubqueriesReplacer(JoinVisitor[JoinNode[Entity], Entity]):
+    """
+    Replaces the entities in the original joins with the relevant
+    SubqueryDraft objects.
+    """
+
     def __init__(self, subqueries: Mapping[str, SubqueryDraft]) -> None:
         self.__subqueries = subqueries
 
@@ -81,6 +103,17 @@ class SubqueriesReplacer(JoinVisitor[JoinNode[Entity], Entity]):
         return IndividualNode(node.alias, self.__subqueries[node.alias].build_query())
 
     def visit_join_clause(self, node: JoinClause[Entity]) -> JoinClause[Entity]:
+        """
+        This tweaks the names of the columns in the ON clause as they
+        cannot reference the entity fields directly (as in the original
+        query) but they have to reference the mangled aliases generated
+        by SubqueriesInitializer.
+
+        This is needed because Clickhouse does not know about the
+        names of the SelectedExpression nodes in the AST. It is only
+        aware of aliases as a way to reference expressions from the
+        external query to the subqueries.
+        """
         left = node.left_node.accept(self)
         right = self.visit_individual_node(node.right_node)
         keys = [
@@ -99,6 +132,34 @@ class SubqueriesReplacer(JoinVisitor[JoinNode[Entity], Entity]):
 
 def generate_subqueries(query: CompositeQuery[Entity]) -> None:
     """
+    Generates correct subqueries for each of the entities referenced in
+    a join query.
+
+    At this stage it pushes down only the columns referenced in the
+    external query so that all the referenced columns are present in
+    the select clause of the subqueries. We need to push down complex
+    expressions to be able to properly process the subqueries.
+
+    Columns in the select clause of the subqueries are referenced
+    by providing them a mangled alias that is referenced in the external
+    query.
+
+    ```
+    SELECT e.a, g.b FROM Events e INNER JOIN Groups g ON ...
+    ```
+
+    becomes
+
+    ```
+    SELECT e._snuba_a, g._snuba_b
+    FROM (
+        SELECT a as _snuba_a
+        FROM events
+    ) e INNER JOIN (
+        SELECT b as _snuba_b
+        FROM groups
+    ) g ON ....
+    ```
     """
 
     from_clause = query.get_from_clause()
@@ -109,12 +170,14 @@ def generate_subqueries(query: CompositeQuery[Entity]) -> None:
         return
 
     # Now this has to be a join, so we can work with it.
-
     subqueries = from_clause.accept(SubqueriesInitializer())
 
     def transform_expression(exp: Expression) -> Expression:
         if isinstance(exp, Column):
             table_alias = exp.table_name
+            # All columns in a joined query need to be qualified. We do
+            # not guess the right subquery for unqualified columns as of
+            # now.
             assert table_alias is not None, f"Invalid query, unqualified column {exp}"
             subqueries[table_alias].add_select_expression(
                 SelectedExpression(
@@ -124,20 +187,6 @@ def generate_subqueries(query: CompositeQuery[Entity]) -> None:
             )
             return Column(None, exp.table_name, aliasify_column(exp.column_name))
 
-        if isinstance(exp, SubscriptableReference):
-            alias = exp.alias or aliasify_column(
-                f"{exp.column.column_name}[{exp.key.value}]"
-            )
-            table_alias = exp.column.table_name
-            assert (
-                table_alias is not None
-            ), f"Invalid query, unqualified subscript {exp}"
-            subqueries[table_alias].add_select_expression(
-                SelectedExpression(
-                    alias, SubscriptableReference(alias, exp.column, exp.key)
-                )
-            )
-            return Column(None, exp.column.table_name, alias)
         return exp
 
     query.transform_expressions(transform_expression)
