3036c1e5cd4a53f9ab2a1c2d230f15209dcd543e
Lyn Nagara <lyn.nagara@gmail.com>
2020-12-17 11:43:33 -0800

    update test to include more than one batch

--- tests/migrations/test_backfill_errors.py
+++ tests/migrations/test_backfill_errors.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 from snuba.clusters.cluster import ClickhouseClientSettings
 from snuba.datasets.storages import StorageKey
 from snuba.datasets.storages.factory import get_writable_storage
@@ -15,13 +17,19 @@ def get_errors_count() -> int:
     return clickhouse.execute(f"SELECT count() from {errors_table_name}")[0][0]
 
 
+@patch("snuba.migrations.backfill_errors.MAX_BATCH_SIZE", 5)
 def test_backfill_errors() -> None:
+    raw_events = []
+    for i in range(10):
+        event = get_raw_event()
+        raw_events.append(event)
+
     events_storage = get_writable_storage(StorageKey.EVENTS)
 
-    write_unprocessed_events(events_storage, [get_raw_event()])
+    write_unprocessed_events(events_storage, raw_events)
 
     assert get_errors_count() == 0
 
     backfill_errors()
 
-    assert get_errors_count() == 1
+    assert get_errors_count() == 10
