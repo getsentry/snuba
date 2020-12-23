f9409c1b3a253d454525cdabf4f7dc68350bf40f
Lyn <lyn.nagara@gmail.com>
2020-12-23 15:04:53 -0800

    feat: Add referrer to Snuplicator logging
    Add the Sentry referrer to Snuplicator logging to help diagnose the underlying
    cause of discrepancies.

--- tests/pipeline/test_pipeline_delegator.py
+++ tests/pipeline/test_pipeline_delegator.py
@@ -53,7 +53,7 @@ def test() -> None:
 
     with cv:
         delegator.build_execution_pipeline(
-            Request("", query, HTTPRequestSettings(), {}, ""), mock_query_runner
+            Request("", query, HTTPRequestSettings(), {}, "ref"), mock_query_runner
         ).execute()
         cv.wait(timeout=5)
 
@@ -61,5 +61,6 @@ def test() -> None:
 
     assert mock_callback.call_args == call(
         query,
+        "ref",
         [Result("events", query_result, ANY), Result("errors", query_result, ANY)],
     )
