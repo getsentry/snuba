3e77cf3d738333f4409c4582a3b26ef6024f7e1d
Lyn <lyn.nagara@gmail.com>
2021-02-01 13:30:16 -0800

    fix(migrations): Fix running first migration with --dry-run
    Fixes a bug that ignored the dry-run flag for the initial migration of the migration
    system, causing this migration to be performed even when --dry-run was passed.

--- snuba/migrations/snuba_migrations/system/0001_migrations.py
+++ snuba/migrations/snuba_migrations/system/0001_migrations.py
@@ -1,6 +1,7 @@
 from typing import Sequence
 
 from snuba.clickhouse.columns import Column, DateTime, Enum, String, UInt
+from snuba.clusters.cluster import get_cluster
 from snuba.clusters.storage_sets import StorageSetKey
 from snuba.migrations import migration, operations
 from snuba.migrations.columns import MigrationModifiers as Modifiers
@@ -28,7 +29,7 @@ class Migration(migration.Migration):
 
     blocking = False
 
-    def __forwards_local(self) -> Sequence[operations.Operation]:
+    def __forwards_local(self) -> Sequence[operations.SqlOperation]:
         return [
             operations.CreateTable(
                 storage_set=StorageSetKey.MIGRATIONS,
@@ -42,14 +43,14 @@ class Migration(migration.Migration):
             ),
         ]
 
-    def __backwards_local(self) -> Sequence[operations.Operation]:
+    def __backwards_local(self) -> Sequence[operations.SqlOperation]:
         return [
             operations.DropTable(
                 storage_set=StorageSetKey.MIGRATIONS, table_name="migrations_local",
             )
         ]
 
-    def __forwards_dist(self) -> Sequence[operations.Operation]:
+    def __forwards_dist(self) -> Sequence[operations.SqlOperation]:
         return [
             operations.CreateTable(
                 storage_set=StorageSetKey.MIGRATIONS,
@@ -61,7 +62,7 @@ class Migration(migration.Migration):
             )
         ]
 
-    def __backwards_dist(self) -> Sequence[operations.Operation]:
+    def __backwards_dist(self) -> Sequence[operations.SqlOperation]:
         return [
             operations.DropTable(
                 storage_set=StorageSetKey.MIGRATIONS, table_name="migrations_dist"
@@ -69,6 +70,10 @@ class Migration(migration.Migration):
         ]
 
     def forwards(self, context: Context, dry_run: bool) -> None:
+        if dry_run:
+            self.__dry_run(self.__forwards_local(), self.__forwards_dist())
+            return
+
         migration_id, logger, update_status = context
         logger.info(f"Running migration: {migration_id}")
         for op in self.__forwards_local():
@@ -79,6 +84,10 @@ class Migration(migration.Migration):
         update_status(Status.COMPLETED)
 
     def backwards(self, context: Context, dry_run: bool) -> None:
+        if dry_run:
+            self.__dry_run(self.__backwards_local(), self.__backwards_dist())
+            return
+
         migration_id, logger, update_status = context
         logger.info(f"Reversing migration: {migration_id}")
         update_status(Status.IN_PROGRESS)
@@ -87,3 +96,25 @@ class Migration(migration.Migration):
         for op in self.__backwards_local():
             op.execute(local=True)
         logger.info(f"Finished reversing: {migration_id}")
+
+    def __dry_run(
+        self,
+        local_operations: Sequence[operations.SqlOperation],
+        dist_operations: Sequence[operations.SqlOperation],
+    ) -> None:
+
+        print("Local operations:")
+
+        for op in local_operations:
+            print(op.format_sql())
+
+        print("\n")
+        print("Dist operations:")
+
+        for op in dist_operations:
+            cluster = get_cluster(op._storage_set)
+
+            if not cluster.is_single_node():
+                print(op.format_sql())
+            else:
+                print("Skipped dist operation - single node cluster")
