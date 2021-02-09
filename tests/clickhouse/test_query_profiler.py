f8e1beaf052094b79f7beb7956eab674e9a9840e
Filippo Pacifici <filippo.pacifici@gmail.com>
2021-02-08 16:56:21 -0800

    feat(joins): Add all the missing columns to the querylog for composite queries (#1655)
    Qualifies all the columns in all the fields in the fields of the querylog so we can make a distinction between different tables on join queries.
    
    It also adds the missing fields for composite queries:
    
    full list of referenced columns in the query
    list of columns in conditions
    list of group by columns
    list of mapping columns
    list of arrayjoin columns

--- tests/clickhouse/test_query_profiler.py
+++ tests/clickhouse/test_query_profiler.py
@@ -3,10 +3,10 @@ from datetime import datetime
 import pytest
 from snuba.clickhouse.columns import ColumnSet
 from snuba.clickhouse.query import Query as ClickhouseQuery
-from snuba.query.composite import CompositeQuery
 from snuba.clickhouse.query_profiler import generate_profile
 from snuba.clickhouse.translators.snuba.mappers import build_mapping_expr
 from snuba.query import SelectedExpression
+from snuba.query.composite import CompositeQuery
 from snuba.query.conditions import (
     BooleanFunctions,
     ConditionFunctions,
@@ -21,7 +21,6 @@ from tests.query.joins.join_structures import (
     clickhouse_groups_node,
     events_groups_join,
 )
-
 
 test_cases = [
     pytest.param(
@@ -68,20 +67,20 @@ test_cases = [
             time_range=31,
             table="events",
             all_columns={
-                "timestamp",
-                "column2",
-                "column3",
-                "contexts.key",
-                "tags.key",
-                "tags.value",
+                "events.timestamp",
+                "events.column2",
+                "events.column3",
+                "events.contexts.key",
+                "events.tags.key",
+                "events.tags.value",
             },
             multi_level_condition=False,
             where_profile=FilterProfile(
-                columns={"timestamp", "tags.key", "tags.value"},
-                mapping_cols={"tags.key", "tags.value"},
+                columns={"events.timestamp", "events.tags.key", "events.tags.value"},
+                mapping_cols={"events.tags.key", "events.tags.value"},
             ),
-            groupby_cols={"column2", "column3"},
-            array_join_cols={"contexts.key"},
+            groupby_cols={"events.column2", "events.column3"},
+            array_join_cols={"events.contexts.key"},
         ),
         id="Query with timestamp, tags, and arrayjoin",
     ),
@@ -108,9 +107,11 @@ test_cases = [
         ClickhouseQueryProfile(
             time_range=None,
             table="events",
-            all_columns={"column2", "timestamp"},
+            all_columns={"events.column2", "events.timestamp"},
             multi_level_condition=True,
-            where_profile=FilterProfile(columns={"timestamp"}, mapping_cols=set(),),
+            where_profile=FilterProfile(
+                columns={"events.timestamp"}, mapping_cols=set(),
+            ),
             groupby_cols=set(),
             array_join_cols=set(),
         ),
@@ -129,6 +130,71 @@ test_cases = [
                     binary_condition(
                         BooleanFunctions.AND,
                         binary_condition(
+                            BooleanFunctions.AND,
+                            binary_condition(
+                                ConditionFunctions.GTE,
+                                Column(None, None, "timestamp"),
+                                Literal(None, datetime(2020, 8, 1)),
+                            ),
+                            binary_condition(
+                                ConditionFunctions.LT,
+                                Column(None, None, "timestamp"),
+                                Literal(None, datetime(2020, 9, 1)),
+                            ),
+                        ),
+                        binary_condition(
+                            ConditionFunctions.EQ,
+                            build_mapping_expr(
+                                "tags[asd]", None, "tags", Literal(None, "asd"),
+                            ),
+                            Literal(None, "sdf"),
+                        ),
+                    ),
+                    [Column("_snuba_group_id", None, "group_id")],
+                ),
+                clickhouse_groups_node(
+                    [SelectedExpression("_snuba_id", Column("_snuba_id", None, "id"))],
+                ),
+            ),
+            selected_columns=[],
+        ),
+        ClickhouseQueryProfile(
+            time_range=31,
+            table="groupedmessage_local,sentry_errors",
+            all_columns={
+                "sentry_errors.group_id",
+                "sentry_errors.timestamp",
+                "groupedmessage_local.id",
+                "sentry_errors.tags.key",
+                "sentry_errors.tags.value",
+            },
+            multi_level_condition=False,
+            where_profile=FilterProfile(
+                columns={
+                    "sentry_errors.timestamp",
+                    "sentry_errors.tags.key",
+                    "sentry_errors.tags.value",
+                },
+                mapping_cols={"sentry_errors.tags.key", "sentry_errors.tags.value"},
+            ),
+            groupby_cols={"sentry_errors.group_id"},
+            array_join_cols=set(),
+        ),
+        id="Simple join with two tables",
+    ),
+    pytest.param(
+        CompositeQuery(
+            from_clause=events_groups_join(
+                clickhouse_events_node(
+                    [
+                        SelectedExpression(
+                            "_snuba_group_id",
+                            Column("_snuba_group_id", None, "group_id"),
+                        ),
+                    ],
+                    binary_condition(
+                        BooleanFunctions.OR,
+                        binary_condition(
                             ConditionFunctions.GTE,
                             Column(None, None, "timestamp"),
                             Literal(None, datetime(2020, 8, 1)),
@@ -141,21 +207,26 @@ test_cases = [
                     ),
                 ),
                 clickhouse_groups_node(
-                    [SelectedExpression("_snuba_id", Column("_snuba_id", None, "id"))],
+                    [SelectedExpression("_snuba_id", Column("_snuba_id", None, "id"))]
                 ),
-            ),
-            selected_columns=[],
+            )
         ),
         ClickhouseQueryProfile(
-            time_range=31,
+            time_range=None,
             table="groupedmessage_local,sentry_errors",
-            all_columns=set(),
-            multi_level_condition=False,
-            where_profile=FilterProfile(columns=set(), mapping_cols=set(),),
+            all_columns={
+                "sentry_errors.timestamp",
+                "sentry_errors.group_id",
+                "groupedmessage_local.id",
+            },
+            multi_level_condition=True,
+            where_profile=FilterProfile(
+                columns={"sentry_errors.timestamp"}, mapping_cols=set(),
+            ),
             groupby_cols=set(),
             array_join_cols=set(),
         ),
-        id="Simple join with two tables",
+        id="Almost empty join query with OR",
     ),
 ]
 
