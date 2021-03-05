ec7720ce61dfe73937b98226d33022498f1f44c3
Evan Hicks <evanh@users.noreply.github.com>
2021-03-05 16:07:14 -0500

    fix(querylog) Don't assume missing keys (#1741)
    The keys sometimes exist and have None values. Need to handle that case as well.

--- snuba/datasets/querylog_processor.py
+++ snuba/datasets/querylog_processor.py
@@ -130,17 +130,22 @@ class QuerylogProcessor(MessageProcessor):
 
         # These fields are sometimes missing from the payload. If they are missing, don't
         # add them to processed so Clickhouse sets a default value for them.
-        missing_fields = {
-            "timestamp": message.get("timing", {}).get("timestamp"),
-            "duration_ms": message.get("timing", {}).get("duration_ms"),
-            "status": message.get("status"),
-        }
-        missing_keys = []
+        missing_fields = {}
+        timing = message.get("timing") or {}
+        if timing.get("timestamp") is not None:
+            missing_fields["timestamp"] = timing["timestamp"]
+        if timing.get("duration_ms") is not None:
+            missing_fields["duration_ms"] = timing["duration_ms"]
+        if message.get("status") is not None:
+            missing_fields["status"] = message["status"]
+
+        missing_keys = set(["timestamp", "duration_ms", "status"])
         for key, val in missing_fields.items():
-            if val is not None and key not in processed:
+            if key in processed:
+                missing_keys.remove(key)
+            elif val is not None:
                 processed[key] = val
-            elif not val:
-                missing_keys.append(key)
+                missing_keys.remove(key)
 
         if missing_keys:
             metrics.increment(
--- tests/datasets/test_querylog_processor.py
+++ tests/datasets/test_querylog_processor.py
@@ -1,4 +1,5 @@
 import uuid
+from copy import deepcopy
 from datetime import datetime
 
 from snuba.consumers.types import KafkaMessageMetadata
@@ -112,3 +113,106 @@ def test_simple() -> None:
         ],
         None,
     )
+
+
+def test_missing_fields() -> None:
+    request_body = {
+        "selected_columns": ["event_id"],
+        "orderby": "event_id",
+        "sample": 0.1,
+        "limit": 100,
+        "offset": 50,
+        "project": 1,
+    }
+
+    query = Query(get_storage(StorageKey.EVENTS).get_schema().get_data_source())
+
+    request = Request(
+        uuid.UUID("a" * 32).hex, request_body, query, HTTPRequestSettings(), "search",
+    )
+
+    time = TestingClock()
+
+    timer = Timer("test", clock=time)
+    time.sleep(0.01)
+
+    orig_message = SnubaQueryMetadata(
+        request=request,
+        dataset="events",
+        timer=timer,
+        query_list=[
+            ClickhouseQueryMetadata(
+                sql="select event_id from sentry_dist sample 0.1 prewhere project_id in (1) limit 50, 100",
+                stats={"sample": 10},
+                status=QueryStatus.SUCCESS,
+                profile=ClickhouseQueryProfile(
+                    time_range=10,
+                    table="events",
+                    all_columns={"timestamp", "tags"},
+                    multi_level_condition=False,
+                    where_profile=FilterProfile(
+                        columns={"timestamp"}, mapping_cols={"tags"},
+                    ),
+                    groupby_cols=set(),
+                    array_join_cols=set(),
+                ),
+                trace_id="b" * 32,
+            )
+        ],
+    ).to_dict()
+
+    messages = []
+    first = deepcopy(orig_message)
+    del first["timing"]
+    del first["status"]
+    messages.append(first)
+
+    second = deepcopy(orig_message)
+    second["timing"] = None
+    second["status"] = None
+
+    for message in messages:
+        processor = (
+            get_writable_storage(StorageKey.QUERYLOG)
+            .get_table_writer()
+            .get_stream_loader()
+            .get_processor()
+        )
+
+        assert processor.process_message(
+            message, KafkaMessageMetadata(0, 0, datetime.now())
+        ) == InsertBatch(
+            [
+                {
+                    "request_id": str(uuid.UUID("a" * 32)),
+                    "request_body": '{"limit": 100, "offset": 50, "orderby": "event_id", "project": 1, "sample": 0.1, "selected_columns": ["event_id"]}',
+                    "referrer": "search",
+                    "dataset": "events",
+                    "projects": [1],
+                    "organization": None,
+                    "clickhouse_queries.sql": [
+                        "select event_id from sentry_dist sample 0.1 prewhere project_id in (1) limit 50, 100"
+                    ],
+                    "clickhouse_queries.status": ["success"],
+                    "clickhouse_queries.trace_id": [str(uuid.UUID("b" * 32))],
+                    "clickhouse_queries.duration_ms": [0],
+                    "clickhouse_queries.stats": ['{"sample": 10}'],
+                    "clickhouse_queries.final": [0],
+                    "clickhouse_queries.cache_hit": [0],
+                    "clickhouse_queries.sample": [10.0],
+                    "clickhouse_queries.max_threads": [0],
+                    "clickhouse_queries.num_days": [10],
+                    "clickhouse_queries.clickhouse_table": [""],
+                    "clickhouse_queries.query_id": [""],
+                    "clickhouse_queries.is_duplicate": [0],
+                    "clickhouse_queries.consistent": [0],
+                    "clickhouse_queries.all_columns": [["tags", "timestamp"]],
+                    "clickhouse_queries.or_conditions": [False],
+                    "clickhouse_queries.where_columns": [["timestamp"]],
+                    "clickhouse_queries.where_mapping_columns": [["tags"]],
+                    "clickhouse_queries.groupby_columns": [[]],
+                    "clickhouse_queries.array_join_columns": [[]],
+                }
+            ],
+            None,
+        )
