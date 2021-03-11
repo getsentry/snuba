6e8c49628e79e028422992627a7d76b887e28ef2
Lyn <lyn.nagara@gmail.com>
2021-03-11 14:47:35 -0800

    feat: Improve UUID processing so it can be easily extended to other columns
    This processor combines the functionality of what is currently split across the
    EventIdColumnProcessor and the UUIDColumnProcessor. It gives us the ability to
    easily add support and optimizations for UUID columns without having a specific
    processor for each column. It also makes it impossible to accidentally apply one
    of the processors but not the other one, or apply them in the wrong order which
    will result in broken behavior. Now you can no longer apply a UUID processor
    without the optimization for conditions as well.
    
    The new processor is only applied to the errors storage to begin with.

--- snuba/query/processors/uuid_column_processor.py
+++ snuba/query/processors/uuid_column_processor.py
@@ -15,6 +15,7 @@ from snuba.query.matchers import (
     Param,
     String,
 )
+from snuba.query.matchers import Any as AnyMatch
 from snuba.query.matchers import Column as ColumnMatch
 from snuba.query.matchers import FunctionCall as FunctionCallMatch
 from snuba.query.matchers import Literal as LiteralMatch
@@ -24,6 +25,141 @@ from snuba.utils.metrics.wrapper import MetricsWrapper
 
 
 metrics = MetricsWrapper(environment.metrics, "api.query.uuid_processor")
+
+
+class UUIDTranslationError(Exception):
+    pass
+
+
+class BetterUUIDColumnProcessor(QueryProcessor):
+    """
+    Processor that handles columns which are stored as UUID in ClickHouse but
+    are represented externally as a non hyphenated hex string.
+    """
+
+    def __init__(self, uuid_columns: Set[str]) -> None:
+        self.__uuid_columns = uuid_columns
+        self.__uuid_column_match = Or([String(col) for col in uuid_columns])
+
+        operator = Param(
+            "operator",
+            Or(
+                [
+                    String(op)
+                    for op in FUNCTION_TO_OPERATOR
+                    if op not in (ConditionFunctions.IN, ConditionFunctions.NOT_IN)
+                ]
+            ),
+        )
+
+        in_operators = Param(
+            "operator",
+            Or((String(ConditionFunctions.IN), String(ConditionFunctions.NOT_IN))),
+        )
+
+        col_func = FunctionCallMatch(
+            String("replaceAll"),
+            (
+                FunctionCallMatch(
+                    String("toString"),
+                    (Param("col", ColumnMatch(None, self.__uuid_column_match)),),
+                ),
+            ),
+            with_optionals=True,
+        )
+
+        literal = Param("literal", LiteralMatch(AnyMatch(str)))
+
+        self.__condition_matcher = Or(
+            [
+                FunctionCallMatch(operator, (literal, col_func)),
+                FunctionCallMatch(operator, (col_func, literal)),
+            ]
+        )
+
+        self.__in_condition_matcher = FunctionCallMatch(
+            in_operators,
+            (col_func, Param("tuple", FunctionCallMatch(String("tuple"), None)),),
+        )
+
+    def process_query(self, query: Query, request_settings: RequestSettings) -> None:
+        def translate_uuid_literal(exp: Expression) -> Expression:
+            try:
+                assert isinstance(exp, Literal)
+                new_val = str(uuid.UUID(str(exp.value)))
+                return Literal(alias=exp.alias, value=new_val)
+            except (AssertionError, ValueError):
+                raise UUIDTranslationError("Not a valid UUID string")
+
+        def process_condition(exp: Expression) -> Expression:
+            match = self.__condition_matcher.match(exp)
+
+            if match:
+                try:
+                    return FunctionCall(
+                        exp.alias,
+                        match.string("operator"),
+                        (
+                            match.expression("col"),
+                            translate_uuid_literal(match.expression("literal")),
+                        ),
+                    )
+                except UUIDTranslationError:
+                    # We probably won't get to this point since we should have validations earlier
+                    # in the pipeline, but just in case return 0 otherwise the query is going to
+                    # cause a type mismatch on ClickHouse
+                    return Literal(exp.alias, 0)
+
+            in_condition_match = self.__in_condition_matcher.match(exp)
+
+            if in_condition_match:
+                try:
+                    tuple_func = in_condition_match.expression("tuple")
+                    assert isinstance(tuple_func, FunctionCall)
+                    new_tuple_func = FunctionCall(
+                        tuple_func.alias,
+                        tuple_func.function_name,
+                        parameters=tuple(
+                            [
+                                translate_uuid_literal(lit)
+                                for lit in tuple_func.parameters
+                            ]
+                        ),
+                    )
+                    return FunctionCall(
+                        exp.alias,
+                        in_condition_match.string("operator"),
+                        (in_condition_match.expression("col"), new_tuple_func,),
+                    )
+                except UUIDTranslationError:
+                    return Literal(exp.alias, 0)
+
+            return exp
+
+        def process_all(exp: Expression) -> Expression:
+            if isinstance(exp, Column):
+                if exp.column_name in self.__uuid_columns:
+                    return FunctionCall(
+                        exp.alias,
+                        "replaceAll",
+                        (
+                            FunctionCall(
+                                None,
+                                "toString",
+                                (Column(None, None, exp.column_name),),
+                            ),
+                            Literal(None, "-"),
+                            Literal(None, ""),
+                        ),
+                    )
+
+            return exp
+
+        query.transform_expressions(process_all)
+
+        condition = query.get_condition()
+        if condition:
+            query.set_ast_condition(condition.transform(process_condition))
 
 
 class UUIDColumnProcessor(QueryProcessor):
