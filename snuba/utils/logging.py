cbb66d3b3af50bb3e20749470723f585361e4dab
Burak Yigit Kaya <ben@byk.im>
2021-03-16 17:29:30 +0300

    less math, more mapping

--- snuba/utils/logging.py
+++ snuba/utils/logging.py
@@ -1,27 +1,15 @@
-# Python log levels are:
-# CRITICAL 50
-# ERROR    40
-# WARNING  30
-# INFO     20
-# DEBUG    10
-# NOTSET    0
-# syslog(3) levels are:
-# EMERGENCY 0
-# ALERT     1
-# CRITICAL  2
-# ERROR     3
-# WARNING   4
-# NOTICE    5
-# INFO      6
-# DEBUG     7
+from logging import NOTSET, DEBUG, INFO, WARNING, ERROR, CRITICAL
+
+
+PYTHON_TO_SYSLOG_MAP = {
+    NOTSET: 7,
+    DEBUG: 7,
+    INFO: 6,
+    WARNING: 4,
+    ERROR: 3,
+    CRITICAL: 2,
+}
 
 
 def pylog_to_syslog_level(level: int) -> int:
-    if level == 0:  # NOTSET
-        return 7  # DEBUG, to process all messages
-
-    syslog_level = 8 - level // 10
-    if syslog_level <= 5:
-        syslog_level -= 1  # Adjust for lack of NOTICE level on Python
-
-    return syslog_level
+    return PYTHON_TO_SYSLOG_MAP.get(level, DEBUG)
