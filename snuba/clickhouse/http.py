1fe7fa44d98aafa841ce59afcfb8b640185a5c7d
Filippo Pacifici <filippo.pacifici@gmail.com>
2021-03-17 15:58:46 -0700

    Address review comments

--- snuba/clickhouse/http.py
+++ snuba/clickhouse/http.py
@@ -33,7 +33,7 @@ CLICKHOUSE_ERROR_RE = re.compile(
 JSONRow = bytes  # a single row in JSONEachRow format
 
 
-class JSONRowEncoder(Encoder[bytes, WriterTableRow]):
+class JSONRowEncoder(Encoder[JSONRow, WriterTableRow]):
     def __default(self, value: Any) -> Any:
         if isinstance(value, datetime):
             return value.strftime(DATETIME_FORMAT)
