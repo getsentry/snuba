e92ff90461eab2ef841c7a62c3f74252a2c209c8
Filippo Pacifici <filippo.pacifici@gmail.com>
2021-03-24 16:20:25 -0700

    Remove cityHash

--- snuba/datasets/errors_replacer.py
+++ snuba/datasets/errors_replacer.py
@@ -413,14 +413,18 @@ def process_tombstone_events(
             ]
         )
     else:
-        event_id_lhs = "cityHash64(event_id)"
-        event_id_list = ", ".join(
-            "cityHash64(toUUID('%s'))" % uuid.UUID(eid) for eid in event_ids
+        event_id_lhs = "event_id"
+        event_id_list = ", ".join("'%s'" % uuid.UUID(eid) for eid in event_ids)
+
+    if old_primary_hash:
+        old_primary_hash_condition = (
+            " AND (%(old_primary_hash)s IS NULL OR primary_hash = %(old_primary_hash)s)"
         )
+    else:
+        old_primary_hash_condition = ""
 
     where = f"""\
-        PREWHERE {event_id_lhs} IN (%(event_ids)s)
-        AND (%(old_primary_hash)s IS NULL OR primary_hash = %(old_primary_hash)s)
+        PREWHERE {event_id_lhs} IN (%(event_ids)s){old_primary_hash_condition}
         WHERE project_id = %(project_id)s {from_condition} {to_condition}
         AND NOT deleted
     """
