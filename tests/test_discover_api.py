9b50058e35d49219f6c2fd500c6546f134669e4a
Lyn Nagara <lyn.nagara@gmail.com>
2021-03-30 15:02:28 -0700

    test: Actually run the errors storage in tests (#1791)
    The previous change f800a2e
    didn't actually do anything since the values were still set to
    false.
    
    Also fixes a Discover test so it works on both storages.

--- tests/test_discover_api.py
+++ tests/test_discover_api.py
@@ -1177,8 +1177,17 @@ class TestDiscoverApi(BaseApiTest):
         )
         data = json.loads(response.data)
         assert len(data["data"]) == 0
+
+        # TODO: This can be simplified once errors rollout is complete
+        # and we no longer need to support tests passing on both storages.
+        release_column = (
+            "`sentry:release`"
+            if self.events_storage == get_writable_storage(StorageKey.EVENTS)
+            else "release"
+        )
+
         assert data["sql"].startswith(
-            "SELECT (type AS _snuba_type), (arrayElement(tags.value, indexOf(tags.key, 'custom_tag')) AS `_snuba_tags[custom_tag]`), (`sentry:release` AS _snuba_release)"
+            f"SELECT (type AS _snuba_type), (arrayElement(tags.value, indexOf(tags.key, 'custom_tag')) AS `_snuba_tags[custom_tag]`), ({release_column} AS _snuba_release)"
         )
 
 
