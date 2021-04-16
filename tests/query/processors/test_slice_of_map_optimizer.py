9ae5ff71dd7086fafce0c05df9ec52dae4b8a95a
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2021-04-16 18:22:36 +0200

    fix(query): Translate queries against Array(UUID) vs Array(FixedString) (#1804)

---
+++ tests/query/processors/test_slice_of_map_optimizer.py
@@ -0,0 +1,107 @@
+import pytest
+
+from snuba.clickhouse.columns import ColumnSet
+from snuba.clickhouse.formatter.expression import ClickhouseExpressionFormatter
+from snuba.query import SelectedExpression
+from snuba.query.data_source.simple import Table
+from snuba.query.expressions import (
+    Column,
+    Expression,
+    FunctionCall,
+    Literal,
+    Lambda,
+    Argument,
+)
+from snuba.clickhouse.query import Query
+from snuba.query.processors.slice_of_map_optimizer import SliceOfMapOptimizer
+from snuba.request.request_settings import HTTPRequestSettings
+
+
+tests = [
+    pytest.param(
+        FunctionCall(
+            None,
+            "arraySlice",
+            (
+                FunctionCall(
+                    None,
+                    "arrayMap",
+                    (
+                        Lambda(
+                            None,
+                            ("x",),
+                            FunctionCall(
+                                None,
+                                "replaceAll",
+                                (
+                                    FunctionCall(
+                                        None, "toString", (Argument(None, "x"),)
+                                    ),
+                                    Literal(None, "-"),
+                                    Literal(None, ""),
+                                ),
+                            ),
+                        ),
+                        Column(None, None, "column1"),
+                    ),
+                ),
+                Literal(None, 0),
+                Literal(None, 2),
+            ),
+        ),
+        FunctionCall(
+            None,
+            "arrayMap",
+            (
+                Lambda(
+                    None,
+                    ("x",),
+                    FunctionCall(
+                        None,
+                        "replaceAll",
+                        (
+                            FunctionCall(None, "toString", (Argument(None, "x"),)),
+                            Literal(None, "-"),
+                            Literal(None, ""),
+                        ),
+                    ),
+                ),
+                FunctionCall(
+                    None,
+                    "arraySlice",
+                    (
+                        Column(None, None, "column1"),
+                        Literal(None, 0),
+                        Literal(None, 2),
+                    ),
+                ),
+            ),
+        ),
+        "arrayMap((x -> replaceAll(toString(x), '-', '')), arraySlice(column1, 0, 2))",
+        id="arrayMap((x -> replaceAll(toString(x), '-', '')), arraySlice(column1, 0, 2))",
+    )
+]
+
+
+@pytest.mark.parametrize("unprocessed, expected, formatted_value", tests)
+def test_uuid_array_column_processor(
+    unprocessed: Expression, expected: Expression, formatted_value: str,
+) -> None:
+    unprocessed_query = Query(
+        Table("transactions", ColumnSet([])),
+        selected_columns=[SelectedExpression("column2", Column(None, None, "column2"))],
+        condition=unprocessed,
+    )
+    expected_query = Query(
+        Table("transactions", ColumnSet([])),
+        selected_columns=[SelectedExpression("column2", Column(None, None, "column2"))],
+        condition=expected,
+    )
+
+    SliceOfMapOptimizer().process_query(unprocessed_query, HTTPRequestSettings())
+
+    assert expected_query.get_condition() == unprocessed_query.get_condition()
+    condition = unprocessed_query.get_condition()
+    assert condition is not None
+    ret = condition.accept(ClickhouseExpressionFormatter())
+    assert ret == formatted_value
