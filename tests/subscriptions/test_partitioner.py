56906cf15e33cc6488c591ce4c12eb85a7fc6959
Lyn <lyn.nagara@gmail.com>
2021-04-26 18:36:57 -0700

    update test

--- tests/subscriptions/test_partitioner.py
+++ tests/subscriptions/test_partitioner.py
@@ -4,14 +4,15 @@ from snuba.datasets.table_storage import KafkaTopicSpec
 from snuba.subscriptions.data import SubscriptionData
 from snuba.subscriptions.partitioner import TopicSubscriptionDataPartitioner
 from tests.subscriptions import BaseSubscriptionTest
+from snuba.utils.streams.topics import Topic
 
 
 class TestBuildRequest(BaseSubscriptionTest):
-    def test(self):
+    def test(self) -> None:
         data = SubscriptionData(
             123, [], [], timedelta(minutes=10), timedelta(minutes=1)
         )
         partitioner = TopicSubscriptionDataPartitioner(
-            KafkaTopicSpec("topic", partitions_number=64)
+            KafkaTopicSpec(Topic.EVENTS, "events-topic", partitions_number=64)
         )
         assert partitioner.build_partition_id(data) == 18
