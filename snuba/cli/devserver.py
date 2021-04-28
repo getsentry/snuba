f6152316900ddde29f225593d9bc4d66fe116a66
Lyn Nagara <lyn.nagara@gmail.com>
2021-04-28 12:07:06 -0700

    feat: Remove the ability to run the events storage in dev and test (#1834)
    Removes support for the ERRORS_ROLLOUT_WRITABLE_STORAGE flag which
    previously allowed switching between the events and errors storage in
    dev and test environments. Only errors can be used now.

--- snuba/cli/devserver.py
+++ snuba/cli/devserver.py
@@ -10,12 +10,6 @@ def devserver(*, bootstrap: bool, workers: bool) -> None:
     import sys
     from subprocess import list2cmdline, call
     from honcho.manager import Manager
-    from snuba import settings
-
-    if settings.ERRORS_ROLLOUT_WRITABLE_STORAGE:
-        events_storage = "errors"
-    else:
-        events_storage = "events"
 
     os.environ["PYTHONUNBUFFERED"] = "1"
 
@@ -79,7 +73,7 @@ def devserver(*, bootstrap: bool, workers: bool) -> None:
                 "consumer",
                 "--auto-offset-reset=latest",
                 "--log-level=debug",
-                f"--storage={events_storage}",
+                "--storage=errors",
             ],
         ),
         (
@@ -89,7 +83,7 @@ def devserver(*, bootstrap: bool, workers: bool) -> None:
                 "replacer",
                 "--auto-offset-reset=latest",
                 "--log-level=debug",
-                f"--storage={events_storage}",
+                "--storage=errors",
             ],
         ),
         (
