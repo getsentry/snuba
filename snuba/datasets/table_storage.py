a3d4d08e483a316f1acbe5ea8afdcdb274411031
Lyn <lyn.nagara@gmail.com>
2021-04-30 17:23:02 -0700

    revert unnecessary change

--- snuba/datasets/table_storage.py
+++ snuba/datasets/table_storage.py
@@ -127,7 +127,6 @@ def build_kafka_stream_loader_from_settings(
         replacement_topic_spec = KafkaTopicSpec(
             replacement_topic, storage_topics.get("replacements"),
         )
-
     elif "replacements" in storage_topics:
         raise ValueError(
             f"invalid topic configuration for {storage_key!r}: replacements unsupported"
