2dd7c91adf0a26f6c40d3e004e59fb6158d53610
Filippo Pacifici <filippo.pacifici@gmail.com>
2021-05-01 20:46:22 -0700

    Add comment

--- snuba/datasets/storages/errors_common.py
+++ snuba/datasets/storages/errors_common.py
@@ -165,7 +165,13 @@ query_processors = [
     MappingOptimizer("tags", "_tags_hash_map", "events_tags_hash_map_enabled"),
     ArrayJoinKeyValueOptimizer("tags"),
     PrewhereProcessor(
-        prewhere_candidates, omit_if_final=["environment", "release", "group_id"]
+        prewhere_candidates,
+        # Environment and release are excluded from prewhere in case of final
+        # queries because of a Clickhouse bug.
+        # group_id instead is excluded since `final` is applied after prewhere.
+        # thus, in this case, we could be filtering out rows that should be
+        # merged together by the final.
+        omit_if_final=["environment", "release", "group_id"],
     ),
 ]
 
