cec3417c9b3356589ccd66e9d7c38e6dd6ef0f38
Filippo Pacifici <filippo.pacifici@gmail.com>
2021-05-19 19:12:33 -0700

    Add basic metrics set entity

--- snuba/datasets/entities/metrics.py
+++ snuba/datasets/entities/metrics.py
@@ -1,36 +1,76 @@
 from typing import Mapping, Sequence
 
-from snuba.clickhouse.columns import ColumnSet
+from snuba.clickhouse.columns import (
+    AggregateFunction,
+    Column,
+    ColumnSet,
+    DateTime,
+    Nested,
+    UInt,
+)
+from snuba.clickhouse.translators.snuba.mappers import ColumnToFunction
+from snuba.clickhouse.translators.snuba.mapping import TranslationMappers
 from snuba.datasets.entity import Entity
 from snuba.datasets.plans.single_storage import SingleStorageQueryPlanBuilder
 from snuba.datasets.storages import StorageKey
-from snuba.datasets.storages.factory import get_writable_storage
+from snuba.datasets.storages.factory import get_storage, get_writable_storage
 from snuba.pipeline.simple_pipeline import SimplePipelineBuilder
+from snuba.query.expressions import Column as ColumnExpr
 from snuba.query.extensions import QueryExtension
 from snuba.query.processors import QueryProcessor
+from snuba.query.processors.basic_functions import BasicFunctionsProcessor
+from snuba.query.processors.project_rate_limiter import ProjectRateLimiterProcessor
+from snuba.query.processors.timeseries_processor import TimeSeriesProcessor
+from snuba.query.validation.validators import EntityRequiredColumnValidator
 
 
-class MetricsEntity(Entity):
+class MetricsSetsEntity(Entity):
     def __init__(self) -> None:
         writable_storage = get_writable_storage(StorageKey.METRICS_BUCKETS)
+        readable_storage = get_storage(StorageKey.METRICS_SETS)
 
         super().__init__(
             # TODO: Add the readable storages
-            storages=[writable_storage],
+            storages=[writable_storage, readable_storage],
             query_pipeline_builder=SimplePipelineBuilder(
-                query_plan_builder=SingleStorageQueryPlanBuilder(writable_storage)
+                query_plan_builder=SingleStorageQueryPlanBuilder(
+                    readable_storage,
+                    mappers=TranslationMappers(
+                        columns=[
+                            ColumnToFunction(
+                                None,
+                                "value",
+                                "uniqCombined64Merge",
+                                (ColumnExpr(None, None, "value"),),
+                            ),
+                        ],
+                    ),
+                )
             ),
-            # TODO: Add the logical schema
-            abstract_column_set=ColumnSet([]),
+            abstract_column_set=ColumnSet(
+                [
+                    Column("org_id", UInt(64)),
+                    Column("project_id", UInt(64)),
+                    Column("metric_id", UInt(64)),
+                    Column("timestamp", DateTime()),
+                    Column("tags", Nested([("key", UInt(64)), ("value", UInt(64))])),
+                    Column(
+                        "value", AggregateFunction("uniqCombined64Merge", [UInt(64)])
+                    ),
+                ]
+            ),
             join_relationships={},
             writable_storage=writable_storage,
-            # TODO: Add the required columns here
-            validators=[],
-            required_time_column=None,
+            validators=[EntityRequiredColumnValidator({"org_id", "project_id"})],
+            required_time_column="timestamp",
         )
 
     def get_extensions(self) -> Mapping[str, QueryExtension]:
         return {}
 
     def get_query_processors(self) -> Sequence[QueryProcessor]:
-        return []
+        return [
+            BasicFunctionsProcessor(),
+            TimeSeriesProcessor({"bucketed_time": "timestamp"}, ("timestamp",)),
+            ProjectRateLimiterProcessor(project_column="project_id"),
+        ]
