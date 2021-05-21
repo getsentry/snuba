0e45ce8f05cd30db463cd047c584eab274398024
Filippo Pacifici <filippo.pacifici@gmail.com>
2021-05-20 20:23:17 -0700

    Support tags properly on metrics

--- snuba/datasets/entities/metrics.py
+++ snuba/datasets/entities/metrics.py
@@ -8,20 +8,46 @@ from snuba.clickhouse.columns import (
     Nested,
     UInt,
 )
-from snuba.clickhouse.translators.snuba.mappers import ColumnToFunction
+from snuba.clickhouse.translators.snuba.mappers import (
+    ColumnToFunction,
+    SubscriptableMapper,
+)
 from snuba.clickhouse.translators.snuba.mapping import TranslationMappers
 from snuba.datasets.entity import Entity
 from snuba.datasets.plans.single_storage import SingleStorageQueryPlanBuilder
 from snuba.datasets.storages import StorageKey
 from snuba.datasets.storages.factory import get_storage, get_writable_storage
 from snuba.pipeline.simple_pipeline import SimplePipelineBuilder
+from snuba.query.exceptions import InvalidExpressionException
 from snuba.query.expressions import Column as ColumnExpr
+from snuba.query.expressions import Expression, Literal, SubscriptableReference
 from snuba.query.extensions import QueryExtension
+from snuba.query.logical import Query
 from snuba.query.processors import QueryProcessor
 from snuba.query.processors.basic_functions import BasicFunctionsProcessor
 from snuba.query.processors.project_rate_limiter import ProjectRateLimiterProcessor
 from snuba.query.processors.timeseries_processor import TimeSeriesProcessor
 from snuba.query.validation.validators import EntityRequiredColumnValidator
+from snuba.request.request_settings import RequestSettings
+
+
+class TagsTypeTransformer(QueryProcessor):
+    def process_query(self, query: Query, request_settings: RequestSettings) -> None:
+        def transform_expression(exp: Expression) -> Expression:
+            if not isinstance(exp, SubscriptableReference):
+                return exp
+
+            key = exp.key
+            if not isinstance(key.value, str) or not key.value.isdigit():
+                raise InvalidExpressionException(
+                    exp, "Expected a string key in subscriptable"
+                )
+
+            return SubscriptableReference(
+                exp.alias, exp.column, Literal(None, int(key.value))
+            )
+
+        query.transform_expressions(transform_expression)
 
 
 class MetricsSetsEntity(Entity):
@@ -43,6 +69,9 @@ class MetricsSetsEntity(Entity):
                                 "uniqCombined64Merge",
                                 (ColumnExpr(None, None, "value"),),
                             ),
+                        ],
+                        subscriptables=[
+                            SubscriptableMapper(None, "tags", None, "tags"),
                         ],
                     ),
                 )
@@ -73,4 +102,5 @@ class MetricsSetsEntity(Entity):
             BasicFunctionsProcessor(),
             TimeSeriesProcessor({"bucketed_time": "timestamp"}, ("timestamp",)),
             ProjectRateLimiterProcessor(project_column="project_id"),
+            TagsTypeTransformer(),
         ]
