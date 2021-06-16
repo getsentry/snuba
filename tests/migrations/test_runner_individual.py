9efe00020a9cd38b323fe3417694cf30acd57de3
Riya Chakraborty <riyamch@yahoo.com>
2021-06-16 15:07:38 -0700

    cleaned up early return from certain tests

--- tests/migrations/test_runner_individual.py
+++ tests/migrations/test_runner_individual.py
@@ -40,70 +40,72 @@ def teardown_function() -> None:
 def test_transactions_compatibility() -> None:
     cluster = get_cluster(StorageSetKey.TRANSACTIONS)
 
-    if cluster.is_single_node():
-        connection = cluster.get_query_connection(ClickhouseClientSettings.MIGRATE)
+    if not cluster.is_single_node():
+        return
 
-        def get_sampling_key() -> str:
-            database = cluster.get_database()
-            ((sampling_key,),) = perform_select_query(
-                ["sampling_key"],
-                "system.tables",
-                {"name": "transactions_local", "database": str(database)},
-                None,
-                connection,
-            )
+    connection = cluster.get_query_connection(ClickhouseClientSettings.MIGRATE)
 
-            return sampling_key
-
-        # Create old style table without sampling expression and insert data
-        connection.execute(
-            """
-            CREATE TABLE transactions_local (`project_id` UInt64, `event_id` UUID,
-            `trace_id` UUID, `span_id` UInt64, `transaction_name` LowCardinality(String),
-            `transaction_hash` UInt64 MATERIALIZED CAST(cityHash64(transaction_name), 'UInt64'),
-            `transaction_op` LowCardinality(String), `transaction_status` UInt8 DEFAULT 2,
-            `start_ts` DateTime, `start_ms` UInt16, `finish_ts` DateTime, `finish_ms` UInt16,
-            `duration` UInt32, `platform` LowCardinality(String), `environment` LowCardinality(Nullable(String)),
-            `release` LowCardinality(Nullable(String)), `dist` LowCardinality(Nullable(String)),
-            `ip_address_v4` Nullable(IPv4), `ip_address_v6` Nullable(IPv6), `user` String DEFAULT '',
-            `user_hash` UInt64 MATERIALIZED cityHash64(user), `user_id` Nullable(String),
-            `user_name` Nullable(String), `user_email` Nullable(String),
-            `sdk_name` LowCardinality(String) DEFAULT CAST('', 'LowCardinality(String)'),
-            `sdk_version` LowCardinality(String) DEFAULT CAST('', 'LowCardinality(String)'),
-            `http_method` LowCardinality(Nullable(String)) DEFAULT CAST('', 'LowCardinality(Nullable(String))'),
-            `http_referer` Nullable(String),
-            `tags.key` Array(String), `tags.value` Array(String), `_tags_flattened` String,
-            `contexts.key` Array(String), `contexts.value` Array(String), `_contexts_flattened` String,
-            `partition` UInt16, `offset` UInt64, `message_timestamp` DateTime, `retention_days` UInt16,
-            `deleted` UInt8) ENGINE = ReplacingMergeTree(deleted) PARTITION BY (retention_days, toMonday(finish_ts))
-            ORDER BY (project_id, toStartOfDay(finish_ts), transaction_name, cityHash64(span_id))
-            TTL finish_ts + toIntervalDay(retention_days);
-            """
+    def get_sampling_key() -> str:
+        database = cluster.get_database()
+        ((sampling_key,),) = perform_select_query(
+            ["sampling_key"],
+            "system.tables",
+            {"name": "transactions_local", "database": str(database)},
+            None,
+            connection,
         )
 
-        assert get_sampling_key() == ""
-        generate_transactions()
+        return sampling_key
 
-        runner = Runner()
-        runner.run_migration(MigrationKey(MigrationGroup.SYSTEM, "0001_migrations"))
+    # Create old style table without sampling expression and insert data
+    connection.execute(
+        """
+        CREATE TABLE transactions_local (`project_id` UInt64, `event_id` UUID,
+        `trace_id` UUID, `span_id` UInt64, `transaction_name` LowCardinality(String),
+        `transaction_hash` UInt64 MATERIALIZED CAST(cityHash64(transaction_name), 'UInt64'),
+        `transaction_op` LowCardinality(String), `transaction_status` UInt8 DEFAULT 2,
+        `start_ts` DateTime, `start_ms` UInt16, `finish_ts` DateTime, `finish_ms` UInt16,
+        `duration` UInt32, `platform` LowCardinality(String), `environment` LowCardinality(Nullable(String)),
+        `release` LowCardinality(Nullable(String)), `dist` LowCardinality(Nullable(String)),
+        `ip_address_v4` Nullable(IPv4), `ip_address_v6` Nullable(IPv6), `user` String DEFAULT '',
+        `user_hash` UInt64 MATERIALIZED cityHash64(user), `user_id` Nullable(String),
+        `user_name` Nullable(String), `user_email` Nullable(String),
+        `sdk_name` LowCardinality(String) DEFAULT CAST('', 'LowCardinality(String)'),
+        `sdk_version` LowCardinality(String) DEFAULT CAST('', 'LowCardinality(String)'),
+        `http_method` LowCardinality(Nullable(String)) DEFAULT CAST('', 'LowCardinality(Nullable(String))'),
+        `http_referer` Nullable(String),
+        `tags.key` Array(String), `tags.value` Array(String), `_tags_flattened` String,
+        `contexts.key` Array(String), `contexts.value` Array(String), `_contexts_flattened` String,
+        `partition` UInt16, `offset` UInt64, `message_timestamp` DateTime, `retention_days` UInt16,
+        `deleted` UInt8) ENGINE = ReplacingMergeTree(deleted) PARTITION BY (retention_days, toMonday(finish_ts))
+        ORDER BY (project_id, toStartOfDay(finish_ts), transaction_name, cityHash64(span_id))
+        TTL finish_ts + toIntervalDay(retention_days);
+        """
+    )
 
-        runner._update_migration_status(
-            MigrationKey(MigrationGroup.TRANSACTIONS, "0001_transactions"),
-            Status.COMPLETED,
-        )
-        runner.run_migration(
-            MigrationKey(
-                MigrationGroup.TRANSACTIONS,
-                "0002_transactions_onpremise_fix_orderby_and_partitionby",
-            ),
-            force=True,
-        )
+    assert get_sampling_key() == ""
+    generate_transactions()
 
-        assert get_sampling_key() == "cityHash64(span_id)"
+    runner = Runner()
+    runner.run_migration(MigrationKey(MigrationGroup.SYSTEM, "0001_migrations"))
 
-        assert perform_select_query(
-            ["count(*)"], "transactions_local", None, None, connection
-        ) == [(5,)]
+    runner._update_migration_status(
+        MigrationKey(MigrationGroup.TRANSACTIONS, "0001_transactions"),
+        Status.COMPLETED,
+    )
+    runner.run_migration(
+        MigrationKey(
+            MigrationGroup.TRANSACTIONS,
+            "0002_transactions_onpremise_fix_orderby_and_partitionby",
+        ),
+        force=True,
+    )
+
+    assert get_sampling_key() == "cityHash64(span_id)"
+
+    assert perform_select_query(
+        ["count(*)"], "transactions_local", None, None, connection
+    ) == [(5,)]
 
 
 def generate_transactions() -> None:
@@ -136,49 +138,50 @@ def generate_transactions() -> None:
 
 
 def test_groupedmessages_compatibility() -> None:
-
     cluster = get_cluster(StorageSetKey.EVENTS)
 
-    if cluster.is_single_node():
-        database = cluster.get_database()
-        connection = cluster.get_query_connection(ClickhouseClientSettings.MIGRATE)
+    if not cluster.is_single_node():
+        return
 
-        # Create old style table witihout project ID
-        connection.execute(
-            """
-            CREATE TABLE groupedmessage_local (`offset` UInt64, `record_deleted` UInt8,
-            `id` UInt64, `status` Nullable(UInt8), `last_seen` Nullable(DateTime),
-            `first_seen` Nullable(DateTime), `active_at` Nullable(DateTime),
-            `first_release_id` Nullable(UInt64)) ENGINE = ReplacingMergeTree(offset)
-            ORDER BY id SAMPLE BY id SETTINGS index_granularity = 8192
-            """
+    database = cluster.get_database()
+    connection = cluster.get_query_connection(ClickhouseClientSettings.MIGRATE)
+
+    # Create old style table witihout project ID
+    connection.execute(
+        """
+        CREATE TABLE groupedmessage_local (`offset` UInt64, `record_deleted` UInt8,
+        `id` UInt64, `status` Nullable(UInt8), `last_seen` Nullable(DateTime),
+        `first_seen` Nullable(DateTime), `active_at` Nullable(DateTime),
+        `first_release_id` Nullable(UInt64)) ENGINE = ReplacingMergeTree(offset)
+        ORDER BY id SAMPLE BY id SETTINGS index_granularity = 8192
+        """
+    )
+
+    migration_id = "0010_groupedmessages_onpremise_compatibility"
+
+    runner = Runner()
+    runner.run_migration(MigrationKey(MigrationGroup.SYSTEM, "0001_migrations"))
+    events_migrations = get_group_loader(MigrationGroup.EVENTS).get_migrations()
+
+    # Mark prior migrations complete
+    for migration in events_migrations[: (events_migrations.index(migration_id))]:
+        runner._update_migration_status(
+            MigrationKey(MigrationGroup.EVENTS, migration), Status.COMPLETED
         )
 
-        migration_id = "0010_groupedmessages_onpremise_compatibility"
+    runner.run_migration(
+        MigrationKey(MigrationGroup.EVENTS, migration_id), force=True,
+    )
 
-        runner = Runner()
-        runner.run_migration(MigrationKey(MigrationGroup.SYSTEM, "0001_migrations"))
-        events_migrations = get_group_loader(MigrationGroup.EVENTS).get_migrations()
+    outcome = perform_select_query(
+        ["primary_key"],
+        "system.tables",
+        {"name": "groupedmessage_local", "database": str(database)},
+        None,
+        connection,
+    )
 
-        # Mark prior migrations complete
-        for migration in events_migrations[: (events_migrations.index(migration_id))]:
-            runner._update_migration_status(
-                MigrationKey(MigrationGroup.EVENTS, migration), Status.COMPLETED
-            )
-
-        runner.run_migration(
-            MigrationKey(MigrationGroup.EVENTS, migration_id), force=True,
-        )
-
-        outcome = perform_select_query(
-            ["primary_key"],
-            "system.tables",
-            {"name": "groupedmessage_local", "database": str(database)},
-            None,
-            connection,
-        )
-
-        assert outcome == [("project_id, id",)]
+    assert outcome == [("project_id, id",)]
 
 
 def run_prior_migrations(
