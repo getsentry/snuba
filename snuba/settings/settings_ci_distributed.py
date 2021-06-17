a99adbdb4acc35f377e9a4b4bd3f8dc6c405dfa8
Riya Chakraborty <riyamch@yahoo.com>
2021-06-17 10:21:12 -0700

    tCI 2 modes method

---
+++ snuba/settings/settings_ci_distributed.py
@@ -0,0 +1,9 @@
+import os
+
+from snuba.settings.settings_test_distributed import *  # NOQA
+
+env = os.environ.get
+
+USE_REDIS_CLUSTER = env("USE_REDIS_CLUSTER", "1") != "0"
+REDIS_DB = 0
+REDIS_PORT = int(env("REDIS_PORT", "7000"))
