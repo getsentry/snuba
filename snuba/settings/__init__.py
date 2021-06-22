71982e9deb1240fe51c472339a2a4cad0b8fd2d5
Lyn <lyn.nagara@gmail.com>
2021-06-21 17:54:23 -0700

    fix

--- snuba/settings/__init__.py
+++ snuba/settings/__init__.py
@@ -48,7 +48,8 @@ DOGSTATSD_HOST = "localhost"
 DOGSTATSD_PORT = 8125
 
 # Redis Options
-USE_REDIS_CLUSTER = os.environ.get("USE_REDIS_CLUSTER", "1") != "1"
+USE_REDIS_CLUSTER = os.environ.get("USE_REDIS_CLUSTER", "0") != "0"
+
 REDIS_CLUSTER_STARTUP_NODES = None
 REDIS_HOST = os.environ.get("REDIS_HOST", "localhost")
 REDIS_PORT = int(os.environ.get("REDIS_PORT", 6379))
