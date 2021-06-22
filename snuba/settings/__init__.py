799bcbeb7c3b060b83e4f6a578a4e2fddaad34e1
Lyn <lyn.nagara@gmail.com>
2021-06-21 17:36:58 -0700

    try switching to the settings_test_distributed file

--- snuba/settings/__init__.py
+++ snuba/settings/__init__.py
@@ -48,12 +48,12 @@ DOGSTATSD_HOST = "localhost"
 DOGSTATSD_PORT = 8125
 
 # Redis Options
-USE_REDIS_CLUSTER = False
+USE_REDIS_CLUSTER = os.environ.get("USE_REDIS_CLUSTER", "1") != "0"
 REDIS_CLUSTER_STARTUP_NODES = None
 REDIS_HOST = os.environ.get("REDIS_HOST", "localhost")
 REDIS_PORT = int(os.environ.get("REDIS_PORT", 6379))
-REDIS_PASSWORD = None
-REDIS_DB = 1
+REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD")
+REDIS_DB = int(os.environ.get("REDIS_DB", 1))
 
 USE_RESULT_CACHE = True
 
