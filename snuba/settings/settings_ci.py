93f1fe6ae46f9be8791b93ec2ffae76ed8b4e6f3
Riya Chakraborty <47572810+ayirr7@users.noreply.github.com>
2021-06-23 17:12:43 -0500

    feat(ci): Run tests against a one shard one replica distributed cluster (#1959)
    This will run the test suite against a second ClickHouse cluster configuration.
    Both are required checks now.

--- snuba/settings/settings_ci.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from snuba.settings.settings_test import *  # NOQA
-
-env = os.environ.get
-
-USE_REDIS_CLUSTER = env("USE_REDIS_CLUSTER", "1") != "0"
-REDIS_DB = 0
-REDIS_PORT = int(env("REDIS_PORT", "7000"))
