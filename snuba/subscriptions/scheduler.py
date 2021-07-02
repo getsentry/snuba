349792b7b3c6605a97625a0b1f30b7b6613cea23
Filippo Pacifici <filippo.pacifici@gmail.com>
2021-07-02 16:44:48 -0700

    Split filter out of the scheduler

--- snuba/subscriptions/scheduler.py
+++ snuba/subscriptions/scheduler.py
@@ -1,12 +1,32 @@
 import math
+from abc import ABC
 from datetime import datetime, timedelta
 from typing import Iterator, List, Optional
 
 from snuba.subscriptions.data import PartitionId, Subscription, SubscriptionIdentifier
 from snuba.subscriptions.store import SubscriptionDataStore
 from snuba.utils.metrics import MetricsBackend
-from snuba.utils.scheduler import Scheduler, ScheduledTask
+from snuba.utils.scheduler import ScheduledTask, Scheduler
 from snuba.utils.types import Interval
+
+
+class SubscriptionFilter(ABC):
+    """
+    Decides when to run a subscription or when to defer it.
+    """
+
+    def filter(self, subscription: Subscription, timestamp: int) -> bool:
+        raise NotImplementedError
+
+
+class ImmediateSubscriptionFilter(SubscriptionFilter):
+    """
+    Schedules a subscription as soon as possible
+    """
+
+    def filter(self, subscription: Subscription, timestamp: int) -> bool:
+        resolution = int(subscription.data.resolution.total_seconds())
+        return timestamp % resolution == 0
 
 
 class SubscriptionScheduler(Scheduler[Subscription]):
@@ -24,6 +44,7 @@ class SubscriptionScheduler(Scheduler[Subscription]):
 
         self.__subscriptions: List[Subscription] = []
         self.__last_refresh: Optional[datetime] = None
+        self.__filter = ImmediateSubscriptionFilter()
 
     def __get_subscriptions(self) -> List[Subscription]:
         current_time = datetime.now()
@@ -56,11 +77,15 @@ class SubscriptionScheduler(Scheduler[Subscription]):
     ) -> Iterator[ScheduledTask[Subscription]]:
         subscriptions = self.__get_subscriptions()
 
+        count = 0
         for timestamp in range(
             math.ceil(interval.lower.timestamp()),
             math.ceil(interval.upper.timestamp()),
         ):
             for subscription in subscriptions:
-                resolution = int(subscription.data.resolution.total_seconds())
-                if timestamp % resolution == 0:
+                if self.__filter.filter(subscription, timestamp):
+                    count += 1
                     yield ScheduledTask(datetime.fromtimestamp(timestamp), subscription)
+
+        if count:
+            self.__metrics.increment("metrics_scheduled", count)
