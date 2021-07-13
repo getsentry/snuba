38d86e24431b25e44c4fe7d65320925d87731c44
Evan Hicks <evan.hicks@sentry.io>
2021-07-13 14:05:11 -0400

    feat(perf) Use snuplicator to test sampling tag queries
    Use snuplicator to run `tagstore.__get_tag_keys` both as normal and with a 10%
    sampling rate and compare the results. For now just track the results, but this
    has the nice property that if the regular query fails we will return the sampled
    results.

--- snuba/datasets/entities/transactions.py
+++ snuba/datasets/entities/transactions.py
@@ -1,7 +1,10 @@
+import random
 from abc import ABC
+from copy import deepcopy
 from datetime import timedelta
-from typing import Mapping, Optional, Sequence
+from typing import List, Mapping, Optional, Sequence, Tuple
 
+from snuba import environment, state
 from snuba.clickhouse.translators.snuba.mappers import (
     ColumnToColumn,
     ColumnToFunction,
@@ -15,10 +18,12 @@ from snuba.datasets.entity import Entity
 from snuba.datasets.plans.single_storage import SingleStorageQueryPlanBuilder
 from snuba.datasets.storages import StorageKey
 from snuba.datasets.storages.factory import get_writable_storage
-from snuba.pipeline.simple_pipeline import SimplePipelineBuilder
+from snuba.pipeline.pipeline_delegator import PipelineDelegator
+from snuba.pipeline.simple_pipeline import EntityQueryPlanner, SimplePipelineBuilder
 from snuba.query.data_source.join import ColumnEquivalence, JoinRelationship, JoinType
 from snuba.query.expressions import Column, FunctionCall, Literal
 from snuba.query.extensions import QueryExtension
+from snuba.query.logical import Query as LogicalQuery
 from snuba.query.processors import QueryProcessor
 from snuba.query.processors.basic_functions import BasicFunctionsProcessor
 from snuba.query.processors.performance_expressions import (
@@ -31,6 +36,13 @@ from snuba.query.processors.timeseries_processor import TimeSeriesProcessor
 from snuba.query.project_extension import ProjectExtension
 from snuba.query.timeseries_extension import TimeSeriesExtension
 from snuba.query.validation.validators import EntityRequiredColumnValidator
+from snuba.request.request_settings import RequestSettings
+from snuba.utils.metrics.wrapper import MetricsWrapper
+from snuba.utils.threaded_function_delegator import Result
+from snuba.web import QueryResult
+
+metrics = MetricsWrapper(environment.metrics, "snuplicator.sampling")
+
 
 transaction_translator = TranslationMappers(
     columns=[
@@ -86,20 +98,89 @@ transaction_translator = TranslationMappers(
 )
 
 
+class SampledSimplePipelineBuilder(SimplePipelineBuilder):
+    def build_planner(
+        self, query: LogicalQuery, settings: RequestSettings,
+    ) -> EntityQueryPlanner:
+        new_query = deepcopy(query)
+        new_query.set_sample(0.1)
+        return super().build_planner(new_query, settings)
+
+
 class BaseTransactionsEntity(Entity, ABC):
     def __init__(self, custom_mappers: Optional[TranslationMappers] = None) -> None:
         storage = get_writable_storage(StorageKey.TRANSACTIONS)
         schema = storage.get_table_writer().get_schema()
 
+        pipeline_builder = SimplePipelineBuilder(
+            query_plan_builder=SingleStorageQueryPlanBuilder(
+                storage=storage,
+                mappers=transaction_translator
+                if custom_mappers is None
+                else transaction_translator.concat(custom_mappers),
+            ),
+        )
+        sampled_pipeline_builder = SampledSimplePipelineBuilder(
+            query_plan_builder=SingleStorageQueryPlanBuilder(
+                storage=storage,
+                mappers=transaction_translator
+                if custom_mappers is None
+                else transaction_translator.concat(custom_mappers),
+            ),
+        )
+
+        def selector_func(query: LogicalQuery, referrer: str) -> Tuple[str, List[str]]:
+            if referrer == "tagstore.__get_tag_keys":
+                sample_query_rate = state.get_config("snuplicator_sampled_query", 0.0)
+                assert isinstance(sample_query_rate, float)
+                if random.random() < sample_query_rate:
+                    return "transactions", ["sampler"]
+
+            return "transactions", []
+
+        def callback_func(
+            query: LogicalQuery, referrer: str, results: List[Result[QueryResult]],
+        ) -> None:
+            if not results:
+                metrics.increment(
+                    "query_result",
+                    tags={"match": "empty", "primary": "none", "referrer": referrer},
+                )
+                return
+
+            primary_result = results.pop(0)
+            primary_function_id = primary_result.function_id
+            secondary_result = results.pop(0) if len(results) > 1 else None
+
+            # compare results
+            comparison = "one_result"
+            if secondary_result:
+                primary_result_data = primary_result.result.result["data"]
+                secondary_result_data = secondary_result.result.result["data"]
+                if secondary_result_data == primary_result_data:
+                    comparison = "match"
+                else:
+                    comparison = "no_match"
+
+            # Track which query(s) succeeded and the execution time
+            metrics.increment(
+                "query_result",
+                tags={
+                    "match": comparison,
+                    "primary": primary_function_id,
+                    "referrer": referrer,
+                },
+            )
+
         super().__init__(
             storages=[storage],
-            query_pipeline_builder=SimplePipelineBuilder(
-                query_plan_builder=SingleStorageQueryPlanBuilder(
-                    storage=storage,
-                    mappers=transaction_translator
-                    if custom_mappers is None
-                    else transaction_translator.concat(custom_mappers),
-                ),
+            query_pipeline_builder=PipelineDelegator(
+                query_pipeline_builders={
+                    "transactions": pipeline_builder,
+                    "sampler": sampled_pipeline_builder,
+                },
+                selector_func=selector_func,
+                callback_func=callback_func,
             ),
             abstract_column_set=schema.get_columns(),
             join_relationships={
