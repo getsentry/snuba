b25c170e68f59f30b3b5b20e3f6e879f3be30033
Vlad Kluev <vlad.kluev@sentry.io>
2021-08-27 13:31:53 -0700

    promote the span_id on errors as well

--- snuba/datasets/storages/errors_common.py
+++ snuba/datasets/storages/errors_common.py
@@ -131,7 +131,7 @@ promoted_tag_columns = {
     "level": "level",
 }
 
-promoted_context_columns = {"trace.trace_id": "trace_id"}
+promoted_context_columns = {"trace.trace_id": "trace_id", "trace.span_id": "span_id"}
 
 mandatory_conditions = [
     binary_condition(
