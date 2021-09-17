16f3eaced51159bed2cfde3c3d56456ed6460f3e
Joris Bayer <joris.bayer@sentry.io>
2021-09-17 14:55:15 +0200

    fix: MetricsDataset.get_all_entities()

--- snuba/datasets/metrics.py
+++ snuba/datasets/metrics.py
@@ -1,7 +1,18 @@
+from typing import Sequence
+
 from snuba.datasets.dataset import Dataset
 from snuba.datasets.entities import EntityKey
+from snuba.datasets.entities.factory import get_entity
+from snuba.datasets.entity import Entity
 
 
 class MetricsDataset(Dataset):
     def __init__(self) -> None:
         super().__init__(default_entity=EntityKey.METRICS_SETS)
+
+    def get_all_entities(self) -> Sequence[Entity]:
+        return (
+            get_entity(EntityKey.METRICS_COUNTERS),
+            get_entity(EntityKey.METRICS_DISTRIBUTIONS),
+            get_entity(EntityKey.METRICS_SETS),
+        )
