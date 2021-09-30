e5537f3d265c2de54e3132de5dda354f072966c4
Vlad Kluev <vlad.kluev@sentry.io>
2021-09-30 16:52:34 -0700

    WIP

--- snuba/state/cache/redis/scripts/get.lua
+++ snuba/state/cache/redis/scripts/get.lua
@@ -6,12 +6,14 @@
 local value_key = KEYS[1]
 local wait_queue_key = KEYS[2]
 local task_id_key = KEYS[3]
+local error_key = KEYS[4]
 local task_timeout_s = ARGV[1]
 local task_id = ARGV[2]
 
 local CODE_RESULT_VALUE = 0
 local CODE_RESULT_EXECUTE = 1
 local CODE_RESULT_WAIT = 2
+local CODE_RESULT_ERROR = 3
 
 -- Check to see if a value already exists at the result key. If one does, we
 -- don't have to do anything other than return it and exit.
@@ -19,6 +21,12 @@ local value = redis.call('GET', value_key)
 if value then
     return {CODE_RESULT_VALUE, value}
 end
+
+local err = redis.call('GET', error_key)
+if err then
+    return {CODE_RESULT_VALUE, err}
+end
+
 
 -- Check to see if a waiting queue has already been established. If we are the
 -- only member of the queue, we can proceed with the task. Otherwise, we need to
