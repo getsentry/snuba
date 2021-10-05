getsentry/snuba

commit eab2c60a0144c2c88ad71e206910ba58acb7b2fe

Cache error values in read through cache for only 100ms (#2125)
### Context

By storing the error values in the cache when errors happen, we end up caching transient errors for way too long. In order to temporarily fix this, error values will have a TTL of only 1s instead of the full 7 minutes that normal result queries have. This will be fixed more properly very shortly.

### Solution

* Make the expiry time for errors values 1s

### Test plan
Add a test that assures that errors values are quickly flushed from the cache. Even when we fix this properly that test should still behave the same way

snuba/state/cache/redis/backend.py
@@ -169,7 +169,9 @@ class RedisCache(Cache[TValue]):
                 argv.extend(
                     [
                         self.__codec.encode_exception(error_value),
-                        get_config("cache_expiry_sec", 1),
+                        # NOTE (Vlad): this is only temporary so we don't cache
+                        # error values for too long
+                        1,
                     ]
                 )
                 raise e

tests/state/test_cache.py
@@ -188,3 +188,35 @@ def test_get_readthrough_set_wait_timeout(backend: Cache[bytes]) -> None:
 
     with pytest.raises(ExecutionTimeoutError):
         waiter_slow.result()
+
+
+def test_transient_error(backend: Cache[bytes]) -> None:
+    key = "key"
+
+    class SomeTransientException(SerializableException):
+        pass
+
+    def error_function() -> bytes:
+        raise SomeTransientException("error")
+
+    def normal_function() -> bytes:
+        return b"hello"
+
+    def transient_error() -> bytes:
+        return backend.get_readthrough(key, error_function, noop, 10)
+
+    def functioning_query() -> bytes:
+        return backend.get_readthrough(key, normal_function, noop, 10)
+
+    setter = execute(transient_error)
+    # if this sleep were removed, the waiter would also raise
+    # SomeTransientException, but because we (temporarily) set
+    # the error value timeout to be 1s, it will go through and execute
+    # the query
+    time.sleep(1.01)
+    waiter = execute(functioning_query)
+
+    with pytest.raises(SomeTransientException):
+        setter.result()
+
+    assert waiter.result() == b"hello"

.github/workflows/ci.yml
@@ -129,7 +129,7 @@ jobs:
 
       - name: Upload to codecov
         run: |
-          bash <(curl -s https://codecov.io/bash)
+          curl -Os https://uploader.codecov.io/latest/linux/codecov && chmod +x codecov && ./codecov -t ${CODECOV_TOKEN}
 
   sentry:
     needs: [snuba-image]

commit 0b6252881cba1a05c35874ec51d589217fbbd81e

Communicate read through cache errors in separate key  (#2128)
### Context
 
The read through cache was changed to communicate errors to waiting clients by storing the error value in place of the result. The result however has a TTL of 7 minutes in production meaning that if a query hits an error, the same error will be propagated for the next 7 minutes.

### Solution

Store error values in a key of their own. If a waiting process receives a notification and there is nothing in the value key, it checks the error key. The value key only holds true result values.

### Test plan

There is already a test for transient errors not being re-raised introduced in #2125 but now the time between tasks can be much smaller as we're not replying on the TTL to handle that invalidation logic for us.

snuba/state/cache/redis/backend.py
@@ -94,6 +94,11 @@ class RedisCache(Cache[TValue]):
         # (or lack thereof.)
         result_key = self.__build_key(key)
 
+        # if we hit an error, we want to communicate that to waiting clients
+        # but we do not want it to be considered a true value. hence we store
+        # the error info in a different key
+        error_key = self.__build_key(key, "error")
+
         # The wait queue (a Redis list) is used to identify clients that are
         # currently "subscribed" to the evaluation of the function and awaiting
         # its result. The first member of this queue is a special case -- it is
@@ -153,6 +158,7 @@ class RedisCache(Cache[TValue]):
                 task_ident,
                 task_timeout,
             )
+            redis_key_to_write_to = result_key
 
             argv = [task_ident, 60]
             try:
@@ -169,11 +175,15 @@ class RedisCache(Cache[TValue]):
                 argv.extend(
                     [
                         self.__codec.encode_exception(error_value),
-                        # NOTE (Vlad): this is only temporary so we don't cache
-                        # error values for too long
-                        1,
+                        # the error data only needs to be present for long enough such that
+                        # the waiting clients know that they all have their queries rejected.
+                        # thus we set it to only three seconds
+                        get_config("error_cache_expiry_sec", 3),
                     ]
                 )
+                # we want the result key to only store real query results in it as the TTL
+                # of a cached query can be fairly long (minutes).
+                redis_key_to_write_to = error_key
                 raise e
             finally:
                 # Regardless of whether the function succeeded or failed, we
@@ -183,7 +193,7 @@ class RedisCache(Cache[TValue]):
                 try:
                     self.__script_set(
                         [
-                            result_key,
+                            redis_key_to_write_to,
                             wait_queue_key,
                             task_ident_key,
                             build_notify_queue_key(task_ident),
@@ -214,7 +224,6 @@ class RedisCache(Cache[TValue]):
                 task_ident,
                 effective_timeout,
             )
-
             notification_received = (
                 self.__client.blpop(
                     build_notify_queue_key(task_ident), effective_timeout
@@ -227,12 +236,17 @@ class RedisCache(Cache[TValue]):
 
             if notification_received:
                 # There should be a value waiting for us at the result key.
-                raw_value = self.__client.get(result_key)
+                raw_value, upsteam_error_payload = self.__client.mget(
+                    [result_key, error_key]
+                )
                 # If there is no value, that means that the client responsible
                 # for generating the cache value errored while generating it.
                 if raw_value is None:
+                    if upsteam_error_payload:
+                        return self.__codec.decode(upsteam_error_payload)
+
                     raise ExecutionError(
-                        "no value at key: this means the original process executing the query crashed before the exception could be handled"
+                        "no value at key: this means the original process executing the query crashed before the exception could be handled or an error was thrown setting the cache result"
                     )
                 else:
                     return self.__codec.decode(raw_value)

snuba/state/cache/redis/scripts/set.lua
@@ -2,7 +2,16 @@
 -- KEYS[2]: The waiting queue key.
 -- KEYS[3]: The task unique ID key.
 -- KEYS[4]: The notify queue key.
+local value_key = KEYS[1]
+local wait_queue_key = KEYS[2]
+local task_id_key = KEYS[3]
+local notify_queue_key = KEYS[4]
+
 -- ARGV[1]: The task unique ID.
+local task_id = ARGV[1]
+local notify_queue_ttl = ARGV[2]
+local value = ARGV[3]
+local value_ttl = ARGV[4]
 -- ARGV[2]: The notify queue TTL.
 -- ARGV[3]: The value. (optional)
 -- ARGV[4]: The value TTL. (optional)
@@ -11,22 +20,22 @@
 -- responsible for executing. If it doesn't exist or does not match the current
 -- task ID, we must have overrun the timeout.
 -- TODO: This may still be able to safely set the cache value?
-local task_id = redis.call('GET', KEYS[3])
-if not task_id or task_id ~= ARGV[1] then
+local cached_task_id = redis.call('GET', task_id_key)
+if not cached_task_id or cached_task_id ~= task_id then
     return {err="invalid task id"}
 end
 
 -- Update the cache value.
-if ARGV[3] ~= nil then
-    redis.call('SETEX', KEYS[1], ARGV[4], ARGV[3])
+if value ~= nil then
+    redis.call('SETEX', value_key, value_ttl, value)
 end
 
 -- Move the data from the waiting queue to the notify queue.
-redis.call('RENAME', KEYS[2], KEYS[4])
-redis.call('EXPIRE', KEYS[4], ARGV[2])
+redis.call('RENAME', wait_queue_key, notify_queue_key)
+redis.call('EXPIRE', notify_queue_key, notify_queue_ttl)
 
 -- Remove one item (representing our own entry) from the notify queue.
-redis.call('LPOP', KEYS[4])
+redis.call('LPOP', notify_queue_key)
 
 -- Delete the task unique key.
-redis.call('DEL', KEYS[3])
+redis.call('DEL', task_id_key)

tests/state/test_cache.py
@@ -5,13 +5,13 @@ import time
 from concurrent.futures import Future, ThreadPoolExecutor
 from functools import partial
 from threading import Thread
-from typing import Any, Callable, Iterator
+from typing import Any, Callable, Iterator, cast
 from unittest import mock
 
 import pytest
 import rapidjson
 
-from snuba.redis import redis_client
+from snuba.redis import RedisClientType, redis_client
 from snuba.state.cache.abstract import Cache, ExecutionTimeoutError
 from snuba.state.cache.redis.backend import RedisCache
 from snuba.utils.codecs import ExceptionAwareCodec
@@ -210,13 +210,80 @@ def test_transient_error(backend: Cache[bytes]) -> None:
 
     setter = execute(transient_error)
     # if this sleep were removed, the waiter would also raise
-    # SomeTransientException, but because we (temporarily) set
-    # the error value timeout to be 1s, it will go through and execute
-    # the query
-    time.sleep(1.01)
+    # SomeTransientException because it would be in the waiting queue and would
+    # have the error value propogated to it
+    time.sleep(0.01)
     waiter = execute(functioning_query)
 
     with pytest.raises(SomeTransientException):
         setter.result()
 
     assert waiter.result() == b"hello"
+
+
+def test_notify_queue_ttl() -> None:
+    # Tests that waiting clients can be notified of the cache status
+    # even with network delays. This test will break if the notify queue
+    # TTL is set below 200ms
+
+    pop_calls = 0
+    num_waiters = 9
+
+    class DelayedRedisClient:
+        def __init__(self, redis_client):
+            self._client = redis_client
+
+        def __getattr__(self, attr: str):
+            # simulate the queue pop taking longer than expected.
+            # the notification queue TTL is 60 seconds so running into a timeout
+            # shouldn't happen (unless something has drastically changed in the TTL
+            # time or use)
+            if attr == "blpop":
+                nonlocal pop_calls
+                pop_calls += 1
+                time.sleep(0.5)
+            return getattr(self._client, attr)
+
+    codec = PassthroughCodec()
+
+    delayed_backend: Cache[bytes] = RedisCache(
+        cast(RedisClientType, DelayedRedisClient(redis_client)),
+        "test",
+        codec,
+        ThreadPoolExecutor(),
+    )
+    key = "key"
+    try:
+
+        def normal_function() -> bytes:
+            # this sleep makes sure that all waiting clients
+            # are put into the waiting queue
+            time.sleep(0.5)
+            return b"hello-cached"
+
+        def normal_function_uncached() -> bytes:
+            return b"hello-not-cached"
+
+        def cached_query() -> bytes:
+            return delayed_backend.get_readthrough(key, normal_function, noop, 10)
+
+        def uncached_query() -> bytes:
+            return delayed_backend.get_readthrough(
+                key, normal_function_uncached, noop, 10
+            )
+
+        setter = execute(cached_query)
+        waiters = []
+        time.sleep(0.1)
+        for _ in range(num_waiters):
+            waiters.append(execute(uncached_query))
+
+        # make sure that all clients actually did hit the cache
+        assert setter.result() == b"hello-cached"
+        for w in waiters:
+            assert w.result() == b"hello-cached"
+        # make sure that all the waiters actually did hit the notification queue
+        # and didn't just get a direct cache hit
+        assert pop_calls == num_waiters
+    finally:
+        redis_client.flushdb()