8d5d0d421e7ff70306478f6009bff2ec6ab56af0
Rahul Saini <rahulsaini805@gmail.com>
2021-10-18 16:41:30 -0700

    Tested new replaced_groups.py functionality

--- tests/datasets/storages/processors/test_replaced_groups.py
+++ tests/datasets/storages/processors/test_replaced_groups.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 from typing import Sequence
 
 import pytest
@@ -32,6 +33,29 @@ def build_in(project_column: str, projects: Sequence[int]) -> Expression:
     )
 
 
+def build_time_range(query_from: datetime, query_to: datetime) -> Expression:
+    return FunctionCall(
+        None,
+        BooleanFunctions.AND,
+        (
+            FunctionCall(
+                None,
+                "greaterOrEquals",
+                (Column(None, None, "timestamp"), Literal(None, query_from)),
+            ),
+            FunctionCall(
+                None,
+                "less",
+                (Column(None, None, "timestamp"), Literal(None, query_to)),
+            ),
+        ),
+    )
+
+
+def build_and(expression_one: Expression, expression_two: Expression) -> Expression:
+    return FunctionCall(None, BooleanFunctions.AND, (expression_one, expression_two))
+
+
 @pytest.fixture
 def query() -> ClickhouseQuery:
     return ClickhouseQuery(
@@ -39,8 +63,40 @@ def query() -> ClickhouseQuery:
     )
 
 
+@pytest.fixture
+def query_with_timestamp() -> ClickhouseQuery:
+    return ClickhouseQuery(
+        Table("my_table", ColumnSet([])),
+        condition=build_and(
+            build_in("project_id", [2]),
+            build_time_range(datetime(2021, 1, 1), datetime(2021, 1, 2)),
+        ),
+    )
+
+
+@pytest.fixture
+def query_with_future_timestamp() -> ClickhouseQuery:
+    return ClickhouseQuery(
+        Table("my_table", ColumnSet([])),
+        condition=build_and(
+            build_in("project_id", [2]),
+            build_time_range(
+                datetime.now() + timedelta(days=1), datetime.now() + timedelta(days=2)
+            ),
+        ),
+    )
+
+
 def teardown_function() -> None:
     redis_client.flushdb()
+
+
+def test_query_set_final(query: ClickhouseQuery) -> None:
+    enforcer = PostReplacementConsistencyEnforcer("project_id", None)
+    enforcer._set_query_final(query, True)
+    assert query.get_from_clause().final
+    enforcer._set_query_final(query, False)
+    assert not query.get_from_clause().final
 
 
 def test_with_turbo(query: ClickhouseQuery) -> None:
@@ -127,3 +183,53 @@ def test_too_many_groups_to_exclude(query: ClickhouseQuery) -> None:
 
     assert query.get_condition() == build_in("project_id", [2])
     assert query.get_from_clause().final
+
+
+def test_query_overlaps_replacements_method(
+    query: ClickhouseQuery, query_with_timestamp: ClickhouseQuery,
+) -> None:
+    enforcer = PostReplacementConsistencyEnforcer("project_id", None)
+    # query from < latest replacement time < query to
+    assert enforcer._query_overlaps_replacements(
+        query_with_timestamp, datetime(2021, 1, 1, 10)
+    )
+    # query to < latest replacement time
+    assert enforcer._query_overlaps_replacements(
+        query_with_timestamp, datetime(2021, 2, 1)
+    )
+    # latest replacement time < query from
+    assert not enforcer._query_overlaps_replacements(
+        query_with_timestamp, datetime(2020, 1, 1)
+    )
+    # replacement time unknown, default to overlaps
+    assert enforcer._query_overlaps_replacements(query_with_timestamp, None)
+    # query time range unknown, default to overlaps
+    assert enforcer._query_overlaps_replacements(query, datetime(2021, 2, 1))
+
+
+def test_query_overlaps_replacements_processor(
+    query_with_timestamp: ClickhouseQuery, query_with_future_timestamp: ClickhouseQuery
+) -> None:
+    enforcer = PostReplacementConsistencyEnforcer("project_id", ReplacerState.EVENTS)
+
+    # replacment time unknown, default to "overlaps" and shouldn't be final
+    enforcer._set_query_final(query_with_timestamp, True)
+    enforcer.process_query(query_with_timestamp, HTTPRequestSettings())
+    assert not query_with_timestamp.get_from_clause().final
+
+    # overlaps replacement and should be final
+    state.set_config("max_group_ids_exclude", 2)
+    set_project_exclude_groups(
+        2,
+        [100, 101, 102],
+        ReplacerState.EVENTS,
+        ReplacementType.EXCLUDE_GROUPS,  # Arbitrary replacement type, no impact on tests
+    )
+    enforcer._set_query_final(query_with_timestamp, True)
+    enforcer.process_query(query_with_timestamp, HTTPRequestSettings())
+    assert query_with_timestamp.get_from_clause().final
+
+    # doesn't overlap replacements
+    enforcer._set_query_final(query_with_future_timestamp, True)
+    enforcer.process_query(query_with_future_timestamp, HTTPRequestSettings())
+    assert not query_with_future_timestamp.get_from_clause().final
