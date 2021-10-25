7ffc33787851e9114f25858b7e776f5da591c3ef
Rahul Kumar Saini <rahul-kumar-saini@users.noreply.github.com>
2021-10-25 11:31:36 -0700

    Perf/avoid final based on query time range (#2138)
    - Refactored errors_replacer.get_project_query_flags() into a Class in the same file
    - Changed redis "project needs final" storage to the timestamp at which the replacement occurred instead of just "True"
    - Introduced logic to find the latest replacement time for a given set of projects
    - Replaced_groups query processor now checks whether or not a given query's time range overlaps the latest replacement, no need to set a query as final if not.
    
    Co-authored-by: Vlad Kluev <vlad.kluev@sentry.io>

--- tests/datasets/storages/processors/test_replaced_groups.py
+++ tests/datasets/storages/processors/test_replaced_groups.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 from typing import Sequence
 
 import pytest
@@ -32,10 +33,51 @@ def build_in(project_column: str, projects: Sequence[int]) -> Expression:
     )
 
 
+def build_time_range(query_from: datetime, query_to: datetime) -> Expression:
+    return build_and(
+        FunctionCall(
+            None,
+            "greaterOrEquals",
+            (Column(None, None, "timestamp"), Literal(None, query_from)),
+        ),
+        FunctionCall(
+            None, "less", (Column(None, None, "timestamp"), Literal(None, query_to)),
+        ),
+    )
+
+
+def build_and(expression_one: Expression, expression_two: Expression) -> Expression:
+    return FunctionCall(None, BooleanFunctions.AND, (expression_one, expression_two))
+
+
 @pytest.fixture
 def query() -> ClickhouseQuery:
     return ClickhouseQuery(
         Table("my_table", ColumnSet([])), condition=build_in("project_id", [2]),
+    )
+
+
+@pytest.fixture
+def query_with_timestamp() -> ClickhouseQuery:
+    return ClickhouseQuery(
+        Table("my_table", ColumnSet([])),
+        condition=build_and(
+            build_in("project_id", [2]),
+            build_time_range(datetime(2021, 1, 1), datetime(2021, 1, 2)),
+        ),
+    )
+
+
+@pytest.fixture
+def query_with_future_timestamp() -> ClickhouseQuery:
+    return ClickhouseQuery(
+        Table("my_table", ColumnSet([])),
+        condition=build_and(
+            build_in("project_id", [2]),
+            build_time_range(
+                datetime.now() + timedelta(days=1), datetime.now() + timedelta(days=2)
+            ),
+        ),
     )
 
 
@@ -88,26 +130,20 @@ def test_not_many_groups_to_exclude(query: ClickhouseQuery) -> None:
         "project_id", ReplacerState.EVENTS
     ).process_query(query, HTTPRequestSettings())
 
-    assert query.get_condition() == FunctionCall(
-        None,
-        BooleanFunctions.AND,
-        (
-            FunctionCall(
-                None,
-                "notIn",
-                (
-                    FunctionCall(
-                        None, "assumeNotNull", (Column(None, None, "group_id"),)
-                    ),
-                    FunctionCall(
-                        None,
-                        "tuple",
-                        (Literal(None, 100), Literal(None, 101), Literal(None, 102),),
-                    ),
+    assert query.get_condition() == build_and(
+        FunctionCall(
+            None,
+            "notIn",
+            (
+                FunctionCall(None, "assumeNotNull", (Column(None, None, "group_id"),)),
+                FunctionCall(
+                    None,
+                    "tuple",
+                    (Literal(None, 100), Literal(None, 101), Literal(None, 102),),
                 ),
             ),
-            build_in("project_id", [2]),
         ),
+        build_in("project_id", [2]),
     )
     assert not query.get_from_clause().final
 
@@ -127,3 +163,38 @@ def test_too_many_groups_to_exclude(query: ClickhouseQuery) -> None:
 
     assert query.get_condition() == build_in("project_id", [2])
     assert query.get_from_clause().final
+
+
+def test_query_overlaps_replacements_processor(
+    query: ClickhouseQuery,
+    query_with_timestamp: ClickhouseQuery,
+    query_with_future_timestamp: ClickhouseQuery,
+) -> None:
+    enforcer = PostReplacementConsistencyEnforcer("project_id", ReplacerState.EVENTS)
+
+    # replacement time unknown, default to "overlaps" but no groups to exclude so shouldn't be final
+    enforcer._set_query_final(query_with_timestamp, True)
+    enforcer.process_query(query_with_timestamp, HTTPRequestSettings())
+    assert not query_with_timestamp.get_from_clause().final
+
+    # overlaps replacement and should be final due to too many groups to exclude
+    state.set_config("max_group_ids_exclude", 2)
+    set_project_exclude_groups(
+        2,
+        [100, 101, 102],
+        ReplacerState.EVENTS,
+        ReplacementType.EXCLUDE_GROUPS,  # Arbitrary replacement type, no impact on tests
+    )
+    enforcer._set_query_final(query_with_timestamp, False)
+    enforcer.process_query(query_with_timestamp, HTTPRequestSettings())
+    assert query_with_timestamp.get_from_clause().final
+
+    # query time range unknown and should be final due to too many groups to exclude
+    enforcer._set_query_final(query, False)
+    enforcer.process_query(query, HTTPRequestSettings())
+    assert query.get_from_clause().final
+
+    # doesn't overlap replacements
+    enforcer._set_query_final(query_with_future_timestamp, True)
+    enforcer.process_query(query_with_future_timestamp, HTTPRequestSettings())
+    assert not query_with_future_timestamp.get_from_clause().final
