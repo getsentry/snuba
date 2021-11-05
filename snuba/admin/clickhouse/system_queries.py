8f1a68165238d8cbf2fbe718fa93829ba3a36308
Vlad Kluev <vlad.kluev@sentry.io>
2021-11-05 10:09:13 -0700

    add system queries

---
+++ snuba/admin/clickhouse/system_queries.py
@@ -0,0 +1,109 @@
+from dataclasses import dataclass
+from typing import Any, Dict, Optional, Sequence, Tuple, Type, Union, cast
+
+from snuba.clickhouse.native import ClickhousePool
+from snuba.datasets.storages import StorageKey
+from snuba.datasets.storages.factory import get_storage
+from snuba.utils.serializable_exception import SerializableException
+
+# from snuba.admin.views import application
+
+# TODO (Vlad): we have to decouple getting a cluster from getting a connection
+# for now though, I'm just going to make it so you can query the query node
+
+
+class NonExistentSystemQuery(SerializableException):
+    pass
+
+
+class _QueryRegistry:
+    """Keep a mapping of SystemQueries to their names"""
+
+    def __init__(self) -> None:
+        self.__mapping: Dict[str, Type["SystemQuery"]] = {}
+
+    def register_class(self, cls: Type["SystemQuery"]) -> None:
+        existing_class = self.__mapping.get(cls.__name__)
+        if not existing_class:
+            self.__mapping[cls.__name__] = cls
+
+    def get_class_by_name(self, cls_name: str) -> Optional[Type["SystemQuery"]]:
+        return self.__mapping.get(cls_name)
+
+    @property
+    def all_queries(self) -> Sequence[Type["SystemQuery"]]:
+        return list(self.__mapping.values())
+
+
+_QUERY_REGISTRY = _QueryRegistry()
+
+
+@dataclass
+class SystemQuery:
+    sql: str
+
+    @classmethod
+    def to_json(cls) -> Dict[str, Optional[str]]:
+        return {
+            "sql": cls.sql,
+            "description": cls.__doc__,
+            "name": cls.__name__,
+        }
+
+    def __init_subclass__(cls) -> None:
+        _QUERY_REGISTRY.register_class(cls)
+        return super().__init_subclass__()
+
+    @classmethod
+    def from_name(cls, name: str) -> Optional[Type["SystemQuery"]]:
+        return _QUERY_REGISTRY.get_class_by_name(name)
+
+    @classmethod
+    def all_queries(cls) -> Sequence[Type["SystemQuery"]]:
+        return _QUERY_REGISTRY.all_queries
+
+
+class CurrentMerges(SystemQuery):
+    """Currently executing merges"""
+
+    sql = """
+        SELECT
+          count(),
+          is_currently_executing
+        FROM system.replication_queue
+        GROUP BY is_currently_executing
+        """
+
+
+class ActivePartitions(SystemQuery):
+    sql = """ SELECT
+        active,
+        count()
+    FROM system.parts
+    GROUP BY active
+    """
+
+
+def run_system_query(
+    clickhouse_host: str, storage_name: str, system_query: Union[str, SystemQuery]
+) -> Tuple[Sequence[Any], Sequence[Tuple[str, str]]]:
+    query = (
+        SystemQuery.from_name(system_query)
+        if isinstance(system_query, str)
+        else system_query
+    )
+    if not query:
+        raise NonExistentSystemQuery
+    clickhouse_port = 9000
+    # TODO: don't create a new pool every time, that's no good
+    storage_key = StorageKey(storage_name)
+    storage = get_storage(storage_key)
+    (clickhouse_user, clickhouse_password) = storage.get_cluster().get_credentials()
+    database = storage.get_cluster().get_database()
+    connection = ClickhousePool(
+        clickhouse_host, clickhouse_port, clickhouse_user, clickhouse_password, database
+    )
+    return cast(
+        Tuple[Sequence[Any], Sequence[Tuple[str, str]]],
+        connection.execute(query=query.sql, with_column_types=True),
+    )
--- snuba/admin/views.py
+++ snuba/admin/views.py
@@ -1,4 +1,6 @@
-from flask import Flask, Response
+from flask import Flask, Response, jsonify, make_response, request
+
+from snuba.admin.clickhouse.system_queries import SystemQuery, run_system_query
 
 application = Flask(__name__, static_url_path="/static", static_folder="dist")
 
@@ -11,3 +13,28 @@ def root() -> Response:
 @application.route("/health")
 def health() -> Response:
     return Response("OK", 200)
+
+
+@application.route("/clickhouse_queries")
+def clickhouse_queries() -> Response:
+    res = [q.to_json() for q in SystemQuery.all_queries()]
+    return make_response(jsonify(res), 200)
+
+
+@application.route("/run_clickhouse_system_query", methods=["POST"])
+def clickhouse_system_query() -> Response:
+    # TODO: You can do something like this to get all the hosts:
+    # SELECT * FROM system.clusters
+    req = request.get_json()
+    results, columns = run_system_query(
+        req.get("host", "localhost"),
+        req.get("storage", "transactions"),
+        req.get("query_name"),
+    )
+    res = {}
+    res["column_names"] = [name for name, _ in columns]
+    res["rows"] = []
+    for row in results:
+        res["rows"].append([str(col) for col in row])
+
+    return make_response(jsonify(res), 200)
