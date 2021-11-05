d44eb4af9314419af59228f72ca6c40f0d88ad96
Lyn Nagara <lyn.nagara@gmail.com>
2021-11-05 10:27:53 -0700

    feat(admin): Add ability to view current configurations (#2191)
    This change adds:
    - A `/configs` endpoint in Snuba admin that returns the current config values (including type information)
    - A table in the `Runtime config` page of Snuba admin app that displays all of the current configurations fetched from that API
    
    There is currently no ability to modify the configurations in any way.

--- snuba/admin/static/data.tsx
+++ snuba/admin/static/data.tsx
@@ -1,10 +1,12 @@
+import RuntimeConfig from "./runtime_config";
+
 function Placeholder(props: any) {
   return null;
 }
 
 const NAV_ITEMS = [
   { id: "overview", display: "Overview", component: Placeholder },
-  { id: "config", display: "Runtime config", component: Placeholder },
+  { id: "config", display: "Runtime config", component: RuntimeConfig },
   {
     id: "clickhouse",
     display: "ClickHouse🏚️",
---
+++ snuba/admin/static/runtime_config/styles.tsx
@@ -0,0 +1,20 @@
+import { COLORS } from "../theme";
+
+const containerStyle = {
+  width: 1200,
+  maxWidth: "100%",
+};
+
+const linkStyle = {
+  cursor: "pointer",
+  fontSize: 13,
+  color: COLORS.TEXT_LIGHTER,
+  textDecoration: "underline",
+};
+
+const paragraphStyle = {
+  fontSize: 15,
+  color: COLORS.TEXT_LIGHTER,
+};
+
+export { containerStyle, linkStyle, paragraphStyle };
