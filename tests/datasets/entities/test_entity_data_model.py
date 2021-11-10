4bf0a480246c72346a9f8aa55356d515f37c2095
Lyn <lyn.nagara@gmail.com>
2021-11-10 10:18:34 -0800

    fix test

--- tests/datasets/entities/test_entity_data_model.py
+++ tests/datasets/entities/test_entity_data_model.py
@@ -1,7 +1,5 @@
 from snuba.clickhouse.columns import Column
-from snuba.clickhouse.columns import ColumnSet as PhysicalColumnSet
 from snuba.datasets.entities.entity_data_model import EntityColumnSet
-from snuba.datasets.entity import convert_to_entity_column_set
 from snuba.utils.schemas import (
     FixedString,
     FlattenedColumn,
@@ -27,7 +25,7 @@ def test_entity_data_model() -> None:
     assert event_id_col.type == FixedString(32)
 
     assert entity_data_model.get("tags[asdf]") == FlattenedColumn(
-        None, "tags", String()
+        None, "tags[asdf]", String()
     )
     assert entity_data_model.get("asdf") is None
     assert entity_data_model.get("tags[asd   f]") is None
@@ -40,17 +38,4 @@ def test_entity_data_model() -> None:
             WildcardColumn("tags", String()),
             WildcardColumn("contexts", String()),
         ]
-    )
-
-
-def test_convert_column_set() -> None:
-    column_set = PhysicalColumnSet(
-        [
-            Column(name="org_id", type=UInt(64)),
-            Column(name="project_id", type=UInt(64)),
-        ]
-    )
-
-    assert convert_to_entity_column_set(column_set) == EntityColumnSet(
-        [Column("org_id", UInt(64)), Column("project_id", UInt(64))]
     )
