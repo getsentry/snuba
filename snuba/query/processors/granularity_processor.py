8da1aae9dfe780d66f44e7bdb0ab83f220a5feda
Ahmed Etefy <ahmed.etefy12@gmail.com>
2021-11-16 14:45:42 +0100

    feat(metric-alerts): Set granularity for subscriptions
    Adds logic that sets the granularity of subscription
    queries based on the time interval passed in query

--- snuba/query/processors/granularity_processor.py
+++ snuba/query/processors/granularity_processor.py
@@ -1,10 +1,15 @@
+from datetime import timedelta
+from typing import Optional
+
+from snuba.clickhouse.query_dsl.accessors import get_time_range
+from snuba.datasets.entities.factory import get_entity
 from snuba.datasets.metrics import DEFAULT_GRANULARITY
 from snuba.query.conditions import ConditionFunctions, binary_condition
 from snuba.query.exceptions import InvalidGranularityException
 from snuba.query.expressions import Column, Literal
 from snuba.query.logical import Query
 from snuba.query.processors import QueryProcessor
-from snuba.request.request_settings import RequestSettings
+from snuba.request.request_settings import RequestSettings, SubscriptionRequestSettings
 
 #: Granularities for which a materialized view exist, in ascending order
 GRANULARITIES_AVAILABLE = (10, 60, 60 * 60, 24 * 60 * 60)
@@ -30,8 +35,32 @@ class GranularityProcessor(QueryProcessor):
             f"Granularity must be multiple of one of {GRANULARITIES_AVAILABLE}"
         )
 
+    @staticmethod
+    def __get_granularity_for_subscription(query: Query) -> Optional[int]:
+        granularity = None
+        entity = get_entity(query.get_from_clause().key)
+        if not entity.required_time_column:
+            raise InvalidGranularityException(
+                "Entity mush have a time column defined to be able to calculate granularity"
+            )
+        from_date, to_date = get_time_range(query, entity.required_time_column)
+        if from_date and to_date:
+            time_interval = to_date - from_date
+            if time_interval <= timedelta(minutes=60):
+                granularity = 10
+            elif time_interval <= timedelta(hours=4):
+                granularity = 60
+            else:
+                granularity = 60 * 60
+        return granularity
+
     def process_query(self, query: Query, request_settings: RequestSettings) -> None:
-        granularity = self.__get_granularity(query)
+        granularity = None
+        if isinstance(request_settings, SubscriptionRequestSettings):
+            granularity = self.__get_granularity_for_subscription(query)
+        if granularity is None:
+            granularity = self.__get_granularity(query)
+
         query.add_condition_to_ast(
             binary_condition(
                 ConditionFunctions.EQ,
--- tests/query/processors/test_granularity_processor.py
+++ tests/query/processors/test_granularity_processor.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 from typing import Optional
+from unittest.mock import patch
 
 import pytest
 
@@ -16,7 +18,10 @@ from snuba.query.exceptions import InvalidGranularityException
 from snuba.query.expressions import Column, Literal
 from snuba.query.logical import Query
 from snuba.query.processors.granularity_processor import GranularityProcessor
-from snuba.request.request_settings import HTTPRequestSettings
+from snuba.request.request_settings import (
+    HTTPRequestSettings,
+    SubscriptionRequestSettings,
+)
 
 
 @pytest.mark.parametrize(
@@ -82,4 +87,96 @@ def test_granularity_added(
                 ),
             ),
             granularity=(requested_granularity),
+        )
+
+
+@pytest.mark.parametrize(
+    "start,end,query_granularity",
+    [
+        (datetime(2020, 9, 1, 12, 00, 00), datetime(2020, 9, 1, 12, 00, 00), 10),
+        (datetime(2020, 9, 1, 12, 00, 00), datetime(2020, 9, 1, 15, 00, 00), 60),
+        (datetime(2020, 9, 1, 12, 00, 00), datetime(2020, 9, 2, 12, 00, 00), 3600),
+        (datetime(2020, 9, 1, 12, 00, 00), datetime(2020, 9, 3, 12, 00, 00), 3600),
+    ],
+)
+@patch("snuba.settings.ENABLE_DEV_FEATURES", True)
+def test_subscription_query_granularity(
+    start: datetime, end: datetime, query_granularity: int,
+) -> None:
+    # Reload factory after enable settings.ENABLE_DEV_FEATURES
+    from importlib import reload
+
+    from snuba.datasets.storages import factory
+
+    reload(factory)
+
+    query = Query(
+        QueryEntity(EntityKey.METRICS_COUNTERS, ColumnSet([])),
+        selected_columns=[SelectedExpression("value", Column(None, None, "value"))],
+        condition=binary_condition(
+            BooleanFunctions.AND,
+            binary_condition(
+                ConditionFunctions.EQ,
+                Column(None, None, "metric_id"),
+                Literal(None, 123),
+            ),
+            binary_condition(
+                BooleanFunctions.AND,
+                binary_condition(
+                    ConditionFunctions.GTE,
+                    Column(None, None, "timestamp"),
+                    Literal(None, start),
+                ),
+                binary_condition(
+                    ConditionFunctions.LT,
+                    Column(None, None, "timestamp"),
+                    Literal(None, end),
+                ),
+            ),
+        ),
+    )
+    try:
+        GranularityProcessor().process_query(
+            query, SubscriptionRequestSettings(referrer="subscriptions_test")
+        )
+    except InvalidGranularityException:
+        assert query_granularity is None
+    else:
+        assert str(query) == str(
+            Query(
+                QueryEntity(EntityKey.METRICS_COUNTERS, ColumnSet([])),
+                selected_columns=[
+                    SelectedExpression("value", Column(None, None, "value"))
+                ],
+                condition=binary_condition(
+                    BooleanFunctions.AND,
+                    binary_condition(
+                        ConditionFunctions.EQ,
+                        Column(None, None, "granularity"),
+                        Literal(None, query_granularity),
+                    ),
+                    binary_condition(
+                        BooleanFunctions.AND,
+                        binary_condition(
+                            ConditionFunctions.EQ,
+                            Column(None, None, "metric_id"),
+                            Literal(None, 123),
+                        ),
+                        binary_condition(
+                            BooleanFunctions.AND,
+                            binary_condition(
+                                ConditionFunctions.GTE,
+                                Column(None, None, "timestamp"),
+                                Literal(None, start),
+                            ),
+                            binary_condition(
+                                ConditionFunctions.LT,
+                                Column(None, None, "timestamp"),
+                                Literal(None, end),
+                            ),
+                        ),
+                    ),
+                ),
+                granularity=query_granularity,
+            )
         )
