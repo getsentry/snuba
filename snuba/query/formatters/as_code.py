1f148b187158974682e9526da56ab726db17d1fa
Vlad Kluev <vlad.kluev@sentry.io>
2021-11-23 13:16:05 -0800

    wip

--- snuba/query/formatters/as_code.py
+++ snuba/query/formatters/as_code.py
@@ -5,7 +5,7 @@ from snuba.query.composite import CompositeQuery
 from snuba.query.data_source.join import IndividualNode, JoinClause, JoinVisitor
 from snuba.query.data_source.simple import SimpleDataSource
 from snuba.query.data_source.visitor import DataSourceVisitor
-from snuba.query.expressions import  AsCodeVisitor
+from snuba.query.expressions import AsCodeVisitor
 
 TExpression = Union[str, Mapping[str, Any], Sequence[Any]]
 
@@ -17,7 +17,7 @@ def _indent_str_list(str_list: List[str], levels: int) -> List[str]:
 
 def format_query(
     query: Union[ProcessableQuery[SimpleDataSource], CompositeQuery[SimpleDataSource]]
-) -> List[str]:
+) -> str:
     """
     Formats a query as a list of strings with each element being a new line
 
@@ -27,20 +27,13 @@ def format_query(
 
     eformatter = AsCodeVisitor(level=0, initial_indent=1)
 
-    selects = ",\n".join(
-        [
-            f"{e.expression.accept(eformatter)} |> {e.name}"
-            for e in query.get_selected_columns()
-        ]
-    )
-    select_str = f"SELECT\n{selects}" if selects else ""
 
-    from_strs = [
-        "FROM",
-        *_indent_str_list(
-            TracingQueryFormatter().visit(query.get_from_clause()), levels=1
-        ),
-    ]
+    selects = ",\n".join(
+        f"SelectedExpression(repr({e.name}), {e.expression.accept(eformatter)}"
+        for e in query.get_selected_columns()
+    )
+    select_str = f"({selects})"
+    from_str = query.get_from_clause().accept(eformatter)
 
     groupbys = ",\n".join([e.accept(eformatter) for e in query.get_groupby()])
     groupby_str = f"GROUPBY\n{groupbys}" if groupbys else ""
@@ -75,66 +68,60 @@ def format_query(
     offset = query.get_offset()
     if offset:
         str_list.append(f"  OFFSET {offset}")
-    # The StringifyVisitor formats expression with newlines but we
-    # need them as a list of strings (to avoid truncation)
-    # therefore as a last step we join our list of strings into one string,
-    # and then split it again by newlines to go from this:
 
-    # ['SELECT\n  c1,  ev.c AS `_snuba_c1`\n  f1,  f(\n    ev.c2\n  ) AS `_snuba_f1`',
-    #  'FROM',
-    #  "  ENTITY(events) AS `ev`",
-    # ]
-
-    # To this:
-
-    # ['SELECT',
-    #  '  c1,  ev.c AS `_snuba_c1`',
-    #  '  f1,  f(',
-    #  '    ev.c2',
-    #  '  ) AS `_snuba_f1`',
-    #  'FROM',
-    #  "  ENTITY(events) AS `ev`",
-    return "\n".join(str_list).rstrip().split("\n")
-
+    return f"""{query.__class__.__name__}(
+        selected_columns={selected_columns_str},
+        array_join={array_join_str},
+        condition={condition_str},
+        groupby={groupby_str},
+        having={having_str},
+        order_by={order_by_str},
+        limitby={limitby_str},
+        limit={limit_str},
+        offset={offset_str},
+        totals={totals_str},
+        granularity={granularity_str},
+        experiments={experiments_str},
+    """
 
 class TracingQueryFormatter(
-    DataSourceVisitor[List[str], SimpleDataSource],
-    JoinVisitor[List[str], SimpleDataSource],
+    DataSourceVisitor[str, SimpleDataSource],
+    JoinVisitor[str, SimpleDataSource],
 ):
     def _indent_str_list(self, str_list: List[str], levels: int) -> List[str]:
         indent = "  " * levels
         return [f"{indent}{s}" for s in str_list]
 
-    def _visit_simple_source(self, data_source: SimpleDataSource) -> List[str]:
-        # Entity and Table define their sampling rates with slightly different
-        # terms and renaming it would introduce a lot of code changes down the line
-        # so we use this dynamic workaround
-        sample_val = getattr(
-            data_source, "sample", getattr(data_source, "sampling_rate", None)
-        )
-        sample_str = f" SAMPLE {sample_val}" if sample_val is not None else ""
-        return [f"{data_source.human_readable_id}{sample_str}"]
+    def _visit_simple_source(self, data_source: SimpleDataSource) -> str:
+        return repr(data_source)
 
-    def _visit_join(self, data_source: JoinClause[SimpleDataSource]) -> List[str]:
-        return self.visit_join_clause(data_source)
+    def _visit_join(self, data_source: JoinClause[SimpleDataSource]) -> str:
+        return repr(data_source)
 
     def _visit_simple_query(
         self, data_source: ProcessableQuery[SimpleDataSource]
-    ) -> List[str]:
+    ) -> str:
         return format_query(data_source)
 
     def _visit_composite_query(
         self, data_source: CompositeQuery[SimpleDataSource]
-    ) -> List[str]:
+    ) -> str:
         return format_query(data_source)
 
     def visit_individual_node(
         self, node: IndividualNode[SimpleDataSource]
-    ) -> List[str]:
-        return [f"{self.visit(node.data_source)} AS `{node.alias}`"]
+    ) -> str:
+        return f"""IndividualNode({repr(node.alias)}, {self.visit(node.data_source)})"""
 
-    def visit_join_clause(self, node: JoinClause[SimpleDataSource]) -> List[str]:
+    def visit_join_clause(self, node: JoinClause[SimpleDataSource]) -> str]:
         # There is only one of these in the on clause (I think)
+        return f"""JoinClause(
+            left_node={node.left_node.accept(self)},
+            right_node={node.right_node.accept(self)},
+            keys=# TODO
+        """
+
+
         on_list = [
             [
                 f"{c.left.table_alias}.{c.left.column}",
