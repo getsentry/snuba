585e4ff637d3a6c2c7c6bd4588f04226d4dad754
Vlad Kluev <vlad.kluev@sentry.io>
2021-11-30 16:17:59 -0800

    remove BS

--- test_query.py
+++ test_query.py
@@ -1,172 +1,126 @@
 FunctionCall(
     None,
     "and",
-    tuple(
-        [
-            FunctionCall(
-                None,
-                "greaterOrEquals",
-                tuple(
-                    [
-                        Column("_snuba_timestamp", None, "timestamp"),
-                        Literal(None, datetime.datetime(2021, 7, 12, 19, 45, 1)),
-                    ]
-                ),
+    (
+        FunctionCall(
+            None,
+            "greaterOrEquals",
+            (
+                Column("_snuba_timestamp", None, "timestamp"),
+                Literal(None, datetime.datetime(2021, 7, 12, 19, 45, 1)),
             ),
-            FunctionCall(
-                None,
-                "and",
-                tuple(
-                    [
+        ),
+        FunctionCall(
+            None,
+            "and",
+            (
+                FunctionCall(
+                    None,
+                    "less",
+                    (
+                        Column("_snuba_timestamp", None, "timestamp"),
+                        Literal(None, datetime.datetime(2021, 8, 11, 19, 45, 1)),
+                    ),
+                ),
+                FunctionCall(
+                    None,
+                    "and",
+                    (
                         FunctionCall(
                             None,
-                            "less",
-                            tuple(
-                                [
-                                    Column("_snuba_timestamp", None, "timestamp"),
-                                    Literal(
-                                        None, datetime.datetime(2021, 8, 11, 19, 45, 1),
-                                    ),
-                                ]
+                            "in",
+                            (
+                                Column("_snuba_project_id", None, "project_id"),
+                                FunctionCall(
+                                    None, "tuple", ({exp.parameters[0].accept(self)},),
+                                ),
                             ),
                         ),
                         FunctionCall(
                             None,
                             "and",
-                            tuple(
-                                [
-                                    FunctionCall(
-                                        None,
-                                        "in",
-                                        tuple(
-                                            [
-                                                Column(
-                                                    "_snuba_project_id",
-                                                    None,
-                                                    "project_id",
-                                                ),
+                            (
+                                FunctionCall(
+                                    None,
+                                    "notEquals",
+                                    (
+                                        FunctionCall(
+                                            None,
+                                            "ifNull",
+                                            (
                                                 FunctionCall(
-                                                    None,
-                                                    "tuple",
-                                                    tuple([Literal(None, 300688)]),
-                                                ),
-                                            ]
-                                        ),
-                                    ),
-                                    FunctionCall(
-                                        None,
-                                        "and",
-                                        tuple(
-                                            [
-                                                FunctionCall(
-                                                    None,
-                                                    "notEquals",
-                                                    tuple(
-                                                        [
-                                                            FunctionCall(
-                                                                None,
-                                                                "ifNull",
-                                                                tuple(
-                                                                    [
-                                                                        FunctionCall(
-                                                                            "_snuba_tags[duration_group]",
-                                                                            "arrayElement",
-                                                                            tuple(
-                                                                                [
-                                                                                    Column(
-                                                                                        None,
-                                                                                        None,
-                                                                                        "tags.value",
-                                                                                    ),
-                                                                                    FunctionCall(
-                                                                                        None,
-                                                                                        "indexOf",
-                                                                                        tuple(
-                                                                                            [
-                                                                                                Column(
-                                                                                                    None,
-                                                                                                    None,
-                                                                                                    "tags.key",
-                                                                                                ),
-                                                                                                Literal(
-                                                                                                    None,
-                                                                                                    "duration_group",
-                                                                                                ),
-                                                                                            ]
-                                                                                        ),
-                                                                                    ),
-                                                                                ]
-                                                                            ),
-                                                                        ),
-                                                                        Literal(
-                                                                            None, ""
-                                                                        ),
-                                                                    ]
+                                                    "_snuba_tags[duration_group]",
+                                                    "arrayElement",
+                                                    (
+                                                        Column(
+                                                            None, None, "tags.value"
+                                                        ),
+                                                        FunctionCall(
+                                                            None,
+                                                            "indexOf",
+                                                            (
+                                                                Column(
+                                                                    None,
+                                                                    None,
+                                                                    "tags.key",
+                                                                ),
+                                                                Literal(
+                                                                    None,
+                                                                    "duration_group",
                                                                 ),
                                                             ),
-                                                            Literal(None, ""),
-                                                        ]
+                                                        ),
                                                     ),
                                                 ),
+                                                Literal(None, ""),
+                                            ),
+                                        ),
+                                        Literal(None, ""),
+                                    ),
+                                ),
+                                FunctionCall(
+                                    None,
+                                    "equals",
+                                    (
+                                        FunctionCall(
+                                            None,
+                                            "ifNull",
+                                            (
                                                 FunctionCall(
-                                                    None,
-                                                    "equals",
-                                                    tuple(
-                                                        [
-                                                            FunctionCall(
-                                                                None,
-                                                                "ifNull",
-                                                                tuple(
-                                                                    [
-                                                                        FunctionCall(
-                                                                            "_snuba_tags[duration_group]",
-                                                                            "arrayElement",
-                                                                            tuple(
-                                                                                [
-                                                                                    Column(
-                                                                                        None,
-                                                                                        None,
-                                                                                        "tags.value",
-                                                                                    ),
-                                                                                    FunctionCall(
-                                                                                        None,
-                                                                                        "indexOf",
-                                                                                        tuple(
-                                                                                            [
-                                                                                                Column(
-                                                                                                    None,
-                                                                                                    None,
-                                                                                                    "tags.key",
-                                                                                                ),
-                                                                                                Literal(
-                                                                                                    None,
-                                                                                                    "duration_group",
-                                                                                                ),
-                                                                                            ]
-                                                                                        ),
-                                                                                    ),
-                                                                                ]
-                                                                            ),
-                                                                        ),
-                                                                        Literal(
-                                                                            None, ""
-                                                                        ),
-                                                                    ]
+                                                    "_snuba_tags[duration_group]",
+                                                    "arrayElement",
+                                                    (
+                                                        Column(
+                                                            None, None, "tags.value"
+                                                        ),
+                                                        FunctionCall(
+                                                            None,
+                                                            "indexOf",
+                                                            (
+                                                                Column(
+                                                                    None,
+                                                                    None,
+                                                                    "tags.key",
+                                                                ),
+                                                                Literal(
+                                                                    None,
+                                                                    "duration_group",
                                                                 ),
                                                             ),
-                                                            Literal(None, "<10s"),
-                                                        ]
+                                                        ),
                                                     ),
                                                 ),
-                                            ]
+                                                Literal(None, ""),
+                                            ),
                                         ),
+                                        Literal(None, "<10s"),
                                     ),
-                                ]
+                                ),
                             ),
                         ),
-                    ]
+                    ),
                 ),
             ),
-        ]
+        ),
     ),
 )
