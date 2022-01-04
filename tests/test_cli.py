0f498059ef67f1108360ca7fb27fc33c7fef0d6d
Evan Hicks <evanh@users.noreply.github.com>
2022-01-04 10:51:48 -0500

    fix(arm64) Fix tests so they work on arm64 as well (#2329)
    This is mostly fixing a bug with our current tests, where in the old version of
    Clickhouse the events are not being merged before the tests run. In the new
    version of Clickhouse the events are merged before the tests, and since the
    fixtures all use the same span_id, they are actually only sending one event.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,7 +8,7 @@ class TestCli(object):
         """
         Check that the consumer daemon runs until it is killed
         """
-        proc = subprocess.Popen(["snuba", "consumer"])
+        proc = subprocess.Popen(["snuba", "consumer", "--storage", "events"])
         time.sleep(1)
         proc.poll()
         assert proc.returncode is None  # still running
