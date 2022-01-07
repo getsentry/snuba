b457918a93814dd89b37abb03d32c5e4f8c99318
Oliver Newland <oliver.newland@sentry.io>
2022-01-07 10:28:01 -0800

    fix bad copy/paste

--- snuba/clusters/storage_sets.py
+++ snuba/clusters/storage_sets.py
@@ -26,7 +26,7 @@ class StorageSetKey(Enum):
     QUERYLOG = "querylog"
     SESSIONS = "sessions"
     TRANSACTIONS = "transactions"
-    TRANSACTIONS_RO = "events_ro"
+    TRANSACTIONS_RO = "transactions_ro"
 
 
 # Storage sets enabled only when development features are enabled.
