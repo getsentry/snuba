c53942d1b1498c4e1a053599b4b034c343ecbeff
Oliver Newland <oliver.newland@sentry.io>
2022-01-07 14:32:14 -0800

    use proper transactions metadata, rather than bad copy/paste from events

--- snuba/datasets/storages/transactions_ro.py
+++ snuba/datasets/storages/transactions_ro.py
@@ -2,19 +2,19 @@ from snuba.clusters.storage_sets import StorageSetKey
 from snuba.datasets.schemas.tables import TableSchema
 from snuba.datasets.storage import ReadableTableStorage
 from snuba.datasets.storages import StorageKey
-from snuba.datasets.storages.events_common import (
-    all_columns,
-    mandatory_conditions,
+from snuba.datasets.storages.transactions import (
+    columns,
+    mandatory_condition_checkers,
     query_processors,
     query_splitters,
 )
 
 schema = TableSchema(
-    columns=all_columns,
-    local_table_name="sentry_local",
-    dist_table_name="sentry_dist_ro",
+    columns=columns,
+    local_table_name="transactions_local",
+    dist_table_name="transactions_dist_ro",
     storage_set_key=StorageSetKey.TRANSACTIONS_RO,
-    mandatory_conditions=mandatory_conditions,
+    mandatory_conditions=[],
 )
 
 storage = ReadableTableStorage(
@@ -23,4 +23,5 @@ storage = ReadableTableStorage(
     schema=schema,
     query_processors=query_processors,
     query_splitters=query_splitters,
+    mandatory_condition_checkers=mandatory_condition_checkers,
 )
