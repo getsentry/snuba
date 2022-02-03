getsentry/snuba

commit e086672d687f10715c35365ec94410cd6f4fb3e6

feat(tracing): Use the readonly user for tracing queries (#2366)
Ensure the tracing tool uses the readonly user.

snuba/admin/clickhouse/common.py
@@ -68,12 +68,13 @@ def get_ro_node_connection(
     return connection
 
 
-CLUSTER_CONNECTIONS: MutableMapping[StorageKey, ClickhousePool] = {}
+CLUSTER_CONNECTIONS: MutableMapping[str, ClickhousePool] = {}
 
 
-def get_ro_cluster_connection(storage_name: str) -> ClickhousePool:
+def get_ro_query_node_connection(storage_name: str) -> ClickhousePool:
+    if storage_name in CLUSTER_CONNECTIONS:
+        return CLUSTER_CONNECTIONS[storage_name]
 
-    storage_key = None
     try:
         storage_key = StorageKey(storage_name)
     except ValueError:
@@ -82,12 +83,12 @@ def get_ro_cluster_connection(storage_name: str) -> ClickhousePool:
             extra_data={"storage_name": storage_name},
         )
 
-    if storage_key in CLUSTER_CONNECTIONS:
-        return CLUSTER_CONNECTIONS[storage_key]
-
     storage = get_storage(storage_key)
     cluster = storage.get_cluster()
-    connection = cluster.get_query_connection(ClickhouseClientSettings.QUERY)
+    connection_id = cluster.get_connection_id()
+    connection = get_ro_node_connection(
+        connection_id.hostname, connection_id.tcp_port, storage_name
+    )
 
-    CLUSTER_CONNECTIONS[storage_key] = connection
+    CLUSTER_CONNECTIONS[storage_name] = connection
     return connection

snuba/admin/clickhouse/tracing.py
@@ -1,4 +1,7 @@
-from snuba.admin.clickhouse.common import InvalidCustomQuery, get_ro_cluster_connection
+from snuba.admin.clickhouse.common import (
+    InvalidCustomQuery,
+    get_ro_query_node_connection,
+)
 from snuba.clickhouse.native import ClickhouseResult
 
 
@@ -22,6 +25,7 @@ def validate_trace_query(sql_query: str) -> None:
 
 def run_query_and_get_trace(storage_name: str, query: str) -> ClickhouseResult:
     validate_trace_query(query)
-    connection = get_ro_cluster_connection(storage_name)
+
+    connection = get_ro_query_node_connection(storage_name)
     query_result = connection.execute(query=query, capture_trace=True)
     return query_result

commit c9c0a8eaf1d4c0f447e386645619d5e2bdaeb777

fix(tracing): Fix querying the query node in the tracing tool (#2372)
The is_valid_node function previously only checked if the node
being queried was a valid local node since we only currently query
local nodes for system qeuries. Now we need to allow the
query node as well since this is what the tracing tool uses.

This fixes a bug introduced in https://github.com/getsentry/snuba/pull/2366
when we switched over to use the readonly user.

snuba/admin/clickhouse/common.py
@@ -21,7 +21,8 @@ class InvalidStorageError(SerializableException):
 
 
 def is_valid_node(host: str, port: int, cluster: ClickhouseCluster) -> bool:
-    nodes = cluster.get_local_nodes()
+    nodes = [*cluster.get_local_nodes(), cluster.get_query_node()]
+
     return any(node.host_name == host and node.port == port for node in nodes)
 
 

snuba/clusters/cluster.py
@@ -265,6 +265,9 @@ class ClickhouseCluster(Cluster[ClickhouseWriterOptions]):
     def get_database(self) -> str:
         return self.__database
 
+    def get_query_node(self) -> ClickhouseNode:
+        return self.__query_node
+
     def get_local_nodes(self) -> Sequence[ClickhouseNode]:
         if self.__single_node:
             return [self.__query_node]