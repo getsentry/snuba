0563fea1655336131d49fa1c8f54dccc65191ffb
Oliver Newland <oliver.newland@sentry.io>
2022-02-08 11:34:32 -0800

    add some doc comments for the MetricsBackend class

--- snuba/utils/metrics/backends/abstract.py
+++ snuba/utils/metrics/backends/abstract.py
@@ -13,16 +13,26 @@ class MetricsBackend(ABC):
     def increment(
         self, name: str, value: Union[int, float] = 1, tags: Optional[Tags] = None
     ) -> None:
+        """
+        Emit a single instance of a non-timing metric that can be counted,
+        sliced/diced into percentiles, or summed
+        """
         raise NotImplementedError
 
     @abstractmethod
     def gauge(
         self, name: str, value: Union[int, float], tags: Optional[Tags] = None
     ) -> None:
+        """
+        Emit a metric that is the authoritative value at a point in time
+        """
         raise NotImplementedError
 
     @abstractmethod
     def timing(
         self, name: str, value: Union[int, float], tags: Optional[Tags] = None
     ) -> None:
+        """
+        Emit a metric for the performance of an operation
+        """
         raise NotImplementedError
