df5706fde80a24c7876f84e1dc9edf0a9884315e
Nikhar Saxena <nikhar.saxena@sentry.io>
2022-02-09 23:11:07 -0800

    Fix the replacements

--- snuba/datasets/storages/errors_v2.py
+++ snuba/datasets/storages/errors_v2.py
@@ -9,15 +9,81 @@ from snuba.datasets.storages import StorageKey
 from snuba.datasets.storages.errors_common import (
     all_columns,
     mandatory_conditions,
+    prewhere_candidates,
+    promoted_context_columns,
     promoted_tag_columns,
-    query_processors,
     query_splitters,
     required_columns,
 )
+from snuba.datasets.storages.events_bool_contexts import EventsBooleanContextsProcessor
+from snuba.datasets.storages.processors.replaced_groups import (
+    PostReplacementConsistencyEnforcer,
+)
+from snuba.datasets.storages.type_condition_optimizer import TypeConditionOptimizer
+from snuba.datasets.storages.user_column_processor import UserColumnProcessor
 from snuba.datasets.table_storage import build_kafka_stream_loader_from_settings
+from snuba.query.processors.arrayjoin_keyvalue_optimizer import (
+    ArrayJoinKeyValueOptimizer,
+)
 from snuba.query.processors.conditions_enforcer import ProjectIdEnforcer
+from snuba.query.processors.empty_tag_condition_processor import (
+    EmptyTagConditionProcessor,
+)
+from snuba.query.processors.mapping_optimizer import MappingOptimizer
+from snuba.query.processors.mapping_promoter import MappingColumnPromoter
+from snuba.query.processors.prewhere import PrewhereProcessor
+from snuba.query.processors.slice_of_map_optimizer import SliceOfMapOptimizer
+from snuba.query.processors.table_rate_limit import TableRateLimit
+from snuba.query.processors.type_converters.hexint_column_processor import (
+    HexIntColumnProcessor,
+)
+from snuba.query.processors.type_converters.uuid_array_column_processor import (
+    UUIDArrayColumnProcessor,
+)
+from snuba.query.processors.type_converters.uuid_column_processor import (
+    UUIDColumnProcessor,
+)
+from snuba.query.processors.uniq_in_select_and_having import (
+    UniqInSelectAndHavingProcessor,
+)
 from snuba.subscriptions.utils import SchedulingWatermarkMode
 from snuba.utils.streams.topics import Topic
+
+# The query processor list is different than the one defined in errors_common.py because the
+# PostReplacementConsistencyEnforcer needs to be initialized with ReplacerState.ERRORS_V2 for
+# the new storage.
+query_processors = [
+    UniqInSelectAndHavingProcessor(),
+    PostReplacementConsistencyEnforcer(
+        project_column="project_id", replacer_state_name=ReplacerState.ERRORS_V2,
+    ),
+    MappingColumnPromoter(
+        mapping_specs={
+            "tags": promoted_tag_columns,
+            "contexts": promoted_context_columns,
+        }
+    ),
+    UserColumnProcessor(),
+    UUIDColumnProcessor({"event_id", "primary_hash", "trace_id"}),
+    HexIntColumnProcessor({"span_id"}),
+    UUIDArrayColumnProcessor({"hierarchical_hashes"}),
+    SliceOfMapOptimizer(),
+    EventsBooleanContextsProcessor(),
+    TypeConditionOptimizer(),
+    MappingOptimizer("tags", "_tags_hash_map", "events_tags_hash_map_enabled"),
+    EmptyTagConditionProcessor(),
+    ArrayJoinKeyValueOptimizer("tags"),
+    PrewhereProcessor(
+        prewhere_candidates,
+        # Environment and release are excluded from prewhere in case of final
+        # queries because of a Clickhouse bug.
+        # group_id instead is excluded since `final` is applied after prewhere.
+        # thus, in this case, we could be filtering out rows that should be
+        # merged together by the final.
+        omit_if_final=["environment", "release", "group_id"],
+    ),
+    TableRateLimit(),
+]
 
 schema = WritableTableSchema(
     columns=all_columns,
