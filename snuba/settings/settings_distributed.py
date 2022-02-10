150b74054e1968ff3abc8ef4368ff13d49a4035f
Pierre Massat <pierre.massat@sentry.io>
2022-02-10 11:16:43 -0800

    Default to the same cluster

--- snuba/settings/settings_distributed.py
+++ snuba/settings/settings_distributed.py
@@ -1,13 +1,20 @@
 import os
 
+CLICKHOUSE_HOST = os.environ.get("CLICKHOUSE_HOST", "localhost")
+CLICKHOUSE_PORT = int(os.environ.get("CLICKHOUSE_PORT", 9000))
+CLICKHOUSE_USER = os.environ.get("CLICKHOUSE_USER", "default")
+CLICKHOUSE_PASSWORD = os.environ.get("CLICKHOUSE_PASSWORD", "")
+CLICKHOUSE_DATABASE = os.environ.get("CLICKHOUSE_DATABASE", "default")
+CLICKHOUSE_HTTP_PORT = int(os.environ.get("CLICKHOUSE_HTTP_PORT", 8123))
+
 CLUSTERS = [
     {
-        "host": os.environ.get("CLICKHOUSE_HOST", "localhost"),
-        "port": int(os.environ.get("CLICKHOUSE_PORT", 9000)),
-        "user": os.environ.get("CLICKHOUSE_USER", "default"),
-        "password": os.environ.get("CLICKHOUSE_PASSWORD", ""),
-        "database": os.environ.get("CLICKHOUSE_DATABASE", "default"),
-        "http_port": int(os.environ.get("CLICKHOUSE_HTTP_PORT", 8123)),
+        "host": CLICKHOUSE_HOST,
+        "port": CLICKHOUSE_PORT,
+        "user": CLICKHOUSE_USER,
+        "password": CLICKHOUSE_PASSWORD,
+        "database": CLICKHOUSE_DATABASE,
+        "http_port": CLICKHOUSE_HTTP_PORT,
         "storage_sets": {
             "cdc",
             "discover",
@@ -27,12 +34,12 @@ CLUSTERS = [
         "distributed_cluster_name": "cluster_one_sh",
     },
     {
-        "host": os.environ.get("CLICKHOUSE_PROFILING_HOST", "localhost"),
-        "port": int(os.environ.get("CLICKHOUSE_PROFILING_PORT", 9001)),
-        "user": os.environ.get("CLICKHOUSE_PROFILING_USER", "default"),
-        "password": os.environ.get("CLICKHOUSE_PROFILING_PASSWORD", ""),
-        "database": os.environ.get("CLICKHOUSE_PROFILING_DATABASE", "default"),
-        "http_port": int(os.environ.get("CLICKHOUSE_PROFILING_HTTP_PORT", 8124)),
+        "host": os.environ.get("CLICKHOUSE_PROFILING_HOST", CLICKHOUSE_HOST),
+        "port": int(os.environ.get("CLICKHOUSE_PROFILING_PORT", CLICKHOUSE_PORT)),
+        "user": os.environ.get("CLICKHOUSE_PROFILING_USER", CLICKHOUSE_USER),
+        "password": os.environ.get("CLICKHOUSE_PROFILING_PASSWORD", CLICKHOUSE_PASSWORD),
+        "database": os.environ.get("CLICKHOUSE_PROFILING_DATABASE", CLICKHOUSE_DATABASE),
+        "http_port": int(os.environ.get("CLICKHOUSE_PROFILING_HTTP_PORT", CLICKHOUSE_HTTP_PORT)),
         "storage_sets": {
             "stacktraces",
         },
