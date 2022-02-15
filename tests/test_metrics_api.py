0fa07b39a4d777d6b5772f76602aa7866eb5b9c9
Oliver Newland <oliver.newland@sentry.io>
2022-02-15 13:58:49 -0800

    very rough e2e set metrics test

--- tests/test_metrics_api.py
+++ tests/test_metrics_api.py
@@ -7,7 +7,10 @@ import simplejson as json
 
 from snuba import state
 from snuba.consumers.types import KafkaMessageMetadata
-from snuba.datasets.metrics_aggregate_processor import METRICS_COUNTERS_TYPE
+from snuba.datasets.metrics_aggregate_processor import (
+    METRICS_COUNTERS_TYPE,
+    METRICS_SET_TYPE,
+)
 from snuba.datasets.storages import StorageKey
 from snuba.datasets.storages.factory import get_writable_storage
 from tests.base import BaseApiTest
@@ -72,40 +75,9 @@ class TestMetricsApiCounters(BaseApiTest):
         state.delete_config("date_align_seconds")
 
     def generate_counters(self) -> None:
-        """
-        Generate some counts
-
-        distribution sample message:
-        {
-            "org_id": 1,
-            "project_id": 2,
-            "name": "sentry.transactions.transaction.duration",
-            "unit": "ms",
-            "type": "d",
-            "value": [
-                21,
-                76,
-                116,
-                142,
-                179
-            ],
-            "timestamp": 1641418510,
-            "tags": {
-                "6": 91,
-                "9": 134,
-                "4": 159,
-                "5": 34
-            },
-            "metric_id": 8,
-            "retention_days": 90
-        }
-        {"org_id":1,"project_id":2,"name":"sentry.transactions.transaction.duration","unit":"ms","type":"d","value":[21.0,76.0,116.0,142.0,179.0],"timestamp":1641418510,"tags":{"6":91,"9":134,"4":159,"5":34},"metric_id":8,"retention_days":90}
-        {"org_id":1,"project_id":2,"name":"sentry.transactions.transaction.duration","unit":"ms","type":"d","value":[24.0,80.0,119.0,146.0,182.0],"timestamp":1641418510,"tags":{"6":91,"9":134,"4":117,"5":7},"metric_id":8,"retention_days":90}
-        """
         events = []
         for n in range(self.seconds):
             for p in self.project_ids:
-                # this counter increases metric
                 processed = (
                     self.storage.get_table_writer()
                     .get_stream_loader()
@@ -140,7 +112,6 @@ class TestMetricsApiCounters(BaseApiTest):
                     AND granularity = 60
                     AND timestamp >= toDateTime('{start_time}')
                     AND timestamp < toDateTime('{end_time}')
-                    LIMIT 1
                     """.format(
             metric_id=self.metric_id,
             org_id=self.org_id,
@@ -148,7 +119,7 @@ class TestMetricsApiCounters(BaseApiTest):
             end_time=(self.base_time + self.skew).isoformat(),
         )
         response = self.app.post(
-            SNQL_ROUTE, data=json.dumps({"query": query_str, "dataset": "transactions"})
+            SNQL_ROUTE, data=json.dumps({"query": query_str, "dataset": "metrics"})
         )
         data = json.loads(response.data)
 
@@ -159,3 +130,105 @@ class TestMetricsApiCounters(BaseApiTest):
         assert aggregation["org_id"] == self.org_id
         assert aggregation["project_id"] == self.project_ids[0]
         assert aggregation["total_seconds"] == self.seconds
+
+
+class TestMetricsApiSets(BaseApiTest):
+    @pytest.fixture
+    def test_app(self) -> Any:
+        return self.app
+
+    @pytest.fixture
+    def test_entity(self) -> Union[str, Tuple[str, str]]:
+        return "metrics_sets"
+
+    @pytest.fixture(autouse=True)
+    def setup_post(self, _build_snql_post_methods: Callable[[str], Any]) -> None:
+        self.post = _build_snql_post_methods
+
+    def setup_method(self, test_method: Any) -> None:
+        super().setup_method(test_method)
+
+        # values for test data
+        self.metric_id = 1002
+        self.org_id = 103
+        self.project_ids = [1, 2]  # 2 projects
+        self.seconds = 180 * 60
+
+        self.default_tags = {
+            TAG_1_KEY: TAG_1_VALUE_1,
+            TAG_2_KEY: TAG_2_VALUE_1,
+            TAG_3_KEY: TAG_3_VALUE_1,
+            TAG_4_KEY: TAG_4_VALUE_1,
+        }
+        self.skew = timedelta(seconds=self.seconds)
+
+        self.base_time = datetime.utcnow().replace(
+            minute=0, second=0, microsecond=0, tzinfo=pytz.utc
+        ) - timedelta(minutes=self.seconds)
+        self.storage = get_writable_storage(StorageKey.METRICS_BUCKETS)
+        self.unique_set_values = 100
+        self.generate_sets()
+
+    def teardown_method(self, test_method: Any) -> None:
+        # Reset rate limits
+        state.delete_config("global_concurrent_limit")
+        state.delete_config("global_per_second_limit")
+        state.delete_config("project_concurrent_limit")
+        state.delete_config("project_concurrent_limit_1")
+        state.delete_config("project_per_second_limit")
+        state.delete_config("date_align_seconds")
+
+    def generate_sets(self) -> None:
+        events = []
+        processor = self.storage.get_table_writer().get_stream_loader().get_processor()
+
+        for n in range(self.seconds):
+            for p in self.project_ids:
+                msg = {
+                    "org_id": self.org_id,
+                    "project_id": p,
+                    "type": METRICS_SET_TYPE,
+                    "value": [n % self.unique_set_values],
+                    "timestamp": self.base_time.timestamp() + n,
+                    "tags": self.default_tags,
+                    "metric_id": self.metric_id,
+                    "retention_days": RETENTION_DAYS,
+                }
+                print(msg)
+
+                processed = processor.process_message(
+                    msg, KafkaMessageMetadata(0, 0, self.base_time),
+                )
+                if processed:
+                    events.append(processed)
+        write_processed_messages(self.storage, events)
+
+    def test_sets_basic(self) -> None:
+        query_str = """MATCH (metrics_sets)
+                    SELECT uniqCombined64Merge(value) AS unique_values BY project_id, org_id
+                    WHERE org_id = {org_id}
+                    AND project_id = 1
+                    AND metric_id = {metric_id}
+                    AND granularity = 60
+                    AND timestamp >= toDateTime('{start_time}')
+                    AND timestamp < toDateTime('{end_time}')
+                    """.format(
+            metric_id=self.metric_id,
+            org_id=self.org_id,
+            start_time=(self.base_time - self.skew).isoformat(),
+            end_time=(self.base_time + self.skew).isoformat(),
+        )
+        response = self.app.post(
+            SNQL_ROUTE, data=json.dumps({"query": query_str, "dataset": "metrics"})
+        )
+        data = json.loads(response.data)
+
+        assert response.status_code == 200
+        assert len(data["data"]) == 1, data
+
+        aggregation = data["data"][0]
+        print(aggregation)
+
+        assert aggregation["org_id"] == self.org_id
+        assert aggregation["project_id"] == self.project_ids[0]
+        assert aggregation["unique_values"] == self.unique_set_values
