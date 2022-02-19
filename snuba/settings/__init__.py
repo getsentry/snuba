ecf269065d4a477ca7e7c51843fddafbc7418b76
Pierre Massat <pierre.massat@sentry.io>
2022-02-18 19:55:21 -0500

    Ignore profiles migration group

--- snuba/settings/__init__.py
+++ snuba/settings/__init__.py
@@ -155,7 +155,7 @@ COLUMN_SPLIT_MAX_LIMIT = 1000
 COLUMN_SPLIT_MAX_RESULTS = 5000
 
 # Migrations in skipped groups will not be run
-SKIPPED_MIGRATION_GROUPS: Set[str] = {"querylog", "spans_experimental"}
+SKIPPED_MIGRATION_GROUPS: Set[str] = {"querylog", "spans_experimental", "profiles"}
 
 MAX_RESOLUTION_FOR_JITTER = 60
 
