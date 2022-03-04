4b3b8257d726cbe29039756b33b09ba043b50511
Oliver Newland <oliver.newland@sentry.io>
2022-03-04 13:58:41 -0800

    add TTL to metrics tables

---
+++ snuba/migrations/snuba_migrations/metrics/0019_aggregate_tables_add_ttl.py
@@ -0,0 +1,45 @@
+from typing import Sequence
+
+from snuba.clusters.storage_sets import StorageSetKey
+from snuba.migrations import migration, operations
+
+
+class Migration(migration.ClickhouseNodeMigration):
+    """
+    Adds TTLs to all the aggergate tables based on retention_days columns
+    """
+
+    blocking = False
+    table_names = [
+        "metrics_distributions_local",
+        "metrics_sets_local",
+        "metrics_counters_local",
+    ]
+
+    def forwards_local(self) -> Sequence[operations.SqlOperation]:
+        return [
+            operations.RunSql(
+                storage_set=StorageSetKey.METRICS,
+                statement=(
+                    f"ALTER TABLE {table_name} "
+                    "MODIFY TTL "
+                    "if(isNull(retention_days), timestamp + toIntervalDay(90), timestamp + toIntervalDay(retention_days))"
+                ),
+            )
+            for table_name in self.table_names
+        ]
+
+    def backwards_local(self) -> Sequence[operations.SqlOperation]:
+        return [
+            operations.RunSql(
+                storage_set=StorageSetKey.METRICS,
+                statement=(f"ALTER TABLE {table_name} REMOVE TTL"),
+            )
+            for table_name in self.table_names
+        ]
+
+    def forwards_dist(self) -> Sequence[operations.SqlOperation]:
+        return []
+
+    def backwards_dist(self) -> Sequence[operations.SqlOperation]:
+        return []
