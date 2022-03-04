e20f289f3ee2e593f8dd2b527c96061b2d5a0bbf
Lyn <lyn.nagara@gmail.com>
2022-03-04 11:41:33 -0800

    feat(subscriptions): Add synchronized consumer for verifier
    The synchronized consumer consist of two SubscriptionResultConsumer
    instances that consume from the old and new result topic simultaneously.
    It's job is to ensure that we make progress on each topic at roughly the
    same pace based on the scheduled subscription timestamp.
    
    It does not attempt to keep track of the progress of individual partitions
    on each result topic, that will be handled by the processing strategy later.

--- snuba/subscriptions/verifier.py
+++ snuba/subscriptions/verifier.py
@@ -2,15 +2,25 @@ import json
 import logging
 from dataclasses import dataclass
 from datetime import datetime
-from typing import Any, Callable, Mapping, Optional, Sequence
+from typing import (
+    Any,
+    Callable,
+    Mapping,
+    MutableMapping,
+    MutableSequence,
+    Optional,
+    Sequence,
+    Tuple,
+)
 
 from arroyo import Message, Partition, Topic
 from arroyo.backends.abstract import Consumer
-from arroyo.backends.kafka import KafkaPayload
+from arroyo.backends.kafka import KafkaConsumer, KafkaPayload
 from arroyo.types import Position
 
 from snuba.reader import Result
 from snuba.utils.codecs import Decoder
+from snuba.utils.streams.configuration_builder import build_kafka_consumer_configuration
 
 logger = logging.getLogger(__name__)
 
@@ -99,3 +109,202 @@ class SubscriptionResultConsumer(Consumer[SubscriptionResultData]):
     @property
     def closed(self) -> bool:
         return self.__consumer.closed
+
+
+class SynchronizedConsumer(Consumer[SubscriptionResultData]):
+    """
+    This is a consumer that attempts to synchronize progress between the old and the
+    new result topics based on the scheduled subscription time so that we can make
+    progress on both at roughly the same pace.
+
+    IMPORTANT: The messages returned by the poll method of this consumer are from BOTH
+    topics interchangeably. Use with caution!
+    """
+
+    def __init__(
+        self,
+        orig_topic: Topic,
+        new_topic: Topic,
+        # The same consumer group name is used on both old and new topics
+        consumer_group: str,
+        auto_offset_reset: str,
+    ) -> None:
+        self.__orig_topic_consumer = self._build_result_consumer(
+            consumer_group, auto_offset_reset
+        )
+        self.__new_topic_consumer = self._build_result_consumer(
+            consumer_group, auto_offset_reset
+        )
+        self.__orig_topic = orig_topic
+        self.__new_topic = new_topic
+
+        self.__orig_consumer_last_message: Optional[
+            Message[SubscriptionResultData]
+        ] = None
+        self.__new_consumer_last_message: Optional[
+            Message[SubscriptionResultData]
+        ] = None
+
+    def _build_result_consumer(
+        self, consumer_group: str, auto_offset_reset: str
+    ) -> Consumer[SubscriptionResultData]:
+        return SubscriptionResultConsumer(
+            KafkaConsumer(
+                build_kafka_consumer_configuration(
+                    None, group_id=consumer_group, auto_offset_reset=auto_offset_reset,
+                ),
+            )
+        )
+
+    def subscribe(
+        self,
+        topics: Sequence[Topic],
+        on_assign: Optional[Callable[[Mapping[Partition, int]], None]] = None,
+        on_revoke: Optional[Callable[[Sequence[Partition]], None]] = None,
+    ) -> None:
+        """
+        Note the topic actually passed here is ignored as each consumer subscribes to a different topic
+        """
+        self.__orig_topic_consumer.subscribe(
+            [self.__orig_topic], on_assign=on_assign, on_revoke=on_revoke
+        )
+        self.__new_topic_consumer.subscribe(
+            [self.__new_topic], on_assign=on_assign, on_revoke=on_revoke
+        )
+
+    def unsubscribe(self) -> None:
+        self.__orig_topic_consumer.unsubscribe()
+        self.__new_topic_consumer.unsubscribe()
+
+    def poll(
+        self, timeout: Optional[float] = None
+    ) -> Optional[Message[SubscriptionResultData]]:
+        if self.__orig_consumer_last_message is None:
+            self.__orig_consumer_last_message = self.__orig_topic_consumer.poll(timeout)
+
+        if self.__new_consumer_last_message is None:
+            self.__new_consumer_last_message = self.__new_topic_consumer.poll(timeout)
+
+        message: Optional[Message[SubscriptionResultData]] = None
+
+        if (
+            self.__orig_consumer_last_message is None
+            and self.__new_consumer_last_message is None
+        ):
+            pass
+        elif self.__new_consumer_last_message is None:
+            message = self.__orig_consumer_last_message
+            self.__orig_consumer_last_message = None
+        elif self.__orig_consumer_last_message is None:
+            message = self.__new_consumer_last_message
+            self.__new_consumer_last_message = None
+        else:
+            # Return the earlier message based on scheduled timestamp
+            # (or the new topic message if both are the same)
+            orig_payload = self.__orig_consumer_last_message.payload
+            new_payload = self.__new_consumer_last_message.payload
+
+            if orig_payload.timestamp < new_payload.timestamp:
+                message = self.__orig_consumer_last_message
+                self.__orig_consumer_last_message = None
+            else:
+                message = self.__new_consumer_last_message
+                self.__new_consumer_last_message = None
+
+        return message
+
+    def __segment_partitions(
+        self, partitions: Sequence[Partition]
+    ) -> Tuple[Sequence[Partition], Sequence[Partition]]:
+        """
+        Partitions the partitions by topic and returns (orig_partitions, new_partitions).
+        Raises an error if any partitions don't belong to one of the two topics.
+        """
+        orig_partitions: MutableSequence[Partition] = []
+        new_partitions: MutableSequence[Partition] = []
+
+        for partition in partitions:
+            if partition.topic == self.__orig_topic:
+                orig_partitions.append(partition)
+            elif partition.topic == self.__new_topic:
+                new_partitions.append(partition)
+            else:
+                raise Exception("invalid topic")
+
+        return (orig_partitions, new_partitions)
+
+    def __segment_offsets(
+        self, offsets: Mapping[Partition, int]
+    ) -> Tuple[Mapping[Partition, int], Mapping[Partition, int]]:
+        orig_offsets: MutableMapping[Partition, int] = {}
+        new_offsets: MutableMapping[Partition, int] = {}
+
+        for partition in offsets.keys():
+            if partition.topic == self.__orig_topic:
+                orig_offsets[partition] = offsets[partition]
+            elif partition.topic == self.__new_topic:
+                new_offsets[partition] = offsets[partition]
+            else:
+                raise Exception("invalid topic")
+
+        return (orig_offsets, new_offsets)
+
+    def __segment_positions(
+        self, positions: Mapping[Partition, Position]
+    ) -> Tuple[Mapping[Partition, Position], Mapping[Partition, Position]]:
+        orig_positions: MutableMapping[Partition, Position] = {}
+        new_positions: MutableMapping[Partition, Position] = {}
+
+        for partition in positions.keys():
+            if partition.topic == self.__orig_topic:
+                orig_positions[partition] = positions[partition]
+            elif partition.topic == self.__new_topic:
+                new_positions[partition] = positions[partition]
+            else:
+                raise Exception("invalid topic")
+
+        return (orig_positions, new_positions)
+
+    def pause(self, partitions: Sequence[Partition]) -> None:
+        orig_partitions, new_partitions = self.__segment_partitions(partitions)
+
+        self.__orig_topic_consumer.pause(orig_partitions)
+        self.__new_topic_consumer.pause(new_partitions)
+
+    def resume(self, partitions: Sequence[Partition]) -> None:
+        orig_partitions, new_partitions = self.__segment_partitions(partitions)
+
+        self.__orig_topic_consumer.resume(orig_partitions)
+        self.__new_topic_consumer.resume(new_partitions)
+
+    def paused(self) -> Sequence[Partition]:
+        return [
+            *self.__orig_topic_consumer.paused(),
+            *self.__new_topic_consumer.paused(),
+        ]
+
+    def tell(self) -> Mapping[Partition, int]:
+        return {**self.__orig_topic_consumer.tell(), **self.__new_topic_consumer.tell()}
+
+    def seek(self, offsets: Mapping[Partition, int]) -> None:
+        orig_offsets, new_offsets = self.__segment_offsets(offsets)
+        self.__orig_topic_consumer.seek(orig_offsets)
+        self.__new_topic_consumer.seek(new_offsets)
+
+    def stage_positions(self, positions: Mapping[Partition, Position]) -> None:
+        orig_positions, new_positions = self.__segment_positions(positions)
+        self.__orig_topic_consumer.stage_positions(orig_positions)
+        self.__new_topic_consumer.stage_positions(new_positions)
+
+    def commit_positions(self) -> Mapping[Partition, Position]:
+        orig_positions = self.__orig_topic_consumer.commit_positions()
+        new_positions = self.__new_topic_consumer.commit_positions()
+        return {**orig_positions, **new_positions}
+
+    def close(self, timeout: Optional[float] = None) -> None:
+        self.__orig_topic_consumer.close(timeout)
+        self.__new_topic_consumer.close(timeout)
+
+    @property
+    def closed(self) -> bool:
+        return self.__orig_topic_consumer.closed and self.__new_topic_consumer.closed
--- tests/subscriptions/test_verifier.py
+++ tests/subscriptions/test_verifier.py
@@ -1,7 +1,9 @@
 import uuid
-from datetime import datetime
+from datetime import datetime, timedelta
+from random import randint
 
 from arroyo import Topic
+from arroyo.backends.abstract import Consumer
 from arroyo.backends.kafka import KafkaPayload
 from arroyo.backends.local.backend import LocalBroker
 from arroyo.backends.local.storages.memory import MemoryMessageStorage
@@ -23,7 +25,9 @@ from snuba.subscriptions.data import (
 from snuba.subscriptions.entity_subscription import EventsSubscription
 from snuba.subscriptions.verifier import (
     SubscriptionResultConsumer,
+    SubscriptionResultData,
     SubscriptionResultDecoder,
+    SynchronizedConsumer,
 )
 from snuba.utils.metrics.timer import Timer
 
@@ -55,7 +59,7 @@ def make_encoded_subscription_result(
             timestamp,
             SubscriptionWithMetadata(
                 EntityKey.EVENTS,
-                Subscription(subscription_identifier, subscription_data,),
+                Subscription(subscription_identifier, subscription_data),
                 5,
             ),
         ),
@@ -116,3 +120,68 @@ def test_subscription_result_consumer() -> None:
         "meta": [{"type": "UInt64", "name": "count"}],
         "data": [{"count": 1}],
     }
+
+
+def test_synchronized_consumer() -> None:
+    orig_topic = Topic("old-result-topic")
+    new_topic = Topic("new-result-topic")
+    consumer_group = "verifier_group"
+    auto_offset_reset = "latest"
+
+    clock = TestingClock()
+    broker: LocalBroker[KafkaPayload] = LocalBroker(MemoryMessageStorage(), clock)
+    broker.create_topic(orig_topic, partitions=1)
+    broker.create_topic(new_topic, partitions=1)
+    producer = broker.get_producer()
+
+    # Don't use actual Kafka
+    class SychronizedConsumerWithFakeKafka(SynchronizedConsumer):
+        def _build_result_consumer(
+            self, consumer_group: str, auto_offset_reset: str
+        ) -> Consumer[SubscriptionResultData]:
+            # Use a randomly generated consumer group as the local consumer doesn't support using same group id
+            group_id = f"{consumer_group}-{randint(0, 100000)}"
+            fake_kafka_consumer = broker.get_consumer(group_id)
+            return SubscriptionResultConsumer(fake_kafka_consumer)
+
+    consumer = SychronizedConsumerWithFakeKafka(
+        orig_topic, new_topic, consumer_group, auto_offset_reset
+    )
+
+    consumer.subscribe([])
+    assert consumer.poll() is None
+
+    now = datetime.now()
+
+    # Produce to original topic
+    for i in range(0, 3):
+        subscription_identifier = SubscriptionIdentifier(PartitionId(0), uuid.uuid4())
+        encoded_message = make_encoded_subscription_result(
+            subscription_identifier, now + timedelta(seconds=2 * i)
+        )
+        producer.produce(orig_topic, encoded_message)
+
+    # Produce to new topic
+    for i in range(0, 3):
+        subscription_identifier = SubscriptionIdentifier(PartitionId(0), uuid.uuid4())
+        encoded_message = make_encoded_subscription_result(
+            subscription_identifier, now + timedelta(seconds=5 * i)
+        )
+        producer.produce(new_topic, encoded_message)
+
+    expected_order = [
+        new_topic,
+        orig_topic,
+        orig_topic,
+        orig_topic,
+        new_topic,
+        new_topic,
+    ]
+
+    for i in range(0, 6):
+        message = consumer.poll()
+        assert message is not None
+        assert message.partition.topic == expected_order[i]
+
+    # No more messages
+    assert consumer.poll() is None
