9ca6cc2086bec1a179e00d6d109d09a6bc24bda3
Nikhar Saxena <84807402+nikhars@users.noreply.github.com>
2022-03-09 13:51:05 -0800

    fix(consumers): Remove deepcopy from multi storage consumers (#2491)
    We recently added the deepcopy in the process_message_multistorage api since the transaction processor was modifying the message in place. Doing a deepcopy would ensure that functionality would work as expected. But it has a side effect of affecting the consumers performance since copying is a heavy operation.
    
    Fix the transaction processor so that the operation where it needs to modify the context does the deepcopy. That way other consumers are not affected.

---
+++ tests/datasets/test_processors_idempotency.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+from typing import Tuple
+
+import pytest
+
+from snuba.consumers.types import KafkaMessageMetadata
+from snuba.datasets.errors_processor import ErrorsProcessor
+from snuba.datasets.events_processor_base import InsertEvent
+from snuba.datasets.storages.errors_common import promoted_tag_columns
+from snuba.datasets.transactions_processor import TransactionsMessageProcessor
+from snuba.processor import MessageProcessor
+from tests.fixtures import get_raw_error_message, get_raw_transaction_message
+
+test_data = [
+    pytest.param(
+        get_raw_error_message(),
+        ErrorsProcessor(promoted_tag_columns),
+        id="errors processor",
+    ),
+    pytest.param(
+        get_raw_transaction_message(),
+        TransactionsMessageProcessor(),
+        id="transaction processor",
+    ),
+]
+
+
+@pytest.mark.parametrize("message,processor", test_data)
+def test_processors_of_multistorage_consumer_are_idempotent(
+    message: Tuple[int, str, InsertEvent], processor: MessageProcessor
+) -> None:
+    """
+    Test that when the same message is provided to the processors, the result would be the same. That is the process
+    message operation is idempotent.
+    """
+    metadata = KafkaMessageMetadata(1000, 1, datetime.now())
+
+    result1 = processor.process_message(message, metadata)
+    result2 = processor.process_message(message, metadata)
+
+    assert result1 == result2
