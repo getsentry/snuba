4e243b0c5721111d21fde3357b679cb6645c4b32
Filippo Pacifici <fpacifici@sentry.io>
2022-03-15 17:39:36 -0700

    Fix test

--- tests/web/test_cache_partitions.py
+++ tests/web/test_cache_partitions.py
@@ -1,14 +1,21 @@
+from snuba.clickhouse.native import ClickhousePool, NativeDriverReader
 from snuba.web.db_query import _get_cache_partition
 
 
 def test_cache_partition() -> None:
-    default_cache = _get_cache_partition(None)
-    another_default_cache = _get_cache_partition(None)
+    pool = ClickhousePool("localhost", 9000, "", "", "")
+    reader1 = NativeDriverReader(None, pool)
+    reader2 = NativeDriverReader(None, pool)
+
+    default_cache = _get_cache_partition(reader1)
+    another_default_cache = _get_cache_partition(reader2)
 
     assert id(default_cache) == id(another_default_cache)
 
-    nondefault_cache = _get_cache_partition("non_default")
-    another_nondefault_cache = _get_cache_partition("non_default")
+    reader3 = NativeDriverReader("non_default", pool)
+    reader4 = NativeDriverReader("non_default", pool)
+    nondefault_cache = _get_cache_partition(reader3)
+    another_nondefault_cache = _get_cache_partition(reader4)
 
     assert id(nondefault_cache) == id(another_nondefault_cache)
     assert id(default_cache) != id(nondefault_cache)
