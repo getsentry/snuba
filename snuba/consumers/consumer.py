8e15a56fb042eb7f6d1e60aa50cdb8c4cd28ce39
Filippo Pacifici <fpacifici@sentry.io>
2022-03-15 20:53:11 -0700

    Try different approach to latency

--- snuba/consumers/consumer.py
+++ snuba/consumers/consumer.py
@@ -106,16 +106,33 @@ class InsertBatchWriter(ProcessingStep[BytesInsertBatch]):
         )
         write_finish = time.time()
 
+        max_latency: Optional[float] = None
+        max_end_to_end_latency: Optional[float] = None
         for message in self.__messages:
-            self.__metrics.timing(
-                "latency_ms", (write_finish - message.timestamp.timestamp()) * 1000
-            )
+            latency = write_finish - message.timestamp.timestamp()
+            if max_latency is None or latency > max_latency:
+                max_latency = latency
+            self.__metrics.timing("latency_ms", latency * 1000)
             if message.payload.origin_timestamp is not None:
-                self.__metrics.timing(
-                    "end_to_end_latency_ms",
-                    (write_finish - message.payload.origin_timestamp.timestamp())
-                    * 1000,
+                end_to_end_latency = (
+                    write_finish - message.payload.origin_timestamp.timestamp()
                 )
+                if (
+                    max_end_to_end_latency is None
+                    or end_to_end_latency > max_end_to_end_latency
+                ):
+                    max_end_to_end_latency = end_to_end_latency
+                self.__metrics.timing(
+                    "end_to_end_latency_ms", end_to_end_latency * 1000,
+                )
+
+        if max_latency is not None:
+            self.__metrics.timing("max_latency_ms", max_latency * 1000)
+        if max_end_to_end_latency is not None:
+            self.__metrics.timing(
+                "max_end_to_end_latency_ms", max_end_to_end_latency * 1000
+            )
+
         self.__metrics.timing("batch_write_ms", write_finish - write_start)
         rows = sum(len(message.payload.rows) for message in self.__messages)
         self.__metrics.increment("batch_write_msgs", rows)
