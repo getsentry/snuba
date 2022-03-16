c2b46b82181735e98f59efa84c060591dde1388c
Filippo Pacifici <fpacifici@sentry.io>
2022-03-15 18:17:20 -0700

    Fix settings delegator

--- tests/pipeline/test_settings_delegator.py
+++ tests/pipeline/test_settings_delegator.py
@@ -1,17 +1,46 @@
+from typing import Sequence, Type, Union
+
+import pytest
+
 from snuba.pipeline.settings_delegator import RateLimiterDelegate
-from snuba.request.request_settings import HTTPRequestSettings
+from snuba.request.request_settings import (
+    HTTPRequestSettings,
+    SubscriptionRequestSettings,
+)
 from snuba.state.rate_limit import RateLimitParameters
 
+test_cases = [
+    pytest.param(
+        HTTPRequestSettings,
+        [
+            RateLimitParameters(
+                rate_limit_name="rate_name",
+                bucket="secondary_project",
+                per_second_limit=10.0,
+                concurrent_limit=22,
+            ),
+            RateLimitParameters(
+                rate_limit_name="second_rate_name",
+                bucket="secondary_table",
+                per_second_limit=11.0,
+                concurrent_limit=23,
+            ),
+        ],
+        id="HTTP Request Settings",
+    ),
+    pytest.param(SubscriptionRequestSettings, [], id="Subscriptions request settings"),
+]
 
-def test_delegate() -> None:
-    settings = HTTPRequestSettings(
+
+@pytest.mark.parametrize("settings_class, expected_rate_limiters", test_cases)
+def test_delegate(
+    settings_class: Type[Union[HTTPRequestSettings, SubscriptionRequestSettings]],
+    expected_rate_limiters: Sequence[RateLimitParameters],
+) -> None:
+    settings = settings_class(
         referrer="test",
-        turbo=False,
         consistent=False,
-        debug=True,
         parent_api="parent",
-        dry_run=False,
-        legacy=False,
         team="team",
         feature="feature",
     )
@@ -36,17 +65,4 @@ def test_delegate() -> None:
     )
 
     assert settings_delegate.referrer == settings.referrer
-    assert settings_delegate.get_rate_limit_params() == [
-        RateLimitParameters(
-            rate_limit_name="rate_name",
-            bucket="secondary_project",
-            per_second_limit=10.0,
-            concurrent_limit=22,
-        ),
-        RateLimitParameters(
-            rate_limit_name="second_rate_name",
-            bucket="secondary_table",
-            per_second_limit=11.0,
-            concurrent_limit=23,
-        ),
-    ]
+    assert settings_delegate.get_rate_limit_params() == expected_rate_limiters
