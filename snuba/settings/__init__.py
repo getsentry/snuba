c962ed3c5c893347df356197415ce64d9f7f34d7
Lyn <lyn.nagara@gmail.com>
2022-04-17 16:36:13 -0700

    remove sampling

--- snuba/settings/__init__.py
+++ snuba/settings/__init__.py
@@ -64,8 +64,6 @@ DOGSTATSD_SAMPLING_RATES = {
     "subscriptions.receive_latency": 0.1,
     "subscriptions.process_message": 0.1,
     "subscriptions.executor.latency": 0.1,
-    "subscriptions.executor.executor.latency": 0.1,
-    "subscriptions.scheduler.scheduling_latency": 0.1,
     "metrics.processor.set.size": 0.1,
     "metrics.processor.distribution.size": 0.1,
 }
