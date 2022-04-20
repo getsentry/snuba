7b85f3a29b0e8a0e94a639d03a2ac0f59f66eeca
Vlad Kluev <vlad.kluev@sentry.io>
2022-04-20 15:31:47 -0700

    add more test cases

--- tests/query/processors/test_tuple_unaliaser.py
+++ tests/query/processors/test_tuple_unaliaser.py
@@ -2,8 +2,15 @@ from __future__ import annotations
 
 import pytest
 
+from snuba.query.dsl import identity as dsl_identity
 from snuba.query.dsl import literals_tuple, tupleElement
-from snuba.query.expressions import Expression, FunctionCall, Literal
+from snuba.query.expressions import (
+    CurriedFunctionCall,
+    Expression,
+    FunctionCall,
+    Lambda,
+    Literal,
+)
 from snuba.query.processors.tuple_unaliaser import TupleUnaliaser
 from snuba.request.request_settings import HTTPRequestSettings
 from tests.query.processors.query_builders import build_query
@@ -17,27 +24,91 @@ def some_tuple(alias: str | None):
     return literals_tuple(alias, [Literal(None, "duration"), Literal(None, 300)])
 
 
-bad_alias = tupleElement(None, some_tuple(alias="doo"), Literal(None, 1))
+def identity(expression: Expression) -> Expression:
+    return dsl_identity(expression, None)
+
 
 TEST_QUERIES = [
     pytest.param(
         build_query(
             selected_columns=[
                 some_tuple(alias="foo"),
-                equals(bad_alias, Literal(None, 300)),
+                equals(
+                    tupleElement(None, some_tuple(alias="doo"), Literal(None, 1)),
+                    Literal(None, 300),
+                ),
             ]
         ),
         build_query(
             selected_columns=[
+                # top level tuple alias persists
                 some_tuple(alias="foo"),
                 equals(
+                    # alias of the tuple of internal function is removed (it is not useful)
                     tupleElement(None, some_tuple(alias=None), Literal(None, 1)),
                     Literal(None, 300),
                 ),
             ]
         ),
         id="simple happy path",
-    )
+    ),
+    pytest.param(
+        build_query(selected_columns=[Lambda(None, ("a",), some_tuple(alias="foo"))]),
+        build_query(selected_columns=[Lambda(None, ("a",), some_tuple(alias=None))]),
+        id="simple lambda",
+    ),
+    pytest.param(
+        build_query(
+            selected_columns=[
+                identity(
+                    identity(
+                        identity(
+                            equals(
+                                # alias of the tuple of internal function is removed (it is not useful)
+                                tupleElement(
+                                    None, some_tuple(alias="ayyy"), Literal(None, 1)
+                                ),
+                                Literal(None, 300),
+                            )
+                        )
+                    )
+                )
+            ]
+        ),
+        build_query(
+            selected_columns=[
+                identity(
+                    identity(
+                        identity(
+                            equals(
+                                # alias of the tuple of internal function is removed (it is not useful)
+                                tupleElement(
+                                    None, some_tuple(alias=None), Literal(None, 1)
+                                ),
+                                Literal(None, 300),
+                            )
+                        )
+                    )
+                )
+            ]
+        ),
+        id="Highly nested",
+    ),
+    pytest.param(
+        build_query(
+            selected_columns=[
+                CurriedFunctionCall(
+                    None, some_tuple(alias="foo"), (Literal(None, "4"),)
+                )
+            ]
+        ),
+        build_query(
+            selected_columns=[
+                CurriedFunctionCall(None, some_tuple(alias=None), (Literal(None, "4"),))
+            ]
+        ),
+        id="curried function",
+    ),
 ]
 
 
@@ -46,4 +117,3 @@ def test_tuple_unaliaser(input_query, expected_query):
     settings = HTTPRequestSettings()
     TupleUnaliaser().process_query(input_query, settings)
     assert input_query == expected_query
-    assert repr(input_query) == repr(expected_query)
