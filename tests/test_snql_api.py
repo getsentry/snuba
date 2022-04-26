a2a5608bf9634da848c2e3e6a7ee19c811b383ff
Lyn <lyn.nagara@gmail.com>
2022-04-26 16:57:15 -0700

    no more contained relationships

--- tests/test_snql_api.py
+++ tests/test_snql_api.py
@@ -198,42 +198,6 @@ class TestSnQLApi(BaseApiTest):
         )
         assert response.status_code == 429
 
-    def test_project_rate_limiting_joins(self) -> None:
-        state.set_config("project_concurrent_limit", self.project_id)
-        state.set_config(f"project_concurrent_limit_{self.project_id}", 0)
-
-        response = self.post(
-            "/transactions/snql",
-            data=json.dumps(
-                {
-                    "query": """MATCH (s: spans) -[contained]-> (t: transactions)
-                    SELECT s.op, avg(s.duration_ms) AS avg BY s.op
-                    WHERE s.project_id = 2
-                    AND t.project_id = 2
-                    AND t.finish_ts >= toDateTime('2021-01-01')
-                    AND t.finish_ts < toDateTime('2021-01-02')
-                    """,
-                }
-            ),
-        )
-        assert response.status_code == 200
-
-        response = self.post(
-            "/transactions/snql",
-            data=json.dumps(
-                {
-                    "query": f"""MATCH (s: spans) -[contained]-> (t: transactions)
-                    SELECT s.op, avg(s.duration_ms) AS avg BY s.op
-                    WHERE s.project_id = {self.project_id}
-                    AND t.project_id = {self.project_id}
-                    AND t.finish_ts >= toDateTime('2021-01-01')
-                    AND t.finish_ts < toDateTime('2021-01-02')
-                    """,
-                }
-            ),
-        )
-        assert response.status_code == 429
-
     def test_project_rate_limiting_subqueries(self) -> None:
         state.set_config("project_concurrent_limit", self.project_id)
         state.set_config(f"project_concurrent_limit_{self.project_id}", 0)
