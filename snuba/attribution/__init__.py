510b29e67eb746a4e51fe067b1cf083d5ee7d8d7
Evan Hicks <evan.hicks@sentry.io>
2022-05-13 16:26:04 -0400

    cleanup

--- snuba/attribution/__init__.py
+++ snuba/attribution/__init__.py
@@ -10,15 +10,12 @@ from .appid import AppID
 metrics = MetricsWrapper(environment.metrics, "snuba.attribution")
 
 
-DEFAULT_APPID = AppID("default", "evanh", datetime(2022, 3, 24))
-INVALID_APPID = AppID("invalid", "evanh", datetime(2022, 3, 25))
+DEFAULT_APPID = AppID("default", "sns", datetime(2022, 3, 24))
+INVALID_APPID = AppID("invalid", "sns", datetime(2022, 3, 25))
 
 
 def get_app_id(app_id: str) -> AppID:
-    if not app_id:
-        return INVALID_APPID
-
-    return AppID(app_id)
+    return AppID(app_id) if app_id else INVALID_APPID
 
 
 __all__ = (
