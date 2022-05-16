d7493723d8e693d5a38807f4ab576ffdc995923d
Rahul Saini <rahulsaini805@gmail.com>
2022-05-16 16:34:15 -0400

    Added Memory Limit Excceeded to list of acceptable clickhouse errors

--- snuba/web/views.py
+++ snuba/web/views.py
@@ -431,9 +431,21 @@ def snql_dataset_query_view(*, dataset: Dataset, timer: Timer) -> Union[Response
 # https://github.com/ClickHouse/ClickHouse/blob/1c0b731ea6b86ce3bf7f88bd3ec27df7b218454d/src/Common/ErrorCodes.cpp
 ILLEGAL_TYPE_OF_ARGUMENT = 43
 TYPE_MISMATCH = 53
+
+# queries that can never return the amount of data requested by the user are not an internal error
+MEMORY_LIMIT_EXCEEDED = 241
+
+# Since the query validator doesn't have a typing system, queries containing type errors are run on
+# Clickhouse and generate ClickhouseErrors. Return a 400 status code for such requests because the problem
+# lies with the query, not Snuba.
 CLICKHOUSE_TYPING_ERROR_CODES = {
     ILLEGAL_TYPE_OF_ARGUMENT,
     TYPE_MISMATCH,
+}
+
+ACCEPTABLE_CLICKHOUSE_ERROR_CODES = {
+    *CLICKHOUSE_TYPING_ERROR_CODES,
+    MEMORY_LIMIT_EXCEEDED,
 }
 
 
@@ -481,12 +493,8 @@ def dataset_query(
                 exc_info=True,
             )
         elif isinstance(cause, ClickhouseError):
-            # Since the query validator doesn't have a typing system, queries containing type errors are run on
-            # Clickhouse and generate ClickhouseErrors. Return a 400 status code for such requests because the problem
-            # lies with the query, not Snuba.
-            if cause.code in CLICKHOUSE_TYPING_ERROR_CODES:
+            if cause.code in ACCEPTABLE_CLICKHOUSE_ERROR_CODES:
                 status = 400
-
             details = {
                 "type": "clickhouse",
                 "message": str(cause),
