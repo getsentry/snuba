59772f209aac2739091bad5d71f69bbb6b880396
Filippo Pacifici <filippo.pacifici@gmail.com>
2022-05-17 17:02:52 -0700

    Revert "ref: factor out gosu and use USER instead (#2696)"
    This reverts commit 0b9ccf9634eb6a628a124de1dd59f95e8b1efdb1.

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -12,6 +12,7 @@ help_return=$?
 
 if [[ "${help_return}" -eq 0 ]]; then
   set -- snuba "$@"
+  set gosu snuba "$@"
 else
   # Print the error message if it returns non-zero, to help with troubleshooting.
   printf "Error running snuba ${1} --help, passing command to exec directly."
