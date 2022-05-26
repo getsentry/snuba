fdc0e200db8caa173c8c88a8eeff70d627e6809a
Vlad Kluev <vlad.kluev@sentry.io>
2022-05-26 11:05:00 -0700

    add test for non-experimental datasets failing healthcheck

--- tests/web/test_check_clickhouse.py
+++ tests/web/test_check_clickhouse.py
@@ -29,10 +29,23 @@ class ExperimentalDataset(Dataset):
         return [BadEntity()]
 
 
+class BadDataset(Dataset):
+    @classmethod
+    def is_experimental(cls) -> bool:
+        return False
+
+    def get_default_entity(self) -> BadEntity:
+        return BadEntity()
+
+    def get_all_entities(self) -> Sequence[BadEntity]:
+        return [BadEntity()]
+
+
 def fake_get_dataset(name: str) -> Dataset:
     return {
         "events": EventsDataset(),
         "experimental": ExperimentalDataset(default_entity=EntityKey.PROFILES),
+        "bad": BadDataset(default_entity=EntityKey.TRANSACTIONS),
     }[name]
 
 
@@ -44,3 +57,16 @@ def fake_get_dataset(name: str) -> Dataset:
 def test_check_clickhouse(mock1: mock.MagicMock, mock2: mock.MagicMock) -> None:
     assert check_clickhouse(ignore_experimental=True)
     assert not check_clickhouse(ignore_experimental=False)
+
+
+@mock.patch(
+    "snuba.web.views.get_enabled_dataset_names",
+    return_value=["events", "bad"],
+)
+@mock.patch("snuba.web.views.get_dataset", side_effect=fake_get_dataset)
+def test_bad_dataset_fails_healthcheck(
+    mock1: mock.MagicMock, mock2: mock.MagicMock
+) -> None:
+    # the bad dataset is enabled and not experimental, therefore the healthcheck
+    # should fail
+    assert not check_clickhouse(ignore_experimental=True)
