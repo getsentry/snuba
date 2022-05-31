f9a409fb1d4088a9b18eb1eda9e376eead6c9e15
Vlad Kluev <vlad.kluev@sentry.io>
2022-05-31 14:02:21 -0700

    add experimental storageset

--- snuba/settings/settings_distributed.py
+++ snuba/settings/settings_distributed.py
@@ -24,6 +24,7 @@ CLUSTERS = [
             "errors_v2",
             "errors_v2_ro",
             "profiles",
+            "experimental",
         },
         "single_node": False,
         "cluster_name": "cluster_one_sh",
