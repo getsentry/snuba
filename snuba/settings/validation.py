ba73a7d42c7c1e5fc4f9ed702a4b3135fcba0860
Tony Xiao <txiao@sentry.io>
2022-07-06 16:16:09 -0400

    fix typo

--- snuba/settings/validation.py
+++ snuba/settings/validation.py
@@ -41,7 +41,7 @@ def validate_settings(locals: Mapping[str, Any]) -> None:
         "metrics-subscription-results",
         "snuba-dead-letter-inserts",
         "processed-profiles",
-        "profiles-call-trees",
+        "profiles-call-tree",
         "snuba-replay-events",
         "snuba-generic-metrics",
         "snuba-dead-letter-generic-metrics",
