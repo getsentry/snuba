a6db3779cca4b042cf887628d7ae510a653d10ce
Pierre Massat <pierre.massat@sentry.io>
2022-07-06 23:07:48 -0700

    Fix typo

--- snuba/migrations/snuba_migrations/profiles/0002_disable_vertical_merge_algorithm.py
+++ snuba/migrations/snuba_migrations/profiles/0002_disable_vertical_merge_algorithm.py
@@ -26,7 +26,7 @@ class Migration(migration.ClickhouseNodeMigration):
             operations.RunSql(
                 storage_set=StorageSetKey.PROFILES,
                 statement=(
-                    "ALTER TABLE profiles_local SETTING enable_vertical_merge_algorithm=1"
+                    "ALTER TABLE profiles_local MODIFY SETTING enable_vertical_merge_algorithm=1"
                 ),
             )
         ]
