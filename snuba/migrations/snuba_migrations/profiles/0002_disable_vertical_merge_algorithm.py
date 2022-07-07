7587e6e0c22193d3618c3bda0d1c32daf5adb4bd
Pierre Massat <pierre.massat@sentry.io>
2022-07-06 22:34:04 -0700

    fix(profiling): Disable vertical merge algorithm

---
+++ snuba/migrations/snuba_migrations/profiles/0002_disable_vertical_merge_algorithm.py
@@ -0,0 +1,31 @@
+from typing import Sequence
+
+from snuba.clusters.storage_sets import StorageSetKey
+from snuba.migrations import migration, operations
+
+
+class Migration(migration.ClickhouseNodeMigration):
+    """
+    Use the horizontal merge algorithm
+    """
+
+    blocking = False
+
+    def forwards_local(self) -> Sequence[operations.SqlOperation]:
+        return [
+            operations.RunSql(
+                storage_set=StorageSetKey.PROFILES,
+                statement=(
+                    "ALTER TABLE profiles_local MODIFY SETTINGS enable_vertical_merge_algorithm=0"
+                ),
+            )
+        ]
+
+    def backwards_local(self) -> Sequence[operations.SqlOperation]:
+        return []
+
+    def forwards_dist(self) -> Sequence[operations.SqlOperation]:
+        return []
+
+    def backwards_dist(self) -> Sequence[operations.SqlOperation]:
+        return []
