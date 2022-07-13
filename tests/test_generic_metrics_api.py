b1a55286a1551084f10c9d0107d3891ecd4877e2
Oliver Newland <oliver.newland@sentry.io>
2022-07-13 16:07:01 -0700

    test that time bucketing actually occurs

--- tests/test_generic_metrics_api.py
+++ tests/test_generic_metrics_api.py
@@ -268,6 +268,9 @@ class TestGenericMetricsApiDistributions(BaseApiTest):
         self.end_time = (
             self.base_time + timedelta(seconds=self.count) + timedelta(seconds=10)
         )
+        self.hour_before_start_time = self.start_time - timedelta(hours=1)
+        self.hour_after_start_time = self.start_time + timedelta(hours=1)
+
         self.generate_dists()
 
     def generate_dists(self) -> None:
@@ -349,3 +352,30 @@ class TestGenericMetricsApiDistributions(BaseApiTest):
         assert aggregation["org_id"] == self.org_id
         assert aggregation["project_id"] == self.project_id
         assert aggregation["quants"] == [2.0, approx(4.0), approx(4.0), approx(4.0)]
+
+    def test_arbitrary_granularity(self) -> None:
+        query_str = f"""MATCH (generic_metrics_distributions)
+                    SELECT quantiles(0.5,0.9,0.95,0.99)(value) AS quants, min(bucketed_time) AS min_time
+                    BY project_id, org_id
+                    WHERE org_id = {self.org_id}
+                    AND project_id = {self.project_id}
+                    AND metric_id = {self.metric_id}
+                    AND timestamp >= toDateTime('{self.hour_before_start_time}')
+                    AND timestamp < toDateTime('{self.hour_after_start_time}')
+                    GRANULARITY 3600
+                    """
+        response = self.app.post(
+            SNQL_ROUTE,
+            data=json.dumps({"query": query_str, "dataset": "generic_metrics"}),
+        )
+        data = json.loads(response.data)
+
+        assert response.status_code == 200
+        assert len(data["data"]) == 1, data
+
+        aggregation = data["data"][0]
+        smallest_time_bucket = datetime.strptime(
+            aggregation["min_time"], "%Y-%m-%dT%H:%M:%S+00:00"
+        )
+        assert smallest_time_bucket.hour == 12
+        assert smallest_time_bucket.minute == 0
