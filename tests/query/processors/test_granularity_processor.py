8f38b4d2be44cb21f42f859763ca50c2c8e6bcc1
Oliver Newland <oliver.newland@sentry.io>
2022-07-14 15:26:48 -0700

    feat(mep): support time bucketing in queries (#2937)
    * add enum-based granularity processor
    * add TimeSeriesProcessor to generic metrics entities

--- tests/query/processors/test_granularity_processor.py
+++ tests/query/processors/test_granularity_processor.py
@@ -15,7 +15,12 @@ from snuba.query.data_source.simple import Entity as QueryEntity
 from snuba.query.exceptions import InvalidGranularityException
 from snuba.query.expressions import Column, Literal
 from snuba.query.logical import Query
-from snuba.query.processors.granularity_processor import GranularityProcessor
+from snuba.query.processors.granularity_processor import (
+    DEFAULT_MAPPED_GRANULARITY_ENUM,
+    PERFORMANCE_GRANULARITIES,
+    GranularityProcessor,
+    MappedGranularityProcessor,
+)
 from snuba.query.query_settings import HTTPQuerySettings
 
 
@@ -62,6 +67,74 @@ def test_granularity_added(
 
     try:
         GranularityProcessor().process_query(query, HTTPQuerySettings())
+    except InvalidGranularityException:
+        assert query_granularity is None
+    else:
+        assert query == Query(
+            QueryEntity(entity_key, ColumnSet([])),
+            selected_columns=[SelectedExpression(column, Column(None, None, column))],
+            condition=binary_condition(
+                BooleanFunctions.AND,
+                binary_condition(
+                    ConditionFunctions.EQ,
+                    Column(None, None, "granularity"),
+                    Literal(None, query_granularity),
+                ),
+                binary_condition(
+                    ConditionFunctions.EQ,
+                    Column(None, None, "metric_id"),
+                    Literal(None, 123),
+                ),
+            ),
+            granularity=(requested_granularity),
+        )
+
+
+@pytest.mark.parametrize(
+    "entity_key,column",
+    [
+        (EntityKey.GENERIC_METRICS_DISTRIBUTIONS, "percentiles"),
+        (EntityKey.GENERIC_METRICS_SETS, "value"),
+    ],
+)
+@pytest.mark.parametrize(
+    "requested_granularity, query_granularity",
+    [
+        (None, 1),
+        (10, None),
+        (60, 1),
+        (90, None),
+        (120, 1),
+        (60 * 60, 2),
+        (90 * 60, 1),
+        (120 * 60, 2),
+        (24 * 60 * 60, 3),
+        (32 * 60 * 60, 2),
+        (48 * 60 * 60, 3),
+        (13, None),
+        (0, None),
+    ],
+)
+def test_granularity_enum_mapping(
+    entity_key: EntityKey,
+    column: str,
+    requested_granularity: Optional[int],
+    query_granularity: int,
+) -> None:
+    query = Query(
+        QueryEntity(entity_key, ColumnSet([])),
+        selected_columns=[SelectedExpression(column, Column(None, None, column))],
+        condition=binary_condition(
+            ConditionFunctions.EQ, Column(None, None, "metric_id"), Literal(None, 123)
+        ),
+        granularity=(requested_granularity),
+    )
+
+    try:
+        MappedGranularityProcessor(
+            accepted_granularities=PERFORMANCE_GRANULARITIES,
+            default_granularity_enum=DEFAULT_MAPPED_GRANULARITY_ENUM,
+        ).process_query(query, HTTPQuerySettings())
     except InvalidGranularityException:
         assert query_granularity is None
     else:
