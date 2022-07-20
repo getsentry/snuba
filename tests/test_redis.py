03712271021f35475d2402b7b784f26dc40eb8a6
Oliver Newland <oliver.newland@sentry.io>
2022-07-20 16:59:06 -0700

    fix typing in test_redis

--- tests/test_redis.py
+++ tests/test_redis.py
@@ -1,12 +1,13 @@
 from redis.exceptions import RedisClusterException  # type: ignore
 from snuba import redis
+from snuba.redis_multi.configuration import ConnectionDescriptor, NodeDescriptor
 
 
 def test_retry_init() -> None:
     fails_left = 2
 
     @redis._retry(2)
-    def my_bad_function() -> int:
+    def my_bad_function(connection_descriptor: ConnectionDescriptor) -> int:
         nonlocal fails_left
         fails_left -= 1
         if fails_left > 0:
@@ -15,4 +16,4 @@ def test_retry_init() -> None:
             )
         return 1
 
-    assert my_bad_function() == 1
+    assert my_bad_function(NodeDescriptor("localhost", 1234, 1, None)) == 1
