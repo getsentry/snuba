664f2ec787be7f0026b7e19080328a937067b5fe
Oliver Newland <oliver.newland@sentry.io>
2022-07-25 12:51:41 -0700

    feat(mep): add support for generic-metrics dataset subscription executors

--- snuba/cli/subscriptions_executor.py
+++ snuba/cli/subscriptions_executor.py
@@ -23,7 +23,7 @@ from snuba.utils.streams.metrics_adapter import StreamMetricsAdapter
     "--dataset",
     "dataset_name",
     required=True,
-    type=click.Choice(["events", "transactions", "metrics"]),
+    type=click.Choice(["events", "transactions", "metrics", "generic_metrics"]),
     help="The dataset to target.",
 )
 @click.option(
@@ -31,7 +31,16 @@ from snuba.utils.streams.metrics_adapter import StreamMetricsAdapter
     "entity_names",
     required=True,
     multiple=True,
-    type=click.Choice(["events", "transactions", "metrics_counters", "metrics_sets"]),
+    type=click.Choice(
+        [
+            "events",
+            "transactions",
+            "metrics_counters",
+            "metrics_sets",
+            "generic_metrics_sets",
+            "generic_metrics_distributions",
+        ]
+    ),
     help="The entity to target.",
 )
 @click.option(
