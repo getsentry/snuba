2d68d986cd7393340872b987977b887e0312dd59
Josh Ferge <josh.ferge@sentry.io>
2022-07-25 12:55:40 -0700

    correct storage

--- snuba/datasets/storages/replays.py
+++ snuba/datasets/storages/replays.py
@@ -18,7 +18,7 @@ DIST_TABLE_NAME = "replays_dist"
 columns = ColumnSet(
     [
         ("replay_id", UUID()),
-        ("segment_id", UInt(16)),
+        ("segment_id", UInt(16, Modifiers(nullable=True))),
         ("timestamp", DateTime()),
         (
             "trace_ids",
