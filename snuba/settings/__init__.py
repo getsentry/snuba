774814f519f81b48b1bf5253825adbbe3645d739
Colton Allen <colton.allen@sentry.io>
2022-08-02 14:14:31 -0500

    Remove replays from the skipped migrations group

--- snuba/settings/__init__.py
+++ snuba/settings/__init__.py
@@ -175,7 +175,7 @@ COLUMN_SPLIT_MAX_LIMIT = 1000
 COLUMN_SPLIT_MAX_RESULTS = 5000
 
 # Migrations in skipped groups will not be run
-SKIPPED_MIGRATION_GROUPS: Set[str] = {"querylog", "profiles", "functions", "replays"}
+SKIPPED_MIGRATION_GROUPS: Set[str] = {"querylog", "profiles", "functions"}
 
 MAX_RESOLUTION_FOR_JITTER = 60
 
