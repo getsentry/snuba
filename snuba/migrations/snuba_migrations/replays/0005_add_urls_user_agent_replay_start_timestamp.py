4409462d95b8346fe2bfc9dcf67f1762ae556ff0
Colton Allen <colton.allen@sentry.io>
2022-08-05 12:41:11 -0500

    Remove drop column ops

--- snuba/migrations/snuba_migrations/replays/0005_add_urls_user_agent_replay_start_timestamp.py
+++ snuba/migrations/snuba_migrations/replays/0005_add_urls_user_agent_replay_start_timestamp.py
@@ -6,7 +6,7 @@ from snuba.migrations import migration, operations
 from snuba.migrations.columns import MigrationModifiers as Modifiers
 
 new_columns: Sequence[Tuple[Column[Modifiers], str]] = [
-    (Column("urls", Array(String())), "title"),
+    (Column("urls", Array(String())), "url"),
     (Column("replay_start_timestamp", DateTime(Modifiers(nullable=True))), "timestamp"),
     (Column("user_agent", String(Modifiers(nullable=True))), "user"),
 ]
@@ -25,19 +25,9 @@ class Migration(migration.ClickhouseNodeMigration):
             )
             for column, after in new_columns
         ]
-
-        drop_column_ops: List[operations.SqlOperation] = [
-            operations.DropColumn(StorageSetKey.REPLAYS, "replays_local", "url")
-        ]
-
-        return new_column_ops + drop_column_ops
+        return new_column_ops
 
     def backwards_local(self) -> Sequence[operations.SqlOperation]:
-        drop_column_ops: List[operations.SqlOperation] = [
-            operations.DropColumn(StorageSetKey.REPLAYS, "replays_local", column.name)
-            for column, _ in reversed(new_columns)
-        ]
-
         new_column_ops: List[operations.SqlOperation] = [
             operations.AddColumn(
                 storage_set=StorageSetKey.REPLAYS,
@@ -47,7 +37,7 @@ class Migration(migration.ClickhouseNodeMigration):
             )
         ]
 
-        return drop_column_ops + new_column_ops
+        return new_column_ops
 
     def forwards_dist(self) -> Sequence[operations.SqlOperation]:
         new_column_ops: List[operations.SqlOperation] = [
@@ -59,19 +49,9 @@ class Migration(migration.ClickhouseNodeMigration):
             )
             for column, after in new_columns
         ]
-
-        drop_column_ops: List[operations.SqlOperation] = [
-            operations.DropColumn(StorageSetKey.REPLAYS, "replays_dist", "url")
-        ]
-
-        return new_column_ops + drop_column_ops
+        return new_column_ops
 
     def backwards_dist(self) -> Sequence[operations.SqlOperation]:
-        drop_column_ops: List[operations.SqlOperation] = [
-            operations.DropColumn(StorageSetKey.REPLAYS, "replays_dist", column.name)
-            for column, _ in reversed(new_columns)
-        ]
-
         new_column_ops: List[operations.SqlOperation] = [
             operations.AddColumn(
                 storage_set=StorageSetKey.REPLAYS,
@@ -81,4 +61,4 @@ class Migration(migration.ClickhouseNodeMigration):
             )
         ]
 
-        return drop_column_ops + new_column_ops
+        return new_column_ops
