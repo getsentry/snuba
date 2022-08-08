313ccf78e06c88e99500cffb1a01c106ce73033a
Vlad Kluev <vlad.kluev@sentry.io>
2022-08-08 15:39:13 -0700

    make entity keys not an enum anymore

--- snuba/datasets/entities/__init__.py
+++ snuba/datasets/entities/__init__.py
@@ -1,30 +1,18 @@
-from enum import Enum
+from dataclasses import dataclass
 
 
-class EntityKey(Enum):
-    """
-    A entity key is a unique identifier for an entity.
-    """
+@dataclass
+class EntityKey:
+    value: str
 
-    DISCOVER = "discover"
-    EVENTS = "events"
-    GROUPS = "groups"
-    GROUPASSIGNEE = "groupassignee"
-    # TODO: This has an S on the end in solidarity with storages, but it's got to go
-    GROUPEDMESSAGES = "groupedmessage"
-    METRICS_SETS = "metrics_sets"
-    METRICS_COUNTERS = "metrics_counters"
-    ORG_METRICS_COUNTERS = "org_metrics_counters"
-    METRICS_DISTRIBUTIONS = "metrics_distributions"
-    OUTCOMES = "outcomes"
-    OUTCOMES_RAW = "outcomes_raw"
-    SESSIONS = "sessions"
-    ORG_SESSIONS = "org_sessions"
-    TRANSACTIONS = "transactions"
-    DISCOVER_TRANSACTIONS = "discover_transactions"
-    DISCOVER_EVENTS = "discover_events"
-    PROFILES = "profiles"
-    FUNCTIONS = "functions"
-    GENERIC_METRICS_DISTRIBUTIONS = "generic_metrics_distributions"
-    GENERIC_METRICS_SETS = "generic_metrics_sets"
-    REPLAYS = "replays"
+    def __hash__(self):
+        return hash(self.value)
+
+
+class _EntityKeys:
+    # TODO: Add comments and explain the history/decision
+    def __getattr__(self, attr: str) -> EntityKey:
+        return EntityKey(attr.lower())
+
+
+EntityKeys = _EntityKeys()
