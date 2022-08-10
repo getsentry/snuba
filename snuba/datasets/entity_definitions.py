31c69d2440d0cdd15c20828eb2ce4546805600a2
Oliver Newland <oliver.newland@sentry.io>
2022-08-10 12:01:40 -0700

    add some column handling (maybe overlap with Rahul's work)

--- snuba/datasets/entity_definitions.py
+++ snuba/datasets/entity_definitions.py
@@ -4,13 +4,45 @@ from typing import Any, Mapping, MutableSequence, Sequence, Type
 
 from yaml import safe_load
 
+from snuba.clickhouse.columns import ColumnSet
 from snuba.datasets.entities.metrics import TagsTypeTransformer  # noqa
 from snuba.query.processors import QueryProcessor, get_query_processor_by_name
+from snuba.utils.schemas import (
+    AggregateFunction,
+    Column,
+    ColumnType,
+    DateTime,
+    Nested,
+    SchemaModifiers,
+    UInt,
+)
 
 
 @dataclass
 class EntityDefinition:
     query_processors: Sequence[QueryProcessor]
+    columns: ColumnSet
+
+
+COLUMN_TYPE_MAPPING: Mapping[str, ColumnType[SchemaModifiers]] = {
+    "UInt(64)": UInt(64),
+    "DateTime": DateTime(),
+}
+
+
+def column_from_spec(yaml_spec: Any) -> Column[SchemaModifiers]:
+    if yaml_spec["type"] == "Nested":
+        subcolumns_spec = yaml_spec["subcolumns"]
+        subcolumns = [
+            (sub["name"], COLUMN_TYPE_MAPPING[sub["type"]]) for sub in subcolumns_spec
+        ]
+        return Column(yaml_spec["name"], Nested(subcolumns))
+    elif yaml_spec["type"] == "AggregateFunction":
+        return Column(
+            yaml_spec["name"],
+            AggregateFunction(yaml_spec["function"], [yaml_spec["datatype"]]),
+        )
+    return Column(yaml_spec["name"], COLUMN_TYPE_MAPPING[yaml_spec["type"]])
 
 
 def load_from_file(path: str) -> EntityDefinition:
@@ -32,7 +64,10 @@ def load_from_file(path: str) -> EntityDefinition:
                 else:
                     processor = processor_cls(**args)  # type: ignore
                 loaded_processors.append(processor)
-        return EntityDefinition(query_processors=loaded_processors)
+        columns = [column_from_spec(c) for c in spec["columns"]]
+        return EntityDefinition(
+            query_processors=loaded_processors, columns=ColumnSet(columns)
+        )
 
 
 print(load_from_file(sys.argv[1]))
