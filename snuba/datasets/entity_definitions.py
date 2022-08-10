fa0fa7bb71b315e07cfbee0da2d037b86b25e6f3
Oliver Newland <oliver.newland@sentry.io>
2022-08-10 10:07:01 -0700

    add mypy workaround

--- snuba/datasets/entity_definitions.py
+++ snuba/datasets/entity_definitions.py
@@ -1,5 +1,6 @@
 import sys
-from typing import Any, Mapping, Sequence, Type
+from dataclasses import dataclass
+from typing import Any, Mapping, MutableSequence, Sequence, Type
 
 from yaml import safe_load
 
@@ -7,8 +8,9 @@ from snuba.datasets.entities.metrics import TagsTypeTransformer  # noqa
 from snuba.query.processors import QueryProcessor, get_query_processor_by_name
 
 
+@dataclass
 class EntityDefinition:
-    pass
+    query_processors: Sequence[QueryProcessor]
 
 
 def load_from_file(path: str) -> EntityDefinition:
@@ -21,16 +23,16 @@ def load_from_file(path: str) -> EntityDefinition:
         assert unstructured_definition["spec"], "must contain spec"
         spec = unstructured_definition["spec"]
         query_processors: Sequence[Mapping[str, Any]] = spec["query_processors"]
+        loaded_processors: MutableSequence[QueryProcessor] = []
         for qp in query_processors:
             for (name, args) in qp.items():
                 processor_cls: Type[QueryProcessor] = get_query_processor_by_name(name)
                 if args is None:
                     processor = processor_cls()
                 else:
-                    print(args)
-                    processor = processor_cls(**args)
-                print(processor)
-        return EntityDefinition()
+                    processor = processor_cls(**args)  # type: ignore
+                loaded_processors.append(processor)
+        return EntityDefinition(query_processors=loaded_processors)
 
 
 print(load_from_file(sys.argv[1]))
