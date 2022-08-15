1f5d4ad198ff0beb8fa39a209e47ac2993278b86
Riya Chakraborty <riyachakraborty@H472HKN6XK-Riya-Chakraborty.local>
2022-08-15 15:58:44 -0500

    test(settings): Ensure Topics validation and Topics enum are in sync
    Add a test that allows to catch an error where a new Topic defintion
    for Kafka is added but is not added to settings validation.

--- tests/settings/test_settings.py
+++ tests/settings/test_settings.py
@@ -18,3 +18,28 @@ def test_invalid_storage() -> None:
         assert False, f"'validate_settings' raised an exception {exc}"
     finally:
         cluster[0]["storage_sets"].remove("non_existing_storage")
+
+
+def test_topics_sync() -> None:
+    from copy import deepcopy
+
+    from snuba.settings.validation import validate_settings
+    from snuba.utils.streams.topics import Topic
+
+    all_settings = {
+        key: value
+        for key, value in settings.__dict__.items()
+        if not key.startswith("__") and not callable(key)
+    }
+    topic_map = all_settings["KAFKA_TOPIC_MAP"]
+    default_map = deepcopy(topic_map)
+    # Remove default settings and build dummy Topic Map
+    topic_map.clear()
+    topic_map = {t.value: {} for t in Topic}
+
+    try:
+        validate_settings(all_settings)
+    except Exception as exc:
+        assert False, f"'validate_settings' raised an exception {exc}"
+    finally:
+        topic_map = default_map
