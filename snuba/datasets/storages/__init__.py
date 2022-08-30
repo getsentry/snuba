6afa42c3c49d239c63ec4d9a24539f0eb7e07882
Rahul Saini <rahulsaini805@gmail.com>
2022-08-30 14:04:10 -0700

    StorageKey no longer enum

--- snuba/datasets/storages/__init__.py
+++ snuba/datasets/storages/__init__.py
@@ -1,44 +1,73 @@
-from enum import Enum
 from functools import lru_cache
+from glob import glob
+from typing import Any
+
+from yaml import safe_load
+
+from snuba import settings
+
+HARDCODED_STORAGE_KEYS = {
+    "DISCOVER": "discover",
+    "EVENTS": "events",
+    "EVENTS_RO": "events_ro",
+    "ERRORS": "errors",
+    "ERRORS_RO": "errors_ro",
+    "GROUPEDMESSAGES": "groupedmessages",
+    "GROUPASSIGNEES": "groupassignees",
+    "METRICS_COUNTERS": "metrics_counters",
+    "ORG_METRICS_COUNTERS": "org_metrics_counters",
+    "METRICS_DISTRIBUTIONS": "metrics_distributions",
+    "METRICS_SETS": "metrics_sets",
+    "METRICS_RAW": "metrics_raw",
+    "OUTCOMES_RAW": "outcomes_raw",
+    "OUTCOMES_HOURLY": "outcomes_hourly",
+    "QUERYLOG": "querylog",
+    "SESSIONS_RAW": "sessions_raw",
+    "SESSIONS_HOURLY": "sessions_hourly",
+    "ORG_SESSIONS": "org_sessions",
+    "SPANS": "spans",
+    "TRANSACTIONS": "transactions",
+    "TRANSACTIONS_RO": "transactions_ro",
+    "TRANSACTIONS_V2": "transactions_v2",
+    "ERRORS_V2": "errors_v2",
+    "PROFILES": "profiles",
+    "FUNCTIONS": "functions",
+    "FUNCTIONS_RAW": "functions_raw",
+    "REPLAYS": "replays",
+    "ERRORS_V2_RO": "errors_v2_ro",
+}
+
+CONFIG_BUILT_STORAGE_KEYS = {
+    key.upper(): key
+    for key in [
+        safe_load(open(config_file))["storage"]["key"]
+        for config_file in glob(settings.STORAGE_CONFIG_FILES_GLOB, recursive=True)
+    ]
+}
+
+ALL_STORAGE_KEYS = {**HARDCODED_STORAGE_KEYS, **CONFIG_BUILT_STORAGE_KEYS}
 
 
-class StorageKey(Enum):
-    """
-    A storage key is a unique identifier for a storage.
-    """
+class _StorageKey(type):
+    def __getattr__(cls, attr: str) -> "StorageKey":
+        if attr not in ALL_STORAGE_KEYS:
+            raise AttributeError(attr)
 
-    DISCOVER = "discover"
-    EVENTS = "events"
-    EVENTS_RO = "events_ro"
-    ERRORS = "errors"
-    ERRORS_RO = "errors_ro"
-    GROUPEDMESSAGES = "groupedmessages"
-    GROUPASSIGNEES = "groupassignees"
-    METRICS_COUNTERS = "metrics_counters"
-    ORG_METRICS_COUNTERS = "org_metrics_counters"
-    METRICS_DISTRIBUTIONS = "metrics_distributions"
-    METRICS_SETS = "metrics_sets"
-    METRICS_RAW = "metrics_raw"
-    OUTCOMES_RAW = "outcomes_raw"
-    OUTCOMES_HOURLY = "outcomes_hourly"
-    QUERYLOG = "querylog"
-    SESSIONS_RAW = "sessions_raw"
-    SESSIONS_HOURLY = "sessions_hourly"
-    ORG_SESSIONS = "org_sessions"
-    SPANS = "spans"
-    TRANSACTIONS = "transactions"
-    TRANSACTIONS_RO = "transactions_ro"
-    TRANSACTIONS_V2 = "transactions_v2"
-    ERRORS_V2 = "errors_v2"
-    PROFILES = "profiles"
-    FUNCTIONS = "functions"
-    FUNCTIONS_RAW = "functions_raw"
-    REPLAYS = "replays"
-    ERRORS_V2_RO = "errors_v2_ro"
-    GENERIC_METRICS_SETS_RAW = "generic_metrics_sets_raw"
-    GENERIC_METRICS_SETS = "generic_metrics_sets"
-    GENERIC_METRICS_DISTRIBUTIONS_RAW = "generic_metrics_distributions_raw"
-    GENERIC_METRICS_DISTRIBUTIONS = "generic_metrics_distributions"
+        return StorageKey(attr.lower())
+
+
+class StorageKey(metaclass=_StorageKey):
+    def __init__(self, value: str) -> None:
+        self.value = value
+
+    def __hash__(self) -> int:
+        return hash(self.value)
+
+    def __eq__(self, other: Any) -> bool:
+        return isinstance(other, StorageKey) and other.value == self.value
+
+    def __repr__(self) -> str:
+        return f"StorageKey.{self.value.upper()}"
 
 
 IDENTICAL_STORAGES = frozenset(
