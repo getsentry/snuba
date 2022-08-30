b8a7a80f272fbdd567f5baa1e9d22587c247d616
Oliver Newland <oliver.newland@sentry.io>
2022-08-30 11:00:41 -0700

    improve documentation of MappedGranularityProcessor

--- snuba/query/processors/granularity_processor.py
+++ snuba/query/processors/granularity_processor.py
@@ -59,7 +59,8 @@ DEFAULT_MAPPED_GRANULARITY_ENUM = 1
 class MappedGranularityProcessor(QueryProcessor):
     """
     Use the granularity set on the query to filter on the granularity column,
-    supporting generic-metrics style enum mapping
+    supporting generic-metrics style enum mapping (e.g. input granularity of 60s
+    is mapped to the enum granularity of 1)
     """
 
     def __init__(
@@ -67,6 +68,15 @@ class MappedGranularityProcessor(QueryProcessor):
         accepted_granularities: Mapping[int, int],
         default_granularity: int,
     ):
+        """
+        Constructs a new MappedGranularityProcessor
+
+        :param accepted_granularities: a Mapping of raw to enumerated-value granularities
+            where the key is the granularity we expect from user input and the value is
+            the granularity we expect to see in the table/query
+        :param default_granularity: the default granularity value (as seen by the table)
+            to use in the query if the user does not supply one
+        """
         accepted_granularities_processed = [
             GranularityMapping(k, v) for (k, v) in accepted_granularities.items()
         ]
