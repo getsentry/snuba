40d7cb655bd1a3ad50b0a583d9eb94c6b054e14c
Oliver Newland <oliver.newland@sentry.io>
2022-08-30 11:22:31 -0700

    feat(mdc): "revert of revert broken entity loader change (#3088)" (#3093)
    - added some requested documentation on top

--- snuba/query/processors/granularity_processor.py
+++ snuba/query/processors/granularity_processor.py
@@ -1,4 +1,4 @@
-from typing import NamedTuple, Sequence
+from typing import Mapping, NamedTuple
 
 from snuba.datasets.metrics import DEFAULT_GRANULARITY
 from snuba.query.conditions import ConditionFunctions, binary_condition
@@ -48,32 +48,47 @@ class GranularityMapping(NamedTuple):
     enum_value: int
 
 
-PERFORMANCE_GRANULARITIES: Sequence[GranularityMapping] = [
-    GranularityMapping(60, 1),
-    GranularityMapping(3600, 2),
-    GranularityMapping(86400, 3),
-]
+PERFORMANCE_GRANULARITIES: Mapping[int, int] = {
+    60: 1,
+    3600: 2,
+    86400: 3,
+}
 DEFAULT_MAPPED_GRANULARITY_ENUM = 1
 
 
 class MappedGranularityProcessor(QueryProcessor):
     """
     Use the granularity set on the query to filter on the granularity column,
-    supporting generic-metrics style enum mapping
+    supporting generic-metrics style enum mapping (e.g. input granularity of 60s
+    is mapped to the enum granularity of 1)
     """
 
     def __init__(
         self,
-        accepted_granularities: Sequence[GranularityMapping],
-        default_granularity_enum: int,
+        accepted_granularities: Mapping[int, int],
+        default_granularity: int,
     ):
+        """
+        Constructs a new MappedGranularityProcessor
+
+        :param accepted_granularities: a Mapping of raw to enumerated-value granularities
+            where the key is the granularity we expect from user input and the value is
+            the granularity we expect to see in the table/query
+        :param default_granularity: the default granularity value (as seen by the table)
+            to use in the query if the user does not supply one
+        """
+        accepted_granularities_processed = [
+            GranularityMapping(k, v) for (k, v) in accepted_granularities.items()
+        ]
         self._accepted_granularities = sorted(
-            accepted_granularities, key=lambda mapping: mapping.raw, reverse=True
+            accepted_granularities_processed,
+            key=lambda mapping: mapping.raw,
+            reverse=True,
         )
         self._available_granularities_values = [
             mapping.raw for mapping in self._accepted_granularities
         ]
-        self._default_granularity_enum = default_granularity_enum
+        self._default_granularity_enum = default_granularity
 
     def __get_granularity(self, query: Query) -> int:
         """Find the best fitting granularity for this query"""
