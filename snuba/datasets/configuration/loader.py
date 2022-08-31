cb43cd7a1830d26336647ffbacb231cf15e06749
Rahul Saini <rahulsaini805@gmail.com>
2022-08-31 15:03:08 -0700

    Remove unnecessary loader function

--- snuba/datasets/configuration/loader.py
+++ snuba/datasets/configuration/loader.py
@@ -1,12 +1,9 @@
 from __future__ import annotations
 
-from glob import glob
 from typing import Any, Mapping
 
 from jsonschema import validate
 from yaml import safe_load
-
-from snuba import settings
 
 
 def load_configuration_data(
@@ -22,16 +19,3 @@ def load_configuration_data(
     assert isinstance(config, dict)
     validate(config, validation_schemas[config["kind"]])
     return config
-
-
-def load_config_built_storage_keys() -> dict[str, str]:
-    """
-    Loads all storage keys from configuration files
-    """
-    return {
-        key.upper(): key
-        for key in [
-            safe_load(open(config_file))["storage"]["key"]
-            for config_file in glob(settings.STORAGE_CONFIG_FILES_GLOB, recursive=True)
-        ]
-    }
