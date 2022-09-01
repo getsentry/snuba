063d68d0abe71f495c7ac17200a9e0ad98ea5278
Rahul Saini <rahulsaini805@gmail.com>
2022-09-01 14:37:16 -0700

    PluggableDataset

---
+++ snuba/datasets/configuration/dataset_builder.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+from snuba.datasets.configuration.json_schema import V1_READABLE_STORAGE_SCHEMA
+from snuba.datasets.configuration.loader import load_configuration_data
+from snuba.datasets.dataset import Dataset
+from snuba.datasets.entities import EntityKey
+from snuba.datasets.pluggable_dataset import PluggableDataset
+
+# TODO: Use a real dataset validation schema
+DATASET_VALIDATION_SCHEMAS = {"dataset": V1_READABLE_STORAGE_SCHEMA}
+
+
+def build_dataset(config_file_path: str) -> Dataset:
+    config = load_configuration_data(config_file_path, DATASET_VALIDATION_SCHEMAS)
+    return PluggableDataset(
+        name=config["name"],
+        is_experimental=bool(config["is_experimental"]),
+        default_entity=EntityKey(config["entities"]["default"]),
+        all_entities=[EntityKey(key) for key in config["entities"]["all"]],
+    )
