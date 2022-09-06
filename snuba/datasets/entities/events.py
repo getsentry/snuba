995396a148b612706835f4f198b82f311f6aa519
Rahul Saini <rahulsaini805@gmail.com>
2022-09-06 13:39:11 -0700

    GROUPEDMESSAGES to GROUPEDMESSAGE

--- snuba/datasets/entities/events.py
+++ snuba/datasets/entities/events.py
@@ -201,7 +201,7 @@ class BaseEventsEntity(Entity, ABC):
             abstract_column_set=columns,
             join_relationships={
                 "grouped": JoinRelationship(
-                    rhs_entity=EntityKey.GROUPEDMESSAGES,
+                    rhs_entity=EntityKey.GROUPEDMESSAGE,
                     columns=[("project_id", "project_id"), ("group_id", "id")],
                     join_type=JoinType.INNER,
                     equivalences=[],
