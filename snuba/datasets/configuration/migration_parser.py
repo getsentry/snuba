2ea6f80c25582da84a50e9a5afa9f354c71aca1f
Evan Hicks <evan.hicks@sentry.io>
2022-09-13 16:38:26 -0400

    fix(MDC): Fix to ensure schema is in the correct place
    Due to a bad merge the schema was in the wrong place and was skipped in
    validation. Fortunately the new CI check caught this before it went to
    prod.

--- snuba/datasets/configuration/migration_parser.py
+++ snuba/datasets/configuration/migration_parser.py
@@ -5,12 +5,12 @@ from typing import Any
 from jsonschema import validate
 from yaml import safe_load
 
-from snuba.datasets.configuration.migration_json_schema import MIGRATION_GROUP_SCHEMA
+from snuba.datasets.configuration.json_schema import V1_MIGRATION_GROUP_SCHEMA
 
 
 def load_migration_group(path_to_file: str) -> dict[str, Any]:
     yaml_file = open(path_to_file)
     config = safe_load(yaml_file)
     assert isinstance(config, dict)
-    validate(config, MIGRATION_GROUP_SCHEMA)
+    validate(config, V1_MIGRATION_GROUP_SCHEMA)
     return config
