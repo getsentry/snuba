96b169e489d4fd893a32bee8dcda35f6beadf698
Rahul Saini <rahulsaini805@gmail.com>
2022-09-19 17:21:48 -0700

    advanced col validation

--- snuba/datasets/configuration/validation/semantics.py
+++ snuba/datasets/configuration/validation/semantics.py
@@ -26,29 +26,67 @@ def validate_storage_semantics(config: dict[str, Any]) -> None:
 
 def validate_storage_schema_semantics(schema: dict[str, Any]) -> None:
     validate_column_semantics(schema["columns"])
-    pass
 
 
 def validate_column_semantics(columns: list[dict[str, Any]]) -> None:
 
+    SIMPLE_COLUMN_TYPES = {"UInt", "Float", "String", "DateTime", "UUID"}
+
     for col in columns:
         __validate_schema_modifiers(col)
-        if col["type"] == "UInt":
-            if "args" not in col or list(col["args"].keys()) != ["size"]:
-                __raise(col["args"], "set of args for UInt")
-            if col["args"]["size"] not in (sizes := [8, 16, 32, 64, 128, 256]):
-                __raise(col["args"]["size"], f"UInt size {sizes}")
-        elif col["type"] == "Float":
-            if "args" not in col or list(col["args"].keys()) != ["size"]:
-                __raise(col["args"], "set of args for Float")
-            if col["args"]["size"] not in (sizes := [32, 64]):
-                __raise(col["args"]["size"], f"Float size {sizes}")
+        if col["type"] in SIMPLE_COLUMN_TYPES:
+            __validate_number_column(col)
         elif col["type"] == "Nested":
-            pass
+            validate_column_semantics(col["args"]["subcolumns"])
         elif col["type"] == "Array":
-            pass
+            if "args" not in col:
+                raise InvalidSemanticsError("Array missing `args`")
+            col_args = set(col["args"].keys())
+            if col_args != {"type", "arg"} and col_args != {
+                "type",
+                "arg",
+                "schema_modifiers",
+            }:
+                __raise(col["args"], "set of args for Array")
+            type_col = {
+                "name": "x",
+                "type": col["args"]["type"],
+                "args": {"size": col["args"]["arg"]},
+            }
+            if "schema_modifiers" in col_args:
+                type_col["schema_modifiers"] = col["args"]["schema_modifiers"]
+            __validate_number_column(type_col)
         elif col["type"] == "AggregateFunction":
-            pass
+            if "args" not in col:
+                raise InvalidSemanticsError("AggregateFunction missing `args`")
+            if set(col["args"].keys()) != {"func", "arg_types"}:
+                __raise(col["args"], "set of args for AggregateFunction")
+            for arg in col["args"]["arg_types"]:
+                __validate_number_args(arg["type"], arg["arg"])
+
+
+def __validate_number_args(type: str, arg: int) -> None:
+    if type == "UInt":
+        if arg not in (sizes := [8, 16, 32, 64, 128, 256]):
+            __raise(arg, f"UInt size {sizes}")
+    elif type == "Float":
+        if arg not in (sizes := [32, 64]):
+            __raise(arg, f"Float size {sizes}")
+
+
+def __validate_number_column(col: dict[str, Any]) -> None:
+    if col["type"] == "UInt":
+        if "args" not in col:
+            raise InvalidSemanticsError("UInt missing `args`")
+        if list(col["args"].keys()) != ["size"]:
+            __raise(col["args"], "set of args for UInt, only `size` is expected")
+        __validate_number_args(col["type"], col["args"]["size"])
+    elif col["type"] == "Float":
+        if "args" not in col:
+            raise InvalidSemanticsError("Float missing `args`")
+        if list(col["args"].keys()) != ["size"]:
+            __raise(col["args"], "set of args for Float, only `size` is expected")
+        __validate_number_args(col["type"], col["args"]["size"])
 
 
 def __validate_schema_modifiers(col: dict[str, Any]) -> None:
