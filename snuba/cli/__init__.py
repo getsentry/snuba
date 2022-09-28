9b3879abe280103013cd6358f0d1e27195a6503b
Rahul Saini <rahulsaini805@gmail.com>
2022-09-28 12:47:17 -0400

    snuba init timing

--- snuba/cli/__init__.py
+++ snuba/cli/__init__.py
@@ -1,6 +1,14 @@
+import logging
+import time
 from pkgutil import walk_packages
 
 import click
+
+logging.basicConfig(level=getattr(logging, "INFO"), format="%(asctime)s %(message)s")
+logger = logging.getLogger("snuba_startup")
+
+start = time.perf_counter()
+logger.info("Initializing Snuba...")
 
 
 @click.group()
@@ -18,7 +26,10 @@ def main() -> None:
 
 
 for loader, module_name, is_pkg in walk_packages(__path__, __name__ + "."):
+    logger.info(f"Loading module {module_name}")
     module = __import__(module_name, globals(), locals(), ["__name__"])
     cmd = getattr(module, module_name.rsplit(".", 1)[-1])
     if isinstance(cmd, click.Command):
         main.add_command(cmd)
+
+logger.info(f"Snuba initialization took {time.perf_counter() - start}s")
