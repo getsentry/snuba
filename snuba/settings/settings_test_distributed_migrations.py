4397cc838d9228b87195186f5d57530d93847aa1
meredith <meredith@sentry.io>
2022-10-03 12:10:54 -0700

    remove query zookeeper

--- snuba/settings/settings_test_distributed_migrations.py
+++ snuba/settings/settings_test_distributed_migrations.py
@@ -2,20 +2,20 @@ import os
 from typing import Any, Mapping, Sequence
 
 CLUSTERS: Sequence[Mapping[str, Any]] = [
-    # {
-    #     "host": "clickhouse-query",
-    #     "port": int(os.environ.get("CLICKHOUSE_PORT", 9000)),
-    #     "user": os.environ.get("CLICKHOUSE_USER", "default"),
-    #     "password": os.environ.get("CLICKHOUSE_PASSWORD", ""),
-    #     "database": os.environ.get("CLICKHOUSE_DATABASE", "snuba_test"),
-    #     "http_port": int(os.environ.get("CLICKHOUSE_HTTP_PORT", 8229)),
-    #     "storage_sets": {},
-    #     "single_node": False,
-    #     "cluster_name": "query_cluster",
-    #     "distributed_cluster_name": "query_cluster",
-    # },
     {
-        "host": "clickhouse-01",
+        "host": "clickhouse-query",
+        "port": int(os.environ.get("CLICKHOUSE_PORT", 9000)),
+        "user": os.environ.get("CLICKHOUSE_USER", "default"),
+        "password": os.environ.get("CLICKHOUSE_PASSWORD", ""),
+        "database": os.environ.get("CLICKHOUSE_DATABASE", "snuba_test"),
+        "http_port": int(os.environ.get("CLICKHOUSE_HTTP_PORT", 8229)),
+        "storage_sets": {},
+        "single_node": False,
+        "cluster_name": "query_cluster",
+        "distributed_cluster_name": "query_cluster",
+    },
+    {
+        "host": "clickhouse-query",
         "port": int(os.environ.get("CLICKHOUSE_PORT", 9000)),
         "user": os.environ.get("CLICKHOUSE_USER", "default"),
         "password": os.environ.get("CLICKHOUSE_PASSWORD", ""),
@@ -24,12 +24,12 @@ CLUSTERS: Sequence[Mapping[str, Any]] = [
         "storage_sets": {
             "migrations",
         },
-        "single_node": False,
+        "single_node": True,
         "cluster_name": "migrations_cluster",
         "distributed_cluster_name": "migrations_cluster",
     },
     {
-        "host": "clickhouse-01",
+        "host": "clickhouse-query",
         "port": int(os.environ.get("CLICKHOUSE_PORT", 9000)),
         "user": os.environ.get("CLICKHOUSE_USER", "default"),
         "password": os.environ.get("CLICKHOUSE_PASSWORD", ""),
