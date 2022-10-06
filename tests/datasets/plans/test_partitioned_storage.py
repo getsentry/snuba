7b7a858e12fe99d9674add7b18d3bcb8ce430ca8
Oliver Newland <oliver.newland@sentry.io>
2022-10-06 08:17:37 -0700

    more precise partitioned storage selector test

--- tests/datasets/plans/test_partitioned_storage.py
+++ tests/datasets/plans/test_partitioned_storage.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 from snuba.clusters.storage_sets import StorageSetKey
 from snuba.datasets.entities.entity_key import EntityKey
 from snuba.datasets.entities.factory import get_entity
@@ -13,8 +15,49 @@ DISTS_ENTITY_KEY = EntityKey("generic_metrics_distributions")
 DISTS_STORAGE_KEY = StorageKey("generic_metrics_distributions")
 DISTS_STORAGE_SET_KEY = StorageSetKey("generic_metrics_distributions")
 
+"""
+This mock logical partition : slice mapping should follow the pattern of
+0: 0
+1: 1
+2: 0
+3: 1
+...
+"""
+MOCK_LOGICAL_PART_MAPPING = {
+    "generic_metrics_distributions": {x: x % 2 for x in range(0, 256)}
+}
 
-def test_column_based_partition_selector() -> None:
+SLICE_0_DATABASE_VALUE = "slice_0_db"
+SLICE_1_DATABASE_VALUE = "slice_1_db"
+
+SLICED_CLUSTERS_CONFIG = [
+    {
+        "host": "host_slice",
+        "port": 9000,
+        "user": "default",
+        "password": "",
+        "database": SLICE_0_DATABASE_VALUE,
+        "http_port": 8123,
+        "storage_set_slices": {("generic_metrics_distributions", 0)},
+        "single_node": True,
+    },
+    {
+        "host": "host_slice",
+        "port": 9001,
+        "user": "default",
+        "password": "",
+        "database": SLICE_1_DATABASE_VALUE,
+        "http_port": 8124,
+        "storage_set_slices": {("generic_metrics_distributions", 1)},
+        "single_node": True,
+    },
+]
+
+
+@patch("snuba.settings.SLICED_STORAGES", {"generic_metrics_distributions": 2})
+@patch("snuba.settings.LOGICAL_PARTITION_MAPPING", MOCK_LOGICAL_PART_MAPPING)
+@patch("snuba.settings.SLICED_CLUSTERS", SLICED_CLUSTERS_CONFIG)
+def test_column_based_partition_selector_slice_1() -> None:
     """
     Tests that the column based partition selector selects the right cluster
     for a query.
@@ -39,4 +82,41 @@ def test_column_based_partition_selector() -> None:
         "org_id",
     )
     cluster = selector.select_cluster(query, settings)
-    assert cluster is not None
+
+    # org_id 1 should be assigned to slice 1 because (1 % 256 == logical partition 1) and
+    # all odd logical partitions are assigned to slice 1
+    assert cluster.get_database() == SLICE_1_DATABASE_VALUE
+
+
+@patch("snuba.settings.SLICED_STORAGES", {"generic_metrics_distributions": 2})
+@patch("snuba.settings.LOGICAL_PARTITION_MAPPING", MOCK_LOGICAL_PART_MAPPING)
+@patch("snuba.settings.SLICED_CLUSTERS", SLICED_CLUSTERS_CONFIG)
+def test_column_based_partition_selector_slice_0() -> None:
+    """
+    Tests that the column based partition selector selects the right cluster
+    for a query.
+    """
+    query = LogicalQuery(
+        QueryEntity(
+            DISTS_ENTITY_KEY,
+            get_entity(DISTS_ENTITY_KEY).get_data_model(),
+        ),
+        selected_columns=[],
+        condition=binary_condition(
+            "equals",
+            Column("_snuba_org_id", None, "org_id"),
+            Literal(None, 500),
+        ),
+    )
+
+    settings = HTTPQuerySettings()
+    selector = ColumnBasedStoragePartitionSelector(
+        DISTS_STORAGE_KEY,
+        DISTS_STORAGE_SET_KEY,
+        "org_id",
+    )
+    cluster = selector.select_cluster(query, settings)
+
+    # org_id 500 should be assigned to slice 0 because (500 % 256 == logical partition 244) and
+    # all even logical partitions are assigned to slice 0
+    assert cluster.get_database() == SLICE_0_DATABASE_VALUE
