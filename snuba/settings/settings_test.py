7b7c58b961f52d4096c6782e497ffb6948074666
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2022-10-11 17:14:49 +0200

    fix tests in gcb

--- snuba/settings/settings_test.py
+++ snuba/settings/settings_test.py
@@ -35,11 +35,11 @@ OPTIMIZE_PARALLEL_MAX_JITTER_MINUTES = 0
 
 REDIS_CLUSTERS = {
     key: {
-        "use_redis_cluster": False,
+        "use_redis_cluster": os.environ.get("USE_REDIS_CLUSTER", "0") != "0",
         "cluster_startup_nodes": None,
-        "host": "localhost",
-        "port": 6379,
-        "password": None,
+        "host": os.environ.get("REDIS_HOST", "localhost"),
+        "port": int(os.environ.get("REDIS_PORT", 6379)),
+        "password": os.environ.get("REDIS_PASSWORD"),
         "db": i,
         "reinitialize_steps": 10,
     }
