1a724183ecaea0183fbddfb52813967e4ad48b9d
Dalitso Banda <dalitso.banda@sentry.io>
2022-10-11 17:41:34 -0700

    SNS-1744 SNS-1745 run and reverse migrations

--- tests/admin/clickhouse_migrations/test_api.py
+++ tests/admin/clickhouse_migrations/test_api.py
@@ -8,6 +8,7 @@ import simplejson as json
 from flask.testing import FlaskClient
 
 from snuba.migrations.groups import get_group_loader
+from snuba.migrations.runner import MigrationKey, Runner
 
 
 @pytest.fixture
@@ -91,3 +92,56 @@ def test_list_migration_status(admin_api: FlaskClient) -> None:
     sorted_expected_json = sorted(expected_json, key=sort_by_migration_id)
 
     assert sorted_response == sorted_expected_json
+
+
+@pytest.mark.parametrize("action", ["run", "reverse"])
+def test_run_reverse_migrations(admin_api: FlaskClient, action: str) -> None:
+
+    method = "run_migration" if action == "run" else "reverse_migration"
+
+    with patch(
+        "snuba.settings.ADMIN_ALLOWED_MIGRATION_GROUPS", {"system", "invalid_group"}
+    ):
+        # invalid action
+        response = admin_api.post("/migrations/system/invalid_action/0001_migrations/")
+        assert response.status_code == 404
+
+        # invalid migration group
+        response = admin_api.post(f"/migrations/invalid_group/{action}/0001_migrations")
+        assert response.status_code == 500
+        assert json.loads(response.data) == {"error": "KeyError('invalid_group')"}
+
+        with patch.object(Runner, method) as mock_run_migration:
+            response = admin_api.post(f"/migrations/system/{action}/0001_migrations")
+            assert response.status_code == 200
+            assert response.data == b"OK"
+            migration_key = MigrationKey(group="system", migration_id="0001_migrations")
+            mock_run_migration.assert_called_once_with(
+                migration_key, force=False, fake=False
+            )
+
+        with patch.object(Runner, method) as mock_run_migration:
+            # not allowed migration group
+            response = admin_api.post(f"/migrations/sessions/{action}/0001_sessions")
+            assert response.status_code == 400
+            assert json.loads(response.data) == {"error": "Group not allowed"}
+            assert mock_run_migration.call_count == 0
+
+            # forced migration
+            response = admin_api.post(
+                f"/migrations/system/{action}/0001_migrations?force=1"
+            )
+            assert response.status_code == 200
+            mock_run_migration.assert_called_once_with(
+                migration_key, force=True, fake=False
+            )
+
+        with patch.object(Runner, method) as mock_run_migration:
+            # fake migration
+            response = admin_api.post(
+                f"/migrations/system/{action}/0001_migrations?fake=1&force=true"
+            )
+            assert response.status_code == 200
+            mock_run_migration.assert_called_once_with(
+                migration_key, force=True, fake=True
+            )
