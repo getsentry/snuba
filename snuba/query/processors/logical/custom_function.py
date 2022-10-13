6edc55c470ac527063bc1fe69ed7de5d13ea1832
Vlad Kluev <vlad.kluev@sentry.io>
2022-10-13 15:20:34 -0700

    make query processors that use custom function

--- snuba/query/processors/logical/custom_function.py
+++ snuba/query/processors/logical/custom_function.py
@@ -1,6 +1,9 @@
 from dataclasses import replace
 from typing import Any, Mapping, Sequence, Tuple
 
+from sentry_relay.consts import SPAN_STATUS_NAME_TO_CODE
+
+from snuba.clickhouse.columns import UInt
 from snuba.query.exceptions import InvalidExpressionException
 from snuba.query.expressions import Column, Expression, FunctionCall, Literal
 from snuba.query.logical import Query
@@ -11,6 +14,8 @@ from snuba.query.processors.logical import (
 )
 from snuba.query.query_settings import QuerySettings
 from snuba.query.validation import InvalidFunctionCall
+from snuba.query.validation.signature import Column as ColType
+from snuba.query.validation.signature import Literal as LiteralType
 from snuba.query.validation.signature import ParamType, SignatureValidator
 
 
@@ -133,3 +138,37 @@ class CustomFunction(LogicalQueryProcessor):
                 return expression
 
         query.transform_expressions(apply_function)
+
+
+class ApdexProcessor(LogicalQueryProcessor):
+    def __init__(self):
+        self.__processor = CustomFunction(
+            "apdex",
+            [("column", ColType({UInt})), ("satisfied", LiteralType({int}))],
+            simple_function(
+                "divide(plus(countIf(lessOrEquals(column, satisfied)), divide(countIf(and(greater(column, satisfied), lessOrEquals(column, multiply(satisfied, 4)))), 2)), count())"
+            ),
+        )
+
+    def process_query(self, query: Query, settings: QuerySettings):
+        return self.__processor.process_query(query, settings)
+
+
+class FailureRateProcessor(LogicalQueryProcessor):
+    def __init__(self) -> None:
+        self.__processor = CustomFunction(
+            "failure_rate",
+            [],
+            partial_function(
+                # We use and(notEquals...) here instead of in(tuple(...)) because it's possible to get an impossible query that sets transaction_status to NULL.
+                # Clickhouse returns an error if an expression such as NULL in (0, 1, 2) appears.
+                "divide(countIf(and(notEquals(transaction_status, ok), and(notEquals(transaction_status, cancelled), notEquals(transaction_status, unknown)))), count())",
+                [
+                    (code, SPAN_STATUS_NAME_TO_CODE[code])
+                    for code in ("ok", "cancelled", "unknown")
+                ],
+            ),
+        )
+
+    def process_query(self, query: Query, settings: QuerySettings):
+        return self.__processor.process_query(query, settings)
