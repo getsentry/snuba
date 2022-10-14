2c24c6f2e418784aaf43c59d8169bdaf009a01f8
Enoch Tang <enoch.tang@sentry.io>
2022-10-14 15:51:31 -0400

    Move validation to proper validation directory

--- snuba/datasets/configuration/validation/post_loader.py
+++ snuba/datasets/configuration/validation/post_loader.py
@@ -14,7 +14,7 @@ def validate_storages() -> None:
 
 def validate_topics_with_settings() -> None:
     """
-    This functoion validates topics specified in settings are valid
+    This function validates topics specified in settings are valid
     topics that have been loaded into the Topics registry (via storage builder).
     """
     topic_names: Set[str] = set([t.value for t in Topic])
