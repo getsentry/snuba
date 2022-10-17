85ab2807801a2a18abce383920a92b49e8229a3a
meredith <meredith@sentry.io>
2022-10-17 09:33:29 -0700

    fix typing

--- tests/migrations/test_policies.py
+++ tests/migrations/test_policies.py
@@ -1,9 +1,11 @@
-from unittest.mock import patch
+from typing import Mapping
+from unittest.mock import Mock, patch
 
 import pytest
 
 from snuba.migrations.groups import MigrationGroup
 from snuba.migrations.policies import (
+    MigrationPolicy,
     ReadOnlyPolicy,
     WriteAllPolicy,
     WriteSafeAndPendingPolicy,
@@ -11,21 +13,21 @@ from snuba.migrations.policies import (
 from snuba.migrations.runner import MigrationKey
 from snuba.migrations.status import Status
 
-POLICIES = {
+POLICIES: Mapping[str, MigrationPolicy] = {
     "read_only": ReadOnlyPolicy(),
     "write_pending": WriteSafeAndPendingPolicy(),
     "write_all": WriteAllPolicy(),
 }
 
 
-def code_migration_key() -> None:
+def code_migration_key() -> MigrationKey:
     """
     Code Migration with blocking == True
     """
     return MigrationKey(MigrationGroup("events"), "0014_backfill_errors")
 
 
-def sql_migration_key() -> None:
+def sql_migration_key() -> MigrationKey:
     """
     SQL Migration with blocking == False
     """
@@ -34,7 +36,7 @@ def sql_migration_key() -> None:
 
 class TestMigrationPolicies:
     @pytest.mark.parametrize(
-        "migration_key, action, policy, expected",
+        "migration_key, action, policy_str, expected",
         [
             pytest.param(
                 code_migration_key(),
@@ -80,17 +82,19 @@ class TestMigrationPolicies:
             ),
         ],
     )
-    def test_policies(self, migration_key, action, policy, expected) -> None:
+    def test_policies(
+        self, migration_key: MigrationKey, action: str, policy_str: str, expected: bool
+    ) -> None:
         if action == "run":
-            assert POLICIES[policy].can_run(migration_key) == expected
+            assert POLICIES[policy_str].can_run(migration_key) == expected
         else:
-            assert POLICIES[policy].can_reverse(migration_key) == expected
+            assert POLICIES[policy_str].can_reverse(migration_key) == expected
 
     @patch(
         "snuba.migrations.runner.Runner.get_status",
         return_value=(Status.IN_PROGRESS, None),
     )
-    def test_pending_migration_reverse(self, mock_get_status):
+    def test_pending_migration_reverse(self, mock_get_status: Mock) -> None:
         migration_key = MigrationKey(
             MigrationGroup("events"), "0016_drop_legacy_events"
         )
