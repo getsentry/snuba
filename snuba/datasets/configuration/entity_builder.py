b215a88066770ea56275455b6e4c494d44f32e8c
Enoch Tang <enoch.tang@sentry.io>
2022-10-18 10:53:40 -0400

    feat(MDC): Add Translation Mapper Support for Transaction Dataset. (#3261)
    * Add column translation mapper support for pluggable entity
    
    * Add tests for loading column mappers in entity loader
    
    * Write custom IP column mapper
    
    * fix test
    
    * minor changes
    
    * remove registered class from Expressions

--- snuba/datasets/configuration/entity_builder.py
+++ snuba/datasets/configuration/entity_builder.py
@@ -4,6 +4,7 @@ from typing import Any, Sequence
 
 import snuba.clickhouse.translators.snuba.function_call_mappers  # noqa
 from snuba.clickhouse.translators.snuba.allowed import (
+    ColumnMapper,
     CurriedFunctionCallMapper,
     FunctionCallMapper,
     SubscriptableReferenceMapper,
@@ -43,6 +44,14 @@ def _build_entity_query_processors(
 def _build_entity_translation_mappers(
     config_translation_mappers: dict[str, Any],
 ) -> TranslationMappers:
+    columns_mappers: list[ColumnMapper] = (
+        [
+            ColumnMapper.get_from_name(col_config["mapper"])(**col_config["args"])
+            for col_config in config_translation_mappers["columns"]
+        ]
+        if "columns" in config_translation_mappers
+        else []
+    )
     function_mappers: list[FunctionCallMapper] = [
         FunctionCallMapper.get_from_name(fm_config["mapper"])(**fm_config["args"])
         for fm_config in config_translation_mappers["functions"]
@@ -55,15 +64,16 @@ def _build_entity_translation_mappers(
     ]
     curried_function_mappers: list[CurriedFunctionCallMapper] = (
         [
-            CurriedFunctionCallMapper.get_from_name(fm_config["mapper"])(
-                **fm_config["args"]
+            CurriedFunctionCallMapper.get_from_name(curr_config["mapper"])(
+                **curr_config["args"]
             )
-            for fm_config in config_translation_mappers["curried_functions"]
+            for curr_config in config_translation_mappers["curried_functions"]
         ]
         if "curried_functions" in config_translation_mappers
         else []
     )
     return TranslationMappers(
+        columns=columns_mappers,
         functions=function_mappers,
         subscriptables=subscriptable_mappers,
         curried_functions=curried_function_mappers,
