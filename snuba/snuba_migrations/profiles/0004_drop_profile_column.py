07793abded6dffc3539cd79931578905f53be628
Pierre Massat <pierre.massat@sentry.io>
2022-10-19 13:08:09 -0400

    ref(profiling): Drop profile column (#3254)

---
+++ snuba/snuba_migrations/profiles/0004_drop_profile_column.py
@@ -0,0 +1,48 @@
+from typing import Sequence
+
+from snuba.clickhouse.columns import Column, String
+from snuba.clusters.storage_sets import StorageSetKey
+from snuba.migrations import migration, operations
+from snuba.migrations.columns import MigrationModifiers as Modifiers
+
+
+class Migration(migration.ClickhouseNodeMigration):
+    blocking = False
+
+    def forwards_local(self) -> Sequence[operations.SqlOperation]:
+        return [
+            operations.DropColumn(
+                storage_set=StorageSetKey.PROFILES,
+                table_name="profiles_local",
+                column_name="profile",
+            )
+        ]
+
+    def backwards_local(self) -> Sequence[operations.SqlOperation]:
+        return [
+            operations.AddColumn(
+                storage_set=StorageSetKey.PROFILES,
+                table_name="profiles_local",
+                column=Column("profile", String(Modifiers(codecs=["LZ4HC(9)"]))),
+                after="received",
+            )
+        ]
+
+    def forwards_dist(self) -> Sequence[operations.SqlOperation]:
+        return [
+            operations.DropColumn(
+                storage_set=StorageSetKey.PROFILES,
+                table_name="profiles_dist",
+                column_name="profile",
+            )
+        ]
+
+    def backwards_dist(self) -> Sequence[operations.SqlOperation]:
+        return [
+            operations.AddColumn(
+                storage_set=StorageSetKey.PROFILES,
+                table_name="profiles_dist",
+                column=Column("profile", String(Modifiers(codecs=["LZ4HC(9)"]))),
+                after="received",
+            )
+        ]
