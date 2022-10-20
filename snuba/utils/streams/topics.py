5e6f76247216b445615205cb442b72b64b756e9b
Riya Chakraborty <riyachakraborty@H472HKN6XK-Riya-Chakraborty.local>
2022-10-20 11:55:20 -0700

    Attempt to rebase

--- snuba/utils/streams/topics.py
+++ snuba/utils/streams/topics.py
@@ -6,37 +6,37 @@ from typing import Any, Iterator, Mapping
 _HARDCODED_TOPICS = {
     "EVENTS": "events",
     "EVENT_REPLACEMENTS": "event-replacements",
-    "SNUBA_COMMIT_LOG": "snuba-commit-log",
+    "COMMIT_LOG": "snuba-commit-log",
     "CDC": "cdc",
     "TRANSACTIONS": "transactions",
-    "SNUBA_TRANSACTIONS_COMMIT_LOG": "snuba-transactions-commit-log",
-    "SNUBA_METRICS": "snuba-metrics",
+    "TRANSACTIONS_COMMIT_LOG": "snuba-transactions-commit-log",
+    "METRICS": "snuba-metrics",
     "OUTCOMES": "outcomes",
-    "INGEST_SESSIONS": "ingest-sessions",
-    "SNUBA_METRICS_COMMIT_LOG": "snuba-metrics-commit-log",
-    "SCHEDULED_SUBSCRIPTIONS_EVENTS": "scheduled-subscriptions-events",
-    "SCHEDULED_SUBSCRIPTIONS_TRANSACTIONS": "scheduled-subscriptions-transactions",
-    "SCHEDULED_SUBSCRIPTIONS_METRICS": "scheduled-subscriptions-metrics",
-    "SCHEDULED_SUBSCRIPTIONS_GENERIC_METRICS_SETS": "scheduled-subscriptions-generic-metrics-sets",
-    "SCHEDULED_SUBSCRIPTIONS_GENERIC_METRICS_DISTRIBUTIONS": "scheduled-subscriptions-generic-metrics-distributions",
-    "EVENTS_SUBSCRIPTION_RESULTS": "events-subscription-results",
-    "TRANSACTIONS_SUBSCRIPTION_RESULTS": "transactions-subscription-results",
-    "METRICS_SUBSCRIPTION_RESULTS": "metrics-subscription-results",
-    "GENERIC_METRICS_SETS_SUBSCRIPTION_RESULTS": "generic-metrics-sets-subscription-results",
-    "GENERIC_METRICS_DISTRIBUTIONS_SUBSCRIPTION_RESULTS": "generic-metrics-distributions-subscription-results",
-    "SNUBA_QUERIES": "snuba-queries",
-    "PROCESSED_PROFILES": "processed-profiles",
-    "PROFILES_CALL_TREE": "profiles-call-tree",
-    "INGEST_REPLAY_EVENTS": "ingest-replay-events",
-    "SNUBA_GENERIC_METRICS": "snuba-generic-metrics",
-    "SNUBA_GENERIC_METRICS_SETS_COMMIT_LOG": "snuba-generic-metrics-sets-commit-log",
-    "SNUBA_GENERIC_METRICS_DISTRIBUTIONS_COMMIT_LOG": "snuba-generic-metrics-distributions-commit-log",
-    "SNUBA_DEAD_LETTER_INSERTS": "snuba-dead-letter-inserts",
-    "SNUBA_ATTRIBUTION": "snuba-attribution",
-    "SNUBA_DEAD_LETTER_METRICS": "snuba-dead-letter-metrics",
-    "SNUBA_DEAD_LETTER_SESSIONS": "snuba-dead-letter-sessions",
-    "SNUBA_DEAD_LETTER_GENERIC_METRICS": "snuba-dead-letter-generic-metrics",
-    "SNUBA_DEAD_LETTER_REPLAYS": "snuba-dead-letter-replays",
+    "SESSIONS": "ingest-sessions",
+    "METRICS_COMMIT_LOG": "snuba-metrics-commit-log",
+    "SUBSCRIPTION_SCHEDULED_EVENTS": "scheduled-subscriptions-events",
+    "SUBSCRIPTION_SCHEDULED_TRANSACTIONS": "scheduled-subscriptions-transactions",
+    "SUBSCRIPTION_SCHEDULED_METRICS": "scheduled-subscriptions-metrics",
+    "SUBSCRIPTION_SCHEDULED_GENERIC_METRICS_SETS": "scheduled-subscriptions-generic-metrics-sets",
+    "SUBSCRIPTION_SCHEDULED_GENERIC_METRICS_DISTRIBUTIONS": "scheduled-subscriptions-generic-metrics-distributions",
+    "SUBSCRIPTION_RESULTS_EVENTS": "events-subscription-results",
+    "SUBSCRIPTION_RESULTS_TRANSACTIONS": "transactions-subscription-results",
+    "SUBSCRIPTION_RESULTS_METRICS": "metrics-subscription-results",
+    "SUBSCRIPTION_RESULTS_GENERIC_METRICS_SETS": "generic-metrics-sets-subscription-results",
+    "SUBSCRIPTION_RESULTS_GENERIC_METRICS_DISTRIBUTIONS": "generic-metrics-distributions-subscription-results",
+    "QUERYLOG": "snuba-queries",
+    "PROFILES": "processed-profiles",
+    "PROFILES_FUNCTIONS": "profiles-call-tree",
+    "REPLAYEVENTS": "ingest-replay-events",
+    "GENERIC_METRICS": "snuba-generic-metrics",
+    "GENERIC_METRICS_SETS_COMMIT_LOG": "snuba-generic-metrics-sets-commit-log",
+    "GENERIC_METRICS_DISTRIBUTIONS_COMMIT_LOG": "snuba-generic-metrics-distributions-commit-log",
+    "DEAD_LETTER_QUEUE_INSERTS": "snuba-dead-letter-inserts",
+    "ATTRIBUTION": "snuba-attribution",
+    "DEAD_LETTER_METRICS": "snuba-dead-letter-metrics",
+    "DEAD_LETTER_SESSIONS": "snuba-dead-letter-sessions",
+    "DEAD_LETTER_GENERIC_METRICS": "snuba-dead-letter-generic-metrics",
+    "DEAD_LETTER_REPLAYS": "snuba-dead-letter-replays",
 }
 
 _REGISTERED_TOPICS: dict[str, str] = {}
@@ -46,7 +46,7 @@ class _Topic(type):
     def __getattr__(self, attr: str) -> "Topic":
         if attr not in _HARDCODED_TOPICS and attr not in _REGISTERED_TOPICS:
             raise AttributeError(attr)
-        return Topic(attr.lower().replace("_", "-"))
+        return Topic(attr.lower())
 
     def __iter__(self) -> Iterator[Topic]:
         return iter(
@@ -70,7 +70,7 @@ class Topic(metaclass=_Topic):
 
 
 def register_topic(key: str) -> Topic:
-    _REGISTERED_TOPICS[key.upper().replace("-", "_")] = key.lower()
+    _REGISTERED_TOPICS[key.upper()] = key.lower()
     return Topic(key)
 
 
