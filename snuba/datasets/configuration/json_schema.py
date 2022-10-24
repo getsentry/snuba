1775f3e83d3b1cc7e1cc7548b0159bbab5586217
Evan Hicks <evanh@users.noreply.github.com>
2022-10-24 16:41:02 -0400

    feat(TDC): Transactions storage as configuration (#3274)
    Duplicate the transactions writable storage in configuration.
    
    This was tested locally by also removing the Python storage completely, to
    ensure backwards compatibility.
    
    Changes made to support the storage:
    
    - Add UUID, IPv4 and IPv6 type
    - Create a function registered_class_schema to reduce duplicated schemas
    - Add writer_options to storage schema
    - Remove importlib calls in a test that are not needed and were breaking the configuration tests
    - Rename `part_format` to `partition_format` in the config. There is a follow up PR to rename it
    in the class as well.
    
    
    This was tested locally by also removing the Python storage completely, to
    ensure backwards compatibility.
    
    Co-authored-by: Rahul Saini <rahulsaini805@gmail.com>

--- snuba/datasets/configuration/json_schema.py
+++ snuba/datasets/configuration/json_schema.py
@@ -204,6 +204,11 @@ SCHEMA_SCHEMA = {
             "type": "string",
             "description": "The distributed table name in distributed ClickHouse",
         },
+        "partition_format": {
+            "type": "array",
+            "items": {"type": "string"},
+            "description": "The format of the partitions in Clickhouse. Used in the cleanup job.",
+        },
     },
     "additionalProperties": False,
 }
@@ -224,24 +229,52 @@ STORAGE_SCHEMA = {
     "additionalProperties": False,
 }
 
-STORAGE_QUERY_PROCESSOR = {
-    "type": "object",
-    "properties": {
-        "processor": {
-            "type": "string",
-            "description": "Name of ClickhouseQueryProcessor class config key. Responsible for the transformation applied to a query.",
+
+def registered_class_schema(
+    property_name: str, class_name: str, description: str
+) -> dict[str, Any]:
+    """
+    There are a number of registered classes that are represented in the
+    YAML in a very similar structure, just with different key names.
+    This function reduces the duplicate schema code.
+
+    :param property_name: The key in the configuration for the class name
+    :param class_name: The name of the class being represented.
+    :param description: The description added to the documentation.
+    """
+    single_class = {
+        "type": "object",
+        "properties": {
+            property_name: {
+                "type": "string",
+                "description": description,
+            },
+            "args": {
+                "type": "object",
+                "description": f"Key/value mappings required to instantiate {class_name} class.",
+            },  # args are a flexible dict
         },
-        "args": {
-            "type": "object",
-            "description": "Key/value mappings required to instantiate QueryProcessor class.",
-        },  # args are a flexible dict
-    },
-    "required": ["processor"],
-    "additionalProperties": False,
-}
+        "required": [property_name],
+        "additionalProperties": False,
+    }
+    return {"type": "array", "items": single_class}
 
 
-STORAGE_QUERY_PROCESSORS_SCHEMA = {"type": "array", "items": STORAGE_QUERY_PROCESSOR}
+STORAGE_QUERY_PROCESSORS_SCHEMA = registered_class_schema(
+    "processor",
+    "QueryProcessor",
+    "Name of ClickhouseQueryProcessor class config key. Responsible for the transformation applied to a query.",
+)
+STORAGE_QUERY_SPLITTERS_SCHEMA = registered_class_schema(
+    "splitter",
+    "QuerySplitStrategy",
+    "Name of QuerySplitStrategy class config key. Responsible for splitting a query into two at runtime and combining the results.",
+)
+STORAGE_MANDATORY_CONDITION_CHECKERS_SCHEMA = registered_class_schema(
+    "condition",
+    "ConditionChecker",
+    "Name of ConditionChecker class config key. Responsible for running final checks on a query to ensure that transformations haven't impacted/removed conditions required for security reasons.",
+)
 
 
 ENTITY_QUERY_PROCESSOR = {
@@ -320,6 +353,13 @@ V1_WRITABLE_STORAGE_SCHEMA = {
         "storage": STORAGE_SCHEMA,
         "schema": SCHEMA_SCHEMA,
         "stream_loader": STREAM_LOADER_SCHEMA,
+        "query_processors": STORAGE_QUERY_PROCESSORS_SCHEMA,
+        "query_splitters": STORAGE_QUERY_SPLITTERS_SCHEMA,
+        "mandatory_condition_checkers": STORAGE_MANDATORY_CONDITION_CHECKERS_SCHEMA,
+        "writer_options": {
+            "type": "object",
+            "description": "Extra Clickhouse fields that are used for consumer writes",
+        },
     },
     "required": [
         "version",
@@ -343,6 +383,8 @@ V1_READABLE_STORAGE_SCHEMA = {
         "storage": STORAGE_SCHEMA,
         "schema": SCHEMA_SCHEMA,
         "query_processors": STORAGE_QUERY_PROCESSORS_SCHEMA,
+        "query_splitters": STORAGE_QUERY_PROCESSORS_SCHEMA,
+        "mandatory_condition_checkers": STORAGE_QUERY_PROCESSORS_SCHEMA,
     },
     "required": [
         "version",
