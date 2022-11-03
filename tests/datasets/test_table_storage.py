fd44db3a6915622164de36a9a0d14dce8f8710f4
Riya Chakraborty <riyachakraborty@H472HKN6XK-Riya-Chakraborty.local>
2022-11-03 00:56:53 -0700

    Add test file

---
+++ tests/datasets/test_table_storage.py
@@ -0,0 +1,10 @@
+# from snuba.datasets.table_storage import KafkaTopicSpec
+
+# # define some SLICED_KAFKA_TOPIC_MAP
+
+# # patch the settings with above map
+# def test_get_physical_topic_name() -> None:
+#     # for some Topic
+#     test_topic_spec = KafkaTopicSpec()
+
+#     # a few asserts
