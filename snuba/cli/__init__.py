61e0965b366b0c69d0bbc34a2d2bc52860bbef93
volokluev <3169433+volokluev@users.noreply.github.com>
2022-11-03 15:56:43 -0700

    Revert "Explicitly initialize snuba take 2 (#3326)"
    This reverts commit 133a75cff571dfc221e6c49466df4c62c41b2cbc.

--- snuba/cli/__init__.py
+++ snuba/cli/__init__.py
@@ -1,79 +1,51 @@
-from __future__ import annotations
-
 import logging
-import os
 import time
-from typing import Any
+from pkgutil import walk_packages
 
 import click
 import sentry_sdk
 import structlog
 
-from snuba.core import initialize
 from snuba.environment import metrics as environment_metrics
 from snuba.environment import setup_logging, setup_sentry
 from snuba.utils.metrics.wrapper import MetricsWrapper
 
 setup_sentry()
+setup_logging("DEBUG")
+logger = logging.getLogger("snuba_init")
+
+start = time.perf_counter()
+logger.info("Initializing Snuba...")
+metrics = MetricsWrapper(environment_metrics, "cli")
+
+
+@click.group()
+@click.version_option()
+def main() -> None:
+    """\b
+                         o
+                        O
+                        O
+                        o
+    .oOo  'OoOo. O   o  OoOo. .oOoO'
+    `Ooo.  o   O o   O  O   o O   o
+        O  O   o O   o  o   O o   O
+    `OoO'  o   O `OoO'o `OoO' `OoO'o"""
+
 
 with sentry_sdk.start_transaction(
     op="snuba_init", name="Snuba CLI Initialization", sampled=True
 ):
+    for loader, module_name, is_pkg in walk_packages(__path__, __name__ + "."):
+        logger.info(f"Loading module {module_name}")
+        with sentry_sdk.start_span(op="import", description=module_name):
+            module = __import__(module_name, globals(), locals(), ["__name__"])
+            cmd = getattr(module, module_name.rsplit(".", 1)[-1])
+            if isinstance(cmd, click.Command):
+                main.add_command(cmd)
 
-    setup_logging("DEBUG")
-    logger = logging.getLogger("snuba_init")
+init_time = time.perf_counter() - start
+metrics.gauge("snuba_init", init_time)
+logger.info(f"Snuba initialization took {init_time}s")
 
-    start = time.perf_counter()
-    logger.info("Initializing Snuba CLI...")
-    metrics = MetricsWrapper(environment_metrics, "cli")
-
-    plugin_folder = os.path.dirname(__file__)
-
-    class SnubaCLI(click.MultiCommand):
-        def list_commands(self, ctx: Any) -> list[str]:
-            rv = []
-            for filename in os.listdir(plugin_folder):
-                if filename.endswith(".py") and filename != "__init__.py":
-                    # IMPORTANT!!!!
-                    # Click replaces underscores with dashes for command names
-                    # by default. To mimic this behavior we have to do that here
-                    # since all of our infrastructure depends on this being the
-                    # case
-                    rv.append(filename[:-3].replace("_", "-"))
-            rv.sort()
-            return rv
-
-        def get_command(self, ctx: Any, name: str) -> click.Command:
-            logger.info(f"Loading command {name}")
-            # IMPORTANT!!!!
-            # Click replaces underscores with dashes for command names
-            # by default. To mimic this behavior we have to do that here
-            # since all of our infrastructure depends on this being the
-            # case
-            actual_command_name = name.replace("-", "_")
-            with sentry_sdk.start_span(op="import", description=name):
-                ns: dict[str, click.Command] = {}
-                fn = os.path.join(plugin_folder, actual_command_name + ".py")
-                with open(fn) as f:
-                    code = compile(f.read(), fn, "exec")
-                    eval(code, ns, ns)
-                initialize.initialize()
-                init_time = time.perf_counter() - start
-                metrics.gauge("snuba_init", init_time)
-                logger.info(f"Snuba initialization took {init_time}s")
-                return ns[actual_command_name]
-
-    @click.command(cls=SnubaCLI)
-    @click.version_option()
-    def main() -> None:
-        """\b
-                             o
-                            O
-                            O
-                            o
-        .oOo  'OoOo. O   o  OoOo. .oOoO'
-        `Ooo.  o   O o   O  O   o O   o
-            O  O   o O   o  o   O o   O
-        `OoO'  o   O `OoO'o `OoO' `OoO'o"""
-
-    structlog.reset_defaults()
+structlog.reset_defaults()
