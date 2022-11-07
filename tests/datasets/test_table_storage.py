8c8425dcd22b95d294d4fc058d320b4a0d544b71
Riya Chakraborty <riyachakraborty@H472HKN6XK-Riya-Chakraborty.local>
2022-11-07 08:33:29 -0800

    Add test for get_physical_topic_name

--- tests/datasets/test_table_storage.py
+++ tests/datasets/test_table_storage.py
@@ -1,10 +1,25 @@
-# from snuba.datasets.table_storage import KafkaTopicSpec
+import importlib
+from unittest.mock import patch
 
-# # define some SLICED_KAFKA_TOPIC_MAP
+from snuba.datasets import table_storage
+from snuba.datasets.storages.factory import get_writable_storage
+from snuba.datasets.storages.storage_key import StorageKey
 
-# # patch the settings with above map
-# def test_get_physical_topic_name() -> None:
-#     # for some Topic
-#     test_topic_spec = KafkaTopicSpec()
+# define some SLICED_KAFKA_TOPIC_MAP
+TEST_SLICED_KAFKA_TOPIC_MAP = {("ingest-replay-events", 2): "ingest-replay-events-2"}
 
-#     # a few asserts
+
+@patch("snuba.settings.SLICED_KAFKA_TOPIC_MAP", TEST_SLICED_KAFKA_TOPIC_MAP)
+def test_get_physical_topic_name() -> None:
+    importlib.reload(table_storage)
+
+    storage_key = StorageKey("replays")
+    storage = get_writable_storage(storage_key)
+
+    stream_loader = storage.get_table_writer().get_stream_loader()
+
+    default_topic_spec = stream_loader.get_default_topic_spec()
+
+    physical_topic_name = default_topic_spec.get_physical_topic_name(slice_id=2)
+
+    assert physical_topic_name == "ingest-replay-events-2"
