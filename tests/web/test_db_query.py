4030241f86550f38e0e8af789a5dc0f8d07b5364
Vlad Kluev <vlad.kluev@sentry.io>
2022-11-14 14:30:44 -0800

    don't mutate function args

--- tests/web/test_db_query.py
+++ tests/web/test_db_query.py
@@ -88,13 +88,12 @@ def test_apply_thread_quota(
     resource_quota: ResourceQuota,
     rate_limit_stats: RateLimitStats,
     expected_query_settings: dict,
-):
+) -> None:
     settings = HTTPQuerySettings()
     for rlimit in rate_limit_params:
         settings.add_rate_limit(rlimit)
     settings.set_resource_quota(resource_quota)
-    clickhouse_query_settings: MutableMapping[str, Any] = {}
-    _apply_thread_quota_to_clickhouse_query_settings(
-        settings, clickhouse_query_settings, rate_limit_stats
+    clickhouse_query_settings = _apply_thread_quota_to_clickhouse_query_settings(
+        settings, {}, rate_limit_stats
     )
     assert clickhouse_query_settings == expected_query_settings
