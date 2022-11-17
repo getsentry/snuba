402b319c0ad55d4a5e59d742574c8096e125f137
Riya Chakraborty <riyachakraborty@H472HKN6XK-Riya-Chakraborty.local>
2022-11-17 11:56:20 -0800

    Add tests for optional attributes

--- tests/consumers/test_consumer_builder.py
+++ tests/consumers/test_consumer_builder.py
@@ -8,17 +8,21 @@ from snuba.consumers.consumer_builder import (
     ProcessingParameters,
 )
 from snuba.datasets.storage import WritableTableStorage
+from snuba.datasets.storages.factory import get_writable_storage
 from snuba.datasets.storages.storage_key import StorageKey
 from snuba.utils.metrics.backends.abstract import MetricsBackend
 from snuba.utils.metrics.wrapper import MetricsWrapper
 
+test_storage_key = StorageKey("errors")
+consumer_group_name = "my_consumer_group"
+
 consumer_builder = ConsumerBuilder(
-    storage_key=StorageKey("errors"),
+    storage_key=test_storage_key,
     kafka_params=KafkaParameters(
         raw_topic=None,
         replacements_topic=None,
         bootstrap_servers=None,
-        group_id="my_consumer_group",
+        group_id=consumer_group_name,
         commit_log_topic=None,
         auto_offset_reset="earliest",
         strict_offset_reset=None,
@@ -35,56 +39,37 @@ consumer_builder = ConsumerBuilder(
     metrics=MetricsWrapper(
         environment.metrics,
         "test_consumer",
-        tags={"group": "my_consumer_group", "storage": StorageKey("errors").value},
+        tags={"group": consumer_group_name, "storage": test_storage_key.value},
     ),
     parallel_collect=False,
     slice_id=None,
 )
 
+# the point of these tests is to try out
+# different configurations of builder
+# and see if any initialization breaks
+
 
 def test_consumer_non_optional_attributes() -> None:
-    try:
-        consumer_builder.storage
-    except NameError:
-        assert False, "ConsumerBuilder must have storage defined"
+    assert consumer_builder.storage is not None
     assert isinstance(consumer_builder.storage, WritableTableStorage)
+    assert consumer_builder.storage == get_writable_storage(test_storage_key)
 
-    try:
-        consumer_builder.consumer_group
-    except NameError:
-        assert False, "ConsumerBuilder must have consumer group defined"
+    assert consumer_builder.consumer_group is not None
     assert isinstance(consumer_builder.consumer_group, str)
+    assert consumer_builder.consumer_group == consumer_group_name
 
-    try:
-        consumer_builder.raw_topic
-    except NameError:
-        assert False, "ConsumerBuilder must have a raw/default topic defined"
+    assert consumer_builder.raw_topic is not None
     assert isinstance(consumer_builder.raw_topic, Topic)
 
-    try:
-        consumer_builder.broker_config
-    except NameError:
-        assert False, "ConsumerBuilder must have a default broker config defined"
-    # assert isinstance(consumer_builder.broker_config, KafkaBrokerConfig)
+    assert consumer_builder.broker_config is not None
 
-    try:
-        consumer_builder.producer_broker_config
-    except NameError:
-        assert (
-            False
-        ), "ConsumerBuilder must have a broker config defined for producer(s)"
-    # assert isinstance(consumer_builder.producer_broker_config, KafkaBrokerConfig)
+    assert consumer_builder.producer_broker_config is not None
 
-    try:
-        consumer_builder.producer
-    except NameError:
-        assert False, "ConsumerBuilder must have a default producer defined"
+    assert consumer_builder.producer is not None
     assert isinstance(consumer_builder.producer, Producer)
 
-    try:
-        consumer_builder.metrics
-    except NameError:
-        assert False, "ConsumerBuilder must have metrics defined"
+    assert consumer_builder.metrics is not None
     assert isinstance(consumer_builder.metrics, MetricsBackend)
 
     assert consumer_builder.max_batch_size == 3
@@ -94,4 +79,59 @@ def test_consumer_non_optional_attributes() -> None:
     assert consumer_builder.queued_min_messages == 2
 
 
-# def test_consumer_optional_attributes() -> None:
+def test_consumer_optional_attributes() -> None:
+    try:
+        consumer_builder.bootstrap_servers
+    except NameError:
+        assert (
+            False
+        ), "ConsumerBuilder's bootstrap_servers must either be defined to some value or None"
+
+    try:
+        consumer_builder.replacements_topic
+    except NameError:
+        assert (
+            False
+        ), "ConsumerBuilder's replacements_topic must either be defined to some value or None"
+
+    try:
+        consumer_builder.commit_log_topic
+    except NameError:
+        assert (
+            False
+        ), "ConsumerBuilder's commit_log_topic must either be defined to some value or None"
+
+    try:
+        consumer_builder.stats_callback
+    except NameError:
+        assert (
+            False
+        ), "ConsumerBuilder's stats_callback must either be defined to some value or None"
+
+    try:
+        consumer_builder.strict_offset_reset
+    except NameError:
+        assert (
+            False
+        ), "ConsumerBuilder's strict_offset_reset must either be defined to some value or None"
+
+    try:
+        consumer_builder.processes
+    except NameError:
+        assert (
+            False
+        ), "ConsumerBuilder's processes must either be defined to some value or None"
+
+    try:
+        consumer_builder.input_block_size
+    except NameError:
+        assert (
+            False
+        ), "ConsumerBuilder's input_block_size must either be defined to some value or None"
+
+    try:
+        consumer_builder.output_block_size
+    except NameError:
+        assert (
+            False
+        ), "ConsumerBuilder's output_block_size must either be defined to some value or None"
