11ca19a30d303115fc2558ba4ee4700953283e44
Dalitso Banda <dalitso.banda@sentry.io>
2022-11-18 17:15:26 -0800

    remove unused code and clear sql text on change

--- snuba/admin/static/clickhouse_migrations/index.tsx
+++ snuba/admin/static/clickhouse_migrations/index.tsx
@@ -3,25 +3,6 @@ import Client from "../api_client";
 import { Table } from "../table";
 import { MigrationData, MigrationGroupResult, GroupOptions, RunMigrationRequest, RunMigrationResult, Action } from "./types";
 
-const SQLforwards =
-  "Local operations:\
-\n\n\
-CREATE TABLE IF NOT EXISTS replays_local (replay_id UUID, event_hash UUID, segment_id Nullable(UInt16), trace_ids Array(UUID), _trace_ids_hashed Array(UInt64) MATERIALIZED arrayMap(t -> cityHash64(t), trace_ids), title String, project_id UInt64, timestamp DateTime, platform LowCardinality(String), environment LowCardinality(Nullable(String)), release Nullable(String), dist Nullable(String), ip_address_v4 Nullable(IPv4), ip_address_v6 Nullable(IPv6), user String, user_id Nullable(String), user_name Nullable(String), user_email Nullable(String), sdk_name String, sdk_version String, tags Nested(key String, value String), retention_days UInt16, partition UInt16, offset UInt64) ENGINE ReplacingMergeTree() ORDER BY (project_id, toStartOfDay(timestamp), cityHash64(replay_id), event_hash) PARTITION BY (retention_days, toMonday(timestamp)) TTL timestamp + toIntervalDay(retention_days) SETTINGS index_granularity=8192;\
-ALTER TABLE replays_local ADD INDEX IF NOT EXISTS bf_trace_ids_hashed _trace_ids_hashed TYPE bloom_filter() GRANULARITY 1;\
-\n\n\
-Dist operations:\
-\n\n\
-Skipped dist operation - single node cluster";
-
-const SQLbackwards =
-  "Local operations:\
-\n\n\
-DROP TABLE IF EXISTS replays_local;\
-\n\n\
-Dist operations:\
-\n\n\
-Skipped dist operation - single node cluster";
-
 function ClickhouseMigrations(props: { api: Client }) {
   const [allGroups, setAllGroups] = useState<GroupOptions>({});
   const [migrationGroup, setMigrationGroup] =
@@ -42,10 +23,12 @@ function ClickhouseMigrations(props: { api: Client }) {
   function selectGroup(groupName: string) {
     const migrationGroup: MigrationGroupResult = allGroups[groupName];
     setMigrationGroup(() => migrationGroup);
+    setSQLText(()=>"")
   }
 
   function selectMigration(migrationId: string) {
     setMigrationId(() => migrationId);
+    setSQLText(()=>"")
   }
 
   function execute(action: Action) {
