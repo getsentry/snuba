171fabee591fe3d5bf7a0ce960abec7fbcdf9c18
Rahul Saini <rahulsaini805@gmail.com>
2022-11-22 12:13:45 -0800

    missed entity sub builder

--- snuba/datasets/configuration/entity_subscription_builder.py
+++ snuba/datasets/configuration/entity_subscription_builder.py
@@ -1,10 +1,7 @@
 import logging
 from typing import Type
 
-import fastjsonschema
-import sentry_sdk
-
-from snuba.datasets.configuration.json_schema import V1_ENTITY_SUBSCIPTION_SCHEMA
+from snuba.datasets.configuration.json_schema import ENIITY_SUBSCRIPTION_VALIDATORS
 from snuba.datasets.configuration.loader import load_configuration_data
 from snuba.datasets.entity_subscriptions.pluggable_entity_subscription import (
     PluggableEntitySubscription,
@@ -16,18 +13,12 @@ logger = logging.getLogger("snuba.entity_subscriptions_builder")
 def build_entity_subscription_from_config(
     file_path: str,
 ) -> Type[PluggableEntitySubscription]:
-    config = load_configuration_data(
-        file_path,
-        {"entity_subscription": fastjsonschema.compile(V1_ENTITY_SUBSCIPTION_SCHEMA)},
-    )
-    with sentry_sdk.start_span(
-        op="build", description=f"Entity Subscription: {config['name']}"
-    ):
-        PluggableEntitySubscription.name = config["name"]
-        PluggableEntitySubscription.MAX_ALLOWED_AGGREGATIONS = config[
-            "max_allowed_aggregations"
-        ]
-        PluggableEntitySubscription.disallowed_aggregations = config[
-            "disallowed_aggregations"
-        ]
-        return PluggableEntitySubscription
+    config = load_configuration_data(file_path, ENIITY_SUBSCRIPTION_VALIDATORS)
+    PluggableEntitySubscription.name = config["name"]
+    PluggableEntitySubscription.MAX_ALLOWED_AGGREGATIONS = config[
+        "max_allowed_aggregations"
+    ]
+    PluggableEntitySubscription.disallowed_aggregations = config[
+        "disallowed_aggregations"
+    ]
+    return PluggableEntitySubscription
