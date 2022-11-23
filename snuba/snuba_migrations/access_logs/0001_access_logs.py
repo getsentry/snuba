0b5bf8546cdf461d987692b0566f36647b902486
Michael Warkentin <michael@sentry.io>
2022-11-23 11:04:50 -0500

    OPS-2635: (wip) configure float and fixed string sizes, codecs

--- snuba/snuba_migrations/access_logs/0001_access_logs.py
+++ snuba/snuba_migrations/access_logs/0001_access_logs.py
@@ -65,47 +65,47 @@ class Migration(migration.ClickhouseNodeMigration):
     granularity = "2048"
     local_table_name = "access_logs"
     columns: Sequence[Column[Modifiers]] = [
-        Column("_time", DateTime()),  # CODEC(DoubleDelta, LZ4),
+        Column("_time", DateTime(codecs=["DoubleDelta", "LZ4"])),  # CODEC(DoubleDelta, LZ4),
         Column("_date", Date()),  # DEFAULT toDate(_time),
         Column("_ms", UInt(32)),  # UInt32,
-        Column("body_bytes_sent", UInt()),  # UInt32 CODEC(ZSTD(1)),
-        Column("status", UInt(32)),  # UInt32 CODEC(T64, ZSTD(1)),
-        Column("upstream_bytes_received", UInt(32)),  # UInt32 CODEC(ZSTD(1)),
-        Column("upstream_response_length", UInt(32)),  # UInt32 CODEC(ZSTD(1)),
-        Column("request_length", UInt(32)),  # UInt32 CODEC(ZSTD(1)),
+        Column("body_bytes_sent", UInt(32, codecs=["ZSTD(1)"])),  # UInt32 CODEC(ZSTD(1)),
+        Column("status", UInt(32, codecs=["T64", "ZSTD(1)"])),  # UInt32 CODEC(T64, ZSTD(1)),
+        Column("upstream_bytes_received", UInt(32, codecs=["ZSTD(1)"])),  # UInt32 CODEC(ZSTD(1)),
+        Column("upstream_response_length", UInt(32, codecs=["ZSTD(1)"])),  # UInt32 CODEC(ZSTD(1)),
+        Column("request_length", UInt(32, codecs=["ZSTD(1)"])),  # UInt32 CODEC(ZSTD(1)),
         Column("project_id", UInt(64)),  # UInt64,
-        Column("request", String()),  # CODEC(LZ4HC(0)) TTL _date + toIntervalDay(90),
-        Column("request_uri", String()),  # CODEC(LZ4) TTL _date + toIntervalDay(30),
+        Column("request", String(codecs=["LZ4HC(0)"])),  # CODEC(LZ4HC(0)) TTL _date + toIntervalDay(90),
+        Column("request_uri", String(codecs=["LZ4"])),  # CODEC(LZ4) TTL _date + toIntervalDay(30),
         Column(
             "request_uri_path", String(Modifiers(low_cardinality=True))
         ),  # LowCardinality(String) MATERIALIZED path(request_uri),
         Column(
-            "request_time", Float()
+            "request_time", Float(32)
         ),  # Float32 CODEC(Gorilla, LZ4) TTL _date + toIntervalDay(1),
-        Column("upstream_connect_time", Float()),  # Float32 CODEC(Gorilla, LZ4),
+        Column("upstream_connect_time", Float(32, codecs=["Gorilla", "LZ4"])),  # Float32 CODEC(Gorilla, LZ4),
         Column(
-            "upstream_response_time", Float()
+            "upstream_response_time", Float(32, codecs=["LZ4"])
         ),  # `upstream_response_time` Float32 CODEC(LZ4) TTL _date + toIntervalDay(1),
         Column(
-            "upstream_response_time_ms", UInt(32)
+            "upstream_response_time_ms", UInt(32, codecs=["T64", "LZ4"])
         ),  # `upstream_response_time_ms` UInt32 MATERIALIZED CAST(round(upstream_response_time * 1000, 0), 'UInt32') CODEC(T64, LZ4),
         Column(
-            "request_id", FixedString()
+            "request_id", FixedString(32, codecs=["LZ4"])
         ),  # `request_id` FixedString(32) CODEC(LZ4) TTL _date + toIntervalDay(1),
         Column(
-            "http_referrer", String()
+            "http_referrer", String(codecs=["LZ4HC(0)"])
         ),  # `http_referrer` String CODEC(LZ4HC(0)) TTL _date + toIntervalDay(30),
         Column(
             "remote_user", String(Modifiers(low_cardinality=True))
         ),  # `remote_user` LowCardinality(String),
         Column(
-            "host", String(Modifiers(low_cardinality=True))
+            "host", String(Modifiers(low_cardinality=True, codecs=["ZSTD(1)"]))
         ),  # `host` LowCardinality(String) CODEC(ZSTD(1)),
         Column(
             "http_host", String(Modifiers(low_cardinality=True))
         ),  # `http_host` LowCardinality(String),
         Column(
-            "http_user_agent", String(Modifiers(low_cardinality=True))
+            "http_user_agent", String(Modifiers(low_cardinality=True, codecs=["LZ4"]))
         ),  # `http_user_agent` LowCardinality(String) CODEC(LZ4),
         Column(
             "request_completion", String(Modifiers(low_cardinality=True))
@@ -125,14 +125,14 @@ class Migration(migration.ClickhouseNodeMigration):
         Column(
             "statsd_path", String(Modifiers(low_cardinality=True))
         ),  # `statsd_path` LowCardinality(String),
-        Column("remote_addr", IPv4()),  # `remote_addr` IPv4 CODEC(ZSTD(1)),
+        Column("remote_addr", IPv4(codecs=["ZSTD(1)"])),  # `remote_addr` IPv4 CODEC(ZSTD(1)),
         Column(
-            "request_time_ms", UInt(32)
+            "request_time_ms", UInt(32, codecs=["ZSTD(1)"])
         ),  # `request_time_ms` UInt32 MATERIALIZED CAST(round(request_time * 1000, 0), 'UInt32') CODEC(ZSTD(1)),
         Column(
             "upstream_name", String(Modifiers(low_cardinality=True))
         ),  # `upstream_name` LowCardinality(String) DEFAULT CAST('', 'LowCardinality(String)'),
         Column(
-            "upstream_remote_address", String()
+            "upstream_remote_address", String(codecs=["LZ4"])
         ),  # `upstream_remote_address` String CODEC(LZ4) TTL _date + toIntervalDay(1),
     ]
