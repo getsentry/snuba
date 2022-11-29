4ed33eda51046a6e0f009a4cfefb0ce08033932b
Colton Allen <colton.allen@sentry.io>
2022-11-29 08:28:36 -0600

    Add migration

---
+++ snuba/snuba_migrations/replays/0007_add_replay_type_column.py
@@ -0,0 +1,74 @@
+from typing import List, Sequence, Tuple
+
+from snuba.clickhouse.columns import Column, Enum
+from snuba.clusters.storage_sets import StorageSetKey
+from snuba.migrations import migration, operations
+from snuba.migrations.columns import MigrationModifiers as Modifiers
+
+new_columns: Sequence[Tuple[Column[Modifiers], str]] = [
+    (
+        Column(
+            "replay_type",
+            Enum([("session", 1), ("error", 2)], Modifiers(nullable=True)),
+        ),
+        "replay_id",
+    )
+]
+
+
+class Migration(migration.ClickhouseNodeMigrationLegacy):
+    blocking = False
+
+    def forwards_local(self) -> Sequence[operations.SqlOperation]:
+        ops: List[operations.SqlOperation] = []
+
+        for column, after in new_columns:
+            ops.append(
+                operations.AddColumn(
+                    storage_set=StorageSetKey.REPLAYS,
+                    table_name="replays_local",
+                    column=column,
+                    after=after,
+                )
+            )
+
+        return ops
+
+    def backwards_local(self) -> Sequence[operations.SqlOperation]:
+        ops: List[operations.SqlOperation] = []
+
+        for column, _ in reversed(new_columns):
+            ops.append(
+                operations.DropColumn(
+                    StorageSetKey.REPLAYS, "replays_local", column.name
+                )
+            )
+
+        return ops
+
+    def forwards_dist(self) -> Sequence[operations.SqlOperation]:
+        ops: List[operations.SqlOperation] = []
+
+        for column, after in new_columns:
+            ops.append(
+                operations.AddColumn(
+                    storage_set=StorageSetKey.REPLAYS,
+                    table_name="replays_dist",
+                    column=column,
+                    after=after,
+                )
+            )
+
+        return ops
+
+    def backwards_dist(self) -> Sequence[operations.SqlOperation]:
+        ops: List[operations.SqlOperation] = []
+
+        for column, _ in reversed(new_columns):
+            ops.append(
+                operations.DropColumn(
+                    StorageSetKey.REPLAYS, "replays_dist", column.name
+                )
+            )
+
+        return ops
