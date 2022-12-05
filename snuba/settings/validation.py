56814ec59ee35e1d63518a30bdb2910098d4b7fb
Gilbert Szeto <gilbert.szeto@sentry.io>
2022-12-05 13:43:17 -0800

    add topic to test validation

--- snuba/settings/validation.py
+++ snuba/settings/validation.py
@@ -58,6 +58,7 @@ def validate_settings(locals: Mapping[str, Any]) -> None:
         "snuba-attribution",
         "profiles-call-tree",
         "ingest-replay-events",
+        "search-issues",
         "snuba-replay-events",
         "snuba-dead-letter-replays",
         "snuba-generic-metrics",
@@ -66,6 +67,7 @@ def validate_settings(locals: Mapping[str, Any]) -> None:
         "snuba-dead-letter-generic-metrics",
         "snuba-dead-letter-sessions",
         "snuba-dead-letter-metrics",
+        "snuba-dead-letter-search-issues",
     }
 
     for key in locals["KAFKA_TOPIC_MAP"].keys():
