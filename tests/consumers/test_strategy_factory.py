facc3feb9ed371bc855e63434896480220ebf57b
Lyn <lyn.nagara@gmail.com>
2022-12-13 12:26:19 -0800

    no need to mock and advance time in test anymore

--- tests/consumers/test_strategy_factory.py
+++ tests/consumers/test_strategy_factory.py
@@ -1,17 +1,13 @@
-from datetime import datetime, timedelta
-from typing import Any
-from unittest.mock import Mock, patch
+from datetime import datetime
+from unittest.mock import Mock
 
 from arroyo.types import BrokerValue, Message, Partition, Topic
 
 from snuba.consumers.strategy_factory import ProduceCommitLog
 
 
-@patch("time.time")
-def test_produce_commit_log(time_mock: Any) -> None:
-    start = datetime(2022, 12, 12)
-    time_mock.return_value = start.timestamp()
-
+def test_produce_commit_log() -> None:
+    now = datetime.now()
     producer = Mock()
     main_topic = Topic("main-topic")
     partition = Partition(main_topic, 0)
@@ -24,13 +20,5 @@ def test_produce_commit_log(time_mock: Any) -> None:
 
     strategy.submit(Message(BrokerValue(0, partition, 1, now)))
 
-    assert commit_func.call_count == 0
-    assert producer.produce.call_count == 0
-
-    # Advance time by 1 second
-    now = start + timedelta(seconds=1)
-    time_mock.return_value = now.timestamp()
-
-    strategy.poll()
     assert commit_func.call_count == 1
     assert producer.produce.call_count == 1
