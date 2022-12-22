e9cab5976f57ed767140f5c146313da48c72b79f
Lyn Nagara <lyn.nagara@gmail.com>
2022-12-22 12:33:47 -0800

    feat: Consumer schema validation (#3553)
    The consumer entrypoint now has a `--validate-schema` option which (if passed) will perform schema validation on every message where a schema is published for that topic.
    
    The `get_schema()` function is supposed to return either the json schema or none if a schema cannot be found for that topic. Currently this function is just a placeholder that never returns a schemas. Eventually it will be updated to return the schema published from `https://github.com/getsentry/sentry-kafka-topics` package.
    
    The insert API used in test mode will always perform schema validation.

--- snuba/cli/consumer.py
+++ snuba/cli/consumer.py
@@ -110,6 +110,7 @@ logger = logging.getLogger(__name__)
     "--output-block-size",
     type=int,
 )
+@click.option("--validate-schema", is_flag=True, default=False)
 @click.option(
     "--profile-path", type=click.Path(dir_okay=True, file_okay=False, exists=True)
 )
@@ -133,6 +134,7 @@ def consumer(
     input_block_size: Optional[int],
     output_block_size: Optional[int],
     log_level: Optional[str] = None,
+    validate_schema: bool,
     profile_path: Optional[str] = None,
 ) -> None:
 
@@ -179,6 +181,7 @@ def consumer(
         metrics=metrics,
         profile_path=profile_path,
         stats_callback=stats_callback,
+        validate_schema=validate_schema,
         parallel_collect=parallel_collect,
         slice_id=slice_id,
     )
