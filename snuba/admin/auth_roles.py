bcf7142015855b2c8285857f2b50d5680161245a
meredith <meredith@sentry.io>
2023-01-11 15:59:41 -0800

    offshoot attempt

--- snuba/admin/auth_roles.py
+++ snuba/admin/auth_roles.py
@@ -1,43 +1,104 @@
 from dataclasses import dataclass
 from enum import Enum
-from typing import Set
+from typing import Sequence, Set
 
 from snuba import settings
 
 
-class Action(Enum):
-    EXECUTE = "execute"
-    LIMITED_EXECUTE = "limited_execute"
-    READ = "read"
+class Category(Enum):
+    MIGRATIONS = "migrations"
+
+
+@dataclass(frozen=True)
+class Resource:
+    name: str
+    category: Category
+
+
+class Action:
+    def __init__(self, resources: Sequence[Resource]) -> None:
+        self._resources = self.validated_resources(resources)
+
+    def validated_resources(self, resources: Sequence[Resource]) -> Sequence[Resource]:
+        raise NotImplementedError
+
+
+class MigrationAction(Action):
+    def validated_resources(self, resources: Sequence[Resource]) -> Sequence[Resource]:
+        invalid_resources = [
+            resource.name
+            for resource in resources
+            if resource.category != Category.MIGRATIONS
+        ]
+        if invalid_resources:
+            raise Exception(
+                f"These resources aren't cateogry Migrations {invalid_resources}"
+            )
+        return resources
+
+
+class ExecuteAllAction(MigrationAction):
+    pass
+
+
+class ExecuteNonBlockingAction(MigrationAction):
+    pass
+
+
+class ExecuteNoneAction(MigrationAction):
+    pass
+
+
+# class MigrationAction:
+#     EXECUTE_ALL = "execute_all"
+#     EXECUTE_NONBLOCKING = "execute_nonblocking"
+#     EXECUTE_NONE = "execute_none"
 
 
 # todo, shoudln't need .keys() once ADMIN_ALLOWED_MIGRATION_GROUPS is a set not dict
-MIGRATIONS_RESOURCES = set(
-    [f"migrations.{group}" for group in settings.ADMIN_ALLOWED_MIGRATION_GROUPS.keys()]
-)
+MIGRATIONS_RESOURCES = {
+    group: Resource(group, Category.MIGRATIONS)
+    for group in settings.ADMIN_ALLOWED_MIGRATION_GROUPS.keys()
+}
 
 
 @dataclass(frozen=True)
 class Role:
     name: str
-    resources: Set[str]
     actions: Set[Action]
 
 
 DEFAULT_ROLES = [
     Role(
         name="MigrationsReader",
-        resources=MIGRATIONS_RESOURCES,
-        actions={Action.READ},
+        actions={ExecuteNoneAction(resources=list(MIGRATIONS_RESOURCES.values()))},
     ),
     Role(
         name="MigrationsLimitedExecutor",
-        resources=MIGRATIONS_RESOURCES,
-        actions={Action.LIMITED_EXECUTE},
+        actions={
+            ExecuteNonBlockingAction(resources=list(MIGRATIONS_RESOURCES.values()))
+        },
     ),
     Role(
         name="TestMigrationsExecutor",
-        resources={"migrations.test_migration"},
-        actions={Action.EXECUTE},
+        actions={ExecuteAllAction(resources=[MIGRATIONS_RESOURCES["test_migration"]])},
     ),
 ]
+
+# DEFAULT_ROLES = [
+#     Role(
+#         name="MigrationsReader",
+#         resources=MIGRATIONS_RESOURCES,
+#         actions={Action.READ},
+#     ),
+#     Role(
+#         name="MigrationsLimitedExecutor",
+#         resources=MIGRATIONS_RESOURCES,
+#         actions={Action.LIMITED_EXECUTE},
+#     ),
+#     Role(
+#         name="TestMigrationsExecutor",
+#         resources={"migrations.test_migration"},
+#         actions={Action.EXECUTE},
+#     ),
+# ]
