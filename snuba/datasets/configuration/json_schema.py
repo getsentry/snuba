c75721c75848993130b4d0033550a7b6f1ce757e
Evan Hicks <evanh@users.noreply.github.com>
2023-01-16 15:37:50 -0500

    fix(config) Support join relationships in configuration (#3594)
    Add support to the schemas to allow entities to specify join relationships.
    Also add a test for the join relationship parser.

--- snuba/datasets/configuration/json_schema.py
+++ snuba/datasets/configuration/json_schema.py
@@ -442,6 +442,50 @@ ENTITY_SUBSCRIPTION_VALIDATORS = {
     },
 }
 
+ENTITY_JOIN_RELATIONSHIPS = {
+    "type": "object",
+    "patternProperties": {
+        "^.*$": {
+            "type": "object",
+            "description": "The join relationship. The key for this relationship is how the relationship is specified in queries (`MATCH x -[key]-> y`)",
+            "properties": {
+                "rhs_entity": {
+                    "type": "string",
+                    "description": "The entity key of the rhs entity to join with",
+                },
+                "columns": {
+                    "type": "array",
+                    "items": {
+                        "type": "array",
+                        "prefixItems": [
+                            {"type": "string"},
+                            {"type": "string"},
+                        ],
+                    },
+                    "description": "A sequence of tuples of columns to join on, in the form (left, right)",
+                },
+                "join_type": {
+                    "type": "string",
+                    "description": "The type of join that can be performed (either 'left' or 'inner'",
+                },
+                "equivalences": {
+                    "type": "array",
+                    "items": {
+                        "type": "array",
+                        "prefixItems": [
+                            {"type": "string"},
+                            {"type": "string"},
+                        ],
+                    },
+                    "description": "Tracking columns in the two entities that are not part of the join key but are still equivalent",
+                },
+            },
+            "required": ["rhs_entity", "columns", "join_type"],
+            "additionalProperties": False,
+        },
+    },
+}
+
 # Full schemas:
 
 V1_WRITABLE_STORAGE_SCHEMA = {
@@ -512,6 +556,7 @@ V1_ENTITY_SCHEMA = {
                 "description": "Name of a ReadableStorage class which provides an abstraction to read from a table or a view in ClickHouse"
             },
         },
+        "join_relationships": ENTITY_JOIN_RELATIONSHIPS,
         "writable_storage": {
             "type": ["string", "null"],
             "description": "Name of a WritableStorage class which provides an abstraction to write to a table in ClickHouse",
