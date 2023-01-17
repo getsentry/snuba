bd0ba60a0a890b0a2e80d200305ae9bae260f1ec
Dalitso Banda <dabanda@microsoft.com>
2023-01-17 09:29:48 -0800

    add errors indexes

--- snuba/migrations/groups.py
+++ snuba/migrations/groups.py
@@ -119,6 +119,7 @@ class EventsLoader(DirectoryLoader):
             "0014_backfill_errors",
             "0015_truncate_events",
             "0016_drop_legacy_events",
+            "0017_errors_add_indexes",
         ]
 
 
