b1b4c72e3a338cb2104bd39bf325efcb093bb79e
Tony Xiao <txiao@sentry.io>
2023-01-17 16:42:29 -0500

    make nullable

--- snuba/snuba_migrations/transactions/0018_transactions_add_profile_id.py
+++ snuba/snuba_migrations/transactions/0018_transactions_add_profile_id.py
@@ -3,6 +3,7 @@ from typing import Sequence
 from snuba.clickhouse.columns import UUID, Column
 from snuba.clusters.storage_sets import StorageSetKey
 from snuba.migrations import migration, operations
+from snuba.migrations.columns import MigrationModifiers as Modifiers
 from snuba.migrations.operations import OperationTarget
 
 
@@ -14,7 +15,7 @@ class Migration(migration.ClickhouseNodeMigration):
             operations.AddColumn(
                 storage_set=StorageSetKey.TRANSACTIONS,
                 table_name=table_name,
-                column=Column("profile_id", UUID()),
+                column=Column("profile_id", UUID(Modifiers(nullable=True))),
                 after="app_start_type",
                 target=target,
             )
