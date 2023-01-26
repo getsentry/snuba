59873cb24dfe756db38962110f591e8a74fbb031
Riya Chakraborty <riya.chakraborty@sentry.io>
2023-01-26 12:44:33 -0600

    Use new migration class

--- snuba/snuba_migrations/metrics/0035_metrics_raw_timeseries_id.py
+++ snuba/snuba_migrations/metrics/0035_metrics_raw_timeseries_id.py
@@ -3,9 +3,11 @@ from typing import Sequence
 from snuba.clickhouse.columns import Column, UInt
 from snuba.clusters.storage_sets import StorageSetKey
 from snuba.migrations import migration, operations
+from snuba.migrations.operations import OperationTarget
 
 
-class Migration(migration.ClickhouseNodeMigrationLegacy):
+class Migration(migration.ClickhouseNodeMigration):
+
     """
     Adds the timeseries_id column to the metrics raw table so we can add
     a sharding key for each timeseries and scale the release health cluster.
@@ -13,36 +15,30 @@ class Migration(migration.ClickhouseNodeMigrationLegacy):
 
     blocking = False
 
-    def __forward_migrations(
-        self, table_name: str
-    ) -> Sequence[operations.SqlOperation]:
+    def forwards_ops(self) -> Sequence[operations.SqlOperation]:
         return [
             operations.AddColumn(
                 storage_set=StorageSetKey.METRICS,
                 table_name=table_name,
                 column=Column("timeseries_id", UInt(32)),
-            ),
+                target=target,
+            )
+            for table_name, target in [
+                ("metrics_raw_v2_local", OperationTarget.LOCAL),
+                ("metrics_raw_v2_dist", OperationTarget.DISTRIBUTED),
+            ]
         ]
 
-    def __backward_migrations(
-        self, table_name: str
-    ) -> Sequence[operations.SqlOperation]:
+    def backwards_ops(self) -> Sequence[operations.SqlOperation]:
         return [
             operations.DropColumn(
                 storage_set=StorageSetKey.METRICS,
                 table_name=table_name,
                 column_name="timeseries_id",
-            ),
+                target=target,
+            )
+            for table_name, target in [
+                ("metrics_raw_v2_dist", OperationTarget.DISTRIBUTED),
+                ("metrics_raw_v2_local", OperationTarget.LOCAL),
+            ]
         ]
-
-    def forwards_local(self) -> Sequence[operations.SqlOperation]:
-        return self.__forward_migrations("metrics_raw_v2_local")
-
-    def backwards_local(self) -> Sequence[operations.SqlOperation]:
-        return self.__backward_migrations("metrics_raw_v2_local")
-
-    def forwards_dist(self) -> Sequence[operations.SqlOperation]:
-        return self.__forward_migrations("metrics_raw_v2_dist")
-
-    def backwards_dist(self) -> Sequence[operations.SqlOperation]:
-        return self.__backward_migrations("metrics_raw_v2_dist")
