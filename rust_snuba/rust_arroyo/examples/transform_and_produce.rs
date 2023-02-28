8ad7c4c78cc8f77c7ec2ad8d3ce11698a41cb7f3
Dalitso Banda <dabanda@microsoft.com>
2023-02-27 19:06:49 -0800

    add produce transform example

--- rust_snuba/rust_arroyo/examples/transform_and_produce.rs
+++ rust_snuba/rust_arroyo/examples/transform_and_produce.rs
@@ -1,6 +1,10 @@
+// An example of using the Transform and Produce strategies together.
+// inspired by https://github.com/getsentry/arroyo/blob/main/examples/transform_and_produce/script.py
 extern crate rust_arroyo;
 
+use rdkafka::message::ToBytes;
 use rust_arroyo::backends::kafka::config::KafkaConfig;
+use rust_arroyo::backends::kafka::producer::KafkaProducer;
 use rust_arroyo::backends::kafka::types::KafkaPayload;
 use rust_arroyo::backends::kafka::KafkaConsumer;
 use rust_arroyo::processing::strategies::produce::Produce;
@@ -9,7 +13,7 @@ use rust_arroyo::processing::strategies::{
     CommitRequest, MessageRejected, ProcessingStrategy, ProcessingStrategyFactory, InvalidMessage,
 };
 use rust_arroyo::processing::StreamProcessor;
-use rust_arroyo::types::{Message, Partition, Position, Topic};
+use rust_arroyo::types::{Message, Partition, Position, Topic, TopicOrPartition};
 use std::collections::HashMap;
 use std::time::Duration;
 
@@ -62,20 +66,25 @@ impl ProcessingStrategyFactory<KafkaPayload> for TestFactory {
     }
 }
 
-// struct HashPasswordAndProduceStrategy {
-//     partitions: HashMap<Partition, Position>,
-// }
+fn reverse_string(value: KafkaPayload) -> Result<KafkaPayload, InvalidMessage> {
+    let payload = value.payload.unwrap();
+    let str_payload = std::str::from_utf8(&payload).unwrap();
+    println!("transforming value: {:?}", str_payload);
 
-fn identity(value: KafkaPayload) -> Result<String, InvalidMessage> {
-    print!("transforming value: {:?}", value.payload);
-    Ok("password hash".to_string())
+    let result_str = str_payload.chars().rev().collect::<String>();
+
+    let result = KafkaPayload {
+        payload: Some(result_str.to_bytes().to_vec()),
+        ..value
+    };
+    Ok(result)
 }
 struct Noop {}
-impl ProcessingStrategy<String> for Noop {
+impl ProcessingStrategy<KafkaPayload> for Noop {
     fn poll(&mut self) -> Option<CommitRequest> {
         None
     }
-    fn submit(&mut self, _message: Message<String>) -> Result<(), MessageRejected> {
+    fn submit(&mut self, _message: Message<KafkaPayload>) -> Result<(), MessageRejected> {
         Ok(())
     }
     fn close(&mut self) {}
@@ -85,19 +94,24 @@ impl ProcessingStrategy<String> for Noop {
     }
 }
 
-struct HashPasswordAndProduceStrategyFactory {}
-impl ProcessingStrategyFactory<KafkaPayload> for HashPasswordAndProduceStrategyFactory {
-    fn create(&self) -> Box<dyn ProcessingStrategy<KafkaPayload>> {
-        let mut hashPasswordAndProduceStrategy = Transform {
-            function: identity,
-            next_step: Box::new(Produce::new(producer, next_step, topic));
-        };
-        Box::new(hashPasswordAndProduceStrategy)
-    }
-}
+
 
 fn main() {
-
+    struct HashPasswordAndProduceStrategyFactory {
+        config: KafkaConfig,
+        topic: Topic,
+    }
+    impl ProcessingStrategyFactory<KafkaPayload> for HashPasswordAndProduceStrategyFactory {
+        fn create(&self) -> Box<dyn ProcessingStrategy<KafkaPayload>> {
+            let producer = KafkaProducer::new(self.config.clone());
+            let topic = TopicOrPartition::Topic(self.topic.clone());
+            let hash_password_and_produce_strategy = Transform {
+                function: reverse_string,
+                next_step: Box::new(Produce::new(producer, Box::new(Noop {}), topic ))
+            };
+            Box::new(hash_password_and_produce_strategy)
+        }
+    }
 
     // TODO we can get these from the yaml file
     let config = KafkaConfig::new_consumer_config(
@@ -109,19 +123,15 @@ fn main() {
     );
 
 
-    let consumer = Box::new(KafkaConsumer::new(config));
-    let topic = Topic {
-        name: "test_static".to_string(),
-    };
-
-    let mut processor = StreamProcessor::new(consumer, Box::new(HashPasswordAndProduceStrategyFactory {}));
-    processor.subscribe(topic);
-    loop {
-        println!("running once");
-        let _ = processor.run_once();
-
-        //sleep
-        std::thread::sleep(std::time::Duration::from_millis(10000));
-
-    }
+    let consumer = Box::new(KafkaConsumer::new(config.clone()));
+    let mut processor = StreamProcessor::new(consumer,
+        Box::new(HashPasswordAndProduceStrategyFactory {
+                            config: config.clone(),
+                            topic: Topic { name: "test_out".to_string() }
+                         }));
+    processor.subscribe(Topic {
+        name: "test_in".to_string(),
+    });
+    println!("running processor. transforming from test_in to test_out");
+    processor.run();
 }
