3aae8959a2d9c6f4fbb794d7a1957c311b508707
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2023-02-28 12:30:35 +0100

    fix eventstream schema based on single message

--- tests/consumers/test_schemas.py
+++ tests/consumers/test_schemas.py
@@ -28,3 +28,51 @@ def test_metrics() -> None:
         },
     }
     assert codec.decode(json.dumps(payload).encode("utf8"), validate=True) == payload
+
+
+def test_errors_eventstream():
+    codec = get_json_codec(Topic.EVENTS)
+    payload = [
+        2,
+        "insert",
+        {
+            "data": {
+                "environment": "production",
+                "event_id": "9cdc4c32dff14fbbb012b0aa9e908126",
+                "level": "error",
+                "logger": "",
+                "platform": "javascript",
+                "received": 1677512412.437706,
+                "release": "123abc",
+                "timestamp": 1677512412.223,
+                "type": "error",
+                "version": "7",
+            },
+            "datetime": "2023-02-27T15:40:12.223000Z",
+            "event_id": "9cdc4c32dff14fbbb012b0aa9e908126",
+            "group_id": 124,
+            "group_ids": [124],
+            "message": "hello world",
+            "organization_id": 123,
+            "platform": "javascript",
+            "primary_hash": "061cf02b26374d108694d6643a7a2f4e",
+            "project_id": 6036610,
+        },
+        {
+            "group_states": [
+                {
+                    "id": "124",
+                    "is_new": False,
+                    "is_new_group_environment": False,
+                    "is_regression": False,
+                }
+            ],
+            "is_new": False,
+            "is_new_group_environment": False,
+            "is_regression": False,
+            "queue": "post_process_errors",
+            "skip_consume": False,
+        },
+    ]
+
+    assert codec.decode(json.dumps(payload).encode("utf8"), validate=True) == payload
