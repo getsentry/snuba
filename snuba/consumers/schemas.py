9467318a2f3175cae9b4f3612cf6b2d0044aacf6
Lyn Nagara <lyn.nagara@gmail.com>
2023-03-06 06:36:11 -0800

    build: Update sentry-kafka-schemas to 0.0.3 (#3823)
    Co-authored-by: Markus Unterwaditzer <markus-tarpit+git@unterwaditzer.net>

--- snuba/consumers/schemas.py
+++ snuba/consumers/schemas.py
@@ -1,32 +1,26 @@
+import logging
 from typing import Any, Mapping, MutableMapping, Optional
 
-# TODO: Remove type: ignore once next version sentry-kafka-schemas is published
-import sentry_kafka_schemas  # type: ignore
+import sentry_kafka_schemas
+import sentry_sdk
 from arroyo.processing.strategies.decoder.json import JsonCodec
 
 from snuba.utils.streams.topics import Topic
 
-_HARDCODED_SCHEMAS: Mapping[Topic, Mapping[str, Any]] = {
-    # Querylog JSON
-    Topic.QUERYLOG: sentry_kafka_schemas.get_schema("querylog")["schema"],
-    # Release health metrics
-    Topic.METRICS: sentry_kafka_schemas.get_schema("metrics")["schema"],
-    # Performance metrics
-    Topic.GENERIC_METRICS: sentry_kafka_schemas.get_schema("generic_metrics")["schema"],
-    # Error events
-    Topic.EVENTS: sentry_kafka_schemas.get_schema("events")["schema"],
-}
+logger = logging.getLogger(__name__)
 
 
 def get_schema(topic: Topic) -> Optional[Mapping[str, Any]]:
     """
-    This is a placeholder. Eventually the schema will be fetched from the
-    sentry-kafka-topics library when it gets published.
-
     This function returns either the schema if it is defined, or None if not.
-
     """
-    return _HARDCODED_SCHEMAS.get(topic)
+    try:
+        return sentry_kafka_schemas.get_schema(topic.value)["schema"]
+    except Exception as err:
+        with sentry_sdk.push_scope() as scope:
+            scope.set_tag("snuba_logical_topic", topic.name)
+            logger.warning(err, exc_info=True)
+        return None
 
 
 _cache: MutableMapping[Topic, JsonCodec] = {}
