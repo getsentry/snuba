0c0f103e71cda8972470b94377e8679536b69c39
Colton Allen <colton.allen@sentry.io>
2023-03-09 13:13:40 -0600

    Add migration to list

--- snuba/migrations/groups.py
+++ snuba/migrations/groups.py
@@ -198,6 +198,7 @@ class ReplaysLoader(DirectoryLoader):
             "0006_add_is_archived_column",
             "0007_add_replay_type_column",
             "0008_add_sample_rate",
+            "0009_add_dom_index_columns",
         ]
 
 
