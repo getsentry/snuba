5e98f36771263885e5348594dff57f223a90b9e2
Rahul Saini <rahulsaini805@gmail.com>
2023-03-23 15:09:10 -0700

    GET instead of MGET

--- snuba/state/sliding_windows.py
+++ snuba/state/sliding_windows.py
@@ -243,9 +243,10 @@ class RedisSlidingWindowRateLimiter:
         # (https://stackoverflow.com/a/3812600/1544347), there are no formal
         # guarantees about it.
         ordered_keys_to_fetch = list(keys_to_fetch)
-        redis_results = dict(
-            zip(ordered_keys_to_fetch, self.client.mget(ordered_keys_to_fetch))
-        )
+        p = self.client.pipeline()
+        for k in ordered_keys_to_fetch:
+            p.get(k)
+        redis_results = dict(zip(ordered_keys_to_fetch, p.execute()))
 
         results = []
 
