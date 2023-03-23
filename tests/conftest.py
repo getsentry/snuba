47536b8d248ab1cd67e8de8daefba49ec7bbbfec
volokluev <3169433+volokluev@users.noreply.github.com>
2023-03-23 16:21:43 -0700

    initialize snuba before tests run (#3913)
    if tests are run in isolation which depend on the entity factory, they will not have initialized them and get an AttributeError
    
    Also fix an unclosed file warning

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,7 @@ from snuba_sdk.legacy import json_to_snql
 
 from snuba import settings, state
 from snuba.clusters.cluster import ClickhouseClientSettings, ClickhouseCluster
+from snuba.core.initialize import initialize_snuba
 from snuba.datasets.factory import reset_dataset_factory
 from snuba.datasets.schemas.tables import WritableTableSchema
 from snuba.datasets.storages.factory import get_all_storage_keys, get_storage
@@ -24,6 +25,7 @@ def pytest_configure() -> None:
     ), "settings.TESTING is False, try `SNUBA_SETTINGS=test` or `make test`"
 
     setup_sentry()
+    initialize_snuba()
 
 
 @pytest.fixture(scope="session")
