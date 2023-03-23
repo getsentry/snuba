119b95e326c1e1394fc47c40a904f2b6587981e8
Colton Allen <colton.allen@sentry.io>
2023-03-23 13:17:57 -0500

    Update coverage

--- tests/datasets/test_replays_processor.py
+++ tests/datasets/test_replays_processor.py
@@ -724,11 +724,11 @@ class TestReplaysActionProcessor:
         assert row["trace_ids"] == []
         assert row["error_ids"] == []
         assert row["urls"] == []
-        assert row["title"] is None
+        assert row["title"] == ""
         assert row["platform"] == "javascript"
-        assert row["user"] is None
-        assert row["sdk_name"] is None
-        assert row["sdk_version"] is None
+        assert row["user"] == ""
+        assert row["sdk_name"] == ""
+        assert row["sdk_version"] == ""
         assert row["retention_days"] == 30
         assert row["partition"] == 0
         assert row["offset"] == 0
