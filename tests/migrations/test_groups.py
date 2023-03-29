fa903104fccb2a0ffbeb5835fc700f9c8105abdc
Enoch Tang <enoch.tang@sentry.io>
2023-03-29 13:04:13 -0400

    add test

--- tests/migrations/test_groups.py
+++ tests/migrations/test_groups.py
@@ -1,4 +1,11 @@
+from snuba.datasets.readiness_state import ReadinessState
 from snuba.migrations.groups import MigrationGroup, get_group_loader
+
+
+def test_load_all_migration_groups_have_readiness_state() -> None:
+    for group in MigrationGroup:
+        assert hasattr(group, "_readiness_state_")
+        assert isinstance(group.readiness_state, ReadinessState)
 
 
 def test_load_all_migrations() -> None:
