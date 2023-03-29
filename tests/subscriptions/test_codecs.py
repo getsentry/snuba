d8459a724439c2939573ae16ffdfa6db6b7c3f82
Lyn <lyn.nagara@gmail.com>
2023-03-28 18:33:48 -0700

    totals is not supposed to be there

--- tests/subscriptions/test_codecs.py
+++ tests/subscriptions/test_codecs.py
@@ -136,7 +136,6 @@ def test_subscription_task_result_encoder() -> None:
     result: Result = {
         "meta": [{"type": "UInt64", "name": "count"}],
         "data": [{"count": 1}],
-        "totals": {},
         "profile": {},
         "trace_output": "",
     }
@@ -222,7 +221,6 @@ def test_metrics_subscription_task_result_encoder(
             {"name": "tags[3]", "type": "UInt64"},
             {"name": "value", "type": "Float64"},
         ],
-        "totals": {},
         "profile": {},
         "trace_output": "",
     }
