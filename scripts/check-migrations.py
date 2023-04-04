45cd9e5f110b2600b1970a47f0923a46ced42f98
Dalitso Banda <dalitso.banda@sentry.io>
2023-04-04 15:22:01 -0700

    allow test and groups.py with migrations

--- scripts/check-migrations.py
+++ scripts/check-migrations.py
@@ -2,23 +2,37 @@
 import argparse
 import subprocess
 from shutil import ExecError
+from typing import Sequence
 
 """
 This script is meant to be run in CI to check that migrations changes are
 not coupled with other changes.
 """
 
+# changes to these files here are allowed to be coupled with migrations
+ALLOWED_MIGRATIONS_GLOBS = [
+    "snuba/migrations/groups.py",
+    "tests",
+    "test_distributed_migrations",
+    "test_initialization",
+    "docs",
+    "*.md",
+]
+
+# changes to files here are migrations changes
+MIGRATIONS_GLOBS = ["snuba/snuba_migrations/*/[0-9]*.py"]
+
 
 class CoupledMigrations(Exception):
     pass
 
 
-def _get_migration_changes(workdir: str = ".", to: str = "origin/master") -> str:
-    return _get_changes("snuba/snuba_migrations/*/[0-9]*.py", workdir, to)
+def _get_migration_changes(workdir: str, to: str) -> str:
+    return _get_changes(MIGRATIONS_GLOBS, workdir, to)
 
 
-def _get_changes(glob: str, workdir: str = ".", to: str = "origin/master") -> str:
-    migrations_changes = subprocess.run(
+def _get_changes(globs: Sequence[str], workdir: str, to: str) -> str:
+    changes = subprocess.run(
         [
             "git",
             "diff",
@@ -26,23 +40,24 @@ def _get_changes(glob: str, workdir: str = ".", to: str = "origin/master") -> st
             "--name-only",
             to,
             "--",
-            glob,
+            *globs,
+            *[f":(exclude){allowed}" for allowed in ALLOWED_MIGRATIONS_GLOBS],
         ],
         stdout=subprocess.PIPE,
         text=True,
         cwd=workdir,
     )
-    if migrations_changes.returncode != 0:
-        raise ExecError(migrations_changes.stdout)
+    if changes.returncode != 0:
+        raise ExecError(changes.stdout)
     else:
-        return migrations_changes.stdout
+        return changes.stdout
 
 
 def main(to: str = "origin/master", workdir: str = ".") -> None:
     migrations_changes = _get_migration_changes(workdir, to)
     has_migrations = len(migrations_changes.splitlines()) > 0
     if has_migrations:
-        all_changes = _get_changes("*", workdir, to)
+        all_changes = _get_changes(["*"], workdir, to)
         if all_changes != migrations_changes:
             raise CoupledMigrations(
                 f"Migration changes are coupled with other changes.\n\nMigrations changes: \n{migrations_changes}\n\nAll changes: \n{all_changes}"
