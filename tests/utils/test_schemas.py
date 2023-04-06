dad8edecfe43f2e7f08f772f8d9ebf05c598eb37
Volo Kluev <volo.kluev@sentry.io>
2023-04-06 14:31:16 -0700

    mypy

--- tests/utils/test_schemas.py
+++ tests/utils/test_schemas.py
@@ -1,6 +1,6 @@
 from snuba.utils.schemas import Column, ColumnSet, Nested, String
 
 
-def test_contains_nested():
+def test_contains_nested() -> None:
     c = ColumnSet([Column("hags", Nested([("key", String()), ("value", String())]))])
     assert "hags" in c
