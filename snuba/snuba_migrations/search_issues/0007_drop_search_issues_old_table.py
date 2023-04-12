eafa918962fb45b90ad9f323897123b39da61712
Gilbert Szeto <gilbert.szeto@sentry.io>
2023-04-11 18:19:07 -0700

    drop dist before local

--- snuba/snuba_migrations/search_issues/0007_drop_search_issues_old_table.py
+++ snuba/snuba_migrations/search_issues/0007_drop_search_issues_old_table.py
@@ -20,8 +20,8 @@ class Migration(migration.ClickhouseNodeMigration):
                 target=params[1],
             )
             for params in [
-                ("search_issues_local", OperationTarget.LOCAL),
                 ("search_issues_dist", OperationTarget.DISTRIBUTED),
+                ("search_issues_local", OperationTarget.LOCAL),
             ]
         ]
 
