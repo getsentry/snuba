ae4e255ae0942746bc77dc727ae6b0066f0d7276
Gilbert Szeto <gilbert.szeto@sentry.io>
2023-04-11 17:41:08 -0700

    params

--- snuba/snuba_migrations/search_issues/0007_drop_search_issues_old_table.py
+++ snuba/snuba_migrations/search_issues/0007_drop_search_issues_old_table.py
@@ -68,14 +68,13 @@ class Migration(migration.ClickhouseNodeMigration):
         return [
             operations.DropTable(
                 storage_set=StorageSetKey.SEARCH_ISSUES,
-                table_name="search_issues_local",
-                target=OperationTarget.LOCAL,
-            ),
-            operations.DropTable(
-                storage_set=StorageSetKey.SEARCH_ISSUES,
-                table_name="search_issues_dist",
-                target=OperationTarget.DISTRIBUTED,
-            ),
+                table_name=params[0],
+                target=params[1],
+            )
+            for params in [
+                ("search_issues_local", OperationTarget.LOCAL),
+                ("search_issues_dist", OperationTarget.DISTRIBUTED),
+            ]
         ]
 
     def backwards_ops(self) -> Sequence[SqlOperation]:
