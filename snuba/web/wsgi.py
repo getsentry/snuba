2e157456ca3d2f38248241f7fd1ec3aebf45d56e
Volo Kluev <volo.kluev@sentry.io>
2023-04-13 13:12:22 -0700

    sleep before init

--- snuba/web/wsgi.py
+++ snuba/web/wsgi.py
@@ -1,8 +1,12 @@
+import time
+
 from snuba.core.initialize import initialize_snuba
 from snuba.environment import setup_logging, setup_sentry
 
 setup_logging()
 setup_sentry()
 initialize_snuba()
+print("sleepin")
+time.sleep(100)
 
 from snuba.web.views import application  # noqa
