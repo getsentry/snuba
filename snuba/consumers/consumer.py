07538099aa32e24631cfce4ca0649824313d04dd
Markus Unterwaditzer <markus-tarpit+git@unterwaditzer.net>
2023-04-15 00:58:30 +0200

    flip default

--- snuba/consumers/consumer.py
+++ snuba/consumers/consumer.py
@@ -536,10 +536,10 @@ def process_message(
             scope.set_tag("invalid_message", "true")
             logger.warning(err, exc_info=True)
             value = message.value
-            if state.get_config(f"disable_new_dlq_{snuba_logical_topic.name}", 0):
-                return None
+            if state.get_config(f"enable_new_dlq_{snuba_logical_topic.name}", 0):
+                raise InvalidMessage(value.partition, value.offset) from err
 
-            raise InvalidMessage(value.partition, value.offset) from err
+            return None
 
     if isinstance(result, InsertBatch):
         return BytesInsertBatch(
