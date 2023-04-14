6f0053f8bce2b151d6203a0cbcd9376a81454bdf
Dalitso Banda <dalitso.banda@sentry.io>
2023-04-14 12:38:51 -0700

    feat(CI): add migrations check to main pipeline (#4013)
    * add migrations check to main pipeline
    
    * deep clone to get more refs

--- scripts/fetch_service_refs.py
+++ scripts/fetch_service_refs.py
@@ -19,7 +19,11 @@ MAX_FETCHES = 100
 def pipeline_passed(pipeline: Dict[str, Any]) -> bool:
     # stage["result"] isn't populated if it isn't run
     # the other possible statuses are Unknown (not run yet), Cancelled, Failed
-    return all(stage["status"] == "Passed" for stage in pipeline["stages"])
+    return all(
+        stage["status"] == "Passed"
+        for stage in pipeline["stages"]
+        if stage["name"] != "migrate"
+    )
 
 
 # print the most recent passing sha for a repo
