ad24610921372d27b6c5c987c3d5599897329888
volokluev <3169433+volokluev@users.noreply.github.com>
2023-04-14 12:02:16 -0700

    feat(CapMan): Errors Allocation Policy V0 (fixed) (#4016)
    Same as #3999 except we make sure the allocation policy is pickleable (and all future allocation policies are pickleable)
    
    
    ### The Policy
    
    For every organization, keep track of how many bytes they've scanned in the last 10 minutes, when they reach a limit, throttle them to one thread
    
    ### How to turn it on/off
    
    In runtime config:
    
    ```
    # completely bypasses the code path
    SET ErrorsAllocationPolicy.is_active 0
    ```
    
    
    ```
    # code runs and emits metrics but no action is taken
    SET ErrorsAllocationPolicy.is_enforced 0
    ```
    
    ```
    # how many bytes the org can scan in ten minutes before being throttled
    SET ErrorsAllocationPolicy.org_scan_limit 42069
    ```
    
    ### Intent
    
    This is not meant to be the final policy, this is just a quick way to get something working end to end and then iterate on it. Most of this code will be rewritten

---
+++ tests/query/allocation_policies/test_errors_allocation_policy.py
@@ -0,0 +1,150 @@
+from __future__ import annotations
+
+import pytest
+
+from snuba.clusters.storage_sets import StorageSetKey
+from snuba.query.allocation_policies import (
+    AllocationPolicyViolation,
+    QueryResultOrError,
+)
+from snuba.query.allocation_policies.errors_allocation_policy import (
+    _ORG_LESS_REFERRERS,
+    ErrorsAllocationPolicy,
+)
+from snuba.state import set_config
+from snuba.web import QueryResult
+
+ORG_SCAN_LIMIT = 1000
+THROTTLED_THREAD_NUMBER = 1
+MAX_THREAD_NUMBER = 400
+
+
+@pytest.fixture(scope="function")
+def policy():
+    policy = ErrorsAllocationPolicy(
+        storage_set_key=StorageSetKey("errors"),
+        required_tenant_types=["referrer", "organization_id"],
+    )
+    return policy
+
+
+def _configure_policy(policy):
+    set_config(f"{policy.rate_limit_prefix}.is_active", True)
+    set_config(f"{policy.rate_limit_prefix}.is_enforced", True)
+    set_config(f"{policy.rate_limit_prefix}.org_limit_bytes_scanned", ORG_SCAN_LIMIT)
+    set_config(
+        f"{policy.rate_limit_prefix}.throttled_thread_number", THROTTLED_THREAD_NUMBER
+    )
+    set_config("query_settings/max_threads", MAX_THREAD_NUMBER)
+
+
+@pytest.mark.redis_db
+def test_consume_quota(policy: ErrorsAllocationPolicy) -> None:
+    # 1. if you scan the limit of bytes, you get throttled to one thread
+    _configure_policy(policy)
+    tenant_ids: dict[str, int | str] = {
+        "organization_id": 123,
+        "referrer": "some_referrer",
+    }
+    allowance = policy.get_quota_allowance(tenant_ids)
+    assert allowance.can_run
+    assert allowance.max_threads == MAX_THREAD_NUMBER
+    policy.update_quota_balance(
+        tenant_ids,
+        QueryResultOrError(
+            query_result=QueryResult(
+                result={"profile": {"bytes": ORG_SCAN_LIMIT}}, extra={}
+            ),
+            error=None,
+        ),
+    )
+    allowance = policy.get_quota_allowance(tenant_ids)
+    assert allowance.can_run
+    assert allowance.max_threads == THROTTLED_THREAD_NUMBER
+    assert allowance.explanation == {
+        "reason": f"organization 123 is over the bytes scanned limit of {ORG_SCAN_LIMIT}",
+        "is_enforced": True,
+        "granted_quota": 0,
+        "limit": ORG_SCAN_LIMIT,
+    }
+
+
+@pytest.mark.redis_db
+def test_org_isolation(policy) -> None:
+    _configure_policy(policy)
+
+    tenant_ids: dict[str, int | str] = {
+        "organization_id": 123,
+        "referrer": "some_referrer",
+    }
+    policy.update_quota_balance(
+        tenant_ids,
+        QueryResultOrError(
+            query_result=QueryResult(
+                result={"profile": {"bytes": 20 * ORG_SCAN_LIMIT}}, extra={}
+            ),
+            error=None,
+        ),
+    )
+    different_tenant_ids: dict[str, int | str] = {
+        "organization_id": 1235,
+        "referrer": "some_referrer",
+    }
+    allowance = policy.get_quota_allowance(different_tenant_ids)
+    assert allowance.max_threads == MAX_THREAD_NUMBER
+
+
+@pytest.mark.redis_db
+def test_killswitch(policy) -> None:
+    _configure_policy(policy)
+    set_config(f"{policy.rate_limit_prefix}.is_active", False)
+    tenant_ids: dict[str, int | str] = {
+        "organization_id": 123,
+        "referrer": "some_referrer",
+    }
+    policy.update_quota_balance(
+        tenant_ids,
+        QueryResultOrError(
+            query_result=QueryResult(
+                result={"profile": {"bytes": 20 * ORG_SCAN_LIMIT}}, extra={}
+            ),
+            error=None,
+        ),
+    )
+    allowance = policy.get_quota_allowance(tenant_ids)
+    # policy is not active so no change
+    assert allowance.max_threads == MAX_THREAD_NUMBER
+
+
+@pytest.mark.redis_db
+def test_enforcement_switch(policy) -> None:
+    _configure_policy(policy)
+    tenant_ids: dict[str, int | str] = {
+        "organization_id": 123,
+        "referrer": "some_referrer",
+    }
+    policy.update_quota_balance(
+        tenant_ids,
+        QueryResultOrError(
+            query_result=QueryResult(
+                result={"profile": {"bytes": 20 * ORG_SCAN_LIMIT}}, extra={}
+            ),
+            error=None,
+        ),
+    )
+    set_config(f"{policy.rate_limit_prefix}.is_enforced", False)
+    allowance = policy.get_quota_allowance(tenant_ids)
+    # policy not enforced
+    assert allowance.max_threads == MAX_THREAD_NUMBER
+
+
+@pytest.mark.redis_db
+def test_reject_queries_without_tenant_ids(policy) -> None:
+    _configure_policy(policy)
+    with pytest.raises(AllocationPolicyViolation):
+        policy.get_quota_allowance(tenant_ids={"organization_id": 1234})
+    with pytest.raises(AllocationPolicyViolation):
+        policy.get_quota_allowance(tenant_ids={"referrer": "bloop"})
+    # These should not fail because we know they don't have an org id
+    for referrer in _ORG_LESS_REFERRERS:
+        policy.get_quota_allowance(tenant_ids={"referrer": referrer})
