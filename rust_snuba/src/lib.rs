2157ad277190e9000104b9ba548b19cc0b8305f5
Dalitso Banda <dalitso.banda@sentry.io>
2023-04-19 14:45:05 -0700

    add sentry dsn

--- rust_snuba/src/lib.rs
+++ rust_snuba/src/lib.rs
@@ -7,6 +7,20 @@ use pyo3::prelude::*;
 
 #[pymodule]
 fn rust_snuba(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    // let DSN
+    let sentry_dsn = Python::with_gil(|py| {
+        let snuba_settings = PyModule::import(py, "snuba.settings")?;
+        let dsn: Result<String, PyErr> = snuba_settings
+            .getattr("SENTRY_DSN")?
+            .extract();
+        dsn
+    }).unwrap();
+
+    let _guard = sentry::init((sentry_dsn, sentry::ClientOptions {
+        release: sentry::release_name!(),
+        ..Default::default()
+    }));
+
     m.add_function(wrap_pyfunction!(consumer::consumer, m)?)?;
     Ok(())
 }
