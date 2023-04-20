23a92a894129f9c868aaf7216632d07db53171bb
Rahul Saini <rahulsaini805@gmail.com>
2023-04-20 14:52:49 -0700

    show config table when storage is selected

--- snuba/admin/static/capacity_management/index.tsx
+++ snuba/admin/static/capacity_management/index.tsx
@@ -2,11 +2,14 @@ import React, { useEffect, useState } from "react";
 import Client from "../api_client";
 import RuntimeConfig from "../runtime_config";
 import { AllocationPolicy } from "./types";
+import { selectStyle } from "./styles";
 
 function CapacityManagement(props: { api: Client }) {
   const { api } = props;
 
   const [allocationPolicies, setPolicies] = useState<AllocationPolicy[]>([]);
+  const [storage, setStorage] = useState<string>();
+  const [policy, setPolicy] = useState<string>();
 
   useEffect(() => {
     props.api.getAllocationPolicies().then((res) => {
@@ -14,14 +17,47 @@ function CapacityManagement(props: { api: Client }) {
     });
   }, []);
 
+  function selectStorage(storage: string) {
+    setStorage(storage);
+    allocationPolicies.map((policy) => {
+      if (policy.storage_name == storage) {
+        setPolicy(policy.allocation_policy);
+      }
+    });
+  }
+
+  function loadConfigEditor() {
+    return RuntimeConfig({ api: api });
+  }
+
   return (
     <div>
-      <p>Policies:</p>
-      {allocationPolicies.map((policy) => (
-        <p>{policy.storage_name + ": " + policy.allocation_policy}</p>
-      ))}{" "}
-      <p>Configs:</p>
-      {RuntimeConfig({ api: api })}
+      <p>Storage:</p>
+
+      <select
+        value={storage || ""}
+        onChange={(evt) => selectStorage(evt.target.value)}
+        style={selectStyle}
+      >
+        <option disabled value="">
+          Select a storage
+        </option>
+        {allocationPolicies.map((policy) => (
+          <option key={policy.storage_name} value={policy.storage_name}>
+            {policy.storage_name}
+          </option>
+        ))}
+      </select>
+
+      {policy ? (
+        <div>
+          <p>{policy}</p>
+          <p>Configs:</p>
+          <RuntimeConfig api={api} />
+        </div>
+      ) : (
+        <p>Storage not selected.</p>
+      )}
     </div>
   );
 }
---
+++ snuba/admin/static/capacity_management/styles.tsx
@@ -0,0 +1,6 @@
+const selectStyle = {
+  marginRight: 8,
+  height: 30,
+};
+
+export { selectStyle };
