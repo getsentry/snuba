d3613be791775f7d33669839dfea1f66b3676b09
Dalitso Banda <dalitso.banda@sentry.io>
2023-05-01 02:58:04 -0700

    skip check migrations if the skip-migrations-check label is used and add a note so that gocd is aware

--- scripts/check-migrations.py
+++ scripts/check-migrations.py
@@ -2,7 +2,7 @@
 import argparse
 import subprocess
 from shutil import ExecError
-from typing import Sequence
+from typing import Optional, Sequence
 
 """
 This script is meant to be run in CI to check that migrations changes are
@@ -24,9 +24,42 @@ ALLOWED_MIGRATIONS_GLOBS = [
 # changes to files here are migrations changes
 MIGRATIONS_GLOBS = ["snuba/snuba_migrations/*/[0-9]*.py"]
 
+SKIP_LABEL = "skip-migrations-check"
+
 
 class CoupledMigrations(Exception):
     pass
+
+
+def _has_skip_label(label: Optional[str]) -> bool:
+    # check the notes from the commit
+    subprocess.run(["git", "fetch", "origin", "refs/notes/*:refs/notes/*"])
+    notes = subprocess.run(
+        ["git", "notes", "show"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+    )
+    if notes.returncode != 0:
+        if "no note found" in notes.stderr:
+            pass
+        else:
+            raise ExecError(notes.stdout)
+    if SKIP_LABEL in notes.stdout:
+        return True
+
+    if label == SKIP_LABEL:
+        # add a note to the commit, so GOCD can see it
+        add_notes_change = subprocess.run(
+            ["git", "notes", "append", "-m", f"skipped migrations check: {SKIP_LABEL}"]
+        )
+        if add_notes_change.returncode != 0:
+            raise ExecError(add_notes_change.stdout)
+        push_notes_change = subprocess.run(["git", "push", "origin", "refs/notes/*"])
+        if push_notes_change.returncode != 0:
+            raise ExecError(push_notes_change.stdout)
+        return True
+    return False
 
 
 def _get_migration_changes(workdir: str, to: str) -> str:
@@ -55,7 +88,11 @@ def _get_changes(globs: Sequence[str], workdir: str, to: str) -> str:
         return changes.stdout
 
 
-def main(to: str = "origin/master", workdir: str = ".") -> None:
+def main(
+    to: str = "origin/master", workdir: str = ".", label: Optional[str] = None
+) -> None:
+    if _has_skip_label(label):
+        return
     migrations_changes = _get_migration_changes(workdir, to)
     has_migrations = len(migrations_changes.splitlines()) > 0
     if has_migrations:
@@ -70,5 +107,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--to", default="origin/master")
     parser.add_argument("--workdir", default=".")
+    parser.add_argument("--label")
     args = parser.parse_args()
-    main(args.to, args.workdir)
+    main(args.to, args.workdir, args.label)
