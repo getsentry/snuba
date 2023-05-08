72cd337f5d2efccb6747a6429b0f2e4a35bf6f0a
Dalitso Banda <dalitso.banda@sentry.io>
2023-05-08 12:46:37 -0700

    feat(ci): allow skip check migrations (#4111)
    * skip check migrations if the skip-migrations-check label is used
    
    * pass labels as list
    
    * fetch head sha
    
    * check commit PR labels

--- scripts/check-migrations.py
+++ scripts/check-migrations.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 import argparse
+import os
 import subprocess
 from shutil import ExecError
-from typing import Sequence
+from typing import Optional, Sequence
+
+import requests
 
 """
 This script is meant to be run in CI to check that migrations changes are
@@ -24,9 +27,65 @@ ALLOWED_MIGRATIONS_GLOBS = [
 # changes to files here are migrations changes
 MIGRATIONS_GLOBS = ["snuba/snuba_migrations/*/[0-9]*.py"]
 
+SKIP_LABEL = "skip-check-migrations"
+BASE_REF = "master"
+
 
 class CoupledMigrations(Exception):
     pass
+
+
+def _get_head_sha(workdir: str) -> str:
+    return (
+        subprocess.check_output(["git", "rev-parse", "HEAD"], cwd=workdir)
+        .decode()
+        .strip()
+    )
+
+
+def _pr_has_skip_label(commit_sha: str) -> bool:
+    GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
+    if not GITHUB_TOKEN:
+        print("GITHUB_TOKEN not set. Skipping github PR labels check.")
+        return False
+
+    # get the PR from the commit using the github api
+    # and check its labels
+    # see: https://docs.github.com/en/rest/commits/commits?apiVersion=2022-11-28#list-pull-requests-associated-with-a-commit
+    pull_request_url = (
+        "https://api.github.com/repos/getsentry/snuba/commits/" + commit_sha + "/pulls"
+    )
+
+    # Make the request
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"token {GITHUB_TOKEN}",
+        # A user agent is required.
+        # https://docs.github.com/en/rest/overview/resources-in-the-rest-api#user-agent-required
+        "User-Agent": "getsentry/gocd-agent",
+    }
+    response = requests.get(pull_request_url, headers=headers)
+
+    # check associated merged PR labels
+    if response.status_code == 200:
+        pulls = response.json()
+        for pull in pulls:
+            if pull["merged_at"] is None or pull["base"]["ref"] != BASE_REF:
+                continue
+            labels = pull["labels"]
+            for label in labels:
+                if SKIP_LABEL in label["name"]:
+                    return True
+    else:
+        print("Error checking github:", response.status_code)
+
+    return False
+
+
+def _head_commit_pr_has_skip_label(workdir: str) -> bool:
+    head_sha = _get_head_sha(workdir)
+    print("Checking if PR for commit", head_sha, "has skip label")
+    return _pr_has_skip_label(head_sha)
 
 
 def _get_migration_changes(workdir: str, to: str) -> str:
@@ -55,7 +114,17 @@ def _get_changes(globs: Sequence[str], workdir: str, to: str) -> str:
         return changes.stdout
 
 
-def main(to: str = "origin/master", workdir: str = ".") -> None:
+def main(
+    to: str = "origin/master", workdir: str = ".", labels: Optional[Sequence[str]] = []
+) -> None:
+    if labels:
+        for label in labels:
+            if SKIP_LABEL in label:
+                return
+
+    if _head_commit_pr_has_skip_label(workdir):
+        return
+
     migrations_changes = _get_migration_changes(workdir, to)
     has_migrations = len(migrations_changes.splitlines()) > 0
     if has_migrations:
@@ -70,5 +139,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--to", default="origin/master")
     parser.add_argument("--workdir", default=".")
+    parser.add_argument("--labels", nargs="*")
     args = parser.parse_args()
-    main(args.to, args.workdir)
+    print(
+        f"migrations changes: to: {args.to}, workdir: {args.workdir}, labels: {args.labels}"
+    )
+    main(args.to, args.workdir, args.labels)
