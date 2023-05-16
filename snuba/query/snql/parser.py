41f38dfc815b6fdc10bc68c8e48faa66d44bfd76
David Tsukernik <david.tsukernik@sentry.io>
2023-05-16 15:15:34 -0400

    Update error message for missing datetime column comparison condition

--- snuba/query/snql/parser.py
+++ snuba/query/snql/parser.py
@@ -1297,11 +1297,11 @@ def _align_max_days_date_align(
     )
     if not lower:
         raise ParsingException(
-            f"missing >= condition on column {entity.required_time_column} for entity {key.value}"
+            f"missing >= condition with a datetime literal on column {entity.required_time_column} for entity {key.value}"
         )
     elif not upper:
         raise ParsingException(
-            f"missing < condition on column {entity.required_time_column} for entity {key.value}"
+            f"missing < condition with a datetime literal on column {entity.required_time_column} for entity {key.value}"
         )
 
     from_date, from_exp = lower
