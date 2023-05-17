3029ab6756ff94385cc260fb605f1020a41c8c4c
Enoch Tang <enoch.tang@sentry.io>
2023-05-17 11:26:29 -0400

    do not enable functions and profiles readiness states

--- snuba/settings/__init__.py
+++ snuba/settings/__init__.py
@@ -249,8 +249,6 @@ SUPPORTED_STATES: Set[str] = {"deprecate", "limited", "partial", "complete"}
 # [04-18-2023] These two readiness state settings are temporary and used to facilitate the rollout of readiness states.
 # We expect to remove them after all storages and migration groups have been migrated.
 READINESS_STATE_MIGRATION_GROUPS_ENABLED: set[str] = {
-    "functions",
-    "profiles",
     "querylog",
     "search_issues",
     "test_migration",
