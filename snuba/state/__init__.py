ca20e97e2673f3acc0a0b2003e64c20939c8a1fc
Lyn <lyn.nagara@gmail.com>
2023-05-22 17:43:56 -0700

    fix: Add querylog `max.message.bytes` to topic configuration
    We define `max.message.bytes` in topic configuration at Sentry,
    and do not allow producers to choose the max message size.

--- snuba/state/__init__.py
+++ snuba/state/__init__.py
@@ -58,23 +58,6 @@ def _kafka_producer() -> Producer:
         kfk = Producer(
             build_kafka_producer_configuration(
                 topic=None,
-                override_params={
-                    # at time of writing (2022-05-09) lz4 was chosen because it
-                    # compresses quickly. If more compression is needed at the cost of
-                    # performance, zstd can be used instead. Recording the query
-                    # is part of the API request, therefore speed is important
-                    # perf-testing: https://indico.fnal.gov/event/16264/contributions/36466/attachments/22610/28037/Zstd__LZ4.pdf
-                    # by default a topic is configured to use whatever compression method the producer used
-                    # https://docs.confluent.io/platform/current/installation/configuration/topic-configs.html#topicconfigs_compression.type
-                    "compression.type": "lz4",
-                    # the querylog payloads can get really large so we allow larger messages
-                    # (double the default)
-                    # The performance is not business critical and therefore we accept the tradeoffs
-                    # in more bandwidth for more observability/debugability
-                    # for this to be meaningful, the following setting has to be at least as large on the broker
-                    # message.max.bytes=2000000
-                    "message.max.bytes": 2000000,
-                },
             )
         )
     return kfk
