0327454a2039b891230c023ac1c88dcd747e5f4e
David Tsukernik <david.tsukernik@sentry.io>
2023-06-08 16:03:23 -0400

    fix google api tests

--- tests/admin/test_google.py
+++ tests/admin/test_google.py
@@ -5,17 +5,19 @@ from snuba.admin.google import check_transitive_membership, get_group_id
 
 
 def test_get_group_id() -> None:
-    http = HttpMock("data/group_lookup_200")
+    http = HttpMock("tests/admin/data/mock_responses/group_lookup_200.json")
     service = build("cloudidentity", "v1", http=http, developerKey="api_key")
     assert get_group_id(service, "group_email") == "groups/group_id"
 
-    http = HttpMock("data/group_lookup_403")
+    http = HttpMock("tests/admin/data/mock_responses/group_lookup_403.json")
     service = build("cloudidentity", "v1", http=http, developerKey="api_key")
     assert get_group_id(service=service, group_email="group_email") == ""
 
 
 def test_check_transitive_membership() -> None:
-    http = HttpMock("data/check_transitive_member_200")
+    http = HttpMock(
+        "tests/admin/data/mock_responses/check_transitive_membership_200.json"
+    )
     service = build("cloudidentity", "v1", http=http, developerKey="api_key")
     assert check_transitive_membership(
         service=service, group_resource_name="groups/group_id", member="member"
