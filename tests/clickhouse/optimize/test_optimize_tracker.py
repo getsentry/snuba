38c23edef6d648ae913372f548214f0ba9e9f795
Lyn <lyn.nagara@gmail.com>
2023-06-08 13:17:54 -0700

    😭

--- tests/clickhouse/optimize/test_optimize_tracker.py
+++ tests/clickhouse/optimize/test_optimize_tracker.py
@@ -65,7 +65,7 @@ def test_optimized_partition_tracker(tracker: OptimizedPartitionTracker) -> None
             if pending is not None:
                 assert tracker.get_partitions_to_optimize() == pending
 
-        for _ in range(10):
+        for _ in range(20):
             try:
                 _do_assertions()
                 return
