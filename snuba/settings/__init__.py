dab09604a69dd9b6e0d6bcc8b64ba2002f864e1a
Enoch Tang <enoch.tang@sentry.io>
2023-06-09 13:49:11 -0400

    allow spans migrations to run locally

--- snuba/settings/__init__.py
+++ snuba/settings/__init__.py
@@ -256,15 +256,6 @@ COLUMN_SPLIT_MIN_COLS = 6
 COLUMN_SPLIT_MAX_LIMIT = 1000
 COLUMN_SPLIT_MAX_RESULTS = 5000
 
-# The migration groups that can be skipped are listed in OPTIONAL_GROUPS.
-# Migrations for skipped groups will not be run.
-SKIPPED_MIGRATION_GROUPS: Set[str] = {
-    "spans",
-}
-
-if os.environ.get("ENABLE_AUTORUN_MIGRATION_SPANS", False):
-    SKIPPED_MIGRATION_GROUPS.remove("spans")
-
 # Dataset readiness states supported in this environment
 SUPPORTED_STATES: Set[str] = {"deprecate", "limited", "partial", "complete"}
 # [04-18-2023] These two readiness state settings are temporary and used to facilitate the rollout of readiness states.
