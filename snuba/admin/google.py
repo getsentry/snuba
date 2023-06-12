3856952622010c29e9818feeb1fbabf799acca1b
David Tsukernik <david.tsukernik@sentry.io>
2023-06-12 14:06:29 -0400

    Fix tests

--- snuba/admin/google.py
+++ snuba/admin/google.py
@@ -19,9 +19,11 @@ class CloudIdentityAPI:
     An interface for the Google Cloud Identity API.
     """
 
-    def __init__(self) -> None:
+    def __init__(self, service: Resource = None) -> None:
         try:
-            self.service: Resource = build("cloudidentity", "v1")
+            self.service: Resource = (
+                service if service else build("cloudidentity", "v1")
+            )
         except Exception as e:
             logger.exception(e)
 
@@ -44,12 +46,10 @@ class CloudIdentityAPI:
 
         return None
 
-    def check_group_membership(self, group_email: str, member: str) -> bool:
+    def _check_transitive_membership(
+        self, group_resource_name: str, member: str
+    ) -> bool:
         try:
-            group_resource_name = self._get_group_id(group_email)
-            if not group_resource_name:
-                return False
-
             query_params = urlencode({"query": "member_key_id == '{}'".format(member)})
             request = (
                 self.service.groups()
@@ -63,9 +63,17 @@ class CloudIdentityAPI:
                     f"An HTTP error occured when checking if user {member} is a member of group {group_resource_name}",
                     google_api_error=response["error"],
                 )
-
             return bool(response.get("hasMembership", False))
         except Exception as e:
             logger.exception(e)
+
+        return False
+
+    def check_group_membership(self, group_email: str, member: str) -> bool:
+        group_resource_name = self._get_group_id(group_email)
+        if group_resource_name:
+            return self._check_transitive_membership(
+                group_resource_name=group_resource_name, member=member
+            )
 
         return False
--- tests/admin/test_google.py
+++ tests/admin/test_google.py
@@ -1,17 +1,19 @@
 from googleapiclient.discovery import build
 from googleapiclient.http import HttpMock
 
-from snuba.admin.google import check_transitive_membership, get_group_id
+from snuba.admin.google import CloudIdentityAPI
 
 
 def test_get_group_id() -> None:
     http = HttpMock("tests/admin/data/mock_responses/group_lookup_200.json")
     service = build("cloudidentity", "v1", http=http, developerKey="api_key")
-    assert get_group_id(service, "group_email") == "groups/group_id"
+    api_client = CloudIdentityAPI(service=service)
+    assert api_client._get_group_id("group_email") == "groups/group_id"
 
     http = HttpMock("tests/admin/data/mock_responses/group_lookup_403.json")
     service = build("cloudidentity", "v1", http=http, developerKey="api_key")
-    assert get_group_id(service=service, group_email="group_email") == ""
+    api_client = CloudIdentityAPI(service=service)
+    assert not api_client._get_group_id(group_email="group_email")
 
 
 def test_check_transitive_membership() -> None:
@@ -19,6 +21,16 @@ def test_check_transitive_membership() -> None:
         "tests/admin/data/mock_responses/check_transitive_membership_200.json"
     )
     service = build("cloudidentity", "v1", http=http, developerKey="api_key")
-    assert check_transitive_membership(
-        service=service, group_resource_name="groups/group_id", member="member"
+    api_client = CloudIdentityAPI(service=service)
+    assert api_client._check_transitive_membership(
+        group_resource_name="groups/group_id", member="member"
+    )
+
+
+def test_check_group_membership() -> None:
+    http = HttpMock("tests/admin/data/mock_responses/group_lookup_403.json")
+    service = build("cloudidentity", "v1", http=http, developerKey="api_key")
+    api_client = CloudIdentityAPI(service=service)
+    assert not api_client.check_group_membership(
+        group_email="group_email", member="member"
     )
