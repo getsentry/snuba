f44cb3a4fe817d237aa10bfe20357f7333bbf8f9
Rahul Saini <rahulsaini805@gmail.com>
2023-06-22 17:39:21 -0400

    is_enforced

--- snuba/settings/__init__.py
+++ snuba/settings/__init__.py
@@ -188,6 +188,7 @@ SNAPSHOT_LOAD_PRODUCT = "snuba"
 BULK_CLICKHOUSE_BUFFER = 10000
 BULK_BINARY_LOAD_CHUNK = 2**22  # 4 MB
 
+
 # Processor/Writer Options
 
 
@@ -244,6 +245,10 @@ TURBO_SAMPLE_RATE = 0.1
 
 PROJECT_STACKTRACE_BLACKLIST: Set[int] = set()
 PRETTY_FORMAT_EXPRESSIONS = True
+
+# Capacity Management
+
+ENFORCE_BYTES_SCANNED_WINDOW_POLICY = True
 
 # By default, allocation policies won't block requests from going through in a production
 # environment to not cause incidents unnecessarily. If something goes wrong with allocation
