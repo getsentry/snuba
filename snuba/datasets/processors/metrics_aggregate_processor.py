b724c2bb9f8c7b1dbd4ffae08284964f02604146
John Yang <john.yang@sentry.io>
2023-06-26 10:14:07 -0700

    remove 10 second granularity from MetricsAggregateProcessor

--- snuba/datasets/processors/metrics_aggregate_processor.py
+++ snuba/datasets/processors/metrics_aggregate_processor.py
@@ -40,11 +40,10 @@ def timestamp_to_bucket(timestamp: datetime, interval_seconds: int) -> datetime:
 
 
 class MetricsAggregateProcessor(DatasetMessageProcessor, ABC):
-    TEN_SECONDS = 10
     ONE_MINUTE = 60
     ONE_HOUR = 3600
     ONE_DAY = 3600 * 24
-    GRANULARITIES_SECONDS = [TEN_SECONDS, ONE_MINUTE, ONE_HOUR, ONE_DAY]
+    GRANULARITIES_SECONDS = [ONE_MINUTE, ONE_HOUR, ONE_DAY]
 
     @abstractmethod
     def _should_process(self, message: Mapping[str, Any]) -> bool:
