fc9060a34c63ce02bc753aaa23cc1d73be3ad12f
Enoch Tang <enoch.tang@sentry.io>
2023-07-04 11:52:41 -0400

    regex parse trace logs

--- snuba/admin/clickhouse/tracing.py
+++ snuba/admin/clickhouse/tracing.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
+import re
 from dataclasses import dataclass
+from typing import Any, List
 
 from snuba.admin.clickhouse.common import (
     get_ro_query_node_connection,
@@ -24,7 +26,7 @@ def run_query_and_get_trace(storage_name: str, query: str) -> TraceOutput:
     query_result = connection.execute(
         query=query, capture_trace=True, with_column_types=True
     )
-    format_trace_logs(query_result.trace_output)
+    format_trace_output(query_result.trace_output)
     return TraceOutput(
         trace_output=query_result.trace_output,
         cols=query_result.meta,  # type: ignore
@@ -32,7 +34,21 @@ def run_query_and_get_trace(storage_name: str, query: str) -> TraceOutput:
     )
 
 
-def format_trace_logs(raw_trace_logs: str) -> None:
-    print("START")
-    print(raw_trace_logs)
-    print("END")
+def format_trace_output(raw_trace_logs: str) -> List[dict[str, Any]]:
+    # LOG STRUCTURE: '[ NODE ] [ THREAD_ID ] {SHA?} <LOG_TYPE> LOG_LINE'
+    formatted_logs = []
+    for line in raw_trace_logs.splitlines():
+        context = re.findall(r"\[.*?\]", line)
+        log_type = ""
+        log_type_regex = re.search(r"<.*?>", line)
+        if log_type_regex is not None:
+            log_type = log_type_regex.group()
+
+        formatted_log = {
+            "node": context[0][2:-2],
+            "thread_id": context[1][2:-2],
+            "log_type": log_type,
+            "log_body": re.split(r"<.*?>", line)[1].strip(),
+        }
+        formatted_logs.append(formatted_log)
+    return formatted_logs
