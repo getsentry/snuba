d7c781ee0368cc4895e92edba1d48f72e6f0d8ec
Dalitso Banda <dalitso.banda@sentry.io>
2023-07-14 12:26:48 -0700

    feat(subscriptions): add clickhouse connection check (#4512)
    * add clickhouse connection check

--- snuba/cli/subscriptions_scheduler_executor.py
+++ snuba/cli/subscriptions_scheduler_executor.py
@@ -3,6 +3,7 @@ from contextlib import contextmanager
 from typing import Any, Iterator, Optional, Sequence
 
 import click
+import structlog
 from arroyo import configure_metrics
 from arroyo.backends.kafka import KafkaProducer
 
@@ -12,6 +13,7 @@ from snuba.datasets.entities.entity_key import EntityKey
 from snuba.datasets.entities.factory import get_entity
 from snuba.datasets.factory import get_enabled_dataset_names
 from snuba.environment import setup_logging, setup_sentry
+from snuba.migrations.connect import check_clickhouse_connections
 from snuba.subscriptions.combined_scheduler_executor import (
     build_scheduler_executor_consumer,
 )
@@ -91,6 +93,8 @@ def subscriptions_scheduler_executor(
     setup_logging(log_level)
     setup_sentry()
 
+    logger = structlog.get_logger().bind(module=__name__)
+
     metrics = MetricsWrapper(
         environment.metrics,
         "subscriptions.scheduler_executor",
@@ -115,6 +119,10 @@ def subscriptions_scheduler_executor(
             override_params={"partitioner": "consistent"},
         )
     )
+
+    logger.info("Checking Clickhouse connections")
+    cluster = storage.get_cluster()
+    check_clickhouse_connections([cluster])
 
     processor = build_scheduler_executor_consumer(
         dataset_name,
