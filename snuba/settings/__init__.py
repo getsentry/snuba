e4e8c785db7f5a7bae0eb5de583fbd1bb71e1e79
Gilbert Szeto <gilbert.szeto@sentry.io>
2023-07-14 10:01:16 -0700

    PR feedback

--- snuba/settings/__init__.py
+++ snuba/settings/__init__.py
@@ -274,7 +274,7 @@ COLUMN_SPLIT_MAX_RESULTS = 5000
 
 # The migration groups that can be skipped are listed in OPTIONAL_GROUPS.
 # Migrations for skipped groups will not be run.
-SKIPPED_MIGRATION_GROUPS: Set[str] = {"group_attributes"}
+SKIPPED_MIGRATION_GROUPS: Set[str] = set()
 
 # Dataset readiness states supported in this environment
 SUPPORTED_STATES: Set[str] = {"deprecate", "limited", "partial", "complete"}
