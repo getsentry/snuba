0ad3154f4f8e8fef71e67d0300af8638c4ce01b9
John Yang <john.yang@sentry.io>
2023-07-17 09:51:31 -0700

    update default retention days for generic set metrics

--- snuba/migrations/group_loader.py
+++ snuba/migrations/group_loader.py
@@ -299,6 +299,7 @@ class GenericMetricsLoader(DirectoryLoader):
             "0015_sets_add_options",
             "0016_counters_add_options",
             "0017_distributions_mv2",
+            "0018_sets_update_options",
         ]
 
 
