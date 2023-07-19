e28d18cdc2fdf16598d4b444b1ddc1cd5a2c5e68
George Gritsouk <989898+gggritso@users.noreply.github.com>
2023-07-19 17:49:22 -0400

    Add spec

---
+++ snuba/admin/static/tests/cardinality_analyzer/CSV.spec.ts
@@ -0,0 +1,21 @@
+import { CSV } from "../../cardinality_analyzer/CSV";
+
+describe("CSV.sheet", () => {
+  it("Should return a CSV value of a grid of values", () => {
+    expect(
+      CSV.sheet([
+        ["name", "age"],
+        ["George", 33],
+      ])
+    ).toEqual(`name,age\nGeorge,33`);
+  });
+
+  it("Should escape commas by wrapping in double quotes", () => {
+    expect(
+      CSV.sheet([
+        ["name", "motto"],
+        ["George", "do a good job, don't do a bad job"],
+      ])
+    ).toEqual(`name,motto\nGeorge,"do a good job, don't do a bad job"`);
+  });
+});
