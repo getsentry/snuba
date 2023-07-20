cfbefac8ffe1f0c6f5314fd5e1ee10d71250ef7f
George Gritsouk <989898+gggritso@users.noreply.github.com>
2023-07-20 12:51:49 -0400

    fix(cardinality-analyzer): Fix CSV escaping (#4546)
    * Prettier
    * Remove unused variables
    * Add some more typing
    * Extract CSV class
    * Add spec
    * Escape double quotes with a backslash

--- snuba/admin/static/cardinality_analyzer/query_display.tsx
+++ snuba/admin/static/cardinality_analyzer/query_display.tsx
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
 import Client from "../api_client";
 import { Collapse } from "../collapse";
+import { CSV } from "./CSV";
 import QueryEditor from "../query_editor";
 
 import { CardinalityQueryRequest, CardinalityQueryResult, PredefinedQuery } from "./types";
 
+enum ClipboardFormats {
+  CSV = "csv",
+  JSON = "json",
+}
 type QueryState = Partial<CardinalityQueryRequest>;
 
 function QueryDisplay(props: {
@@ -13,9 +18,7 @@ function QueryDisplay(props: {
   predefinedQueryOptions: Array<PredefinedQuery>;
 }) {
   const [query, setQuery] = useState<QueryState>({});
-  const [queryResultHistory, setCardinalityQueryResultHistory] = useState<
-    CardinalityQueryResult[]
-  >([]);
+  const [queryResultHistory, setCardinalityQueryResultHistory] = useState<CardinalityQueryResult[]>([]);
 
   function updateQuerySql(sql: string) {
     setQuery((prevQuery) => {
@@ -40,18 +43,20 @@ function QueryDisplay(props: {
   }
 
   function convertResultsToCSV(queryResult: CardinalityQueryResult) {
-    let output = queryResult.column_names.join(",");
-    for (const row of queryResult.rows) {
-      const escaped = row.map((v) =>
-        typeof v == "string" && v.includes(",") ? '"' + v + '"' : v
-      );
-      output = output + "\n" + escaped.join(",");
-    }
-    return output;
+    return CSV.sheet([queryResult.column_names, ...queryResult.rows]);
   }
 
-  function copyText(queryResult: CardinalityQueryResult, format: string) {
-    const formatter = format == "csv" ? convertResultsToCSV : JSON.stringify;
+  function copyText(queryResult: CardinalityQueryResult, format: ClipboardFormats) {
+    let formatter: (input: CardinalityQueryResult) => string = (s) => s.toString();
+
+    if (format === ClipboardFormats.JSON) {
+      formatter = JSON.stringify;
+    }
+
+    if (format === ClipboardFormats.CSV) {
+      formatter = convertResultsToCSV;
+    }
+
     window.navigator.clipboard.writeText(formatter(queryResult));
   }
 
@@ -86,18 +91,12 @@ function QueryDisplay(props: {
               <div key={idx}>
                 <p>{queryResult.input_query}</p>
                 <p>
-                  <button
-                    style={executeButtonStyle}
-                    onClick={() => copyText(queryResult, "json")}
-                  >
+                  <button style={executeButtonStyle} onClick={() => copyText(queryResult, ClipboardFormats.JSON)}>
                     Copy to clipboard (JSON)
                   </button>
                 </p>
                 <p>
-                  <button
-                    style={executeButtonStyle}
-                    onClick={() => copyText(queryResult, "csv")}
-                  >
+                  <button style={executeButtonStyle} onClick={() => copyText(queryResult, ClipboardFormats.CSV)}>
                     Copy to clipboard (CSV)
                   </button>
                 </p>
@@ -108,16 +107,10 @@ function QueryDisplay(props: {
 
           return (
             <Collapse key={idx} text={queryResult.input_query}>
-              <button
-                style={executeButtonStyle}
-                onClick={() => copyText(queryResult, "json")}
-              >
+              <button style={executeButtonStyle} onClick={() => copyText(queryResult, ClipboardFormats.JSON)}>
                 Copy to clipboard (JSON)
               </button>
-              <button
-                style={executeButtonStyle}
-                onClick={() => copyText(queryResult, "csv")}
-              >
+              <button style={executeButtonStyle} onClick={() => copyText(queryResult, ClipboardFormats.CSV)}>
                 Copy to clipboard (CSV)
               </button>
               {props.resultDataPopulator(queryResult)}
@@ -142,31 +135,4 @@ const executeButtonStyle = {
   marginRight: 10,
 };
 
-const selectStyle = {
-  marginRight: 8,
-  height: 30,
-};
-
-function TextArea(props: {
-  value: string;
-  onChange: (nextValue: string) => void;
-}) {
-  const { value, onChange } = props;
-  return (
-    <textarea
-      spellCheck={false}
-      value={value}
-      onChange={(evt) => onChange(evt.target.value)}
-      style={{ width: "100%", height: 140 }}
-      placeholder={"Write your query here"}
-    />
-  );
-}
-
-const queryDescription = {
-  minHeight: 10,
-  width: "auto",
-  fontSize: 16,
-  padding: "10px 5px",
-};
 export default QueryDisplay;
