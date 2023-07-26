26e91a3b0ae09f009106ccc27084ab0a63c62f2c
Lyn <lyn.nagara@gmail.com>
2023-07-26 09:46:11 -0700

    feat(clickhouse): Enable 23.3
    We are going to put group-attributes on 23.3 and start building
    support for this version

--- snuba/migrations/clickhouse.py
+++ snuba/migrations/clickhouse.py
@@ -1,2 +1,5 @@
 CLICKHOUSE_SERVER_MIN_VERSION = "20.3.5.21"
-CLICKHOUSE_SERVER_MAX_VERSION = "21.8.13.1"
+# Note only versions up to 21.8.13.1 are currently supported,
+# 23.3.8.22 is enabled for testing purposes in a minimal number of
+# datasets
+CLICKHOUSE_SERVER_MAX_VERSION = "23.3.8.22"
