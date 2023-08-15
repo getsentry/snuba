4018791c41ae14f39af37f2c6f8b7f283bb28405
Dalitso Banda <dalitso.banda@sentry.io>
2023-08-15 02:07:04 -0700

    test clickhouse 21 as its own test

--- tests/test_cleanup.py
+++ tests/test_cleanup.py
@@ -68,7 +68,9 @@ class TestCleanup:
             return datetime(rounded.year, rounded.month, rounded.day)
 
         # In prod the dates have hours/seconds etc.
-        base = datetime(1999, 12, 26, 1, 14, 35)  # a sunday
+        # use future dates to avoid hitting TTLs
+        base = datetime(2100, 1, 3, 1, 14, 35)  # a sunday
+
         current_time.return_value = base
 
         storage = get_writable_storage(storage_key)
