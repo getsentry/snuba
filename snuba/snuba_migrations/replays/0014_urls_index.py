46e1f8d3bf9a9a06c457c98bf9225140cd0cf354
Colton Allen <colton.allen@sentry.io>
2023-08-28 20:20:49 -0500

    Index frequently accessed columns

--- snuba/snuba_migrations/replays/0014_urls_index.py
+++ snuba/snuba_migrations/replays/0014_urls_index.py
@@ -17,63 +17,68 @@ class Migration(migration.ClickhouseNodeMigration):
 
 
 def forward_columns_iter() -> Iterator[operations.SqlOperation]:
-    yield operations.AddColumn(
-        storage_set=StorageSetKey.REPLAYS,
-        table_name="replays_local",
-        column=Column(
-            "_urls_hashed",
-            Array(
-                UInt(64),
-                Modifiers(materialized="arrayMap(t -> cityHash64(t), urls)"),
+    for column in materialized_columns:
+        yield operations.AddColumn(
+            storage_set=StorageSetKey.REPLAYS,
+            table_name="replays_local",
+            column=Column(
+                f"_{column}_hashed",
+                Array(
+                    UInt(64),
+                    Modifiers(materialized=f"arrayMap(t -> cityHash64(t), {column})"),
+                ),
             ),
-        ),
-        after="urls",
-        target=operations.OperationTarget.LOCAL,
-    )
+            after=column,
+            target=operations.OperationTarget.LOCAL,
+        )
 
-    yield operations.AddColumn(
-        storage_set=StorageSetKey.REPLAYS,
-        table_name="replays_dist",
-        column=Column(
-            "_urls_hashed",
-            Array(
-                UInt(64),
-                Modifiers(materialized="arrayMap(t -> cityHash64(t), urls)"),
+        yield operations.AddColumn(
+            storage_set=StorageSetKey.REPLAYS,
+            table_name="replays_dist",
+            column=Column(
+                f"_{column}_hashed",
+                Array(
+                    UInt(64),
+                    Modifiers(materialized=f"arrayMap(t -> cityHash64(t), {column})"),
+                ),
             ),
-        ),
-        after="urls",
-        target=operations.OperationTarget.DISTRIBUTED,
-    )
+            after=column,
+            target=operations.OperationTarget.DISTRIBUTED,
+        )
 
-    yield operations.AddIndex(
-        storage_set=StorageSetKey.REPLAYS,
-        table_name="replays_local",
-        index_name="bf_urls_hashed",
-        index_expression="_urls_hashed",
-        index_type="bloom_filter()",
-        granularity=1,
-        target=operations.OperationTarget.LOCAL,
-    )
+        yield operations.AddIndex(
+            storage_set=StorageSetKey.REPLAYS,
+            table_name="replays_local",
+            index_name=f"bf_{column}_hashed",
+            index_expression=f"_{column}_hashed",
+            index_type="bloom_filter()",
+            granularity=1,
+            target=operations.OperationTarget.LOCAL,
+        )
 
 
 def backward_columns_iter() -> Iterator[operations.SqlOperation]:
-    yield operations.DropColumn(
-        StorageSetKey.REPLAYS,
-        "replays_local",
-        "_urls_hashed",
-        operations.OperationTarget.LOCAL,
-    )
+    for column in materialized_columns:
+        yield operations.DropColumn(
+            StorageSetKey.REPLAYS,
+            "replays_local",
+            f"_{column}_hashed",
+            operations.OperationTarget.LOCAL,
+        )
 
-    yield operations.DropColumn(
-        StorageSetKey.REPLAYS,
-        "replays_dist",
-        "_urls_hashed",
-        operations.OperationTarget.DISTRIBUTED,
-    )
+        yield operations.DropColumn(
+            StorageSetKey.REPLAYS,
+            "replays_dist",
+            f"_{column}_hashed",
+            operations.OperationTarget.DISTRIBUTED,
+        )
 
-    yield operations.DropIndex(
-        StorageSetKey.REPLAYS,
-        "replays_local",
-        "bf_urls_hashed",
-        target=operations.OperationTarget.LOCAL,
-    )
+        yield operations.DropIndex(
+            StorageSetKey.REPLAYS,
+            "replays_local",
+            f"bf_{column}_hashed",
+            target=operations.OperationTarget.LOCAL,
+        )
+
+
+materialized_columns = ["urls", "user_id", "user_name", "user_email", "ip_address_v4"]
