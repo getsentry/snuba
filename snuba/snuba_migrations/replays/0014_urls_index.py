84e3e7009edd8679212b3a2ed2eaf24a7be7a97f
Colton Allen <colton.allen@sentry.io>
2023-09-07 11:06:54 -0500

    Drop materialization of urls column

--- snuba/snuba_migrations/replays/0014_urls_index.py
+++ snuba/snuba_migrations/replays/0014_urls_index.py
@@ -1,9 +1,7 @@
 from typing import Iterator, Sequence
 
-from snuba.clickhouse.columns import Array, Column, UInt
 from snuba.clusters.storage_sets import StorageSetKey
 from snuba.migrations import migration, operations
-from snuba.migrations.columns import MigrationModifiers as Modifiers
 
 
 class Migration(migration.ClickhouseNodeMigration):
@@ -17,44 +15,6 @@ class Migration(migration.ClickhouseNodeMigration):
 
 
 def forward_columns_iter() -> Iterator[operations.SqlOperation]:
-    yield operations.AddColumn(
-        storage_set=StorageSetKey.REPLAYS,
-        table_name="replays_local",
-        column=Column(
-            "_urls_hashed",
-            Array(
-                UInt(64),
-                Modifiers(materialized="arrayMap(t -> cityHash64(t), urls)"),
-            ),
-        ),
-        after="urls",
-        target=operations.OperationTarget.LOCAL,
-    )
-
-    yield operations.AddColumn(
-        storage_set=StorageSetKey.REPLAYS,
-        table_name="replays_dist",
-        column=Column(
-            "_urls_hashed",
-            Array(
-                UInt(64),
-                Modifiers(materialized="arrayMap(t -> cityHash64(t), urls)"),
-            ),
-        ),
-        after="urls",
-        target=operations.OperationTarget.DISTRIBUTED,
-    )
-
-    yield operations.AddIndex(
-        storage_set=StorageSetKey.REPLAYS,
-        table_name="replays_local",
-        index_name="bf_urls_hashed",
-        index_expression="_urls_hashed",
-        index_type="bloom_filter()",
-        granularity=1,
-        target=operations.OperationTarget.LOCAL,
-    )
-
     for column in materialized_columns:
         yield operations.AddIndex(
             storage_set=StorageSetKey.REPLAYS,
@@ -76,26 +36,5 @@ def backward_columns_iter() -> Iterator[operations.SqlOperation]:
             target=operations.OperationTarget.LOCAL,
         )
 
-    yield operations.DropIndex(
-        StorageSetKey.REPLAYS,
-        "replays_local",
-        "bf_urls_hashed",
-        target=operations.OperationTarget.LOCAL,
-    )
 
-    yield operations.DropColumn(
-        StorageSetKey.REPLAYS,
-        "replays_dist",
-        "_urls_hashed",
-        operations.OperationTarget.DISTRIBUTED,
-    )
-
-    yield operations.DropColumn(
-        StorageSetKey.REPLAYS,
-        "replays_local",
-        "_urls_hashed",
-        operations.OperationTarget.LOCAL,
-    )
-
-
-materialized_columns = ["user_id", "user_name", "user_email", "ip_address_v4"]
+materialized_columns = ["urls", "user_id", "user_name", "user_email", "ip_address_v4"]
