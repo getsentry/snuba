4dad769a52d1b450715061ae37c3c994ff4bd855
Rahul Saini <rahulsaini805@gmail.com>
2023-09-15 12:34:30 -0700

    add checks

---
+++ gocd/templates/bash/saas-sentry-health-check.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+/devinfra/scripts/checks/sentry/release_new_issues.py \
+  --project-id=300688 \
+  --project-slug=snuba \
+  --release="${GO_REVISION_SNUBA_REPO}" \
+  --new-issues-limit=0 \
+  --skip-check=${SKIP_CANARY_CHECKS}
