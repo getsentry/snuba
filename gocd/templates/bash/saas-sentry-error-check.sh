77b007abe506fa93129ae9eb85ee9ca54763d008
Rahul Kumar Saini <rahul-kumar-saini@users.noreply.github.com>
2023-09-21 10:44:02 -0700

    feat(deploy-gates): Add SaaS -> ST deploy gates (#4729)
    * add checks
    
    * actually add the stages

---
+++ gocd/templates/bash/saas-sentry-error-check.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+/devinfra/scripts/checks/sentry/release_error_events.py \
+  --project-id=300688 \
+  --project-slug=snuba \
+  --release="${GO_REVISION_SNUBA_REPO}" \
+  --duration=5 \
+  --error-events-limit=500 \
+  --skip-check=${SKIP_CANARY_CHECKS}
