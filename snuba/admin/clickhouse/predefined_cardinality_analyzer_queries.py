1d4e056d45c1c4a3af1911b0eee0b2d064c33f3a
Nikhar Saxena <nikhar.saxena@sentry.io>
2023-09-25 23:15:01 -0700

    Fix the queries

--- snuba/admin/clickhouse/predefined_cardinality_analyzer_queries.py
+++ snuba/admin/clickhouse/predefined_cardinality_analyzer_queries.py
@@ -8,7 +8,9 @@ from snuba.utils.registered_class import RegisteredClass
 # Start index is 9223372036854775808
 METRICS_START_INDEX = 1 << 63
 
-
+# TODO: These enum definitions do not work right now. Most likely because
+# f-string formatting does not work as intended when defining the sql. We hardcode
+# the values in the queries for now.
 class IndexedIDs(Enum):
     # Tags
     SPAN_DESCRIPTION_TAG = METRICS_START_INDEX + 249
@@ -29,19 +31,19 @@ class CardinalityQuery(PreDefinedQuery, metaclass=RegisteredClass):
 class SpanGroupingCardinality(CardinalityQuery):
     """Get the span groups with the highest cardinality across all projects and orgs."""
 
-    sql = f"""
-SELECT org_id, project_id,
-    tags.raw_value [indexOf(tags.key, {IndexedIDs.SPAN_CATEGORY_TAG})] AS `span.category`,
-    uniq(tags.raw_value [indexOf(tags.key, {IndexedIDs.SPAN_GROUP_TAG})] AS `span group`) AS groups
-FROM generic_metric_distributions_aggregated_dist
-WHERE (granularity = 3)
-AND (timestamp >= now() - INTERVAL {{hour_window}} HOUR)
-AND (metric_id IN [{IndexedIDs.SPAN_DURATION_METRIC}, {IndexedIDs.SPAN_EXCLUSIVE_TIME_METRIC}])
-AND `span.category` = {{span_category}}
-GROUP BY org_id, project_id, `span.category`
-ORDER BY groups DESC
-LIMIT 100
-"""
+    sql = """
+    SELECT org_id, project_id,
+    tags.raw_value [indexOf(tags.key, 9223372036854776062)] AS `span.category`,
+    uniq(tags.raw_value [indexOf(tags.key, 9223372036854776060)]) AS count_groups
+    FROM generic_metric_distributions_aggregated_dist
+    WHERE (granularity = 3)
+    AND (timestamp >= now() - INTERVAL {{hour_window}} HOUR)
+    AND (metric_id IN [9223372036854776212, 9223372036854776213])
+    AND `span.category` = '{{span_category}}'
+    GROUP BY org_id, project_id, `span.category`
+    ORDER BY count_groups DESC
+    LIMIT 100
+    """
 
 
 class SpanGroupingCardinalitySamples(CardinalityQuery):
@@ -49,17 +51,17 @@ class SpanGroupingCardinalitySamples(CardinalityQuery):
     Get a sample list of the span groups for a given org and project.
     """
 
-    sql = f"""
-SELECT DISTINCT
-    tags.raw_value[indexOf(tags.key, {IndexedIDs.SPAN_CATEGORY_TAG})] AS `span.category`,
-    tags.raw_value[indexOf(tags.key, {IndexedIDs.SPAN_GROUP_TAG})] AS `span.group`,
-    tags.raw_value[indexOf(tags.key, {IndexedIDs.SPAN_DESCRIPTION_TAG})] AS `span.description`
-FROM generic_metric_distributions_aggregated_dist
-WHERE (granularity = 3)
-AND (timestamp >= now() - INTERVAL {{hour_window}} HOUR)
-AND (org_id = {{org_id}})
-AND (project_id = {{project_id}})
-AND (metric_id = {IndexedIDs.SPAN_EXCLUSIVE_TIME_LIGHT_METRIC})
-ORDER BY span.description
-LIMIT 1000
-"""
+    sql = """
+    SELECT DISTINCT
+    tags.raw_value[indexOf(tags.key, 9223372036854776062)] AS `span.category`,
+    tags.raw_value[indexOf(tags.key, 9223372036854776060)] AS `span.group`,
+    tags.raw_value[indexOf(tags.key, 9223372036854776057)] AS `span.description`
+    FROM generic_metric_distributions_aggregated_dist
+    WHERE (granularity = 3)
+    AND (timestamp >= now() - INTERVAL {{hour_window}} HOUR)
+    AND (org_id = {{org_id}})
+    AND (project_id = {{project_id}})
+    AND (metric_id = 9223372036854776214)
+    ORDER BY span.description
+    LIMIT 1000
+    """
