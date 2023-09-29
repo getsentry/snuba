82576a453f8bfb9b55a48bf459ad80817596f441
Lyn <lyn.nagara@gmail.com>
2023-09-29 12:13:25 -0700

    refactor

--- rust_snuba/src/strategies/commit_log.rs
+++ rust_snuba/src/strategies/commit_log.rs
@@ -16,30 +16,37 @@ struct Payload {
     orig_message_ts: f64,
 }
 
-impl From<KafkaPayload> for Commit {
-    fn from(payload: KafkaPayload) -> Self {
+impl TryFrom<KafkaPayload> for Commit {
+    type Error = anyhow::Error;
+
+    fn try_from(payload: KafkaPayload) -> Result<Self, anyhow::Error> {
         let key = payload.key.unwrap();
 
         let data: Vec<&str> = str::from_utf8(&key).unwrap().split(':').collect();
-        assert!(data.len() == 3);
+        if data.len() != 3 {
+            return Err(anyhow::anyhow!("Invalid payload"));
+        }
 
         let topic = data[0].to_string();
         let partition = data[1].parse::<u16>().unwrap();
         let consumer_group = data[2].to_string();
 
-        let d: Payload = serde_json::from_slice(&payload.payload.unwrap()).unwrap();
+        let d: Payload =
+            serde_json::from_slice(&payload.payload.ok_or(anyhow::anyhow!("Invalid payload"))?)?;
 
-        Commit {
+        Ok(Commit {
             topic,
             partition,
             consumer_group,
             orig_message_ts: d.orig_message_ts,
-        }
+        })
     }
 }
 
-impl From<Commit> for KafkaPayload {
-    fn from(commit: Commit) -> Self {
+impl TryFrom<Commit> for KafkaPayload {
+    type Error = anyhow::Error;
+
+    fn try_from(commit: Commit) -> Result<Self, anyhow::Error> {
         let key = Some(
             format!(
                 "{}:{}:{}",
@@ -48,38 +55,36 @@ impl From<Commit> for KafkaPayload {
             .into_bytes(),
         );
 
-        let payload = Some(serde_json::to_vec(&commit).unwrap());
+        let payload = Some(serde_json::to_vec(&commit)?);
 
-        KafkaPayload {
+        Ok(KafkaPayload {
             key,
             headers: None,
             payload,
-        }
+        })
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::Produce;
-    use crate::backends::kafka::config::KafkaConfig;
-    use crate::backends::kafka::producer::KafkaProducer;
-    use crate::processing::strategies::{CommitRequest, MessageRejected, ProcessingStrategy};
-    use crate::types::{BrokerMessage, InnerMessage};
-    use crate::types::{Message, Partition, Topic, TopicOrPartition};
-    use chrono::Utc;
-    use rust_arroyo::backends::kafka::types::KafkaPayload;
-    use std::time::Duration;
+    use super::*;
 
     #[test]
     fn commit() {
         let payload = KafkaPayload {
-            key: Some(b"topic:0:group1"),
+            key: Some(b"topic:0:group1".to_vec()),
             headers: None,
-            payload: Some(b"{'offset': 5, 'orig_message_ts': '2023-09-26T21:58:14.191325Z'}"),
+            payload: Some(
+                b"{'offset': 5, 'orig_message_ts': '2023-09-26T21:58:14.191325Z'}".to_vec(),
+            ),
         };
 
-        let commit: Commit = payload.into();
-        assert_eq!(commit.partition.index, 0);
-        assert_eq!(commit.into(), payload);
+        let payload_clone = payload.clone();
+
+        let commit: Commit = payload.try_into().unwrap();
+        assert_eq!(commit.partition, 0);
+        let transformed: KafkaPayload = commit.try_into().unwrap();
+        assert_eq!(transformed.key, payload_clone.key);
+        assert_eq!(transformed.payload, payload_clone.payload);
     }
 }
