5f713efbdf9add59a2bf209f9d4394afaff34529
Rahul Saini <rahulsaini805@gmail.com>
2023-10-02 12:33:00 -0700

    api call

--- snuba/admin/static/welcome/index.tsx
+++ snuba/admin/static/welcome/index.tsx
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Client from "../api_client";
 
-function Welcome() {
+function Welcome(props: { api: Client }) {
   const AVAILABLE_REGIONS =
     "https://www.notion.so/sentry/Snuba-Admin-8344d5d508014d4a867cdb1b8e8d22cf";
+
+  const [adminRegions, setAdminRegions] = useState<string[]>([]);
+
+  useEffect(() => {
+    props.api.getAdminRegions().then((res) => {
+      setAdminRegions(res);
+    });
+  }, []);
 
   function urls() {
     return (
       <div>
-        <p>
-          Available{" "}
-          <a href={AVAILABLE_REGIONS} target="_blank">
-            regions
-          </a>
-        </p>
+        <p>Available regions:</p>
+        <ul>
+          <li>
+            <a href="https://snuba-admin.getsentry.net/" target="_blank">
+              SaaS
+            </a>
+          </li>
+          {adminRegions.map((region) => (
+            <li>
+              <a
+                href={"https://snuba-admin." + region + ".getsentry.net/"}
+                target="_blank"
+              >
+                {region}
+              </a>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
