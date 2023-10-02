b0a4b7d514d4e5fde73585cf0dad914d0906c7e5
Rahul Saini <rahulsaini805@gmail.com>
2023-10-02 12:09:02 -0700

    top bar

--- snuba/admin/static/welcome/index.tsx
+++ snuba/admin/static/welcome/index.tsx
@@ -1,23 +1,12 @@
 import React from "react";
 
 function Welcome() {
-  const PROD_URL_START = "https://snuba-admin";
-
   const AVAILABLE_REGIONS =
     "https://www.notion.so/sentry/Snuba-Admin-8344d5d508014d4a867cdb1b8e8d22cf";
 
-  const URL = window.location.origin;
-
   function urls() {
-    let current_region = "localhost";
-    if (URL.startsWith(PROD_URL_START)) {
-      current_region = currentRegion();
-    }
     return (
       <div>
-        <p>
-          Current region: <strong>{current_region}</strong>
-        </p>
         <p>
           Available{" "}
           <a href={AVAILABLE_REGIONS} target="_blank">
@@ -26,20 +15,6 @@ function Welcome() {
         </p>
       </div>
     );
-  }
-
-  function currentRegion() {
-    if (URL.startsWith(PROD_URL_START + ".getsentry.net")) {
-      return "SaaS";
-    }
-
-    let regex = /https:\/\/snuba-admin\.(.*?)\.getsentry\.net/;
-    let match = URL.match(regex);
-
-    if (match && match[1]) {
-      return match[1];
-    }
-    return "<unknown current region>";
   }
 
   return <div>{urls()}</div>;
