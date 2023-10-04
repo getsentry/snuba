a19eb77fa019072ece22e41d3d9237991febb845
Richard Gibert <richard@gibert.ca>
2023-10-04 11:23:52 -0400

    fix(gocd): Add snuba-admin to ST deployments
    Add snuba-admin to ST deployments.
    Use LABEL_SELECTOR var for consistency with SaaS deployments.

--- gocd/templates/bash/deploy-st.sh
+++ gocd/templates/bash/deploy-st.sh
@@ -6,12 +6,14 @@ eval $(/devinfra/scripts/regions/project_env_vars.py --region="${SENTRY_REGION}"
 
 /devinfra/scripts/k8s/k8s-deploy.py \
   --context="gke_${GCP_PROJECT}_${GKE_REGION}-${GKE_CLUSTER_ZONE}_${GKE_CLUSTER}" \
-  --label-selector="service=snuba" \
+  --label-selector="${LABEL_SELECTOR}" \
   --image="us.gcr.io/sentryio/snuba:${GO_REVISION_SNUBA_REPO}" \
-  --container-name="snuba"
+  --container-name="snuba" \
+  --container-name="snuba-admin"
 
 /devinfra/scripts/k8s/k8s-deploy.py \
-  --label-selector="service=snuba" \
+  --label-selector="${LABEL_SELECTOR}" \
   --image="us.gcr.io/sentryio/snuba:${GO_REVISION_SNUBA_REPO}" \
   --type="cronjob" \
   --container-name="cleanup"
+
