a62ed3e64f4ac815d11ceadcefb1df448f232464
Lyn <lyn.nagara@gmail.com>
2023-10-11 15:37:20 -0700

    feat: Reduce logging in Rust consumers
    Logging is expensive, let's stop writing the most useless logs. we already have a metric for this anyway.

--- rust_snuba/rust_arroyo/src/processing/strategies/run_task_in_threads.rs
+++ rust_snuba/rust_arroyo/src/processing/strategies/run_task_in_threads.rs
@@ -112,7 +112,6 @@ impl<TPayload: Clone + Send + Sync, TTransformed: Clone + Send + Sync + 'static>
         }
 
         if self.handles.len() > self.concurrency {
-            log::warn!("Reached max concurrency, rejecting message");
             return Err(MessageRejected { message });
         }
 
