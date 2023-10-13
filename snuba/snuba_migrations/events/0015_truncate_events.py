b73b22b135d53d9843b66dea151a1200b6da28ad
Lyn <lyn.nagara@gmail.com>
2023-10-13 14:51:51 -0700

    more squash

--- snuba/snuba_migrations/events/0015_truncate_events.py
+++ snuba/snuba_migrations/events/0015_truncate_events.py
@@ -1,28 +1,5 @@
-from typing import Sequence
-
-from snuba.clusters.storage_sets import StorageSetKey
-from snuba.migrations import migration, operations
+from snuba.migrations import migration
 
 
-class Migration(migration.ClickhouseNodeMigrationLegacy):
-    """
-    Truncate the events table. Cannot be reversed.
-    """
-
-    blocking = False
-
-    def forwards_local(self) -> Sequence[operations.SqlOperation]:
-        return [
-            operations.TruncateTable(
-                storage_set=StorageSetKey.EVENTS, table_name="sentry_local"
-            )
-        ]
-
-    def backwards_local(self) -> Sequence[operations.SqlOperation]:
-        return []
-
-    def forwards_dist(self) -> Sequence[operations.SqlOperation]:
-        return []
-
-    def backwards_dist(self) -> Sequence[operations.SqlOperation]:
-        return []
+class Migration(migration.SquashedMigration):
+    pass
