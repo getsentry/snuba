17f6b5cb30e3ba8bfa6b2f4ac3b587cb4da57e7b
Dalitso Banda <dalitso.banda@sentry.io>
2023-10-13 15:51:30 -0700

    stateful sets need own deploys

--- gocd/templates/bash/deploy.sh
+++ gocd/templates/bash/deploy.sh
@@ -53,7 +53,6 @@ eval $(/devinfra/scripts/regions/project_env_vars.py --region="${SENTRY_REGION}"
   --container-name="rust-profiles-consumer" \
   --container-name="rust-profiling-functions-consumer" \
   --container-name="spans-exp-static-off" \
-  --container-name="spans-exp-static-on" \
   --container-name="dlq-consumer" \
   --container-name="group-attributes-consumer" \
 && /devinfra/scripts/k8s/k8s-deploy.py \
@@ -61,4 +60,9 @@ eval $(/devinfra/scripts/regions/project_env_vars.py --region="${SENTRY_REGION}"
   --image="us.gcr.io/sentryio/snuba:${GO_REVISION_SNUBA_REPO}" \
   --type="cronjob" \
   --container-name="cleanup" \
-  --container-name="optimize"
+  --container-name="optimize" \
+&& /devinfra/scripts/k8s/k8s-deploy.py \
+  --label-selector="${LABEL_SELECTOR}" \
+  --image="us.gcr.io/sentryio/snuba:${GO_REVISION_SNUBA_REPO}" \
+  --type="statefulset" \
+  --container-name="spans-exp-static-on"
