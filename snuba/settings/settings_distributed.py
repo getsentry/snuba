4caac6502da31eeb19b494641b22ad915c0be74a
Riya Chakraborty <riya.chakraborty@sentry.io>
2023-10-18 10:47:31 -0700

    add gauges storage set locally

--- snuba/settings/settings_distributed.py
+++ snuba/settings/settings_distributed.py
@@ -28,6 +28,7 @@ CLUSTERS = [
             "generic_metrics_counters",
             "spans",
             "group_attributes",
+            "generic_metrics_gauges",
         },
         "single_node": False,
         "cluster_name": "cluster_one_sh",
