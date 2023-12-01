b2198f1c2bf7733d3c7a17cdc8a752fe58523455
Volo Kluev <volo.kluev@sentry.io>
2023-11-30 16:57:55 -0800

    change comment

--- tests/admin/test_production_queries.py
+++ tests/admin/test_production_queries.py
@@ -16,9 +16,7 @@ def test_validate_projects_with_subquery() -> None:
 
 
 def test_disallowed_project_ids() -> None:
-    # make sure that a subquery does not raise an exception when it specifies project ids
-    # the larger query does not specify the project id and this is still considered valid because the subquery filters
-    # on project_id = 1
+    # project_id 42069 is not an allowed project id, make sure we don't allow the query through
     query = """MATCH (events) SELECT time, group_id, count() AS event_count BY time, group_id WHERE timestamp >= toDateTime('2023-11-20T16:02:34.565803') AND timestamp < toDateTime('2023-11-27T16:02:34.565803') AND project_id=42069 HAVING event_count > 1 ORDER BY time ASC GRANULARITY 3600"""
     with pytest.raises(InvalidQueryException):
         prod_queries._validate_projects_in_query(
