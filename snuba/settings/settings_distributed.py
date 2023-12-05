00c381726ff6433dd63a668d72cfe4f5692d745b
Pierre Massat <pierre.massat@sentry.io>
2023-12-05 14:33:11 -0500

    Register settings

--- snuba/settings/settings_distributed.py
+++ snuba/settings/settings_distributed.py
@@ -29,6 +29,7 @@ CLUSTERS = [
             "spans",
             "group_attributes",
             "generic_metrics_gauges",
+            "metrics_summaries",
         },
         "single_node": False,
         "cluster_name": "cluster_one_sh",
