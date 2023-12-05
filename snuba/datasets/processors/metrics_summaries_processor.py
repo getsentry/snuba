bb60e16948096679876cbca2c3aa3a395b830447
Pierre Massat <pierre.massat@sentry.io>
2023-12-05 17:12:16 -0500

    Generate many metrics summary rows from one span message

--- snuba/datasets/processors/metrics_summaries_processor.py
+++ snuba/datasets/processors/metrics_summaries_processor.py
@@ -1,5 +1,6 @@
 import time
 import uuid
+from copy import deepcopy
 from datetime import datetime, timezone
 from typing import Any, Mapping, MutableMapping, MutableSequence, Optional, Tuple
 
@@ -25,7 +26,8 @@ logger = structlog.get_logger(__name__)
 
 metrics = MetricsWrapper(environment.metrics, "metrics_summaries.processor")
 
-MetricsSummaryEvent = MutableMapping[str, Any]
+SpanEvent = MutableMapping[str, Any]
+MetricsSummaries = MutableSequence[MutableMapping[str, Any]]
 RetentionDays = int
 
 
@@ -43,8 +45,8 @@ class MetricsSummariesMessageProcessor(DatasetMessageProcessor):
 
     @staticmethod
     def _structure_and_validate_message(
-        message: MetricsSummaryEvent,
-    ) -> Optional[Tuple[MetricsSummaryEvent, RetentionDays]]:
+        message: SpanEvent,
+    ) -> Optional[Tuple[SpanEvent, RetentionDays]]:
         try:
             # We are purposely using a naive datetime here to work with the
             # rest of the codebase. We can be confident that clients are only
@@ -60,55 +62,56 @@ class MetricsSummariesMessageProcessor(DatasetMessageProcessor):
 
     def _process_metrics_summary_event(
         self,
-        processed: MutableMapping[str, Any],
-        metrics_summary_event: MetricsSummaryEvent,
-    ) -> None:
-        processed["trace_id"] = str(uuid.UUID(metrics_summary_event["trace_id"]))
-        processed["span_id"] = int(metrics_summary_event["span_id"], 16)
-        processed["project_id"] = metrics_summary_event["project_id"]
-        processed["metric_id"] = metrics_summary_event["metric_id"]
+        span_event: SpanEvent,
+        retention_days: Optional[RetentionDays],
+    ) -> MetricsSummaries:
+        common_fields = {
+            "deleted": 0,
+            "end_timestamp": self.__extract_timestamp(span_event["end_timestamp"]),
+            "project_id": span_event["project_id"],
+            "retention_days": retention_days,
+            "span_id": int(span_event["span_id"], 16),
+            "trace_id": str(uuid.UUID(span_event["trace_id"])),
+        }
 
-        processed["start_timestamp"] = self.__extract_timestamp(
-            metrics_summary_event["start_timestamp"],
+        tags: Mapping[str, Any] = _as_dict_safe(span_event.get("tags", None))
+        common_fields["tags.key"], common_fields["tags.value"] = extract_extra_tags(
+            tags
         )
-        for key in {"min", "max", "sum", "count"}:
-            processed[key] = float(metrics_summary_event[key])
 
-        tags: Mapping[str, Any] = _as_dict_safe(metrics_summary_event.get("tags", None))
-        processed["tags.key"], processed["tags.value"] = extract_extra_tags(tags)
+        processed_rows: MetricsSummaries = []
+
+        for metric_name, metric_values in span_event.get("metrics_summary", {}).items():
+            for metric_value in metric_values:
+                processed = deepcopy(common_fields)
+                processed["metric_id"] = metric_name
+                for key in {"min", "max", "sum", "count"}:
+                    processed[key] = float(metric_value[key])
+
+        return processed_rows
 
     def process_message(
         self,
-        message: MetricsSummaryEvent,
+        message: SpanEvent,
         metadata: KafkaMessageMetadata,
     ) -> Optional[ProcessedMessage]:
-        metrics_summary_event, retention_days = self._structure_and_validate_message(
-            message
-        ) or (
+        span_event, retention_days = self._structure_and_validate_message(message) or (
             None,
             None,
         )
-        if not metrics_summary_event:
+        if not span_event:
             return None
-
-        processed_rows: MutableSequence[MutableMapping[str, Any]] = []
-        processed: MutableMapping[str, Any] = {
-            "deleted": 0,
-            "retention_days": retention_days,
-            "partition": metadata.partition,
-            "offset": metadata.offset,
-        }
-
         try:
-            self._process_metrics_summary_event(processed, metrics_summary_event)
-            processed_rows.append(processed)
+            processed_rows = self._process_metrics_summary_event(
+                span_event, retention_days
+            )
         except Exception:
             metrics.increment("message_processing_error")
             return None
 
         received = (
-            datetime.fromtimestamp(metrics_summary_event["received"], tz=timezone.utc)
-            if "received" in metrics_summary_event
+            datetime.fromtimestamp(span_event["received"], tz=timezone.utc)
+            if "received" in span_event
             else None
         )
         return InsertBatch(rows=processed_rows, origin_timestamp=received)
