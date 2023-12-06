6baa848c3f45dece1e2925c68b7d9c4f469d6604
Markus Unterwaditzer <markus-tarpit+git@unterwaditzer.net>
2023-12-06 15:51:00 +0100

    fmt

--- rust_snuba/src/types.rs
+++ rust_snuba/src/types.rs
@@ -40,7 +40,11 @@ impl LatencyRecorder {
         let write_time = write_time.timestamp() as u64;
 
         let max_latency = write_time.saturating_sub(self.max_secs) * 1000;
-        metrics.timing(&format!("insertions.max_{}_ms", metric_name), max_latency * 1000, None);
+        metrics.timing(
+            &format!("insertions.max_{}_ms", metric_name),
+            max_latency * 1000,
+            None,
+        );
 
         let latency = write_time as f64 - (self.sum_secs / self.num_values as f64);
         let latency = (latency * 1000.0) as u64;
