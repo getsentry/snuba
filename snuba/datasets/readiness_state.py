2efaced11d4c7cc129b74a0e93bec3fb5b2afdb1
Evan Hicks <evan.hicks@sentry.io>
2023-12-07 11:20:25 -0500

    ref: Add experimental readiness state
    This state is meant for experimental features that should only be used locally
    or in S4S.

--- snuba/datasets/readiness_state.py
+++ snuba/datasets/readiness_state.py
@@ -8,9 +8,16 @@ class ReadinessState(Enum):
     DEPRECATE = "deprecate"
     PARTIAL = "partial"
     COMPLETE = "complete"
+    EXPERIMENTAL = "experimental"
 
     def __init__(self, _: str) -> None:
-        self.order = {"limited": 1, "deprecate": 2, "partial": 3, "complete": 4}
+        self.order = {
+            "limited": 1,
+            "deprecate": 2,
+            "experimental": 3,
+            "partial": 4,
+            "complete": 5,
+        }
 
     def __gt__(self, other_rs: ReadinessState) -> bool:
         return self.order[self.value] > self.order[other_rs.value]
