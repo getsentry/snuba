a2d7eb042d1edd7b09b1e740346b261df17e92b8
Sebastian Zivota <loewenheim@mailbox.org>
2023-12-12 17:48:59 +0100

    WIP

--- rust_snuba/rust_arroyo/src/processing/strategies/produce.rs
+++ rust_snuba/rust_arroyo/src/processing/strategies/produce.rs
@@ -87,13 +87,60 @@ impl ProcessingStrategy<KafkaPayload> for Produce {
 
 #[cfg(test)]
 mod tests {
+    use std::sync::Mutex;
+    use std::time::SystemTime;
+
     use super::*;
     use crate::backends::kafka::config::KafkaConfig;
     use crate::backends::kafka::producer::KafkaProducer;
     use crate::backends::kafka::InitialOffset;
+    use crate::backends::local::broker::LocalBroker;
+    use crate::backends::local::LocalProducer;
+    use crate::backends::storages::memory::MemoryMessageStorage;
     use crate::processing::strategies::InvalidMessage;
     use crate::types::{BrokerMessage, InnerMessage, Partition, Topic};
+    use crate::utils::clock::TestingClock;
     use chrono::Utc;
+
+    #[derive(Debug, Clone, Copy, Default, PartialEq, Eq)]
+    struct Counts {
+        submit: u8,
+        poll: u8,
+    }
+
+    struct Mock(Arc<Mutex<Counts>>);
+
+    impl Mock {
+        fn new() -> Self {
+            Self(Arc::new(Mutex::new(Default::default())))
+        }
+
+        fn counts(&self) -> Arc<Mutex<Counts>> {
+            self.0.clone()
+        }
+    }
+
+    impl ProcessingStrategy<KafkaPayload> for Mock {
+        fn poll(&mut self) -> Result<Option<CommitRequest>, InvalidMessage> {
+            self.0.lock().unwrap().poll += 1;
+            Ok(None)
+        }
+        fn submit(
+            &mut self,
+            _message: Message<KafkaPayload>,
+        ) -> Result<(), SubmitError<KafkaPayload>> {
+            self.0.lock().unwrap().submit += 1;
+            Ok(())
+        }
+        fn close(&mut self) {}
+        fn terminate(&mut self) {}
+        fn join(
+            &mut self,
+            _timeout: Option<Duration>,
+        ) -> Result<Option<CommitRequest>, InvalidMessage> {
+            Ok(None)
+        }
+    }
 
     #[test]
     fn test_produce() {
@@ -151,5 +198,67 @@ mod tests {
         strategy.submit(message).unwrap();
         strategy.close();
         let _ = strategy.join(None);
+    }
+
+    #[test]
+    fn test_produce_local() {
+        let orig_topic = Topic::new("orig-topic");
+        let result_topic = Topic::new("result-topic");
+        let clock = TestingClock::new(SystemTime::now());
+        let storage = MemoryMessageStorage::default();
+        let mut broker = LocalBroker::new(Box::new(storage), Box::new(clock));
+        broker.create_topic(result_topic, 1).unwrap();
+
+        let broker = Arc::new(Mutex::new(broker));
+        let mut producer = LocalProducer::new(broker.clone());
+
+        let next_step = Mock::new();
+        let counts = next_step.counts();
+        let mut strategy = Produce::new(
+            next_step,
+            producer,
+            &ConcurrencyConfig::new(10),
+            result_topic.into(),
+        );
+
+        let value = br#"{"something": "something"}"#.into();
+        let data = KafkaPayload::new(None, None, Some(value));
+        let now = chrono::Utc::now();
+
+        let message = Message::new_broker_message(data, Partition::new(orig_topic, 0), 1, now);
+        strategy.submit(message).unwrap();
+
+        let produced_message = broker
+            .lock()
+            .unwrap()
+            .storage_mut()
+            .consume(&Partition::new(result_topic, 0), 0)
+            .unwrap()
+            .unwrap();
+
+        assert_eq!(produced_message.payload.payload().unwrap(), &value);
+
+        assert!(broker
+            .lock()
+            .unwrap()
+            .storage_mut()
+            .consume(&Partition::new(result_topic, 0), 1)
+            .unwrap()
+            .is_none());
+
+        assert_eq!(*counts.lock().unwrap(), Counts { submit: 0, poll: 0 });
+
+        strategy.poll();
+        assert_eq!(*counts.lock().unwrap(), Counts { submit: 1, poll: 1 });
+
+        strategy.submit(message).unwrap();
+        strategy.poll();
+        assert_eq!(*counts.lock().unwrap(), Counts { submit: 2, poll: 2 });
+
+        for _ in 0..3 {
+            assert!(strategy.submit(message).is_err());
+        }
+
+        strategy.join(None);
     }
 }
