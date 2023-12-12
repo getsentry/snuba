22638c27cbae97391aa4532bca87a4ef90fa55f9
Volo Kluev <volo.kluev@sentry.io>
2023-12-11 16:15:21 -0800

    add comment, mypy

--- snuba/query/processors/logical/calculated_average_processor.py
+++ snuba/query/processors/logical/calculated_average_processor.py
@@ -1,3 +1,5 @@
+from typing import cast
+
 from snuba.query.expressions import Column, Expression, FunctionCall
 from snuba.query.logical import Query
 from snuba.query.matchers import Column as ColumnMatch
@@ -8,6 +10,13 @@ from snuba.query.query_settings import QuerySettings
 
 
 class CalculatedAverageProcessor(LogicalQueryProcessor):
+    """Transforms the expression avg(value) -> sum(value) / count(value)
+
+    This processor was introduced for the gauges entity which has sum and count fields
+    but not avg. This processor provides syntactic sugar for the product to be able to avg gauges.
+
+    """
+
     def process_query(self, query: Query, query_settings: QuerySettings) -> None:
         # use a matched to find something like avg(value)
         matcher = FunctionCallMatch(
@@ -18,7 +27,7 @@ class CalculatedAverageProcessor(LogicalQueryProcessor):
         def transform_expression(exp: Expression) -> Expression:
             match = matcher.match(exp)
             if match is not None:
-                column = match.expression("avg_column")
+                column = cast(Column, match.expression("avg_column"))
                 return FunctionCall(
                     alias=exp.alias,
                     function_name="divide",
