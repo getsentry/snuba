5f1b51fd7f2510767b6af519e283a646519f66fc
Pierre Massat <pierre.massat@sentry.io>
2023-12-15 15:30:42 -0800

    feat(metrics-summaries): Deploy metrics-summaries consumer automatically

--- gocd/templates/bash/deploy.sh
+++ gocd/templates/bash/deploy.sh
@@ -53,6 +53,7 @@ eval $(/devinfra/scripts/regions/project_env_vars.py --region="${SENTRY_REGION}"
   --container-name="spans-exp-static-off" \
   --container-name="dlq-consumer" \
   --container-name="group-attributes-consumer" \
+  --container-name="metrics-summaries-consumer" \
 && /devinfra/scripts/k8s/k8s-deploy.py \
   --label-selector="${LABEL_SELECTOR}" \
   --image="us.gcr.io/sentryio/snuba:${GO_REVISION_SNUBA_REPO}" \
