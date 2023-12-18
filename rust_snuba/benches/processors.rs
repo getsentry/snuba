5ec4d864320e15131c737736b85c71dbcf0439ed
Lyn <lyn.nagara@gmail.com>
2023-12-18 15:28:53 -0800

    .

--- rust_snuba/benches/processors.rs
+++ rust_snuba/benches/processors.rs
@@ -1,7 +1,6 @@
 use std::collections::HashMap;
-use std::sync::{Arc, Once};
+use std::sync::{Arc, Mutex, Once};
 use std::time::Duration;
-use parking_lot::Mutex;
 
 use divan::counter::ItemsCount;
 use once_cell::sync::Lazy;
