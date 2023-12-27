1607af443a709693a0895a2c15cdc2cbbbbf985b
Markus Unterwaditzer <markus-tarpit+git@unterwaditzer.net>
2023-12-27 17:27:15 +0100

    remove unused logger

--- snuba/datasets/processors/querylog_processor.py
+++ snuba/datasets/processors/querylog_processor.py
@@ -1,10 +1,6 @@
 from __future__ import annotations
 
-import logging
-
 from snuba.datasets.processors.rust_compat_processor import RustCompatProcessor
-
-logger = logging.getLogger(__name__)
 
 
 class QuerylogProcessor(RustCompatProcessor):
