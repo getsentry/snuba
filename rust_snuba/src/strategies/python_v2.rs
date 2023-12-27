89c73f64e2d06203993f3e2e4d97d221cba34b49
Lyn Nagara <lyn.nagara@gmail.com>
2023-12-27 10:11:34 -0800

    feat(rust): Python multiprocessing v2
    This is an attempt to call the RunTaskWithMultiprocessing stratagy
    from Arroyo from a Rust consumer.

---
+++ rust_snuba/src/strategies/python_v2.rs
@@ -0,0 +1,289 @@
+use crate::config::MessageProcessorConfig;
+
+use crate::types::{BytesInsertBatch, RowData};
+use anyhow::Error;
+use chrono::{DateTime, Utc};
+use parking_lot::Mutex;
+use pyo3::prelude::*;
+use rust_arroyo::backends::kafka::types::KafkaPayload;
+use rust_arroyo::processing::strategies::{
+    merge_commit_request, CommitRequest, InvalidMessage, MessageRejected, ProcessingStrategy,
+    SubmitError,
+};
+use rust_arroyo::types::{BrokerMessage, InnerMessage, Message, Partition, Topic};
+use rust_arroyo::utils::timing::Deadline;
+use std::collections::{BTreeMap, VecDeque};
+use std::sync::Arc;
+use std::time::Duration;
+
+type ReturnValue = (Vec<Vec<u8>>, Option<DateTime<Utc>>, Option<DateTime<Utc>>);
+type MessageTimestamp = DateTime<Utc>;
+type Committable = BTreeMap<(String, u16), u64>;
+type PyReturnValue = (ReturnValue, MessageTimestamp, Committable);
+
+pub struct PythonTransformStep {
+    next_step: Box<dyn ProcessingStrategy<BytesInsertBatch>>,
+    // processor_config: MessageProcessorConfig,
+    python_strategy: Arc<Mutex<Py<PyAny>>>,
+    transformed_messages: VecDeque<Message<BytesInsertBatch>>,
+    commit_request_carried_over: Option<CommitRequest>,
+}
+
+impl PythonTransformStep {
+    pub fn new<N>(next_step: N, processor_config: MessageProcessorConfig) -> Result<Self, Error>
+    where
+        N: ProcessingStrategy<BytesInsertBatch> + 'static,
+    {
+        let instance = Python::with_gil(|py| -> PyResult<Py<PyAny>> {
+            let module = PyModule::import(py, "snuba.consumers.rust_processor")?;
+
+            let initializer: Py<PyAny> = module.getattr("initialize_processor")?.into();
+
+            initializer.call1(
+                py,
+                (
+                    processor_config.python_module.clone(),
+                    processor_config.python_class_name.clone(),
+                ),
+            )?;
+
+            let cls: Py<PyAny> = module.getattr("RunPythonMultiprocessing")?.into();
+            cls.call0(py)
+        })?;
+
+        Ok(Self {
+            next_step: Box::new(next_step),
+            // processor_config: processor_config,
+            python_strategy: Arc::new(Mutex::new(instance)),
+            transformed_messages: VecDeque::new(),
+            commit_request_carried_over: None,
+        })
+    }
+    fn handle_py_return_value(&mut self, messages: Vec<PyReturnValue>) {
+        // Used internally by "poll" and "join" to populate `self.transformed_messages` from Python return values
+        // TODO: Handle invalid message
+        for py_message in messages {
+            let (
+                (payload, origin_timestamp, sentry_received_timestamp),
+                message_timestamp,
+                offsets,
+            ) = py_message;
+
+            let commit_log_offsets = offsets
+                .iter()
+                .map(|((_t, p), o)| (*p, (*o, message_timestamp)))
+                .collect();
+
+            let payload = BytesInsertBatch::new(
+                RowData::from_encoded_rows(payload),
+                message_timestamp,
+                origin_timestamp,
+                sentry_received_timestamp,
+                commit_log_offsets,
+            );
+
+            let mut committable: BTreeMap<Partition, u64> = BTreeMap::new();
+            for ((t, p), o) in offsets {
+                committable.insert(Partition::new(Topic::new(&t), p), o);
+            }
+
+            let message = Message::new_any_message(payload, committable);
+            self.transformed_messages.push_back(message);
+        }
+    }
+}
+
+impl ProcessingStrategy<KafkaPayload> for PythonTransformStep {
+    fn poll(&mut self) -> Result<Option<CommitRequest>, InvalidMessage> {
+        let messages = Python::with_gil(|py| -> PyResult<Vec<PyReturnValue>> {
+            let python_strategy = self.python_strategy.lock();
+            let result = python_strategy.call_method0(py, "poll")?;
+            Ok(result.extract(py).unwrap())
+        })
+        .unwrap();
+
+        self.handle_py_return_value(messages);
+
+        // Attempt to submit all transformed messages
+        while let Some(msg) = self.transformed_messages.pop_front() {
+            let commit_request = self.next_step.poll()?;
+            self.commit_request_carried_over =
+                merge_commit_request(self.commit_request_carried_over.take(), commit_request);
+
+            match self.next_step.submit(msg) {
+                Err(SubmitError::MessageRejected(MessageRejected {
+                    message: transformed_message,
+                })) => {
+                    self.transformed_messages.push_front(transformed_message);
+                    break;
+                }
+                Err(SubmitError::InvalidMessage(invalid_message)) => {
+                    return Err(invalid_message);
+                }
+                Ok(_) => {}
+            }
+        }
+
+        let commit_request = self.next_step.poll()?;
+
+        Ok(merge_commit_request(
+            self.commit_request_carried_over.take(),
+            commit_request,
+        ))
+    }
+
+    fn submit(&mut self, message: Message<KafkaPayload>) -> Result<(), SubmitError<KafkaPayload>> {
+        if self.transformed_messages.len() > 100_000 {
+            return Err(SubmitError::MessageRejected(MessageRejected { message }));
+        }
+
+        let mut apply_backpressure = false;
+
+        let _ = Python::with_gil(|py| -> PyResult<()> {
+            let python_strategy = self.python_strategy.lock();
+            match message.clone().inner_message {
+                InnerMessage::AnyMessage(..) => {
+                    // Snuba message processors, as their interface is defined in Python, expect a
+                    // single, specific partition/offset combination for every payload.
+                    panic!("AnyMessage cannot be processed by a message processor");
+                }
+                InnerMessage::BrokerMessage(BrokerMessage {
+                    payload,
+                    offset,
+                    partition,
+                    timestamp,
+                }) => {
+                    // XXX: Python message processors do not support null payload, even though this is valid in
+                    // Kafka so we convert it to an empty vec.
+                    let payload_bytes = payload.payload().cloned().unwrap_or_default();
+                    let args = (
+                        payload_bytes,
+                        partition.topic.as_str(),
+                        offset,
+                        partition.index,
+                        timestamp,
+                    );
+
+                    let rv: u8 = python_strategy
+                        .call_method1(py, "submit", args)?
+                        .extract(py)?;
+
+                    match rv {
+                        1 => {
+                            apply_backpressure = true;
+                        }
+                        _ => {
+                            tracing::debug!("successfully submitted to multiprocessing");
+                        }
+                    }
+                }
+            };
+            Ok(())
+        });
+
+        if apply_backpressure {
+            return Err(SubmitError::MessageRejected(MessageRejected { message }));
+        }
+
+        Ok(())
+    }
+
+    fn close(&mut self) {}
+
+    fn terminate(&mut self) {
+        self.next_step.terminate()
+    }
+
+    fn join(&mut self, timeout: Option<Duration>) -> Result<Option<CommitRequest>, InvalidMessage> {
+        let deadline = timeout.map(Deadline::new);
+        let timeout_secs = timeout.map(|d| d.as_secs()).unwrap();
+
+        let messages = Python::with_gil(|py| -> PyResult<Vec<PyReturnValue>> {
+            let python_strategy = self.python_strategy.lock();
+            let result = python_strategy
+                .call_method1(py, "join", (timeout_secs,))
+                .unwrap();
+
+            Ok(result.extract(py).unwrap())
+        })
+        .unwrap();
+
+        self.handle_py_return_value(messages);
+
+        // Attempt to submit all transformed messages up to the deadline
+        while let Some(msg) = self.transformed_messages.pop_front() {
+            let commit_request = self.next_step.poll()?;
+            self.commit_request_carried_over =
+                merge_commit_request(self.commit_request_carried_over.take(), commit_request);
+
+            match self.next_step.submit(msg) {
+                Err(SubmitError::MessageRejected(MessageRejected {
+                    message: transformed_message,
+                })) => {
+                    self.transformed_messages.push_front(transformed_message);
+                    if deadline.map_or(false, |d| d.has_elapsed()) {
+                        tracing::warn!("Timeout reached");
+                        break;
+                    }
+                }
+                Err(SubmitError::InvalidMessage(invalid_message)) => {
+                    return Err(invalid_message);
+                }
+                Ok(_) => {}
+            }
+        }
+
+        self.next_step.close();
+        let next_commit = self.next_step.join(timeout)?;
+        Ok(merge_commit_request(
+            self.commit_request_carried_over.take(),
+            next_commit,
+        ))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rust_arroyo::testutils::TestStrategy;
+
+    fn set_sys_executable() {
+        let python_executable = std::env::var("PYTHONEXECUTABLE").unwrap();
+        Python::with_gil(|py| -> PyResult<()> {
+            PyModule::import(py, "sys")?.setattr("executable", python_executable)?;
+            Ok(())
+        })
+        .unwrap();
+    }
+
+    #[test]
+    fn test_python() {
+        set_sys_executable();
+
+        let sink = TestStrategy::new();
+
+        let processor_config = MessageProcessorConfig {
+            python_class_name: "OutcomesProcessor".to_owned(),
+            python_module: "snuba.datasets.processors.outcomes_processor".to_owned(),
+        };
+
+        let mut step = PythonTransformStep::new(sink.clone(), processor_config).unwrap();
+        let _ = step.poll();
+        step.submit(Message::new_broker_message(
+            KafkaPayload::new(
+                None,
+                None,
+                Some(br#"{ "timestamp": "2023-03-28T18:50:44.000000Z", "org_id": 1, "project_id": 1, "key_id": 1, "outcome": 1, "reason": "discarded-hash", "event_id": "4ff942d62f3f4d5db9f53b5a015b5fd9", "category": 1, "quantity": 1 }"#.to_vec()),
+            ),
+            Partition::new(Topic::new("test"), 1),
+            1,
+            Utc::now(),
+        ))
+        .unwrap();
+        step.poll().unwrap();
+
+        let _ = step.join(Some(Duration::from_secs(10)));
+
+        assert_eq!(sink.messages.lock().unwrap().len(), 1);
+    }
+}
--- snuba/consumers/rust_processor.py
+++ snuba/consumers/rust_processor.py
@@ -14,14 +14,28 @@
 import importlib
 import os
 from datetime import datetime
-from typing import Optional, Sequence, Tuple, Type
+from typing import Optional, Sequence, Tuple, Type, Deque, Union, Mapping, cast
+from collections import deque
+import logging
 
+from arroyo.dlq import InvalidMessage
+from arroyo.processing.strategies import MessageRejected
+from arroyo.types import Message, BrokerValue, Topic, Partition, FilteredPayload
+from arroyo.processing.strategies import ProcessingStrategy
 import rapidjson
 
 from snuba.consumers.consumer import json_row_encoder
 from snuba.consumers.types import KafkaMessageMetadata
 from snuba.datasets.processors import DatasetMessageProcessor
 from snuba.processor import InsertBatch
+
+from arroyo.processing.strategies.run_task_with_multiprocessing import (
+    # RunTaskWithMultiprocessing,
+    MultiprocessingPool,
+)
+from arroyo.processing.strategies.run_task import RunTask
+
+logger = logging.getLogger(__name__)
 
 processor: Optional[DatasetMessageProcessor] = None
 
@@ -66,3 +80,172 @@ def process_rust_message(
         rv.origin_timestamp,
         rv.sentry_received_timestamp,
     )
+
+
+Committable = Mapping[Tuple[str, int], int]
+
+MessageTimestamp = datetime
+
+ReturnValue = Tuple[Sequence[bytes], Optional[datetime], Optional[datetime]]
+
+ReturnValueWithCommittable = Tuple[ReturnValue, MessageTimestamp, Committable]
+
+
+def wrap_process_message(
+    message: Message[bytes],
+) -> Union[FilteredPayload, ReturnValue]:
+    value = message.value
+    assert isinstance(value, BrokerValue)
+
+    return process_rust_message(
+        value.payload, value.offset, value.partition.index, value.timestamp
+    )
+
+
+class TransformedMessages:
+    """
+    Transformed messages to be returned to Rust
+    """
+
+    def __init__(self) -> None:
+        self.messages: Deque[Message[ReturnValue]] = deque()
+
+    def append(self, value: Message[ReturnValue]) -> None:
+        self.messages.append(value)
+
+    def pop(self) -> Sequence[Message[ReturnValue]]:
+        """
+        Returns all the transformed messages and clears the internal buffer
+        """
+        messages = self.messages
+        self.messages = deque()
+        return messages
+
+
+class Next(ProcessingStrategy[Union[FilteredPayload, ReturnValue]]):
+    """
+    Messages are passed to this step from RunTaskWithMultiprocessing,
+    and they are added to transformed messages. They are removed and
+    passed back to Rust by the RunPythonMultiprocessing class.
+    """
+
+    def __init__(self, transformed_messages: TransformedMessages) -> None:
+        self.__transformed_messages = transformed_messages
+
+    def submit(self, message: Message[Union[FilteredPayload, ReturnValue]]) -> None:
+        self.__transformed_messages.append(cast(Message[ReturnValue], message))
+
+    def poll(self) -> None:
+        pass
+
+    def join(self, timeout: Optional[float] = None) -> None:
+        pass
+
+    def close(self) -> None:
+        pass
+
+    def terminate(self) -> None:
+        pass
+
+
+DEFAULT_BLOCK_SIZE = int(32 * 1e6)
+
+
+class RunPythonMultiprocessing:
+    """
+    Similar to Arroyo's RunTaskWithMultiprocessing class (which it uses internally),
+    but instead of submitting messages to a nested processing strategy, returns
+    transformed rows in poll() and join() instead. This makes it easier to use from
+    Rust.
+    """
+
+    def __init__(self) -> None:
+        self.__transformed_messages = TransformedMessages()
+        self.__next = Next(self.__transformed_messages)
+        transform_fn = wrap_process_message
+        self.__pool = MultiprocessingPool(1)
+        # Message is carried over if we got MessageRejected from the next step
+        self.__carried_over_message: Optional[Message[bytes]] = None
+
+        self.__inner = RunTask(
+            transform_fn,
+            self.__next,
+            # 1,
+            # 1,
+            # self.__pool,
+            # DEFAULT_BLOCK_SIZE,
+            # DEFAULT_BLOCK_SIZE,
+        )
+
+    def submit(
+        self,
+        payload: bytes,
+        topic: str,
+        offset: int,
+        partition: int,
+        timestamp: datetime,
+    ) -> int:
+        # HACK: There is probably a better way to handle exceptions in Rust
+        # 0 means message successfully submitted
+        # 1 means backpressure
+        # Invalid message is not currently supported
+        if self.__carried_over_message is not None:
+            return 1
+
+        message = Message(
+            BrokerValue(payload, Partition(Topic(topic), partition), offset, timestamp)
+        )
+        try:
+            self.__inner.submit(message)
+        except InvalidMessage:
+            # TODO: Handle invalid message
+            logger.warning("Invalid message", exc_info=True)
+        except MessageRejected:
+            self.__carried_over_message = message
+
+        return 0
+
+    def __get_transformed_messages(self) -> Sequence[ReturnValueWithCommittable]:
+        return [
+            (
+                m.payload,
+                cast(datetime, m.timestamp),
+                {
+                    (partition.topic.name, partition.index): offset
+                    for (partition, offset) in m.committable.items()
+                },
+            )
+            for m in self.__transformed_messages.pop()
+        ]
+
+    def poll(self) -> Sequence[ReturnValueWithCommittable]:
+        """
+        Returns all available transformed rows
+        """
+        try:
+            self.__inner.poll()
+        except InvalidMessage:
+            print("TODO: handle invalid message")
+
+        return self.__get_transformed_messages()
+
+    def join(
+        self, timeout: Optional[float] = None
+    ) -> Sequence[ReturnValueWithCommittable]:
+        """
+        Close and join inner strategy. Returns all available transformed rows
+        """
+        self.__inner.close()
+        try:
+            self.__inner.join(timeout=timeout)
+        except InvalidMessage:
+            # We cannot forward offsets here since the inner strategy is already
+            # marked as closed. Log the exception and move on. The message will get
+            # reprocessed properly when the consumer is restarted.
+            logger.warning("Invalid message in join", exc_info=True)
+        self.__pool.close()
+
+        return self.__get_transformed_messages()
+
+    def terminate(self) -> None:
+        self.__inner.terminate()
