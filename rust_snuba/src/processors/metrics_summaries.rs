219696464435347a97e8e5289080559f3129108f
Arpad Borsos <arpad.borsos@sentry.io>
2024-01-05 12:48:23 +0100

    Micro-optimize `metrics_summaries` (#5306)

--- rust_snuba/src/processors/metrics_summaries.rs
+++ rust_snuba/src/processors/metrics_summaries.rs
@@ -12,21 +12,25 @@ pub fn process_message(
     _: KafkaMessageMetadata,
 ) -> anyhow::Result<InsertBatch> {
     let payload_bytes = payload.payload().context("Expected payload")?;
-    let from: FromSpanMessage = serde_json::from_slice(payload_bytes)?;
+    let from: InputMessage = serde_json::from_slice(payload_bytes)?;
 
     let mut metrics_summaries: Vec<MetricsSummary> = Vec::new();
 
     let end_timestamp_ms = from.start_timestamp_ms + from.duration_ms as u64;
-    for (metric_mri, summaries) in from._metrics_summary {
+    for (metric_mri, summaries) in &from._metrics_summary {
         for summary in summaries {
-            let (tag_keys, tag_values) = summary.tags.into_iter().unzip();
+            let (tag_keys, tag_values) = summary
+                .tags
+                .iter()
+                .map(|(k, v)| (k.as_str(), v.as_str()))
+                .unzip();
 
             metrics_summaries.push(MetricsSummary {
                 count: summary.count,
                 deleted: 0,
                 end_timestamp: end_timestamp_ms / 1000,
                 max: summary.max,
-                metric_mri: metric_mri.clone(),
+                metric_mri,
                 min: summary.min,
                 project_id: from.project_id,
                 retention_days: from.retention_days.unwrap_or(DEFAULT_RETENTION_DAYS),
@@ -43,7 +47,7 @@ pub fn process_message(
 }
 
 #[derive(Debug, Default, Deserialize)]
-struct FromSpanMessage {
+struct InputMessage {
     #[serde(default)]
     _metrics_summary: BTreeMap<String, Vec<FromMetricsSummary>>,
     #[serde(default)]
@@ -68,21 +72,21 @@ struct FromMetricsSummary {
 }
 
 #[derive(Debug, Default, Serialize)]
-struct MetricsSummary {
+struct MetricsSummary<'a> {
     count: u64,
     deleted: u8,
     end_timestamp: u64,
     max: f64,
-    metric_mri: String,
+    metric_mri: &'a str,
     min: f64,
     project_id: u64,
     retention_days: u16,
     span_id: u64,
     sum: f64,
     #[serde(rename(serialize = "tags.key"))]
-    tag_keys: Vec<String>,
+    tag_keys: Vec<&'a str>,
     #[serde(rename(serialize = "tags.value"))]
-    tag_values: Vec<String>,
+    tag_values: Vec<&'a str>,
     trace_id: Uuid,
 }
 
@@ -90,80 +94,54 @@ struct MetricsSummary {
 mod tests {
     use super::*;
     use chrono::DateTime;
-    use std::collections::BTreeMap;
     use std::time::SystemTime;
-
-    #[derive(Debug, Default, Deserialize, Serialize)]
-    struct TestSpanMessage {
-        _metrics_summary: BTreeMap<String, Vec<FromMetricsSummary>>,
-        duration_ms: Option<u32>,
-        project_id: Option<u64>,
-        retention_days: Option<u16>,
-        span_id: Option<String>,
-        start_timestamp_ms: Option<u64>,
-        trace_id: Option<Uuid>,
-    }
-
-    fn valid_span() -> TestSpanMessage {
-        TestSpanMessage {
-            _metrics_summary: BTreeMap::new(),
-            duration_ms: Some(1000),
-            project_id: Some(1),
-            retention_days: Some(90),
-            span_id: Some("deadbeefdeadbeef".into()),
-            start_timestamp_ms: Some(1691105878720),
-            trace_id: Some(Uuid::new_v4()),
-        }
-    }
 
     #[test]
     fn test_valid_span() {
-        let mut span = valid_span();
-        span._metrics_summary = BTreeMap::from([
-            (
-                "c:sentry.events.outcomes@none".into(),
-                vec![FromMetricsSummary {
-                    count: 1,
-                    max: 1.0,
-                    min: 1.0,
-                    sum: 1.0,
-                    tags: BTreeMap::from([
-                        ("category".into(), "error".into()),
-                        ("environment".into(), "unknown".into()),
-                        ("event_type".into(), "error".into()),
-                        ("outcome".into(), "accepted".into()),
-                        (
-                            "release".into(),
-                            "backend@2af74c237fbd61489a1ccc46650f4f85befaf8b8".into(),
-                        ),
-                        ("topic".into(), "outcomes-billing".into()),
-                        ("transaction".into(), "sentry.tasks.store.save_event".into()),
-                    ]),
-                }],
-            ),
-            (
-                "c:sentry.events.post_save.normalize.errors@none".into(),
-                vec![FromMetricsSummary {
-                    count: 1,
-                    max: 0.0,
-                    min: 0.0,
-                    sum: 0.0,
-                    tags: BTreeMap::from([
-                        ("environment".into(), "unknown".into()),
-                        ("event_type".into(), "error".into()),
-                        ("from_relay".into(), "False".into()),
-                        (
-                            "release".into(),
-                            "backend@2af74c237fbd61489a1ccc46650f4f85befaf8b8".into(),
-                        ),
-                        ("transaction".into(), "sentry.tasks.store.save_event".into()),
-                    ]),
-                }],
-            ),
-        ]);
-        let data = serde_json::to_string(&span);
-        assert!(data.is_ok());
-        let payload = KafkaPayload::new(None, None, Some(data.unwrap().as_bytes().to_vec()));
+        let span = br#"{
+          "_metrics_summary": {
+            "c:sentry.events.outcomes@none": [
+              {
+                "min": 1.0,
+                "max": 1.0,
+                "sum": 1.0,
+                "count": 1,
+                "tags": {
+                  "category": "error",
+                  "environment": "unknown",
+                  "event_type": "error",
+                  "outcome": "accepted",
+                  "release": "backend@2af74c237fbd61489a1ccc46650f4f85befaf8b8",
+                  "topic": "outcomes-billing",
+                  "transaction": "sentry.tasks.store.save_event"
+                }
+              }
+            ],
+            "c:sentry.events.post_save.normalize.errors@none": [
+              {
+                "min": 0.0,
+                "max": 0.0,
+                "sum": 0.0,
+                "count": 1,
+                "tags": {
+                  "environment": "unknown",
+                  "event_type": "error",
+                  "from_relay": "False",
+                  "release": "backend@2af74c237fbd61489a1ccc46650f4f85befaf8b8",
+                  "transaction": "sentry.tasks.store.save_event"
+                }
+              }
+            ]
+          },
+          "duration_ms": 1000,
+          "project_id": 1,
+          "retention_days": 90,
+          "span_id": "deadbeefdeadbeef",
+          "start_timestamp_ms": 1691105878720,
+          "trace_id": "1f32eb1d-9caf-49ac-a21f-6823486cf581"
+        }"#;
+
+        let payload = KafkaPayload::new(None, None, Some(span.to_vec()));
         let meta = KafkaMessageMetadata {
             partition: 0,
             offset: 1,
