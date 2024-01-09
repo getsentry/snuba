614da0d5bc7e0376b95abbe48f3aa8679350ffcd
Colton Allen <colton.allen@sentry.io>
2024-01-09 08:08:01 -0600

    Don't wrap with array

--- rust_snuba/src/processors/replays.rs
+++ rust_snuba/src/processors/replays.rs
@@ -12,7 +12,7 @@ pub fn process_message(
 ) -> anyhow::Result<InsertBatch> {
     let payload_bytes = payload.payload().context("Expected payload")?;
     let replay_row = deserialize_message(payload_bytes, metadata.partition, metadata.offset)?;
-    InsertBatch::from_rows([replay_row])
+    InsertBatch::from_rows(replay_row)
 }
 
 pub fn deserialize_message(
