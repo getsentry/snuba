0d566d432a01697cb7549e773084c2b8035b2b58
Pierre Massat <pierre.massat@sentry.io>
2024-01-09 09:18:16 -0500

    ref(functions): Switch the functions processor to use the Rust processor

--- tests/datasets/test_functions_processor.py
+++ tests/datasets/test_functions_processor.py
@@ -145,4 +145,4 @@ class TestFunctionsProcessor:
 
         assert FunctionsMessageProcessor().process_message(
             message.serialize(), meta
-        ) == InsertBatch(batch, datetime.utcfromtimestamp(message.received))
+        ) == InsertBatch(rows=batch, origin_timestamp=None)
