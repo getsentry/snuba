037418ae1c13fe38eac6e2ee56b2289f6caa00f7
Meredith Heller <meredith@sentry.io>
2024-01-11 11:17:29 -0800

    some more

--- snuba/cli/query_comparer.py
+++ snuba/cli/query_comparer.py
@@ -11,12 +11,38 @@ from snuba.environment import setup_logging, setup_sentry
 logger = structlog.get_logger().bind(module=__name__)
 
 
-@dataclass
+@dataclass(frozen=True)
 class QueryMeasurement:
     query_id: str
     query_duration_ms: int
     result_rows: int
     result_bytes: int
+    read_rows: int
+    read_bytes: int
+
+@dataclass(frozen=True)
+class MismatchedValues:
+    base_value: int
+    new_value: int
+    delta: int
+    delta_percent: float
+
+@dataclass
+class QueryMismatch:
+    query_id: str
+    query_duration_ms: MismatchedValues
+    result_rows: MismatchedValues
+    result_bytes: MismatchedValues
+    read_rows: MismatchedValues
+    read_bytes: MismatchedValues
+
+MEASUREMENTS = [
+    "query_duration_ms",
+    "result_rows",
+    "result_bytes",
+    "read_rows",
+    "read_bytes",
+]
 
 
 @click.command()
@@ -52,25 +78,57 @@ def query_comparer(
     def query_measurement(row) -> QueryMeasurement:
         return QueryMeasurement(
             query_id=row[0],
-            query_duration_ms=row[1],
-            result_rows=row[2],
-            result_bytes=row[3],
+            query_duration_ms=int(row[1]),
+            result_rows=int(row[2]),
+            result_bytes=int(row[3]),
+            read_rows=int(row[4]),
+            read_bytes=int(row[5]),
         )
 
-    def compare_query(v1_data, v2_data):
-        # TODO() compare the results from each time
-        # we ran the query and decide what to do with
-        # the mismatched results
-
-        pass
-
+    mismatches = []
+    total_rows = 0
     for v1_row, v2_row in itertools.zip_longest(base_reader, upgrade_reader):
         if v1_row[0] == "query_id":
             # csv header row
             continue
+
+        assert v1_row[0] == v2_row[0], "Invalid query_ids: ids must match"
+        total_rows += 1
+
+        mismatches_exist = any([1 for i in range(1, len(v1_row)) if v1_row[i] != v2_row[i]])
+        if not mismatches_exist:
+            continue
+
         v1_data = query_measurement(v1_row)
         v2_data = query_measurement(v2_row)
 
-        assert v1_data.query_id == v2_data.query_id
+        mismatches.append(_create_mismatch(v1_data, v2_data))
 
-        compare_query(v1_data, v2_data)
+    print("\ntotal queries:", total_rows)
+    print("total mismatches:", len(mismatches))
+    print("percent mismatches:", len(mismatches)/total_rows)
+
+
+def _create_mismatch(v1_data, v2_data):
+    mismatch = {}
+    for m in MEASUREMENTS:
+        v1_value = v1_data.__getattribute__(m)
+        v2_value = v2_data.__getattribute__(m)
+
+        delta = v2_value - v1_value
+        delta_percent = (delta/v1_value) * 100
+        mismatched_values = MismatchedValues(
+            v1_value,
+            v2_value,
+            delta,
+            delta_percent,
+        )
+        mismatch[m] = mismatched_values
+    return QueryMismatch(
+        query_id=v1_data.query_id,
+        query_duration_ms=mismatch["query_duration_ms"],
+        result_rows=mismatch["result_rows"],
+        result_bytes=mismatch["result_bytes"],
+        read_rows=mismatch["read_rows"],
+        read_bytes=mismatch["read_bytes"],
+    )
