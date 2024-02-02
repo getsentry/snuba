f2f319e11cc6c01caf00c2a099d8f5e1597ecc6f
Yining Huang <victoria.huang777@gmail.com>
2024-02-01 20:36:01 -0800

    remove wrong file

--- snuba/vic.py
+++
@@ -1,3 +0,0 @@
-from sentry_sdk import init, capture_exception
-init('http://cd8ff258cdbe7ec401de57cf915273ad@localhost:8000/1')
-capture_exception(Exception("This is an example of an error message."))
