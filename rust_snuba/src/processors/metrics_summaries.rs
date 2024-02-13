490b7ba84dcc7845c62cb1d707f1bb40dff6241f
Pierre Massat <pierre.massat@sentry.io>
2024-02-13 15:08:21 -0700

    Lint Rust code

--- rust_snuba/src/processors/metrics_summaries.rs
+++ rust_snuba/src/processors/metrics_summaries.rs
@@ -18,9 +18,9 @@ pub fn process_message(
     let payload_bytes = payload.payload().context("Expected payload")?;
     let from: FromMetricsSummariesMessage = serde_json::from_slice(payload_bytes)?;
 
-    let group: u64 = u64::from_str_radix(&from.group, 16)?;
-    let span_id = u64::from_str_radix(&from.span_id, 16)?;
-    let segment_id = u64::from_str_radix(&from.segment_id, 16)?;
+    let group: u64 = u64::from_str_radix(from.group, 16)?;
+    let span_id = u64::from_str_radix(from.span_id, 16)?;
+    let segment_id = u64::from_str_radix(from.segment_id, 16)?;
     let (tag_keys, tag_values) = from
         .tags
         .iter()
