a155ae21b300c099049d8993e3298d553c92a904
Ben McKerry <110857332+bmckerry@users.noreply.github.com>
2024-02-15 13:54:18 -0500

    remove context from deploy scripts for real this time

--- gocd/templates/bash/migrate.sh
+++ gocd/templates/bash/migrate.sh
@@ -4,7 +4,6 @@ eval $(/devinfra/scripts/regions/project_env_vars.py --region="${SENTRY_REGION}"
 /devinfra/scripts/k8s/k8stunnel
 
 /devinfra/scripts/k8s/k8s-spawn-job.py \
-  --context="gke_${GCP_PROJECT}_${GKE_REGION}-${GKE_CLUSTER_ZONE}_${GKE_CLUSTER}" \
   --label-selector="service=${SNUBA_SERVICE_NAME}" \
   --container-name="${SNUBA_SERVICE_NAME}" \
   "snuba-migrate" \
