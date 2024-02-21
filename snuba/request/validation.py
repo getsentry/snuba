308faa2a36b7eebbdc16067372ed106bd8b5a933
Kyle Mumma <kyle.mumma@sentry.io>
2024-02-21 09:59:36 -0800

    fix: recursion error during parsing (#5522)

--- snuba/request/validation.py
+++ snuba/request/validation.py
@@ -12,11 +12,13 @@ from snuba.attribution import get_app_id
 from snuba.attribution.attribution_info import AttributionInfo
 from snuba.clickhouse.query_dsl.accessors import get_object_ids_in_query_ast
 from snuba.datasets.dataset import Dataset
+from snuba.datasets.factory import get_dataset_name
 from snuba.query.composite import CompositeQuery
 from snuba.query.data_source.simple import Entity
 from snuba.query.exceptions import InvalidQueryException
 from snuba.query.logical import Query
 from snuba.query.mql.parser import parse_mql_query as _parse_mql_query
+from snuba.query.parser.exceptions import PostProcessingError
 from snuba.query.query_settings import (
     HTTPQuerySettings,
     QuerySettings,
@@ -25,7 +27,7 @@ from snuba.query.query_settings import (
 from snuba.query.snql.parser import CustomProcessors
 from snuba.query.snql.parser import parse_snql_query as _parse_snql_query
 from snuba.querylog import record_error_building_request, record_invalid_request
-from snuba.querylog.query_metadata import get_request_status
+from snuba.querylog.query_metadata import SnubaQueryMetadata, get_request_status
 from snuba.request import Request
 from snuba.request.exceptions import InvalidJsonRequestException
 from snuba.request.schema import RequestParts, RequestSchema
@@ -116,64 +118,26 @@ def build_request(
     with sentry_sdk.start_span(description="build_request", op="validate") as span:
         try:
             request_parts = schema.validate(body)
-            tenant_referrer = request_parts.attribution_info["tenant_ids"].get(
-                "referrer"
-            )
-            if tenant_referrer != referrer:
-                metrics.increment(
-                    "referrer_mismatch",
-                    tags={
-                        "tenant_referrer": tenant_referrer or "none",
-                        "request_referrer": referrer,
-                    },
+            referrer = _get_referrer(request_parts, referrer)
+            settings_obj = _get_settings_object(settings_class, request_parts, referrer)
+            try:
+                query, snql_anonymized = parser(
+                    request_parts, settings_obj, dataset, custom_processing
                 )
-            # Handle an edge case where the legacy endpoint is used.
-            referrer = tenant_referrer or referrer
-
-            if settings_class == HTTPQuerySettings:
-                query_settings: MutableMapping[str, bool | str] = {
-                    **request_parts.query_settings,
-                    "consistent": _consistent_override(
-                        request_parts.query_settings.get("consistent", False), referrer
-                    ),
-                }
-                query_settings["referrer"] = referrer
-                # TODO: referrer probably doesn't need to be passed in, it should be from the body
-                settings_obj: Union[HTTPQuerySettings, SubscriptionQuerySettings]
-                # the parameters accept either `str` or `bool` but we pass in `str | bool`
-                settings_obj = settings_class(**query_settings)  # type: ignore
-            elif settings_class == SubscriptionQuerySettings:
-                settings_obj = settings_class(
-                    consistent=_consistent_override(True, referrer),
+            except PostProcessingError as exception:
+                query = exception.query
+                snql_anonymized = exception.snql_anonymized
+                request = _build_request(
+                    body, request_parts, referrer, settings_obj, query, snql_anonymized
                 )
-            query, snql_anonymized = parser(
-                request_parts, settings_obj, dataset, custom_processing
-            )
+                query_metadata = SnubaQueryMetadata(
+                    request, get_dataset_name(dataset), timer
+                )
+                state.record_query(query_metadata.to_dict())
+                raise
 
-            project_ids = get_object_ids_in_query_ast(query, "project_id")
-            query_project_id = None
-            if project_ids is not None and len(project_ids) == 1:
-                query_project_id = project_ids.pop()
-                sentry_sdk.set_tag("snuba_project_id", query_project_id)
-
-            org_ids = get_object_ids_in_query_ast(query, "org_id")
-            if org_ids is not None and len(org_ids) == 1:
-                sentry_sdk.set_tag("snuba_org_id", org_ids.pop())
-
-            attribution_info = update_attribution_info(
-                request_parts, referrer, query_project_id
-            )
-            request_id = uuid.uuid4().hex
-            request = Request(
-                id=request_id,
-                # TODO: Replace this with the actual query raw body.
-                # this can have an impact on subscriptions so we need
-                # to be careful with the change.
-                original_body=body,
-                query=query,
-                attribution_info=AttributionInfo(**attribution_info),
-                query_settings=settings_obj,
-                snql_anonymized=snql_anonymized,
+            request = _build_request(
+                body, request_parts, referrer, settings_obj, query, snql_anonymized
             )
         except (InvalidJsonRequestException, InvalidQueryException) as exception:
             request_status = get_request_status(exception)
@@ -215,3 +179,83 @@ def build_request(
 
         timer.mark("validate_schema")
         return request
+
+
+def _get_referrer(request_parts: RequestParts, referrer: str) -> str:
+    tenant_referrer = request_parts.attribution_info["tenant_ids"].get("referrer")
+    if tenant_referrer != referrer:
+        metrics.increment(
+            "referrer_mismatch",
+            tags={
+                "tenant_referrer": tenant_referrer or "none",
+                "request_referrer": referrer,
+            },
+        )
+    # Handle an edge case where the legacy endpoint is used.
+    return tenant_referrer or referrer
+
+
+def _get_settings_object(
+    settings_class: Type[HTTPQuerySettings] | Type[SubscriptionQuerySettings],
+    request_parts: RequestParts,
+    referrer: str,
+) -> HTTPQuerySettings | SubscriptionQuerySettings:
+    if settings_class == HTTPQuerySettings:
+        query_settings: MutableMapping[str, bool | str] = {
+            **request_parts.query_settings,
+            "consistent": _consistent_override(
+                request_parts.query_settings.get("consistent", False), referrer
+            ),
+        }
+        # TODO: referrer probably doesn't need to be passed in, it should be from the body
+        query_settings["referrer"] = referrer
+        # the parameters accept either `str` or `bool` but we pass in `str | bool`
+        return settings_class(**query_settings)  # type: ignore
+    elif settings_class == SubscriptionQuerySettings:
+        return settings_class(
+            consistent=_consistent_override(True, referrer),
+        )
+    return None  # type: ignore
+
+
+def _get_project_id(query: Query | CompositeQuery[Entity]) -> int | None:
+    project_ids = get_object_ids_in_query_ast(query, "project_id")
+    if project_ids is not None and len(project_ids) == 1:
+        return project_ids.pop()
+    return None
+
+
+def _get_attribution_info(
+    request_parts: RequestParts, referrer: str, query_project_id: int | None
+) -> AttributionInfo:
+    return AttributionInfo(
+        **update_attribution_info(request_parts, referrer, query_project_id)
+    )
+
+
+def _build_request(
+    original_body: dict[str, Any],
+    request_parts: RequestParts,
+    referrer: str,
+    settings: QuerySettings,
+    query: Query | CompositeQuery[Entity],
+    snql_anonymized: str,
+) -> Request:
+    org_ids = get_object_ids_in_query_ast(query, "org_id")
+    if org_ids is not None and len(org_ids) == 1:
+        sentry_sdk.set_tag("snuba_org_id", org_ids.pop())
+
+    query_project_id = _get_project_id(query)
+    if query_project_id:
+        sentry_sdk.set_tag("snuba_project_id", query_project_id)
+
+    attribution_info = _get_attribution_info(request_parts, referrer, query_project_id)
+
+    return Request(
+        id=uuid.uuid4().hex,
+        original_body=original_body,
+        query=query,
+        attribution_info=attribution_info,
+        query_settings=settings,
+        snql_anonymized=snql_anonymized,
+    )
