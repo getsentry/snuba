ee0e529d083ee71d9c470b1238638970f731e749
John Yang <john.yang@sentry.io>
2024-03-01 14:13:59 -0800

    add tests

--- snuba/admin/static/tests/capacity_management/index.spec.tsx
+++ snuba/admin/static/tests/capacity_management/index.spec.tsx
@@ -3,8 +3,28 @@ import Client from "../../api_client";
 import CapacityManagement from "../../capacity_management/index";
 import { it, expect, jest } from "@jest/globals";
 import { AllocationPolicy } from "../../capacity_management/types";
-import { act, fireEvent, render, waitFor } from "@testing-library/react";
+import {
+  fireEvent,
+  getByText,
+  render,
+  waitFor,
+  within,
+} from "@testing-library/react";
 import React from "react";
+
+function verifyRowContents(row: HTMLElement, texts: string[]) {
+  const cols = within(row).getAllByRole("cell");
+  expect(cols).toHaveLength(texts.length);
+  cols.forEach((col, i) => expect(getByText(col, texts[i])).toBeTruthy());
+}
+
+function verifyTableContents(table: HTMLElement, texts: string[][]) {
+  const rows = within(within(table).getAllByRole("rowgroup")[1]).getAllByRole(
+    "row"
+  );
+  expect(rows).toHaveLength(texts.length);
+  rows.forEach((row, i) => verifyRowContents(row, texts[i]));
+}
 
 it("should display allocation policy configs once a storage is selected", async () => {
   let storages = ["storage1", "storage2"];
@@ -47,7 +67,7 @@ it("should display allocation policy configs once a storage is selected", async 
       .mockResolvedValueOnce(allocationPolicies),
   };
 
-  let { getByRole, getByText } = render(
+  let { getByRole, getAllByRole, getByText } = render(
     <CapacityManagement api={mockClient} />
   );
 
@@ -82,4 +102,18 @@ it("should display allocation policy configs once a storage is selected", async 
   expect(getByText("string")).toBeTruthy();
   expect(getByText("something_else")).toBeTruthy();
   expect(getByText('{"some_param":1}')).toBeTruthy();
+
+  const tables = getAllByRole("table");
+
+  expect(tables).toHaveLength(4);
+
+  const storage1GlobalPolicyTable = tables[0];
+  verifyTableContents(storage1GlobalPolicyTable, [
+    ["key1", "10", "something", "int", "edit"],
+  ]);
+
+  const storage2TenantPolicyTable = tables[3];
+  verifyTableContents(storage2TenantPolicyTable, [
+    ["key2", '{"some_param":1}', "a_value", "something_else", "string", "edit"],
+  ]);
 });
