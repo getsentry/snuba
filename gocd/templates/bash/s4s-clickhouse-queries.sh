3e29cb15118324d4441be5bb802df5a6c3b90a92
MeredithAnya <meredith@sentry.io>
2024-03-11 11:28:28 -0700

    fix(gocd): fix unbound variable (#5641)

--- gocd/templates/bash/s4s-clickhouse-queries.sh
+++ gocd/templates/bash/s4s-clickhouse-queries.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
 
-FETCHER_ARGS="--querylog-host ${QUERYLOG_HOST} --querylog-port ${QUERYLOG_PORT} --window-hours ${WINDOW_HOURS} --tables ${TABLES} --gcs-bucket ${GCS_BUCKET}"
-REPLAYER_ARGS="--clickhouse-host ${CLICKHOUSE_HOST} --clickhouse-port ${CLICKHOUSE_PORT}  --gcs-bucket ${GCS_BUCKET}"
-COMPARER_ARGS="--gcs-bucket ${GCS_BUCKET}"
-
 SNUBA_SERVICE_NAME="query-${SNUBA_CMD_TYPE}-gocd"
 
 if [ "${SNUBA_CMD_TYPE}" == "fetcher" ]
 then
-    ARGS=$FETCHER_ARGS
+    ARGS="--querylog-host ${QUERYLOG_HOST} --querylog-port ${QUERYLOG_PORT} --window-hours ${WINDOW_HOURS} --tables ${TABLES} --gcs-bucket ${GCS_BUCKET}"
 fi
 
 if [ "${SNUBA_CMD_TYPE}" == "replayer" ]
 then
-    ARGS=$REPLAYER_ARGS_ARGS
+    ARGS="--clickhouse-host ${CLICKHOUSE_HOST} --clickhouse-port ${CLICKHOUSE_PORT}  --gcs-bucket ${GCS_BUCKET}"
 fi
 
 if [ "${SNUBA_CMD_TYPE}" == "comparer" ]
 then
-    ARGS=$COMPARER_ARGS
+    ARGS="--gcs-bucket ${GCS_BUCKET}"
 fi
 
 SNUBA_SERVICE_NAME="query-${SNUBA_CMD_TYPE}-gocd"
