74f8619fefb13d1a1629a57803cfd8906c1fdea6
Evan Hicks <evan.hicks@sentry.io>
2024-04-02 16:23:56 -0400

    clean up comments

--- snuba/query/mql/parser.py
+++ snuba/query/mql/parser.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import functools
 import logging
 from dataclasses import dataclass, replace
 from typing import Any, Callable, Dict, Optional, Sequence, Tuple, Union
@@ -15,7 +14,7 @@ from snuba.datasets.dataset import Dataset
 from snuba.datasets.entities.entity_key import EntityKey
 from snuba.datasets.entities.factory import get_entity
 from snuba.datasets.factory import get_dataset_name
-from snuba.query import OrderBy, OrderByDirection, SelectedExpression
+from snuba.query import SelectedExpression
 from snuba.query.composite import CompositeQuery
 from snuba.query.conditions import (
     BooleanFunctions,
@@ -28,7 +27,6 @@ from snuba.query.data_source.join import (
     JoinClause,
     JoinCondition,
     JoinConditionExpression,
-    JoinRelationship,
     JoinType,
 )
 from snuba.query.data_source.simple import Entity as QueryEntity
@@ -58,7 +56,6 @@ from snuba.query.processors.logical.filter_in_select_optimizer import (
 from snuba.query.query_settings import QuerySettings
 from snuba.query.snql.anonymize import format_snql_anonymized
 from snuba.query.snql.parser import (
-    MAX_LIMIT,
     POST_PROCESSORS,
     VALIDATORS,
     _post_process,
@@ -66,8 +63,6 @@ from snuba.query.snql.parser import (
     _treeify_or_and_conditions,
 )
 from snuba.state import explain_meta
-from snuba.util import parse_datetime
-from snuba.utils.constants import GRANULARITIES_AVAILABLE
 
 # The parser returns a bunch of different types, so create a single aggregate type to
 # capture everything.
@@ -135,108 +130,6 @@ class MQLVisitor(NodeVisitor):  # type: ignore
 
     def visit_unary_op(self, node: Node, children: Sequence[Any]) -> Any:
         return UNARY_OPERATORS[node.text]
-
-    # def _build_timeseries_formula_param(
-    #     self, param: InitialParseResult
-    # ) -> SelectedExpression:
-    #     """
-    #     Timeseries inside a formula need to have three things done to them:
-    #     1. Add the -If suffix to their aggregate function
-    #     2. Put all the filters for the timeseries inside the aggregate expression
-    #     3. Add a metric_id condition to the conditions in the aggregate
-
-    #     Given an input parse result from `sum(mri){x:y}`, this should output
-    #     an expression like `sumIf(value, x = y AND metric_id = mri)`.
-    #     """
-    #     assert param.expression is not None
-    #     exp = param.expression.expression
-    #     assert isinstance(exp, (FunctionCall, CurriedFunctionCall))
-
-    #     conditions = param.conditions or []
-    #     metric_id_condition = binary_condition(
-    #         ConditionFunctions.EQ,
-    #         Column(None, None, "metric_id"),
-    #         Literal(None, param.mri),
-    #     )
-    #     conditions.append(metric_id_condition)
-    #     value_column = exp.parameters[0]
-    #     if isinstance(exp, FunctionCall):
-    #         return SelectedExpression(
-    #             None,
-    #             FunctionCall(
-    #                 None,
-    #                 f"{exp.function_name}If",
-    #                 parameters=(
-    #                     value_column,
-    #                     combine_and_conditions(conditions),
-    #                 ),
-    #             ),
-    #         )
-    #     else:
-    #         return SelectedExpression(
-    #             None,
-    #             CurriedFunctionCall(
-    #                 None,
-    #                 FunctionCall(
-    #                     exp.internal_function.alias,
-    #                     f"{exp.internal_function.function_name}If",
-    #                     exp.internal_function.parameters,
-    #                 ),
-    #                 (
-    #                     value_column,
-    #                     combine_and_conditions(conditions),
-    #                 ),
-    #             ),
-    #         )
-
-    # def _visit_formula(
-    #     self,
-    #     term_operator: str,
-    #     operand_1: InitialParseResult,
-    #     operand_2: InitialParseResult | None = None,
-    # ) -> InitialParseResult:
-    #     # TODO: If the formula has filters/group by, where do those appear?
-
-    #     # If the parameters of the query are timeseries, extract the expressions from the result
-    #     if (
-    #         isinstance(operand_1, InitialParseResult)
-    #         and operand_1.expression is not None
-    #     ):
-    #         operand_1 = replace(
-    #             operand_1, expression=self._build_timeseries_formula_param(operand_1)
-    #         )
-    #     if (
-    #         operand_2 is not None
-    #         and isinstance(operand_2, InitialParseResult)
-    #         and operand_2.expression is not None
-    #     ):
-    #         operand_2 = replace(
-    #             operand_2,
-    #             expression=self._build_timeseries_formula_param(operand_2),
-    #         )
-
-    #     if (
-    #         operand_2 is not None
-    #         and isinstance(operand_1, InitialParseResult)
-    #         and isinstance(operand_2, InitialParseResult)
-    #         and operand_1.groupby != operand_2.groupby
-    #     ):
-    #         raise InvalidQueryException(
-    #             "All terms in a formula must have the same groupby"
-    #         )
-
-    #     groupby = (
-    #         operand_1.groupby if isinstance(operand_1, InitialParseResult) else None
-    #     )
-    #     parameters = [operand_1]
-    #     if operand_2 is not None:
-    #         parameters.append(operand_2)
-    #     return InitialParseResult(
-    #         expression=None,
-    #         formula=term_operator,
-    #         parameters=parameters,
-    #         groupby=groupby,
-    #     )
 
     def visit_term(
         self,
@@ -423,7 +316,9 @@ class MQLVisitor(NodeVisitor):  # type: ignore
         self,
         node: Node,
         children: Tuple[
-            Tuple[InitialParseResult,],
+            Tuple[
+                InitialParseResult,
+            ],
             Sequence[list[SelectedExpression]],
         ],
     ) -> InitialParseResult:
@@ -927,8 +822,6 @@ def convert_formula_to_query(
     if groupby:
         query.set_ast_groupby(groupby)
 
-    print("SELECTED", selected_columns)
-
     query.set_ast_selected_columns(selected_columns)
 
     # Go through all the conditions, populate the conditions with the table alias, add them to the query conditions
@@ -1079,207 +972,6 @@ def parse_mql_query_body(
     return query
 
 
-# def populate_start_end_time(
-#     query: LogicalQuery, mql_context: MQLContext, entity_key: EntityKey
-# ) -> None:
-#     try:
-#         start = parse_datetime(mql_context.start)
-#         end = parse_datetime(mql_context.end)
-#     except Exception as e:
-#         raise ParsingException("Invalid start or end time") from e
-
-#     entity = get_entity(entity_key)
-#     required_timestamp_column = (
-#         entity.required_time_column if entity.required_time_column else "timestamp"
-#     )
-#     filters = []
-#     filters.append(
-#         binary_condition(
-#             ConditionFunctions.GTE,
-#             Column(None, None, column_name=required_timestamp_column),
-#             Literal(None, value=start),
-#         ),
-#     )
-#     filters.append(
-#         binary_condition(
-#             ConditionFunctions.LT,
-#             Column(None, None, column_name=required_timestamp_column),
-#             Literal(None, value=end),
-#         ),
-#     )
-#     query.add_condition_to_ast(combine_and_conditions(filters))
-
-
-# def populate_scope(
-#     query: LogicalQuery | CompositeQuery[QueryEntity], mql_context: MQLContext
-# ) -> None:
-#     if isinstance(query, CompositeQuery):
-#         raise Exception("need a condition for each alias in the mris")
-#     filters = []
-#     filters.append(
-#         binary_condition(
-#             ConditionFunctions.IN,
-#             Column(alias=None, table_name=None, column_name="project_id"),
-#             FunctionCall(
-#                 alias=None,
-#                 function_name="tuple",
-#                 parameters=tuple(
-#                     Literal(alias=None, value=project_id)
-#                     for project_id in mql_context.scope.project_ids
-#                 ),
-#             ),
-#         )
-#     )
-#     filters.append(
-#         binary_condition(
-#             ConditionFunctions.IN,
-#             Column(alias=None, table_name=None, column_name="org_id"),
-#             FunctionCall(
-#                 alias=None,
-#                 function_name="tuple",
-#                 parameters=tuple(
-#                     Literal(alias=None, value=int(org_id))
-#                     for org_id in mql_context.scope.org_ids
-#                 ),
-#             ),
-#         )
-#     )
-#     filters.append(
-#         binary_condition(
-#             ConditionFunctions.EQ,
-#             Column(alias=None, table_name=None, column_name="use_case_id"),
-#             Literal(alias=None, value=mql_context.scope.use_case_id),
-#         )
-#     )
-#     query.add_condition_to_ast(combine_and_conditions(filters))
-
-
-# def populate_rollup(
-#     query: LogicalQuery | CompositeQuery[QueryEntity], mql_context: MQLContext
-# ) -> None:
-#     rollup = mql_context.rollup
-
-#     # Validate/populate granularity
-#     if rollup.granularity not in GRANULARITIES_AVAILABLE:
-#         raise ParsingException(
-#             f"granularity '{rollup.granularity}' is not valid, must be one of {GRANULARITIES_AVAILABLE}"
-#         )
-
-#     if isinstance(query, CompositeQuery):
-#         raise Exception("this won't work, needs one for each alias of the mris")
-#     query.add_condition_to_ast(
-#         binary_condition(
-#             ConditionFunctions.EQ,
-#             Column(None, None, "granularity"),
-#             Literal(None, rollup.granularity),
-#         )
-#     )
-
-#     # Validate totals/orderby
-#     if rollup.with_totals is not None and rollup.with_totals not in ("True", "False"):
-#         raise ParsingException("with_totals must be a string, either 'True' or 'False'")
-#     if rollup.orderby is not None and rollup.orderby not in ("ASC", "DESC"):
-#         raise ParsingException("orderby must be either 'ASC' or 'DESC'")
-#     if rollup.interval is not None and rollup.orderby is not None:
-#         raise ParsingException("orderby is not supported when interval is specified")
-#     if rollup.interval and (
-#         rollup.interval < GRANULARITIES_AVAILABLE[0]
-#         or rollup.interval < rollup.granularity
-#     ):
-#         raise ParsingException(
-#             f"interval {rollup.interval} must be greater than or equal to granularity {rollup.granularity}"
-#         )
-
-#     with_totals = rollup.with_totals == "True"
-#     if rollup.interval:
-#         if isinstance(query, CompositeQuery):
-#             raise Exception("this won't work")
-#         # If an interval is specified, then we need to group the time by that interval,
-#         # return the time in the select, and order the results by that time.
-#         time_expression = FunctionCall(
-#             "time",
-#             "toStartOfInterval",
-#             parameters=(
-#                 Column(None, None, "timestamp"),
-#                 FunctionCall(
-#                     None,
-#                     "toIntervalSecond",
-#                     (Literal(None, rollup.interval),),
-#                 ),
-#                 Literal(None, "Universal"),
-#             ),
-#         )
-#         selected = list(query.get_selected_columns())
-#         selected.append(SelectedExpression("time", time_expression))
-#         query.set_ast_selected_columns(selected)
-
-#         groupby = query.get_groupby()
-#         if groupby:
-#             query.set_ast_groupby(list(groupby) + [time_expression])
-#         else:
-#             query.set_ast_groupby([time_expression])
-
-#         orderby = OrderBy(OrderByDirection.ASC, time_expression)
-#         query.set_ast_orderby([orderby])
-
-#         if with_totals:
-#             query.set_totals(True)
-#     elif rollup.orderby is not None:
-#         direction = (
-#             OrderByDirection.ASC if rollup.orderby == "ASC" else OrderByDirection.DESC
-#         )
-#         orderby = OrderBy(direction, Column(None, None, AGGREGATE_ALIAS))
-#         query.set_ast_orderby([orderby])
-
-
-# def populate_limit(
-#     query: LogicalQuery | CompositeQuery[QueryEntity], mql_context: MQLContext
-# ) -> None:
-#     limit = 1000
-#     if mql_context.limit:
-#         if mql_context.limit > MAX_LIMIT:
-#             raise ParsingException(
-#                 "queries cannot have a limit higher than 10000", should_report=False
-#             )
-#         limit = mql_context.limit
-
-#     query.set_limit(limit)
-
-
-# def populate_offset(
-#     query: LogicalQuery | CompositeQuery[QueryEntity], mql_context: MQLContext
-# ) -> None:
-#     if mql_context.offset:
-#         if mql_context.offset < 0:
-#             raise ParsingException("offset must be greater than or equal to 0")
-#         query.set_offset(mql_context.offset)
-
-
-# def populate_composite_query_from_mql_context(
-#     query: CompositeQuery[QueryEntity], mql_context: MQLContext
-# ) -> CompositeQuery[QueryEntity]:
-#     join_clause = query.get_from_clause()
-#     assert isinstance(join_clause, JoinClause)
-
-#     # Flatten join clause
-#     nodes = [join_clause.right_node]
-#     node = join_clause.left_node
-#     while isinstance(node, JoinClause):
-#         nodes.append(node.right_node)
-#         node = node.left_node
-
-#     assert isinstance(node, IndividualNode)
-#     nodes.append(node)
-
-#     for node in nodes:
-#         entity_key = node.data_source.key
-#         time_condition = start_end_time_condition(
-#             mql_context, entity_key, table_name=node.alias
-#         )
-
-#     return query
-
-
 def populate_query_from_mql_context(
     query: LogicalQuery | CompositeQuery[QueryEntity], mql_context_dict: dict[str, Any]
 ) -> tuple[LogicalQuery | CompositeQuery[QueryEntity], MQLContext]:
@@ -1301,7 +993,6 @@ def populate_query_from_mql_context(
 
     selected_time_found = False
     for entity_key, alias in entity_data:
-        print("ENTITY", entity_key, alias)
         time_condition = start_end_time_condition(mql_context, entity_key, alias)
         scope_condition = scope_conditions(mql_context, alias)
         granularity_condition, with_totals, orderby, selected_time = rollup_expressions(
@@ -1328,9 +1019,6 @@ def populate_query_from_mql_context(
                 query.set_ast_groupby(list(groupby) + [selected_time.expression])
             else:
                 query.set_ast_groupby([selected_time.expression])
-
-    for p in query.get_selected_columns():
-        print("POST", p)
 
     if isinstance(query, CompositeQuery) and selected_time_found:
         # If the query is grouping by time, that needs to be added to the JoinClause keys to
