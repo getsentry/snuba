673a9a32f1948e27e2bb6ff2553c1b4e1a8387e4
Riya Chakraborty <riya.chakraborty@sentry.io>
2024-04-02 17:31:20 -0700

    clean up code and add test

--- rust_snuba/src/processors/generic_metrics.rs
+++ rust_snuba/src/processors/generic_metrics.rs
@@ -1,5 +1,5 @@
 use adler::Adler32;
-use anyhow::Context;
+use anyhow::{Context, Error};
 use chrono::DateTime;
 use serde::{
     de::value::{MapAccessDeserializer, SeqAccessDeserializer},
@@ -249,6 +249,14 @@ impl Parse for CountersRawRow {
     }
 }
 
+fn should_use_killswitch(config: Result<Option<String>, Error>, use_case: &MessageUseCase) -> bool {
+    if let Some(killswitch) = config.ok().flatten() {
+        return killswitch.contains(use_case.use_case_id.as_str());
+    }
+
+    false
+}
+
 fn process_message<T>(
     payload: KafkaPayload,
     config: &ProcessorConfig,
@@ -257,13 +265,11 @@ where
     T: Parse + Serialize,
 {
     let payload_bytes = payload.payload().context("Expected payload")?;
-    // let killswitch_config = get_str_config("key");
+    let killswitch_config = get_str_config("key");
+    let use_case: MessageUseCase = serde_json::from_slice(payload_bytes)?;
 
-    // if let Some(killswitch) = killswitch_config.ok().flatten() {
-    //     let use_case: MessageUseCase = serde_json::from_slice(payload_bytes)?;
-    //if killswitch == use_case.use_case_id
-
-    if true {
+    if should_use_killswitch(killswitch_config, &use_case) {
+        counter!("generic_metrics.messages.killswitched_use_case", 1, "use_case_id" => use_case.use_case_id.as_str());
         return Ok(InsertBatch::skip());
     }
 
@@ -679,6 +685,36 @@ mod tests {
         "value": {"count": 10, "last": 10.0, "max": 10.0, "min": 1.0, "sum": 20.0},
         "aggregation_option": "ten_second"
     }"#;
+
+    #[test]
+    fn test_shouldnt_killswitch() {
+        let fake_config = Ok(Some("[custom]".to_string()));
+        let use_case = MessageUseCase {
+            use_case_id: "transactions".to_string(),
+        };
+
+        assert_eq!(should_use_killswitch(fake_config, &use_case), false);
+    }
+
+    #[test]
+    fn test_should_killswitch() {
+        let use_case = MessageUseCase {
+            use_case_id: "transactions".to_string(),
+        };
+        let fake_config = Ok(Some("[transactions]".to_string()));
+
+        assert_eq!(should_use_killswitch(fake_config, &use_case), true);
+    }
+
+    #[test]
+    fn test_should_killswitch_again() {
+        let use_case = MessageUseCase {
+            use_case_id: "transactions".to_string(),
+        };
+        let fake_config = Ok(Some("[transactions, custom]".to_string()));
+
+        assert_eq!(should_use_killswitch(fake_config, &use_case), true);
+    }
 
     #[test]
     fn test_validate_timeseries_id() {
