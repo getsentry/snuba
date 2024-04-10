db9a833eeb77992f98b0d0c8553f42a5116ea614
Colton Allen <colton.allen@sentry.io>
2024-04-10 09:50:00 -0500

    Remove aggregated arrays

--- snuba/snuba_migrations/replays/0019_add_materialization.py
+++ snuba/snuba_migrations/replays/0019_add_materialization.py
@@ -3,7 +3,6 @@ from typing import Iterator, List, Sequence
 from snuba.clickhouse.columns import (
     UUID,
     AggregateFunction,
-    Array,
     Column,
     DateTime,
     IPv4,
@@ -33,10 +32,10 @@ def forward_iter() -> Iterator[operations.SqlOperation]:
         columns=columns,
         engine=table_engines.AggregatingMergeTree(
             storage_set=StorageSetKey.REPLAYS,
-            order_by="(project_id, toStartOfHour(ts), replay_id)",
-            partition_by="(retention_days, toMonday(ts))",
+            order_by="(project_id, toStartOfHour(to_hour_timestamp), replay_id)",
+            partition_by="(retention_days, toMonday(to_hour_timestamp))",
             settings={"index_granularity": "8192"},
-            ttl="ts + toIntervalDay(retention_days)",
+            ttl="to_hour_timestamp + toIntervalDay(retention_days)",
         ),
         target=operations.OperationTarget.LOCAL,
     )
@@ -59,17 +58,17 @@ def forward_iter() -> Iterator[operations.SqlOperation]:
         query="""
 SELECT
     project_id,
-    toStartOfHour(timestamp) as ts,
+    toStartOfHour(timestamp) as to_hour_timestamp,
     replay_id,
     anyIfState(browser_name, browser_name != '') as browser_name,
     anyIfState(browser_version, browser_version != '') as browser_version,
     sumState(toUInt64(click_is_dead)) as count_dead_clicks,
-    sumState(toUInt64(error_id != '00000000-0000-0000-0000-000000000000' OR fatal_id != '00000000-0000-0000-0000-000000000000')) as count_error_events,
-    sumState(toUInt64(debug_id != '00000000-0000-0000-0000-000000000000' OR info_id != '00000000-0000-0000-0000-000000000000')) as count_info_events,
+    sumState(toUInt64(error_id != '00000000-0000-0000-0000-000000000000' OR fatal_id != '00000000-0000-0000-0000-000000000000')) as count_errors,
+    sumState(toUInt64(debug_id != '00000000-0000-0000-0000-000000000000' OR info_id != '00000000-0000-0000-0000-000000000000')) as count_infos,
     sumState(toUInt64(click_is_rage)) as count_rage_clicks,
     countState(toUInt64(segment_id)) as count_segments,
     sumState(length(urls)) as count_urls,
-    sumState(toUInt64(warning_id != '00000000-0000-0000-0000-000000000000')) as count_warning_events,
+    sumState(toUInt64(warning_id != '00000000-0000-0000-0000-000000000000')) as count_warnings,
     anyIfState(device_brand, device_brand != '') as device_brand,
     anyIfState(device_family, device_family != '') as device_family,
     anyIfState(device_model, device_model != '') as device_model,
@@ -91,12 +90,7 @@ SELECT
     anyIfState(user, user != '') as user,
     anyIfState(user_id, user_id != '') as user_id,
     anyIfState(user_name, user_name != '') as user_name,
-    anyIfState(user_email, user_email != '') as user_email,
-    groupArrayArrayState(urls) as agg_urls,
-    groupArrayArrayState(arrayFilter(x -> x != '00000000-0000-0000-0000-000000000000', [error_id, fatal_id])) as error_ids,
-    groupArrayArrayState(arrayFilter(x -> x != '00000000-0000-0000-0000-000000000000', [info_id, debug_id])) as info_ids,
-    groupArrayState(warning_id) as warning_ids,
-    groupArrayState(viewed_by_id) as viewed_by_ids
+    anyIfState(user_email, user_email != '') as user_email
 FROM replays_local
 GROUP BY project_id, toStartOfHour(timestamp), replay_id
 """,
@@ -155,19 +149,18 @@ def count_nullable(column_name: str) -> Column[Modifiers]:
 columns: List[Column[Modifiers]] = [
     # Primary-key.
     Column("project_id", UInt(64)),
-    Column("ts", DateTime()),
+    Column("to_hour_timestamp", DateTime()),
     Column("replay_id", UUID()),
     # Columns ordered by column-name.
-    Column("agg_urls", AggregateFunction("groupArrayArray", [Array(String())])),
     any_if_nullable_string("browser_name"),
     any_if_nullable_string("browser_version"),
     sum("count_dead_clicks"),
-    sum("count_error_events"),
-    sum("count_info_events"),
+    sum("count_errors"),
+    sum("count_infos"),
     sum("count_rage_clicks"),
     count_nullable("count_segments"),
     sum("count_urls"),
-    sum("count_warning_events"),
+    sum("count_warnings"),
     any_if_nullable_string("device_brand"),
     any_if_nullable_string("device_family"),
     any_if_nullable_string("device_model"),
@@ -175,8 +168,6 @@ columns: List[Column[Modifiers]] = [
     any_if_nullable_string("dist"),
     Column("end", AggregateFunction("max", [DateTime()])),
     any_if_nullable_string("environment"),
-    Column("error_ids", AggregateFunction("groupArrayArray", [Array(UUID())])),
-    Column("info_ids", AggregateFunction("groupArrayArray", [Array(UUID())])),
     Column("ip_address_v4", AggregateFunction("any", [IPv4(Modifiers(nullable=True))])),
     Column("ip_address_v6", AggregateFunction("any", [IPv6(Modifiers(nullable=True))])),
     Column(
@@ -194,6 +185,4 @@ columns: List[Column[Modifiers]] = [
     any_if_nullable_string("user_id"),
     any_if_nullable_string("user_name"),
     any_if_nullable_string("user_email"),
-    Column("warning_ids", AggregateFunction("groupArray", [UUID()])),
-    Column("viewed_by_ids", AggregateFunction("groupArray", [UInt(64)])),
 ]
