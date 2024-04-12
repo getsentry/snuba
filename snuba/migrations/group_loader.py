76badcdfdcf93183ae8ddaff7bfec3ed2898f7e0
William Mak <william@wmak.io>
2024-04-12 14:30:34 -0400

    ref: Remove migration on discover
    - Pretty sure we dont' need this since clickhouse docs mention:
      - "When reading, the indexes of tables that are actually being read
        are used, if they exist."

--- snuba/migrations/group_loader.py
+++ snuba/migrations/group_loader.py
@@ -136,7 +136,6 @@ class DiscoverLoader(DirectoryLoader):
             "0005_discover_fix_transaction_name",
             "0006_discover_add_trace_id",
             "0007_discover_add_span_id",
-            "0008_discover_add_index_on_trace_id",
         ]
 
 
