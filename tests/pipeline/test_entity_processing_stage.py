e5753ebe0b7ffd10150661691f8884f771ddccfe
Kyle Mumma <kyle.mumma@sentry.io>
2024-04-15 15:14:09 -0700

    updated entity stage test

--- tests/pipeline/test_entity_processing_stage.py
+++ tests/pipeline/test_entity_processing_stage.py
@@ -1,48 +1,117 @@
-from datetime import datetime
-
 from snuba.attribution import get_app_id
 from snuba.attribution.attribution_info import AttributionInfo
+from snuba.clickhouse.columns import ColumnSet
 from snuba.clickhouse.query import Query
-from snuba.datasets.factory import get_dataset
+from snuba.clickhouse.translators.snuba.mapping import TranslationMappers
+from snuba.clusters.cluster import _STORAGE_SET_CLUSTER_MAP, CLUSTERS
+from snuba.clusters.storage_sets import StorageSetKey
+from snuba.datasets.entities.entity_key import EntityKey
+from snuba.datasets.entities.factory import override_entity_map, reset_entity_factory
+from snuba.datasets.entities.storage_selectors.selector import (
+    DefaultQueryStorageSelector,
+)
+from snuba.datasets.pluggable_entity import PluggableEntity
+from snuba.datasets.readiness_state import ReadinessState
+from snuba.datasets.schemas.tables import TableSchema
+from snuba.datasets.storage import EntityStorageConnection, ReadableTableStorage
 from snuba.datasets.storages.storage_key import StorageKey
 from snuba.pipeline.query_pipeline import QueryPipelineResult
 from snuba.pipeline.stages.query_processing import EntityProcessingStage
 from snuba.query import SelectedExpression
-from snuba.query.dsl import and_cond, binary_condition, column, equals, literal
-from snuba.query.expressions import Column, FunctionCall
+from snuba.query.data_source.simple import Table
+from snuba.query.dsl import and_cond, column, equals, literal
 from snuba.query.query_settings import HTTPQuerySettings
-from snuba.query.snql.parser import parse_snql_query
+from snuba.query.snql.parser import parse_snql_query_initial
 from snuba.request import Request
 from snuba.utils.metrics.timer import Timer
+from snuba.utils.schemas import Column as ColumnSchema
+from snuba.utils.schemas import DateTime, UInt
+
+"""
+Create a fake storage and entity, put the entity in global definitions
+"""
+entkey = EntityKey("mock_entity")
+storkey = StorageKey("mockstorage")
+storsetkey = StorageSetKey("mockstorageset")
+colset = ColumnSet(
+    [
+        ColumnSchema("org_id", UInt(64)),
+        ColumnSchema("project_id", UInt(64)),
+        ColumnSchema("timestamp", DateTime()),
+    ]
+)
+storage = ReadableTableStorage(
+    storage_key=storkey,
+    storage_set_key=storsetkey,
+    schema=TableSchema(
+        columns=colset,
+        local_table_name=f"{storkey.value}_local",
+        dist_table_name=f"{storkey.value}_dist",
+        storage_set_key=storsetkey,
+    ),
+    readiness_state=ReadinessState.COMPLETE,
+)
+_STORAGE_SET_CLUSTER_MAP[storage.get_storage_set_key()] = CLUSTERS[0]
+entity = PluggableEntity(
+    entity_key=entkey,
+    storages=[EntityStorageConnection(storage, TranslationMappers())],
+    query_processors=[],
+    columns=colset.columns,
+    validators=[],
+    required_time_column="timestamp",
+    storage_selector=DefaultQueryStorageSelector(),
+)
+override_entity_map(entkey, entity)
 
 
 def test_basic():
     query_body = {
         "query": (
-            "MATCH (metrics_distributions) "
-            "SELECT avg(granularity) "
+            f"MATCH ({entkey.value}) "
+            "SELECT timestamp "
             "WHERE "
-            "timestamp >= toDateTime('2021-05-17 19:42:01') AND "
-            "timestamp < toDateTime('2021-05-17 23:42:01') AND "
             "org_id = 1 AND "
             "project_id = 1"
         ),
     }
-    metrics_dataset = get_dataset("metrics")
-    query, _ = parse_snql_query(query_body["query"], metrics_dataset)
+    logical_query = parse_snql_query_initial(query_body["query"])
     query_settings = HTTPQuerySettings()
     timer = Timer("test")
-    request = request = Request(
+    request = Request(
         id="",
         original_body=query_body,
-        query=query,
+        query=logical_query,
         snql_anonymized="",
         query_settings=query_settings,
         attribution_info=AttributionInfo(
             get_app_id("blah"), {"tenant_type": "tenant_id"}, "blah", None, None, None
         ),
     )
-    res = EntityProcessingStage().execute(
+    schema = storage.get_schema()
+    assert isinstance(schema, TableSchema)
+    expected = QueryPipelineResult(
+        data=Query(
+            from_clause=Table(
+                table_name=schema.get_table_name(),
+                schema=storage.get_schema().get_columns(),
+                storage_key=storkey,
+                allocation_policies=storage.get_allocation_policies(),
+                final=logical_query.get_final(),
+                sampling_rate=logical_query.get_sample(),
+                mandatory_conditions=storage.get_mandatory_condition_checkers(),
+            ),
+            selected_columns=[SelectedExpression("timestamp", column("timestamp"))],
+            condition=and_cond(
+                equals(column("org_id"), literal(1)),
+                equals(column("project_id"), literal(1)),
+            ),
+            limit=1000,
+        ),
+        query_settings=query_settings,
+        timer=timer,
+        error=None,
+    )
+    actual = EntityProcessingStage().execute(
         QueryPipelineResult(
             data=request,
             query_settings=request.query_settings,
@@ -50,53 +119,5 @@ def test_basic():
             error=None,
         )
     )
-    assert res.data and not res.error
-    assert res.query_settings == query_settings and res.timer == timer
-    assert isinstance(res.data, Query)
-    actual = res.data
-    assert (
-        actual.get_arrayjoin() is None
-        and actual.get_prewhere_ast() is None
-        and actual.get_groupby() == []
-        and actual.get_having() is None
-        and actual.get_orderby() == []
-        and actual.get_limitby() is None
-        and actual.get_offset() == 0
-        and not actual.has_totals()
-        and actual.get_granularity() is None
-        and actual.get_limit() == 1000
-    )
-    assert actual.get_from_clause().storage_key == StorageKey.METRICS_DISTRIBUTIONS
-    expected_selected = [
-        SelectedExpression(
-            "avg(granularity)",
-            FunctionCall(
-                "_snuba_avg(granularity)",
-                "avg",
-                (Column("_snuba_granularity", None, "granularity"),),
-            ),
-        )
-    ]
-    assert actual.get_selected_columns() == expected_selected
-    expected_cond = and_cond(
-        equals(column("granularity"), literal(60)),
-        and_cond(
-            binary_condition(
-                "greaterOrEquals",
-                column("timestamp", alias="_snuba_timestamp"),
-                literal(datetime(2021, 5, 17, 19, 42, 1)),
-            ),
-            and_cond(
-                binary_condition(
-                    "less",
-                    column("timestamp", alias="_snuba_timestamp"),
-                    literal(datetime(2021, 5, 17, 23, 42, 1)),
-                ),
-                and_cond(
-                    equals(column("org_id", alias="_snuba_org_id"), literal(1)),
-                    equals(column("project_id", alias="_snuba_project_id"), literal(1)),
-                ),
-            ),
-        ),
-    )
-    assert actual.get_condition() == expected_cond
+    assert actual == expected
+    reset_entity_factory()
