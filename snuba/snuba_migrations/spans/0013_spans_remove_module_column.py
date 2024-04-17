f3326d38c913ec9dda838d6696d83b15c8e1c054
Pierre Massat <pierre.massat@sentry.io>
2024-04-17 16:06:06 -0400

    Let's not be too clever about targets

--- snuba/snuba_migrations/spans/0013_spans_remove_module_column.py
+++ snuba/snuba_migrations/spans/0013_spans_remove_module_column.py
@@ -13,26 +13,24 @@ class Migration(migration.ClickhouseNodeMigration):
     blocking = False
 
     def forwards_ops(self) -> Sequence[operations.SqlOperation]:
-        target = operations.OperationTarget.LOCAL
         return [
             operations.DropColumn(
                 storage_set=StorageSetKey.SPANS,
-                table_name=f"{table_name_prefix}_{target}",
+                table_name=f"{table_name_prefix}_local",
                 column_name=column_name,
-                target=target,
+                target=operations.OperationTarget.LOCAL,
             ),
         ]
 
     def backwards_ops(self) -> Sequence[operations.SqlOperation]:
-        target = operations.OperationTarget.LOCAL
         return [
             operations.AddColumn(
                 storage_set=StorageSetKey.SPANS,
-                table_name=f"{table_name_prefix}_{target}",
+                table_name=f"{table_name_prefix}_local",
                 column=Column(
                     column_name,
                     String(MigrationModifiers(low_cardinality=True, nullable=True)),
                 ),
-                target=target,
+                target=operations.OperationTarget.LOCAL,
             ),
         ]
