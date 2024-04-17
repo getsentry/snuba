afcdd5ad3704befac44e289c69e4807fc60eb834
Kyle Mumma <kyle.mumma@sentry.io>
2024-04-17 12:51:17 -0700

    fix mypy

--- tests/pipeline/test_storage_processing_stage.py
+++ tests/pipeline/test_storage_processing_stage.py
@@ -40,38 +40,46 @@ get_config_built_storages()[mock_storage.get_storage_key()] = mock_storage
 _STORAGE_SET_CLUSTER_MAP[mock_storage.get_storage_set_key()] = CLUSTERS[0]
 
 
-def test_basic():
-    theinput = QueryPipelineResult(
-        data=Query(
-            from_clause=Table(
-                "dontmatter",
-                ColumnSet([]),
-                storage_key=mock_storage.get_storage_key(),
-            ),
-            selected_columns=[],
-            condition=None,
+def test_basic() -> None:
+    query = Query(
+        from_clause=Table(
+            "dontmatter",
+            ColumnSet([]),
+            storage_key=mock_storage.get_storage_key(),
         ),
-        timer=Timer("test"),
-        query_settings=HTTPQuerySettings(),
-        error=None,
+        selected_columns=[],
+        condition=None,
     )
+    timer = Timer("test")
+    settings = HTTPQuerySettings()
+    schema = mock_storage.get_schema()
+    assert isinstance(schema, TableSchema)
     expected = QueryPipelineResult(
         data=Query(
             from_clause=Table(
-                table_name=mock_storage.get_schema().get_table_name(),
+                table_name=schema.get_table_name(),
                 schema=mock_storage.get_schema().get_columns(),
                 storage_key=mock_storage.get_storage_key(),
                 allocation_policies=mock_storage.get_allocation_policies(),
-                final=theinput.data.get_from_clause().final,
-                sampling_rate=theinput.data.get_from_clause().sampling_rate,
-                mandatory_conditions=mock_storage.get_mandatory_condition_checkers(),
+                final=query.get_from_clause().final,
+                sampling_rate=query.get_from_clause().sampling_rate,
+                mandatory_conditions=mock_storage.get_schema()
+                .get_data_source()
+                .get_mandatory_conditions(),
             ),
             selected_columns=[],
             condition=equals(literal(1), literal(1)),
         ),
-        timer=theinput.timer,
-        query_settings=theinput.query_settings,
+        timer=timer,
+        query_settings=settings,
         error=None,
     )
-    actual = StorageProcessingStage().execute(theinput)
+    actual = StorageProcessingStage().execute(
+        QueryPipelineResult(
+            data=query,
+            timer=timer,
+            query_settings=settings,
+            error=None,
+        )
+    )
     assert actual == expected
