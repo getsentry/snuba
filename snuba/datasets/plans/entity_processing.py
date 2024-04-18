03b8a872896a262c513139e33975e98f6f8ccf3a
Enoch Tang <enoch.tang@sentry.io>
2024-04-18 17:07:57 -0400

    change

--- snuba/datasets/plans/entity_processing.py
+++ snuba/datasets/plans/entity_processing.py
@@ -14,7 +14,6 @@ from snuba.datasets.plans.storage_processing import (
     get_query_data_source,
 )
 from snuba.datasets.plans.translator.query import QueryTranslator
-from snuba.datasets.pluggable_entity import PluggableEntity
 from snuba.datasets.slicing import is_storage_set_sliced
 from snuba.datasets.storage import (
     EntityStorageConnection,
@@ -130,6 +129,7 @@ def run_entity_processing_executor(
     query: LogicalQuery, query_settings: QuerySettings
 ) -> Query:
     from snuba.datasets.entities.factory import get_entity
+    from snuba.datasets.pluggable_entity import PluggableEntity
 
     entity = get_entity(query.get_from_clause().key)
     assert isinstance(entity, PluggableEntity)
