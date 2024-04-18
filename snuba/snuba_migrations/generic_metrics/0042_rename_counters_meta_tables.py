048ed141b7f9af9a11b2548446059a2a06c4e47b
Evan Hicks <evan.hicks@sentry.io>
2024-04-18 17:35:52 -0400

    just drop and recreate instead of renaming

--- snuba/snuba_migrations/generic_metrics/0042_rename_counters_meta_tables.py
+++ snuba/snuba_migrations/generic_metrics/0042_rename_counters_meta_tables.py
@@ -2,7 +2,7 @@ from typing import Sequence
 
 from snuba.clickhouse.columns import AggregateFunction, Column, DateTime, String, UInt
 from snuba.clusters.storage_sets import StorageSetKey
-from snuba.migrations import migration, operations
+from snuba.migrations import migration, operations, table_engines
 from snuba.migrations.columns import MigrationModifiers as Modifiers
 from snuba.migrations.operations import OperationTarget
 from snuba.utils.schemas import Float
@@ -46,35 +46,76 @@ class Migration(migration.ClickhouseNodeMigration):
 
     storage_set_key = StorageSetKey.GENERIC_METRICS_COUNTERS
 
-    def forwards_ops(self) -> Sequence[operations.SqlOperation]:
-        """
-        The tables can't simply be renamed, since the materialized view depends on the specific table names.
-        Drop the view, change the table names, then recreate the view with the correct table names.
-        """
-
-        meta_ops = [
+    def drop_ops(self, prefix: str) -> list[operations.SqlOperation]:
+        return [
             operations.DropTable(
                 storage_set=self.storage_set_key,
-                table_name=self.old_meta_view_name,
+                table_name=getattr(self, f"{prefix}_tag_value_view_name"),
                 target=OperationTarget.LOCAL,
             ),
-            operations.RenameTable(
+            operations.DropTable(
                 storage_set=self.storage_set_key,
-                old_table_name=self.old_meta_dist_table_name,
-                new_table_name=self.new_meta_dist_table_name,
+                table_name=getattr(self, f"{prefix}_tag_value_dist_table_name"),
                 target=OperationTarget.DISTRIBUTED,
             ),
-            operations.RenameTable(
+            operations.DropTable(
                 storage_set=self.storage_set_key,
-                old_table_name=self.old_meta_local_table_name,
-                new_table_name=self.new_meta_local_table_name,
+                table_name=getattr(self, f"{prefix}_tag_value_local_table_name"),
                 target=OperationTarget.LOCAL,
+            ),
+            operations.DropTable(
+                storage_set=self.storage_set_key,
+                table_name=getattr(self, f"{prefix}_meta_view_name"),
+                target=OperationTarget.LOCAL,
+            ),
+            operations.DropTable(
+                storage_set=self.storage_set_key,
+                table_name=getattr(self, f"{prefix}_meta_dist_table_name"),
+                target=OperationTarget.DISTRIBUTED,
+            ),
+            operations.DropTable(
+                storage_set=self.storage_set_key,
+                table_name=getattr(self, f"{prefix}_meta_local_table_name"),
+                target=OperationTarget.LOCAL,
+            ),
+        ]
+
+    def create_ops(self, prefix: str) -> list[operations.SqlOperation]:
+        return [
+            operations.CreateTable(
+                storage_set=self.storage_set_key,
+                table_name=getattr(self, f"{prefix}_meta_local_table_name"),
+                engine=table_engines.AggregatingMergeTree(
+                    storage_set=self.storage_set_key,
+                    order_by="(org_id, project_id, use_case_id, metric_id, tag_key, timestamp)",
+                    primary_key="(org_id, project_id, use_case_id, metric_id, tag_key, timestamp)",
+                    partition_by="toMonday(timestamp)",
+                    settings={
+                        "index_granularity": self.granularity,
+                        # Since the partitions contain multiple retention periods, need to ensure
+                        # that rows within partitions are dropped
+                        "ttl_only_drop_parts": 0,
+                    },
+                    ttl="timestamp + toIntervalDay(retention_days)",
+                ),
+                columns=self.meta_table_columns,
+                target=OperationTarget.LOCAL,
+            ),
+            operations.CreateTable(
+                storage_set=self.storage_set_key,
+                table_name=getattr(self, f"{prefix}_meta_dist_table_name"),
+                engine=table_engines.Distributed(
+                    local_table_name=getattr(self, f"{prefix}_meta_local_table_name"),
+                    sharding_key=None,
+                ),
+                columns=self.meta_table_columns,
+                target=OperationTarget.DISTRIBUTED,
             ),
             operations.CreateMaterializedView(
                 storage_set=self.storage_set_key,
-                view_name=self.new_meta_view_name,
+                view_name=getattr(self, f"{prefix}_meta_view_name"),
                 columns=self.meta_table_columns,
-                destination_table_name=self.new_meta_local_table_name,
+                destination_table_name=getattr(self, f"{prefix}_meta_local_table_name"),
                 target=OperationTarget.LOCAL,
                 query="""
                 SELECT
@@ -99,31 +140,44 @@ class Migration(migration.ClickhouseNodeMigration):
                     retention_days
                 """,
             ),
-        ]
-
-        tag_value_ops = [
-            operations.DropTable(
+            operations.CreateTable(
                 storage_set=self.storage_set_key,
-                table_name=self.old_tag_value_view_name,
+                table_name=getattr(self, f"{prefix}_tag_value_local_table_name"),
+                engine=table_engines.AggregatingMergeTree(
+                    storage_set=self.storage_set_key,
+                    order_by="(project_id, metric_id, tag_key, tag_value, timestamp)",
+                    primary_key="(project_id, metric_id, tag_key, tag_value, timestamp)",
+                    partition_by="toMonday(timestamp)",
+                    settings={
+                        "index_granularity": self.granularity,
+                        # Since the partitions contain multiple retention periods, need to ensure
+                        # that rows within partitions are dropped
+                        "ttl_only_drop_parts": 0,
+                    },
+                    ttl="timestamp + toIntervalDay(retention_days)",
+                ),
+                columns=self.tag_value_table_columns,
                 target=OperationTarget.LOCAL,
             ),
-            operations.RenameTable(
+            operations.CreateTable(
                 storage_set=self.storage_set_key,
-                old_table_name=self.old_tag_value_dist_table_name,
-                new_table_name=self.new_tag_value_dist_table_name,
+                table_name=getattr(self, f"{prefix}_tag_value_dist_table_name"),
+                engine=table_engines.Distributed(
+                    local_table_name=getattr(
+                        self, f"{prefix}_tag_value_local_table_name"
+                    ),
+                    sharding_key=None,
+                ),
+                columns=self.tag_value_table_columns,
                 target=OperationTarget.DISTRIBUTED,
-            ),
-            operations.RenameTable(
-                storage_set=self.storage_set_key,
-                old_table_name=self.old_tag_value_local_table_name,
-                new_table_name=self.new_tag_value_local_table_name,
-                target=OperationTarget.LOCAL,
             ),
             operations.CreateMaterializedView(
                 storage_set=self.storage_set_key,
-                view_name=self.new_tag_value_view_name,
+                view_name=getattr(self, f"{prefix}_tag_value_view_name"),
                 columns=self.tag_value_table_columns,
-                destination_table_name=self.new_tag_value_local_table_name,
+                destination_table_name=getattr(
+                    self, f"{prefix}_tag_value_local_table_name"
+                ),
                 target=OperationTarget.LOCAL,
                 query="""
                 SELECT
@@ -149,104 +203,14 @@ class Migration(migration.ClickhouseNodeMigration):
             ),
         ]
 
-        return meta_ops + tag_value_ops
+    def forwards_ops(self) -> Sequence[operations.SqlOperation]:
+        # Drop existing tables
+        ops = self.drop_ops("old")
+        ops += self.create_ops("new")
+        return ops
 
     def backwards_ops(self) -> Sequence[operations.SqlOperation]:
-        meta_ops = [
-            operations.DropTable(
-                storage_set=self.storage_set_key,
-                table_name=self.new_meta_view_name,
-                target=OperationTarget.LOCAL,
-            ),
-            operations.RenameTable(
-                storage_set=self.storage_set_key,
-                old_table_name=self.new_meta_dist_table_name,
-                new_table_name=self.old_meta_dist_table_name,
-                target=OperationTarget.DISTRIBUTED,
-            ),
-            operations.RenameTable(
-                storage_set=self.storage_set_key,
-                old_table_name=self.new_meta_local_table_name,
-                new_table_name=self.old_meta_local_table_name,
-                target=OperationTarget.LOCAL,
-            ),
-            operations.CreateMaterializedView(
-                storage_set=self.storage_set_key,
-                view_name=self.old_meta_view_name,
-                columns=self.meta_table_columns,
-                destination_table_name=self.old_meta_local_table_name,
-                target=OperationTarget.LOCAL,
-                query="""
-                SELECT
-                    org_id,
-                    project_id,
-                    use_case_id,
-                    metric_id,
-                    tag_key,
-                    toMonday(timestamp) as timestamp,
-                    retention_days,
-                    sumState(count_value) as count
-                FROM generic_metric_counters_raw_local
-                ARRAY JOIN tags.key AS tag_key
-                WHERE record_meta = 1
-                GROUP BY
-                    org_id,
-                    project_id,
-                    use_case_id,
-                    metric_id,
-                    tag_key,
-                    timestamp,
-                    retention_days
-                """,
-            ),
-        ]
-
-        tag_value_ops = [
-            operations.DropTable(
-                storage_set=self.storage_set_key,
-                table_name=self.new_tag_value_view_name,
-                target=OperationTarget.LOCAL,
-            ),
-            operations.RenameTable(
-                storage_set=self.storage_set_key,
-                old_table_name=self.new_tag_value_dist_table_name,
-                new_table_name=self.old_tag_value_dist_table_name,
-                target=OperationTarget.DISTRIBUTED,
-            ),
-            operations.RenameTable(
-                storage_set=self.storage_set_key,
-                old_table_name=self.new_tag_value_local_table_name,
-                new_table_name=self.old_tag_value_local_table_name,
-                target=OperationTarget.LOCAL,
-            ),
-            operations.CreateMaterializedView(
-                storage_set=self.storage_set_key,
-                view_name=self.old_tag_value_view_name,
-                columns=self.tag_value_table_columns,
-                destination_table_name=self.old_tag_value_local_table_name,
-                target=OperationTarget.LOCAL,
-                query="""
-                SELECT
-                    project_id,
-                    metric_id,
-                    tag_key,
-                    tag_value,
-                    toMonday(timestamp) as timestamp,
-                    retention_days,
-                    sumState(count_value) as count
-                FROM generic_metric_counters_raw_local
-                ARRAY JOIN
-                    tags.key AS tag_key, tags.raw_value AS tag_value
-                WHERE record_meta = 1
-                GROUP BY
-                    project_id,
-                    metric_id,
-                    tag_key,
-                    tag_value,
-                    timestamp,
-                    retention_days
-                """,
-            ),
-        ]
-
-        return meta_ops + tag_value_ops
+        # Drop new tables
+        ops = self.drop_ops("new")
+        ops += self.create_ops("old")
+        return ops
