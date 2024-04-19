f5218447aa0cfdbfc2d0044cccadc15498b42d93
Evan Hicks <evan.hicks@sentry.io>
2024-04-19 16:49:16 -0400

    remove print

--- tests/test_discover_api.py
+++ tests/test_discover_api.py
@@ -747,7 +747,6 @@ class TestDiscoverApi(BaseApiTest):
                 entity="discover_transactions",
             ).data
         )
-        print("RESULT", result)
         assert result["data"] == []
 
     def test_contexts(self) -> None:
