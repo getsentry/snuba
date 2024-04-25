9319c498cf712834ab477064aba56d64a642c9d5
Snigdha Sharma <snigdha.sharma@sentry.io>
2024-04-25 10:27:24 -0700

    Make the column nullable

--- snuba/snuba_migrations/group_attributes/0002_add_priority_to_group_attributes.py
+++ snuba/snuba_migrations/group_attributes/0002_add_priority_to_group_attributes.py
@@ -1,6 +1,6 @@
 from typing import Sequence
 
-from snuba.clickhouse.columns import Column, UInt
+from snuba.clickhouse.columns import Column, UInt, SchemaModifiers as Modifiers
 from snuba.clusters.storage_sets import StorageSetKey
 from snuba.migrations import migration, operations
 from snuba.migrations.operations import OperationTarget, SqlOperation
@@ -14,14 +14,14 @@ class Migration(migration.ClickhouseNodeMigration):
             operations.AddColumn(
                 storage_set=StorageSetKey.GROUP_ATTRIBUTES,
                 table_name="group_attributes_local",
-                column=Column("group_priority", UInt(8)),
+                column=Column("group_priority", UInt(8, Modifiers(nullable=True))),
                 target=OperationTarget.LOCAL,
                 after="group_substatus",
             ),
             operations.AddColumn(
                 storage_set=StorageSetKey.GROUP_ATTRIBUTES,
                 table_name="group_attributes_dist",
-                column=Column("group_priority", UInt(8)),
+                column=Column("group_priority", UInt(8, Modifiers(nullable=True))),
                 target=OperationTarget.DISTRIBUTED,
                 after="group_substatus",
             ),
