03f73c8505750e86a6597740735dc4b6eea856ef
Evan Hicks <evanh@users.noreply.github.com>
2024-05-09 11:24:29 -0400

    fix: Handle tag columns in low cardinality processor (#5875)
    Discover will sometimes send filters on columns as tags instead of actual
    columns, e.g. tags[environment]. This was causing the allocator bug to be hit
    again on some queries. This change handles checking for this types of queries
    and correctly casting them as well to avoid the allocator bugs.

--- snuba/query/processors/logical/low_cardinality_processor.py
+++ snuba/query/processors/logical/low_cardinality_processor.py
@@ -1,7 +1,13 @@
 from dataclasses import replace
 
 from snuba import state
-from snuba.query.expressions import Column, Expression, FunctionCall, Literal
+from snuba.query.expressions import (
+    Column,
+    Expression,
+    FunctionCall,
+    Literal,
+    SubscriptableReference,
+)
 from snuba.query.logical import Query
 from snuba.query.processors.logical import LogicalQueryProcessor
 from snuba.query.query_settings import QuerySettings
@@ -22,24 +28,43 @@ class LowCardinalityProcessor(LogicalQueryProcessor):
     """
 
     def __init__(self, columns: list[str]) -> None:
-        self.low_card_columns = set(columns)
+        self.low_card_columns = set()
+        self.subscriptable_columns: dict[str, set[str]] = dict()
+        for c in columns:
+            if c.startswith("tags") or c.startswith("contexts"):
+                column, key = c.split("[")
+                key = key[:-1]
+                self.subscriptable_columns.setdefault(column, set()).add(key)
+            else:
+                self.low_card_columns.add(c)
 
     def process_query(self, query: Query, query_settings: QuerySettings) -> None:
         def transform_expressions(exp: Expression) -> Expression:
-            if (
-                not isinstance(exp, Column)
-                or exp.column_name not in self.low_card_columns
-            ):
-                return exp
-
-            return FunctionCall(
-                exp.alias,
-                "cast",
-                (
-                    replace(exp, alias=None),
-                    Literal(None, "Nullable(String)"),
-                ),
-            )
+            if isinstance(exp, Column) and exp.column_name in self.low_card_columns:
+                return FunctionCall(
+                    exp.alias,
+                    "cast",
+                    (
+                        replace(exp, alias=None),
+                        Literal(None, "Nullable(String)"),
+                    ),
+                )
+            if isinstance(exp, SubscriptableReference):
+                column = exp.column.column_name
+                key = exp.key.value
+                if (
+                    column in self.subscriptable_columns
+                    and key in self.subscriptable_columns[column]
+                ):
+                    return FunctionCall(
+                        exp.alias,
+                        "cast",
+                        (
+                            replace(exp, alias=None),
+                            Literal(None, "Nullable(String)"),
+                        ),
+                    )
+            return exp
 
         if state.get_int_config("use.low.cardinality.processor", 1) == 0:
             return
