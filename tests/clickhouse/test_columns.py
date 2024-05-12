41b505160668719d3892f000333bf1cf5bfded3f
Pierre Massat <pierre.massat@sentry.io>
2024-05-12 15:29:53 -0400

    Fix typing

--- tests/clickhouse/test_columns.py
+++ tests/clickhouse/test_columns.py
@@ -90,14 +90,14 @@ TEST_CASES = [
     pytest.param(
         DateTime64(3, "America/New_York"),
         DateTime64(3, "America/New_York"),
-        DateTime64(9, Modifier(nullable=True)),
+        DateTime64(9, modifiers=Modifier(nullable=True)),
         "DateTime64(3, 'America/New_York')",
         id="datetime64s_tz",
     ),
     pytest.param(
         DateTime64(3),
         DateTime64(3),
-        DateTime64(9, Modifier(nullable=True)),
+        DateTime64(9, modifiers=Modifier(nullable=True)),
         "DateTime64(3)",
         id="datetime64s_notz",
     ),
