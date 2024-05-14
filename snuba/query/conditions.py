cc8482b96d9ed8d0c6ade9d3ab75c5858d729230
Kyle Mumma <kyle.mumma@sentry.io>
2024-05-14 14:29:31 -0700

    rollout percent

--- snuba/query/conditions.py
+++ snuba/query/conditions.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import random
 from typing import Any, Mapping, Optional, Sequence, Set, Union
 
 from snuba.query.dsl import literals_tuple
@@ -12,7 +13,7 @@ from snuba.query.matchers import Integer
 from snuba.query.matchers import Literal as LiteralPattern
 from snuba.query.matchers import Or, Param, Pattern, String
 from snuba.query.matchers import SubscriptableReference as SubscriptableReferencePattern
-from snuba.state import get_int_config
+from snuba.state import get_float_config
 
 
 class ConditionFunctions:
@@ -284,8 +285,11 @@ def combine_and_conditions(conditions: Sequence[Expression]) -> Expression:
 
 
 def _combine_conditions(conditions: Sequence[Expression], function: str) -> Expression:
-    flag = get_int_config("use_new_combine_conditions", default=1)
-    if flag:
+    rollout_percent = get_float_config(
+        "use_new_combine_conditions_percent", default=0.0
+    )
+    assert isinstance(rollout_percent, float)
+    if random.random() < rollout_percent:
         return _combine_conditions_new(conditions, function)
     else:
         return _combine_conditions_old(conditions, function)
