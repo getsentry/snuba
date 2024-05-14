fb419d42206381600c91d602425c951e9c8d7f08
Pierre Massat <pierre.massat@sentry.io>
2024-05-14 10:28:49 -0400

    Revert "Fix wrong storage set for migration group"
    This reverts commit 0a5af69c869bcc41ab817c8e597a44d73c96286a.

--- snuba/migrations/groups.py
+++ snuba/migrations/groups.py
@@ -174,7 +174,7 @@ _REGISTERED_MIGRATION_GROUPS: Dict[MigrationGroup, _MigrationGroup] = {
     ),
     MigrationGroup.PROFILE_CHUNKS: _MigrationGroup(
         loader=ProfileChunksLoader(),
-        storage_sets_keys={StorageSetKey.PROFILES},
+        storage_sets_keys={StorageSetKey.PROFILE_CHUNKS},
         readiness_state=ReadinessState.PARTIAL,
     ),
 }
