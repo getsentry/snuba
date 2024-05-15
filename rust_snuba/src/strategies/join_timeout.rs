bce0d48bfeefe2ada5b8a9756cc387596520cff2
Markus Unterwaditzer <markus-tarpit+git@unterwaditzer.net>
2024-05-15 14:07:54 +0200

    ref(rust-consumer): join-timeout per-step
    Ref https://github.com/getsentry/team-sns/issues/4

---
+++ rust_snuba/src/strategies/join_timeout.rs
@@ -0,0 +1,38 @@
+use std::time::Duration;
+
+use rust_arroyo::processing::strategies::ProcessingStrategy;
+
+pub struct SetJoinTimeout<N> {
+    next_step: N,
+    new_timeout: Option<Duration>,
+}
+
+impl SetJoinTimeout<N> {
+    pub fn new(next_step: N, new_timeout: Option<Duration>) -> Self {
+        SetJoinTimeout {
+            new_timeout,
+            next_step,
+        }
+    }
+}
+
+impl<N, TPayload> ProcessingStrategy<TPayload> for SetJoinTimeout<N>
+where
+    N: ProcessingStrategy<TPayload>,
+{
+    fn poll(&mut self) -> Result<Option<CommitRequest>, StrategyError> {
+        self.next_step.poll()
+    }
+
+    fn submit(&mut self, message: Message<TPayload>) -> Result<(), SubmitError<TPayload>> {
+        self.next_step.submit(message)
+    }
+
+    fn terminate(&mut self) {
+        self.next_step.terminate()
+    }
+
+    fn join(&mut self, timeout: Option<Duration>) -> Result<Option<CommitRequest>, StrategyError> {
+        self.next_step.join(self.new_timeout)
+    }
+}
