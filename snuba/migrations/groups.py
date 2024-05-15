00489efcaaec35ef57dc92152e53e1739d758391
volokluev <3169433+volokluev@users.noreply.github.com>
2024-05-15 14:11:51 -0700

    return limited readiness state for storage set key (#5921)

--- snuba/migrations/groups.py
+++ snuba/migrations/groups.py
@@ -213,7 +213,10 @@ def get_group_readiness_state_from_storage_set(
     storage_set_key: StorageSetKey,
 ) -> ReadinessState:
     migration_group = _STORAGE_SET_TO_MIGRATION_GROUP_MAPPING[storage_set_key]
-    return _REGISTERED_MIGRATION_GROUPS[migration_group].readiness_state
+    registered_migration_group = _REGISTERED_MIGRATION_GROUPS.get(migration_group, None)
+    if registered_migration_group:
+        return registered_migration_group.readiness_state
+    return ReadinessState.LIMITED
 
 
 def get_group_readiness_state(group: MigrationGroup) -> ReadinessState:
