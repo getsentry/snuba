ec4ba9c3524ac929e03853400c2d2b7dabe362ff
Volo Kluev <volo.kluev@sentry.io>
2024-05-29 15:53:34 -0700

    fix discover api test

--- tests/test_discover_api.py
+++ tests/test_discover_api.py
@@ -14,7 +14,7 @@ from tests.fixtures import get_raw_event, get_raw_transaction
 from tests.helpers import write_unprocessed_events
 
 
-@pytest.mark.clickhouse_db
+@pytest.mark.clickhouse_db(storage_keys=["discover", "errors", "transactions"])
 @pytest.mark.redis_db
 class TestDiscoverApi(BaseApiTest):
     @pytest.fixture
