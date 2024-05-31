e269f3b8b953a59d4222791c877aa48fb1b35d52
Volo Kluev <volo.kluev@sentry.io>
2024-05-31 14:53:07 -0700

    s

--- tests/subscriptions/test_store.py
+++ tests/subscriptions/test_store.py
@@ -12,7 +12,7 @@ from tests.subscriptions import BaseSubscriptionTest
 
 
 @pytest.mark.redis_db
-@pytest.mark.clickhouse_db
+@pytest.mark.clickhouse_db(storage_keys=["errors", "group_attributes"])
 class TestRedisSubscriptionStore(BaseSubscriptionTest):
     @property
     def subscription(self) -> Sequence[SubscriptionData]:
