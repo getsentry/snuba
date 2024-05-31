26895596cb2069ae4341cce51226b47dbea0568f
Volo Kluev <volo.kluev@sentry.io>
2024-05-31 13:56:50 -0700

    query cache test works

--- tests/web/test_query_cache.py
+++ tests/web/test_query_cache.py
@@ -60,7 +60,7 @@ def run_query() -> None:
     assert result.result["data"] == []
 
 
-@pytest.mark.clickhouse_db
+@pytest.mark.clickhouse_db(storage_keys=["errors"])
 @pytest.mark.redis_db
 def test_cache_retries_on_bad_query_id(
     monkeypatch: pytest.MonkeyPatch, snuba_set_config: Callable[[str, Any], None]
