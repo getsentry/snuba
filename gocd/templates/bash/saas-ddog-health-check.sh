cbabc1811e8d90ee23304fd8990c1544591d292b
Evan Hicks <evan.hicks@sentry.io>
2024-06-06 11:49:52 -0400

    saas too

--- gocd/templates/bash/saas-ddog-health-check.sh
+++ gocd/templates/bash/saas-ddog-health-check.sh
@@ -2,9 +2,11 @@
 
 /devinfra/scripts/checks/datadog/monitor_status.py --dry-run=true \
   113296727 \
-  42722121
+  42722121 \
+  146517092
 
 
 # Above monitor IDs map to following monitors respectively:
 # Snuba - SLO - High API error rate
 # Snuba - Too many restarts on Snuba pods
+# Snuba Pods Crashlooping in {{sentry_region.name}}
