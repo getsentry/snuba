e328c98bddca649c3aab0266dd1bf7f5da753a14
Evan Hicks <evan.hicks@sentry.io>
2024-06-06 11:48:25 -0400

    ref(gocd): Add a crashloop monitor to deploys
    Add a crashloop monitor to the S4S deploy to ensure that the changes at least keep the pods stable.

--- gocd/templates/bash/s4s-ddog-health-check.sh
+++ gocd/templates/bash/s4s-ddog-health-check.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+/devinfra/scripts/checks/datadog/monitor_status.py --dry-run=true \
+  146517092
+
+
+# Above monitor IDs map to following monitors respectively:
+# Snuba Pods Crashlooping in {{sentry_region.name}}
