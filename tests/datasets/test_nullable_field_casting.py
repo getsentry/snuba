02f21eb9331d8f9148a45ee4ac6ed53edc6bfb08
Volo Kluev <volo.kluev@sentry.io>
2024-06-07 15:14:20 -0700

    nullable

--- tests/datasets/test_nullable_field_casting.py
+++ tests/datasets/test_nullable_field_casting.py
@@ -26,7 +26,7 @@ from snuba.utils.metrics.timer import Timer
         )
     ],
 )
-@pytest.mark.clickhouse_db
+@pytest.mark.clickhouse_db(storage_keys=["errors", "discover"])
 def test_nullable_field_casting(entity: Entity, expected_table_name: str) -> None:
     dataset_name = "discover"
 
