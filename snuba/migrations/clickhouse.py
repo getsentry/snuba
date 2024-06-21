c779cc678e4d5ac8cfeab3908304c2cde7b49eed
Evan Hicks <evanh@users.noreply.github.com>
2024-06-21 14:48:26 -0400

    ref: Increase max version as we upgrade clusters (#6046)

--- snuba/migrations/clickhouse.py
+++ snuba/migrations/clickhouse.py
@@ -1,4 +1,4 @@
 CLICKHOUSE_SERVER_MIN_VERSION = "21.8.12.29"
 # Note: 21.8.12.29 and 21.8.13.1 are used in self-hosted builds
 # even though SaaS clusters are all on 22.8 or above
-CLICKHOUSE_SERVER_MAX_VERSION = "23.3.19.33"
+CLICKHOUSE_SERVER_MAX_VERSION = "23.8.11.29"
