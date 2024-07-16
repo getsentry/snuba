19bfa726d0ba215f2ebddbad6f49d63196199481
Kyle Mumma <kyle.mumma@sentry.io>
2024-07-16 14:26:31 -0700

    init

--- snuba/settings/settings_distributed.py
+++ snuba/settings/settings_distributed.py
@@ -35,7 +35,6 @@ CLUSTERS = [
             "generic_metrics_gauges",
             "metrics_summaries",
             "profile_chunks",
-            "search_issues_tmp",
         },
         "single_node": False,
         "cluster_name": "cluster_one_sh",
