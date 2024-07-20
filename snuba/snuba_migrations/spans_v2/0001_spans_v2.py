480b0e18b60a67c9881db285287ed08356e91ec6
Colin Chartier <colin.chartier@sentry.io>
2024-07-19 22:03:35 -0400

    fix mypy?

--- snuba/snuba_migrations/spans_v2/0001_spans_v2.py
+++ snuba/snuba_migrations/spans_v2/0001_spans_v2.py
@@ -1,4 +1,4 @@
-from typing import Sequence
+from typing import List, Sequence
 
 from snuba.clusters.storage_sets import StorageSetKey
 from snuba.migrations import migration, operations, table_engines
@@ -21,56 +21,59 @@ storage_set_name = StorageSetKey.SPANS_V2
 local_table_name = "spans_v2_local"
 dist_table_name = "spans_v2_dist"
 
-columns: Sequence[Column[Modifiers]] = (
+columns: List[Column[Modifiers]] = [
+    Column("organization_id", UInt(64)),
+    Column("project_id", UInt(64)),
+    Column("trace_id", UUID()),
+    Column("span_id", UInt(64)),
+    Column("parent_span_id", UInt(64, Modifiers(codecs=["ZSTD(1)"]))),
+    Column("segment_id", UInt(64, Modifiers(codecs=["ZSTD(1)"]))),
+    Column("segment_name", String(Modifiers(codecs=["ZSTD(1)"]))),
+    Column("is_segment", Bool()),
+    Column("_sort_timestamp", DateTime(Modifiers(codecs=["DoubleDelta", "ZSTD(1)"]))),
+    Column(
+        "start_timestamp",
+        DateTime64(6, modifiers=Modifiers(codecs=["DoubleDelta", "ZSTD(1)"])),
+    ),
+    Column(
+        "end_timestamp",
+        DateTime64(6, modifiers=Modifiers(codecs=["DoubleDelta", "ZSTD(1)"])),
+    ),
+    Column(
+        "duration_ms",
+        UInt(32, modifiers=Modifiers(codecs=["DoubleDelta", "ZSTD(1)"])),
+    ),
+    Column("exclusive_time_ms", Float(64, modifiers=Modifiers(codecs=["ZSTD(1)"]))),
+    Column(
+        "retention_days",
+        UInt(16, modifiers=Modifiers(codecs=["DoubleDelta", "ZSTD(1)"])),
+    ),
+    Column("name", String(modifiers=Modifiers(codecs=["ZSTD(1)"]))),
+    Column("sampling_factor", Float(64, modifiers=Modifiers(codecs=["ZSTD(1)"]))),
+    Column("sampling_weight", Float(64, modifiers=Modifiers(codecs=["ZSTD(1)"]))),
+    Column("sign", Int(8, modifiers=Modifiers(codecs=["DoubleDelta(1)"]))),
+]
+columns.extend(
     [
-        Column("organization_id", UInt(64)),
-        Column("project_id", UInt(64)),
-        Column("trace_id", UUID()),
-        Column("span_id", UInt(64)),
-        Column("parent_span_id", UInt(64, Modifiers(codecs=["ZSTD(1)"]))),
-        Column("segment_id", UInt(64, Modifiers(codecs=["ZSTD(1)"]))),
-        Column("segment_name", String(Modifiers(codecs=["ZSTD(1)"]))),
-        Column("is_segment", Bool()),
-        Column(
-            "_sort_timestamp", DateTime(Modifiers(codecs=["DoubleDelta", "ZSTD(1)"]))
-        ),
-        Column(
-            "start_timestamp",
-            DateTime64(6, modifiers=Modifiers(codecs=["DoubleDelta", "ZSTD(1)"])),
-        ),
-        Column(
-            "end_timestamp",
-            DateTime64(6, modifiers=Modifiers(codecs=["DoubleDelta", "ZSTD(1)"])),
-        ),
-        Column(
-            "duration_ms",
-            UInt(32, modifiers=Modifiers(codecs=["DoubleDelta", "ZSTD(1)"])),
-        ),
-        Column("exclusive_time_ms", Float(64, modifiers=Modifiers(codecs=["ZSTD(1)"]))),
-        Column(
-            "retention_days",
-            UInt(16, modifiers=Modifiers(codecs=["DoubleDelta", "ZSTD(1)"])),
-        ),
-        Column("name", String(modifiers=Modifiers(codecs=["ZSTD(1)"]))),
-        Column("sampling_factor", Float(64, modifiers=Modifiers(codecs=["ZSTD(1)"]))),
-        Column("sampling_weight", Float(64, modifiers=Modifiers(codecs=["ZSTD(1)"]))),
-        Column("sign", Int(8, modifiers=Modifiers(codecs=["DoubleDelta(1)"]))),
-    ]
-    + [
         Column(
             f"attr_str_{i}",
             Map(String(), String(), modifiers=Modifiers(codecs=["ZSTD(1)"])),
         )
         for i in range(50)
     ]
-    + [
+)
+
+columns.extend(
+    [
         Column(
             f"attr_num_{i}",
             Map(String(), Float(64), modifiers=Modifiers(codecs=["ZSTD(1)"])),
         )
         for i in range(50)
     ]
-    + [
+)
+columns.extend(
+    [
         Column(
             f"attr_bool_{i}",
             Map(String(), Bool(), modifiers=Modifiers(codecs=["ZSTD(1)"])),
@@ -146,7 +149,7 @@ class Migration(migration.ClickhouseNodeMigration):
     blocking = False
 
     def forwards_ops(self) -> Sequence[SqlOperation]:
-        return [
+        res: List[SqlOperation] = [
             operations.CreateTable(
                 storage_set=storage_set_name,
                 table_name=local_table_name,
@@ -172,7 +175,9 @@ class Migration(migration.ClickhouseNodeMigration):
                 ),
                 target=OperationTarget.DISTRIBUTED,
             ),
-        ] + index_create_ops
+        ]
+        res.extend(index_create_ops)
+        return res
 
     def backwards_ops(self) -> Sequence[SqlOperation]:
         return [
