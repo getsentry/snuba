8f417c41bc001d0cd8a760a1118c0f30cf8e34c4
Evan Hicks <evan.hicks@sentry.io>
2024-07-26 14:58:46 -0400

    feat(eap): Add attribute key/value lookup table
    Create a table that stores just the attribute keys and values that have been seen by an org. This
    can be used for autocomplete or any other validation feature.
    
    The table doesn't have any indexes on it at the moment. It also provides a count, to do simple
    ranking if that's useful in the future.

--- snuba/utils/constants.py
+++ snuba/utils/constants.py
@@ -5,3 +5,6 @@ NESTED_COL_EXPR_RE = re.compile(r"^([a-zA-Z0-9_\.]+)\[([a-zA-Z0-9_\.:-]+)\]$")
 
 #: Metrics granularities for which a materialized view exist, in ascending order
 GRANULARITIES_AVAILABLE = (10, 60, 60 * 60, 24 * 60 * 60)
+
+# Number of EAP Span buckets
+ATTRIBUTE_BUCKETS = 20
