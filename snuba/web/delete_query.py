c60bc6a545d7c44165de9eebcf40a16594fbd9b0
xurui-c <159840875+xurui-c@users.noreply.github.com>
2024-08-01 09:38:17 -0700

    ref(lwdeletes): Integrate deletion processors into the pipeline (#6147)
    https://getsentry.atlassian.net/browse/SNS-2820
    
    Now that the lightweight deletes pipeline/skeleton has been built, we
    integrate deletion processors into the pipeline
    
    ---------
    
    Co-authored-by: Rachel Chen <rachelchen@PL6VFX9HP4.local>
    Co-authored-by: Meredith Heller <meredith@sentry.io>

--- snuba/web/delete_query.py
+++ snuba/web/delete_query.py
@@ -13,6 +13,7 @@ from snuba.query.data_source.simple import Table
 from snuba.query.dsl import column, equals, in_cond, literal, literals_tuple
 from snuba.query.exceptions import TooManyDeleteRowsException
 from snuba.query.expressions import Expression, FunctionCall
+from snuba.query.query_settings import HTTPQuerySettings
 from snuba.reader import Result
 from snuba.state import get_config
 from snuba.utils.metrics.util import with_span
@@ -20,7 +21,8 @@ from snuba.utils.metrics.util import with_span
 
 @with_span()
 def delete_from_storage(
-    storage: WritableTableStorage, columns: Dict[str, list[Any]]
+    storage: WritableTableStorage,
+    columns: Dict[str, list[Any]],
 ) -> dict[str, Result]:
     """
     Inputs:
@@ -97,7 +99,9 @@ def _enforce_max_rows(delete_query: Query) -> None:
 
 
 def _delete_from_table(
-    storage: WritableTableStorage, table: str, conditions: Dict[str, Any]
+    storage: WritableTableStorage,
+    table: str,
+    conditions: Dict[str, Any],
 ) -> Result:
     cluster_name = storage.get_cluster().get_clickhouse_cluster_name()
     on_cluster = literal(cluster_name) if cluster_name else None
@@ -114,6 +118,12 @@ def _delete_from_table(
         is_delete=True,
     )
     _enforce_max_rows(query)
+
+    deletion_processors = storage.get_deletion_processors()
+    # These settings aren't needed at the moment
+    dummy_query_settings = HTTPQuerySettings()
+    for deletion_procesor in deletion_processors:
+        deletion_procesor.process_query(query, dummy_query_settings)
 
     formatted_query = format_query(query)
     # TODO error handling and the lot
