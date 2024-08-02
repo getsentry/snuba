ff94a0c031b915d1154b36b4e92a46c40493432d
Enoch Tang <enoch.tang@sentry.io>
2024-08-02 14:17:00 -0400

    use array states rather than array join

--- snuba/snuba_migrations/generic_metrics/0060_distributions_mv4.py
+++ snuba/snuba_migrations/generic_metrics/0060_distributions_mv4.py
@@ -51,25 +51,26 @@ class Migration(migration.ClickhouseNodeMigration):
                     org_id,
                     project_id,
                     metric_id,
-                    arrayJoin(granularities) AS granularity,
+                    granularity,
                     toDateTime(multiIf(granularity = 0, 10, granularity = 1, 60, granularity = 2, 3600, granularity = 3, 86400, -1) * intDiv(toUnixTimestamp(timestamp), multiIf(granularity = 0, 10, granularity = 1, 60, granularity = 2, 3600, granularity = 3, 86400, -1))) AS timestamp,
                     least(retention_days, multiIf(granularity = 0, decasecond_retention_days, granularity = 1, min_retention_days, granularity = 2, hr_retention_days, granularity = 3, day_retention_days, 0)) AS retention_days,
                     tags.key,
                     tags.indexed_value,
                     tags.raw_value,
                     use_case_id,
-                    quantilesStateIf(0.5, 0.75, 0.9, 0.95, 0.99)(arrayJoin(distribution_values) AS values_rows, disable_percentiles = 0) AS percentiles,
-                    quantilesTDigestWeightedStateIf(0.5, 0.75, 0.9, 0.95, 0.99)(values_rows, sampling_weight, disable_percentiles = 0) AS percentiles_weighted,
-                    minState(values_rows) AS min,
-                    maxState(values_rows) AS max,
-                    avgState(values_rows) as avg,
-                    avgWeightedState(values_rows, sampling_weight) AS avg_weighted,
-                    sumState(values_rows) AS sum,
-                    sumState(values_rows * sampling_weight) AS sum_weighted,
-                    countState(values_rows) AS count,
-                    sumState(sampling_weight) AS count_weighted,
-                    histogramStateIf(250)(values_rows, (disable_percentiles = 0) AND enable_histogram) AS histogram_buckets
+                    quantilesStateArrayIf(0.5, 0.75, 0.9, 0.95, 0.99)(distribution_values, disable_percentiles = 0) AS percentiles,
+                    quantilesTDigestWeightedStateArrayIf(0.5, 0.75, 0.9, 0.95, 0.99)(distribution_values, arrayWithConstant(length(distribution_values) AS distribution_length, sampling_weight) AS sampling_weight_array, disable_percentiles = 0) AS percentiles_weighted,
+                    minStateArray(distribution_values) AS min,
+                    maxStateArray(distribution_values) AS max,
+                    avgStateArray(distribution_values) as avg,
+                    avgWeightedStateArray(distribution_values, sampling_weight_array) AS avg_weighted,
+                    sumStateArray(distribution_values) AS sum,
+                    sumStateArray(arrayMap(x -> (x * sampling_weight), distribution_values)) AS sum_weighted,
+                    countStateArray(distribution_values) AS count,
+                    sumState(distribution_length * sampling_weight) AS count_weighted,
+                    histogramStateArrayIf(250)(distribution_values, (disable_percentiles = 0) AND enable_histogram) AS histogram_buckets
                 FROM generic_metric_distributions_raw_local
+                ARRAY JOIN granularities AS granularity
                 WHERE (materialization_version = 4) AND (metric_type = 'distribution')
                 GROUP BY
                     org_id,
