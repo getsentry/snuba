3e9ebf96bb71f2587a923b47453d7017c45d1a95
Colin Chartier <colin.chartier@sentry.io>
2024-08-07 12:57:14 -0400

    ws

--- snuba/utils/constants.py
+++ snuba/utils/constants.py
@@ -6,7 +6,6 @@ NESTED_COL_EXPR_RE = re.compile(r"^([a-zA-Z0-9_\.]+)\[([a-zA-Z0-9_\.:-]+)\]$")
 #: Metrics granularities for which a materialized view exist, in ascending order
 GRANULARITIES_AVAILABLE = (10, 60, 60 * 60, 24 * 60 * 60)
 
-
 # Number of EAP Span buckets
 # Changing this column will change Snuba's understanding of how attributes
 # on spans are bucketed into different columns
