dac58f6d6a67518f79630e95b6c187de5a575c34
Evan Hicks <evan.hicks@sentry.io>
2024-08-09 12:43:40 -0400

    switch to add column

--- snuba/snuba_migrations/events_analytics_platform/0004_modify_sampling_weight.py
+++ snuba/snuba_migrations/events_analytics_platform/0004_modify_sampling_weight.py
@@ -3,31 +3,50 @@ from typing import Sequence
 from snuba.clusters.storage_sets import StorageSetKey
 from snuba.migrations import migration, operations
 from snuba.migrations.columns import MigrationModifiers as Modifiers
-from snuba.utils.schemas import Column, Float, UInt
+from snuba.utils.schemas import Column, UInt
 
 
 class Migration(migration.ClickhouseNodeMigration):
     blocking = False
+    local_table_name = "eap_spans_local"
+    dist_table_name = "eap_spans_dist"
 
     def forwards_ops(self) -> Sequence[operations.SqlOperation]:
         return [
-            operations.ModifyColumn(
+            operations.AddColumn(
                 storage_set=StorageSetKey.EVENTS_ANALYTICS_PLATFORM,
-                table_name="eap_spans_local",
+                table_name=self.local_table_name,
                 column=Column(
-                    "sampling_weight", UInt(64, modifiers=Modifiers(codecs=["ZSTD(1)"]))
+                    "sampling_weight_2",
+                    UInt(64, modifiers=Modifiers(codecs=["ZSTD(1)"])),
                 ),
-            )
+                after="sampling_weight",
+                target=operations.OperationTarget.LOCAL,
+            ),
+            operations.AddColumn(
+                storage_set=StorageSetKey.EVENTS_ANALYTICS_PLATFORM,
+                table_name=self.dist_table_name,
+                column=Column(
+                    "sampling_weight_2",
+                    UInt(64, modifiers=Modifiers(codecs=["ZSTD(1)"])),
+                ),
+                after="sampling_weight",
+                target=operations.OperationTarget.DISTRIBUTED,
+            ),
         ]
 
     def backwards_ops(self) -> Sequence[operations.SqlOperation]:
         return [
-            operations.ModifyColumn(
+            operations.DropColumn(
                 storage_set=StorageSetKey.EVENTS_ANALYTICS_PLATFORM,
-                table_name="eap_spans_local",
-                column=Column(
-                    "sampling_weight",
-                    Float(64, modifiers=Modifiers(codecs=["ZSTD(1)"])),
-                ),
-            )
+                table_name=self.local_table_name,
+                column_name="sampling_weight_2",
+                target=operations.OperationTarget.LOCAL,
+            ),
+            operations.DropColumn(
+                storage_set=StorageSetKey.EVENTS_ANALYTICS_PLATFORM,
+                table_name=self.dist_table_name,
+                column_name="sampling_weight_2",
+                target=operations.OperationTarget.DISTRIBUTED,
+            ),
         ]
