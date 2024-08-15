349249e897e102fc71ac80b272985517938a0ba4
MeredithAnya <meredith@sentry.io>
2024-08-15 11:28:39 -0700

    ref(snuba-deletes): add ColumnValidator (#6198)
    We have some [schema
    validation](https://github.com/getsentry/snuba/blob/d4b164c06710e15bed33d868102305adf8fe495c/snuba/query/schema.py#L38)
    on delete api, but because the columns depend on the storage, we can't
    do that level of type validation when parsing the payload. The only
    required column we have is the `project_id` (which is debatable on
    whether we should have that there but anyway)
    
    If the column `occurrence_id` is an uuid and someone sends:
    
    ```json
    { "columns": {"occurrence_id": ["hello"]}
    ```
    we'll get a clickhouse error when trying to run the delete.
    
    **ColumnValidator**
    This validator will check to make sure that the type defined in the
    schema matches the values passed through in the api. I've only
    implemented a handful of types and I don't expect that we will need many
    more, at least for now.

--- snuba/web/delete_query.py
+++ snuba/web/delete_query.py
@@ -11,12 +11,17 @@ from snuba.query import SelectedExpression
 from snuba.query.conditions import combine_and_conditions
 from snuba.query.data_source.simple import Table
 from snuba.query.dsl import column, equals, in_cond, literal, literals_tuple
-from snuba.query.exceptions import NoRowsToDeleteException, TooManyDeleteRowsException
+from snuba.query.exceptions import (
+    InvalidQueryException,
+    NoRowsToDeleteException,
+    TooManyDeleteRowsException,
+)
 from snuba.query.expressions import Expression, FunctionCall
 from snuba.query.query_settings import HTTPQuerySettings
 from snuba.reader import Result
 from snuba.state import get_config
 from snuba.utils.metrics.util import with_span
+from snuba.utils.schemas import ColumnValidator, InvalidColumnType
 
 
 class DeletesNotEnabledError(Exception):
@@ -149,6 +154,15 @@ def _delete_from_table(
         on_cluster=on_cluster,
         is_delete=True,
     )
+
+    columns = storage.get_schema().get_columns()
+    column_validator = ColumnValidator(columns)
+    try:
+        for col, values in conditions.items():
+            column_validator.validate(col, values)
+    except InvalidColumnType as e:
+        raise InvalidQueryException(e.message)
+
     try:
         _enforce_max_rows(query)
     except NoRowsToDeleteException:
