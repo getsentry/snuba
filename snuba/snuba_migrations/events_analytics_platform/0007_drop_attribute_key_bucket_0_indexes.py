a98661774716b12a941f543b09e5fa13d92d63c1
Pierre Massat <pierre.massat@sentry.io>
2024-08-15 14:07:03 -0400

    Remove unneeded code

--- snuba/snuba_migrations/events_analytics_platform/0007_drop_attribute_key_bucket_0_indexes.py
+++ snuba/snuba_migrations/events_analytics_platform/0007_drop_attribute_key_bucket_0_indexes.py
@@ -2,25 +2,10 @@ from typing import Sequence
 
 from snuba.clusters.storage_sets import StorageSetKey
 from snuba.migrations import migration, operations
-from snuba.migrations.operations import AddIndicesData, SqlOperation
+from snuba.migrations.operations import SqlOperation
 
 storage_set_name = StorageSetKey.EVENTS_ANALYTICS_PLATFORM
 local_table_name = "eap_spans_local"
-
-indices: Sequence[AddIndicesData] = [
-    AddIndicesData(
-        name="bf_attr_str_0",
-        expression="mapKeys(attr_str_0)",
-        type="bloom_filter",
-        granularity=1,
-    ),
-    AddIndicesData(
-        name="bf_attr_num_0",
-        expression="mapKeys(attr_num_0)",
-        type="bloom_filter",
-        granularity=1,
-    ),
-]
 
 
 class Migration(migration.ClickhouseNodeMigration):
@@ -31,10 +16,15 @@ class Migration(migration.ClickhouseNodeMigration):
             operations.DropIndex(
                 storage_set=StorageSetKey.EVENTS_ANALYTICS_PLATFORM,
                 table_name="eap_spans_local",
-                index_name=idx.name,
+                index_name="bf_attr_str_0",
                 target=operations.OperationTarget.LOCAL,
-            )
-            for idx in indices
+            ),
+            operations.DropIndex(
+                storage_set=StorageSetKey.EVENTS_ANALYTICS_PLATFORM,
+                table_name="eap_spans_local",
+                index_name="bf_attr_num_0",
+                target=operations.OperationTarget.LOCAL,
+            ),
         ]
 
     def backwards_ops(self) -> Sequence[SqlOperation]:
