47905bffa97f4d408d934cb7ac381d46a50e9e52
Enoch Tang <enoch.tang@sentry.io>
2024-08-15 16:30:38 -0400

    fix with totals

--- snuba/query/mql/parser.py
+++ snuba/query/mql/parser.py
@@ -1136,7 +1136,6 @@ def populate_query_from_mql_context(
         )
         query.add_condition_to_ast(context_condition)
 
-        query.set_totals(with_totals)
         if orderby:
             query.set_ast_orderby([orderby])
 
@@ -1149,6 +1148,10 @@ def populate_query_from_mql_context(
                 query.set_ast_groupby(list(groupby) + [selected_time.expression])
             else:
                 query.set_ast_groupby([selected_time.expression])
+
+        if query.get_groupby():
+            # Only set WITH TOTALS if there is a group by. WITH TOTALS does not work without one.
+            query.set_totals(with_totals)
 
     if isinstance(query, CompositeQuery):
 
