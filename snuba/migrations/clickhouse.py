24264d3645e1e6fab89b4f351f61648962b09c80
Meredith Heller <meredith@sentry.io>
2024-08-28 11:16:58 -0700

    ref(clickhouse): update versions and add 24.3

--- snuba/migrations/clickhouse.py
+++ snuba/migrations/clickhouse.py
@@ -1,4 +1,5 @@
-CLICKHOUSE_SERVER_MIN_VERSION = "21.8.12.29"
-# Note: 21.8.12.29 and 21.8.13.1 are used in self-hosted builds
-# even though SaaS clusters are all on 22.8 or above
-CLICKHOUSE_SERVER_MAX_VERSION = "23.8.11.29"
+CLICKHOUSE_SERVER_MIN_VERSION = "22.8.15.25"
+# Note: some test regions are using 22.8.15.25.
+# 23.8.11.29 is the base image for self-hosted and
+# saas clusters should be on 23.8.11.29
+CLICKHOUSE_SERVER_MAX_VERSION = "24.3.5.47"
