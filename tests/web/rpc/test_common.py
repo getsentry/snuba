1112218b82660bfc9d224f9aff51801d076c2b40
Colin Chartier <colin.chartier@sentry.io>
2024-08-29 15:35:07 -0400

    mypy

--- tests/web/rpc/test_common.py
+++ tests/web/rpc/test_common.py
@@ -5,9 +5,9 @@ from sentry_protos.snuba.v1alpha.trace_item_attribute_pb2 import (
     AttributeKeyTransformContext,
 )
 
-from snuba.query import SubscriptableReference
 from snuba.query.dsl import Functions as f
 from snuba.query.dsl import column, literal, literals_array
+from snuba.query.expressions import SubscriptableReference
 from snuba.web.rpc.common import attribute_key_to_expression
 
 
