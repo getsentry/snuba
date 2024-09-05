96a0d9765fe0e2726f6460759fdf27ae98203d71
Colin Chartier <colin.chartier@sentry.io>
2024-09-05 16:08:42 -0400

    pr feedback

--- snuba/web/rpc/tags_list.py
+++ snuba/web/rpc/tags_list.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import List, Optional
 
 from sentry_protos.snuba.v1alpha.endpoint_tags_list_pb2 import (
@@ -6,7 +7,7 @@ from sentry_protos.snuba.v1alpha.endpoint_tags_list_pb2 import (
 )
 
 from snuba.clickhouse.formatter.nodes import FormattedQuery, StringNode
-from snuba.datasets.plans.storage_processing import get_query_data_source
+from snuba.datasets.schemas.tables import TableSource
 from snuba.datasets.storages.factory import get_storage
 from snuba.datasets.storages.storage_key import StorageKey
 from snuba.utils.metrics.timer import Timer
@@ -19,35 +20,39 @@ def tags_list_query(
     str_storage = get_storage(StorageKey("spans_str_attrs"))
     num_storage = get_storage(StorageKey("spans_num_attrs"))
 
-    str_from = get_query_data_source(
-        str_storage.get_schema().get_data_source(),
-        allocation_policies=str_storage.get_allocation_policies(),
-        final=False,
-        sampling_rate=None,
-        storage_key=str_storage.get_storage_key(),
-    )
-
-    num_from = get_query_data_source(
-        num_storage.get_schema().get_data_source(),
-        allocation_policies=num_storage.get_allocation_policies(),
-        final=False,
-        sampling_rate=None,
-        storage_key=num_storage.get_storage_key(),
-    )
+    str_data_source = str_storage.get_schema().get_data_source()
+    assert isinstance(str_data_source, TableSource)
+    num_data_source = num_storage.get_schema().get_data_source()
+    assert isinstance(num_data_source, TableSource)
 
     if request.limit > 1000:
         raise BadSnubaRPCRequestException("Limit can be at most 1000")
 
+    start_timestamp = datetime.utcfromtimestamp(request.meta.start_timestamp.seconds)
+    if start_timestamp.day >= datetime.now().day and start_timestamp.hour != 0:
+        raise BadSnubaRPCRequestException(
+            "Tags' timestamps are stored per-day, you probably want to set start_timestamp to UTC 00:00 today or a time yesterday."
+        )
+
     query = f"""
 SELECT * FROM (
-    SELECT DISTINCT attr_key, 'str' as type
-    FROM {str_from.table_name}
+    SELECT DISTINCT attr_key, 'str' as type, timestamp
+    FROM {str_data_source.get_table_name()}
+    WHERE organization_id={request.meta.organization_id}
+    AND project_id IN ({', '.join(str(pid) for pid in request.meta.project_ids)})
+    AND timestamp BETWEEN fromUnixTimestamp({request.meta.start_timestamp.seconds}) AND fromUnixTimestamp({request.meta.end_timestamp.seconds})
+
     UNION ALL
-    SELECT DISTINCT attr_key, 'num' as type
-    FROM {num_from.table_name}
-) LIMIT {request.limit} OFFSET {request.offset}
-SETTINGS max_bytes_to_read=1000000000
+
+    SELECT DISTINCT attr_key, 'num' as type, timestamp
+    FROM {num_data_source.get_table_name()}
+    WHERE organization_id={request.meta.organization_id}
+    AND project_id IN ({', '.join(str(pid) for pid in request.meta.project_ids)})
+)
+ORDER BY attr_key
+LIMIT {request.limit} OFFSET {request.offset}
 """
+    print(query, file=open("/tmp/derp", "w+"))
 
     cluster = str_storage.get_cluster()
     reader = cluster.get_reader()
