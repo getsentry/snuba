067fbd8365cdc6e50a70a88f39a492671a68aebc
colin-sentry <161344340+colin-sentry@users.noreply.github.com>
2024-09-10 14:34:25 -0400

    fix(eap): Make span_id be returned as a string correctly (#6283)
    Adds some tests for span_samples, and solves some issues with span_id

---
+++ tests/web/rpc/test_span_samples.py
@@ -0,0 +1,233 @@
+import random
+import uuid
+from datetime import datetime, timedelta
+from typing import Any, Mapping
+
+import pytest
+from google.protobuf.timestamp_pb2 import Timestamp
+from sentry_protos.snuba.v1alpha.endpoint_span_samples_pb2 import SpanSamplesRequest
+from sentry_protos.snuba.v1alpha.request_common_pb2 import RequestMeta
+from sentry_protos.snuba.v1alpha.trace_item_attribute_pb2 import (
+    AttributeKey,
+    AttributeValue,
+)
+from sentry_protos.snuba.v1alpha.trace_item_filter_pb2 import (
+    ComparisonFilter,
+    ExistsFilter,
+    OrFilter,
+    TraceItemFilter,
+)
+
+from snuba.datasets.storages.factory import get_storage
+from snuba.datasets.storages.storage_key import StorageKey
+from snuba.web.rpc.span_samples import span_samples_query
+from tests.base import BaseApiTest
+from tests.helpers import write_raw_unprocessed_events
+
+
+def gen_message(dt: datetime) -> Mapping[str, Any]:
+    return {
+        "description": "/api/0/relays/projectconfigs/",
+        "duration_ms": 152,
+        "event_id": "d826225de75d42d6b2f01b957d51f18f",
+        "exclusive_time_ms": 0.228,
+        "is_segment": True,
+        "data": {
+            "sentry.environment": "development",
+            "sentry.release": "backend@24.7.0.dev0+c45b49caed1e5fcbf70097ab3f434b487c359b6b",
+            "thread.name": "uWSGIWorker1Core0",
+            "thread.id": "8522009600",
+            "sentry.segment.name": "/api/0/relays/projectconfigs/",
+            "sentry.sdk.name": "sentry.python.django",
+            "sentry.sdk.version": "2.7.0",
+            "my.float.field": 101.2,
+            "my.int.field": 2000,
+            "my.neg.field": -100,
+            "my.neg.float.field": -101.2,
+            "my.true.bool.field": True,
+            "my.false.bool.field": False,
+        },
+        "measurements": {
+            "num_of_spans": {"value": 50.0},
+            "eap.measurement": {"value": random.choice([1, 100, 1000])},
+        },
+        "organization_id": 1,
+        "origin": "auto.http.django",
+        "project_id": 1,
+        "received": 1721319572.877828,
+        "retention_days": 90,
+        "segment_id": "8873a98879faf06d",
+        "sentry_tags": {
+            "category": "http",
+            "environment": "development",
+            "op": "http.server",
+            "platform": "python",
+            "release": "backend@24.7.0.dev0+c45b49caed1e5fcbf70097ab3f434b487c359b6b",
+            "sdk.name": "sentry.python.django",
+            "sdk.version": "2.7.0",
+            "status": "ok",
+            "status_code": "200",
+            "thread.id": "8522009600",
+            "thread.name": "uWSGIWorker1Core0",
+            "trace.status": "ok",
+            "transaction": "/api/0/relays/projectconfigs/",
+            "transaction.method": "POST",
+            "transaction.op": "http.server",
+            "user": "ip:127.0.0.1",
+        },
+        "span_id": "123456781234567D",
+        "tags": {
+            "http.status_code": "200",
+            "relay_endpoint_version": "3",
+            "relay_id": "88888888-4444-4444-8444-cccccccccccc",
+            "relay_no_cache": "False",
+            "relay_protocol_version": "3",
+            "relay_use_post_or_schedule": "True",
+            "relay_use_post_or_schedule_rejected": "version",
+            "server_name": "D23CXQ4GK2.local",
+            "spans_over_limit": "False",
+            "color": random.choice(["red", "green", "blue"]),
+            "location": random.choice(["mobile", "frontend", "backend"]),
+        },
+        "trace_id": uuid.uuid4().hex,
+        "start_timestamp_ms": int(dt.timestamp()) * 1000 - int(random.gauss(1000, 200)),
+        "start_timestamp_precise": dt.timestamp(),
+        "end_timestamp_precise": dt.timestamp() + 1,
+    }
+
+
+BASE_TIME = datetime.utcnow().replace(minute=0, second=0, microsecond=0) - timedelta(
+    minutes=180
+)
+
+
+@pytest.fixture(autouse=True)
+def setup_teardown(clickhouse_db: None, redis_db: None) -> None:
+    spans_storage = get_storage(StorageKey("eap_spans"))
+    start = BASE_TIME
+    messages = [gen_message(start - timedelta(minutes=i)) for i in range(120)]
+    write_raw_unprocessed_events(spans_storage, messages)  # type: ignore
+
+
+@pytest.mark.clickhouse_db
+@pytest.mark.redis_db
+class TestSpanSamples(BaseApiTest):
+    def test_basic(self) -> None:
+        ts = Timestamp()
+        ts.GetCurrentTime()
+        message = SpanSamplesRequest(
+            meta=RequestMeta(
+                project_ids=[1, 2, 3],
+                organization_id=1,
+                cogs_category="something",
+                referrer="something",
+                start_timestamp=ts,
+                end_timestamp=ts,
+            ),
+            filter=TraceItemFilter(
+                exists_filter=ExistsFilter(
+                    key=AttributeKey(type=AttributeKey.TYPE_STRING, name="category")
+                )
+            ),
+            keys=[AttributeKey(type=AttributeKey.TYPE_STRING, name="platform")],
+            order_by=[
+                SpanSamplesRequest.OrderBy(
+                    key=AttributeKey(type=AttributeKey.TYPE_STRING, name="status")
+                )
+            ],
+            limit=10,
+        )
+        response = self.app.post(
+            "/rpc/SpanSamplesRequest", data=message.SerializeToString()
+        )
+        assert response.status_code == 200, response.text
+
+    def test_with_data(self, setup_teardown: Any) -> None:
+        ts = Timestamp(seconds=int(BASE_TIME.timestamp()))
+        hour_ago = int((BASE_TIME - timedelta(hours=1)).timestamp())
+        message = SpanSamplesRequest(
+            meta=RequestMeta(
+                project_ids=[1, 2, 3],
+                organization_id=1,
+                cogs_category="something",
+                referrer="something",
+                start_timestamp=Timestamp(seconds=hour_ago),
+                end_timestamp=ts,
+            ),
+            filter=TraceItemFilter(
+                exists_filter=ExistsFilter(
+                    key=AttributeKey(type=AttributeKey.TYPE_STRING, name="category")
+                )
+            ),
+            keys=[AttributeKey(type=AttributeKey.TYPE_STRING, name="sentry.sdk.name")],
+            order_by=[
+                SpanSamplesRequest.OrderBy(
+                    key=AttributeKey(type=AttributeKey.TYPE_STRING, name="status")
+                )
+            ],
+            limit=61,
+            attribute_key_transform_context=None,
+        )
+        response = span_samples_query(message)
+        assert [
+            dict((k, x.results[k].val_str) for k in x.results)
+            for x in response.span_samples
+        ] == [{"sentry.sdk.name": "sentry.python.django"} for _ in range(60)]
+
+    def test_booleans_and_number_compares(self, setup_teardown: Any) -> None:
+        ts = Timestamp(seconds=int(BASE_TIME.timestamp()))
+        hour_ago = int((BASE_TIME - timedelta(hours=1)).timestamp())
+        message = SpanSamplesRequest(
+            meta=RequestMeta(
+                project_ids=[1, 2, 3],
+                organization_id=1,
+                cogs_category="something",
+                referrer="something",
+                start_timestamp=Timestamp(seconds=hour_ago),
+                end_timestamp=ts,
+            ),
+            filter=TraceItemFilter(
+                or_filter=OrFilter(
+                    filters=[
+                        TraceItemFilter(
+                            comparison_filter=ComparisonFilter(
+                                key=AttributeKey(
+                                    type=AttributeKey.TYPE_STRING,
+                                    name="eap.measurement",
+                                ),
+                                op=ComparisonFilter.OP_LESS_THAN_OR_EQUALS,
+                                value=AttributeValue(val_float=101),
+                            ),
+                        ),
+                        TraceItemFilter(
+                            comparison_filter=ComparisonFilter(
+                                key=AttributeKey(
+                                    type=AttributeKey.TYPE_STRING,
+                                    name="eap.measurement",
+                                ),
+                                op=ComparisonFilter.OP_GREATER_THAN,
+                                value=AttributeValue(val_float=999),
+                            ),
+                        ),
+                    ]
+                )
+            ),
+            keys=[
+                AttributeKey(type=AttributeKey.TYPE_BOOLEAN, name="is_segment"),
+                AttributeKey(type=AttributeKey.TYPE_STRING, name="span_id"),
+            ],
+            order_by=[
+                SpanSamplesRequest.OrderBy(
+                    key=AttributeKey(type=AttributeKey.TYPE_STRING, name="status")
+                )
+            ],
+            limit=61,
+            attribute_key_transform_context=None,
+        )
+        response = span_samples_query(message)
+        assert [
+            dict(
+                (k, (x.results[k].val_bool or x.results[k].val_str)) for k in x.results
+            )
+            for x in response.span_samples
+        ] == [{"is_segment": True, "span_id": "123456781234567d"} for _ in range(60)]
