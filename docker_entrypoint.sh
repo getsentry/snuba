36163162f48e98b93c7db06279e9097b3781820c
Riya Chakraborty <riya.chakraborty@sentry.io>
2024-09-09 18:56:50 -0700

    make fewer changes to existing entrypoint

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 set -e
 
-if [[ -z "${ENABLE_HEAPTRACK}" ]]; then
+# first check if we're passing flags, if so
+# prepend with snuba
+if [ "${1:0:1}" = '-' ]; then
+    set -- snuba "$@"
+fi
+
+help_result=$(snuba "${1}" --help)
+help_return=$?
+
+if [[ "${help_return}" -eq 0 ]]; then
   set -- snuba "$@"
 else
-  set -- heaptrack snuba "$@"
+  # Print the error message if it returns non-zero, to help with troubleshooting.
+  printf "Error running snuba ${1} --help, passing command to exec directly."
+  printf "\n${help_result}"
+fi
+
+if [[ -n "${ENABLE_HEAPTRACK}" ]]; then
+  set -- heaptrack "$@"
 fi
 
 exec "$@"
