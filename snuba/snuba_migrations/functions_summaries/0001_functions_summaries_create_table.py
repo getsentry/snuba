b6a82fc52d5a7dd17319888fad5f5a620eed9fff
Francesco Vigliaturo <francesco.vigliaturo@sentry.io>
2024-09-12 17:25:29 +0200

    address feedback
    * add start_timestamp
    * add fingerprint to the order by
    * remove one of the ID columns and add is_continuous

--- snuba/snuba_migrations/functions_summaries/0001_functions_summaries_create_table.py
+++ snuba/snuba_migrations/functions_summaries/0001_functions_summaries_create_table.py
@@ -13,10 +13,8 @@ dist_table_name = "functions_summaries_dist"
 
 columns: List[Column[Modifiers]] = [
     Column("project_id", UInt(64)),
-    # profile_id is nullable since this will only be used by transaction-based profiling
     Column("profile_id", UUID(Modifiers(nullable=True, low_cardinality=False))),
-    # profiler_id is nullable since this will only be used by continuous profiling
-    Column("profiler_id", UUID(Modifiers(nullable=True, low_cardinality=False))),
+    Column("is_continuous", UInt(8)),
     # transaction_name is nullable since this will only be used by transaction-based profiling
     Column("transaction_name", String(Modifiers(nullable=True, low_cardinality=True))),
     Column("fingerprint", UInt(64)),
@@ -27,6 +25,7 @@ columns: List[Column[Modifiers]] = [
     Column("max", Float(64)),
     Column("sum", Float(64)),
     Column("count", UInt(64)),
+    Column("start_timestamp", DateTime()),
     Column("end_timestamp", DateTime()),
     Column("platform", String(Modifiers(low_cardinality=True))),
     Column("environment", String(Modifiers(nullable=True, low_cardinality=True))),
@@ -46,7 +45,7 @@ class Migration(migration.ClickhouseNodeMigration):
                 table_name=local_table_name,
                 columns=columns,
                 engine=table_engines.MergeTree(
-                    order_by="(project_id, end_timestamp)",
+                    order_by="(project_id, fingerprint, start_timestamp)",
                     partition_by="(retention_days, toMonday(end_timestamp))",
                     settings={"index_granularity": "8192"},
                     storage_set=storage_set_name,
