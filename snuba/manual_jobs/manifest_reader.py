f030a4da06a42958acfc81878bf60a65f8b7b865
xurui-c <159840875+xurui-c@users.noreply.github.com>
2024-09-18 13:21:34 -0700

    feat(job-runner): enable snuba jobs command to run from JSON manifest (#6320)
    https://github.com/getsentry/snuba/pull/6288
    
    Co-authored-by: Rachel Chen <rachelchen@PL6VFX9HP4.attlocal.net>

--- snuba/manual_jobs/manifest_reader.py
+++ snuba/manual_jobs/manifest_reader.py
@@ -1,18 +1,34 @@
 import os
-from typing import Any, Sequence
+from typing import Mapping
 
 import simplejson
+
+from snuba.manual_jobs import JobSpec
 
 
 class _ManifestReader:
     @staticmethod
-    def read(filename: str) -> Sequence[Any]:
+    def read(filename: str) -> Mapping[str, JobSpec]:
         local_root = os.path.dirname(__file__)
+
         with open(os.path.join(local_root, filename)) as stream:
             contents = simplejson.loads(stream.read())
-            assert isinstance(contents, Sequence)
-            return contents
+
+            job_specs = {}
+            for content in contents:
+                job_id = content["id"]
+                assert isinstance(job_id, str)
+                job_type = content["job_type"]
+                assert isinstance(job_type, str)
+
+                job_spec = JobSpec(
+                    job_id=job_id,
+                    job_type=job_type,
+                    params=content.get("params"),
+                )
+                job_specs[job_id] = job_spec
+            return job_specs
 
 
-def read_jobs_manifest() -> Sequence[Any]:
+def read_jobs_manifest() -> Mapping[str, JobSpec]:
     return _ManifestReader.read("job_manifest.json")
