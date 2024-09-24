843e3bb7f7ae3b315b8d7c80d3497ef6663388b9
Markus Unterwaditzer <markus-tarpit+git@unterwaditzer.net>
2024-09-24 17:32:31 +0200

    fix clippy

--- rust_snuba/src/mutations/clickhouse.rs
+++ rust_snuba/src/mutations/clickhouse.rs
@@ -131,7 +131,9 @@ fn format_query(table: &str, batch: &MutationBatch) -> Vec<Vec<u8>> {
 
     let input_schema = format!("organization_id UInt64, _sort_timestamp DateTime, trace_id UUID, span_id UInt64 {attr_columns}");
     let create_tmp_table = format!("CREATE TEMPORARY TABLE new_data ({input_schema})").into_bytes();
-    let mut insert_tmp_table = format!("INSERT INTO new_data FORMAT JSONEachRow\n").into_bytes();
+    let mut insert_tmp_table = "INSERT INTO new_data FORMAT JSONEachRow\n"
+        .to_owned()
+        .into_bytes();
 
     for (filter, update) in &batch.0 {
         let mut attributes = AttributeMap::default();
