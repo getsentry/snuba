153f7d211769e0795ba603942f2c532b954013de
Meredith Heller <meredith@sentry.io>
2024-09-25 15:42:07 -0700

    add UI again

--- snuba/admin/static/capacity_management/index.tsx
+++ snuba/admin/static/capacity_management/index.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import Client from "SnubaAdmin/api_client";
-import {AllocationPolicyConfigs} from "SnubaAdmin/capacity_management/allocation_policy";
+import { AllocationPolicyConfigs } from "SnubaAdmin/capacity_management/allocation_policy";
 import { AllocationPolicy } from "SnubaAdmin/capacity_management/types";
 import { CustomSelect, getParamFromStorage } from "SnubaAdmin/select";
+import { COLORS } from "SnubaAdmin/theme";
 
 function CapacityManagement(props: { api: Client }) {
   const { api } = props;
@@ -39,6 +40,30 @@ function CapacityManagement(props: { api: Client }) {
       });
   }
 
+  function renderPolicies(policies: AllocationPolicy[]) {
+    if (!selectedStorage) {
+      return <p>Storage not selected.</p>;
+    }
+    if (policies.length == 0) {
+      return null;
+    }
+    return (
+      <div>
+        <p style={policyTypeStyle}>
+          Policy Type: {policies[0].query_type.toUpperCase()}
+        </p>
+        {policies.map((policy: AllocationPolicy) => (
+          <AllocationPolicyConfigs
+            api={api}
+            storage={selectedStorage}
+            policy={policy}
+            key={selectedStorage + policy.policy_name}
+          />
+        ))}
+      </div>
+    );
+  }
+
   return (
     <div>
       <p>
@@ -51,20 +76,24 @@ function CapacityManagement(props: { api: Client }) {
         />
       </p>
 
-      {selectedStorage && allocationPolicies ? (
-        allocationPolicies.map((policy: AllocationPolicy) => (
-          <AllocationPolicyConfigs
-            api={api}
-            storage={selectedStorage}
-            policy={policy}
-            key={selectedStorage + policy.policy_name}
-          />
-        ))
-      ) : (
-        <p>Storage not selected.</p>
+      {renderPolicies(
+        allocationPolicies.filter((policy) => policy.query_type == "select")
+      )}
+      {renderPolicies(
+        allocationPolicies.filter((policy) => policy.query_type == "delete")
       )}
     </div>
   );
 }
+
+const policyTypeStyle = {
+  fontSize: 18,
+  fontWeight: 600,
+  color: COLORS.HEADER_TEXT,
+  backgroundColor: COLORS.TEXT_LIGHTER,
+  maxWidth: "100%",
+  margin: "10px 0px",
+  padding: "5px",
+};
 
 export default CapacityManagement;
