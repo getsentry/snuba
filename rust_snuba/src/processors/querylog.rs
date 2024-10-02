f1643e2537e56f4d37b6fbb255e3f9aca35c0e2d
Rachel Chen <rachelchen@PL6VFX9HP4.attlocal.net>
2024-10-01 20:00:50 -0700

    c

--- rust_snuba/src/processors/querylog.rs
+++ rust_snuba/src/processors/querylog.rs
@@ -101,7 +101,7 @@ struct Profile {
 #[derive(Debug, Deserialize, JsonSchema, Default)]
 #[serde(default)]
 struct ResultProfile {
-    bytes: u64,
+    progress_bytes: u64,
     elapsed: f64,
 }
 
@@ -224,7 +224,7 @@ impl TryFrom<Vec<FromQuery>> for QueryList {
             groupby_columns.push(q.profile.groupby_cols);
             array_join_columns.push(q.profile.array_join_cols);
             let result_profile = q.result_profile.unwrap_or_default();
-            bytes_scanned.push(result_profile.bytes);
+            bytes_scanned.push(result_profile.progress_bytes);
             duration_ms.push((result_profile.elapsed * 1000.0) as u64);
 
             // consistent, cache hit, max_threads and is_duplicated may not be present
@@ -382,7 +382,7 @@ mod tests {
                   "array_join_cols": ["errors_local.tags.key", "errors_local.tags.value"]
                 },
                 "result_profile": {
-                  "bytes": 1305,
+                  "progress_bytes": 1305,
                   "blocks": 1,
                   "rows": 22,
                   "elapsed": 0.009863138198852539
@@ -489,7 +489,7 @@ mod tests {
                   "array_join_cols": ["errors_local.tags.key", "errors_local.tags.value"]
                 },
                 "result_profile": {
-                  "bytes": 1305,
+                  "progress_bytes": 1305,
                   "blocks": 1,
                   "rows": 22,
                   "elapsed": 0.009863138198852539
@@ -596,7 +596,7 @@ mod tests {
                   "array_join_cols": ["errors_local.tags.key", "errors_local.tags.value"]
                 },
                 "result_profile": {
-                  "bytes": 1305,
+                  "progress_bytes": 1305,
                   "blocks": 1,
                   "rows": 22,
                   "elapsed": 0.009863138198852539
