0bcbbb1a7639c47768633cac7787a17cc464944a
Evan Hicks <evan.hicks@sentry.io>
2024-10-02 16:42:17 -0400

    cleanup

--- snuba/snuba_migrations/events_analytics_platform/0016_remove_old_span_attribute_table.py
+++ snuba/snuba_migrations/events_analytics_platform/0016_remove_old_span_attribute_table.py
@@ -3,36 +3,25 @@ from __future__ import annotations
 from typing import Sequence
 
 from snuba.clusters.storage_sets import StorageSetKey
-from snuba.migrations import migration, operations
-from snuba.migrations.columns import MigrationModifiers as Modifiers
-from snuba.migrations.operations import OperationTarget, SqlOperation
-from snuba.utils.schemas import AggregateFunction, Column, DateTime, String, UInt
+from snuba.migrations.migration import ClickhouseNodeMigration
+from snuba.migrations.operations import DropTable, OperationTarget, SqlOperation
 
 
-class Migration(migration.ClickhouseNodeMigration):
+class Migration(ClickhouseNodeMigration):
     blocking = False
     storage_set_key = StorageSetKey.EVENTS_ANALYTICS_PLATFORM
 
     meta_local_table_name = "spans_attributes_meta_local"
     meta_dist_table_name = "spans_attributes_meta_dist"
-    meta_table_columns: Sequence[Column[Modifiers]] = [
-        Column("organization_id", UInt(64)),
-        Column("attribute_type", String()),
-        Column("attribute_key", String()),
-        Column("attribute_value", String()),
-        Column("timestamp", DateTime(modifiers=Modifiers(codecs=["DoubleDelta"]))),
-        Column("retention_days", UInt(16)),
-        Column("count", AggregateFunction("sum", [UInt(64)])),
-    ]
 
     def forwards_ops(self) -> Sequence[SqlOperation]:
         return [
-            operations.DropTable(
+            DropTable(
                 storage_set=self.storage_set_key,
                 table_name=self.meta_local_table_name,
                 target=OperationTarget.LOCAL,
             ),
-            operations.DropTable(
+            DropTable(
                 storage_set=self.storage_set_key,
                 table_name=self.meta_dist_table_name,
                 target=OperationTarget.DISTRIBUTED,
