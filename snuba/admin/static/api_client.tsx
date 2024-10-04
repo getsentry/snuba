19dfead09c8ee77bd0e161e84cda7e84a296779a
davidtsuk <132949946+davidtsuk@users.noreply.github.com>
2024-10-04 12:37:02 -0700

    feat(database-clusters): Add server settings for each node and additional data (#6386)
    Added server settings data for each node as well as additional columns
    in the main table.
    
    <img width="1487" alt="image"
    src="https://github.com/user-attachments/assets/1e94c297-b906-429b-b5fe-c8c56331c1c1">

--- snuba/admin/static/api_client.tsx
+++ snuba/admin/static/api_client.tsx
@@ -39,7 +39,7 @@ import { AllocationPolicy } from "SnubaAdmin/capacity_management/types";
 
 import { ReplayInstruction, Topic } from "SnubaAdmin/dead_letter_queue/types";
 import { AutoReplacementsBypassProjectsData } from "SnubaAdmin/auto_replacements_bypass_projects/types";
-import { ClickhouseNodeInfo } from "./database_clusters/types";
+import { ClickhouseNodeInfo, ClickhouseSystemSetting } from "SnubaAdmin/database_clusters/types";
 
 interface Client {
   getSettings: () => Promise<Settings>;
@@ -110,6 +110,7 @@ interface Client {
   ) => Promise<Response>;
   listJobSpecs: () => Promise<JobSpecMap>;
   runJob(job_id: string): Promise<String>;
+  getClickhouseSystemSettings: (host: string, port: number, storage: string) => Promise<ClickhouseSystemSetting[]>;
 }
 
 function Client(): Client {
@@ -533,7 +534,23 @@ function Client(): Client {
         headers: { "Content-Type": "application/json" },
         method: "POST",
       }).then((resp) => resp.text());
-    }
+    },
+    getClickhouseSystemSettings: (host: string, port: number, storage: string) => {
+      const url = `${baseUrl}clickhouse_system_settings?host=${encodeURIComponent(host)}&port=${encodeURIComponent(port)}&storage=${encodeURIComponent(storage)}`;
+      return fetch(url, {
+        headers: { "Content-Type": "application/json" },
+        method: "GET",
+      }).then((resp) => {
+        if (resp.ok) {
+          return resp.json();
+        } else {
+          return resp.json().then((err) => {
+            let errMsg = err?.error || "Could not get Clickhouse system settings";
+            throw new Error(errMsg);
+          });
+        }
+      });
+    },
   };
 }
 
