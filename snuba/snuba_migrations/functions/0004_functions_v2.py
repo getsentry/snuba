9ccdb3cb95386f6e4b7d9dd8e6f93a00fa2a1eb4
Francesco Vigliaturo <francesco.vigliaturo@sentry.io>
2024-10-17 20:37:07 +0200

    add columns to existing functions_mv and create a new view (functions_v2_local)

--- snuba/snuba_migrations/functions/0004_functions_v2.py
+++ snuba/snuba_migrations/functions/0004_functions_v2.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from typing import List, Sequence
 
 from snuba.clickhouse.columns import (
@@ -12,23 +13,34 @@ from snuba.clickhouse.columns import (
     UInt,
 )
 from snuba.clusters.storage_sets import StorageSetKey
-from snuba.migrations import migration, operations, table_engines
+from snuba.migrations import migration, operations
 from snuba.migrations.columns import MigrationModifiers as Modifiers
-from snuba.migrations.operations import OperationTarget, SqlOperation
+from snuba.migrations.operations import OperationTarget
+
+
+@dataclass(frozen=True)
+class NewColumn:
+    column: Column[Modifiers]
+    after: str | None
+
 
 columns: List[Column[Modifiers]] = [
     Column("project_id", UInt(64)),
     Column("transaction_name", String()),
     Column("timestamp", DateTime()),
+    Column("depth", UInt(32)),
+    Column("parent_fingerprint", UInt(64)),
     Column("fingerprint", UInt(64)),
     Column("name", String()),
     Column("package", String()),
+    Column("path", String()),
     Column("is_application", UInt(8)),
-    Column("profiling_type", String(Modifiers(low_cardinality=True))),
     Column("platform", String(Modifiers(low_cardinality=True))),
     Column("environment", String(Modifiers(nullable=True, low_cardinality=True))),
     Column("release", String(Modifiers(nullable=True, low_cardinality=True))),
-    # aggregate columns
+    Column("os_name", String(Modifiers(low_cardinality=True))),
+    Column("os_version", String(Modifiers(low_cardinality=True))),
+    Column("retention_days", UInt(16)),
     Column("count", AggregateFunction("count", [Float(64)])),
     Column(
         "percentiles",
@@ -38,121 +50,125 @@ columns: List[Column[Modifiers]] = [
     Column("max", AggregateFunction("max", [Float(64)])),
     Column("avg", AggregateFunction("avg", [Float(64)])),
     Column("sum", AggregateFunction("sum", [Float(64)])),
-    Column(
-        "worst",
-        AggregateFunction(
-            "argMax",
-            [
-                Tuple(
-                    (
-                        UUID(),  # profile_id
-                        String(),  # thread_id
-                        DateTime64(  # start_timestamp
-                            precision=6,
-                            modifiers=Modifiers(nullable=True),
-                        ),
-                        DateTime64(  # end_timestamp
-                            precision=6,
-                            modifiers=Modifiers(nullable=True),
-                        ),
-                    )
-                ),  # end Tuple
-                Float(64),
-            ],
-        ),
-    ),
-    Column(
-        "examples",
-        AggregateFunction(
-            "groupUniqArray(5)",
-            [
-                Tuple(
-                    (
-                        UUID(),  # profile_id
-                        String(),  # thread_id
-                        DateTime64(
-                            precision=6,
-                            modifiers=Modifiers(nullable=True),
-                        ),  # start_timestamp
-                        DateTime64(
-                            precision=6,
-                            modifiers=Modifiers(nullable=True),
-                        ),  # end_timestamp
-                    )
-                ),  # end Tuple
-            ],
-        ),
-    ),
-    Column("retention_days", UInt(16)),
+    Column("worst", AggregateFunction("argMax", [UUID(), Float(64)])),
+    Column("examples", AggregateFunction("groupUniqArray(5)", [UUID()])),
 ]
+
+
+new_columns: Sequence[NewColumn] = [
+    NewColumn(
+        Column(
+            "worst_v2",
+            AggregateFunction(
+                "argMax",
+                [
+                    Tuple(
+                        (
+                            UUID(),  # profile_id
+                            String(),  # thread_id
+                            DateTime64(  # start_timestamp
+                                precision=6,
+                                modifiers=Modifiers(nullable=True),
+                            ),
+                            DateTime64(  # end_timestamp
+                                precision=6,
+                                modifiers=Modifiers(nullable=True),
+                            ),
+                        )
+                    ),  # end Tuple
+                    Float(64),
+                ],
+            ),
+        ),
+        after="worst",
+    ),  # end new column
+    NewColumn(
+        Column(
+            "examples_v2",
+            AggregateFunction(
+                "groupUniqArray(5)",
+                [
+                    Tuple(
+                        (
+                            UUID(),  # profile_id
+                            String(),  # thread_id
+                            DateTime64(
+                                precision=6,
+                                modifiers=Modifiers(nullable=True),
+                            ),  # start_timestamp
+                            DateTime64(
+                                precision=6,
+                                modifiers=Modifiers(nullable=True),
+                            ),  # end_timestamp
+                        )
+                    ),  # end Tuple
+                ],
+            ),
+        ),
+        after="examples",
+    ),
+]
+
+all_columns = columns + [col.column for col in new_columns]
 
 
 class Migration(migration.ClickhouseNodeMigration):
     blocking = False
+    data_granularity = 60 * 60  # 1 hour buckets
     storage_set = StorageSetKey.FUNCTIONS
 
-    data_granularity = 60 * 60  # 1 hour buckets
-
     local_raw_table = "functions_raw_local"
+    dist_raw_table = "functions_raw_dist"
 
-    local_functions_table = "functions_v2_local"
-    dist_functions_table = "functions_v2_dist"
+    local_materialized_table = "functions_mv_local"
+    dist_materialized_table = "functions_mv_dist"
 
-    local_mv_table = "functions_mv_v2_local"
+    local_view_table = "functions_v2_local"  # newer materialized
 
-    def forwards_ops(self) -> Sequence[SqlOperation]:
+    def forwards_ops(self) -> Sequence[operations.SqlOperation]:
         return [
-            operations.CreateTable(
+            operations.AddColumn(
                 storage_set=self.storage_set,
-                table_name=self.local_functions_table,
-                columns=columns,
-                engine=table_engines.AggregatingMergeTree(
-                    storage_set=self.storage_set,
-                    order_by="(project_id, timestamp, transaction_name, fingerprint, name, package, is_application, profiling_type, platform, environment, release, retention_days)",
-                    primary_key="(project_id, timestamp, transaction_name, fingerprint)",
-                    partition_by="(retention_days, toMonday(timestamp))",
-                    settings={"index_granularity": "2048", "allow_nullable_key": 1},
-                    ttl="timestamp + toIntervalDay(retention_days)",
-                ),
-                target=OperationTarget.LOCAL,
-            ),
+                table_name=table_name,
+                column=new_column.column,
+                after=new_column.after,
+                target=target,
+            )
+            for table_name, target in [
+                (self.local_materialized_table, OperationTarget.LOCAL),
+                (self.dist_materialized_table, OperationTarget.DISTRIBUTED),
+            ]
+            for new_column in new_columns
+        ] + [
             operations.CreateMaterializedView(
                 storage_set=self.storage_set,
-                view_name=self.local_mv_table,
-                destination_table_name=self.local_functions_table,
-                columns=columns,
+                view_name=self.local_view_table,
+                destination_table_name=self.local_materialized_table,
+                columns=all_columns,
                 query=self.__MATVIEW_STATEMENT,
                 target=OperationTarget.LOCAL,
             ),
-            operations.CreateTable(
-                storage_set=self.storage_set,
-                table_name=self.dist_functions_table,
-                columns=columns,
-                engine=table_engines.Distributed(
-                    local_table_name=self.local_functions_table,
-                    sharding_key=None,
-                ),
-                target=OperationTarget.DISTRIBUTED,
-            ),
         ]
 
-    def backwards_ops(self) -> Sequence[SqlOperation]:
+    def backwards_ops(self) -> Sequence[operations.SqlOperation]:
         return [
+            operations.DropColumn(
+                storage_set=self.storage_set,
+                table_name=table_name,
+                column_name=new_column.column.name,
+                target=target,
+            )
+            for table_name, target in [
+                (self.dist_raw_table, OperationTarget.DISTRIBUTED),
+                (self.local_raw_table, OperationTarget.LOCAL),
+            ]
+            for new_column in new_columns
+        ] + [
             operations.DropTable(
                 storage_set=self.storage_set,
-                table_name=self.dist_functions_table,
-                target=OperationTarget.DISTRIBUTED,
-            ),
-            operations.DropTable(
-                storage_set=self.storage_set,
-                table_name=self.local_functions_table,
-                target=OperationTarget.LOCAL,
-            ),
-            operations.DropTable(
-                storage_set=self.storage_set,
-                table_name=self.local_mv_table,
-                target=OperationTarget.LOCAL,
-            ),
+                table_name=self.local_view_table,
+                target=operations.OperationTarget.LOCAL,
+            )
         ]
 
     @property
@@ -163,12 +179,16 @@ class Migration(migration.ClickhouseNodeMigration):
                 transaction_name,
                 name,
                 package,
+                path,
                 fingerprint,
+                parent_fingerprint,
+                depth,
                 is_application,
-                profiling_type,
                 platform,
                 environment,
                 release,
+                os_name,
+                os_version,
                 toDateTime({self.data_granularity} * intDiv(toUnixTimestamp(timestamp), {self.data_granularity})) AS timestamp,
                 retention_days,
                 countState(arrayJoin(durations) AS duration) AS count,
@@ -177,21 +197,27 @@ class Migration(migration.ClickhouseNodeMigration):
                 maxState(duration) AS max,
                 avgState(duration) AS avg,
                 sumState(duration) AS sum,
-                argMaxState((profile_id, thread_id, start_timestamp, end_timestamp), duration) as worst,
-                groupUniqArrayState(5)((profile_id, thread_id, start_timestamp, end_timestamp)) as examples
+                argMaxState(profile_id, duration) as worst,
+                argMaxState((profile_id, thread_id, start_timestamp, end_timestamp), duration) as worst_v2,
+                groupUniqArrayState(5)(profile_id) as examples
+                groupUniqArrayState(5)((profile_id, thread_id, start_timestamp, end_timestamp)) as examples_v2
             FROM {self.local_raw_table}
-            WHERE materialization_version = 0
+            WHERE materialization_version = 1
             GROUP BY
                 project_id,
                 transaction_name,
                 name,
                 package,
+                path,
                 fingerprint,
+                parent_fingerprint,
+                depth,
                 is_application,
-                profiling_type,
                 platform,
                 environment,
                 release,
+                os_name,
+                os_version,
                 timestamp,
                 retention_days
         """
