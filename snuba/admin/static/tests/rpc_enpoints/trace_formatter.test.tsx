2d48e399eecbe57e55b55a213814a8bda25d5bbb
nachi <nachi.nachiappan@sentry.io>
2024-11-01 14:37:24 -0700

    Remove unused jest-dom import from trace formatter test

--- snuba/admin/static/tests/rpc_enpoints/trace_formatter.test.tsx
+++ snuba/admin/static/tests/rpc_enpoints/trace_formatter.test.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import '@testing-library/jest-dom';
 import { TraceLog } from 'SnubaAdmin/rpc_endpoints/trace_formatter';
 
 describe('TraceLog', () => {
