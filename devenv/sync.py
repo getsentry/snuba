1b66c5a17da3c4523a0db1fd5ffd66529f65dae2
Joshua Li <joshuarli98@gmail.com>
2024-11-05 12:38:57 -0800

    [skip ci] wip

--- devenv/sync.py
+++ devenv/sync.py
@@ -1,5 +1,5 @@
 from devenv import constants
-from devenv.lib import brew, config, colima, limactl, proc
+from devenv.lib import brew, config, colima, limactl, proc, venv
 
 def main(context: dict[str, str]) -> int:
     reporoot = context["reporoot"]
@@ -24,6 +24,16 @@ def main(context: dict[str, str]) -> int:
         cfg["lima"][f"{constants.SYSTEM_MACHINE}_sha256"],
         reporoot,
     )
+
+    venv_dir, python_version, requirements, editable_paths, bins = venv.get(reporoot, "venv")
+    url, sha256 = config.get_python(reporoot, python_version)
+    print(f"ensuring venv at {venv_dir}...")
+    venv.ensure(venv_dir, python_version, url, sha256)
+
+    print(f"syncing venv with {requirements}...")
+    venv.sync(reporoot, venv_dir, requirements, editable_paths, bins)
+
+    # TODO: make install-python-dependencies install-rs-dev setup-git
 
     # start colima if it's not already running
     colima.start(reporoot)
