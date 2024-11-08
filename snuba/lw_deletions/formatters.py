2d54feaa4594201c82cabda23d7cf6fad32e9b0a
MeredithAnya <meredith@sentry.io>
2024-11-08 11:11:29 -0800

    ref(deletes): bulk delete consumer (#6510)
    **context**:
    The work for bulk deleting in snuba has so far included the following:
    * Added the kafka schemas -
    https://github.com/getsentry/sentry-kafka-schemas/pull/347
    * Adding the new endpoint including producing to the topic -
    https://github.com/getsentry/snuba/pull/6440
    * Created the topic in production for all env -
    https://github.com/getsentry/ops/pull/12711
    
    **what's left**:
    Now that we have the topics created we can finish up the consumer side.
    - [ ] Add the consumer logic to snuba (This PR)
    - [ ] Add the consumer deployment to S4S region in the ops repository
    - [ ] Set up datadog alerts/metrics and other observability
    
    **this PR**:
    It's a bit of a larger PR but it can be reviewed in a couple sections:
    * The main consumer logic and strategy
    * This has the logic to create the strategy factory for the consumer and
    composes the strategy steps. The `strategy.py` file has the details for
    actually executing the delete query
    * The `batching.py` file - I have an [arroyo
    PR](https://github.com/getsentry/arroyo/pull/390) that makes this file
    obsolete but in the meantime I don't think it needs to block this PR
    since it will be easy to remove after
    * The formatters are going to be the only code that someone will need to
    write in the future when deploying the deletions consumer for a
    different storage. How one formats the conditions for the `DELETE` query
    is up to that logic.

---
+++ snuba/lw_deletions/formatters.py
@@ -0,0 +1,63 @@
+from abc import ABC, abstractmethod
+from collections import defaultdict
+from typing import Mapping, MutableMapping, Sequence, Type
+
+from snuba.datasets.storages.storage_key import StorageKey
+from snuba.web.bulk_delete_query import DeleteQueryMessage
+from snuba.web.delete_query import ConditionsType
+
+
+class Formatter(ABC):
+    """
+    Simple class with just a format method, which should
+    be implemented for each storage type used for deletes.
+
+    The `format` method takes a list of batched up messages
+    and formats the conditions for the storage, if needed.
+    """
+
+    @abstractmethod
+    def format(
+        self, messages: Sequence[DeleteQueryMessage]
+    ) -> Sequence[ConditionsType]:
+        raise NotImplementedError
+
+
+class SearchIssuesFormatter(Formatter):
+    def format(
+        self, messages: Sequence[DeleteQueryMessage]
+    ) -> Sequence[ConditionsType]:
+        """
+        For the search issues storage we want the additional
+        formatting step of combining group ids for messages
+        that have the same project id.
+
+        ex.
+            project_id [1] and group_id [1, 2]
+            project_id [1] and group_id [3, 4]
+
+        would be grouped into one condition:
+
+            project_id [1] and group_id [1, 2, 3, 4]
+
+        """
+        mapping: MutableMapping[int, set[int]] = defaultdict(set)
+        conditions = [message["conditions"] for message in messages]
+        for condition in conditions:
+            project_id = condition["project_id"][0]
+            # appease mypy
+            assert isinstance(project_id, int)
+            mapping[project_id] = mapping[project_id].union(
+                # using int() to make mypy happy
+                set([int(g_id) for g_id in condition["group_id"]])
+            )
+
+        return [
+            {"project_id": [project_id], "group_id": list(group_ids)}
+            for project_id, group_ids in mapping.items()
+        ]
+
+
+STORAGE_FORMATTER: Mapping[str, Type[Formatter]] = {
+    StorageKey.SEARCH_ISSUES.value: SearchIssuesFormatter
+}
