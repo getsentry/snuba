47af90a5a369bb2cca000e06a097238e0a92a083
Volo Kluev <volo.kluev@sentry.io>
2024-11-12 11:50:53 -0800

    wip, no tests

--- rust_snuba/src/runtime_config.rs
+++ rust_snuba/src/runtime_config.rs
@@ -4,13 +4,40 @@ use pyo3::prelude::{PyModule, Python};
 use std::collections::BTreeMap;
 use std::time::Duration;
 
+use redis;
 use rust_arroyo::timer;
 use rust_arroyo::utils::timing::Deadline;
 
 static CONFIG: RwLock<BTreeMap<String, (Option<String>, Deadline)>> = RwLock::new(BTreeMap::new());
 
-/// Runtime config is cached for 10 seconds
 pub fn get_str_config(key: &str) -> Result<Option<String>, Error> {
+    let redis_host = std::env::var("REDIS_HOST").unwrap_or(String::from("127.0.0.1"));
+    let redis_port = std::env::var("REDIS_PORT").unwrap_or(String::from("6379"));
+    let redis_password = std::env::var("REDIS_PASSWORD").unwrap_or(String::from(""));
+    let redis_db = std::env::var("REDIS_DB").unwrap_or(String::from("1"));
+    let redis_ssl = std::env::var("REDIS_SSL").unwrap_or(String::from("1"));
+    let deadline = Deadline::new(Duration::from_secs(10));
+    let url = format!(
+        "redis://{}:{}@{}:{}/{}",
+        "default", redis_password, redis_host, redis_port, redis_db
+    );
+
+    if let Some(value) = CONFIG.read().get(key) {
+        let (config, deadline) = value;
+        if !deadline.has_elapsed() {
+            return Ok(config.clone());
+        }
+    }
+
+    let client = redis::Client::open(url)?;
+    let mut con = client.get_connection()?;
+
+    let configmap: HashMap<String, String> = client.hgetall("snuba-config");
+    return configmap.get(String::from(key));
+}
+
+/// Runtime config is cached for 10 seconds
+pub fn get_str_config_python(key: &str) -> Result<Option<String>, Error> {
     let deadline = Deadline::new(Duration::from_secs(10));
 
     if let Some(value) = CONFIG.read().get(key) {
