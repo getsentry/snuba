a6c9fcb80c22852f8b48b5829ead96cc2dea1a0c
Rachel Chen <rachelchen@PL6VFX9HP4.attlocal.net>
2024-11-21 00:01:09 -0800

    c

--- tests/web/rpc/v1/test_endpoint_time_series.py
+++ tests/web/rpc/v1/test_endpoint_time_series.py
@@ -226,6 +226,48 @@ class TestTimeSeriesApi(BaseApiTest):
             ),
         ]
 
+    def test_rachel(self) -> None:
+        start_timestamp_seconds = 1500
+        # store a a test metric with a value of 1, every second of one hour
+        granularity_secs = 15
+        query_duration = 60 * 30
+        store_timeseries(
+            datetime.fromtimestamp(start_timestamp_seconds, tz=UTC),
+            1,
+            3600,
+            metrics=[DummyMetric("test_metric", get_value=lambda x: 1)],
+        )
+
+        message = TimeSeriesRequest(
+            meta=RequestMeta(
+                project_ids=[1, 2, 3],
+                organization_id=1,
+                cogs_category="something",
+                referrer="something",
+                start_timestamp=Timestamp(seconds=start_timestamp_seconds),
+                end_timestamp=Timestamp(
+                    seconds=int(start_timestamp_seconds + query_duration)
+                ),
+            ),
+            aggregations=[
+                AttributeAggregation(
+                    aggregate=Function.FUNCTION_SUM,
+                    key=AttributeKey(type=AttributeKey.TYPE_FLOAT, name="test_metric"),
+                    label="sum",
+                    extrapolation_mode=ExtrapolationMode.EXTRAPOLATION_MODE_NONE,
+                ),
+                AttributeAggregation(
+                    aggregate=Function.FUNCTION_AVG,
+                    key=AttributeKey(type=AttributeKey.TYPE_FLOAT, name="test_metric"),
+                    label="avg",
+                    extrapolation_mode=ExtrapolationMode.EXTRAPOLATION_MODE_NONE,
+                ),
+            ],
+            granularity_secs=granularity_secs,
+        )
+        response = EndpointTimeSeries().execute(message)
+        assert len(response.result_timeseries) != 0
+
     def test_with_group_by(self) -> None:
         store_timeseries(
             BASE_TIME,
