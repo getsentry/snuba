4c362c0d6ebdc7308b39b42f1ee0278b431bfd38
Pierre Massat <pierre.massat@sentry.io>
2024-12-13 10:50:40 -0500

    feat(eap): Add a FindTraces endpoint

---
+++ snuba/web/rpc/v1/endpoint_find_traces.py
@@ -0,0 +1,262 @@
+import uuid
+from collections import defaultdict
+from typing import Any, Callable, Dict, Iterable, Sequence, Type
+
+from google.protobuf.json_format import MessageToDict
+from sentry_protos.snuba.v1.endpoint_find_traces_pb2 import (
+    FindTracesRequest,
+    FindTracesResponse,
+    TraceColumn,
+)
+from sentry_protos.snuba.v1.request_common_pb2 import PageToken
+from sentry_protos.snuba.v1.trace_item_attribute_pb2 import AttributeKey, AttributeValue
+
+from snuba.attribution.appid import AppID
+from snuba.attribution.attribution_info import AttributionInfo
+from snuba.datasets.entities.entity_key import EntityKey
+from snuba.datasets.entities.factory import get_entity
+from snuba.datasets.pluggable_dataset import PluggableDataset
+from snuba.query import OrderBy, OrderByDirection, SelectedExpression
+from snuba.query.data_source.simple import Entity
+from snuba.query.dsl import Functions as f
+from snuba.query.dsl import column
+from snuba.query.expressions import Expression
+from snuba.query.logical import Query
+from snuba.query.query_settings import HTTPQuerySettings
+from snuba.request import Request as SnubaRequest
+from snuba.web.query import run_query
+from snuba.web.rpc import RPCEndpoint
+from snuba.web.rpc.common.common import (
+    base_conditions_and,
+    trace_item_filters_to_expression,
+    treeify_or_and_conditions,
+)
+from snuba.web.rpc.common.debug_info import (
+    extract_response_meta,
+    setup_trace_query_settings,
+)
+from snuba.web.rpc.common.exceptions import BadSnubaRPCRequestException
+
+_DEFAULT_ROW_LIMIT = 10_000
+
+_COLUMN_TO_NAME: dict[TraceColumn.Name, str] = {
+    TraceColumn.Name.TRACE_ID: "trace_id",
+    TraceColumn.Name.START_TIMESTAMP: "start_timestamp",
+}
+
+_NAME_TO_COLUMN: dict[str, TraceColumn.Name] = {
+    v: k for k, v in _COLUMN_TO_NAME.items()
+}
+
+_TYPES_TO_CLICKHOUSE: dict[AttributeKey.Type, str] = {
+    AttributeKey.Type.TYPE_STRING: "String",
+    AttributeKey.Type.TYPE_INT: "Int64",
+    AttributeKey.Type.TYPE_FLOAT: "Float64",
+}
+
+_POSSIBLE_TYPES: dict[TraceColumn.Name, set[AttributeKey.Type]] = {
+    TraceColumn.Name.TRACE_ID: {
+        AttributeKey.Type.TYPE_STRING,
+    },
+    TraceColumn.Name.START_TIMESTAMP: {
+        AttributeKey.Type.TYPE_STRING,
+        AttributeKey.Type.TYPE_INT,
+        AttributeKey.Type.TYPE_FLOAT,
+    },
+}
+
+
+def _column_to_expression(trace_column: TraceColumn) -> Expression:
+    if (
+        trace_column.name in _COLUMN_TO_NAME
+        and trace_column.type in _POSSIBLE_TYPES.get(trace_column.name, {})
+    ):
+        return f.CAST(
+            column(
+                _COLUMN_TO_NAME[trace_column.name],
+            ),
+            _TYPES_TO_CLICKHOUSE[trace_column.type],
+            alias=_COLUMN_TO_NAME[trace_column.name],
+        )
+    raise BadSnubaRPCRequestException(
+        f"{trace_column.name} had an unknown or unset type: {trace_column.type}"
+    )
+
+
+def _convert_order_by(
+    order_by: Sequence[FindTracesRequest.OrderBy],
+) -> Sequence[OrderBy]:
+    res: list[OrderBy] = []
+    for x in order_by:
+        direction = OrderByDirection.DESC if x.descending else OrderByDirection.ASC
+        res.append(
+            OrderBy(
+                direction=direction,
+                expression=_column_to_expression(x.column),
+            )
+        )
+    return res
+
+
+def _build_query(request: FindTracesRequest) -> Query:
+    entity = Entity(
+        key=EntityKey("eap_spans"),
+        schema=get_entity(EntityKey("eap_spans")).get_data_model(),
+        sample=None,
+    )
+
+    selected_columns = []
+
+    for trace_column in request.columns:
+        expression = _column_to_expression(trace_column)
+        selected_columns.append(
+            SelectedExpression(
+                name=_COLUMN_TO_NAME[trace_column.name],
+                expression=expression,
+            )
+        )
+
+    res = Query(
+        from_clause=entity,
+        selected_columns=selected_columns,
+        condition=base_conditions_and(
+            request.meta,
+            trace_item_filters_to_expression(request.filter),
+        ),
+        order_by=_convert_order_by(request.order_by),
+        groupby=[
+            _column_to_expression(
+                TraceColumn(
+                    type=AttributeKey.TYPE_STRING,
+                    name=TraceColumn.Name.TRACE_ID,
+                ),
+            ),
+        ],
+        limit=request.limit if request.limit > 0 else _DEFAULT_ROW_LIMIT,
+    )
+    treeify_or_and_conditions(res)
+    return res
+
+
+def _build_snuba_request(request: FindTracesRequest) -> SnubaRequest:
+    query_settings = (
+        setup_trace_query_settings() if request.meta.debug else HTTPQuerySettings()
+    )
+
+    return SnubaRequest(
+        id=uuid.UUID(request.meta.request_id),
+        original_body=MessageToDict(request),
+        query=_build_query(request),
+        query_settings=query_settings,
+        attribution_info=AttributionInfo(
+            referrer=request.meta.referrer,
+            team="eap",
+            feature="eap",
+            tenant_ids={
+                "organization_id": request.meta.organization_id,
+                "referrer": request.meta.referrer,
+            },
+            app_id=AppID("eap"),
+            parent_api="eap_span_samples",
+        ),
+    )
+
+
+def _convert_results(
+    request: FindTracesRequest, data: Iterable[Dict[str, Any]]
+) -> list[FindTracesResponse.Trace]:
+    converters: Dict[str, Callable[[Any], AttributeValue]] = {}
+
+    for trace_column in request.columns:
+        if trace_column.type == AttributeKey.TYPE_BOOLEAN:
+            converters[trace_column.name] = lambda x: AttributeValue(val_bool=bool(x))
+        elif trace_column.type == AttributeKey.TYPE_STRING:
+            converters[trace_column.name] = lambda x: AttributeValue(val_str=str(x))
+        elif trace_column.type == AttributeKey.TYPE_INT:
+            converters[trace_column.name] = lambda x: AttributeValue(val_int=int(x))
+        elif trace_column.type == AttributeKey.TYPE_FLOAT:
+            converters[trace_column.name] = lambda x: AttributeValue(val_float=float(x))
+
+    res: list[FindTracesResponse.Trace] = []
+    column_ordering = {
+        trace_column.name: i for i, trace_column in enumerate(request.columns)
+    }
+
+    for row in data:
+        values: defaultdict[
+            TraceColumn.Name, FindTracesResponse.Trace.Column
+        ] = defaultdict(FindTracesResponse.Trace.Column)
+        for column_name, value in row.items():
+            name = _NAME_TO_COLUMN[column_name]
+            if name in converters.keys():
+                values[name] = FindTracesResponse.Trace.Column(
+                    value=converters[name](value),
+                )
+        res.append(
+            FindTracesResponse.Trace(
+                # we return the columns in the order they were requested
+                columns=sorted(
+                    values.values(),
+                    key=lambda c: column_ordering.__getitem__(c.name),
+                )
+            )
+        )
+
+    return res
+
+
+def _get_page_token(
+    request: FindTracesRequest, response: list[FindTracesResponse.Trace]
+) -> PageToken:
+    if not response:
+        return PageToken(offset=0)
+    num_rows = len(response)
+    return PageToken(offset=request.page_token.offset + num_rows)
+
+
+def _validate_order_by(in_msg: FindTracesRequest) -> None:
+    order_by_cols = set([ob.column.name for ob in in_msg.order_by])
+    selected_columns = set([c.name for c in in_msg.columns])
+    if not order_by_cols.issubset(selected_columns):
+        raise BadSnubaRPCRequestException(
+            f"Ordered by columns {order_by_cols} not selected: {selected_columns}"
+        )
+
+
+class EndpointFindTraces(RPCEndpoint[FindTracesRequest, FindTracesResponse]):
+    @classmethod
+    def version(cls) -> str:
+        return "v1"
+
+    @classmethod
+    def request_class(cls) -> Type[FindTracesRequest]:
+        return FindTracesRequest
+
+    @classmethod
+    def response_class(cls) -> Type[FindTracesResponse]:
+        return FindTracesResponse
+
+    def _execute(self, in_msg: FindTracesRequest) -> FindTracesResponse:
+        _validate_order_by(in_msg)
+
+        in_msg.meta.request_id = getattr(in_msg.meta, "request_id", None) or str(
+            uuid.uuid4()
+        )
+        snuba_request = _build_snuba_request(in_msg)
+        res = run_query(
+            dataset=PluggableDataset(name="eap", all_entities=[]),
+            request=snuba_request,
+            timer=self._timer,
+        )
+        traces = _convert_results(in_msg, res.result.get("data", []))
+        response_meta = extract_response_meta(
+            in_msg.meta.request_id,
+            in_msg.meta.debug,
+            [res],
+            [self._timer],
+        )
+        return FindTracesResponse(
+            traces=traces,
+            page_token=_get_page_token(in_msg, traces),
+            meta=response_meta,
+        )
---
+++ tests/web/rpc/v1/test_endpoint_find_traces.py
@@ -0,0 +1,311 @@
+import random
+import uuid
+from datetime import datetime, timedelta, timezone
+from typing import Any, Mapping
+
+import pytest
+from google.protobuf.timestamp_pb2 import Timestamp
+from sentry_protos.snuba.v1.endpoint_find_traces_pb2 import (
+    FindTracesRequest,
+    FindTracesResponse,
+    TraceColumn,
+)
+from sentry_protos.snuba.v1.error_pb2 import Error as ErrorProto
+from sentry_protos.snuba.v1.request_common_pb2 import (
+    PageToken,
+    RequestMeta,
+    ResponseMeta,
+)
+from sentry_protos.snuba.v1.trace_item_attribute_pb2 import AttributeKey, AttributeValue
+from sentry_protos.snuba.v1.trace_item_filter_pb2 import (
+    ComparisonFilter,
+    TraceItemFilter,
+)
+
+from snuba.datasets.storages.factory import get_storage
+from snuba.datasets.storages.storage_key import StorageKey
+from snuba.web.rpc.v1.endpoint_find_traces import EndpointFindTraces
+from tests.base import BaseApiTest
+from tests.helpers import write_raw_unprocessed_events
+
+_RELEASE_TAG = "backend@24.7.0.dev0+c45b49caed1e5fcbf70097ab3f434b487c359b6b"
+_SERVER_NAME = "D23CXQ4GK2.local"
+_TRACE_IDS = [uuid.uuid4().hex for _ in range(10)]
+_BASE_TIME = datetime.now(tz=timezone.utc).replace(
+    minute=0,
+    second=0,
+    microsecond=0,
+) - timedelta(minutes=180)
+
+
+def gen_message(
+    dt: datetime,
+    trace_id: str,
+    measurements: dict[str, dict[str, float]] | None = None,
+    tags: dict[str, str] | None = None,
+) -> Mapping[str, Any]:
+    measurements = measurements or {}
+    tags = tags or {}
+    return {
+        "description": "/api/0/relays/projectconfigs/",
+        "duration_ms": 152,
+        "event_id": "d826225de75d42d6b2f01b957d51f18f",
+        "exclusive_time_ms": 0.228,
+        "is_segment": True,
+        "data": {
+            "sentry.environment": "development",
+            "sentry.release": _RELEASE_TAG,
+            "thread.name": "uWSGIWorker1Core0",
+            "thread.id": "8522009600",
+            "sentry.segment.name": "/api/0/relays/projectconfigs/",
+            "sentry.sdk.name": "sentry.python.django",
+            "sentry.sdk.version": "2.7.0",
+            "my.float.field": 101.2,
+            "my.int.field": 2000,
+            "my.neg.field": -100,
+            "my.neg.float.field": -101.2,
+            "my.true.bool.field": True,
+            "my.false.bool.field": False,
+        },
+        "measurements": {
+            "num_of_spans": {"value": 50.0},
+            "eap.measurement": {"value": random.choice([1, 100, 1000])},
+            **measurements,
+        },
+        "organization_id": 1,
+        "origin": "auto.http.django",
+        "project_id": 1,
+        "received": 1721319572.877828,
+        "retention_days": 90,
+        "segment_id": "8873a98879faf06d",
+        "sentry_tags": {
+            "category": "http",
+            "environment": "development",
+            "op": "http.server",
+            "platform": "python",
+            "release": _RELEASE_TAG,
+            "sdk.name": "sentry.python.django",
+            "sdk.version": "2.7.0",
+            "status": "ok",
+            "status_code": "200",
+            "thread.id": "8522009600",
+            "thread.name": "uWSGIWorker1Core0",
+            "trace.status": "ok",
+            "transaction": "/api/0/relays/projectconfigs/",
+            "transaction.method": "POST",
+            "transaction.op": "http.server",
+            "user": "ip:127.0.0.1",
+        },
+        "span_id": "123456781234567D",
+        "tags": {
+            "http.status_code": "200",
+            "relay_endpoint_version": "3",
+            "relay_id": "88888888-4444-4444-8444-cccccccccccc",
+            "relay_no_cache": "False",
+            "relay_protocol_version": "3",
+            "relay_use_post_or_schedule": "True",
+            "relay_use_post_or_schedule_rejected": "version",
+            "server_name": _SERVER_NAME,
+            "spans_over_limit": "False",
+            "color": random.choice(["red", "green", "blue"]),
+            "location": random.choice(["mobile", "frontend", "backend"]),
+            **tags,
+        },
+        "trace_id": trace_id,
+        "start_timestamp_ms": int(dt.timestamp()) * 1000 - int(random.gauss(1000, 200)),
+        "start_timestamp_precise": dt.timestamp(),
+        "end_timestamp_precise": dt.timestamp() + 1,
+    }
+
+
+@pytest.fixture(autouse=False)
+def setup_teardown(clickhouse_db: None, redis_db: None) -> None:
+    spans_storage = get_storage(StorageKey("eap_spans"))
+    start = _BASE_TIME
+    messages = [
+        gen_message(
+            dt=start - timedelta(minutes=i),
+            trace_id=_TRACE_IDS[i % len(_TRACE_IDS)],
+        )
+        for i in range(120)
+    ]
+    write_raw_unprocessed_events(spans_storage, messages)  # type: ignore
+
+
+@pytest.mark.clickhouse_db
+@pytest.mark.redis_db
+class TestFindTraces(BaseApiTest):
+    def test_no_data(self) -> None:
+        ts = Timestamp()
+        ts.GetCurrentTime()
+        message = FindTracesRequest(
+            meta=RequestMeta(
+                project_ids=[1, 2, 3],
+                organization_id=1,
+                cogs_category="something",
+                referrer="something",
+                start_timestamp=ts,
+                end_timestamp=ts,
+            ),
+            columns=[
+                TraceColumn(
+                    name=TraceColumn.Name.TRACE_ID,
+                    type=AttributeKey.TYPE_STRING,
+                )
+            ],
+            limit=10,
+        )
+        response = self.app.post(
+            "/rpc/EndpointFindTraces/v1", data=message.SerializeToString()
+        )
+        error_proto = ErrorProto()
+        if response.status_code != 200:
+            error_proto.ParseFromString(response.data)
+        assert response.status_code == 200, error_proto
+
+    def test_with_data_and_order_by(self, setup_teardown: Any) -> None:
+        ts = Timestamp(seconds=int(_BASE_TIME.timestamp()))
+        hour_ago = int((_BASE_TIME - timedelta(hours=1)).timestamp())
+        message = FindTracesRequest(
+            meta=RequestMeta(
+                project_ids=[1, 2, 3],
+                organization_id=1,
+                cogs_category="something",
+                referrer="something",
+                start_timestamp=Timestamp(seconds=hour_ago),
+                end_timestamp=ts,
+                request_id="be3123b3-2e5d-4eb9-bb48-f38eaa9e8480",
+            ),
+            columns=[
+                TraceColumn(
+                    name=TraceColumn.Name.TRACE_ID,
+                    type=AttributeKey.TYPE_STRING,
+                )
+            ],
+            order_by=[
+                FindTracesRequest.OrderBy(
+                    column=TraceColumn(
+                        name=TraceColumn.Name.TRACE_ID,
+                        type=AttributeKey.TYPE_STRING,
+                    ),
+                ),
+            ],
+        )
+        response = EndpointFindTraces().execute(message)
+        expected_response = FindTracesResponse(
+            traces=[
+                FindTracesResponse.Trace(
+                    columns=[
+                        FindTracesResponse.Trace.Column(
+                            name=TraceColumn.Name.TRACE_ID,
+                            value=AttributeValue(
+                                val_str=trace_id,
+                            ),
+                        ),
+                    ],
+                )
+                for trace_id in sorted(_TRACE_IDS)
+            ],
+            page_token=PageToken(offset=len(_TRACE_IDS)),
+            meta=ResponseMeta(request_id="be3123b3-2e5d-4eb9-bb48-f38eaa9e8480"),
+        )
+        assert response == expected_response
+
+    def test_with_data_order_by_and_limit(self, setup_teardown: Any) -> None:
+        ts = Timestamp(seconds=int(_BASE_TIME.timestamp()))
+        hour_ago = int((_BASE_TIME - timedelta(hours=1)).timestamp())
+        message = FindTracesRequest(
+            meta=RequestMeta(
+                project_ids=[1, 2, 3],
+                organization_id=1,
+                cogs_category="something",
+                referrer="something",
+                start_timestamp=Timestamp(seconds=hour_ago),
+                end_timestamp=ts,
+                request_id="be3123b3-2e5d-4eb9-bb48-f38eaa9e8480",
+            ),
+            columns=[
+                TraceColumn(
+                    name=TraceColumn.Name.TRACE_ID,
+                    type=AttributeKey.TYPE_STRING,
+                ),
+            ],
+            order_by=[
+                FindTracesRequest.OrderBy(
+                    column=TraceColumn(
+                        name=TraceColumn.Name.TRACE_ID,
+                        type=AttributeKey.TYPE_STRING,
+                    ),
+                ),
+            ],
+            limit=1,
+        )
+        response = EndpointFindTraces().execute(message)
+        expected_response = FindTracesResponse(
+            traces=[
+                FindTracesResponse.Trace(
+                    columns=[
+                        FindTracesResponse.Trace.Column(
+                            name=TraceColumn.Name.TRACE_ID,
+                            value=AttributeValue(
+                                val_str=sorted(_TRACE_IDS)[0],
+                            ),
+                        )
+                    ],
+                )
+            ],
+            page_token=PageToken(offset=1),
+            meta=ResponseMeta(request_id="be3123b3-2e5d-4eb9-bb48-f38eaa9e8480"),
+        )
+        assert response == expected_response
+
+    def test_with_data_and_filter(self, setup_teardown: Any) -> None:
+        ts = Timestamp(seconds=int(_BASE_TIME.timestamp()))
+        hour_ago = int((_BASE_TIME - timedelta(hours=1)).timestamp())
+        message = FindTracesRequest(
+            meta=RequestMeta(
+                project_ids=[1, 2, 3],
+                organization_id=1,
+                cogs_category="something",
+                referrer="something",
+                start_timestamp=Timestamp(seconds=hour_ago),
+                end_timestamp=ts,
+                request_id="be3123b3-2e5d-4eb9-bb48-f38eaa9e8480",
+            ),
+            filter=TraceItemFilter(
+                comparison_filter=ComparisonFilter(
+                    key=AttributeKey(
+                        type=AttributeKey.TYPE_STRING,
+                        name="sentry.trace_id",
+                    ),
+                    op=ComparisonFilter.OP_EQUALS,
+                    value=AttributeValue(
+                        val_str=_TRACE_IDS[0],
+                    ),
+                ),
+            ),
+            columns=[
+                TraceColumn(
+                    name=TraceColumn.Name.TRACE_ID,
+                    type=AttributeKey.TYPE_STRING,
+                ),
+            ],
+        )
+        response = EndpointFindTraces().execute(message)
+        expected_response = FindTracesResponse(
+            traces=[
+                FindTracesResponse.Trace(
+                    columns=[
+                        FindTracesResponse.Trace.Column(
+                            name=TraceColumn.Name.TRACE_ID,
+                            value=AttributeValue(
+                                val_str=_TRACE_IDS[0],
+                            ),
+                        )
+                    ],
+                )
+            ],
+            page_token=PageToken(offset=1),
+            meta=ResponseMeta(request_id="be3123b3-2e5d-4eb9-bb48-f38eaa9e8480"),
+        )
+        assert response == expected_response
