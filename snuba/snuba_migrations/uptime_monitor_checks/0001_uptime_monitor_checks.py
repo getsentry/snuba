753312df831a40b4b61a81a44861834a582fca6a
Joshua Ferge <josh.ferge@sentry.io>
2024-12-18 15:19:04 -0700

    pr feedback

--- snuba/snuba_migrations/uptime_monitor_checks/0001_uptime_monitor_checks.py
+++ snuba/snuba_migrations/uptime_monitor_checks/0001_uptime_monitor_checks.py
@@ -4,7 +4,7 @@ from snuba.clickhouse.columns import UUID, Column, DateTime, String, UInt
 from snuba.clusters.storage_sets import StorageSetKey
 from snuba.migrations import migration, operations, table_engines
 from snuba.migrations.columns import MigrationModifiers as Modifiers
-from snuba.migrations.operations import AddIndicesData, OperationTarget, SqlOperation
+from snuba.migrations.operations import OperationTarget, SqlOperation
 
 storage_set = StorageSetKey.UPTIME_MONITOR_CHECKS
 table_prefix = "uptime_monitor_checks"
@@ -20,8 +20,7 @@ columns: List[Column[Modifiers]] = [
     Column("uptime_check_id", UUID()),
     Column("scheduled_check_time", DateTime()),
     Column("timestamp", DateTime()),
-    Column("_sort_timestamp", DateTime()),
-    Column("duration", UInt(64)),
+    Column("duration_ms", UInt(64)),
     Column("region_id", UInt(16, Modifiers(nullable=True))),
     Column("check_status", String(Modifiers(low_cardinality=True))),
     Column(
@@ -31,15 +30,6 @@ columns: List[Column[Modifiers]] = [
     Column("http_status_code", UInt(16)),
     Column("trace_id", UUID()),
     Column("retention_days", UInt(16)),
-]
-
-indices: Sequence[AddIndicesData] = [
-    AddIndicesData(
-        name="bf_trace_id",
-        expression="trace_id",
-        type="bloom_filter",
-        granularity=1,
-    )
 ]
 
 
@@ -53,12 +43,12 @@ class Migration(migration.ClickhouseNodeMigration):
                 table_name=local_table_name,
                 columns=columns,
                 engine=table_engines.ReplacingMergeTree(
-                    primary_key="(organization_id, project_id, _sort_timestamp, uptime_check_id)",
-                    order_by="(organization_id, project_id, uptime_subscription_id, _sort_timestamp, uptime_check_id)",
-                    partition_by="(retention_days, toMonday(_sort_timestamp))",
+                    primary_key="(organization_id, project_id, timestamp, uptime_check_id, trace_id)",
+                    order_by="(organization_id, project_id, timestamp, uptime_check_id, trace_id)",
+                    partition_by="(retention_days, toMonday(timestamp))",
                     settings={"index_granularity": "8192"},
                     storage_set=storage_set,
-                    ttl="_sort_timestamp + toIntervalDay(retention_days)",
+                    ttl="timestamp + toIntervalDay(retention_days)",
                 ),
                 target=OperationTarget.LOCAL,
             ),
@@ -71,12 +61,6 @@ class Migration(migration.ClickhouseNodeMigration):
                     sharding_key="cityHash64(reinterpretAsUInt128(trace_id))",
                 ),
                 target=OperationTarget.DISTRIBUTED,
-            ),
-            operations.AddIndices(
-                storage_set=storage_set,
-                table_name=local_table_name,
-                indices=indices,
-                target=OperationTarget.LOCAL,
             ),
         ]
 
