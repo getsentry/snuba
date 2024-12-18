c443ab3f3f84aa137bfa213fb50aa1ce686368e7
Joshua Ferge <josh.ferge@sentry.io>
2024-12-17 17:41:55 -0700

    fix up

--- snuba/snuba_migrations/uptime_monitor_checks/0001_uptime_monitor_checks.py
+++ snuba/snuba_migrations/uptime_monitor_checks/0001_uptime_monitor_checks.py
@@ -4,26 +4,43 @@ from snuba.clickhouse.columns import UUID, Column, DateTime, String, UInt
 from snuba.clusters.storage_sets import StorageSetKey
 from snuba.migrations import migration, operations, table_engines
 from snuba.migrations.columns import MigrationModifiers as Modifiers
-from snuba.migrations.operations import OperationTarget, SqlOperation
+from snuba.migrations.operations import AddIndicesData, OperationTarget, SqlOperation
 
 storage_set = StorageSetKey.UPTIME_MONITOR_CHECKS
 table_prefix = "uptime_monitor_checks"
 local_table_name = f"{table_prefix}_local"
 dist_table_name = f"{table_prefix}_dist"
 
-
-# do i need any of the domain or mutable info in postgres?
+## what about all the fany codecs?
+## do i need retention days?
 columns: List[Column[Modifiers]] = [
+    Column("organization_id", UInt(64)),
     Column("project_id", UInt(64)),
-    Column("environment_id", String(Modifiers(nullable=True))),
+    Column("environment", String(Modifiers(nullable=True, low_cardinality=True))),
     Column("uptime_subscription_id", UInt(64)),
-    Column("timestamp", DateTime()),
     Column("uptime_check_id", UUID()),
-    Column("duration", UInt(32)),
+    Column("scheduled_check_time", DateTime()),
+    Column("timestamp", DateTime()),
+    Column("_sort_timestamp", DateTime()),
+    Column("duration", UInt(64)),
     Column("location_id", UInt(32, Modifiers(nullable=True))),
-    Column("status", UInt(16)),
-    Column("timeout", UInt(16)),
+    Column("check_status", String(Modifiers(low_cardinality=True))),
+    Column(
+        "check_status_reason",
+        String(Modifiers(nullable=True, low_cardinality=True)),
+    ),
+    Column("http_status_code", UInt(16)),
     Column("trace_id", UUID()),
+    Column("retention_days", UInt(16)),
+]
+
+indices: Sequence[AddIndicesData] = [
+    AddIndicesData(
+        name="bf_trace_id",
+        expression="trace_id",
+        type="bloom_filter",
+        granularity=1,
+    )
 ]
 
 
@@ -38,11 +55,13 @@ class Migration(migration.ClickhouseNodeMigration):
                 columns=columns,
                 engine=table_engines.ReplacingMergeTree(
                     # is this the right order by? what about tostartOfDay?
-                    # should environment be in the order by?
-                    order_by="(project_id, uptime_subscription_id, timestamp, uptime_check_id)",
-                    partition_by="(toMonday(timestamp))",
+                    # do i need a primary key?
+                    primary_key="(organization_id, project_id, uptime_subscription_id, _sort_timestamp, trace_id)",
+                    order_by="(organization_id, project_id, uptime_subscription_id, _sort_timestamp, trace_id)",
+                    partition_by="(toMonday(_sort_timestamp))",
                     settings={"index_granularity": "8192"},
                     storage_set=storage_set,
+                    ttl="_sort_timestamp + toIntervalDay(retention_days)",
                 ),
                 target=OperationTarget.LOCAL,
             ),
@@ -52,7 +71,7 @@ class Migration(migration.ClickhouseNodeMigration):
                 columns=columns,
                 engine=table_engines.Distributed(
                     local_table_name=local_table_name,
-                    sharding_key="cityHash64(uptime_check_id)",
+                    sharding_key="cityHash64(reinterpretAsUInt128(trace_id))",
                 ),
                 target=OperationTarget.DISTRIBUTED,
             ),
