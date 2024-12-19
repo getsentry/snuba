0c4ee83f1d862fdfe4b4552cd01d0821cf2939c9
Joshua Ferge <josh.ferge@sentry.io>
2024-12-19 13:05:22 -0700

    fix rust test

--- rust_snuba/src/processors/uptime_monitor_checks.rs
+++ rust_snuba/src/processors/uptime_monitor_checks.rs
@@ -27,44 +27,52 @@ pub fn deserialize_message(
     let monitor_message: UptimeMonitorCheckMessage = serde_json::from_slice(payload)?;
 
     let rows = vec![UptimeMonitorCheckRow {
-        organization_id: monitor_message.organization_id,
-        project_id: monitor_message.project_id,
+        organization_id: 1,
+        project_id: 1,
         environment: monitor_message.environment,
-        uptime_subscription_id: monitor_message.uptime_subscription_id,
-        uptime_check_id: monitor_message.uptime_check_id,
-        scheduled_check_time: monitor_message.scheduled_check_time as u32,
-        timestamp: monitor_message.timestamp as u32,
-        _sort_timestamp: monitor_message.timestamp as u32,
-        duration: monitor_message.duration,
-        region_id: monitor_message.region_id,
-        check_status: monitor_message.check_status,
-        check_status_reason: monitor_message.check_status_reason,
-        http_status_code: monitor_message.http_status_code,
+        uptime_subscription_id: monitor_message.subscription_id,
+        uptime_check_id: monitor_message.guid,
+        scheduled_check_time: monitor_message.scheduled_check_time_ms,
+        timestamp: monitor_message.actual_check_time_ms,
+        duration: monitor_message.duration_ms,
+        region_slug: Some("global".to_string()),
+        check_status: monitor_message.status,
+        check_status_reason: monitor_message.status_reason,
+        http_status_code: monitor_message
+            .request_info
+            .unwrap()
+            .http_status_code
+            .unwrap_or(0),
         trace_id: monitor_message.trace_id,
-        retention_days: monitor_message.retention_days,
+        retention_days: 30,
         partition,
         offset,
     }];
 
-    Ok((rows, monitor_message.timestamp))
+    Ok((rows, monitor_message.actual_check_time_ms))
 }
 
 #[derive(Debug, Deserialize)]
 struct UptimeMonitorCheckMessage {
-    organization_id: u64,
-    project_id: u64,
+    // TODO: add these to the message
+    // organization_id: u64,
+    // project_id: u64,
+    // retention_days: u16,
+    // region_slug: Option<String>,
     environment: Option<String>,
-    uptime_subscription_id: u64,
-    uptime_check_id: Uuid,
-    scheduled_check_time: f64,
-    timestamp: f64,
-    duration: u64,
-    region_id: Option<u16>,
-    check_status: String,
-    check_status_reason: Option<String>,
-    http_status_code: u16,
+    subscription_id: Uuid,
+    guid: Uuid,
+    scheduled_check_time_ms: f64,
+    actual_check_time_ms: f64,
+    duration_ms: u64,
+    status: String,
+    status_reason: Option<String>,
     trace_id: Uuid,
-    retention_days: u16,
+    request_info: Option<RequestInfo>,
+}
+#[derive(Debug, Deserialize)]
+pub struct RequestInfo {
+    pub http_status_code: Option<u16>,
 }
 
 #[derive(Debug, Default, Serialize)]
@@ -72,13 +80,12 @@ pub struct UptimeMonitorCheckRow {
     organization_id: u64,
     project_id: u64,
     environment: Option<String>,
-    uptime_subscription_id: u64,
+    uptime_subscription_id: Uuid,
     uptime_check_id: Uuid,
-    scheduled_check_time: u32,
-    timestamp: u32,
-    _sort_timestamp: u32,
+    scheduled_check_time: f64,
+    timestamp: f64,
     duration: u64,
-    region_id: Option<u16>,
+    region_slug: Option<String>,
     check_status: String,
     check_status_reason: Option<String>,
     http_status_code: u16,
@@ -95,33 +102,35 @@ mod tests {
     #[test]
     fn test_parse_monitor_checkin() {
         let data = r#"{
-            "organization_id": 789,
-            "project_id": 456,
             "environment": "prod",
-            "uptime_subscription_id": 123,
-            "uptime_check_id": "550e8400-e29b-41d4-a716-446655440000",
-            "scheduled_check_time": 1702659277,
-            "timestamp": 1702659277,
-            "duration": 100,
-            "region_id": 42,
-            "check_status": "ok",
-            "check_status_reason": "Request successful",
+            "subscription_id": "123e4567-e89b-12d3-a456-426614174000",
+            "guid": "550e8400-e29b-41d4-a716-446655440000",
+            "scheduled_check_time_ms": 1702659277,
+            "actual_check_time_ms": 1702659277,
+            "duration_ms": 100,
+            "status": "ok",
+            "status_reason": "Request successful",
             "http_status_code": 200,
             "trace_id": "550e8400-e29b-41d4-a716-446655440000",
-            "retention_days": 30
+            "request_info": {
+                "request_type": "GET",
+                "http_status_code": 200
+            }
         }"#;
 
         let (rows, timestamp) = deserialize_message(data.as_bytes(), 0, 0).unwrap();
         let monitor_row = rows.first().unwrap();
 
-        assert_eq!(monitor_row.organization_id, 789);
-        assert_eq!(monitor_row.project_id, 456);
+        assert_eq!(monitor_row.organization_id, 1);
+        assert_eq!(monitor_row.project_id, 1);
         assert_eq!(monitor_row.environment, Some("prod".to_string()));
-        assert_eq!(monitor_row.uptime_subscription_id, 123);
+        assert_eq!(
+            monitor_row.uptime_subscription_id,
+            Uuid::parse_str("123e4567-e89b-12d3-a456-426614174000").unwrap()
+        );
         assert_eq!(monitor_row.duration, 100);
-        assert_eq!(monitor_row.timestamp, 1702659277);
-        assert_eq!(monitor_row._sort_timestamp, 1702659277);
-        assert_eq!(monitor_row.region_id, Some(42));
+        assert_eq!(monitor_row.timestamp, 1702659277.0);
+        assert_eq!(monitor_row.region_slug, Some("global".to_string()));
         assert_eq!(&monitor_row.check_status, "ok");
         assert_eq!(
             monitor_row.check_status_reason,
