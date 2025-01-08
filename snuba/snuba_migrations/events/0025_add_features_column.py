35df5eed552172463b01932475d185cb38a315ae
Colton Allen <colton.allen@sentry.io>
2025-01-08 10:15:45 -0600

    Fix syntax error

--- snuba/snuba_migrations/events/0025_add_features_column.py
+++ snuba/snuba_migrations/events/0025_add_features_column.py
@@ -1,4 +1,4 @@
-from typing import Iterator
+from typing import Iterator, Sequence
 
 from snuba.clickhouse.columns import Array, Column, Nested, String, UInt
 from snuba.clusters.storage_sets import StorageSetKey
@@ -18,67 +18,75 @@ targets = [OperationTarget.LOCAL, OperationTarget.DISTRIBUTED]
 class Migration(migration.ClickhouseNodeMigration):
     blocking = False
 
-    def forwards_ops(self) -> Iterator[operations.SqlOperation]:
-        # Add local and dist columns for the three tables.
-        for target in targets:
-            for table_name, storage_set in table_names:
-                yield from [
-                    operations.AddColumn(
-                        storage_set=storage_set,
-                        table_name=table_name,
-                        column=Column(
-                            "features", Nested([("key", String()), ("value", String())])
+    def forwards_ops(self) -> Sequence[operations.SqlOperation]:
+        return list(forward_ops())
+
+    def backwards_ops(self) -> Sequence[operations.SqlOperation]:
+        return list(backward_ops())
+
+
+def forward_ops() -> Iterator[operations.SqlOperation]:
+    # Add local and dist columns for the three tables.
+    for target in targets:
+        for table_name, storage_set in table_names:
+            yield from [
+                operations.AddColumn(
+                    storage_set=storage_set,
+                    table_name=table_name,
+                    column=Column(
+                        "features", Nested([("key", String()), ("value", String())])
+                    ),
+                    after="_tags_hash_map",
+                    target=target,
+                ),
+                operations.AddColumn(
+                    storage_set=storage_set,
+                    table_name=table_name,
+                    column=Column(
+                        "_features_hash_map",
+                        Array(
+                            UInt(64),
+                            Modifiers(materialized=FEATURES_HASH_MAP_COLUMN),
                         ),
-                        after="_tags_hash_map",
-                        target=target,
                     ),
-                    operations.AddColumn(
-                        storage_set=storage_set,
-                        table_name=table_name,
-                        column=Column(
-                            "_features_hash_map",
-                            Array(
-                                UInt(64),
-                                Modifiers(materialized=FEATURES_HASH_MAP_COLUMN),
-                            ),
-                        ),
-                        after="features.value",
-                        target=target,
-                    ),
-                ]
+                    after="features.value",
+                    target=target,
+                ),
+            ]
 
-        # Add index to the local table.
-        yield operations.AddIndex(
-            storage_set=StorageSetKey.EVENTS,
-            table_name="errors_local",
-            index_name="bf_features_hash_map",
-            index_expression="_features_hash_map",
-            index_type="bloom_filter",
-            granularity=1,
-            target=OperationTarget.LOCAL,
-        )
+    # Add index to the local table.
+    yield operations.AddIndex(
+        storage_set=StorageSetKey.EVENTS,
+        table_name="errors_local",
+        index_name="bf_features_hash_map",
+        index_expression="_features_hash_map",
+        index_type="bloom_filter",
+        granularity=1,
+        target=OperationTarget.LOCAL,
+    )
 
-    def backwards_ops(self) -> Iterator[operations.SqlOperation]:
-        yield operations.DropIndex(
-            StorageSetKey.EVENTS,
-            "errors_local",
-            "bf_features_hash_map",
-            target=OperationTarget.LOCAL,
-        )
 
-        for target in targets:
-            for table_name, storage_set in table_names:
-                yield from [
-                    operations.DropColumn(
-                        storage_set=storage_set,
-                        table_name=table_name,
-                        column_name="_features_hash_map",
-                        target=target,
-                    ),
-                    operations.DropColumn(
-                        storage_set=storage_set,
-                        table_name=table_name,
-                        column_name="features",
-                        target=target,
-                    ),
-                ]
+def backward_ops() -> Iterator[operations.SqlOperation]:
+    yield operations.DropIndex(
+        StorageSetKey.EVENTS,
+        "errors_local",
+        "bf_features_hash_map",
+        target=OperationTarget.LOCAL,
+    )
+
+    for target in targets:
+        for table_name, storage_set in table_names:
+            yield from [
+                operations.DropColumn(
+                    storage_set=storage_set,
+                    table_name=table_name,
+                    column_name="_features_hash_map",
+                    target=target,
+                ),
+                operations.DropColumn(
+                    storage_set=storage_set,
+                    table_name=table_name,
+                    column_name="features",
+                    target=target,
+                ),
+            ]
