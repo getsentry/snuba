7717f9b31df4ca59ba5289f7228c60144abf1ab1
Simon Hellmayr <simon.hellmayr@sentry.io>
2025-01-14 16:14:12 +0100

    fix comment

--- snuba/web/rpc/v1/resolvers/R_eap_spans/common/aggregation.py
+++ snuba/web/rpc/v1/resolvers/R_eap_spans/common/aggregation.py
@@ -506,7 +506,7 @@ def get_confidence_interval_column(
             ),
             **alias_dict,
         ),
-        # confidence interval = Z * \sqrt{\frac{N * (\sum_{i=1}^n w_ix_i^2 - \frac{(\sum_{i=1}^n w_ix_i)^2}{N})}{n * (N-1) c* (N-1)}}
+        # confidence interval = Z * \sqrt{\frac{\sum_{i=1}^n w_ix_i^2 - \frac{(\sum_{i=1}^n w_ix_i)^2}{N}}{n * N}}
         #          ┌────────────────────────────┐
         #          │              ₙ
         #          │              ⎲
