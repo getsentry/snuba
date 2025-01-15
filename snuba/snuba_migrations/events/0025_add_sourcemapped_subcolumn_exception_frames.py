3bf22d0a67dec1b1c1d8878050e92e5a46cc091a
Jodi Jang <jodi.jang@sentry.io>
2025-01-15 14:07:05 -0500

    fix: Add column

--- snuba/snuba_migrations/events/0025_add_sourcemapped_subcolumn_exception_frames.py
+++ snuba/snuba_migrations/events/0025_add_sourcemapped_subcolumn_exception_frames.py
@@ -1,11 +1,10 @@
 from typing import Sequence
 
-from snuba.clickhouse.columns import Column, String, UInt
+from snuba.clickhouse.columns import Column, UInt
 from snuba.clusters.storage_sets import StorageSetKey
 from snuba.migrations import migration, operations
 from snuba.migrations.columns import MigrationModifiers as Modifiers
 from snuba.migrations.operations import OperationTarget
-from snuba.utils.schemas import Nested
 
 
 class Migration(migration.ClickhouseNodeMigration):
@@ -16,47 +15,15 @@ class Migration(migration.ClickhouseNodeMigration):
     local_table_name = "errors_local"
     dist_table_name = "errors_dist"
 
-    column_before = Column(
-        "exception_frames",
-        Nested(
-            [
-                ("abs_path", String(Modifiers(nullable=True))),
-                ("colno", UInt(32, Modifiers(nullable=True))),
-                ("filename", String(Modifiers(nullable=True))),
-                ("function", String(Modifiers(nullable=True))),
-                ("lineno", UInt(32, Modifiers(nullable=True))),
-                ("in_app", UInt(8, Modifiers(nullable=True))),
-                ("package", String(Modifiers(nullable=True))),
-                ("module", String(Modifiers(nullable=True))),
-                ("stack_level", UInt(16, Modifiers(nullable=True))),
-            ]
-        ),
-    )
-
-    column_after = Column(
-        "exception_frames",
-        Nested(
-            [
-                ("abs_path", String(Modifiers(nullable=True))),
-                ("colno", UInt(32, Modifiers(nullable=True))),
-                ("filename", String(Modifiers(nullable=True))),
-                ("function", String(Modifiers(nullable=True))),
-                ("lineno", UInt(32, Modifiers(nullable=True))),
-                ("in_app", UInt(8, Modifiers(nullable=True))),
-                ("package", String(Modifiers(nullable=True))),
-                ("module", String(Modifiers(nullable=True))),
-                ("stack_level", UInt(16, Modifiers(nullable=True))),
-                ("sourcemapped", UInt(8, Modifiers(nullable=True))),
-            ]
-        ),
-    )
-
     def forwards_ops(self) -> Sequence[operations.SqlOperation]:
         return [
-            operations.ModifyColumn(
+            operations.AddColumn(
                 storage_set=self.storage_set_key,
                 table_name=table_name,
-                column=self.column_after,
+                column=Column(
+                    "exception_frames.sourcemapped", UInt(8, Modifiers(nullable=True))
+                ),
+                after="exception_frames.stack_level",
                 target=target,
             )
             for table_name, target in [
@@ -67,14 +34,14 @@ class Migration(migration.ClickhouseNodeMigration):
 
     def backwards_ops(self) -> Sequence[operations.SqlOperation]:
         return [
-            operations.ModifyColumn(
+            operations.DropColumn(
                 storage_set=self.storage_set_key,
                 table_name=table_name,
-                column=self.column_before,
+                column_name="exception_frames.sourcemapped",
                 target=target,
             )
             for table_name, target in [
-                (self.local_table_name, OperationTarget.LOCAL),
                 (self.dist_table_name, OperationTarget.DISTRIBUTED),
+                (self.local_table_name, OperationTarget.LOCAL),
             ]
         ]
