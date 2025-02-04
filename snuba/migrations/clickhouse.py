ef5469f1c8ec607e46778226bf5910116ce745f7
Pierre Massat <pierre.massat@sentry.io>
2025-02-04 13:59:12 -0500

    chore(ci): Test with ClickHouse 24.8 (#6851)

--- snuba/migrations/clickhouse.py
+++ snuba/migrations/clickhouse.py
@@ -1,4 +1,4 @@
 CLICKHOUSE_SERVER_MIN_VERSION = "23.8.11.29"
 # Note: SaaS, self-hosted, and sentry dev
 # environements should all be on 23.8.11.29
-CLICKHOUSE_SERVER_MAX_VERSION = "24.3.5.47"
+CLICKHOUSE_SERVER_MAX_VERSION = "24.8.11.51285"
