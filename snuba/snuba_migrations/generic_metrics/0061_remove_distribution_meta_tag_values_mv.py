fea27bc6204e1f2adce3421aa09c3a434f537395
Oliver Newland <oliver.newland@sentry.io>
2025-02-13 09:45:04 -0800

    fix dumb copy/paste errors

--- snuba/snuba_migrations/generic_metrics/0061_remove_distribution_meta_tag_values_mv.py
+++ snuba/snuba_migrations/generic_metrics/0061_remove_distribution_meta_tag_values_mv.py
@@ -12,7 +12,6 @@ class Migration(migration.ClickhouseNodeMigration):
     blocking = False
     granularity = "2048"
     tag_value_view_name = "generic_metric_distributions_meta_tag_values_mv"
-    tag_value_view_name = "generic_metric_distributions_meta_tag_values_mv"
     tag_value_local_table_name = "generic_metric_distributions_meta_tag_values_local"
     tag_value_dist_table_name = "generic_metric_distributions_meta_tag_values_dist"
     tag_value_table_columns: Sequence[Column[Modifiers]] = [
@@ -44,9 +43,25 @@ class Migration(migration.ClickhouseNodeMigration):
                 destination_table_name=self.tag_value_local_table_name,
                 target=OperationTarget.LOCAL,
                 query="""
-                CREATE MATERIALIZED VIEW snuba_test.generic_metric_distributions_meta_tag_values_mv
-                TO snuba_test.generic_metric_distributions_meta_tag_values_local
-                (`project_id` UInt64, `metric_id` UInt64, `tag_key` UInt64, `tag_value` String, `timestamp` DateTime CODEC(DoubleDelta), `retention_days` UInt16, `count` AggregateFunction(sum, Float64)) AS SELECT project_id, metric_id, tag_key, tag_value, toMonday(timestamp) AS timestamp, retention_days, sumState(count_value) AS count FROM snuba_test.generic_metric_distributions_raw_local ARRAY JOIN tags.key AS tag_key, tags.raw_value AS tag_value WHERE record_meta = 1 GROUP BY project_id, metric_id, tag_key, tag_value, timestamp, retention_days
+                SELECT
+                    project_id,
+                    metric_id,
+                    tag_key,
+                    tag_value,
+                    toMonday(timestamp) as timestamp,
+                    retention_days,
+                    sumState(count_value) as count
+                FROM generic_metric_distributions_raw_local
+                ARRAY JOIN
+                    tags.key AS tag_key, tags.raw_value AS tag_value
+                WHERE record_meta = 1
+                GROUP BY
+                    project_id,
+                    metric_id,
+                    tag_key,
+                    tag_value,
+                    timestamp,
+                    retention_days
                 """,
             )
         ]
