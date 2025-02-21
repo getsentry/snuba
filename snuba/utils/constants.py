defe9c22fd5156d188666b242f5be3a9ecd4af15
Kyle Mumma <kyle.mumma@sentry.io>
2025-02-21 10:59:55 -0800

    init

--- snuba/utils/constants.py
+++ snuba/utils/constants.py
@@ -12,6 +12,9 @@ GRANULARITIES_AVAILABLE = (10, 60, 60 * 60, 24 * 60 * 60)
 # This will affect migrations and querying.
 ATTRIBUTE_BUCKETS = 20
 
+# number of buckets in eap_items_1_local table
+ITEM_ATTRIBUTE_BUCKETS = 40
+
 # Maximum number of attempts to fetch profile events
 PROFILE_EVENTS_MAX_ATTEMPTS = (
     4  # Will result in ~23 seconds total wait time with exponential backoff
