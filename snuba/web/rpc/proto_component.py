4a33f7390014fe31e37c762b101be93d7fb0c9d9
Rachel Chen <rachelchen@PL6VFX9HP4.local>
2025-02-24 16:32:22 -0800

    redesignn

--- snuba/web/rpc/proto_component.py
+++ snuba/web/rpc/proto_component.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
+from typing import Any
 
 from sentry_protos.snuba.v1.attribute_conditional_aggregation_pb2 import (
     AttributeConditionalAggregation,
@@ -8,108 +9,116 @@ from sentry_protos.snuba.v1.attribute_conditional_aggregation_pb2 import (
 from sentry_protos.snuba.v1.endpoint_time_series_pb2 import (
     Expression as TimeSeriesExpression,
 )
-from sentry_protos.snuba.v1.endpoint_time_series_pb2 import TimeSeriesRequest
 from sentry_protos.snuba.v1.endpoint_trace_item_table_pb2 import (
     AggregationComparisonFilter,
-    AggregationFilter,
     Column,
-    TraceItemTableRequest,
 )
 
 
 class ProtoWrapper(ABC):
+    def __init__(self, underlying_proto: Any):
+        self.underlying_proto = underlying_proto
+
     @abstractmethod
-    def accept(self, visitor: ProtoVisitor) -> None:
+    def accept(self, visitor: BaseVisitor) -> None:
         pass
 
 
 class ColumnWrapper(ProtoWrapper):
-    def __init__(self, column: Column):
-        self.column = column
-
-    def accept(self, visitor: ProtoVisitor) -> None:
-        visitor.visit(self)
-
-        if self.column.HasField("formula"):
-            ColumnWrapper(self.column.formula.left).accept(visitor)
-            ColumnWrapper(self.column.formula.right).accept(visitor)
-
-
-class AggregationComparisonFilterWrapper(ProtoWrapper):
-    def __init__(self, aggregation_comparison_filter: AggregationComparisonFilter):
-        self.aggregation_comparison_filter = aggregation_comparison_filter
-
-    def accept(self, visitor):
-        visitor.visit(self)
+    def accept(self, visitor: BaseVisitor) -> None:
+        visitor.visit_column(self)
 
 
 class AggregationFilterWrapper(ProtoWrapper):
-    def __init__(self, aggregation_filter: AggregationFilter):
-        self.aggregation_filter = aggregation_filter
-
-    def accept(self, visitor: ProtoVisitor) -> None:
-        if self.aggregation_filter.HasField("comparison_filter"):
-            AggregationComparisonFilterWrapper(
-                self.aggregation_filter.comparison_filter
-            ).accept(visitor)
-
-        if self.aggregation_filter.HasField("and_filter"):
-            for agg_filter in self.aggregation_filter.and_filter.filters:
-                AggregationFilterWrapper(agg_filter).accept(visitor)
-
-        if self.aggregation_filter.HasField("or_filter"):
-            for agg_filter in self.aggregation_filter.or_filter.filters:
-                AggregationFilterWrapper(agg_filter).accept(visitor)
+    def accept(self, visitor: BaseVisitor) -> None:
+        visitor.visit_aggregation_filter(self)
 
 
 class TimeSeriesExpressionWrapper(ProtoWrapper):
-    def __init__(self, time_series_expression: TimeSeriesExpression):
-        self.time_series_expression = time_series_expression
-
-    def accept(self, visitor: ProtoVisitor) -> None:
-        if self.time_series_expression.HasField("aggregation"):
-            visitor.visit(TimeSeriesExpressionWrapper(self.time_series_expression))
-
-        if self.time_series_expression.HasField("formula"):
-            TimeSeriesExpressionWrapper(
-                self.time_series_expression.formula.left
-            ).accept(visitor)
-            TimeSeriesExpressionWrapper(
-                self.time_series_expression.formula.right
-            ).accept(visitor)
+    def accept(self, visitor: BaseVisitor) -> None:
+        visitor.visit_time_series_expression(self)
 
 
 class TraceItemTableRequestWrapper(ProtoWrapper):
-    def __init__(self, trace_item_table_request: TraceItemTableRequest):
-        self.trace_item_table_request = trace_item_table_request
-
-    def accept(self, visitor: ProtoVisitor) -> None:
-        for col in self.trace_item_table_request.columns:
-            ColumnWrapper(col).accept(visitor)
-        for ob in self.trace_item_table_request.order_by:
-            ColumnWrapper(ob.column).accept(visitor)
-        if self.trace_item_table_request.HasField("aggregation_filter"):
-            AggregationFilterWrapper(
-                self.trace_item_table_request.aggregation_filter
-            ).accept(visitor)
+    def accept(self, visitor: BaseVisitor) -> None:
+        visitor.visit_trace_item_table_request(self)
 
 
 class TimeSeriesRequestWrapper(ProtoWrapper):
-    def __init__(self, time_series_request: TimeSeriesRequest):
-        self.time_series_request = time_series_request
-
-    def accept(self, visitor: ProtoVisitor) -> None:
-        for expression in self.time_series_request.expressions:
-            TimeSeriesExpressionWrapper(expression).accept(visitor)
+    def accept(self, visitor: BaseVisitor) -> None:
+        visitor.visit_time_series_request(self)
 
 
-class ProtoVisitor(ABC):
-    @abstractmethod
-    def visit(self, exp: ProtoWrapper) -> None:
-        pass
+class OrderByWrapper(ProtoWrapper):
+    def accept(self, visitor: BaseVisitor) -> None:
+        visitor.visit_order_by(self)
 
 
-class AggregationToConditionalAggregationVisitor(ProtoVisitor):
+class BaseVisitor:
+    def visit_trace_item_table_request(self, field: ProtoWrapper) -> None:
+        trace_item_table_request = field.underlying_proto
+        for column in trace_item_table_request.columns:
+            ColumnWrapper(column).accept(self)
+
+        for ob in trace_item_table_request.order_by:
+            OrderByWrapper(ob).accept(self)
+
+        AggregationFilterWrapper(trace_item_table_request.aggregation_filter).accept(
+            self
+        )
+
+    def visit_time_series_request(self, field: ProtoWrapper) -> None:
+        time_series_request = field.underlying_proto
+        for expression in time_series_request.expressions:
+            TimeSeriesExpressionWrapper(expression).accept(self)
+
+    def visit_order_by(self, field: ProtoWrapper) -> None:
+        ColumnWrapper(field.underlying_proto.column).accept(self)
+
+    def visit_column(self, field: ProtoWrapper) -> None:
+        column = field.underlying_proto
+        if column.HasField("formula"):
+            if column.formula.HasField("left"):
+                ColumnWrapper(column.formula.left).accept(self)
+            if column.formula.HasField("right"):
+                ColumnWrapper(column.formula.right).accept(self)
+
+    def visit_aggregation_filter(self, field: ProtoWrapper) -> None:
+        aggregation_filter = field.underlying_proto
+        for agg_filter in aggregation_filter.and_filter.filters:
+            AggregationFilterWrapper(agg_filter).accept(self)
+        for agg_filter in aggregation_filter.or_filter.filters:
+            AggregationFilterWrapper(agg_filter).accept(self)
+
+    def visit_time_series_expression(self, field: ProtoWrapper) -> None:
+        expression = field.underlying_proto
+        if expression.HasField("formula"):
+            TimeSeriesExpressionWrapper(expression.formula.left).accept(self)
+            TimeSeriesExpressionWrapper(expression.formula.right).accept(self)
+
+    def visit_time_series_expressions(self, field: ProtoWrapper) -> None:
+        expressions = field.underlying_proto
+        for expression in expressions:
+            TimeSeriesExpressionWrapper(expression).accept(self)
+
+
+def _convert_aggregation_to_conditional_aggregation(
+    input: Column | AggregationComparisonFilter | TimeSeriesExpression,
+) -> None:
+    if input.HasField("aggregation"):
+        aggregation = input.aggregation
+        input.ClearField("aggregation")
+        input.conditional_aggregation.CopyFrom(
+            AttributeConditionalAggregation(
+                aggregate=aggregation.aggregate,
+                key=aggregation.key,
+                label=aggregation.label,
+                extrapolation_mode=aggregation.extrapolation_mode,
+            )
+        )
+
+
+class AggregationToConditionalAggregationVisitor(BaseVisitor):
     """
     We support aggregation, but now we want to support conditional aggregation for the insights team, which only aggregates
     if the field satisfies the condition: https://clickhouse.com/docs/en/sql-reference/aggregate-functions/combinators#-if.
@@ -120,28 +129,21 @@ class AggregationToConditionalAggregationVisitor(ProtoVisitor):
     if the request contains `AttributeAggregation` or `AttributeConditionalAggregation`
     """
 
-    def visit(
-        self,
-        input: ProtoWrapper,
-    ) -> None:
-        proto: Column | AggregationComparisonFilter | TimeSeriesExpression = None  # type: ignore
-        if isinstance(input, ColumnWrapper):
-            proto = input.column
+    def visit_column(self, field: ProtoWrapper) -> None:
+        column = field.underlying_proto
+        _convert_aggregation_to_conditional_aggregation(column)
+        super().visit_column(field)
 
-        if isinstance(input, AggregationComparisonFilterWrapper):
-            proto = input.aggregation_comparison_filter
-
-        if isinstance(input, TimeSeriesExpressionWrapper):
-            proto = input.time_series_expression
-
-        if proto.HasField("aggregation"):
-            aggregation = proto.aggregation
-            proto.ClearField("aggregation")
-            proto.conditional_aggregation.CopyFrom(
-                AttributeConditionalAggregation(
-                    aggregate=aggregation.aggregate,
-                    key=aggregation.key,
-                    label=aggregation.label,
-                    extrapolation_mode=aggregation.extrapolation_mode,
-                )
+    def visit_aggregation_filter(self, field: ProtoWrapper) -> None:
+        aggregation_filter = field.underlying_proto
+        if aggregation_filter.HasField("comparison_filter"):
+            _convert_aggregation_to_conditional_aggregation(
+                aggregation_filter.comparison_filter
             )
+        else:
+            super().visit_aggregation_filter(field)
+
+    def visit_time_series_expression(self, field: ProtoWrapper) -> None:
+        time_series_expression = field.underlying_proto
+        _convert_aggregation_to_conditional_aggregation(time_series_expression)
+        super().visit_time_series_expression(field)
