aa5cadee42b1bdd20d47f2fdce80684c460dfa75
Kyle Mumma <kyle.mumma@sentry.io>
2025-03-03 12:02:14 -0800

    update to 1 table, remove aggs

--- snuba/snuba_migrations/events_analytics_platform/0029_items_attribute_table_v1.py
+++ snuba/snuba_migrations/events_analytics_platform/0029_items_attribute_table_v1.py
@@ -7,93 +7,72 @@ from snuba.migrations import migration, operations, table_engines
 from snuba.migrations.columns import MigrationModifiers as Modifiers
 from snuba.migrations.operations import OperationTarget, SqlOperation
 from snuba.utils.constants import ITEM_ATTRIBUTE_BUCKETS
-from snuba.utils.schemas import (
-    Column,
-    DateTime,
-    Float,
-    SimpleAggregateFunction,
-    String,
-    UInt,
-)
+from snuba.utils.schemas import Column, DateTime, String, UInt
 
 
 class Migration(migration.ClickhouseNodeMigration):
     """
     This migration creates a table meant to store just the attributes seen in a particular org.
-    The table is populated by a separate materialized view for each type of attribute.
+
+    * attr_type can either be "string" or "float"
+    * attr_value is null for float attributes
     """
 
     blocking = False
     storage_set_key = StorageSetKey.EVENTS_ANALYTICS_PLATFORM
     granularity = "8192"
 
-    str_mv = "items_str_attrs_1_mv"
-    str_local_table = "items_str_attrs_1_local"
-    str_dist_table = "items_str_attrs_1_dist"
-    str_columns: Sequence[Column[Modifiers]] = [
+    mv = "items_attrs_1_mv"
+    local_table = "items_attrs_1_local"
+    dist_table = "items_attrs_1_dist"
+    columns: Sequence[Column[Modifiers]] = [
         Column("organization_id", UInt(64)),
         Column("project_id", UInt(64)),
         Column("item_type", UInt(8)),
         Column("attr_key", String(modifiers=Modifiers(codecs=["ZSTD(1)"]))),
-        Column("attr_value", String(modifiers=Modifiers(codecs=["ZSTD(1)"]))),
+        Column("attr_type", String(Modifiers(low_cardinality=True))),
         Column(
             "timestamp",
             DateTime(modifiers=Modifiers(codecs=["DoubleDelta", "ZSTD(1)"])),
         ),
         Column("retention_days", UInt(16)),
-        Column("count", SimpleAggregateFunction("sum", [UInt(64)])),
-    ]
-
-    num_mv = "items_num_attrs_1_mv"
-    num_local_table = "items_num_attrs_1_local"
-    num_dist_table = "items_num_attrs_1_dist"
-    num_columns: Sequence[Column[Modifiers]] = [
-        Column("organization_id", UInt(64)),
-        Column("project_id", UInt(64)),
-        Column("item_type", UInt(8)),
-        Column("attr_key", String(modifiers=Modifiers(codecs=["ZSTD(1)"]))),
-        Column("attr_min_value", SimpleAggregateFunction("min", [Float(64)])),
-        Column("attr_max_value", SimpleAggregateFunction("max", [Float(64)])),
         Column(
-            "timestamp",
-            DateTime(modifiers=Modifiers(codecs=["DoubleDelta", "ZSTD(1)"])),
+            "attr_value", String(modifiers=Modifiers(codecs=["ZSTD(1)"], nullable=True))
         ),
-        Column("retention_days", UInt(16)),
-        Column("count", SimpleAggregateFunction("sum", [UInt(64)])),
     ]
 
     def forwards_ops(self) -> Sequence[SqlOperation]:
         return [
             operations.CreateTable(
                 storage_set=self.storage_set_key,
-                table_name=self.str_local_table,
-                engine=table_engines.AggregatingMergeTree(
+                table_name=self.local_table,
+                engine=table_engines.ReplacingMergeTree(
                     storage_set=self.storage_set_key,
                     primary_key="(organization_id, project_id, timestamp, item_type, attr_key)",
-                    order_by="(organization_id, project_id, timestamp, item_type, attr_key, attr_value, retention_days)",
+                    order_by="(organization_id, project_id, timestamp, item_type, attr_key, attr_type, attr_value, retention_days)",
                     partition_by="(retention_days, toMonday(timestamp))",
                     settings={
                         "index_granularity": self.granularity,
                     },
                     ttl="timestamp + toIntervalDay(retention_days)",
                 ),
-                columns=self.str_columns,
+                columns=self.columns,
                 target=OperationTarget.LOCAL,
             ),
             operations.CreateTable(
                 storage_set=self.storage_set_key,
-                table_name=self.str_dist_table,
+                table_name=self.dist_table,
                 engine=table_engines.Distributed(
-                    local_table_name=self.str_local_table, sharding_key=None
+                    local_table_name=self.local_table, sharding_key=None
                 ),
-                columns=self.str_columns,
+                columns=self.columns,
                 target=OperationTarget.DISTRIBUTED,
             ),
             operations.CreateMaterializedView(
                 storage_set=self.storage_set_key,
-                view_name=self.str_mv,
-                columns=self.str_columns,
-                destination_table_name=self.str_local_table,
+                view_name=self.mv,
+                columns=self.columns,
+                destination_table_name=self.local_table,
                 target=OperationTarget.LOCAL,
                 query=f"""
 SELECT
@@ -102,13 +81,14 @@ SELECT
     item_type,
     attrs.1 as attr_key,
     attrs.2 as attr_value,
+    attrs.3 as attr_type,
     toStartOfWeek(timestamp) AS timestamp,
     retention_days,
-    1 AS count
 FROM eap_items_1_local
 LEFT ARRAY JOIN
     arrayConcat(
-        {", ".join(f"CAST(attributes_string_{n}, 'Array(Tuple(String, String))')" for n in range(ITEM_ATTRIBUTE_BUCKETS))}
+        {", ".join(f"arrayMap(x -> tuple(x.1, x.2, 'string'), CAST(attributes_string_{n}, 'Array(Tuple(String, String))'))" for n in range(ITEM_ATTRIBUTE_BUCKETS))},
+        {",".join(f"arrayMap(x -> tuple(x, Null, 'float'), mapKeys(attributes_float_{n}))" for n in range(ITEM_ATTRIBUTE_BUCKETS))}
     ) AS attrs
 GROUP BY
     organization_id,
@@ -116,63 +96,7 @@ GROUP BY
     item_type,
     attr_key,
     attr_value,
-    timestamp,
-    retention_days
-""",
-            ),
-            operations.CreateTable(
-                storage_set=self.storage_set_key,
-                table_name=self.num_local_table,
-                engine=table_engines.AggregatingMergeTree(
-                    storage_set=self.storage_set_key,
-                    primary_key="(organization_id, project_id, timestamp, item_type, attr_key)",
-                    order_by="(organization_id, project_id, timestamp, item_type, attr_key, retention_days)",
-                    partition_by="(retention_days, toMonday(timestamp))",
-                    settings={
-                        "index_granularity": self.granularity,
-                    },
-                    ttl="timestamp + toIntervalDay(retention_days)",
-                ),
-                columns=self.num_columns,
-                target=OperationTarget.LOCAL,
-            ),
-            operations.CreateTable(
-                storage_set=self.storage_set_key,
-                table_name=self.num_dist_table,
-                engine=table_engines.Distributed(
-                    local_table_name=self.num_local_table, sharding_key=None
-                ),
-                columns=self.num_columns,
-                target=OperationTarget.DISTRIBUTED,
-            ),
-            operations.CreateMaterializedView(
-                storage_set=self.storage_set_key,
-                view_name=self.num_mv,
-                columns=self.num_columns,
-                destination_table_name=self.num_local_table,
-                target=OperationTarget.LOCAL,
-                query=f"""
-SELECT
-    organization_id,
-    project_id,
-    item_type,
-    attrs.1 as attr_key,
-    attrs.2 as attr_min_value,
-    attrs.2 as attr_max_value,
-    toStartOfWeek(timestamp) AS timestamp,
-    retention_days,
-    1 AS count
-FROM eap_items_1_local
-LEFT ARRAY JOIN
-    arrayConcat(
-        {",".join(f"CAST(attributes_float_{n}, 'Array(Tuple(String, Float64))')" for n in range(ITEM_ATTRIBUTE_BUCKETS))}
-    ) AS attrs
-GROUP BY
-    organization_id,
-    project_id,
-    item_type,
-    attrs.1,
-    attrs.2,
+    attr_type,
     timestamp,
     retention_days
 """,
@@ -183,32 +107,17 @@ GROUP BY
         return [
             operations.DropTable(
                 storage_set=self.storage_set_key,
-                table_name=self.str_mv,
+                table_name=self.mv,
                 target=OperationTarget.LOCAL,
             ),
             operations.DropTable(
                 storage_set=self.storage_set_key,
-                table_name=self.str_local_table,
+                table_name=self.local_table,
                 target=OperationTarget.LOCAL,
             ),
             operations.DropTable(
                 storage_set=self.storage_set_key,
-                table_name=self.str_dist_table,
-                target=OperationTarget.DISTRIBUTED,
-            ),
-            operations.DropTable(
-                storage_set=self.storage_set_key,
-                table_name=self.num_mv,
-                target=OperationTarget.LOCAL,
-            ),
-            operations.DropTable(
-                storage_set=self.storage_set_key,
-                table_name=self.num_local_table,
-                target=OperationTarget.LOCAL,
-            ),
-            operations.DropTable(
-                storage_set=self.storage_set_key,
-                table_name=self.num_dist_table,
+                table_name=self.dist_table,
                 target=OperationTarget.DISTRIBUTED,
             ),
         ]
