0f55a35ecc4a9af8445fabf09aa04491dab16e07
Volo Kluev <volo.kluev@sentry.io>
2025-03-12 13:42:43 -0700

    move eap_spans stuff to its own file

--- snuba/web/rpc/v1/endpoint_trace_item_attribute_names.py
+++ snuba/web/rpc/v1/endpoint_trace_item_attribute_names.py
@@ -13,6 +13,7 @@ from sentry_protos.snuba.v1.trace_item_filter_pb2 import (
     TraceItemFilter,
 )
 
+from snuba import settings, state
 from snuba.attribution.appid import AppID
 from snuba.attribution.attribution_info import AttributionInfo
 from snuba.datasets.entities.entity_key import EntityKey
@@ -36,88 +37,18 @@ from snuba.web.rpc.common.common import (
 )
 from snuba.web.rpc.common.debug_info import extract_response_meta
 from snuba.web.rpc.common.exceptions import BadSnubaRPCRequestException
+from snuba.web.rpc.v1.legacy.trace_item_attribute_names import (
+    convert_to_snuba_request as legacy_convert_to_snuba_request,
+)
 
 # max value the user can provide for 'limit' in their request
 MAX_REQUEST_LIMIT = 1000
 
 
 def convert_to_snuba_request(req: TraceItemAttributeNamesRequest) -> SnubaRequest:
-    if req.limit > MAX_REQUEST_LIMIT:
-        raise BadSnubaRPCRequestException(
-            f"Limit can be at most {MAX_REQUEST_LIMIT}, but was {req.limit}. For larger limits please utilize pagination via the page_token variable in your request."
-        )
 
-    if req.type == AttributeKey.Type.TYPE_STRING:
-        entity = Entity(
-            key=EntityKey("spans_str_attrs"),
-            schema=get_entity(EntityKey("spans_str_attrs")).get_data_model(),
-            sample=None,
-        )
-    elif req.type in (
-        AttributeKey.Type.TYPE_FLOAT,
-        AttributeKey.Type.TYPE_DOUBLE,
-        AttributeKey.Type.TYPE_INT,
-        AttributeKey.Type.TYPE_BOOLEAN,
-    ):
-        entity = Entity(
-            key=EntityKey("spans_num_attrs"),
-            schema=get_entity(EntityKey("spans_num_attrs")).get_data_model(),
-            sample=None,
-        )
-    else:
-        raise BadSnubaRPCRequestException(
-            f"Attribute type '{req.type}' is not supported. Supported types are: TYPE_STRING, TYPE_FLOAT, TYPE_INT, TYPE_BOOLEAN"
-        )
-
-    condition = (
-        base_conditions_and(
-            req.meta,
-            f.like(column("attr_key"), f"%{req.value_substring_match}%"),
-            convert_filter_offset(req.page_token.filter_offset),
-        )
-        if req.page_token.HasField("filter_offset")
-        else base_conditions_and(
-            req.meta, f.like(column("attr_key"), f"%{req.value_substring_match}%")
-        )
-    )
-
-    query = Query(
-        from_clause=entity,
-        selected_columns=[
-            SelectedExpression(
-                name="attr_key",
-                expression=column("attr_key", alias="attr_key"),
-            ),
-        ],
-        condition=condition,
-        order_by=[
-            OrderBy(direction=OrderByDirection.ASC, expression=column("attr_key")),
-        ],
-        groupby=[
-            column("attr_key", alias="attr_key"),
-        ],
-        limit=req.limit,
-        offset=req.page_token.offset,
-    )
-    treeify_or_and_conditions(query)
-
-    return SnubaRequest(
-        id=uuid.UUID(req.meta.request_id),
-        original_body=MessageToDict(req),
-        query=query,
-        query_settings=HTTPQuerySettings(),
-        attribution_info=AttributionInfo(
-            referrer=req.meta.referrer,
-            team="eap",
-            feature="eap",
-            tenant_ids={
-                "organization_id": req.meta.organization_id,
-                "referrer": req.meta.referrer,
-            },
-            app_id=AppID("eap"),
-            parent_api=EndpointTraceItemAttributeNames.config_key(),
-        ),
-    )
+    if not _should_use_items_attrs(req):
+        return legacy_convert_to_snuba_request(req)
 
 
 def convert_to_attributes(
@@ -134,6 +65,28 @@ def convert_to_attributes(
         )
 
     return list(map(t, query_res.result["data"]))
+
+
+def _should_use_items_attrs(request: TraceItemAttributeNamesRequest) -> bool:
+    request_meta = request.meta
+    if getattr(settings, "USE_EAP_ITEMS_TABLE", False):
+        return True
+    if request_meta.referrer.startswith("force_use_eap_spans_table"):
+        return False
+
+    use_eap_items_attrs_table_start_timestamp_seconds = state.get_int_config(
+        "use_eap_items_attrs_table_start_timestamp_seconds"
+    )
+    if (
+        state.get_config("use_eap_items_attrs_table", False)
+        and use_eap_items_attrs_table_start_timestamp_seconds is not None
+    ):
+        return (
+            request_meta.start_timestamp.seconds
+            >= use_eap_items_attrs_table_start_timestamp_seconds
+        )
+
+    return False
 
 
 class EndpointTraceItemAttributeNames(
