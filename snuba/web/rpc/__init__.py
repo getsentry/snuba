f017011217fe6efbbda8aa0c2c79c4eb20e6d93c
Volo Kluev <volo.kluev@sentry.io>
2025-03-17 15:57:41 -0700

    change error message

--- snuba/web/rpc/__init__.py
+++ snuba/web/rpc/__init__.py
@@ -274,6 +274,6 @@ def run_rpc_handler(
         return convert_rpc_exception_to_proto(
             RPCRequestException(
                 status_code=500,
-                message=f"internal error occurred while executing this RPC call: {e}",
+                message=f"500 error: {e}",
             )
         )
