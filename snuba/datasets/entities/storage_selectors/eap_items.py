b903a77333f07a21ddc475af1b372aacc8d1ff94
Rachel Chen <rachelchen@PL6VFX9HP4.local>
2025-03-20 13:00:26 -0700

    selector

--- snuba/datasets/entities/storage_selectors/eap_items.py
+++ snuba/datasets/entities/storage_selectors/eap_items.py
@@ -4,6 +4,7 @@ from snuba.datasets.entities.storage_selectors import QueryStorageSelector
 from snuba.datasets.storage import EntityStorageConnection
 from snuba.datasets.storages.factory import get_storage
 from snuba.datasets.storages.storage_key import StorageKey
+from snuba.downsampled_storage_tiers import Tier
 from snuba.query.logical import Query
 from snuba.query.query_settings import HTTPQuerySettings, QuerySettings
 
@@ -16,11 +17,14 @@ class EAPItemsStorageSelector(QueryStorageSelector):
         storage_connections: Sequence[EntityStorageConnection],
     ) -> EntityStorageConnection:
         assert isinstance(query_settings, HTTPQuerySettings)
-        tier = query_settings.get_sampling_tier().value
-        if tier == 1:
+
+        tier = query_settings.get_sampling_tier()
+
+        if tier == Tier.TIER_1:
             storage_key = StorageKey.EAP_ITEMS
         else:
-            storage_key = getattr(StorageKey, f"EAP_ITEMS_DOWNSAMPLE_{tier}")
+            storage_key = getattr(StorageKey, f"EAP_ITEMS_DOWNSAMPLE_{tier.value}")
+
         return EntityStorageConnection(
             storage=get_storage(storage_key),
             translation_mappers=storage_connections[0].translation_mappers,
