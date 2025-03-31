cc88aa779463851ca1160281d786986d1697ce99
Rachel Chen <rachelchen@PL6VFX9HP4.local>
2025-03-31 11:30:29 -0700

    test

--- tests/web/rpc/v1/test_sampling_in_storage_util.py
+++ tests/web/rpc/v1/test_sampling_in_storage_util.py
@@ -1,39 +1,50 @@
+import uuid
 from unittest.mock import MagicMock, patch
 
-from snuba.reader import Result
+from snuba.attribution import AppID
+from snuba.attribution.attribution_info import AttributionInfo
+from snuba.query.logical import Query as LogicalQuery
+from snuba.query.query_settings import HTTPQuerySettings
 from snuba.request import Request
 from snuba.utils.metrics import MetricsBackend
 from snuba.utils.metrics.timer import Timer
-from snuba.web import QueryExtraData, QueryResult
 from snuba.web.rpc.v1.resolvers.R_eap_spans.common.sampling_in_storage_util import (
     _run_query_on_most_downsampled_tier,
 )
 
+DOESNT_MATTER_STR = "doesntmatter"
+DOESNT_MATTER_INT = 2
 
-def test_sampling_in_storage_estimation_duration_metric_is_sent() -> None:
+
+def test_sampling_in_storage_most_downsampled_tier_query_duration_metric_is_sent() -> None:
+    timer = Timer(DOESNT_MATTER_STR)
     metrics_mock = MagicMock(spec=MetricsBackend)
-    timer = Timer("doesntmatter")
-    doesntmatterresult = QueryResult(
-        result=MagicMock(spec=Result),
-        extra=MagicMock(spec=QueryExtraData),
-    )
 
+    doesnt_matter_request = Request(
+        id=uuid.uuid4(),
+        original_body={},
+        query=LogicalQuery(from_clause=None),
+        query_settings=HTTPQuerySettings(),
+        attribution_info=AttributionInfo(
+            app_id=AppID(key=DOESNT_MATTER_STR),
+            tenant_ids={},
+            referrer=DOESNT_MATTER_STR,
+            team=None,
+            feature=None,
+            parent_api=None,
+        ),
+    )
     with patch(
         "snuba.web.rpc.v1.resolvers.R_eap_spans.common.sampling_in_storage_util.run_query",
-        return_value=doesntmatterresult,
+    ), patch(
+        "snuba.web.rpc.v1.resolvers.R_eap_spans.common.sampling_in_storage_util._get_query_duration",
+        return_value=DOESNT_MATTER_INT,
     ):
-
         _run_query_on_most_downsampled_tier(
-            request_to_most_downsampled_tier=MagicMock(spec=Request),
-            timer=timer,
-            metrics_backend=metrics_mock,
-            referrer="doesntmatter",
-        )
-
-        duration = timer.get_duration_between_marks(
-            "right_before_execute",
-            "execute",
+            doesnt_matter_request, timer, metrics_mock, DOESNT_MATTER_STR
         )
         metrics_mock.timing.assert_called_once_with(
-            "sampling_in_storage_estimation_duration", duration
+            "sampling_in_storage_most_downsampled_tier_query_duration",
+            DOESNT_MATTER_INT,
+            tags={"referrer": DOESNT_MATTER_STR},
         )
