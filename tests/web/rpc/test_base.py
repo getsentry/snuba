f5ce456eddb14d9251e9882cf2aea8f8fb6f55b9
Volo Kluev <volo.kluev@sentry.io>
2025-04-02 15:43:14 -0700

    fucks sake

--- tests/web/rpc/test_base.py
+++ tests/web/rpc/test_base.py
@@ -22,6 +22,7 @@ from snuba.web.rpc import (
 )
 from snuba.web.rpc.v1.endpoint_trace_item_table import EndpointTraceItemTable
 from tests.backends.metrics import TestingMetricsBackend
+from tests.conftest import SnubaSetConfig
 from tests.helpers import write_raw_unprocessed_events
 
 
@@ -162,7 +163,7 @@ def gen_message(
 
 @pytest.mark.clickhouse_db
 @pytest.mark.redis_db
-def test_trim_time_range(snuba_set_config) -> None:
+def test_trim_time_range(snuba_set_config: SnubaSetConfig) -> None:
     snuba_set_config("use_eap_items_table", True)
     snuba_set_config("use_eap_items_table_start_timestamp_seconds", 0)
 
@@ -222,7 +223,7 @@ _REFERRER = "something"
     ],
 )
 def test_tagged_metrics(
-    hours: int, expected_time_bucket: str, snuba_set_config
+    hours: int, expected_time_bucket: str, snuba_set_config: SnubaSetConfig
 ) -> None:
     snuba_set_config("use_eap_items_table", True)
     snuba_set_config("use_eap_items_table_start_timestamp_seconds", 0)
