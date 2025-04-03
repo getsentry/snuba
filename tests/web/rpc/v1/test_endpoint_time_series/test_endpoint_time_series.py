446d8fd51684ea362aa7d204cc969d1eac9417d7
xurui-c <159840875+xurui-c@users.noreply.github.com>
2025-04-03 09:36:02 -0700

    fix(sampling-in-storage): make tests not flakey (#7027)
    Bounds are too tight, let's loosen them. As long as the count is
    reasonably away from 3600/512 (Tier 512) and 3600/8 (Tier 8) we're good.
    
    We don't set the bound to be exactly 3600/512 or 3600/8 because that
    could mean the query was routed to tier 512 or tier 8
    
    See visualization (not drawn to scale):
    ![Note Sep 20,
    2024](https://github.com/user-attachments/assets/04aa05a2-9f55-49b7-84a6-7acdadc7680b)
    
    Co-authored-by: Rachel Chen <rachelchen@PL6VFX9HP4.attlocal.net>

--- tests/web/rpc/v1/test_endpoint_time_series/test_endpoint_time_series.py
+++ tests/web/rpc/v1/test_endpoint_time_series/test_endpoint_time_series.py
@@ -1535,9 +1535,9 @@ class TestTimeSeriesApiEAPItems(TestTimeSeriesApi):
                 non_downsampled_tier_response.result_timeseries[0].data_points[0].data
             )
             assert (
-                non_downsampled_best_effort_metric_sum / 90
+                non_downsampled_best_effort_metric_sum / 200
                 <= best_effort_metric_sum
-                <= non_downsampled_best_effort_metric_sum / 40
+                <= non_downsampled_best_effort_metric_sum / 16
             )
 
             assert (
