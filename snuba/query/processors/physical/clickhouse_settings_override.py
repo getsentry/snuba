46f2f5942f4dd274fd44e5bf1dacf6683635725e
Volo Kluev <volo.kluev@sentry.io>
2025-04-11 11:07:40 -0700

    don't overwrite clickhouse settings that were already set

--- snuba/query/processors/physical/clickhouse_settings_override.py
+++ snuba/query/processors/physical/clickhouse_settings_override.py
@@ -8,13 +8,35 @@ from snuba.query.query_settings import QuerySettings
 class ClickhouseSettingsOverride(ClickhouseQueryProcessor):
     """
     Overrides arbitrary clickhouse settings via a dictionary specifying the clickhouse settings to override.
+
+    the `overwrite_existing` parameter controls whether the processor overwrites the clickhouse settings already present on the query
+    settings or only updates the ones which are not set.
+
+    E.g.
+
+    processor_settings = {"max_rows_to_group_by": 1, "groupby_overflow_mode": "break"}
+
+    query_settings.clickhouse_settings = {"max_rows_to_group_by": 12345}
+
+    if overwrite_existing == True:
+        query_settings.clickhouse_settings = {"max_rows_to_group_by": 1, "groupby_overflow_mode": "break"}
+    if overwrite_existing == False:
+        query_settings.clickhouse_settings = {"max_rows_to_group_by": 12345, "groupby_overflow_mode": "break"}
+
     """
 
-    def __init__(self, settings: MutableMapping[str, Any]) -> None:
+    def __init__(
+        self, settings: MutableMapping[str, Any], overwrite_existing: bool = True
+    ) -> None:
         self.__settings = settings
+        self.__overwrite_existing = overwrite_existing
 
     def process_query(self, query: Query, query_settings: QuerySettings) -> None:
         new_settings: MutableMapping[str, Any] = {}
-        new_settings.update(query_settings.get_clickhouse_settings())
-        new_settings.update(self.__settings)
+        if self.__overwrite_existing:
+            new_settings.update(query_settings.get_clickhouse_settings())
+            new_settings.update(self.__settings)
+        else:
+            new_settings.update(self.__settings)
+            new_settings.update(query_settings.get_clickhouse_settings())
         query_settings.set_clickhouse_settings(new_settings)
