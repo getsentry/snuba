af3406312a4af6ba8c2f1727b644a52e0917fe78
Oliver Newland <oliver.newland@sentry.io>
2025-04-21 18:56:32 -0700

    remove commented code

--- snuba/web/rpc/v1/resolvers/R_eap_spans/common/common.py
+++ snuba/web/rpc/v1/resolvers/R_eap_spans/common/common.py
@@ -105,48 +105,6 @@ ATTRIBUTE_MAPPINGS: Final[Mapping[str, str]] = {
 
 
 def use_eap_items_table(request_meta: RequestMeta) -> bool:
-    # if request_meta.referrer.startswith("force_use_eap_spans_table"):
-    #     return False
-
-    # if request_meta.trace_item_type == TraceItemType.TRACE_ITEM_TYPE_LOG:
-    #     return True
-
-    # use_eap_items_orgs = state.get_str_config("use_eap_items_orgs")
-    # eap_items_enabled_orgs = []
-    # use_eap_items_for_all_orgs = True
-    # if use_eap_items_orgs:
-    #     try:
-    #         eap_items_enabled_orgs = list(
-    #             map(int, use_eap_items_orgs.strip("[]").split(","))
-    #         )
-    #         use_eap_items_for_all_orgs = False
-    #     except ValueError:
-    #         pass
-
-    # turned_on_for_org = (
-    #     use_eap_items_for_all_orgs
-    #     or request_meta.organization_id in eap_items_enabled_orgs
-    # )
-
-    # use_eap_items_table_start_timestamp_seconds = state.get_int_config(
-    #     "use_eap_items_table_start_timestamp_seconds",
-    #     settings.USE_EAP_ITEMS_TABLE_START_TIMESTAMP_SECONDS,
-    # )
-
-    # assert use_eap_items_table_start_timestamp_seconds is not None
-    # use_eap_items_table_start_timestamp_seconds = int(
-    #     use_eap_items_table_start_timestamp_seconds
-    # )
-
-    # if (
-    #     state.get_int_config("use_eap_items_table", settings.USE_EAP_ITEMS_TABLE)
-    #     and turned_on_for_org
-    # ):
-    #     return (
-    #         request_meta.start_timestamp.seconds
-    #         >= use_eap_items_table_start_timestamp_seconds
-    #     )
-
     return True
 
 
