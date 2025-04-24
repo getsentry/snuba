f394c17f641a037ee66001536cc1386c698bf8d5
Rachel Chen <rachelchen@PL6VFX9HP4.local>
2025-04-24 10:28:30 -0700

    comments

--- snuba/web/rpc/v1/resolvers/R_eap_items/storage_routing/routing_strategies/normal_mode_linear_bytes_scanned.py
+++ snuba/web/rpc/v1/resolvers/R_eap_items/storage_routing/routing_strategies/normal_mode_linear_bytes_scanned.py
@@ -258,14 +258,14 @@ class NormalModeLinearBytesScannedRoutingStrategy(BaseRoutingStrategy):
             self._record_value_in_span_and_DD(
                 routing_context,
                 self.metrics.distribution,
-                f"actual_bytes_scanned_in_target_tier_{target_tier}",
+                "actual_bytes_scanned_in_target_tier",
                 self._get_query_bytes_scanned(routing_context.query_result),
                 tags={"tier": str(target_tier)},
             )
             self._record_value_in_span_and_DD(
                 routing_context,
                 self.metrics.timing,
-                f"time_to_run_query_in_target_tier_{target_tier}",
+                "time_to_run_query_in_target_tier",
                 query_duration_ms,
                 tags={"tier": str(target_tier)},
             )
