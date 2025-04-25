38c85f1252325286bd7c8ffc6815f9fc8ccaebdc
Rachel Chen <rachelchen@MacBookPro.attlocal.net>
2025-04-25 11:22:03 -0700

    better approach

--- snuba/web/rpc/v1/resolvers/R_eap_items/storage_routing/routing_strategies/normal_mode_linear_bytes_scanned.py
+++ snuba/web/rpc/v1/resolvers/R_eap_items/storage_routing/routing_strategies/normal_mode_linear_bytes_scanned.py
@@ -97,41 +97,38 @@ class NormalModeLinearBytesScannedRoutingStrategy(BaseRoutingStrategy):
                 most_downsampled_tier_query_bytes_scanned
                 * self._get_multiplier(target_tier)
             )
-            if (
-                most_downsampled_tier_query_bytes_scanned
-                < self._get_bytes_scanned_limit()
-            ):
-                for tier in sorted(Tier, reverse=True)[:-1]:
-                    with sentry_sdk.start_span(
-                        op=f"_get_target_tier.Tier_{tier}"
-                    ) as tier_specific_span:
-                        estimated_query_bytes_scanned_to_this_tier = (
-                            most_downsampled_tier_query_bytes_scanned
-                            * self._get_multiplier(tier)
-                        )
-                        self._record_value_in_span_and_DD(
-                            routing_context,
-                            self.metrics.distribution,
-                            "estimated_query_bytes_scanned_to_this_tier",
-                            estimated_query_bytes_scanned_to_this_tier,
-                            {"tier": str(tier)},
-                        )
-                        bytes_scanned_limit = self._get_bytes_scanned_limit()
-                        if (
+
+            for tier in sorted(Tier, reverse=True)[:-1]:
+                with sentry_sdk.start_span(
+                    op=f"_get_target_tier.Tier_{tier}"
+                ) as tier_specific_span:
+                    estimated_query_bytes_scanned_to_this_tier = (
+                        most_downsampled_tier_query_bytes_scanned
+                        * self._get_multiplier(tier)
+                    )
+                    self._record_value_in_span_and_DD(
+                        routing_context,
+                        self.metrics.distribution,
+                        "estimated_query_bytes_scanned_to_this_tier",
+                        estimated_query_bytes_scanned_to_this_tier,
+                        {"tier": str(tier)},
+                    )
+                    bytes_scanned_limit = self._get_bytes_scanned_limit()
+                    if (
+                        estimated_query_bytes_scanned_to_this_tier
+                        <= bytes_scanned_limit
+                    ):
+                        target_tier = tier
+                        estimated_target_tier_bytes_scanned = (
                             estimated_query_bytes_scanned_to_this_tier
-                            <= bytes_scanned_limit
-                        ):
-                            target_tier = tier
-                            estimated_target_tier_bytes_scanned = (
-                                estimated_query_bytes_scanned_to_this_tier
-                            )
-                        tier_specific_span.set_data(
-                            _SAMPLING_IN_STORAGE_PREFIX + "target_tier", target_tier
                         )
-                        tier_specific_span.set_data(
-                            _SAMPLING_IN_STORAGE_PREFIX + "bytes_scanned_limit",
-                            bytes_scanned_limit,
-                        )
+                    tier_specific_span.set_data(
+                        _SAMPLING_IN_STORAGE_PREFIX + "target_tier", target_tier
+                    )
+                    tier_specific_span.set_data(
+                        _SAMPLING_IN_STORAGE_PREFIX + "bytes_scanned_limit",
+                        bytes_scanned_limit,
+                    )
             self.metrics.increment(
                 _SAMPLING_IN_STORAGE_PREFIX + "target_tier",
                 1,
@@ -193,6 +190,18 @@ class NormalModeLinearBytesScannedRoutingStrategy(BaseRoutingStrategy):
         routing_context.query_result = self._run_query_on_most_downsampled_tier(
             routing_context
         )
+
+        if (
+            self._get_query_bytes_scanned(routing_context.query_result)
+            >= self._get_bytes_scanned_limit()
+        ):
+            self._record_value_in_span_and_DD(
+                routing_context,
+                self.metrics.increment,
+                "most_downsampled_tier_query_bytes_scanned_exceeds_limit",
+                1,
+            )
+            return self._get_most_downsampled_tier(), {}
 
         return (
             self._get_target_tier(routing_context.query_result, routing_context),
