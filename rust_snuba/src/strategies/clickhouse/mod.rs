eb4cd432ac29108cef66e3f85722203b48eac480
Meredith Heller <meredith@sentry.io>
2025-04-28 11:20:32 -0700

    logging

--- rust_snuba/src/strategies/clickhouse/mod.rs
+++ rust_snuba/src/strategies/clickhouse/mod.rs
@@ -46,8 +46,9 @@ impl TaskRunner<BytesInsertBatch<HttpBatch>, BytesInsertBatch<()>, anyhow::Error
 
             let write_start = SystemTime::now();
 
-            tracing::debug!("performing write");
+            tracing::info!("performing write");
             http_batch.finish().await.map_err(RunTaskError::Other)?;
+            tracing::info!("finished performing write");
 
             let write_finish = SystemTime::now();
 
