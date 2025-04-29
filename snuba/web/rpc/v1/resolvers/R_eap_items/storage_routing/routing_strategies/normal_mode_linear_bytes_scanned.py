b09f7fdadd4f935716e074160290d175d2c5ce5b
Rachel Chen <rachelchen@MacBookPro.attlocal.net>
2025-04-29 12:52:19 -0700

    fix(storage-routing): fix metric name

--- snuba/web/rpc/v1/resolvers/R_eap_items/storage_routing/routing_strategies/normal_mode_linear_bytes_scanned.py
+++ snuba/web/rpc/v1/resolvers/R_eap_items/storage_routing/routing_strategies/normal_mode_linear_bytes_scanned.py
@@ -109,7 +109,7 @@ class NormalModeLinearBytesScannedRoutingStrategy(BaseRoutingStrategy):
                     self._record_value_in_span_and_DD(
                         routing_context,
                         self.metrics.distribution,
-                        "estimated_query_bytes_scanned_to_this_tier",
+                        f"estimated_query_bytes_scanned_to_tier_{tier}",
                         estimated_query_bytes_scanned_to_this_tier,
                         {"tier": str(tier)},
                     )
