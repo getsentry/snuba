0d3362b3c937c914f5db518c76538717cf9c4571
Volo Kluev <volo.kluev@sentry.io>
2025-05-05 14:18:58 -0700

    Revert "Revert "feat(eap): Add an item consumer (#7122)""
    This reverts commit 0b2e3280e9e895a3157fe71b2df99ed8e1bd4e0d.

--- rust_snuba/src/processors/mod.rs
+++ rust_snuba/src/processors/mod.rs
@@ -1,4 +1,5 @@
 mod eap_items;
+mod eap_items_span;
 pub(crate) mod eap_spans;
 mod errors;
 mod functions;
@@ -69,8 +70,8 @@ define_processing_functions! {
     ("PolymorphicMetricsProcessor", "snuba-metrics", ProcessingFunctionType::ProcessingFunction(release_health_metrics::process_metrics_message)),
     ("ErrorsProcessor", "events", ProcessingFunctionType::ProcessingFunctionWithReplacements(errors::process_message_with_replacement)),
     ("ProfileChunksProcessor", "snuba-profile-chunks", ProcessingFunctionType::ProcessingFunction(profile_chunks::process_message)),
-    ("EAPItemsProcessor", "snuba-spans", ProcessingFunctionType::ProcessingFunction(eap_items::process_message)),
-    ("EAPItemsSpanProcessor", "snuba-spans", ProcessingFunctionType::ProcessingFunction(eap_items::process_message)),
+    ("EAPItemsProcessor", "snuba-items", ProcessingFunctionType::ProcessingFunction(eap_items::process_message)),
+    ("EAPItemsSpanProcessor", "snuba-spans", ProcessingFunctionType::ProcessingFunction(eap_items_span::process_message)),
 }
 
 // COGS is recorded for these processors
@@ -166,7 +167,11 @@ mod tests {
                     settings.add_redaction(".**[\"sentry.timestamp_precise\"]", "<item timestamp>");
                 }
 
-                settings.set_description(std::str::from_utf8(example.payload()).unwrap());
+                // This payload is protobuf (so binary), not JSON (so text).
+                if *topic_name != "snuba-items" {
+                    settings.set_description(std::str::from_utf8(example.payload()).unwrap());
+                }
+
                 let _guard = settings.bind_to_scope();
 
                 let payload = KafkaPayload::new(None, None, Some(example.payload().to_vec()));
