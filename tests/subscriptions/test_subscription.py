4f010981cfc4d6851cea8ec940c20cb1846f7de4
Oliver Newland <oliver.newland@sentry.io>
2025-05-27 13:45:45 -0700

    attempt manually creating counters topic in test

--- tests/subscriptions/test_subscription.py
+++ tests/subscriptions/test_subscription.py
@@ -2,6 +2,7 @@ from typing import List, Tuple, cast
 from uuid import UUID
 
 import pytest
+from confluent_kafka.admin import AdminClient
 from pytest import raises
 from sentry_protos.snuba.v1.endpoint_create_subscription_pb2 import (
     CreateSubscriptionRequest as CreateSubscriptionRequestProto,
@@ -35,7 +36,10 @@ from snuba.subscriptions.data import (
 )
 from snuba.subscriptions.store import RedisSubscriptionDataStore
 from snuba.subscriptions.subscription import SubscriptionCreator, SubscriptionDeleter
+from snuba.utils.manage_topics import create_topics
 from snuba.utils.metrics.timer import Timer
+from snuba.utils.streams.configuration_builder import get_default_kafka_configuration
+from snuba.utils.streams.topics import Topic as SnubaTopic
 from snuba.web import QueryException
 from tests.helpers import override_entity_column_validator
 from tests.subscriptions import BaseSubscriptionTest
@@ -273,6 +277,8 @@ class TestMetricsCountersSubscriptionCreator:
     @pytest.mark.clickhouse_db
     @pytest.mark.redis_db
     def test(self, subscription: SubscriptionData, entity_key: EntityKey) -> None:
+        admin_client = AdminClient(get_default_kafka_configuration())
+        create_topics(admin_client, [SnubaTopic.GENERIC_METRICS], 2)
         creator = SubscriptionCreator(self.dataset, entity_key)
         identifier = creator.create(subscription, self.timer)
         assert (
