25bd6949c6da08728e0ff149ba479e2d258c51b4
Lorenzo Cian <lorenzo.cian@sentry.io>
2025-07-02 19:01:41 +0200

    chore(consumers): Bump Rust toolchain version (#7252)
    Bumps the Rust toolchain version to the latest stable. Bumping to >=1.85
    is one of the solutions to proceed with
    https://github.com/getsentry/snuba/pull/7250
    
    Also fixes the new lints.

--- rust_snuba/src/consumer.rs
+++ rust_snuba/src/consumer.rs
@@ -557,7 +557,14 @@ pub fn consumer_impl(
     }
 }
 
-pyo3::create_exception!(rust_snuba, SnubaRustError, pyo3::exceptions::PyException);
+#[expect(
+    unexpected_cfgs,
+    reason = "Fixed in pyo3 v0.23, see https://github.com/PyO3/pyo3/issues/4743"
+)]
+mod exceptions {
+    pyo3::create_exception!(rust_snuba, SnubaRustError, pyo3::exceptions::PyException);
+}
+use exceptions::*;
 
 /// insert: encoded rows
 type PyInsert = PyObject;
