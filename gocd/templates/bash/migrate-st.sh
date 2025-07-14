3980da4c4eefc00ec5013cfb3905fd1b5f2f8e5c
mchen-sentry <icswon1108@gmail.com>
2025-07-14 10:09:28 -0700

    typo

--- gocd/templates/bash/migrate-st.sh
+++ gocd/templates/bash/migrate-st.sh
@@ -9,7 +9,7 @@
 eval $(regions-project-env-vars --region="${SENTRY_REGION}")
 /devinfra/scripts/get-cluster-credentials
 
-/k8s-spawn-job \
+k8s-spawn-job \
   --label-selector="service=${SNUBA_SERVICE_NAME}" \
   --container-name="${SNUBA_SERVICE_NAME}" \
   "snuba-migrate" \
