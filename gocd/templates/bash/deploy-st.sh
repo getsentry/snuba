888daaa4a34c17906f4861eda895312609e6e8b4
mchen-sentry <ming.chen@sentry.io>
2025-07-15 10:15:51 -0700

    ref(gocd): use console script entry points (#7273)

--- gocd/templates/bash/deploy-st.sh
+++ gocd/templates/bash/deploy-st.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-eval $(/devinfra/scripts/regions/project_env_vars.py --region="${SENTRY_REGION}")
+eval $(regions-project-env-vars --region="${SENTRY_REGION}")
 
-/devinfra/scripts/k8s/k8stunnel
+/devinfra/scripts/get-cluster-credentials
 
-/devinfra/scripts/k8s/k8s-deploy.py \
+k8s-deploy \
   --label-selector="${LABEL_SELECTOR}" \
   --image="us-central1-docker.pkg.dev/sentryio/snuba/image:${GO_REVISION_SNUBA_REPO}" \
   --container-name="snuba" \
   --container-name="snuba-admin"
 
-/devinfra/scripts/k8s/k8s-deploy.py \
+k8s-deploy \
   --label-selector="${LABEL_SELECTOR}" \
   --image="us-central1-docker.pkg.dev/sentryio/snuba/image:${GO_REVISION_SNUBA_REPO}" \
   --type="cronjob" \
