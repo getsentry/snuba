7efab6a6685c8e67cedc58a46ee667a6d8c33d3a
Pierre Massat <pierre.massat@sentry.io>
2025-07-31 16:03:29 -0700

    feat(eap): Update downsampled tables materialized views to use proper retention (#7319)

---
+++ snuba/snuba_migrations/events_analytics_platform/0047_use_downsampled_retention_for_downsampled_tables.py
@@ -0,0 +1,160 @@
+from typing import List, Sequence
+
+from snuba.clickhouse.columns import Column, UInt
+from snuba.clusters.storage_sets import StorageSetKey
+from snuba.migrations import migration, operations
+from snuba.migrations.columns import MigrationModifiers as Modifiers
+from snuba.migrations.operations import OperationTarget, SqlOperation
+from snuba.utils.schemas import UUID, Bool, DateTime, Float, Int, Map, String
+
+num_attr_buckets = 40
+storage_set_key = StorageSetKey.EVENTS_ANALYTICS_PLATFORM
+sampling_weights = [8, 8**2, 8**3]
+old_version = 2
+new_version = 3
+
+columns: List[Column[Modifiers]] = [
+    Column("organization_id", UInt(64)),
+    Column("project_id", UInt(64)),
+    Column("item_type", UInt(8)),
+    Column("timestamp", DateTime(Modifiers(codecs=["DoubleDelta", "ZSTD(1)"]))),
+    Column("trace_id", UUID()),
+    Column("item_id", UInt(128)),
+    Column("sampling_weight", UInt(64, modifiers=Modifiers(codecs=["ZSTD(1)"]))),
+    Column("sampling_factor", Float(64, modifiers=Modifiers(codecs=["ZSTD(1)"]))),
+    Column(
+        "retention_days",
+        UInt(16, modifiers=Modifiers(codecs=["T64", "ZSTD(1)"])),
+    ),
+    Column(
+        "attributes_bool",
+        Map(
+            String(),
+            Bool(),
+        ),
+    ),
+    Column(
+        "attributes_int",
+        Map(
+            String(),
+            Int(64),
+        ),
+    ),
+]
+
+
+columns.extend(
+    [
+        Column(
+            f"attributes_string_{i}",
+            Map(
+                String(),
+                String(),
+                modifiers=Modifiers(
+                    codecs=["ZSTD(1)"],
+                ),
+            ),
+        )
+        for i in range(num_attr_buckets)
+    ]
+)
+
+columns.extend(
+    [
+        Column(
+            f"attributes_float_{i}",
+            Map(
+                String(),
+                Float(64),
+                modifiers=Modifiers(
+                    codecs=["ZSTD(1)"],
+                ),
+            ),
+        )
+        for i in range(num_attr_buckets)
+    ]
+)
+
+
+def generate_old_materialized_view_expression(sampling_weight: int) -> str:
+    column_names_str = ", ".join(
+        [
+            c.name
+            for c in columns
+            if c.name != "sampling_weight" and c.name != "sampling_factor"
+        ]
+    )
+    return f"SELECT {column_names_str}, sampling_weight * {sampling_weight} AS sampling_weight, sampling_factor / {sampling_weight} AS sampling_factor FROM eap_items_1_local WHERE (cityHash64(item_id + {sampling_weight})  % {sampling_weight}) = 0"
+
+
+def generate_new_materialized_view_expression(sampling_weight: int) -> str:
+    column_names_str = ", ".join(
+        [
+            c.name
+            for c in columns
+            if c.name != "sampling_weight"
+            and c.name != "sampling_factor"
+            and c.name != "retention_days"
+        ]
+    )
+    return f"SELECT {column_names_str}, downsampled_retention_days AS retention_days, sampling_weight * {sampling_weight} AS sampling_weight, sampling_factor / {sampling_weight} AS sampling_factor FROM eap_items_1_local WHERE (cityHash64(item_id + {sampling_weight})  % {sampling_weight}) = 0"
+
+
+class Migration(migration.ClickhouseNodeMigration):
+    blocking = False
+
+    def forwards_ops(self) -> Sequence[SqlOperation]:
+        ops: List[SqlOperation] = []
+
+        for sampling_weight in sampling_weights:
+            local_table_name = f"eap_items_1_downsample_{sampling_weight}_local"
+
+            ops.extend(
+                [
+                    operations.CreateMaterializedView(
+                        storage_set=storage_set_key,
+                        view_name=f"eap_items_1_downsample_{sampling_weight}_mv_{new_version}",
+                        columns=columns,
+                        destination_table_name=local_table_name,
+                        target=OperationTarget.LOCAL,
+                        query=generate_new_materialized_view_expression(
+                            sampling_weight
+                        ),
+                    ),
+                    operations.DropTable(
+                        storage_set=storage_set_key,
+                        table_name=f"eap_items_1_downsample_{sampling_weight}_mv_{old_version}",
+                        target=OperationTarget.LOCAL,
+                    ),
+                ]
+            )
+
+        return ops
+
+    def backwards_ops(self) -> Sequence[SqlOperation]:
+        ops: List[SqlOperation] = []
+
+        for sampling_weight in sampling_weights:
+            local_table_name = f"eap_items_1_downsample_{sampling_weight}_local"
+
+            ops.extend(
+                [
+                    operations.CreateMaterializedView(
+                        storage_set=storage_set_key,
+                        view_name=f"eap_items_1_downsample_{sampling_weight}_mv_{old_version}",
+                        columns=columns,
+                        destination_table_name=local_table_name,
+                        target=OperationTarget.LOCAL,
+                        query=generate_old_materialized_view_expression(
+                            sampling_weight
+                        ),
+                    ),
+                    operations.DropTable(
+                        storage_set=storage_set_key,
+                        table_name=f"eap_items_1_downsample_{sampling_weight}_mv_{new_version}",
+                        target=OperationTarget.LOCAL,
+                    ),
+                ]
+            )
+
+        return ops
