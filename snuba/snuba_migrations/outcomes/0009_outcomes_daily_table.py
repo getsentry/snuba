9088d752af92b0a7f7949b77071ee90c03dd9d83
Meredith Heller <meredith@sentry.io>
2025-08-14 11:03:00 -0700

    ref(outcomes): add daily local and mv tables

---
+++ snuba/snuba_migrations/outcomes/0009_outcomes_daily_table.py
@@ -0,0 +1,101 @@
+from typing import Sequence
+
+from snuba.clickhouse.columns import Column, DateTime, String, UInt
+from snuba.clusters.storage_sets import StorageSetKey
+from snuba.migrations import migration, operations, table_engines
+from snuba.migrations.columns import MigrationModifiers as Modifiers
+
+daily_columns: Sequence[Column[Modifiers]] = [
+    Column("org_id", UInt(64)),
+    Column("project_id", UInt(64)),
+    Column("key_id", UInt(64)),
+    Column("timestamp", DateTime()),
+    Column("category", UInt(8)),
+    Column("outcome", UInt(8)),
+    Column("reason", String(Modifiers(low_cardinality=True))),
+    Column("quantity", UInt(64)),
+    Column("times_seen", UInt(64)),
+]
+
+materialized_view_columns: Sequence[Column[Modifiers]] = [
+    Column("org_id", UInt(64)),
+    Column("project_id", UInt(64)),
+    Column("key_id", UInt(64)),
+    Column("timestamp", DateTime()),
+    Column("category", UInt(8)),
+    Column("outcome", UInt(8)),
+    Column("reason", String()),
+    Column("quantity", UInt(64)),
+    Column("times_seen", UInt(64)),
+]
+
+
+class Migration(migration.ClickhouseNodeMigrationLegacy):
+    blocking = False
+
+    def forwards_local(self) -> Sequence[operations.SqlOperation]:
+        return [
+            operations.CreateTable(
+                storage_set=StorageSetKey.OUTCOMES,
+                table_name="outcomes_daily_local",
+                columns=daily_columns,
+                engine=table_engines.SummingMergeTree(
+                    storage_set=StorageSetKey.OUTCOMES,
+                    order_by="(org_id, project_id, key_id, outcome, reason, timestamp, category)",
+                    partition_by="(toMonth(timestamp))",
+                    ttl="timestamp + toIntervalMonth(13)",
+                ),
+            ),
+            operations.CreateMaterializedView(
+                storage_set=StorageSetKey.OUTCOMES,
+                view_name="outcomes_mv_daily_local",
+                destination_table_name="outcomes_daily_local",
+                columns=materialized_view_columns,
+                query="""
+                    SELECT
+                        org_id,
+                        project_id,
+                        ifNull(key_id, 0) AS key_id,
+                        toStartOfDay(timestamp) AS timestamp,
+                        outcome,
+                        ifNull(reason, 'none') AS reason,
+                        category,
+                        count() AS times_seen,
+                        sum(quantity) AS quantity
+                    FROM outcomes_raw_local
+                    GROUP BY org_id, project_id, key_id, timestamp, outcome, reason, category
+                """,
+            ),
+        ]
+
+    def backwards_local(self) -> Sequence[operations.SqlOperation]:
+        return [
+            operations.DropTable(
+                storage_set=StorageSetKey.OUTCOMES,
+                table_name="outcomes_mv_daily_local",
+            ),
+            operations.DropTable(
+                storage_set=StorageSetKey.OUTCOMES,
+                table_name="outcomes_daily_local",
+            ),
+        ]
+
+    def forwards_dist(self) -> Sequence[operations.SqlOperation]:
+        return [
+            operations.CreateTable(
+                storage_set=StorageSetKey.OUTCOMES,
+                table_name="outcomes_daily_dist",
+                columns=daily_columns,
+                engine=table_engines.Distributed(
+                    local_table_name="outcomes_daily_local",
+                    sharding_key="org_id",
+                ),
+            ),
+        ]
+
+    def backwards_dist(self) -> Sequence[operations.SqlOperation]:
+        return [
+            operations.DropTable(
+                storage_set=StorageSetKey.OUTCOMES, table_name="outcomes_daily_dist"
+            ),
+        ]
