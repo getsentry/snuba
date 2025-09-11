3a33200fe56d0628a67929a2ececa16bdb3a12cd
Rachel Chen <rachelchen@PL6VFX9HP4.local>
2025-09-11 10:01:35 -0700

    tests

--- snuba/admin/static/tests/capacity_management/table_color.spec.tsx
+++ snuba/admin/static/tests/capacity_management/table_color.spec.tsx
@@ -1,6 +1,7 @@
-import {getTableColor} from "SnubaAdmin/capacity_management/allocation_policy";
+import { getTableColor } from "SnubaAdmin/capacity_management/allocation_policy";
 import { COLORS } from "SnubaAdmin/theme";
 import { it, expect } from "@jest/globals";
+import { ConfigurableComponentData } from "SnubaAdmin/capacity_management/types";
 
 let bonus_configs = [{
       name: "key1",
@@ -19,65 +20,81 @@ let bonus_configs = [{
 ]
 
 
-
 it("should be gray when the policy is inactive", () => {
-  let configs = [
-    {
-      name: "is_active",
-      value: "0",
-      description: "",
-      type: "int",
-      params: {},
-    },
-    {
-      name: "is_enforced",
-      value: "1",
-      description: "",
-      type: "int",
-      params: {},
-    },
-  ].concat(bonus_configs);
-  expect(getTableColor(configs)).toBe("gray")
+  let configurableComponentData: ConfigurableComponentData = {
+    configurable_component_namespace: "some_namespace",
+    configurable_component_class_name: "some_policy",
+    resource_identifier: "some_resource",
+    configurations: [
+      {
+        name: "is_active",
+        value: "0",
+        description: "",
+        type: "int",
+        params: {},
+      },
+      {
+        name: "is_enforced",
+        value: "1",
+        description: "",
+        type: "int",
+        params: {},
+      }
+    ].concat(bonus_configs),
+    optional_config_definitions: [],
+  };
+    expect(getTableColor(configurableComponentData)).toBe(COLORS.SNUBA_BLUE);
 })
 
-
 it("should be blue when the policy is active and enforced", () => {
-  let configs = [
-    {
-      name: "is_active",
-      value: "1",
-      description: "",
-      type: "int",
-      params: {},
-    },
-    {
-      name: "is_enforced",
-      value: "1",
-      description: "",
-      type: "int",
-      params: {},
-    }
-  ].concat(bonus_configs);
-  expect(getTableColor(configs)).toBe(COLORS.SNUBA_BLUE);
+  let configurableComponentData: ConfigurableComponentData = {
+    configurable_component_namespace: "some_namespace",
+    configurable_component_class_name: "some_policy",
+    resource_identifier: "some_resource",
+    configurations: [
+      {
+        name: "is_active",
+        value: "1",
+        description: "",
+        type: "int",
+        params: {},
+      },
+      {
+        name: "is_enforced",
+        value: "1",
+        description: "",
+        type: "int",
+        params: {},
+      }
+    ].concat(bonus_configs),
+    optional_config_definitions: [],
+  };
+    expect(getTableColor(configurableComponentData)).toBe(COLORS.SNUBA_BLUE);
 })
 
 
 it("should be orange when the policy is active and enforced", () => {
-  let configs = [
-    {
-      name: "is_active",
-      value: "1",
-      description: "",
-      type: "int",
-      params: {},
-    },
-    {
-      name: "is_enforced",
-      value: "0",
-      description: "",
-      type: "int",
-      params: {},
-    }
-  ].concat(bonus_configs);
-  expect(getTableColor(configs)).toBe("orange")
+  let configurableComponentData: ConfigurableComponentData = {
+    configurable_component_namespace: "some_namespace",
+    configurable_component_class_name: "some_policy",
+    resource_identifier: "some_resource",
+    configurations: [
+      {
+        name: "is_active",
+        value: "1",
+        description: "",
+        type: "int",
+        params: {},
+      },
+      {
+        name: "is_enforced",
+        value: "0",
+        description: "",
+        type: "int",
+        params: {},
+      }
+    ].concat(bonus_configs),
+    optional_config_definitions: [],
+  };
+    expect(getTableColor(configurableComponentData)).toBe(COLORS.SNUBA_BLUE);
 })
