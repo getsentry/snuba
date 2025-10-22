99d64c30d632d2d4cad92f1bee3e1e404f79c19f
David Tsukernik <david.tsukernik@sentry.io>
2025-10-22 15:34:00 -0700

    fix test

--- tests/web/rpc/v1/test_endpoint_delete_trace_items.py
+++ tests/web/rpc/v1/test_endpoint_delete_trace_items.py
@@ -10,13 +10,7 @@ from sentry_protos.snuba.v1.endpoint_delete_trace_items_pb2 import (
     DeleteTraceItemsRequest,
     DeleteTraceItemsResponse,
 )
-from sentry_protos.snuba.v1.request_common_pb2 import (
-    QueryInfo,
-    QueryMetadata,
-    QueryStats,
-    RequestMeta,
-    ResponseMeta,
-)
+from sentry_protos.snuba.v1.request_common_pb2 import RequestMeta, ResponseMeta
 from sentry_protos.snuba.v1.trace_item_pb2 import AnyValue
 
 from snuba.datasets.storages.factory import get_storage
@@ -105,18 +99,7 @@ class TestEndpointDeleteTrace(BaseApiTest):
         response = EndpointDeleteTraceItems().execute(message)
 
         expected_response = DeleteTraceItemsResponse(
-            meta=ResponseMeta(
-                request_id=_REQUEST_ID,
-                query_info=[
-                    QueryInfo(
-                        stats=QueryStats(
-                            progress_bytes=response.meta.query_info[0].stats.progress_bytes
-                        ),
-                        metadata=QueryMetadata(),
-                        trace_logs="",
-                    )
-                ],
-            ),
+            meta=ResponseMeta(request_id=_REQUEST_ID),
             matching_items_count=_SPAN_COUNT,
         )
 
