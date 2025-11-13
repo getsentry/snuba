30317103b05464d0d2e2826290877212bf242322
Reinaldy Rafli <github@reinaldyrafli.com>
2025-11-13 10:07:24 +0700

    fix: lowercase variables & set default value on env invocation

--- snuba/utils/metrics/addr_config.py
+++ snuba/utils/metrics/addr_config.py
@@ -4,15 +4,15 @@ from typing import Optional
 
 def get_statsd_addr() -> tuple[Optional[str], Optional[int]]:
     """Returns the address of the StatsD server."""
-    SNUBA_STATSD_ADDR = os.environ.get("SNUBA_STATSD_ADDR")
-    if SNUBA_STATSD_ADDR:
-        ip, separator, port = SNUBA_STATSD_ADDR.rpartition(":")
+    snuba_statsd_address = os.environ.get("SNUBA_STATSD_ADDR")
+    if snuba_statsd_address:
+        ip, separator, port = snuba_statsd_address.rpartition(":")
         assert separator == ":", "SNUBA_STATSD_ADDR must be in the format of ip:port"
         return ip, int(port or 8125)
 
-    DOGSTATSD_HOST = os.environ.get("DOGSTATSD_HOST")
-    DOGSTATSD_PORT = os.environ.get("DOGSTATSD_PORT")
-    if DOGSTATSD_HOST and DOGSTATSD_PORT:
-        return DOGSTATSD_HOST, int(DOGSTATSD_PORT or 8125)
+    dogstatsd_host = os.environ.get("DOGSTATSD_HOST")
+    dogstatsd_port = os.environ.get("DOGSTATSD_PORT", "8125")
+    if dogstatsd_host and dogstatsd_port:
+        return dogstatsd_host, int(dogstatsd_port)
 
     return None, None
