b8b8d89616b03adfa4877f5c487dd5875e1af9ec
Hubert Deng <hubert.deng@sentry.io>
2025-12-01 14:14:17 -0800

    feat(self-hosted): Manually run post release script (#7563)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,7 +7,7 @@ project = "Snuba"
 copyright = "2021, Sentry Team and Contributors"
 author = "Sentry Team and Contributors"
 
-release = "25.11.1"
+release = "25.12.0.dev0"
 
 
 # -- General configuration ---------------------------------------------------
