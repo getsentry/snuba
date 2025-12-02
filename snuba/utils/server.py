86376c0d89251fb8bb1060155bc68449f2f9e876
Giovanni Barillari <giovanni.barillari@sentry.io>
2025-12-02 14:41:33 +0100

    prodeng-605: migrate uwsgi to granian

---
+++ snuba/utils/server.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+from typing import NoReturn
+
+from granian import Granian
+from granian.constants import Interfaces
+
+
+def serve(
+    module: str,
+    bind: str,
+    processes: int = 1,
+    threads: int = 1,
+    reload: bool = False,
+    name: str | None = None
+) -> NoReturn:
+    host, port = bind.rsplit(":", maxsplit=1)
+    server = Granian(
+        target=module,
+        address=host,
+        port=port,
+        interface=Interfaces.WSGI,
+        workers=processes,
+        workers_kill_timeout=30,
+        blocking_threads=threads,
+        respawn_failed_workers=True,
+        reload=reload,
+        process_name=name,
+    )
+    server.serve()
