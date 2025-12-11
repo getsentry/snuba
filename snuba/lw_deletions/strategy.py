53a4c5899684397b9c801fe8d4309f7a0bd3e6e0
Oliver Newland <onewland@gmail.com>
2025-12-11 15:58:54 -0800

    feat(deletes): add lw delete killswitch

--- snuba/lw_deletions/strategy.py
+++ snuba/lw_deletions/strategy.py
@@ -64,7 +64,7 @@ class FormatQuery(ProcessingStrategy[ValuesBatch[KafkaPayload]]):
         self.__next_step.poll()
 
     def submit(self, message: Message[ValuesBatch[KafkaPayload]]) -> None:
-        if get_int_config("killswitch_lw_deletes") is not None:
+        if get_int_config("killswitch_lw_deletes", 0):
             raise MessageRejected("LW deletes killswitch is enabled")
 
         decode_messages = [rapidjson.loads(m.payload.value) for m in message.value.payload]
