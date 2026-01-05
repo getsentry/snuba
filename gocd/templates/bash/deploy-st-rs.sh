7a40671b58db0ce6617fcfbd819679677998ce60
Oliver Newland <onewland@gmail.com>
2026-01-05 15:05:09 -0800

    Revert "feat(gocd): make ST deploys actually split rs/py (#7606)"
    This reverts commit 8ead4598aaa96332c23af8ec17e1d59a406bba4e.

--- gocd/templates/bash/deploy-st-rs.sh
+++ gocd/templates/bash/deploy-st-rs.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 
-eval $(regions-project-env-vars --region="${SENTRY_REGION}")
-
-/devinfra/scripts/get-cluster-credentials \
-&& k8s-deploy \
-  --label-selector="${LABEL_SELECTOR}" \
-  --image="us-docker.pkg.dev/sentryio/snuba-mr/image:${GO_REVISION_SNUBA_REPO}" \
-  --container-name="consumer" \
-  --container-name="eap-items-consumer" \
-  --container-name="generic-metrics-counters-consumer" \
-  --container-name="generic-metrics-distributions-consumer" \
-  --container-name="generic-metrics-sets-consumer" \
-  --container-name="loadbalancer-outcomes-consumer" \
-  --container-name="metrics-consumer" \
-  --container-name="outcomes-billing-consumer" \
-  --container-name="outcomes-consumer" \
-  --container-name="profile-chunks-consumer" \
-  --container-name="profiles-consumer" \
-  --container-name="profiling-functions-consumer" \
-  --container-name="querylog-consumer" \
-  --container-name="replays-consumer" \
-  --container-name="transactions-consumer"
+echo "TODO fix"
+echo "This is a no-op for single-tenants. deploy-snuba-py handles everything"
+echo "(and traffic is low enough that we don't have rebalance issues)"
