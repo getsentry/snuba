df4d26b848f8f892ae9d9c14a8bf3032a77a97ee
Meredith Heller <meredith@sentry.io>
2026-01-22 11:31:47 -0800

    ref(INC-1584): job to delete event data by a tag,value pair

---
+++ snuba/manual_jobs/delete_events_by_tag_key_value.py
@@ -0,0 +1,47 @@
+from typing import Any, Mapping, Optional
+
+from snuba.clusters.cluster import ClickhouseClientSettings, get_cluster
+from snuba.clusters.storage_sets import StorageSetKey
+from snuba.manual_jobs import Job, JobLogger, JobSpec
+
+
+class DeleteEventsByTagKeyValue(Job):
+    def __init__(self, job_spec: JobSpec) -> None:
+        self.__validate_job_params(job_spec.params)
+        super().__init__(job_spec)
+
+    def __validate_job_params(self, params: Optional[Mapping[Any, Any]]) -> None:
+        assert params
+        assert isinstance(params["project_ids"], list)
+        assert len(params["project_ids"]) > 0
+        assert isinstance(params["tag_key"], str)
+        assert isinstance(params["tag_value"], str)
+        assert params["tag_key"] and params["tag_value"]
+        assert all([isinstance(p, int) for p in params["project_ids"]])
+        self._project_ids = params["project_ids"]
+        self._tag_key = params["tag_key"]
+        self._tag_value = params["tag_value"]
+
+    def _get_query(self, cluster_name: str | None) -> str:
+        project_ids = ",".join([str(p) for p in self._project_ids])
+        key = self._tag_key
+        value = self._tag_value
+        on_cluster = f"ON CLUSTER '{cluster_name}'" if cluster_name else ""
+        return f"""DELETE FROM errors_local {on_cluster} WHERE project_id IN [{project_ids}] AND has(tags.key, '{key}') AND has(tags.value, '{value}')"""
+
+    def execute(self, logger: JobLogger) -> None:
+        cluster = get_cluster(StorageSetKey.EVENTS)
+        storage_node = cluster.get_local_nodes()[0]
+        connection = cluster.get_node_connection(ClickhouseClientSettings.CLEANUP, storage_node)
+        if not cluster.is_single_node():
+            cluster_name = cluster.get_clickhouse_cluster_name()
+        else:
+            cluster_name = None
+        query = self._get_query(cluster_name)
+        logger.info("Executing query: {query}")
+        result = connection.execute(
+            query=query, settings={"mutations_sync": 0, "lightweight_deletes_sync": 0}
+        )
+
+        logger.info("complete")
+        logger.info(repr(result))
