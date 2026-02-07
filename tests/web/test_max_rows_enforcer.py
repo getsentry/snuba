acd4818e3266a04f65aad35c7f9607cdc8693acd
Meredith Heller <meredith@sentry.io>
2026-02-06 16:18:31 -0800

    fix(deletes): correct place for enforce_max_rows_to_delete

--- tests/web/test_max_rows_enforcer.py
+++ tests/web/test_max_rows_enforcer.py
@@ -1,6 +1,5 @@
-import uuid
 from datetime import datetime
-from typing import Any, Callable, MutableMapping
+from typing import Any, Callable
 from unittest import mock
 
 import pytest
@@ -19,30 +18,28 @@ from snuba.state import set_config
 from snuba.web.delete_query import _enforce_max_rows
 from tests.base import BaseApiTest
 from tests.datasets.configuration.utils import ConfigurationTest
-from tests.helpers import write_unprocessed_events
 from tests.test_api import SimpleAPITest
+from tests.web.rpc.v1.test_utils import write_eap_item
 
 
 class TestMaxRowsEnforcer(SimpleAPITest, BaseApiTest, ConfigurationTest):
     def setup_method(self, test_method: Callable[..., Any]) -> None:
         super().setup_method(test_method)
         initialize_snuba()
-        self.events_storage = get_entity(EntityKey.SEARCH_ISSUES).get_writable_storage()
+        self.events_storage = get_entity(EntityKey.EAP_ITEMS).get_writable_storage()
         assert self.events_storage is not None
 
         from_clause = Table(
-            "search_issues_local_v2",
+            "eap_items_1_local",
             ColumnSet([]),
-            storage_key=StorageKey.SEARCH_ISSUES,
-            # TODO: add allocation policies
+            storage_key=StorageKey.EAP_ITEMS,
             allocation_policies=[],
         )
-        self.occurrence_id = str(uuid.uuid4())
         self.query = Query(
             from_clause=from_clause,
             condition=and_cond(
-                equals(column("occurrence_id"), literal(self.occurrence_id)),
-                equals(column("project_id"), literal(3)),
+                equals(column("organization_id"), literal(1)),
+                equals(column("project_id"), literal(1)),
             ),
             on_cluster=None,
             is_delete=True,
@@ -52,45 +49,43 @@ class TestMaxRowsEnforcer(SimpleAPITest, BaseApiTest, ConfigurationTest):
         set_config("read_through_cache.short_circuit", 1)
         now = datetime.now().replace(minute=0, second=0, microsecond=0)
 
-        evt: MutableMapping[str, Any] = dict(
-            organization_id=1,
-            project_id=3,
-            event_id=str(uuid.uuid4().hex),
-            group_id=3,
-            primary_hash=str(uuid.uuid4().hex),
-            datetime=datetime.utcnow().isoformat() + "Z",
-            platform="other",
-            message="message",
-            data={"received": now.timestamp()},
-            occurrence_data=dict(
-                id=self.occurrence_id,
-                type=1,
-                issue_title="search me",
-                fingerprint=["one", "two"],
-                detection_time=now.timestamp(),
-            ),
-            retention_days=90,
+        write_eap_item(
+            start_timestamp=now,
+            raw_attributes={"test_attribute": "test_value"},
+            count=1,
         )
 
-        assert self.events_storage
-        write_unprocessed_events(self.events_storage, [evt])
-
-    @pytest.mark.clickhouse_db
+    @pytest.mark.eap
     def test_max_row_enforcer_passes(self) -> None:
         self._insert_event()
         _enforce_max_rows(self.query)
 
-    @pytest.mark.clickhouse_db
+    @pytest.mark.eap
     @mock.patch(
         "snuba.datasets.storage.ReadableTableStorage.get_deletion_settings",
         return_value=DeletionSettings(
             is_enabled=1,
-            tables=["search_issues_local_v2"],
+            tables=["eap_items_1_local"],
             max_rows_to_delete=0,
-            allowed_columns=["project_id", "group_id"],
+            allowed_columns=["project_id", "organization_id"],
         ),
     )
     def test_max_row_enforcer_rejects(self, mock: mock.MagicMock) -> None:
         self._insert_event()
         with pytest.raises(TooManyDeleteRowsException):
             _enforce_max_rows(self.query)
+
+    @pytest.mark.eap
+    @mock.patch(
+        "snuba.datasets.storage.ReadableTableStorage.get_deletion_settings",
+        return_value=DeletionSettings(
+            is_enabled=1,
+            tables=["eap_items_1_local"],
+            max_rows_to_delete=0,
+            allowed_columns=["project_id", "organization_id"],
+        ),
+    )
+    def test_bypass_enforce_max_rows(self, mock: mock.MagicMock) -> None:
+        set_config("enforce_max_rows_to_delete", 0)
+        self._insert_event()
+        _enforce_max_rows(self.query)
