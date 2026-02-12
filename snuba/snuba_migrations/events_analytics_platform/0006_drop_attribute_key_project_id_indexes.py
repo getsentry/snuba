9a2b5fe423fbafa52c62aee84c983cd9e3c59de0
Meredith Heller <meredith@sentry.io>
2026-02-12 13:29:50 -0800

    ref(migrations): squash some eap migrations

--- snuba/snuba_migrations/events_analytics_platform/0006_drop_attribute_key_project_id_indexes.py
+++ snuba/snuba_migrations/events_analytics_platform/0006_drop_attribute_key_project_id_indexes.py
@@ -1,14 +1,5 @@
-from typing import Sequence
-
 from snuba.migrations import migration
-from snuba.migrations.operations import SqlOperation
 
 
-class Migration(migration.ClickhouseNodeMigration):
-    blocking = False
-
-    def forwards_ops(self) -> Sequence[SqlOperation]:
-        return []
-
-    def backwards_ops(self) -> Sequence[SqlOperation]:
-        return []
+class Migration(migration.SquashedMigration):
+    pass
--- snuba/snuba_migrations/events_analytics_platform/0043_remove_hashed_keys_column.py
+++ snuba/snuba_migrations/events_analytics_platform/0043_remove_hashed_keys_column.py
@@ -1,30 +1,5 @@
-from snuba.clusters.storage_sets import StorageSetKey
-from snuba.migrations import migration, operations
-from snuba.migrations.operations import OperationTarget
+from snuba.migrations import migration
 
 
-class Migration(migration.ClickhouseNodeMigration):
-    blocking = False
-    storage_set_key = StorageSetKey.EVENTS_ANALYTICS_PLATFORM
-    local_table_name = "eap_items_1_local"
-    dist_table_name = "eap_items_1_dist"
-    column_to_remove = "hashed_keys"
-
-    def forwards_ops(self) -> list[operations.SqlOperation]:
-        return [
-            operations.DropColumn(
-                storage_set=self.storage_set_key,
-                table_name=self.dist_table_name,
-                column_name=self.column_to_remove,
-                target=OperationTarget.DISTRIBUTED,
-            ),
-            operations.DropColumn(
-                storage_set=self.storage_set_key,
-                table_name=self.local_table_name,
-                column_name=self.column_to_remove,
-                target=OperationTarget.LOCAL,
-            ),
-        ]
-
-    def backwards_ops(self) -> list[operations.SqlOperation]:
-        return []
+class Migration(migration.SquashedMigration):
+    pass
