d833c5cbcc8d764d816346a5829d9e750bf98346
Meredith Heller <meredith@sentry.io>
2026-02-12 13:29:50 -0800

    ref(migrations): squash some eap migrations

--- snuba/snuba_migrations/events_analytics_platform/0003_eap_spans_project_id_index.py
+++ snuba/snuba_migrations/events_analytics_platform/0003_eap_spans_project_id_index.py
@@ -1,31 +1,5 @@
-from typing import Sequence
-
-from snuba.clusters.storage_sets import StorageSetKey
-from snuba.migrations import migration, operations
+from snuba.migrations import migration
 
 
-class Migration(migration.ClickhouseNodeMigration):
-    blocking = False
-
-    def forwards_ops(self) -> Sequence[operations.SqlOperation]:
-        return [
-            operations.AddIndex(
-                storage_set=StorageSetKey.EVENTS_ANALYTICS_PLATFORM,
-                table_name="eap_spans_local",
-                index_name="bf_project_id",
-                index_expression="project_id",
-                index_type="bloom_filter",
-                granularity=1,
-                target=operations.OperationTarget.LOCAL,
-            ),
-        ]
-
-    def backwards_ops(self) -> Sequence[operations.SqlOperation]:
-        return [
-            operations.DropIndex(
-                storage_set=StorageSetKey.EVENTS_ANALYTICS_PLATFORM,
-                table_name="eap_spans_local",
-                index_name="bf_project_id",
-                target=operations.OperationTarget.LOCAL,
-            ),
-        ]
+class Migration(migration.SquashedMigration):
+    pass
--- snuba/snuba_migrations/events_analytics_platform/0010_drop_indexes_on_attribute_values.py
+++ snuba/snuba_migrations/events_analytics_platform/0010_drop_indexes_on_attribute_values.py
@@ -1,26 +1,5 @@
-from typing import Sequence
-
-from snuba.clusters.storage_sets import StorageSetKey
-from snuba.migrations import migration, operations
+from snuba.migrations import migration
 
 
-class Migration(migration.ClickhouseNodeMigration):
-    blocking = False
-
-    def forwards_ops(self) -> Sequence[operations.SqlOperation]:
-        return [
-            operations.DropIndices(
-                storage_set=StorageSetKey.EVENTS_ANALYTICS_PLATFORM,
-                table_name="eap_spans_local",
-                indices=[
-                    index_name
-                    for bucket in range(0, 20)
-                    for index_name in {f"bf_attr_str_val_{bucket}"}
-                ],
-                target=operations.OperationTarget.LOCAL,
-                run_async=True,
-            )
-        ]
-
-    def backwards_ops(self) -> Sequence[operations.SqlOperation]:
-        return []
+class Migration(migration.SquashedMigration):
+    pass
