a63aa5722a37f992163b9805e92e8042fef7439b
Meredith Heller <meredith@sentry.io>
2026-02-18 16:41:09 -0800

    add this too

--- tests/datasets/test_group_attributes_join.py
+++ tests/datasets/test_group_attributes_join.py
@@ -56,7 +56,7 @@ def _clickhouse_datetime_str(date_time: datetime) -> str:
 
 class TestEventsGroupAttributes(BaseApiTest):
     @pytest.fixture(autouse=True)
-    def setup_fixture(self, clickhouse_db, redis_db):
+    def setup_fixture(self, events_db, redis_db):
         self.app.post = partial(self.app.post, headers={"referer": "test"})
         self.event = get_raw_event()
         self.project_id = self.event["project_id"]
@@ -152,9 +152,7 @@ class TestEventsGroupAttributes(BaseApiTest):
         # make sure we're explicitly applying FINAL when querying on group_attributes table
         # so deduplication happens when we join the entity from events -> group_attributes
         assert right_node.data_source.get_from_clause().final
-        assert right_node.data_source.get_from_clause().table_name.startswith(
-            "group_attributes"
-        )
+        assert right_node.data_source.get_from_clause().table_name.startswith("group_attributes")
 
         return self.app.post(
             "/events/snql",
@@ -229,9 +227,7 @@ class TestEventsGroupAttributes(BaseApiTest):
         # make sure we're explicitly applying FINAL when querying on group_attributes table
         # so deduplication happens when we join the entity from events -> group_attributes
         assert right_node.data_source.get_from_clause().final
-        assert right_node.data_source.get_from_clause().table_name.startswith(
-            "group_attributes"
-        )
+        assert right_node.data_source.get_from_clause().table_name.startswith("group_attributes")
 
         return self.app.post(
             "/events/snql",
@@ -244,7 +240,7 @@ class TestEventsGroupAttributes(BaseApiTest):
             ),
         )
 
-    @pytest.mark.clickhouse_db
+    @pytest.mark.events_db
     @pytest.mark.redis_db
     def test_group_attributes_join(self) -> None:
         response = self.query_events_inner_joined_group_attributes()
@@ -260,9 +256,7 @@ class TestEventsGroupAttributes(BaseApiTest):
                 "g.group_first_seen": _convert_clickhouse_datetime_str(
                     self.initial_group_attributes["group_first_seen"]
                 ),
-                "g.group_num_comments": self.initial_group_attributes[
-                    "group_num_comments"
-                ],
+                "g.group_num_comments": self.initial_group_attributes["group_num_comments"],
                 "g.assignee_user_id": self.initial_group_attributes["assignee_user_id"],
                 "g.assignee_team_id": self.initial_group_attributes["assignee_team_id"],
                 "g.owner_suspect_commit_user_id": self.initial_group_attributes[
@@ -283,7 +277,7 @@ class TestEventsGroupAttributes(BaseApiTest):
             }.items()
         )
 
-    @pytest.mark.clickhouse_db
+    @pytest.mark.events_db
     @pytest.mark.redis_db
     def test_group_attributes_inner_join(self) -> None:
         response = self.query_events_joined_group_attributes()
@@ -299,9 +293,7 @@ class TestEventsGroupAttributes(BaseApiTest):
                 "g.group_first_seen": _convert_clickhouse_datetime_str(
                     self.initial_group_attributes["group_first_seen"]
                 ),
-                "g.group_num_comments": self.initial_group_attributes[
-                    "group_num_comments"
-                ],
+                "g.group_num_comments": self.initial_group_attributes["group_num_comments"],
                 "g.assignee_user_id": self.initial_group_attributes["assignee_user_id"],
                 "g.assignee_team_id": self.initial_group_attributes["assignee_team_id"],
                 "g.owner_suspect_commit_user_id": self.initial_group_attributes[
@@ -322,7 +314,7 @@ class TestEventsGroupAttributes(BaseApiTest):
             }.items()
         )
 
-    @pytest.mark.clickhouse_db
+    @pytest.mark.events_db
     @pytest.mark.redis_db
     def test_group_attributes_join_after_delete(self) -> None:
         delete_row = self.initial_group_attributes.copy()
@@ -356,7 +348,7 @@ class TestEventsGroupAttributes(BaseApiTest):
 
 class TestSearchIssuesGroupAttributes(BaseApiTest):
     @pytest.fixture(autouse=True)
-    def setup_fixture(self, clickhouse_db, redis_db):
+    def setup_fixture(self, events_db, redis_db):
         self.app.post = partial(self.app.post, headers={"referer": "test"})
 
         self.base_time = datetime.utcnow().replace(
@@ -366,9 +358,7 @@ class TestSearchIssuesGroupAttributes(BaseApiTest):
         self.occurrence = self.get_search_issue_occurrence(self.base_time)
         self.project_id = self.occurrence["project_id"]
 
-        self.search_issues_storage = get_entity(
-            EntityKey.SEARCH_ISSUES
-        ).get_writable_storage()
+        self.search_issues_storage = get_entity(EntityKey.SEARCH_ISSUES).get_writable_storage()
         write_unprocessed_events(self.search_issues_storage, [self.occurrence])
 
         self.initial_group_attributes = {
@@ -458,9 +448,7 @@ class TestSearchIssuesGroupAttributes(BaseApiTest):
             assert isinstance(left_node, IndividualNode)
             assert isinstance(left_node.data_source.get_from_clause(), Table)
             assert not left_node.data_source.get_from_clause().final
-            assert left_node.data_source.get_from_clause().table_name.startswith(
-                "search_issues"
-            )
+            assert left_node.data_source.get_from_clause().table_name.startswith("search_issues")
             right_node = clickhouse_query.get_from_clause().right_node
             assert isinstance(right_node.data_source.get_from_clause(), Table)
             # make sure we're explicitly applying FINAL when querying on group_attributes table
@@ -509,17 +497,13 @@ class TestSearchIssuesGroupAttributes(BaseApiTest):
         assert isinstance(left_node, IndividualNode)
         assert isinstance(left_node.data_source.get_from_clause(), Table)
         assert not left_node.data_source.get_from_clause().final
-        assert left_node.data_source.get_from_clause().table_name.startswith(
-            "search_issues"
-        )
+        assert left_node.data_source.get_from_clause().table_name.startswith("search_issues")
         right_node = clickhouse_query.get_from_clause().right_node
         assert isinstance(right_node.data_source.get_from_clause(), Table)
         # make sure we're explicitly applying FINAL when querying on group_attributes table
         # so deduplication happens when we join the entity from events -> group_attributes
         assert right_node.data_source.get_from_clause().final
-        assert right_node.data_source.get_from_clause().table_name.startswith(
-            "group_attributes"
-        )
+        assert right_node.data_source.get_from_clause().table_name.startswith("group_attributes")
 
         return self.app.post(
             "/search_issues/snql",
@@ -532,7 +516,7 @@ class TestSearchIssuesGroupAttributes(BaseApiTest):
             ),
         )
 
-    @pytest.mark.clickhouse_db
+    @pytest.mark.events_db
     @pytest.mark.redis_db
     def test_group_attributes_join(self) -> None:
         response = self.query_search_issues_joined_group_attributes()
@@ -541,18 +525,14 @@ class TestSearchIssuesGroupAttributes(BaseApiTest):
         assert (
             data["data"][0].items()
             == {
-                "s.occurrence_id": uuid.UUID(
-                    self.occurrence["occurrence_data"]["id"]
-                ).hex,
+                "s.occurrence_id": uuid.UUID(self.occurrence["occurrence_data"]["id"]).hex,
                 "g.group_id": self.initial_group_attributes["group_id"],
                 "g.group_status": self.initial_group_attributes["group_status"],
                 "g.group_substatus": self.initial_group_attributes["group_substatus"],
                 "g.group_first_seen": _convert_clickhouse_datetime_str(
                     self.initial_group_attributes["group_first_seen"]
                 ),
-                "g.group_num_comments": self.initial_group_attributes[
-                    "group_num_comments"
-                ],
+                "g.group_num_comments": self.initial_group_attributes["group_num_comments"],
                 "g.assignee_user_id": self.initial_group_attributes["assignee_user_id"],
                 "g.assignee_team_id": self.initial_group_attributes["assignee_team_id"],
                 "g.owner_suspect_commit_user_id": self.initial_group_attributes[
@@ -573,7 +553,7 @@ class TestSearchIssuesGroupAttributes(BaseApiTest):
             }.items()
         )
 
-    @pytest.mark.clickhouse_db
+    @pytest.mark.events_db
     @pytest.mark.redis_db
     def test_group_attributes_join_after_delete(self) -> None:
         delete_row = self.initial_group_attributes.copy()
@@ -585,9 +565,7 @@ class TestSearchIssuesGroupAttributes(BaseApiTest):
         assert (
             data_after["data"][0].items()
             == {
-                "s.occurrence_id": uuid.UUID(
-                    self.occurrence["occurrence_data"]["id"]
-                ).hex,
+                "s.occurrence_id": uuid.UUID(self.occurrence["occurrence_data"]["id"]).hex,
                 # values joined from group_attributes below should be 'null' since we 'deleted' the
                 # existing group_attributes row that joins to the events but the projected values seems to
                 # take on some defaults that are non-null since the columns themselves are not nullable
