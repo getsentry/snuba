4d080b1efcb480cd51068c7728dafc75462a9e09
MeredithAnya <meredith@sentry.io>
2026-06-10 10:02:49 -0700

    ref(outcomes): add quantity_64 column (#8014)
    Adds `quantity_64` so that we can convert the quantity column from
    `UInt32` to `UInt64`. This is because we will need to be able to support
    bigger quantity types (now that we support quantities in terms of bytes,
    not just counts).
    
    After this PR we can add the logic to update the consumer to start
    writing to the new column, and to also change the materialized views for
    the hourly and daily table.

---
+++ snuba/snuba_migrations/outcomes/0011_add_quantity64.py
@@ -0,0 +1,50 @@
+from typing import Sequence
+
+from snuba.clusters.storage_sets import StorageSetKey
+from snuba.migrations.migration import ClickhouseNodeMigration
+from snuba.migrations.operations import (
+    AddColumn,
+    DropColumn,
+    OperationTarget,
+    SqlOperation,
+)
+from snuba.utils import schemas
+from snuba.utils.schemas import Column
+
+
+class Migration(ClickhouseNodeMigration):
+    blocking = False
+
+    def forwards_ops(self) -> Sequence[SqlOperation]:
+        return [
+            AddColumn(
+                storage_set=StorageSetKey.OUTCOMES,
+                table_name="outcomes_raw_local",
+                column=Column("quantity64", schemas.UInt(64, modifiers=None)),
+                after="quantity",
+                target=OperationTarget.LOCAL,
+            ),
+            AddColumn(
+                storage_set=StorageSetKey.OUTCOMES,
+                table_name="outcomes_raw_dist",
+                column=Column("quantity64", schemas.UInt(64, modifiers=None)),
+                after="quantity",
+                target=OperationTarget.DISTRIBUTED,
+            ),
+        ]
+
+    def backwards_ops(self) -> Sequence[SqlOperation]:
+        return [
+            DropColumn(
+                storage_set=StorageSetKey.OUTCOMES,
+                table_name="outcomes_raw_dist",
+                column_name="quantity64",
+                target=OperationTarget.DISTRIBUTED,
+            ),
+            DropColumn(
+                storage_set=StorageSetKey.OUTCOMES,
+                table_name="outcomes_raw_local",
+                column_name="quantity64",
+                target=OperationTarget.LOCAL,
+            ),
+        ]
