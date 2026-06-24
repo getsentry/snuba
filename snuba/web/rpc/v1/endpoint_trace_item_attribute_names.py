a5a6fcf96324d66df0d1f09a5782d4b6a474a4a7
William Mak <william@wmak.io>
2026-06-24 16:46:18 -0400

    feat(attributes): Add filter mode
    - This adds support for filter mode to the trace item attributes
      endpoint so we can change how we filter between hasAny or hasAll

--- snuba/web/rpc/v1/endpoint_trace_item_attribute_names.py
+++ snuba/web/rpc/v1/endpoint_trace_item_attribute_names.py
@@ -242,9 +242,13 @@ def get_co_occurring_attributes(
     )
 
     if attribute_keys_to_search:
+        if request.filter_mode == TraceItemAttributeNamesRequest.FilterMode.FILTER_MODE_MATCH_ANY:
+            match_function = f.hasAny
+        else:
+            match_function = f.hasAll
         condition = and_cond(
             condition,
-            f.hasAll(
+            match_function(
                 column("attribute_keys_hash"),
                 f.array(*[f.cityHash64(k) for k in attribute_keys_to_search]),
             ),
