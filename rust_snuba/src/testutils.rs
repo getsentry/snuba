651cfc2c26d0a82f21b51fddb251e707f77a4fad
tomaioo <hongsaygio.com@gmail.com>
2026-07-04 11:28:28 -0700

    fix: address review feedback — This function doesn't exist in py3 0.24.
    Signed-off-by: tomaioo <203048277+tomaioo@users.noreply.github.com>

--- rust_snuba/src/testutils.rs
+++ rust_snuba/src/testutils.rs
@@ -20,8 +20,9 @@ pub fn initialize_python() {
     let python_path: Vec<_> = python_path.split(':').map(String::from).collect();
 
     Python::with_gil(|py| -> PyResult<()> {
-        PyModule::import(py, "sys")?.setattr("executable", python_executable)?;
-        PyModule::import(py, "sys")?.setattr("path", python_path)?;
+        let sys = PyModule::import(py, "sys")?;
+        sys.setattr("executable", python_executable)?;
+        sys.setattr("path", python_path)?;
 
         // monkeypatch signal handlers in Python to noop, because otherwise python multiprocessing
         // strategies cannot be tested
