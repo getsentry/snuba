0cce9388a34a64f34fe137681fb59e8eef0995a7
Meredith Heller <meredith@sentry.io>
2026-07-24 16:27:57 -0700

    one day

--- rust_snuba/src/processors/eap_items.rs
+++ rust_snuba/src/processors/eap_items.rs
@@ -66,7 +66,7 @@ fn process_eap_item(msg: KafkaPayload, config: &ProcessorConfig) -> anyhow::Resu
         let now = Utc::now();
 
         if let Some(grace_min) = get_dlq_grace_period_min(&config.storage_name) {
-            let is_future = event_ts > now + chrono::Duration::days(5);
+            let is_future = event_ts > now + chrono::Duration::days(1);
             if should_dlq_for_prior_partition(event_ts, now, grace_min) || is_future {
                 let item_type_str = item_type_name(item_type);
                 let reason = if is_future {
