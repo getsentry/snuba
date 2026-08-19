54145b22cb2fe63b3c6817b2fcf7590d3796ee5b
tryangul <11639460+tryangul@users.noreply.github.com>
2026-08-19 16:18:29 -0700

    Add GoCD container names

--- gocd/templates/bash/deploy-rs.sh
+++ gocd/templates/bash/deploy-rs.sh
@@ -25,8 +25,5 @@ IMAGE_TAG="${GO_REVISION_SNUBA_REPO}"
   --container-name="querylog-consumer" \
   --container-name="replays-consumer" \
   --container-name="transactions-consumer-new" \
-&& k8s-deploy \
-  --label-selector="${LABEL_SELECTOR}" \
-  --image="us-docker.pkg.dev/sentryio/snuba-mr/image:${IMAGE_TAG}" \
-  --type="statefulset" \
-  --container-name="eap-items-sts-consumer"
+  --container-name="hackweek-pull-outcomes-consumer" \
+  --container-name="hackweek-pull-eap-items-consumer"
